f5550c216b53a4fedc6b431adcb49a0d594afbf5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-25 23:13:04 +0000

    blush: add 0.6.0 bottle.

--- Formula/b/blush.rb
+++ Formula/b/blush.rb
@@ -6,6 +6,14 @@ class Blush < Formula
   license "MIT"
   head "https://github.com/arsham/blush.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e4965bcf1d5c76f970c887b6ceb0b3ad781a96da41aa578d2216cd24ff65275"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3f5239d798d04c3f2f38363014e313ab070b30aeca154dee51dfda0a5760e436"
+    sha256 cellar: :any_skip_relocation, ventura:       "bc466066f6da541f0a8873fd5e5f82ab1dbd70d450fb3ad9d81b9dae4a5e0be0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed0d94801b9e22702add2c7618aa2de11350f719725ae462e53ae8c56f64359d"
+  end
+
   depends_on "go" => :build
 
   def install
