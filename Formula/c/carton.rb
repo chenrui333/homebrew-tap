e2914f05b810afe0aa47fe5b61562f0930883a73
Rui Chen <rui@chenrui.dev>
2025-01-25 22:31:49 +0000

    carton: add 1.1.3_1 bottle.

--- Formula/c/carton.rb
+++ Formula/c/carton.rb
@@ -6,6 +6,14 @@ class Carton < Formula
   license "Apache-2.0"
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d853189003a54db3e5fbb367e33f18246102bc47766b570dcc2f375b027002df"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "89ad220dcc8745113bcf675f746eebbdd482323fe1c88ab7967ac04c959bed0a"
+    sha256 cellar: :any_skip_relocation, ventura:       "19549734dbf7fb96934dffb9b4df3d071e7986382d3fb55910dfb2e3c1c3f383"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cba387440cd91f8ba1311c2a5f808df0e31ba3a0f9dd1d1c30687e390d7d04de"
+  end
+
   depends_on xcode: ["14.3", :build]
 
   uses_from_macos "swift" => :build
