48fb80b953e0a14ab84dba8ca070bb00fb73a6c9
Rui Chen <rui@chenrui.dev>
2025-01-25 01:10:56 -0500

    carton: needs curl for linux build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/carton.rb
+++ Formula/c/carton.rb
@@ -4,11 +4,11 @@ class Carton < Formula
   url "https://github.com/swiftwasm/carton/archive/refs/tags/1.1.3.tar.gz"
   sha256 "3a0ff4ba68f68d7a84dbe1768f1af1db6575ea23d2c5be9069d58cbfa681dc9e"
   license "Apache-2.0"
-  revision 1
 
   depends_on xcode: ["14.3", :build]
 
   uses_from_macos "swift" => :build
+  uses_from_macos "curl"
 
   def install
     args = if OS.mac?
