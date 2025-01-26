06e589331378713094f34dd52f2bfb6d2e338668
Rui Chen <rui@chenrui.dev>
2025-01-25 19:08:02 -0500

    dvm 1.9.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/d/dvm.rb
@@ -0,0 +1,27 @@
+class Dvm < Formula
+  desc "Deno Version Manager"
+  homepage "https://dvm.deno.dev"
+  url "https://github.com/justjavac/dvm/archive/refs/tags/v1.9.1.tar.gz"
+  sha256 "3b9bb668c6bdac67c201c7de823c9737d302687a8bae98cab881b24c59207a4e"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    generate_completions_from_executable(bin/"dvm", "completions")
+  end
+
+  test do
+    ENV["HOME"] = testpath
+
+    assert_match <<~EOS, shell_output("#{bin}/dvm info")
+      dvm 1.9.0
+      deno -
+      dvm root #{testpath}/.dvm
+    EOS
+
+    system bin/"dvm", "--version"
+  end
+end
