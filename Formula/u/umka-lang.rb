d6c68eb3a4a6366a2f42b37a5c15332544afa5c2
Rui Chen <rui@chenrui.dev>
2025-01-25 23:48:03 -0500

    umka-lang 1.5.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/u/umka-lang.rb
+++ Formula/u/umka-lang.rb
@@ -1,10 +1,16 @@
-class Umka < Formula
+class UmkaLang < Formula
   desc "Statically typed embeddable scripting language"
   homepage "https://github.com/vtereshkov/umka-lang"
   url "https://github.com/vtereshkov/umka-lang/archive/refs/tags/v1.5.1.tar.gz"
   sha256 "d5f4071f142ecb8859663e41654594b9d53fddfc0f60f9e9dd0fedda9f921b99"
   license "BSD-2-Clause"
   head "https://github.com/vtereshkov/umka-lang.git", branch: "master"
+
+  # fix dynlib, https://github.com/vtereshkov/umka-lang/pull/482
+  patch do
+    url "https://github.com/vtereshkov/umka-lang/commit/90fa58ba5a99b8e29a14a22b8fe594c4325bed47.patch?full_index=1"
+    sha256 "607953aa7be3fa5f3c0985a65ccb24d32804f8e486c0dad724db2ba3fd197cbe"
+  end
 
   def install
     system "make", "all"
