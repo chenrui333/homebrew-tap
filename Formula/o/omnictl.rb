d4aea848a08fbbbe28537704e4ec647dc8d57a05
Rui Chen <rui@chenrui.dev>
2025-01-26 10:54:01 -0500

    omnictl 0.45.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/o/omnictl.rb
@@ -0,0 +1,26 @@
+class Omnictl < Formula
+  desc "CLI for the Sidero Omni Kubernetes management platform"
+  homepage "https://omni.siderolabs.com/"
+  url "https://github.com/siderolabs/omni/archive/refs/tags/v0.45.1.tar.gz"
+  sha256 "8e352a7970cf36bcbf9c632a639b4a3c097a72d9e94ff9462ca1cc0de0a04b6c"
+  license "BSL-1.1"
+  head "https://github.com/siderolabs/omni.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/omnictl"
+
+    generate_completions_from_executable(bin/"omnictl", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/omnictl --version")
+
+    system bin/"omnictl", "config", "new"
+    assert_match "Current context: default", shell_output("#{bin}/omnictl config info")
+
+    output = shell_output("#{bin}/omnictl cluster status test 2>&1", 1)
+    assert_match "connect: connection refused", output
+  end
+end
