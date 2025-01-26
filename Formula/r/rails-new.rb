8c2d500101939c352094cf727672892c9821227e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-26 15:47:55 +0000

    rails-new: add 0.5.0 bottle.

--- Formula/r/rails-new.rb
+++ Formula/r/rails-new.rb
@@ -6,6 +6,14 @@ class RailsNew < Formula
   license "MIT"
   head "https://github.com/rails/rails-new.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "afdd30e8144f0c9fe4ad4f14cf4829ad18a4ade31e82180264dca060eea09b87"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "629782b738376ac5cd08464111b69a05fce2dac116ade8c49bed1a7d4a2181f5"
+    sha256 cellar: :any_skip_relocation, ventura:       "6d6082c208ac44ee3c2249b4d7124548529823a74c40657de727859d6c9d4dd2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "261786e81a8927b76dbc092f8b27a30bb854e0460075ab968f7b0eac40b60a5c"
+  end
+
   depends_on "rust" => :build
   depends_on "docker" => :test
 
