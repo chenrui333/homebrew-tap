6694c01f1302eb0f1e28e141951e19463c43bc25
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-26 14:39:28 +0000

    seamstress: add 2.0.0 bottle.

--- Formula/s/seamstress.rb
+++ Formula/s/seamstress.rb
@@ -6,6 +6,14 @@ class Seamstress < Formula
   sha256 "e61cda863afbf5c1555bbee35cdc5a3f0f8b1235451af2ac5a3a6e680eba4e02"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "24581e9413a9d581c4926108bd4e880f627d06c0a6b29f5860ec0ad4c976780f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d921f2a44be80a5f708485664f07d20dd0b088ab99c300f12cad05f43cab8d65"
+    sha256 cellar: :any_skip_relocation, ventura:       "32a28b1d7a5dcb213d086ce4da13538ee3d176820533d7a90f2ec5b28058a682"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "20ca9b29083bfdb0a8d5c54b9aa849ab722704b91e1d53137fc44266343d76fe"
+  end
+
   depends_on "zig" => :build
   depends_on "busted" => :test
 
