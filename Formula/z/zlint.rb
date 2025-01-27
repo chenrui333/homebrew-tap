899185fee32e1758f7f26ea59f3b26127f9f3eca
Rui Chen <rui@chenrui.dev>
2025-01-27 17:29:00 -0500

    zlint 0.6.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/z/zlint.rb
@@ -0,0 +1,43 @@
+class Zlint < Formula
+  desc "Linter for the Zig programming language"
+  homepage "https://github.com/DonIsaac/zlint"
+  url "https://github.com/DonIsaac/zlint/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "d8f5e1a6a5eaa6896d3a5586ea7182473590165640e859f9542c8b3efcadc67a"
+  license "MIT"
+
+  depends_on "zig" => :build
+
+  def install
+    # Fix illegal instruction errors when using bottles on older CPUs.
+    # https://github.com/Homebrew/homebrew-core/issues/92282
+    cpu = case Hardware.oldest_cpu
+    when :arm_vortex_tempest then "apple_m1" # See `zig targets`.
+    else Hardware.oldest_cpu
+    end
+
+    args = %W[
+      --prefix #{prefix}
+      -Doptimize=ReleaseFast
+      -Dversion=#{version}
+    ]
+
+    args << "-Dcpu=#{cpu}" if build.bottle?
+    system "zig", "build", *args
+  end
+
+  test do
+    (testpath/"valid.zig").write <<~ZIG
+      const std = @import("std");
+
+      pub fn main() void {
+          const message = "Hello, world!";
+          std.debug.print("{s}\\n", .{message});
+      }
+    ZIG
+
+    output = shell_output("#{bin}/zlint #{testpath}/valid.zig 2>&1")
+    assert_match "Found \e[33m0\e[39m errors and \e[33m0\e[39m warnings across \e[33m1\e[39m files in \e[33m1ms\e[39m", output
+
+    assert_match version.to_s, shell_output("#{bin}/zlint --version")
+  end
+end
