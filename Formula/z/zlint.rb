91597d2d2d5ccc6fa5d23d1aa67f43de5f8e09fa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-27 22:45:57 +0000

    zlint: add 0.6.1 bottle.

--- Formula/z/zlint.rb
+++ Formula/z/zlint.rb
@@ -5,6 +5,14 @@ class Zlint < Formula
   sha256 "d8f5e1a6a5eaa6896d3a5586ea7182473590165640e859f9542c8b3efcadc67a"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8019602cf52ae0eba0f69db85b1baa915423ed856d66d046c7c18ff1540d20af"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "717ce2e9e8ec4185216bce0523f219fb8f1a6e68c9b898f22e1773b43cc24635"
+    sha256 cellar: :any_skip_relocation, ventura:       "7db39349cd3bd2f4d7e252e89daebbe7aebbc9a03c8e270eb3b8255b6b74dc5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a227290b147d46626dc5334b8a2075457b7039c30b0af2bc787d0620d99705ea"
+  end
+
   depends_on "zig" => :build
 
   def install
