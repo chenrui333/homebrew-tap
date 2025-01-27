3993d21a89eaec7672072eae4fec1451d61de2b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-27 05:11:08 +0000

    sig: add 0.2.0 bottle.

--- Formula/s/sig.rb
+++ Formula/s/sig.rb
@@ -7,6 +7,14 @@ class Sig < Formula
   sha256 "a8d71e20a06fe15dbbcecf9f430a8e02f30eedbeb19d966db568255b5c426f66"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "30b734feb682dba0d6864312b2f33b61aeae5fc7c969465850fa66b9bd110b03"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "703080275d83f4db12b74d8b07df7f9700c0193584c0e7e65e24b24c3eb9b580"
+    sha256 cellar: :any_skip_relocation, ventura:       "9857d2f204b17390441eb9241428f606c8b5ba74ee12b15b473d1cae069b1161"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "559be70f1997733c0d6b057d4d028bec37e394b1343d7ac91db1626df9d28d76"
+  end
+
   depends_on "zig" => :build
 
   def install
