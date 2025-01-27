059f605a3bfc1df4dc4a3e1161490713da60ede9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-27 05:24:16 +0000

    lola: add 0.1 bottle.

--- Formula/l/lola.rb
+++ Formula/l/lola.rb
@@ -6,6 +6,14 @@ class Lola < Formula
   sha256 "ae996b9ca3137537dabe42926f61e1fa88a73315dab4c9b6d4469d1eeb242430"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a05a9cf1f1a33a8c720a1626817fe27df4631975176e63e4deb9e412774fc502"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3494b16b6b506d554b61c3e5df9e2b268a1a00291e1776ff6857ac8e4c702957"
+    sha256 cellar: :any_skip_relocation, ventura:       "fd872b713dfe1c576b7568cf7fad66a6efc4e1378ae604f0166ce7742f5c36a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3bd599b98e7a45c41bf80d79801dcd01d246520ff0e49184139c33123cef3237"
+  end
+
   depends_on "zig" => :build
 
   def install
