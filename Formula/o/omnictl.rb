b56ecccaba713309997d8443a29590006b110df1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-27 05:06:44 +0000

    omnictl: add 0.45.1 bottle.

--- Formula/o/omnictl.rb
+++ Formula/o/omnictl.rb
@@ -6,6 +6,14 @@ class Omnictl < Formula
   # license "BSL-1.1"
   head "https://github.com/siderolabs/omni.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8b48882e9c9e9e63704248b23e8dc87f56c3bf9988f9635551dd495a52e601f5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dde72a20f9cba9b01232a1c73edcaf8e745d3dc82e5c7d9b155cbfa537de84b9"
+    sha256 cellar: :any_skip_relocation, ventura:       "1a5572f745c7e74915e83aae6ad85dad1e6749aed19901779fd724193c60bfc1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ad9c58fe374ae25a0ed8ee7365487263d37a403e1727256b5648a808a80a33b"
+  end
+
   depends_on "go" => :build
 
   def install
