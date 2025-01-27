401c0b8350d99a82750a4900cf845045488235c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-27 22:49:14 +0000

    keyhunter: add 0.2.0 bottle.

--- Formula/k/keyhunter.rb
+++ Formula/k/keyhunter.rb
@@ -5,6 +5,14 @@ class Keyhunter < Formula
   sha256 "dc377e67f3593e710f17159f3fcfd2c6f60591cd908a294f9ea7f3a50a9f42fa"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "43f6cf7b7eda4355e466a7b40ea488f98d3a27091e1d5575a0a97164a73532d5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c67f96a41f1ea1b2e66bff15b43e760d748a3412abd039f68db3a8d6ec0efc0e"
+    sha256 cellar: :any_skip_relocation, ventura:       "bf3badd300e0070ee50f56ab3bf04f1a74f6f2a6a6ecfc91da2ab15a37a07fee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "37bf73fa0a47f5934e32745bad4890e27cc93ed662dace0220f11c52b8955625"
+  end
+
   depends_on "rust" => :build
 
   def install
