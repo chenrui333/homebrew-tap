098e19095cee63ecf295ec7ba8be58f94d2b5a6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-28 00:34:14 +0000

    poop: add 0.5.0 bottle.

--- Formula/p/poop.rb
+++ Formula/p/poop.rb
@@ -5,6 +5,14 @@ class Poop < Formula
   sha256 "b67d62c3583994fb262ccaf05094b215d3514d4d2935a25a3867dcab0cf89c93"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "58748a9375bbde162f69a55f9c920067eb3e3c825a4aa471107ba16dbd7a2f99"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "73116ee7298e02fe3b83bfb7b0d9c276940dd85963377e8b6c1394711ac0b952"
+    sha256 cellar: :any_skip_relocation, ventura:       "721bf504f50e7888c007dc482ca9c4ea99835685a268f675f6a6940b3fc782a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19c23a696d890f4e6a523cdd27627f16e3288ce84cf3b94c3d62b34b6f53ce88"
+  end
+
   depends_on "zig" => :build
 
   def install
