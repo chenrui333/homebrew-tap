f02b567970fd7e11def4cc3df75866a292cc8b00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-28 04:19:19 +0000

    zware: add 0.0.1 bottle.

--- Formula/z/zware.rb
+++ Formula/z/zware.rb
@@ -6,6 +6,14 @@ class Zware < Formula
   sha256 "7135b821013c1a94631865ab300daf2291c6fd0e96a76b3e70c1ea09cf4a8379"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "334ba0e2221a5899f492ab229b242539cab9aa3c3b4dbca912691e291d081e0e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b9fbda903ba869eec33d5439446279e79aec3f62e8b255efa889aac839d69b0c"
+    sha256 cellar: :any_skip_relocation, ventura:       "43733c5ca07c347a65c685278fea324ac2a40b2d0ca58cab521de8c5304628c5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c3a72d915d1a59d9fb85e678e94a13b4aa291469027297fe3d86adfc5644db2"
+  end
+
   depends_on "zig" => :build
   depends_on "wabt" => :test
 
