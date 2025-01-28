8cacdc81651e3ce51d6db2277cc72d4467a61bf1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-28 00:50:07 +0000

    glsl-analyzer: add 1.5.1 bottle.

--- Formula/g/glsl-analyzer.rb
+++ Formula/g/glsl-analyzer.rb
@@ -6,6 +6,14 @@ class GlslAnalyzer < Formula
       revision: "5b9848b2dd563170cd702c41443659d6b3cb11de"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fed318c8d224e2a07fdffa959cf6b8e8d9c247e290f1640468f475fab0cd364b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9825ac041cb64ec021c521a832825e7884e1975a796b9c1e29dc67a1651c1fe7"
+    sha256 cellar: :any_skip_relocation, ventura:       "40095b4656368f1020fe0a6c8b293161eb95b8da1069d8b7fd88c8253d9608a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "456c597b9a83b3d4e83a2728f3d28619409f55e12f432cf7e696f9110b982f11"
+  end
+
   depends_on "zig" => :build
 
   def install
