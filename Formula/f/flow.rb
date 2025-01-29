24761b81da48528b9106c9520426b2bc050a3abb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-29 02:24:46 +0000

    flow: add 0.3.0 bottle.

--- Formula/f/flow.rb
+++ Formula/f/flow.rb
@@ -5,6 +5,14 @@ class Flow < Formula
   sha256 "8a018a8a7ca80cf43c6b9ddd10ece44dd4e8fa3f85797fc94c8d15020475cc1c"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c521990016b20f9ee8d8cf8a1d4fd796880c705ebf7b74212fa6e37767db8b42"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "73020a693ed47bcc4ed2a82eb74df6639fc64e40e57a3447ef8a0c1f4af24e9d"
+    sha256 cellar: :any_skip_relocation, ventura:       "7ec919f0c3f32c2e88ad070ab98bb641587b47a904ecfc791d72c64067f393f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "798d2980761573eb30a3a0e8034fdb62255d20755146aae3a55c040313f2eb66"
+  end
+
   depends_on "zig" => :build
 
   def install
