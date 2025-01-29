8f647562dedc6bb31d714f768315f9310487e24d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-29 18:44:43 +0000

    fex: add 0.1.4 bottle.

--- Formula/f/fex.rb
+++ Formula/f/fex.rb
@@ -5,6 +5,14 @@ class Fex < Formula
   sha256 "a6e51e07802442e2e938197f78b287816ea2688200c9c86738d44230d4bc780e"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fc91544ff705a4c8ef572a688bde9692a71de10a45b84883e6b5feb38d2ce4a0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aab5dcfac107ef3b44c5db99028b6f92ee4bbc48ba0ce7b7606cbed374e22db1"
+    sha256 cellar: :any_skip_relocation, ventura:       "70c3ffd0f2b130dca1bc2538a3e23877ac9e2ede2185ecb72a84c362910c404a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e1ae8158ca1318af2056b30ac9844944f82a82a57152ff1d1201e0036e09b07"
+  end
+
   depends_on "zig" => :build
 
   def install
