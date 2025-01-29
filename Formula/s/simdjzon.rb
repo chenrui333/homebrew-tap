8a25db8c1fdea62bbbc39fcd6ecbd0801de7b982
Rui Chen <rui@chenrui.dev>
2025-01-29 00:24:17 -0500

    simdjzon 0.13.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/s/simdjzon.rb
@@ -0,0 +1,51 @@
+class Simdjzon < Formula
+  desc "Simdjson ported to zig"
+  homepage "https://github.com/travisstaloch/simdjzon"
+  url "https://github.com/travisstaloch/simdjzon/archive/refs/tags/0.13.0.tar.gz"
+  sha256 "91e1bb7fe972898aa1c9ac6c5577ccfa8a5c64066ed7dcfc8345dca1a5f4a28b"
+  license "Apache-2.0"
+
+  depends_on "zig" => :build
+
+  def install
+    # Fix illegal instruction errors when using bottles on older CPUs.
+    # https://github.com/Homebrew/homebrew-core/issues/92282
+    cpu = case Hardware.oldest_cpu
+    when :arm_vortex_tempest then "apple_m1" # See `zig targets`.
+    else Hardware.oldest_cpu
+    end
+
+    args = %W[
+      --prefix #{prefix}
+      -Doptimize=ReleaseSafe
+    ]
+
+    args << "-Dcpu=#{cpu}" if build.bottle?
+    system "zig", "build", *args
+  end
+
+  test do
+    (testpath/"test.json").write <<~JSON
+      {
+          "Width": 800,
+          "Height": 600,
+          "Title": "View from my room",
+          "Url": "http://ex.com/img.png",
+          "Private": false,
+          "Thumbnail": {
+              "Url": "http://ex.com/th.png",
+              "Height": 125,
+              "Width": 100
+          },
+          "array": [
+              116,
+              943,
+              234
+          ],
+          "Owner": null
+      }
+    JSON
+
+    system bin/"simdjzon", testpath/"test.json"
+  end
+end
