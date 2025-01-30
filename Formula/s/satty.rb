35b81d6b6c82d23df98d9538c2f68f13fc3739e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 04:45:28 +0000

    satty: add 0.16.0 bottle.

--- Formula/s/satty.rb
+++ Formula/s/satty.rb
@@ -5,6 +5,14 @@ class Satty < Formula
   sha256 "36ffbea973fd844d92998f1d5aed53c779692e956ed07490d324812223eab4bd"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_sequoia: "e2224070983790bf63da7e81fa39d6f93454e44901481272d926a06bd4c20f28"
+    sha256 cellar: :any,                 arm64_sonoma:  "09ea7c97bff0da752e2e6071e505792cefe9101dc8b5fbc5ddc8c5e97e855353"
+    sha256 cellar: :any,                 ventura:       "1f3fe2166c9b0e7b5c515b8ef97d272801c5b6b8e6c1f429c7cb90f0acca1f11"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ded338edf1c6289d1b2fdee30149adce3a68417285402bbc912577d6abaf6d36"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "cairo"
