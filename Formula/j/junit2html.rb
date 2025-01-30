7eb4aae25b3246c38ce1d92d5d5a135c7a2e80b9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 15:53:57 +0000

    junit2html: add 0.1.0 bottle.

--- Formula/j/junit2html.rb
+++ Formula/j/junit2html.rb
@@ -6,6 +6,14 @@ class Junit2html < Formula
   license "MIT"
   head "https://github.com/kitproj/junit2html.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f47a633c105f7e589e583531397a2b635b2de9fb143422d6e34923dd0cc52b3b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fad1a3e2cfbbbaf91666a9330012acb9159e05a215ac7eda94c43da025e581d2"
+    sha256 cellar: :any_skip_relocation, ventura:       "936febd8ff387f567f92a97e6611cc81b29c9a8a2b57d7516df97565edbabdae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5fc49742e25f874959b4b03633aa00b7d1c6b0d158a86ef311ad2c018bdb233"
+  end
+
   depends_on "go" => [:build, :test]
   depends_on "go-junit-report" => :test
 
