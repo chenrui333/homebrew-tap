3281dc1c38d3668102b60813f5e3ce0676730617
Rui Chen <rui@chenrui.dev>
2025-01-30 00:11:01 -0500

    alacritty: needs fontconfig for linux
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/alacritty.rb
+++ Formula/a/alacritty.rb
@@ -5,7 +5,12 @@ class Alacritty < Formula
   sha256 "aa4479c99547c0b6860760b5b704865f629ffe1f1ec374153c2cd84e53ce5412"
   license any_of: ["Apache-2.0", "MIT"]
 
+  depends_on "pkgconf" => :build
   depends_on "rust" => :build
+
+  on_linux do
+    depends_on "fontconfig"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args(path: "alacritty")
