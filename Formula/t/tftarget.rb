75b09dd8ba479906b0eb64ae076f4c8ec9ff96d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 19:07:57 +0000

    tftarget: add 0.0.9 bottle.

--- Formula/t/tftarget.rb
+++ Formula/t/tftarget.rb
@@ -6,6 +6,14 @@ class Tftarget < Formula
   license "MIT"
   head "https://github.com/future-architect/tftarget.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1a53d3517095858edeafeb7c2ed9756116a87201dfd54190d50c49ca6ba49537"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62ec7a291f2b961d4f016c33ee9b2411df3f0b1796f6f0e0f263f13e4cbcf4ac"
+    sha256 cellar: :any_skip_relocation, ventura:       "583874019b91e16940636864e9fd0bcc0300abf477f5529032f4448c679f534c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c953d2538f62d69327698d518660887afba3378d033734e2a60f9ce9a569b9d1"
+  end
+
   depends_on "go" => :build
   depends_on "opentofu"
 
