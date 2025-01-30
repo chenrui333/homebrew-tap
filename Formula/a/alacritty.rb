9af47da50a7e6d64ab67098e1d9abe35c5bea956
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 05:21:28 +0000

    alacritty: add 0.15.0 bottle.

--- Formula/a/alacritty.rb
+++ Formula/a/alacritty.rb
@@ -5,6 +5,14 @@ class Alacritty < Formula
   sha256 "aa4479c99547c0b6860760b5b704865f629ffe1f1ec374153c2cd84e53ce5412"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8c416b19bee072e30da1a2e9224c976b82e5434f656ba3f8ae3f68326f894aef"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b757fc738c35ef31b03f8f921c1cb3379ef0786d631731a84d9ef71d1b31885c"
+    sha256 cellar: :any_skip_relocation, ventura:       "14aaaf3ff042680531d69122ef1190ceacaea3180402b86b882267187fbfe374"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ff1dbb74aeeb9865f0a8194e34aeb6d9b141fc9c06866a846d3aa2f0ea45afc"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
