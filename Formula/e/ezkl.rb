2b10ddc91276980beec5d00948509b9d76f9d394
Rui Chen <rui@chenrui.dev>
2025-01-30 00:44:58 -0500

    ezkl: add openssl and linkage check
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/e/ezkl.rb
+++ Formula/e/ezkl.rb
@@ -5,9 +5,14 @@ class Ezkl < Formula
   sha256 "6894fc4b2851f3bcfc124f85690b2e6c40ef1f3d2cab970572612b79d2431eea"
   # license :unfree
 
+  depends_on "pkgconf" => :build
   depends_on "rustup" => :build
+  depends_on "openssl@3"
 
   def install
+    ENV["OPENSSL_NO_VENDOR"] = "1"
+    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
+
     ENV.prepend_path "PATH", Formula["rustup"].bin
     system "rustup", "set", "profile", "minimal"
     system "rustup", "component", "add", "rustfmt", "clippy"
@@ -17,5 +22,13 @@ class Ezkl < Formula
 
   test do
     system bin/"ezkl", "--version"
+
+    [
+      Formula["openssl@3"].opt_lib/shared_library("libssl"),
+      Formula["openssl@3"].opt_lib/shared_library("libcrypto"),
+    ].each do |library|
+      assert Utils.binary_linked_to_library?(bin/"gerust", library),
+             "No linkage with #{library.basename}! Cargo is likely using a vendored version."
+    end
   end
 end
