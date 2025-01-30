aa9fbadc21797ad72cd8b986555eaac522f1ea63
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-30 05:51:51 +0000

    asciinema: add 3.0.0-rc.3 bottle.

--- Formula/a/asciinema.rb
+++ Formula/a/asciinema.rb
@@ -5,6 +5,14 @@ class Asciinema < Formula
   sha256 "3e7402589eac1a704951e3c48d769f5d007c52630f17ec895dfad6676e4ba6b9"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2617be3b02bb19fb32c00684c63924c79391b86f0a37cddf46fbf3b825fe903f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "811e2a6b8a655b4fa8dff9d36f7ca300f07d9e6b5b40d21e9ad45a2ed8325032"
+    sha256 cellar: :any_skip_relocation, ventura:       "b36dfbe26d0012a44b2c3e232ea710030ac68bf6681a965ae4ed0ced19f456c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48722435f1d71ca839c07d01183d850fa1bf1667fe9e06b665a2f7455d07b38d"
+  end
+
   depends_on "rust" => :build
 
   def install
