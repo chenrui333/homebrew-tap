5689d1067bdddfa578f6ef9cbc6a4e717e40ef31
Rui Chen <rui@chenrui.dev>
2025-01-29 23:46:30 -0500

    alacritty 0.15.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/a/alacritty.rb
@@ -0,0 +1,18 @@
+class Alacritty < Formula
+  desc "Cross-platform, OpenGL terminal emulator"
+  homepage "https://github.com/alacritty/alacritty"
+  url "https://github.com/alacritty/alacritty/archive/refs/tags/v0.15.0.tar.gz"
+  sha256 "aa4479c99547c0b6860760b5b704865f629ffe1f1ec374153c2cd84e53ce5412"
+  license any_of: ["Apache-2.0", "MIT"]
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "alacritty")
+  end
+
+  test do
+    # it is a emulator
+    assert_match version.to_s, shell_output("#{bin}/alacritty --version")
+  end
+end
