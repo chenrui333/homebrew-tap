e7d0b3a50b7098c866ced85e37f33b56ca88eefa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-31 04:45:47 +0000

    hclgrep: add 0.0.0 bottle.

--- Formula/h/hclgrep.rb
+++ Formula/h/hclgrep.rb
@@ -7,6 +7,14 @@ class Hclgrep < Formula
   license "BSD-3-Clause"
   head "https://github.com/magodo/hclgrep.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a79a820731932a61a1ad259c70714ac3da8a69831f6305f42f64c0cbda523690"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cf3b71b3bf7334c20d3d6a3a5610c023fa3301b891a87af4268051bf72a8eda2"
+    sha256 cellar: :any_skip_relocation, ventura:       "9f1e17eca3761bc11f4aee7e1bbb1e16d6f8e016c424b6e8d83baa5d5430a5bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "324ee0ac1044ac7425541ea087b2b4efcc293d4a3f2ae4c107f066ae5b3cf46c"
+  end
+
   depends_on "go" => :build
 
   def install
