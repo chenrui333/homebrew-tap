0a43798c854bc738dbea81b7dff71e1a6c3e432b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-31 05:08:29 +0000

    hcldump: add 0.0.0 bottle.

--- Formula/h/hcldump.rb
+++ Formula/h/hcldump.rb
@@ -7,6 +7,14 @@ class Hcldump < Formula
   # no license
   head "https://github.com/magodo/hcldump.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f5faf44f9fc4c5b5799fbd6afc4e3953b30c530727309dc8f187d9f989731129"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "305f323cd3a1ac8bcc59799500a919bed98e5ba4a535815b01b7c3581619d2b8"
+    sha256 cellar: :any_skip_relocation, ventura:       "2cc350ebbaea9b90b4f7691ddf025169b39f33e74afad7fe6a7042f3ab9b23c8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8e62781833b2ec892a3f58423f303447bdd013a9c71099a8233a6221575d1a4"
+  end
+
   depends_on "go" => :build
 
   def install
