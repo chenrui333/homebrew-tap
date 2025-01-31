70d1e2ac476da0a24735be5054d249427908aaaa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-31 05:13:31 +0000

    pluralith: add 0.2.2 bottle.

--- Formula/p/pluralith.rb
+++ Formula/p/pluralith.rb
@@ -6,6 +6,14 @@ class Pluralith < Formula
   license "MPL-2.0"
   head "https://github.com/Pluralith/pluralith-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dcf44646d73cd9f1436479e7ebf6ae6aed37ccdbe3d6f2df91e6b49358ae5e2a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5f17b58cb40d7c1cb6f4bb9814b62cc462685eb83717a2f2f378fd6974d27b1c"
+    sha256 cellar: :any_skip_relocation, ventura:       "a71c9f50ca91d20a850fd12e9219f5afcfc3ed25963872cd6a63eedde450089a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f32aaa1530c7a9007062c76e1c4474e922182c24f88d60792b1f165129a7e9ab"
+  end
+
   depends_on "go" => :build
 
   def install
