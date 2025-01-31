0d67a4ba024de68dee7603c196768bd5f38a14e5
Rui Chen <rui@chenrui.dev>
2025-01-30 23:06:20 -0500

    balcony: remove completion support for now
    Closes #116.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/b/balcony.rb
+++ Formula/b/balcony.rb
@@ -237,8 +237,6 @@ class Balcony < Formula
 
   def install
     virtualenv_install_with_resources
-
-    generate_completions_from_executable(bin/"balcony", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
