96c78bb84e5281502c0f17813e1ce1c87ce265bb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-31 03:37:52 +0000

    cf-vault: add 0.0.18 bottle.

--- Formula/c/cf-vault.rb
+++ Formula/c/cf-vault.rb
@@ -6,6 +6,14 @@ class CfVault < Formula
   license "MPL-2.0"
   head "https://github.com/jacobbednarz/cf-vault.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c1ec5c490acf361a089c46d54d4657cf01aef45be04d8cad7a7c3430e724cc5c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e69cb52e7a7bc690c6b82c403710a98889f31a9395213b0a2a93db6ad01046d8"
+    sha256 cellar: :any_skip_relocation, ventura:       "e2cd6b98b42d727a3d4d9c6accc7f68eaf5f43ebe624bfcc5e73e6b35421460b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4029f75d7b847cc539c32c240eed70aade4228a34f5f9b33ff690699b8b569f3"
+  end
+
   depends_on "go" => :build
 
   def install
