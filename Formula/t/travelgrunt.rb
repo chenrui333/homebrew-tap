5141897bd5c81e60b8b04adfc210fbf86a14421d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-01-31 05:36:13 +0000

    travelgrunt: add 0.6.0 bottle.

--- Formula/t/travelgrunt.rb
+++ Formula/t/travelgrunt.rb
@@ -6,6 +6,14 @@ class Travelgrunt < Formula
   license "Apache-2.0"
   head "https://github.com/ivanilves/travelgrunt.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "622841722e37578f9821b6850e25ab6adf93abbd17c7f6fc5c0b0660552e1be4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43744b70810727436d9d4c89606fea99086ba14d83e3e9e14fd984c9df6dffbe"
+    sha256 cellar: :any_skip_relocation, ventura:       "a03a087822ea718746f172898344546045735a85d83dad618802c1c81061c708"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f18d5c11b724aa81aa39f9175677f2df9ca9cc10c44549a2b170dff5cb7030e"
+  end
+
   depends_on "go" => :build
 
   def install
