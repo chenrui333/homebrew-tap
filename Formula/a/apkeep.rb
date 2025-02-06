767a4bc764af2f6589e8b011b7d2d30141e6d506
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-06 04:28:21 +0000

    apkeep: add 0.17.0 bottle.

--- Formula/a/apkeep.rb
+++ Formula/a/apkeep.rb
@@ -6,6 +6,14 @@ class Apkeep < Formula
   license "MIT"
   head "https://github.com/EFForg/apkeep.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fbd98a43607b52c3e8f47854e1bd3c0fe04bcac711b13fdb98684a4491198fb4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5ef7df84f1fe13312cd1096abbcb04cdd5eecf2a94e07d7e4a24d12e994bc317"
+    sha256 cellar: :any_skip_relocation, ventura:       "4419fc3890843867991226173e5493e2145d333b5bdddd6d93c3c9140ad73599"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70e851ecd533938b55112588b15fa6841a998261903e524d0b1f4d35cc176166"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
