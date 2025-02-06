123c935bc38d5991e2a2723f137b4a6e08c12d13
Rui Chen <rui@chenrui.dev>
2025-02-05 23:13:17 -0500

    aoc-cli: needs openssl@3 for linux build
    Closes #134.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/a/aoc-cli.rb
+++ Formula/a/aoc-cli.rb
@@ -6,7 +6,12 @@ class AocCli < Formula
   license "MIT"
   head "https://github.com/scarvalhojr/aoc-cli.git", branch: "main"
 
+  depends_on "pkgconf" => :build
   depends_on "rust" => :build
+
+  on_linux do
+    depends_on "openssl@3"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
