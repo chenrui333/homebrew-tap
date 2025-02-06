b4722325a54fd634995168f05454ad077c3bafd0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-06 04:31:36 +0000

    envtpl: add 2.0.3 bottle.

--- Formula/e/envtpl.rb
+++ Formula/e/envtpl.rb
@@ -6,6 +6,14 @@ class Envtpl < Formula
   license "MIT"
   head "https://github.com/chenrui333/envtpl.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "631c7c0ee3790fe5352d472640c36018be1cdd8f957b1b1e976d9670a4227c65"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "da01ad707ffb148f59889dd475bc6f16b5f118df38f75297aafa6d8cd50e0954"
+    sha256 cellar: :any_skip_relocation, ventura:       "177004d5ba30a049f791c038be5106384912e3b3fff1d085496574fc1e2b661c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8284118f17fb923b5da1d368a28068723095ca377f8f0aa2d20860a245cac1f"
+  end
+
   depends_on "go" => :build
 
   def install
