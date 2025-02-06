e796a0953b688f71eda110f3d0d7c4b99d909d9e
Rui Chen <rui@chenrui.dev>
2025-02-05 23:11:14 -0500

    apkeep 0.17.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/a/apkeep.rb
@@ -0,0 +1,22 @@
+class Apkeep < Formula
+  desc "Command-line tool for downloading APK files from various sources"
+  homepage "https://github.com/EFForg/apkeep"
+  url "https://github.com/EFForg/apkeep/archive/refs/tags/0.17.0.tar.gz"
+  sha256 "f5fa0d8c02d5c078f69ec18e080463113c3794be8b94130f6a81f463c36bca0b"
+  license "MIT"
+  head "https://github.com/EFForg/apkeep.git", branch: "master"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/apkeep --version")
+
+    # hello world apk, https://play.google.com/store/apps/details?id=dev.egl.com.holamundo&hl=en_US
+    system bin/"apkeep", "--app", "dev.egl.com.holamundo", testpath
+    assert_path_exists "dev.egl.com.holamundo.xapk"
+  end
+end
