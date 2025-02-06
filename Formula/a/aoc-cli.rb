2632bc69636d5d040bbf03c8fc3cd707eced3704
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-06 04:23:10 +0000

    aoc-cli: add 0.12.0 bottle.

--- Formula/a/aoc-cli.rb
+++ Formula/a/aoc-cli.rb
@@ -6,6 +6,14 @@ class AocCli < Formula
   license "MIT"
   head "https://github.com/scarvalhojr/aoc-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f7c19053f68761ab056c7c5031ffa487b6f314acdcce2a6a000becb638950586"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e0c9e38392799aa1d216333e5cd8f6354a54d08c225f0cb70f091eb57f3c5b8e"
+    sha256 cellar: :any_skip_relocation, ventura:       "a85befc69f944e03bb6ae2e0527c924da751ffafb5ebda901155eda49005dd93"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80c9cc39374bc6d3947584111683f697223909b16d12551a069df53fc8b3517f"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
