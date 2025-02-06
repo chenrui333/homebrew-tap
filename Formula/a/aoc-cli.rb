1a4584509b615bcf1527b24aab576b28b6b43ce8
Rui Chen <rui@chenrui.dev>
2025-02-05 23:03:28 -0500

    aoc-cli 0.12.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/a/aoc-cli.rb
@@ -0,0 +1,21 @@
+class AocCli < Formula
+  desc "Advent of Code command-line tool"
+  homepage "https://github.com/scarvalhojr/aoc-cli"
+  url "https://github.com/scarvalhojr/aoc-cli/archive/refs/tags/0.12.0.tar.gz"
+  sha256 "5bd2eef8a310564c122be34ea9116967fe887ea549146adf38f4fbb0cddc0539"
+  license "MIT"
+  head "https://github.com/scarvalhojr/aoc-cli.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/aoc --version")
+
+    output = shell_output("#{bin}/aoc read 2>&1", 66)
+    assert_match "Session cookie file not found in home or config directory", output
+  end
+end
