39ed44eab5195b1113753edc530a926061939cbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-08 06:32:35 +0000

    mdbook-linkcheck: add 0.7.7 bottle.

--- Formula/m/mdbook-linkcheck.rb
+++ Formula/m/mdbook-linkcheck.rb
@@ -5,6 +5,14 @@ class MdbookLinkcheck < Formula
   sha256 "3194243acf12383bd328a9440ab1ae304e9ba244d3bd7f85f1c23b0745c4847a"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "868df859c381ad2bd641c57d04115fe3d9bb94e83860789549e5a22bbe5e3e50"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "58c998c81ff24783c0fbc51d97857e1a42d88fc975a2a54779edaec31ee8fac4"
+    sha256 cellar: :any_skip_relocation, ventura:       "4646eb2abc72942cc985586944a366a91c1308b40379091d0e833bc995c7e3da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c74ee710f61f9116b93b89d29651e7d58270788819198202a1cc7ebb44efd972"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "mdbook"
