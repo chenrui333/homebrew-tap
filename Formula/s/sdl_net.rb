76d43f690e44dc2d5b5c222a46de64489fc2d6e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-08 02:06:44 +0000

    sdl_net: add 1.2.8 bottle.

--- Formula/s/sdl_net.rb
+++ Formula/s/sdl_net.rb
@@ -5,6 +5,14 @@ class SdlNet < Formula
   sha256 "5f4a7a8bb884f793c278ac3f3713be41980c5eedccecff0260411347714facb4"
   license "Zlib"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_sequoia: "6290cb3a2ab8ca9379e43443250c7f323f286804eac389b5d1decca81d22c2b3"
+    sha256 cellar: :any,                 arm64_sonoma:  "ff31cf3b4532d9a4de50892b2738aa788bedad8bca71e6f55f2914461d4ea184"
+    sha256 cellar: :any,                 ventura:       "f26f218b290b37b0b13d94ebe5a004c2c4f2890ae7881b57f7349c3980f098b7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1acfdfa46899600bcc0ce4d57a6a74c821766155af44a3cee7bdf48e34b539f4"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "sdl12-compat"
 
