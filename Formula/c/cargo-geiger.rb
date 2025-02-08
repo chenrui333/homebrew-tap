7aec2b18f34862c7ddf841d3ba5bb9e02528a510
Rui Chen <rui@chenrui.dev>
2025-02-08 13:05:44 -0500

    cargo-geiger: needs openssl@3 dep
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/cargo-geiger.rb
+++ Formula/c/cargo-geiger.rb
@@ -5,7 +5,9 @@ class CargoGeiger < Formula
   sha256 "6ddc447b0b8a46ee2b303897fbe2d416df794942cd23984c44b0ee69c4675bad"
   license any_of: ["Apache-2.0", "MIT"]
 
+  depends_on "pkgconf" => :build
   depends_on "rust" => :build
+  depends_on "openssl@3"
 
   patch do
     url "https://raw.githubusercontent.com/chenrui333/homebrew-tap/ceb0ef84f4d34b992c0f20da279467c06c2590c7/patches/cargo-geiger/0.11.7.patch"
@@ -13,10 +15,16 @@ class CargoGeiger < Formula
   end
 
   def install
+    # Ensure that the `openssl` crate picks up the intended library.
+    ENV["OPENSSL_DIR"] = Formula["openssl@3"].opt_prefix
+    ENV["OPENSSL_NO_VENDOR"] = "1"
+
     system "cargo", "install", *std_cargo_args(path: "cargo-geiger")
   end
 
   test do
+    require "utils/linkage"
+
     assert_match version.to_s, shell_output("#{bin}/cargo-geiger --version")
 
     mkdir "brewtest" do
@@ -37,6 +45,14 @@ class CargoGeiger < Formula
 
       output = shell_output("#{bin}/cargo-geiger --offline")
       assert_match "1 warning emitted", output
+    end
+
+    [
+      Formula["openssl@3"].opt_lib/shared_library("libcrypto"),
+      Formula["openssl@3"].opt_lib/shared_library("libssl"),
+    ].each do |library|
+      assert Utils.binary_linked_to_library?(bin/"cargo-geiger", library),
+             "No linkage with #{library.basename}! Cargo is likely using a vendored version."
     end
   end
 end
