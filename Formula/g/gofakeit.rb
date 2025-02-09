c9242c44020e20a76f65eeab1942309cbab334a0
Rui Chen <rui@chenrui.dev>
2025-02-09 18:02:27 -0500

    gofakeit 7.0.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/g/gofakeit.rb
@@ -0,0 +1,19 @@
+class Gofakeit < Formula
+  desc "Random fake data generator written in go"
+  homepage "https://github.com/brianvoe/gofakeit"
+  url "https://github.com/brianvoe/gofakeit/archive/refs/tags/v7.0.0.tar.gz"
+  sha256 "93469831881fa9c46c0032623570eaef185335b7116866246ab0c09186733058"
+  license "MIT"
+  head "https://github.com/brianvoe/gofakeit.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/gofakeit"
+  end
+
+  test do
+    system bin/"gofakeit", "street"
+    system bin/"gofakeit", "school"
+  end
+end
