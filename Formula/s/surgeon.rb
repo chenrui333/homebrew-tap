506cc8d4a285f90459915bc78a109c49e11a5039
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-09 19:13:24 +0000

    surgeon: add 0.1.0 bottle.

--- Formula/s/surgeon.rb
+++ Formula/s/surgeon.rb
@@ -6,6 +6,14 @@ class Surgeon < Formula
   license "MIT"
   head "https://github.com/bketelsen/surgeon.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3998de9aa8e5872210f5fad95e0f03b925d6e07e921b98a5dc7372ad185d3cdb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0eae1232d473117277580df197f575cce660bdd0c30ac93579a0c4c527949677"
+    sha256 cellar: :any_skip_relocation, ventura:       "c2d68b1efda7a916ab3711320d30cdc0076c1aea3d88b17401c3ba7c304b034b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0046a7cccbd36311bdced1eaf2e85a42d6ca240d826c082454daa6e4da0bd89"
+  end
+
   depends_on "go" => :build
 
   def install
