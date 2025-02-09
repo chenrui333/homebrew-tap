3393ee1a138a1dd031317c9af0d153e7173867ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-09 23:08:13 +0000

    gofakeit: add 7.0.0 bottle.

--- Formula/g/gofakeit.rb
+++ Formula/g/gofakeit.rb
@@ -6,6 +6,14 @@ class Gofakeit < Formula
   license "MIT"
   head "https://github.com/brianvoe/gofakeit.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe058a16a32dc9e92808e0af178079061038fcba033ff0e7ede3ce6e7d404fa0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bcd9a7004a26188cd1de5897b6cb686ff6e65328a27dbd15ecd3c153533ccec7"
+    sha256 cellar: :any_skip_relocation, ventura:       "65fc761ddff28e7d4aee103584375bf1e8abf8d4affb7d06e344c6ee6dd1cbc9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e93fd145af6b8d1d4f11360ff8ef5fbfde70b7e4d24d374fcac0169fabf830a2"
+  end
+
   depends_on "go" => :build
 
   def install
