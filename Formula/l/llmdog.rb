d6a75b3d2ac02a3839134580b7281f340c97b84b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-09 21:58:35 +0000

    llmdog: add 1.0.0 bottle.

--- Formula/l/llmdog.rb
+++ Formula/l/llmdog.rb
@@ -7,6 +7,14 @@ class Llmdog < Formula
   license "MIT"
   head "https://github.com/doganarif/llmdog.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e2a23352cf7b1233797f2d57baad9ea407b7a89bb70f4a9f47ab1118b36bff0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cdaf86589210c41a912c21de2ba068d9eec285b22dbc6a5d263cac74b446239b"
+    sha256 cellar: :any_skip_relocation, ventura:       "139c2355f841377ae5ca2d443d810b4d27724fb49f403599039b981a5cd401cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4cc1605dc5e09f03d5a77cc42752c7f42b0d9adfa99600e609af95b277ddf5eb"
+  end
+
   depends_on "go" => :build
 
   def install
