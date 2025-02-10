3295ed360d90fb26d9bf6480208d95d3fd2113fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-10 21:18:17 +0000

    ohy: add 0.0.0 bottle.

--- Formula/o/ohy.rb
+++ Formula/o/ohy.rb
@@ -10,6 +10,14 @@ class Ohy < Formula
     skip "no tagged releases"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "861de6548c75b3b48ec1e13b591d8725c5d70e6e45e0dd2feab682d160e6132a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2b301210aad7569f4cabaaf29c6adb993a0ab2edf586fc538f067b363f9f9495"
+    sha256 cellar: :any_skip_relocation, ventura:       "80afe741ee00180a71244227105e88bfcd8e94b8af446b842d53a794b805afd1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f9702870b084a346adb3339c00451b97add79c72e28ea971aac5752cf03cca0"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
