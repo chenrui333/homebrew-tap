4916ebc1885d997c4084f5fadc9695bd5873d35d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-10 03:17:48 +0000

    threatcl: add 0.2.2 bottle.

--- Formula/t/threatcl.rb
+++ Formula/t/threatcl.rb
@@ -6,6 +6,14 @@ class Threatcl < Formula
   license "MIT"
   head "https://github.com/threatcl/threatcl.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f70a41ab79251a39ec7731f86430573042bd2f8b73c3fb432fae55898d497c58"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "48a9d400c1d7117456bb23952b3ed0c779fff5ce6a6b69617834569ab5d9464c"
+    sha256 cellar: :any_skip_relocation, ventura:       "c647abd671a3319f4d643ade61912472b24b065e5fa6b34331117980680a5a8e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8ceab5bc27c4d8b5165ea9d5523b20ae6027e02cd25b6f09f7f65819f9564b0f"
+  end
+
   depends_on "go" => :build
 
   def install
