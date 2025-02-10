bb4c08a33f3e0f758caae4e069a331bbd136087d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-10 03:07:05 +0000

    hclq: add 0.5.3 bottle.

--- Formula/h/hclq.rb
+++ Formula/h/hclq.rb
@@ -7,6 +7,14 @@ class Hclq < Formula
   license "Unlicense"
   head "https://github.com/mattolenik/hclq.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "630b2e30736fa59136be44ece3159a475798da30bd33411935a3083d96deda74"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e04975185b939d924fdf67e1348820ae4d0d1d9df81af1ab615f215025c963f7"
+    sha256 cellar: :any_skip_relocation, ventura:       "88c51e77d890f910dd7b4cb883c6d31eb9c50c03441c26d5d895501a09f61f12"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97b23edaa21b122a4d6e9c257418ed88206a2adb34a67057d5875e6ca42ac12a"
+  end
+
   depends_on "go" => :build
 
   def install
