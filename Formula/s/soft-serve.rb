da7317d862cfa86f57eea0b1a9e3b5618738d4ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-11 20:59:56 +0000

    soft-serve: add 0.8.2 bottle.

--- Formula/s/soft-serve.rb
+++ Formula/s/soft-serve.rb
@@ -7,6 +7,14 @@ class SoftServe < Formula
   license "MIT"
   head "https://github.com/charmbracelet/soft-serve.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3c5e8470f089f309f0790ff844b8c9e9d163b4befa26073aa3bcc3fc639ee387"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "35ee69080c49c0a54627bc81008b58e3fb586f205d1853c8052254710c5b64b8"
+    sha256 cellar: :any_skip_relocation, ventura:       "f3118af3237c82d9973d046f5e41e027c85a2aec7c987406312d229f12bc548b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc097e6e26ae6c3cfc1cb57b43ad6ed21e18604f67bdc06187e365605fa89230"
+  end
+
   depends_on "go" => :build
 
   def install
