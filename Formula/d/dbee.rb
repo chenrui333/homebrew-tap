88caf463f29cfbff9250ec8734d819ac35d211a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-11 21:43:22 +0000

    dbee: add 0.2.2 bottle.

--- Formula/d/dbee.rb
+++ Formula/d/dbee.rb
@@ -6,6 +6,14 @@ class Dbee < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/murat-cileli/dbee.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "886526a70a83380147335f8914b9766de45cc74fbb7e73bd3dca75ae09537695"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "753f95855d36463a4043da50067e501e3da806c2a41ace3e64f5d2cb3791d8fd"
+    sha256 cellar: :any_skip_relocation, ventura:       "5714ad5a05eb7a62b721b77128e08b1f2d2cb53eb5ed391dd395da76b1540ab6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49c4f46a8537b352c66ac0c6045afc5fbff56bcdefdd2693634f8c394a929ec6"
+  end
+
   depends_on "go" => :build
 
   def install
