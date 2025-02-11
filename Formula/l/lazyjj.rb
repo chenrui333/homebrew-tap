a631fff8a2c54faaf19c231856e4fc94d7b14098
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-11 22:05:44 +0000

    lazyjj: add 0.4.2 bottle.

--- Formula/l/lazyjj.rb
+++ Formula/l/lazyjj.rb
@@ -7,6 +7,14 @@ class Lazyjj < Formula
   license "Apache-2.0"
   head "https://github.com/Cretezy/lazyjj.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "241a612eae41ac43e14a43f42717425dabb47f8809229d6b89fb0f20a853fa7c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7353750e4ab9b2301814ff0ea8f520b1f3647ce6813fa23021565a6deccbb57f"
+    sha256 cellar: :any_skip_relocation, ventura:       "38ab3f37e152d6e9fcbc7c03c7c1d71fd28fbb0048694a7df4dfde05618cbd73"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d471e22a80e25fda766bce58cfd4559596b60c41bbc574bb133da58256c1eadc"
+  end
+
   depends_on "rust" => :build
   depends_on "jj"
 
