3d22f821b8bcaeaf2165a1806ccd0ada0eb48bda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-11 21:06:08 +0000

    cloudlens: add 0.1.4 bottle.

--- Formula/c/cloudlens.rb
+++ Formula/c/cloudlens.rb
@@ -7,6 +7,14 @@ class Cloudlens < Formula
   license "Apache-2.0"
   head "https://github.com/one2nc/cloudlens.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7e651f79bb14827e279d544136f2280989c6c49ac9b95c9f4557d638e57c202c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f964aefbb52d1c18a718569d82b4c6e0ed52c53efbabbc6a0b129911d698b2c0"
+    sha256 cellar: :any_skip_relocation, ventura:       "76fb4fc8f6040e138bf7cd3a9d56f48792ebd83dccd275b5eaba045911816f7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a100349310ca0e076bbce82f717ec1004da943cbe8f9936ecda2362453900765"
+  end
+
   depends_on "go" => :build
 
   def install
