508923b0de57e1b174a0c45852c6b229ac5578c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-11 10:43:34 +0000

    cargo-spellcheck: update 0.15.2 bottle.

--- Formula/c/cargo-spellcheck.rb
+++ Formula/c/cargo-spellcheck.rb
@@ -8,10 +8,10 @@ class CargoSpellcheck < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "58bc78ca33ace41a7d631359260b566b1128636ad55c030b6d17783f5a41cad8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "79086404ea755424338fa807c71c374c86094cd411c20aec735113102352bbc6"
-    sha256 cellar: :any_skip_relocation, ventura:       "4fcfa2fb403fa89ca170f18d0d1889abc93e678e5caedf07699345a07e677464"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d59fad1895735757f01f608f137dd8d9a6a94118d96639085aa519fc8fb0a77c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80cb73e7f3ac8642ed2ca9597c49c803ceb7f6b7b4bb891157b21118cef5004e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2be54c5f36d186f14cc5a5bc54bea7e8aafe477706e1b30189e65796967e6308"
+    sha256 cellar: :any_skip_relocation, ventura:       "c56f6662bd68cbe7928308a4d887bd05c21a9bfc5894a4cbfee2c88c5ff2d893"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "82dfc847976da783ad3f86568b09a31cc18c60319a4281e7496ad35e57be5835"
   end
 
   depends_on "llvm" => :build
