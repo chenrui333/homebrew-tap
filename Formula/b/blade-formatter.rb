82d4b963f081de4ec1bc9302be93e1b98b3929cf
Rui Chen <rui@chenrui.dev>
2025-02-12 22:34:39 -0500

    blade-formater 1.4.2 (new formula)
    Closes #196.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/b/blade-formatter.rb
@@ -0,0 +1,37 @@
+class BladeFormatter < Formula
+  desc "Opinionated blade template formatter for Laravel that respects readability"
+  homepage "https://github.com/shufo/blade-formatter"
+  url "https://registry.npmjs.org/blade-formatter/-/blade-formatter-1.42.2.tgz"
+  sha256 "af46f2340f5076aef1636140c53901228e6b36f6585509a39a0bfa1079cba141"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"bin/blade-formatter"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/blade-formatter --version")
+
+    (testpath/"test.blade.php").write <<~BLADE
+      <div>
+      @if (true)
+      <p>Hello, World!</p>
+      @endif
+      </div>
+    BLADE
+
+    system bin/"blade-formatter", "--write", "test.blade.php"
+    expected_content = <<~BLADE
+      <div>
+          @if (true)
+              <p>Hello, World!</p>
+          @endif
+      </div>
+    BLADE
+
+    assert_equal expected_content, (testpath/"test.blade.php").read
+  end
+end
