a763f6697aa675e1a73abba75ba345fbac1846db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 00:20:15 +0000

    asmfmt: add 1.3.2 bottle.

--- Formula/a/asmfmt.rb
+++ Formula/a/asmfmt.rb
@@ -5,6 +5,14 @@ class Asmfmt < Formula
   sha256 "4bb6931aefcf105c0e0bc6d239845f6350aceba5b2b76e84c961ba8d100f8fc6"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "20383fb7390daf782baec428f06559feb79550eb0aca0406b24d0e5fd0b61501"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d969c7ae2c5f634bba66267b11cd402e02c490640479556a935b6ed56b88ea36"
+    sha256 cellar: :any_skip_relocation, ventura:       "73d0bffd8e9bff94127b6467e0322ea88ddff0f442e52de9478d697898fde968"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c7f7c91a5f5009a39df2be9d07c99e6cd4927e17ef1b2499566d36db51715289"
+  end
+
   depends_on "go" => :build
 
   def install
