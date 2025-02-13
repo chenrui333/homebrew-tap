749cfee9940e74241ec3e3446c35f10dce1e0b86
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 23:23:42 +0000

    mdslw: add 0.15.0 bottle.

--- Formula/m/mdslw.rb
+++ Formula/m/mdslw.rb
@@ -7,6 +7,14 @@ class Mdslw < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/razziel89/mdslw.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8b8f0f4bea50e64f1caaee1426738129d8f4fd4bfa3c33ae3025b07b7d417dde"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "01c2adf47dc0faf3da697f4cdcfef8aa48faeb5874c22c327816e03e58d1898f"
+    sha256 cellar: :any_skip_relocation, ventura:       "0d122247fbdf064c3a45598bf21233154e69b56946cb54272ebc33913aa296c2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b3fc3c40c9cac5bb49beceddc03e906e26045c28ad5f38817830d2a15855b6f"
+  end
+
   depends_on "rust" => :build
 
   def install
