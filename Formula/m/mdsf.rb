3a1543c93d7b207897f9ee9cb2cdc48fb83eb20f
Rui Chen <rui@chenrui.dev>
2025-02-13 17:51:16 -0500

    mdsf 0.5.1 (new formula)
    Closes #216.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/m/mdsf.rb
@@ -0,0 +1,37 @@
+# framework: clap
+class Mdsf < Formula
+  desc "Format, and lint, markdown code snippets using your favorite tools"
+  homepage "https://github.com/hougesen/mdsf"
+  url "https://github.com/hougesen/mdsf/archive/refs/tags/v0.5.1.tar.gz"
+  sha256 "0dc50a2e800548bf87d9abd9a6fdebdd1abda707a621d87dbaad0be821368f05"
+  license "MIT"
+  head "https://github.com/hougesen/mdsf.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "mdsf")
+
+    generate_completions_from_executable(bin/"mdsf", "completions")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mdsf --version")
+
+    (testpath/"test.md").write <<~MARKDOWN
+      ```python
+      print( "Hello, World!" )
+      ```
+    MARKDOWN
+
+    system bin/"mdsf", "format", "test.md"
+
+    expected_content = <<~MARKDOWN
+      ```python
+      print( "Hello, World!" )
+      ```
+    MARKDOWN
+
+    assert_equal expected_content, (testpath/"test.md").read
+  end
+end
