1099ca9afad4fed048c088ca0435719cfd31c485
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 03:43:03 +0000

    blade-formatter: add 1.42.2 bottle.

--- Formula/b/blade-formatter.rb
+++ Formula/b/blade-formatter.rb
@@ -5,6 +5,14 @@ class BladeFormatter < Formula
   sha256 "af46f2340f5076aef1636140c53901228e6b36f6585509a39a0bfa1079cba141"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e47189ad9a990dfe729ee7770b4a3303a90897db6c8539bbd99e47f03a6de18"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4ffb8fd1f5cd322787b6b608c99bbfd438e4db2eb6362c8149c17856e0fe7928"
+    sha256 cellar: :any_skip_relocation, ventura:       "b7477b8626ede7203f927904890a2b9df9988b9029f51bcb6930f6d87103cc6e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55356aa8202a07c251c0e22e68d45dc8f666fe75b97e94a855dbe563047ec50c"
+  end
+
   depends_on "node"
 
   def install
