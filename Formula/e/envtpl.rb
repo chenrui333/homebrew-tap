49e7e3b4f85f306eaf870c70d7fb93cb7a7d0962
Rui Chen <rui@chenrui.dev>
2025-02-13 14:34:09 -0500

    envtpl 2.0.4

--- Formula/e/envtpl.rb
+++ Formula/e/envtpl.rb
@@ -1,8 +1,8 @@
 class Envtpl < Formula
   desc "Render Go templates on the command-line with shell environment variables"
   homepage "https://github.com/chenrui333/envtpl"
-  url "https://github.com/chenrui333/envtpl/archive/refs/tags/v2.0.3.tar.gz"
-  sha256 "60d82242c64b577fb4522fc49da8cf25582c8ddb6b1c4905d8d895acacac0d20"
+  url "https://github.com/chenrui333/envtpl/archive/refs/tags/v2.0.4.tar.gz"
+  sha256 "162e968db5149c57996d79b38ae78ccebb6b551a16d77cc3075c2ba897b68fdb"
   license "MIT"
   head "https://github.com/chenrui333/envtpl.git", branch: "main"
 
