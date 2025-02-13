bbc599d6a31cbd746bf588eac60b2a76c28b11f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 01:54:44 +0000

    bacon-ls: add 0.15.0 bottle.

--- Formula/b/bacon-ls.rb
+++ Formula/b/bacon-ls.rb
@@ -6,6 +6,14 @@ class BaconLs < Formula
   license "MIT"
   head "https://github.com/crisidev/bacon-ls.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "91dc8a3f3cd5addd65a801c58b868d85cc8de298594563a3b475e9de91b35f29"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "da746058ce09b540d709533c390fc50d4e0bbec34e2df485cc3020981366700d"
+    sha256 cellar: :any_skip_relocation, ventura:       "b80329abbc4dfeadf028c79de44e4395cf862b58f65393dd3fbd569d1ae77d56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ea5be7eb9754de173e7ff952bcb4e6b86b0416ddfe4e70d5ed22b8767554b279"
+  end
+
   depends_on "rust" => :build
 
   def install
