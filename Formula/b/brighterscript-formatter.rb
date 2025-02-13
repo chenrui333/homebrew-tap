6dc792b61e88418fa568aca830ee4175293eaf7e
Rui Chen <rui@chenrui.dev>
2025-02-12 22:50:47 -0500

    brighterscript-formatter 1.7.10 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/b/brighterscript-formatter.rb
@@ -0,0 +1,34 @@
+class BrighterscriptFormatter < Formula
+  desc "Code formatter for BrighterScript (and BrightScript)"
+  homepage "https://github.com/rokucommunity/brighterscript-formatter"
+  url "https://registry.npmjs.org/brighterscript-formatter/-/brighterscript-formatter-1.7.10.tgz"
+  sha256 "e65a2cab2fb557cb0460abe6f47a1d41b9819e3301b3031e0b831779964ff1ee"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"bin/brighterscript-formatter"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/brighterscript-formatter --version")
+
+    (testpath/"test.bs").write <<~BRIGHTERSCRIPT
+      sub Main()
+      print "Hello, World!"
+      end sub
+    BRIGHTERSCRIPT
+
+    system bin/"brighterscript-formatter", "--write", testpath/"test.bs"
+
+    expected_content = <<~BRIGHTERSCRIPT
+      sub Main()
+          print "Hello, World!"
+      end sub
+    BRIGHTERSCRIPT
+
+    assert_equal expected_content, (testpath/"test.bs").read
+  end
+end
