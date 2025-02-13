266f791bbae0888a557dd37a903496fdd70f1861
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-13 23:16:02 +0000

    mdsf: add 0.5.1 bottle.

--- Formula/m/mdsf.rb
+++ Formula/m/mdsf.rb
@@ -7,6 +7,14 @@ class Mdsf < Formula
   license "MIT"
   head "https://github.com/hougesen/mdsf.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "06fb55311bb9b59937c2a701dda2ca891b299e8dd86e66cb431c6df7a8147e4c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "402a890c06c6902b3d9df0f1d5222787a4c11461131160d7796408c510218f2b"
+    sha256 cellar: :any_skip_relocation, ventura:       "012f10b52386625f13f786b13aa640fea0e4db5980ca38681e582696f7631d8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a73a3f02c6bb1f3da78b5e645530e66c83e6c0c439c04883dc5a8634cc3e2d07"
+  end
+
   depends_on "rust" => :build
 
   def install
