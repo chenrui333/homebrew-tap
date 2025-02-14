e46f9cce89cd81f678c3da8fcc2a212bb2771c65
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-14 04:31:44 +0000

    rustywind: add 0.23.1 bottle.

--- Formula/r/rustywind.rb
+++ Formula/r/rustywind.rb
@@ -6,6 +6,14 @@ class Rustywind < Formula
   license "Apache-2.0"
   head "https://github.com/avencera/rustywind.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "361bf6fe91ef6cde756450fa5a5eda41f749d70f27fe6f51643460359fcec8c8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b83780bcda68c96802dce615f8238601c0594a2896f793c346d776c5884447f5"
+    sha256 cellar: :any_skip_relocation, ventura:       "5008a6817081c8e5e08b192968cef2621bf2f22af8bfa88d07d0bbff606521d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f2ad5575d5dba4fcb9268dda429b957617ceb71d844b52abc765ca34c58efab"
+  end
+
   depends_on "rust" => :build
 
   def install
