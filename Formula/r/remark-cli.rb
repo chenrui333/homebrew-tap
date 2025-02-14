1ca66d0adea87e2b74719f1fa3c2dddbb340292d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-14 06:33:59 +0000

    remark-cli: add 12.0.1 bottle.

--- Formula/r/remark-cli.rb
+++ Formula/r/remark-cli.rb
@@ -5,6 +5,14 @@ class RemarkCli < Formula
   sha256 "e100dfaffa5b3a50312313391fe493d2d51693f337c4035f6edd4f7090c64815"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6e6b3e99175230241fa58cc0c2daa684ebb83633d849fa88cf53157148faf1fc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fb35085f98c82c869accfa74748d89592dda1ab8f0e5e714bffffd58c4ff88ac"
+    sha256 cellar: :any_skip_relocation, ventura:       "ec35a1e1d97ec837bb744f6a78127b320f800cbed9bca2db629c4ad1761cbf73"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c9f590233222ed32b89476a10b627117d6bb2ae2eb544e4d01d0508ccf69027e"
+  end
+
   depends_on "node"
 
   def install
