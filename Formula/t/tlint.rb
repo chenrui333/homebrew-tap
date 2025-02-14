11fa84c86eea4d94ce24c7bfcf48d0ca02819865
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-14 06:19:55 +0000

    tlint: add 9.4.0 bottle.

--- Formula/t/tlint.rb
+++ Formula/t/tlint.rb
@@ -6,6 +6,14 @@ class Tlint < Formula
   license "MIT"
   head "https://github.com/tighten/tlint.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "977e11fd8d9b94e70467b715e9b0758657dbf0dec343d6426eee45419604571e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "656054ca4d546f8824c5c275f4cc90708e6591522927aa19ab0fe752ec92ffac"
+    sha256 cellar: :any_skip_relocation, ventura:       "791d898302a45c897aaaf8d34e4d04dbefd34ca34a833bc5b1b446635f51befe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4865fc52f823048237dba46a230f5b36a9719fc5379494d4e582870483a91cce"
+  end
+
   depends_on "composer" => :build
   depends_on "php"
 
