a451a082f547eee9a3dd3374c7b8684a44da2763
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-15 22:55:09 +0000

    pgdog: add 0.1.0 bottle.

--- Formula/p/pgdog.rb
+++ Formula/p/pgdog.rb
@@ -11,6 +11,14 @@ class Pgdog < Formula
     skip "no tagged releases"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c5d2a04b006ab3a58a3eddba4815de2a429f3402b7f9a24fdd8a3ac7ba8f6bf3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "596d623a069d26b1ca3463a549f701d3fda99448fa0af7c9e978587df74ee35a"
+    sha256 cellar: :any_skip_relocation, ventura:       "ddaf67d03b285050da4c147ce99f7179aa61be5398efaae61d784a2e522f1f4e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7bca9caed9f75c30693f1c1be41913851c4ed7d32d8a32480c5c4f721169e57a"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
