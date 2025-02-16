c1e39650395669efbdde39288c671ca37d8b790c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-16 20:50:22 +0000

    gitman: add 1.2.0 bottle.

--- Formula/g/gitman.rb
+++ Formula/g/gitman.rb
@@ -6,6 +6,14 @@ class Gitman < Formula
   license "Apache-2.0"
   head "https://github.com/pyrod3v/gitman.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "27fcff2b6080b93465ac715c6e16bc003740db6f45d1985be5776d5682c2b601"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a9513aac425d8987466e77bed4407b0f25b474f79cf5687296cc9b1c837474f7"
+    sha256 cellar: :any_skip_relocation, ventura:       "219acb8e09d9bf734e61acac25a5c3834332ca733b44f0d9f1a15ccad1abc657"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5e5481b5f31509ce0adff296d609b6d8f8044447d810cdb2c8401aab61649c1"
+  end
+
   depends_on "go" => :build
 
   def install
