04c5189e095aa3dcadbfa9d36ba3b43a39ea333a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-16 19:58:07 +0000

    gowall: add 0.2.0 bottle.

--- Formula/g/gowall.rb
+++ Formula/g/gowall.rb
@@ -7,6 +7,14 @@ class Gowall < Formula
   license "MIT"
   head "https://github.com/Achno/gowall.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1d0fc03d62900eeaeefc878cc814681a883ba537fe11c17a64fd4dbb6d92196c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cac6262efd42fa86feb44960d32e28ddc5cfd5b84b372b06775b8c72a4bfb731"
+    sha256 cellar: :any_skip_relocation, ventura:       "1e92d1615ef7a4e80c67ade61cbd5d562c98b15a22ffeca80e92393831576bf5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46a92199b94937d2b99aca00138a4e8653f0ad3d4427ad33668608ca5c3eefdd"
+  end
+
   depends_on "go" => :build
 
   def install
