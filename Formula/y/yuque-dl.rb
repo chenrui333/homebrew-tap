0d9f0be58e5ae2253a0c28194470028af5f99b8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-16 07:04:38 +0000

    yuque-dl: add 1.0.74 bottle.

--- Formula/y/yuque-dl.rb
+++ Formula/y/yuque-dl.rb
@@ -5,6 +5,14 @@ class YuqueDl < Formula
   sha256 "479067e978a98c8063584d9b9916ea8ade4f559fd23557a66a07e7014c03c090"
   license "ISC"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_sequoia: "841ae0e14b1c47057cf268379484909deab16ce22c19699f42881adcea05cc6a"
+    sha256 cellar: :any,                 arm64_sonoma:  "5359606e8d57e9cb14999f08d8f472f5bc445ca3b571743512500e7936c7f5f4"
+    sha256 cellar: :any,                 ventura:       "7a0058ec3f67473cd09ed68f5c6f5c88c3c9294df4188a91fb9c50b24bc3db36"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "525ae8944bd844f0296355e73e506ebea043f20372517a50138cea5215d60510"
+  end
+
   depends_on "node"
 
   def install
