51674b1abf1063e51a1c15e61ca84594af615d29
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 12:33:31 +0000

    backport: update 9.6.5 bottle.

--- Formula/b/backport.rb
+++ Formula/b/backport.rb
@@ -7,10 +7,10 @@ class Backport < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3d3fcec3c6578ce02de07651e145c10fbf1166a454535414e8a04cac3e6f16ae"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0f4750670ebd095630030d4733e9d1ca245ff83a25a1aec1c657e6d04e9b54ce"
-    sha256 cellar: :any_skip_relocation, ventura:       "0c3a6fe800b8803cd6a5cb3cee5ef85a3e152ce752e8bdde14e7d72ce637262d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a9204d4d7efa0d573411bf3fbbeb1c7552a6d8e6996866d4767e947c007caa0f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fde28c0cbb05e56b90748244eefb326f865e7980ac695d07f154270e8a126feb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a63f6c57a36cf98c4dc03a1112c29fd2a138655b3ef60814dbc5cae40fd339c9"
+    sha256 cellar: :any_skip_relocation, ventura:       "f9031e46eb014396479fca82fc3bc4728ab8645cc78e5fca6795da76e98255f6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "056cb232bff00b6c37ecf19a8e492457239296d2d6ad24a92b2a6cf49e1a03ac"
   end
 
   depends_on "node"
