c8df9d666ba1673e2ef6f2411450207c6d7007af
Rui Chen <rui@chenrui.dev>
2025-02-17 01:09:30 -0500

    cai 0.8.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/cai.rb
@@ -0,0 +1,23 @@
+# framework: clap
+class Cai < Formula
+  desc "CLI tool for prompting LLMs"
+  homepage "https://github.com/ad-si/cai"
+  url "https://github.com/ad-si/cai/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "30602c2204ed4e5e78e483e8cf783e18163847f013c4992ec552bcde7bdc0a96"
+  license "ISC"
+  head "https://github.com/ad-si/cai.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    output = shell_output("#{bin}/cai anthropic claude-opus Which year did the Titanic sink 2>&1", 1)
+    assert_match "An API key must be provided", output
+
+    output = shell_output("#{bin}/cai ollama llama3 Which year did the Titanic sink 2>&1", 1)
+    assert_match "error sending request for url", output
+  end
+end
