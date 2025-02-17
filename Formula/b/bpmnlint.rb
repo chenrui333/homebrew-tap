971ea5601e2c34827c725b0c32282e5a8af8f518
Rui Chen <rui@chenrui.dev>
2025-02-17 01:27:22 -0500

    bpmnlint 11.0.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/b/bpmnlint.rb
@@ -0,0 +1,33 @@
+class Bpmnlint < Formula
+  desc "Validate BPMN diagrams based on configurable lint rules"
+  homepage "https://github.com/bpmn-io/bpmnlint"
+  url "https://registry.npmjs.org/bpmnlint/-/bpmnlint-11.0.0.tgz"
+  sha256 "a85d578145f707575a82485fd6c05861e577e459b13de605f588ff27108e1cc3"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"bin/bpmnlint"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bpmnlint --version")
+
+    system bin/"bpmnlint", "--init"
+    assert_match "\"extends\": \"bpmnlint:recommended\"", (testpath/".bpmnlintrc").read
+
+    (testpath/"diagram.bpmn").write <<~XML
+      <?xml version="1.0" encoding="UTF-8"?>
+      <bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" id="Definitions_1">
+        <bpmn:process id="Process_1" isExecutable="false">
+          <bpmn:startEvent id="StartEvent_1"/>
+        </bpmn:process>
+      </bpmn:definitions>
+    XML
+
+    output = shell_output("#{bin}/bpmnlint diagram.bpmn 2>&1", 1)
+    assert_match "Process_1     error  Process is missing end event   end-event-required", output
+  end
+end
