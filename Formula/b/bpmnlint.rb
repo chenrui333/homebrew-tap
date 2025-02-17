e19478ba65320d052626c58d7391aa9e0f63c35f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 06:35:01 +0000

    bpmnlint: add 11.0.0 bottle.

--- Formula/b/bpmnlint.rb
+++ Formula/b/bpmnlint.rb
@@ -5,6 +5,14 @@ class Bpmnlint < Formula
   sha256 "a85d578145f707575a82485fd6c05861e577e459b13de605f588ff27108e1cc3"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7e6f7370ead5e14a25558fcc7cd975544fb43d2b84478b22d1f5a117b7863505"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "374169ff1f9b0e61789c462627eb092215599dbca0743256385ef8b756aad855"
+    sha256 cellar: :any_skip_relocation, ventura:       "9139def10201cc02d0d2e70eb46c96b3c956f6435fcb9e83d157d8728c2483cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a626ee9ac540de65eaa22195dfb0c7038b2f210520e737db0dd241c8ba3a3515"
+  end
+
   depends_on "node"
 
   def install
