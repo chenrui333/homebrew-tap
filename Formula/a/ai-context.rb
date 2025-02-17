5a871a72f6588e4e63d0ceefd4f668d64228fc6f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 04:28:27 +0000

    ai-context: update 1.3 bottle.

--- Formula/a/ai-context.rb
+++ Formula/a/ai-context.rb
@@ -9,10 +9,10 @@ class AiContext < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "934cff1d7b31a10db911c317a4af8dc40a096f5ffae0a4bd019aea7ff5321523"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1437828f5e3434865905a23f5fec3d6bd4597972c9f602c9c78c6d765ec5fbdd"
-    sha256 cellar: :any_skip_relocation, ventura:       "9efd4bb6fdd0c2a2d845f5e26b18f5f5d043903c58edf71ea932779a8514eba4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cd921eb8718dab64bdc4339cf17e3af07667d7323dcd6ffa9cb0509e0c0e0bef"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ee738f6f3a5da876db048cf341c9e34aee564f5d2b6a2075fdc1cb324359ef7b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4db731add349f402a021a9e1683312199bf5b2565dbb5c9d28adbb1ae466e0a7"
+    sha256 cellar: :any_skip_relocation, ventura:       "33789b0e730b07a72b99ab2af3fc261ca9be5cab8dd48f33d8b1b5d2a79c75e0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4b175152be5603378e881edff62ebaa3b6e93eb298cdb1f0886dcde5bab4aba4"
   end
 
   depends_on "go" => :build
