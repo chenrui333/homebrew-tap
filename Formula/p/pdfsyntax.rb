0fe8f4a7603efc7218df5de66da65c427b0cb298
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 04:04:20 +0000

    pdfsyntax: add 0.1.4 bottle.

--- Formula/p/pdfsyntax.rb
+++ Formula/p/pdfsyntax.rb
@@ -7,6 +7,14 @@ class Pdfsyntax < Formula
   sha256 "6969b60ba1214325b35d2a176310078b13ac01aed81643d1160f6fa93e19a114"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "30e8d697324cd44bb0fdcf82759a744b132953daf5d070719f03fc85af250f5f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e37665ecb685c3909891a7417c4de4a46a438831c202614bb1823628bef54cc8"
+    sha256 cellar: :any_skip_relocation, ventura:       "c42ef578ea1b199fc194b0720705b1063aa704da2c000dfbb4ccc06a4d25cef1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ef994d9d05416bc5016ecadfc00abf56ffee7ff4e11cfcb667389551435766e0"
+  end
+
   depends_on "python@3.13"
 
   def install
