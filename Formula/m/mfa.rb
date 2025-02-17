80faa1b6bf3f320da53fbcced66aad8c178b4be3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 06:42:17 +0000

    mfa: add 0.0.9 bottle.

--- Formula/m/mfa.rb
+++ Formula/m/mfa.rb
@@ -7,6 +7,14 @@ class Mfa < Formula
   license "MIT"
   head "https://github.com/k-saiki/mfa.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b6e96dbd4d25bb6c936e41a5d57b1c3072b8c642c058d77d86b823e891b1bdc4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "29ed873954dd2aa75b3f93f8ddb1771ba3647a04c16b31d4ec6245ef33212b69"
+    sha256 cellar: :any_skip_relocation, ventura:       "820e2f70b78ed1b1af2aa47c4222174c23f4bf19de4a0d3f888a7585e563efef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b6a85c9d1868908c71cd44bd5ba5fb1c1883a25b81c0300559d49a7c49a0a8e"
+  end
+
   depends_on "go" => :build
 
   def install
