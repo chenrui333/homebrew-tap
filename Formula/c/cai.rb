153a42bae7ca31633fe2a17cf8edf98000940403
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 06:49:54 +0000

    cai: add 0.8.0 bottle.

--- Formula/c/cai.rb
+++ Formula/c/cai.rb
@@ -7,6 +7,14 @@ class Cai < Formula
   license "ISC"
   head "https://github.com/ad-si/cai.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d5b517d494253346580da00c7f4d07e4d4ee6458195b60fdfb70f69b857e19c8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "591f68053de5f77972d579b33017e32e9e58a9fc6eff88b7f023ad499b4a21ae"
+    sha256 cellar: :any_skip_relocation, ventura:       "dd316b641a0231c22fa2f9de69e74cd76bec324564b285136668d6cda8a4734a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d17530b699de505002bea9c2789f1608fa368ffcc29ce853c7f6cadb50316673"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
