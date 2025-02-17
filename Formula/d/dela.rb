3fdd88c211300bf43c255abb446999b3866714d7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-17 19:26:07 +0000

    dela: add 0.0.2 bottle.

--- Formula/d/dela.rb
+++ Formula/d/dela.rb
@@ -5,6 +5,14 @@ class Dela < Formula
   sha256 "767edcc040a536a0143905791466035b6f6614fced56e5bc38dbd40c0a5d419a"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f9a76d0ebc74cd04023241263d6282e95d1de99de2e4e9adca2ca5567479edb3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e1c94899f560fed7f354fb05e8215088dd5b96c4cdc3c86e8b85aa67d2f3ba0d"
+    sha256 cellar: :any_skip_relocation, ventura:       "b08823a2bbe19ebe3941141540d3c3dfef147898527394770809a62047a0ff80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9fb96955c6be4fc9b9f8c5fa19f7a35ad9a91f578a7e823ebe8601a9c6831ab2"
+  end
+
   depends_on "rust" => :build
 
   def install
