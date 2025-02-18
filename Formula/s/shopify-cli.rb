fd761c6d81b974ef90e93ab7bafad83775700735
Rui Chen <rui@chenrui.dev>
2025-02-18 01:01:38 -0500

    shopify-cli 3.75.3 (new formula)
    Closes #292.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/s/shopify-cli.rb
@@ -0,0 +1,20 @@
+class ShopifyCli < Formula
+  desc "CLI which helps you build against the Shopify platform faster"
+  homepage "https://shopify.dev/"
+  url "https://registry.npmjs.org/@shopify/cli/-/cli-3.75.3.tgz"
+  sha256 "9e944cb30eebe702c877f7b926e7f214615d89f88d096dff60df4475945e4144"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"bin/shopify"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/shopify --version")
+
+    assert_match "app build", shell_output("#{bin}/shopify commands")
+  end
+end
