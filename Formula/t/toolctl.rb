d63a1cc148a1c07732c1ebd990566a68c7ff41eb
Rui Chen <rui@chenrui.dev>
2025-02-17 20:22:15 -0500

    toolctl 0.4.11 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/toolctl.rb
@@ -0,0 +1,30 @@
+class Toolctl < Formula
+  desc "Tool to control your tools"
+  homepage "https://github.com/toolctl/toolctl"
+  url "https://github.com/toolctl/toolctl/archive/refs/tags/v0.4.11.tar.gz"
+  sha256 "6b07e94e661790d6ef78b83c3617d4579b6d648544a5320b033169f272b73b6f"
+  license "MIT"
+  head "https://github.com/toolctl/toolctl.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/toolctl/toolctl/internal/cmd.gitVersion=#{version}
+      -X github.com/toolctl/toolctl/internal/cmd.gitCommit=#{tap.user}
+      -X github.com/toolctl/toolctl/internal/cmd.buildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"toolctl", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/toolctl --version")
+
+    assert_match "toolctl", shell_output("#{bin}/toolctl list")
+    output = shell_output("#{bin}/toolctl info 2>&1")
+    assert_match "The tool to control your tools", output
+  end
+end
