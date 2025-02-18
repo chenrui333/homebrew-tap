f1a7e36ed123df9176e17ff47d40a591afca9ec5
Rui Chen <rui@chenrui.dev>
2025-02-17 20:12:05 -0500

    mermaid-cli 11.4.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/m/mermaid-cli.rb
@@ -0,0 +1,37 @@
+class MermaidCli < Formula
+  desc "CLI for Mermaid library"
+  homepage "https://github.com/mermaid-js/mermaid-cli"
+  url "https://registry.npmjs.org/@mermaid-js/mermaid-cli/-/mermaid-cli-11.4.2.tgz"
+  sha256 "5a46e845cd6cd05ac9ba5172acdadbe3daa878d8af4a15213b374fa5de96f0de"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    node_modules = libexec/"lib/node_modules/@mermaid-js/mermaid-cli/node_modules"
+
+    # Remove incompatible pre-built `bare-fs`/`bare-os` binaries
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    node_modules.glob("{bare-fs,bare-os}/prebuilds/*")
+                .each { |dir| rm_r(dir) if dir.basename.to_s != "#{os}-#{arch}" }
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mmdc --version")
+
+    (testpath/"diagram.mmd").write <<~EOS
+      graph TD;
+        A-->B;
+        A-->C;
+        B-->D;
+        C-->D;
+    EOS
+
+    output = shell_output("#{bin}/mmdc -i diagram.mmd -o diagram.svg 2>&1", 1)
+    assert_match "Could not find Chrome", output
+  end
+end
