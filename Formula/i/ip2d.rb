806589b17231af7e9b4bd45705ad477c7a72aab9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-18 00:30:47 +0000

    ip2d: add 1.0.0 bottle.

--- Formula/i/ip2d.rb
+++ Formula/i/ip2d.rb
@@ -6,6 +6,14 @@ class Ip2d < Formula
   license "MIT"
   head "https://github.com/0xflotus/ip2d-rust.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80803223ecfd3e79dd9bd409a78b1e20389d9cbfadf2176564dc41837a940aab"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7e9870f333c24f2b5543f436ba888adca6eeea44e5b1b67b1831b30fdc8d3ad3"
+    sha256 cellar: :any_skip_relocation, ventura:       "5169ab4314f78bb44772ae4d3aa749e1bea80e44b29d104283600bde7bcdf073"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19a052a5d308e058726b6cfb09bb457bfa3d55386b9441ed24f01f5d0827385e"
+  end
+
   depends_on "rust" => :build
 
   def install
