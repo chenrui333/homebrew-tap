1db6f22a34e4b296117a5dfb9d30ce08e421b0e7
Rui Chen <rui@chenrui.dev>
2025-02-18 10:21:21 -0500

    castor 0.22.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/castor.rb
@@ -0,0 +1,32 @@
+class Castor < Formula
+  desc "DX-oriented task runner and command launcher built in PHP"
+  homepage "https://castor.jolicode.com/"
+  url "https://github.com/jolicode/castor/archive/refs/tags/v0.22.1.tar.gz"
+  sha256 "5fc23c0d8f9d30e3d8f990ce3a9241db9bb83e01c7ebcea08c539524dc9dfbeb"
+  license "MIT"
+
+  depends_on "composer" => :build
+  depends_on "php"
+
+  def install
+    system "composer", "install", "--no-dev", "--prefer-dist", "--optimize-autoloader"
+    libexec.install Dir["*"]
+
+    # Create a wrapper script in bin that calls the installed castor binary
+    (bin/"castor").write <<~EOS
+      #!/bin/bash
+      exec php "#{libexec}/bin/castor" "$@"
+    EOS
+    chmod 0755, bin/"castor"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/castor --version")
+
+    output = shell_output("#{bin}/castor list")
+    assert_match "Available commands", output
+
+    output = pipe_output("#{bin}/castor init", "no\n")
+    assert_match "Could not find root \"castor.php\" file", output
+  end
+end
