83d6022e1ebfe141e1ffb54e94961a950e8e497d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-18 06:08:27 +0000

    shopify-cli: add 3.75.3 bottle.

--- Formula/s/shopify-cli.rb
+++ Formula/s/shopify-cli.rb
@@ -5,6 +5,14 @@ class ShopifyCli < Formula
   sha256 "9e944cb30eebe702c877f7b926e7f214615d89f88d096dff60df4475945e4144"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_sequoia: "838a4241ffa640573478be6335f9de8050cff46548903cdd4a2b8d85001fd4be"
+    sha256 cellar: :any,                 arm64_sonoma:  "a8a40285e1d102512ca22767916253a312141939cd7a67276531743780fdef8d"
+    sha256 cellar: :any,                 ventura:       "276b246339c54282f39a546fe7e6f991a38364a55383a4135ee0fbdc8895da74"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "00e58bc8a642ed93feee2fc6937e4ce8d33c1d7415f646e8f283abe4fb9f6805"
+  end
+
   depends_on "node"
 
   def install
