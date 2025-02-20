25680969cd934dd92a86a207e7011ebe2f556079
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-20 07:23:27 +0000

    pls: add 0.0.1-beta.9 bottle.

--- Formula/p/pls.rb
+++ Formula/p/pls.rb
@@ -10,6 +10,14 @@ class Pls < Formula
     regex(/^v(\d+(?:\.\d+)+(?:[._-]beta\.\d+)?)/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "efd5ac22d746a8161ec5032b8c40739f40c4a2ce129a89f51a910a82fe26a489"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8611f96a5f6270658b08fc26767a46537c17f5506121074b318f1939d93ce1c"
+    sha256 cellar: :any_skip_relocation, ventura:       "5153126f221acd8b360d35f0ed19e217420fec7314096dc549c7a713274a87fd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dbcab0d216cfd82b019700a4c31bdb90501457dbb12ca0d8f6c38d22c61d5ce9"
+  end
+
   depends_on "rust" => :build
 
   uses_from_macos "zlib"
