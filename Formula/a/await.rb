1c8d4f9353935901980226c7559ef32f752ebadb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-20 07:04:44 +0000

    await: add 1.0.8 bottle.

--- Formula/a/await.rb
+++ Formula/a/await.rb
@@ -5,6 +5,14 @@ class Await < Formula
   sha256 "727d4a96ee5d45a80dd2645dc0b13ee3facac709c3e778118259a73a88ee8243"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "344eaa819cd80b38efb874962a7c2fef4afee5e4e942e9408aadb32eb3f962cb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00e90c8fe612176435217a4842bffa01a8dd470e300b90b159af185fa7e47915"
+    sha256 cellar: :any_skip_relocation, ventura:       "be686dcefbe3659551fa5a321a154ed1c29d2030cb6d5975a8a8bbafb306c77e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45f6fc1691f05b5c906c17c189f2ab60e59792a2e84e676a1022341ab33cd4e1"
+  end
+
   def install
     # patch version
     inreplace "await.c", "1.0.9", version.to_s
