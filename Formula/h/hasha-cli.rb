0a582fa53cc60231a351ea9bcccdb5be0cab7f14
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-21 21:33:59 +0000

    hasha-cli: add 6.0.0 bottle.

--- Formula/h/hasha-cli.rb
+++ Formula/h/hasha-cli.rb
@@ -5,6 +5,14 @@ class HashaCli < Formula
   sha256 "ab5264615b9d218dfe8956e7af5a0c1b24a51b0493ca1d13339036968531ada1"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "28b555d76a765511530cf837e3dd6ef5193d913882057218638f32fff3f227e9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0059d582853dd556a5dad2922825039533bf964d6fdb944a2f7f1a3273b1be29"
+    sha256 cellar: :any_skip_relocation, ventura:       "4a4c149d132c01b910584093e20dc04c45229fe7e4a1096cca6b1ed4470eae22"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cf7dbad17221a5a7be6aa58ede1f3d24b6c987cb4278f30241e15fdc6dd0e40c"
+  end
+
   depends_on "node"
 
   def install
