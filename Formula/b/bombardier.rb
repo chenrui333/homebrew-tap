27f5dfbc102368319f387a238951206ce1333289
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-22 05:11:30 +0000

    bombardier: add 1.2.6 bottle.

--- Formula/b/bombardier.rb
+++ Formula/b/bombardier.rb
@@ -6,6 +6,14 @@ class Bombardier < Formula
   license "MIT"
   head "https://github.com/codesenberg/bombardier.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d3b598724dfca13dd47af122864bd098e862054e75be5109a982461bf78fadd3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d5bc6bf052ff1a1f2833f70bb6b4fb193b9c5d303b981505f5ff271472e6a26"
+    sha256 cellar: :any_skip_relocation, ventura:       "cdd0ef609bee5846a2b1a3556422f66819701349c0ac90000c282d5c79bd7a6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e0363490a5d9b55df4bb0ff86b312533d54588e89961d63489cba81c600fce6"
+  end
+
   depends_on "go" => :build
 
   def install
