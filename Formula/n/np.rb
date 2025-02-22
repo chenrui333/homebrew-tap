5065a24cd3b525a56331625eaef157c37f4bbef2
Rui Chen <rui@chenrui.dev>
2025-02-21 22:31:29 -0500

    np 10.2.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/n/np.rb
@@ -0,0 +1,45 @@
+class Np < Formula
+  desc "Better `npm publish`"
+  homepage "https://github.com/sindresorhus/np"
+  url "https://registry.npmjs.org/np/-/np-10.2.0.tgz"
+  sha256 "e89644fe0a2100f468f3159f402256fa4392cd5782f28fa174b42e5fd46d30ee"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec/"bin/np"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/np --version")
+
+    mkdir "test" do
+      (testpath/"test/package.json").write <<~EOS
+        {
+          "name": "test",
+          "version": "1.0.0",
+          "main": "index.js",
+          "scripts": {
+            "test": "echo 'Running tests'"
+          }
+        }
+      EOS
+
+      # Setup test git repo
+      system "git", "init", "--initial-branch", "main"
+      system "git", "config", "user.name", "Test User"
+      system "git", "config", "user.email", "test@example.com"
+      system "git", "add", "package.json"
+      system "git", "commit", "-m", "Initial commit"
+
+      (testpath/"test/index.js").write("console.log('Hello, world!');")
+      system "git", "add", "index.js"
+      system "git", "commit", "-m", "Add index.js"
+
+      output = shell_output("#{bin}/np --no-cleanup --no-publish --yolo patch")
+      assert_match "Publish a new version of test", output
+    end
+  end
+end
