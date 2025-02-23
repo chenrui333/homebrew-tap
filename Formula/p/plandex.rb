aba2e6cc5afcaefe866f88f9cd5f985f9293e906
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-23 05:30:57 +0000

    plandex: add 1.1.1 bottle.

--- Formula/p/plandex.rb
+++ Formula/p/plandex.rb
@@ -11,6 +11,14 @@ class Plandex < Formula
     regex(%r{^cli/v?(\d+(?:\.\d+)+)$}i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a1670e3dcecb566a60d3ad34e3963c254911938f533f302e91a4f9e92229868e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6d357735c7c1c56027902fb2808eb34c366cd3087043f5858ce2b9a30b3f3e4d"
+    sha256 cellar: :any_skip_relocation, ventura:       "43d740aef2a8d941dde28ae620d6f63b81b834a86be6ccf052f9ff209a9ffe0f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "baa9c77dbc4773cc3b2d6289b89ced169b49495385ec1fcc1f2dee6f44d736b8"
+  end
+
   depends_on "go" => :build
 
   def install
