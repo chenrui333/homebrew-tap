05eda8843cbb8067338194d679c1a4b226e5eac7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-23 05:59:19 +0000

    emoj: add 4.1.0 bottle.

--- Formula/e/emoj.rb
+++ Formula/e/emoj.rb
@@ -5,6 +5,14 @@ class Emoj < Formula
   sha256 "18ef4b06e9ab7dff8dfb15257bf6e9110d3c6842fa0285728d9e4e3c0a0ad298"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8dc3fa3b0f4251f4d20886334a9bbe160b195e3ecc4d3af475c5a33ebbf0daa9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a3a9a0f4c4c76caafa319e4238e4fe49175ef44b3c8eeafc08af0c155f5e67d0"
+    sha256 cellar: :any_skip_relocation, ventura:       "1ea4d68ffdf59c8d2b26d7878ac5240c4ab04f8cc3e240f0d68ec70bcc0a7ca2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b32b443c969b5cc8f13f504eda6380244793320e037d82dc655ee66a7f54ea9"
+  end
+
   depends_on "node"
 
   on_linux do
