b5a80b4b7e200cd37d444170851862f9abec52f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-23 15:51:52 +0000

    percollate: add 4.2.3 bottle.

--- Formula/p/percollate.rb
+++ Formula/p/percollate.rb
@@ -5,6 +5,14 @@ class Percollate < Formula
   sha256 "a14bba93972213434a383de1e1ce720fd53ccf67283179c14f347686ae2c0c90"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "daeca915611aed03736272e469c085e40673f2a38f53280da8fc881bed94dfaf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1c47f497a464975794248b829e2497da6f73ad32142a069775e6256e3d7b4863"
+    sha256 cellar: :any_skip_relocation, ventura:       "2f40fab462e024890696336d58b248a42be7844022b7f4c30e7aa5417be8c37e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2eef1fa701ab259e864aac4d8ac8eab107a49d0d247d50248dc6c38083a94f7"
+  end
+
   depends_on "node"
 
   def install
