3603d04dc5655db6adc17e4c7459fde42dca13f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-23 06:07:25 +0000

    vercel-serve: add 14.2.4 bottle.

--- Formula/v/vercel-serve.rb
+++ Formula/v/vercel-serve.rb
@@ -5,6 +5,14 @@ class VercelServe < Formula
   sha256 "317f5fe08ccc44f535ba593b99ac14254cc3cd8de1e4e876b284b78ad40c6c41"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aad0095b2d710033a2ffd552025618d469bd059c5316cc1dc93b09a01002a08e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "039a01cd06edf1aa8f1f55312f0a73ebaef5a2e9ae765257aee2f1c52d61675f"
+    sha256 cellar: :any_skip_relocation, ventura:       "7c0efbe7a226d1fb0be6ef6008aede15e034acf4f441b271ef04f985a28ca347"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "759fce3c368339c8573d827b964580ce7ae0e5a6ca17580829ee3769ceca151d"
+  end
+
   depends_on "node"
 
   on_linux do
