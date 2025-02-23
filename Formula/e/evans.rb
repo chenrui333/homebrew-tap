a0ca968eb95fe1563f4149fbb1055a1c66a78ee8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-23 15:41:12 +0000

    evans: add 0.10.11 bottle.

--- Formula/e/evans.rb
+++ Formula/e/evans.rb
@@ -7,6 +7,14 @@ class Evans < Formula
   license "MIT"
   head "https://github.com/ktr0731/evans.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "589043f45ce64a1e4a125c723657761ce1ee9f2c2e18138afa1f9b3d98656f46"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1324831bf766c86c33947e5c05cfc536ad661267748433bb48308739afa3fe14"
+    sha256 cellar: :any_skip_relocation, ventura:       "b185962ff6b3b87b99dca48a854856ea65a22c34316294656bf80dee1ad1b1c9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "02269ecc89f6971db5024e80296e235c6649e8b4bef895e60d9675a687f34b8e"
+  end
+
   depends_on "go" => :build
 
   def install
