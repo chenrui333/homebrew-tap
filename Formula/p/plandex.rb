816cd1fa41b06f6566ca9ae01c6e1ccd41827ca9
Rui Chen <rui@chenrui.dev>
2025-02-23 00:25:10 -0500

    plandex 1.1.1 (new formula)
    Closes #356.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/p/plandex.rb
@@ -0,0 +1,27 @@
+class Plandex < Formula
+  desc "AI driven development in your terminal. Designed for large, real-world tasks"
+  homepage "https://plandex.ai/"
+  url "https://github.com/plandex-ai/plandex/archive/refs/tags/cli/v1.1.1.tar.gz"
+  sha256 "0617ad3ae14428c1ae660facdd263e17ce7855987b3ec8c08c5c5e142195ab7f"
+  license "MIT"
+  head "https://github.com/plandex-ai/plandex.git", branch: "main"
+
+  livecheck do
+    url :stable
+    regex(%r{^cli/v?(\d+(?:\.\d+)+)$}i)
+  end
+
+  depends_on "go" => :build
+
+  def install
+    cd "app/cli" do
+      system "go", "build", *std_go_args(ldflags: "-s -w -X plandex/version.Version=#{version}")
+      generate_completions_from_executable(bin/"plandex", "completion")
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/plandex version")
+    system bin/"plandex"
+  end
+end
