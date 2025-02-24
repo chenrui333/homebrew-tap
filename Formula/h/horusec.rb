5c52a72240e4f1e164c1c821b4426bc3226e0e92
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-24 05:02:08 +0000

    horusec: add 2.8.0 bottle.

--- Formula/h/horusec.rb
+++ Formula/h/horusec.rb
@@ -6,6 +6,14 @@ class Horusec < Formula
   license "Apache-2.0"
   head "https://github.com/ZupIT/horusec.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1693b4fabee9526e7871b41744711ef25f89be6bacfc98f5421c25bf91ac74e3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c17573ecb0ad13fb69f1fd221ae0829439f65c4879501ba3a6ad83ef365ccc1"
+    sha256 cellar: :any_skip_relocation, ventura:       "abdd3013a485c880e375bca54fa7a84eb22344466f2a79f137a21ca1486b3250"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "39ffeb3c55db70f3be676e76c25378ea20ade68db6bb13942b5184de1698dcc8"
+  end
+
   depends_on "go" => :build
 
   def install
