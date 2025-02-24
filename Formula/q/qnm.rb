03762f0808219ab30fe09cb41c5078ebab790dc0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-24 04:38:06 +0000

    qnm: add 2.10.4 bottle.

--- Formula/q/qnm.rb
+++ Formula/q/qnm.rb
@@ -5,6 +5,14 @@ class Qnm < Formula
   sha256 "205044b4bbc4637917ac55f936c17b2763e622040cfa84acb1a0289b50b21098"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3547a40ae4fb9b63a789e9fe9093bb5b0904e030d5396e4e06037e2d7a34b4ca"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4a3c013b244bb4515d8e687f81df3d51de95fba4d068c5d93efe68848c277b21"
+    sha256 cellar: :any_skip_relocation, ventura:       "b4263ac8fd2bc23e9387c8f39fd13bbf8cff202e02be5ece78a835770d8e5742"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04cdb543da4d353fcb2c5d32a3ef4fc2cafbd937d30e3832177e1fcd9438befd"
+  end
+
   depends_on "node"
 
   def install
