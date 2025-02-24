8a06b85f17d98bbfa777250dfae14ad51151dffc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-24 05:06:41 +0000

    hostctl: add 1.1.4 bottle.

--- Formula/h/hostctl.rb
+++ Formula/h/hostctl.rb
@@ -7,6 +7,14 @@ class Hostctl < Formula
   license "MIT"
   head "https://github.com/guumaster/hostctl.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2a12fa09914274387cf504f8609c810019290a8109bc1849d2e3e9c745dc8956"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "81a0d5821718a65966d6f87ac2c87886fa4ae71b7a22e8e79a1fdc06e4b00330"
+    sha256 cellar: :any_skip_relocation, ventura:       "913802157946af5cc6669dfb836fddfe7266ea72fc12a918629db1dfda0572cc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ca094692b76bdbb5dbd7dbe14fd97ff4cbfaafd9369232443581d31d6261157"
+  end
+
   depends_on "go" => :build
 
   def install
