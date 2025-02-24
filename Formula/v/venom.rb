380b32c921c88e6adcbe3e2acb218d786c74aeed
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-24 05:32:04 +0000

    venom: add 1.2.0 bottle.

--- Formula/v/venom.rb
+++ Formula/v/venom.rb
@@ -7,6 +7,14 @@ class Venom < Formula
   license "Apache-2.0"
   head "https://github.com/ovh/venom.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e0c116950e759523bb1a478e818e1894b7d0fed64a958f42e8499a257a7aef38"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c45bf0f445dc8a3c6062e2013cc8e615539b45f5cf44146e2fb2cd85e5c9c338"
+    sha256 cellar: :any_skip_relocation, ventura:       "a2a68ace298cf3934a0df8c77c252b81f2f7a90d3b152d0de51a0f15c5c1b08f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf148470ac8eb306b770119f2738fbb262995832f1395387a0fe87ebdb0780d2"
+  end
+
   depends_on "go" => :build
 
   def install
