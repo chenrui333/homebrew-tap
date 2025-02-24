40b99ab9d185ae610aace5528d8d33746011a707
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-24 01:03:38 +0000

    dblab: add 0.30.1 bottle.

--- Formula/d/dblab.rb
+++ Formula/d/dblab.rb
@@ -6,6 +6,14 @@ class Dblab < Formula
   license "MIT"
   head "https://github.com/danvergara/dblab.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c67d5ea7906af3e085347b9c58de8abe22e45c8fd0b1c74f4218188d3aa22134"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "73f9a10916dfb32b13a614f0f12cfdbbd33fb3d6e8af675bcc6f5e1266856b47"
+    sha256 cellar: :any_skip_relocation, ventura:       "14fdee29e2611af2d6ac2c7cbebe3e985f19f5154e4d5c74b1174471ddbcf639"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8948f8d13030435468b83ab6d09c3048013ec8bf9a806fc6072763a55da8e8fa"
+  end
+
   depends_on "go" => :build
 
   def install
