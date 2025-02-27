538da5931562b0a58299129cb99b34264efe2c88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-02-27 20:35:30 +0000

    surgeon: update 0.1.3 bottle.

--- Formula/s/surgeon.rb
+++ Formula/s/surgeon.rb
@@ -9,10 +9,10 @@ class Surgeon < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f3b9bdf418156497b15f9849aeae5fab25f4eff245024babc811dc09698f57a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb14b841705383dc97965c698d7eca7f3ab8d9e43801e75d5dd45559c6ffcbf5"
-    sha256 cellar: :any_skip_relocation, ventura:       "05ba32bd5108bc4a60e9d9b47aa979016c25287711b8580f627740292881d429"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46b29900d7d59a7268fada0a3706d22593f44c88cc340042e14ee635875a0b04"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2ad80dc16655b2e1674e27a32cfd3244fcbf81e97da05a59d2baf9959eac6887"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62e743ecc3187b72b665791a17c4066bc73aa3dbf21fb8c522e14a4d04009ea6"
+    sha256 cellar: :any_skip_relocation, ventura:       "b75a58ea475835040ba4e9e2dd87f55547893fdfd078094fea6a011631350d55"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "344bf8c820d763f2cd4b12b43376df764ee4b78f78d85ee4abbe7fc46eb34569"
   end
 
   depends_on "go" => :build
