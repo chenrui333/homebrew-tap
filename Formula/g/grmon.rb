ae45c8fffdeefa3072ebb9bb1028651ead2c9c9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-01 06:57:25 +0000

    grmon: add 0.1 bottle.

--- Formula/g/grmon.rb
+++ Formula/g/grmon.rb
@@ -6,6 +6,14 @@ class Grmon < Formula
   license "MIT"
   head "https://github.com/bcicen/grmon.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "02bf92a0760bfbbce688798bdf453fb0f87c0d7f47acaf54beb853a0f470b39d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d0f16539d66dcc0b2312f3ed4ec48c79cbd0928c005045986a80e866186a67a"
+    sha256 cellar: :any_skip_relocation, ventura:       "6b25c35b56820782cb2289c29619d52975270bc6ed3388c579a09a4acc08f1b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4906e7e06b260cea0061a56f84f54477b70f5cb726fe3dfd9a5fa784ab8c3a8d"
+  end
+
   depends_on "go" => :build
 
   def install
