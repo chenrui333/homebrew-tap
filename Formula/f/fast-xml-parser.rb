8017824e13ed5cdfba1acd1a3d1dfe62e4b0cfdb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-01 05:18:58 +0000

    fast-xml-parser: add 5.0.8 bottle.

--- Formula/f/fast-xml-parser.rb
+++ Formula/f/fast-xml-parser.rb
@@ -5,6 +5,14 @@ class FastXmlParser < Formula
   sha256 "f6aa5152a73ef045a0011e5b5b53cbfa80000ca2fa61d20d4617c0e9da4e855a"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b388ba954b35a9c5ca0219539c0d3e49da540c065b380ce6f6ade092db1fdab"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ebe3c9710752fa68cd2e29f36c718ad6cb9314dce5e24815fe585b4299b7a6b"
+    sha256 cellar: :any_skip_relocation, ventura:       "a257f51b78301af357430fe1526b907ee9ae11ca77555a21ed01db5dbf6063ed"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1397822c67898f0ec8eb96d144c39f039f339717e326cf93c819cbb4537a44af"
+  end
+
   depends_on "node"
 
   def install
