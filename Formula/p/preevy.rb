4efa94768602ddc3947c9ee31c0147c2bfe3b608
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-01 16:39:02 +0000

    preevy: add 0.0.64 bottle.

--- Formula/p/preevy.rb
+++ Formula/p/preevy.rb
@@ -5,6 +5,14 @@ class Preevy < Formula
   sha256 "24956a8a0248bcbde2cb8462a7deab3e6b9f8465fe17566703ca5d7cd78c841a"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_sequoia: "7d0ebd2d57b55acc109497cf1a6da06fd989d1254b8dcabee03cc01726b921d3"
+    sha256                               arm64_sonoma:  "0ad50dba46d862991bfb9492ed0539aceca95e6cbbcef1cff33d632e436223aa"
+    sha256                               ventura:       "45b6c86876641dc1e74056c2c899b70d68231bf384ba91c51c928c7fb5d684b0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b830f88b375f79fe4af61ff78b082e404fbabbe0baed9f5c8350d68c6c73771f"
+  end
+
   depends_on "node"
 
   def install
