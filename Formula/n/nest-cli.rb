320304497056f96851dae522c92468159ecc4bed
Rui Chen <rui@chenrui.dev>
2025-03-01 12:05:19 -0500

    nest-cli 11.0.5 (new formula)
    Closes #414.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/n/nest-cli.rb
@@ -0,0 +1,21 @@
+class NestCli < Formula
+  desc "CLI tool for Nest applications"
+  homepage "https://nestjs.com/"
+  url "https://registry.npmjs.org/@nestjs/cli/-/cli-11.0.5.tgz"
+  sha256 "c1d24094ead26b0a00cc952e049ffbd2a6d9914da9df0d73ee464b2e03ba2415"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nest --version")
+
+    output = shell_output("#{bin}/nest info")
+    assert_match "System Information", output
+  end
+end
