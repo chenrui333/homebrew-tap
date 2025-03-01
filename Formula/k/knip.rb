84e97254250ee632c02e03e69bd5bcd3c496100a
Rui Chen <rui@chenrui.dev>
2025-03-01 12:29:17 -0500

    knip 5.45.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/k/knip.rb
@@ -0,0 +1,29 @@
+class Knip < Formula
+  desc "Declutter your JavaScript & TypeScript projects"
+  homepage "https://knip.dev/"
+  url "https://registry.npmjs.org/knip/-/knip-5.45.0.tgz"
+  sha256 "4de00a2c3295db4f0abaf898716b73a23c8fb17e903878752f43d19dc06598c6"
+  license "ISC"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/knip --version")
+
+    (testpath/"package.json").write <<~JSON
+      {
+        "name": "my-project",
+        "scripts": {
+          "knip": "knip"
+        }
+      }
+    JSON
+
+    system bin/"knip", "--production"
+  end
+end
