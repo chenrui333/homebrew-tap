23cc41256b698bd27f2fdffc7c6ace7c3a3ce4fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-01 16:08:02 +0000

    ytt: add 0.51.1 bottle.

--- Formula/y/ytt.rb
+++ Formula/y/ytt.rb
@@ -6,6 +6,14 @@ class Ytt < Formula
   license "Apache-2.0"
   head "https://github.com/carvel-dev/ytt.git", branch: "develop"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f9d4d4825c02b61faba8fc54e6214717685518127033e3a15cf82c01e12215c0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "17098c2746291775d14f90396434ea0890cb5162d7d414224fe007ebdcb375c1"
+    sha256 cellar: :any_skip_relocation, ventura:       "49f03c659a489438f09e8cc745091272ac18f119d18f81ab9872b44fda5b2a07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ef4a93ee243e6c5a9210c210e002e4da6282d69c64dbf73a26fe4d4de2d43d22"
+  end
+
   depends_on "go" => :build
 
   def install
