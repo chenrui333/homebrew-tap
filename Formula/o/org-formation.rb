7bebd286002687d04169e883438a96d1b9495deb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-02 16:21:55 +0000

    org-formation: add 1.0.16 bottle.

--- Formula/o/org-formation.rb
+++ Formula/o/org-formation.rb
@@ -5,6 +5,14 @@ class OrgFormation < Formula
   sha256 "a3d4be909939bb85b033886bbf8913ffe20e0946ad62f19a0169e2cfc8811406"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ace4c98cebc9c0da6a4a67b38f8fd04f3e0cb45ce8fa8b4da5e609827e76444e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "032746d59f8dfae760329bdea9b6f831be2afb31693fa4dd1fc2505fc9417e1e"
+    sha256 cellar: :any_skip_relocation, ventura:       "3fa19f653c2500f396a2d2dbd0e7eb4def907c833b28b9263568c118c01a9192"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1cafa2cf8c394e619b0f58177148271f90dde3699760794272076761142e3ac4"
+  end
+
   depends_on "node"
 
   def install
