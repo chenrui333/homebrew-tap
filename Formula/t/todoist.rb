4ed71d101cf9449c3440e323b2da05e845e2f90c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-02 15:04:29 +0000

    todoist: add 0.22.0 bottle.

--- Formula/t/todoist.rb
+++ Formula/t/todoist.rb
@@ -6,6 +6,14 @@ class Todoist < Formula
   license "MIT"
   head "https://github.com/sachaos/todoist.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "32b5113bcdb5387a66333644b1a69804b0a7459d5198860f7de484e85a806fc1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a92f3ad078d39d4d140ee52129b540038e1363269b9708f805fd03782d8429a3"
+    sha256 cellar: :any_skip_relocation, ventura:       "f5eceed2a5cab601adc87d9085822ebe1e0be43e8a08ade5eee0b80620328009"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1c1c8dd8fc31abff0e7e321f33c16aed1527cd72f0a645920f2b14e3c655602e"
+  end
+
   depends_on "go" => :build
 
   def install
