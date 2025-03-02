3cb3d3897162e323226224eee3c4f0a3f640a8c7
Rui Chen <rui@chenrui.dev>
2025-03-02 11:42:58 -0500

    reformat-gherkin: add shell completion support
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/reformat-gherkin.rb
+++ Formula/r/reformat-gherkin.rb
@@ -50,6 +50,9 @@ class ReformatGherkin < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"reformat-gherkin", shells:                 [:fish, :zsh],
+                                                                 shell_parameter_format: :click)
   end
 
   test do
