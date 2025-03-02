2d1b2690f82cbe7e385be5123efed237cde82574
Rui Chen <rui@chenrui.dev>
2025-03-02 11:42:07 -0500

    readmeai: add shell completion support
    Closes #443.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/r/readmeai.rb
+++ Formula/r/readmeai.rb
@@ -211,6 +211,8 @@ class Readmeai < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"readmeai", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
