cf0b5bb44cf9901884f6a29ea0769f39840ccf0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-02 14:48:12 +0000

    rang: add 3.2 bottle.

--- Formula/r/rang.rb
+++ Formula/r/rang.rb
@@ -9,6 +9,14 @@ class Rang < Formula
     skip "no rencet releases"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dc51e87d0046034c644a86254247f57234975fa10b793d09d50af1b1995bc62c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cb27d5603710a5b78a9427887243c9ecd320a928fab5cd0e1b76374ad32f5c57"
+    sha256 cellar: :any_skip_relocation, ventura:       "4c1a641de098889523d3830c61d8e1780d6674b57607806230f165d2549d8f43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "010f3396387c58cb799dc6359c134dee5955095dfad80649703658919a6b9f39"
+  end
+
   depends_on "cmake" => :build
 
   def install
