836c2eac90a85b464768f1ced004de35c0ef123f
Rui Chen <rui@chenrui.dev>
2025-03-02 11:39:15 -0500

    gtts: add shell completion support
    Closes #439.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/gtts.rb
+++ Formula/g/gtts.rb
@@ -45,6 +45,8 @@ class Gtts < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"gtts-cli", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
