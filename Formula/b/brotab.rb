931ee35420f72c674c3f274635172ee22fb3a180
Rui Chen <rui@chenrui.dev>
2025-03-02 11:33:14 -0500

    brotab: add shell completion support
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/b/brotab.rb
+++ Formula/b/brotab.rb
@@ -84,6 +84,8 @@ class Brotab < Formula
 
   def install
     virtualenv_install_with_resources
+
+    generate_completions_from_executable(bin/"brotab", shells: [:fish, :zsh], shell_parameter_format: :click)
   end
 
   test do
