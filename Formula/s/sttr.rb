d9d99ad4659afeba7b0ca0590b11c5b506d94f49
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-02 19:19:53 +0000

    sttr: add 0.2.24 bottle.

--- Formula/s/sttr.rb
+++ Formula/s/sttr.rb
@@ -6,6 +6,14 @@ class Sttr < Formula
   license "MIT"
   head "https://github.com/abhimanyu003/sttr.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aca3fc9da1ae6a6382227fa4f34df1a44b3741982b23549b3d39593c15572b18"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "22bb4866cfed55da1efd316ecc54e54740d69f341d3f39100f7d722fe40c3f0e"
+    sha256 cellar: :any_skip_relocation, ventura:       "ba3643d70584b7c40c3fec5e90a741f4a80753c3da3cc9fcc901c66769d33d4a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eb3080c90ab6b978ecb95336003e948d748f302262e8ce65bd012b8309c0f261"
+  end
+
   depends_on "go" => :build
 
   def install
