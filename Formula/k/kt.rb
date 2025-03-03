ce6612dc4238074ce7deca1716460fd833f1887d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-03 01:17:14 +0000

    kt: add 13.1.0 bottle.

--- Formula/k/kt.rb
+++ Formula/k/kt.rb
@@ -6,6 +6,14 @@ class Kt < Formula
   license "MIT"
   head "https://github.com/fgeller/kt.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9ebebc1f334db10a2b34e2f0328df97dbfde76f47f4bf524faecd6b5b3ba5f91"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f3aaf3437195b988829e9b3744285bbeb62f0a54090ec9a3f622b035ea79d6bd"
+    sha256 cellar: :any_skip_relocation, ventura:       "4b000b09986b5cb600a701dbabaa0b20fe6e57ef050ea7ade0a7f48e4147c7d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b35e31ffbc9b2326e265f414571e891efda3eb1d67e01d8b5f3085f810defb2d"
+  end
+
   depends_on "go" => :build
 
   def install
