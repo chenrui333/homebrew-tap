0ea7fa700fdedd9ae6d7344f62f5abd297b7de6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-04 13:56:13 +0000

    fortran-linter: add 1.1.3 bottle.

--- Formula/f/fortran-linter.rb
+++ Formula/f/fortran-linter.rb
@@ -7,6 +7,14 @@ class FortranLinter < Formula
   sha256 "a5337e19ec13243011760dda6ed4c3c41eb9b9ad2983cf34179b4829c23ed5f2"
   license "GPL-2.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "acaa22ac7d2eb0aef1d13110279e250ebec62fdca5554f6991ebefed59296570"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1b94239b0a42189413680b6bb7c18ae3c7dbf61e2a4f23dc0fac28d3c232b797"
+    sha256 cellar: :any_skip_relocation, ventura:       "80fe0288be18511de43e9cefbe4a136f991cc0c63d1cf9480d6962cd4aa97e16"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e2de8e05a0980c9d7095bbb9fa48632093b9daf796a7162481bf2940ad5c27a9"
+  end
+
   depends_on "python@3.13"
 
   def install
