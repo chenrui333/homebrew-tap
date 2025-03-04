aace400a1340fcc840e62f0e08e213be244197bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-04 08:35:10 +0000

    eas-cli: update 15.0.13 bottle.

--- Formula/e/eas-cli.rb
+++ Formula/e/eas-cli.rb
@@ -7,10 +7,10 @@ class EasCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "822ef06607dd19a38445b62c40521056dd03fce01ba315b56e4ae9ff927f5916"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "89acfdbea9854fa77aa8ac958d9908139cd6ba696a9330631b9bba5085d4900d"
-    sha256 cellar: :any_skip_relocation, ventura:       "490441b5193cabdfe829a1cc0bf7c50fa85f0b608d7642f1e66aae4773a3bf8e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b1721b2478a32c61b7ac1b75e1cb64afa3113a47af267bbe54c5b7cd9f87b860"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4dbb578ed03ffd7d5ae7e2f4348f05ab3f0fb595540dc77cb214425e39489a85"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7fb8f43b2aca1d7ac827ba8cc69deeb87c01fb6ec298745103ed04c2da765cee"
+    sha256 cellar: :any_skip_relocation, ventura:       "1defec86fe4f8f03edef35c3c3b7b20ed41d031ed72382f22fcbb070a1383fdf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0cb9fbce27cd2b4c4a320bd5e4309f564bf167882b7a7ce717f30404102d7510"
   end
 
   depends_on "node"
