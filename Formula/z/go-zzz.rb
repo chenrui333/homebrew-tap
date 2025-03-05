bdeafb226f169645d41562eb8cc3637107082d4f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 19:53:40 +0000

    go-zzz: add 1.0.40 bottle.

--- Formula/z/go-zzz.rb
+++ Formula/z/go-zzz.rb
@@ -7,6 +7,14 @@ class GoZzz < Formula
   license "Apache-2.0"
   head "https://github.com/sohaha/zzz.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a30a2c3ab842f93307ad91e2863cfa364a2be8b26a73c4006736d4ed93cff6dc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cd8e1aca5997517fa797bbb9e2b824a14b45caf1b8fb2633ad8d8626f67d96ac"
+    sha256 cellar: :any_skip_relocation, ventura:       "eced6a13676bff0658472d660199e19bc9a50243fb90af55697f03068669bc60"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb87b8e6103a7a6256f5bd0ffd16cb21ae3c4c5b78b238b2fced2e96bcc3d2a4"
+  end
+
   depends_on "go"
 
   conflicts_with "zzz", because: "both install `zzz` binaries"
