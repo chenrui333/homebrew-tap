f0a1103d56719e2ba0a547472eed5fa73ff50d9b
Rui Chen <rui@chenrui.dev>
2025-03-05 13:43:30 -0500

    go-zzz 1.0.40 (new formula)
    Closes #483.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/z/go-zzz.rb
@@ -0,0 +1,44 @@
+# framework: cobra
+class GoZzz < Formula
+  desc "Hot compilation of Go programs, stress testing for Golang development"
+  homepage "https://github.com/sohaha/zzz"
+  url "https://github.com/sohaha/zzz/archive/refs/tags/v1.0.40.tar.gz"
+  sha256 "c01cec76bc188a7ad627ae85f05a7136121555fb9b3c8f4646c9da1895e7c5d5"
+  license "Apache-2.0"
+  head "https://github.com/sohaha/zzz.git", branch: "master"
+
+  depends_on "go"
+
+  conflicts_with "zzz", because: "both install `zzz` binaries"
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X 'github.com/sohaha/zzz/util.BuildTime=#{time.iso8601}'
+      -X 'github.com/sohaha/zzz/util.Version=#{version}'
+    ]
+    system "go", "build", *std_go_args(ldflags:, output: bin/"zzz")
+
+    generate_completions_from_executable(bin/"zzz", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/zzz --version")
+    assert_match "Zzz one-click installation command", shell_output("#{bin}/zzz more")
+
+    system "go", "mod", "init", "brewtest"
+
+    (testpath/"main.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+        fmt.Println("Hello, world!")
+      }
+    EOS
+
+    system bin/"zzz", "build", "--", "-o", "main"
+    assert_match "Hello, world!", shell_output("./main")
+  end
+end
