2e10220903ae33967ac67515c9cdc432c93bdd7b
Rui Chen <rui@chenrui.dev>
2025-03-05 15:45:09 -0500

    goodls 2.0.5 (new formula)
    Closes #489.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/g/goodls.rb
@@ -0,0 +1,25 @@
+class Goodls < Formula
+  desc "CLI tool to download shared files and folders from Google Drive"
+  homepage "https://github.com/tanaikech/goodls"
+  url "https://github.com/tanaikech/goodls/archive/refs/tags/v2.0.5.tar.gz"
+  sha256 "c9f4edd53c0296dfca4500e9e0ab9ae8fcd9e231693bafdd07dac8a121949036"
+  license "MIT"
+  head "https://github.com/tanaikech/goodls.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/goodls --version")
+
+    expected = if OS.mac?
+      "URL is wrong"
+    else
+      "no URL data"
+    end
+    assert_match expected, shell_output("#{bin}/goodls -u https://drive.google.com/file/d/1dummyURL 2>&1", 1)
+  end
+end
