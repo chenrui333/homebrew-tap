d7ecb41b741bd92d063e27e489cd9e951760ba42
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 17:46:27 +0000

    infraspec: add 0.0.2 bottle.

--- Formula/i/infraspec.rb
+++ Formula/i/infraspec.rb
@@ -7,6 +7,14 @@ class Infraspec < Formula
   # license "Fair"
   head "https://github.com/robmorgan/infraspec.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "693764d4253b3565209cd3414e3f08dc5f730f17caa606dca4ba5f347e974b7e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "71ffe6ed7adb6b98ea24ffc95987f1ce1052a12a341e01e0f40c376a2c4abad2"
+    sha256 cellar: :any_skip_relocation, ventura:       "fb40158b10fed5b69994a496d82affc09dada0e6d3b0b17760af11a411a3fcd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4911b4a13f48c968719ece0e0f7f21ede6f6a421f070ce664d47a5d87f3d4129"
+  end
+
   depends_on "go" => :build
 
   def install
