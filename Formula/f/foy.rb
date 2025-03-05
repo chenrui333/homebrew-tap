4aaf5867de1f3f72e91d93a68f954a27252e92d2
Rui Chen <rui@chenrui.dev>
2025-03-05 14:01:51 -0500

    foy 0.2.43 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/f/foy.rb
@@ -0,0 +1,36 @@
+class Foy < Formula
+  desc "Simple, light-weight and modern task runner for general purpose"
+  homepage "https://zaaack.github.io/foy/"
+  url "https://registry.npmjs.org/foy/-/foy-0.2.43.tgz"
+  sha256 "e5332890ed1e7eb770d2bd39795db8b12e39b08e303eb6da15feb2b719776ea2"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"package.json").write <<~JSON
+      {
+        "name": "test",
+        "version": "1.0.0",
+        "main": "index.js",
+        "dependencies": {
+          "foy": "#{version}"
+        }
+      }
+    JSON
+
+    (testpath/"Foyfile.js").write <<~JS
+      const { task } = require('foy')
+      task('hello', async ctx => {
+        console.log('Hello, world!')
+      })
+    JS
+
+    assert_match "Hello, world!", shell_output("#{bin}/foy hello")
+  end
+end
