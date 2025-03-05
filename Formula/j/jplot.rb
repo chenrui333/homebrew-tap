0c4a3580ea0a1a7c860e1f35520f7a3042c1cd8f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 04:13:45 +0000

    jplot: add 1.1.6 bottle.

--- Formula/j/jplot.rb
+++ Formula/j/jplot.rb
@@ -6,6 +6,14 @@ class Jplot < Formula
   license "MIT"
   head "https://github.com/rs/jplot.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "338d99a80997f9862c673f201caea8665b08a265903a4deeaf0a97118d624364"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ab103d5a315a72407272b6f2a90551b9ce1d7726278a6cec43fa25ba85f5ab37"
+    sha256 cellar: :any_skip_relocation, ventura:       "21ed22e5cceca680e31ca8171a299f4a9ba804eff5b64a68ed1bfdbe9f8c1aaa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec58db4294b0f762e4692d69e27e131d4c16eb0606d836f9c569c2b7e7464b7d"
+  end
+
   depends_on "go" => :build
 
   def install
