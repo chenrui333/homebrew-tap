4641f408c54d8d27e6fa4a6a019bfcb539d82cda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 18:36:26 +0000

    otel-cli: add 0.4.5 bottle.

--- Formula/o/otel-cli.rb
+++ Formula/o/otel-cli.rb
@@ -7,6 +7,14 @@ class OtelCli < Formula
   license "Apache-2.0"
   head "https://github.com/equinix-labs/otel-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0a1abdfc3c9a26b4c3065f6f2be4d3d31b6b3f81bb82bc571512dfa4756dc37c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "76450a7f24f407f3ba61830a37ff270cce5ba3ed0019bfbdef31c7947ad14724"
+    sha256 cellar: :any_skip_relocation, ventura:       "79f100f4a787d528f01410e2a102c383b3233b343a7de78b53397ebf83d7b4d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eeaf21b45ea2c36529799c242c6160ba05a90dd04353c9b6e06dd1420ccde447"
+  end
+
   depends_on "go" => :build
 
   def install
