463b1175887ebe09ac580d77ba8e4f92fc3e5ac9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-05 03:50:31 +0000

    certok: add 0.5.5 bottle.

--- Formula/c/certok.rb
+++ Formula/c/certok.rb
@@ -9,6 +9,14 @@ class Certok < Formula
     skip "no recent releases"
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83d4a29052d1c279e449ef42652a33a1f4d087a03d36997429c09b0c2e273105"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1ee72d6beca808ee3a263d49e95031b7c714d446a752f50819a003167b65da7a"
+    sha256 cellar: :any_skip_relocation, ventura:       "c86e862ae79070e79b32984394a8e11e9d77a7920c87b1cc7a83ab9469fdb466"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a61f4dfba0bdb890bd4348e871592e175b52f33caf425335b132c21a696f5e47"
+  end
+
   depends_on "go" => :build
 
   def install
