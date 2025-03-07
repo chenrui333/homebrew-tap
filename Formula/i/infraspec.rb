e5802416370395807e62548258fc73f449801242
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 08:25:01 +0000

    infraspec 0.0.3

--- Formula/i/infraspec.rb
+++ Formula/i/infraspec.rb
@@ -2,8 +2,8 @@
 class Infraspec < Formula
   desc "Tool for running infrastructure tests written in pure Gherkin syntax"
   homepage "https://github.com/robmorgan/infraspec"
-  url "https://github.com/robmorgan/infraspec/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "e7ddad0d555edf0cb5df28c2e7c8c9ec7336765feaa3b0304386e2ee01427293"
+  url "https://github.com/robmorgan/infraspec/archive/refs/tags/v0.0.3.tar.gz"
+  sha256 "66735ec5a36f0839104111b92cf19a71f880004082b5d70bcc5c401a5ef88188"
   # license "Fair"
   head "https://github.com/robmorgan/infraspec.git", branch: "main"
 
