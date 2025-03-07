ceaa778e5dbc96406da8f6e43328b0e0037ecec8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 04:24:00 +0000

    dela 0.0.3

--- Formula/d/dela.rb
+++ Formula/d/dela.rb
@@ -1,8 +1,8 @@
 class Dela < Formula
   desc "Task runner"
   homepage "https://github.com/aleyan/dela"
-  url "https://static.crates.io/crates/dela/dela-0.0.2.crate"
-  sha256 "767edcc040a536a0143905791466035b6f6614fced56e5bc38dbd40c0a5d419a"
+  url "https://static.crates.io/crates/dela/dela-0.0.3.crate"
+  sha256 "75d12e658f5e4555cb04e39d209ee75773c34005b9db34b4b9ff894dfc339e39"
   license "MIT"
 
   bottle do
