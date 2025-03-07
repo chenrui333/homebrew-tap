dcbe575c8b48e242e61251475d0b5576559894aa
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 04:05:38 +0000

    leveldb-cli: add 0.4.1 bottle.

--- Formula/l/leveldb-cli.rb
+++ Formula/l/leveldb-cli.rb
@@ -6,6 +6,14 @@ class LeveldbCli < Formula
   license "MIT"
   head "https://github.com/liderman/leveldb-cli.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6d24f38596d5bc2c1900aa986e920bad07bfc4c428effc445f9bfe9271c8d425"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c441774c705c3778647b3e0baf1bc36fe606b89438c39e61674b6ce5b1b81a97"
+    sha256 cellar: :any_skip_relocation, ventura:       "3a12dc0dca2dd2c4c037f10713aa1353a18731d183623ed74f583ed73c9bd4c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e72ed30cc8ca68009eae11b8e650be1f8a9664d7cd9ae7a13a52a91cce6203bf"
+  end
+
   depends_on "go" => :build
 
   def install
