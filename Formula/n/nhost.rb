daf1ec3bf00c63339573b0a01b43ebd05669ef3d
Rui Chen <rui@chenrui.dev>
2025-03-06 21:41:34 -0500

    nhost 1.29.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/n/nhost.rb
@@ -0,0 +1,23 @@
+class Nhost < Formula
+  desc "Developing locally with the Nhost CLI"
+  homepage "https://docs.nhost.io/development/cli/overview"
+  url "https://github.com/nhost/cli/archive/refs/tags/v1.29.3.tar.gz"
+  sha256 "78b7de0ecb9de12f687320ef4d255f1c16b2d97ca9395468f0102374ae5ba681"
+  license "MIT"
+  head "https://github.com/nhost/cli.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nhost --version")
+
+    system bin/"nhost", "init"
+    assert_path_exists testpath/"nhost/config.yaml"
+    assert_match "[global]", (testpath/"nhost/nhost.toml").read
+  end
+end
