49310afd8a397f115cc2339278d3d11ea93299ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 03:00:08 +0000

    terraform-module-versions: add 3.3.6 bottle.

--- Formula/t/terraform-module-versions.rb
+++ Formula/t/terraform-module-versions.rb
@@ -6,6 +6,14 @@ class TerraformModuleVersions < Formula
   license "MIT"
   head "https://github.com/keilerkonzept/terraform-module-versions.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0c5581e6361dafb52440b508b6dca128db58346e2cef9713d1fa116c6fed14dd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0788f4faad616a03e4f2f78cf961b117d8d1fcb221c74aac0f895c50bb7f53dc"
+    sha256 cellar: :any_skip_relocation, ventura:       "1d691462007d8f9d071ce7de75a8d113eb43dd208a9fca3b1f8535794a4e608b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f2d71c7012aa83de6792c78906e9148eba22b2d644eacbc991e251f6ef4fb7a3"
+  end
+
   depends_on "go" => :build
 
   def install
