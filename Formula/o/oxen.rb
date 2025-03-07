fae659986e2028cfebe0dffad064da3775fd8ec1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 15:19:02 +0000

    oxen: update 0.29.1 bottle.

--- Formula/o/oxen.rb
+++ Formula/o/oxen.rb
@@ -8,10 +8,10 @@ class Oxen < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6ef906cd867f6d373a0100e487df76a5c9fc3b8860966880070f839209d0d4ff"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9f4c66014ebe3bfe510527bdacb0fda1aed7495749622eb7625c3c35c668620b"
-    sha256 cellar: :any_skip_relocation, ventura:       "ad202686e22e0bc9be11fa6631d723f3d989a4ae7a6057547caa9311f21ad810"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0395e7765e874c5eec23b543696fe7b82bfc960efac12ef06943fd38b22f7410"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "17dfd858eaa06816edc6b0b6e694a03bb996acfb6a4a1dd29bd6195a3261b075"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4a69a4c4efcd4cbc5529e875c9b3468cb7361097a67af2781786babb65b84c40"
+    sha256 cellar: :any_skip_relocation, ventura:       "3ba561f51e5ed9f7409db5be3848cf94cd0bd4cf1720dfe9348da12e9e326cf6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a4128f47621e88136ab1c610148bbd1c74bd96c6bce6a5076806cd7083ce9e44"
   end
 
   depends_on "cmake" => :build # for libz-ng-sys
