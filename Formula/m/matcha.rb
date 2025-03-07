d52f4ee74a23dbeca104343f08f538a22515a8e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 04:00:33 +0000

    matcha: add 0.7.1 bottle.

--- Formula/m/matcha.rb
+++ Formula/m/matcha.rb
@@ -6,6 +6,14 @@ class Matcha < Formula
   license "MIT"
   head "https://github.com/piqoni/matcha.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b3f4bede60b719c3787c1bf4aea90d429e8fb91d95f4c533eac588673ca7eb18"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c47adf5a2174ce60efb686952a6608f645e51e95803883409f77ae6ca30771d2"
+    sha256 cellar: :any_skip_relocation, ventura:       "c7cef9206053bf4726fd17105b1e316cd298bfd1fb40c7aadb9043b490864957"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21b9db48ee8d8a22f68a82626c2ac9fcc758dfee23b4bfab17daa1331ba7bb4a"
+  end
+
   depends_on "go" => :build
 
   def install
