ddf7eabb5ab84cb33ee92d9d3376e5076969a198
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 03:18:24 +0000

    algolia: add 1.6.11 bottle.

--- Formula/a/algolia.rb
+++ Formula/a/algolia.rb
@@ -6,6 +6,14 @@ class Algolia < Formula
   license "MIT"
   head "https://github.com/algolia/cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2ec24885ccff9da1162f16d61b4f7506f581da7f9598ffaa2af282542db566f9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eae4a5fa07ca96e5bb60e5d24e02bb81f4b18b63e210f50a73ce9feccacd0bed"
+    sha256 cellar: :any_skip_relocation, ventura:       "d0c697ed1b7161ebe459b2eae03e5f2cd5dcf9900e5ff3126c6ae75eca635cf2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80df9c535295608eb5317c1e9003b4896a2ec0843838843ad43af8a51c2e60ff"
+  end
+
   depends_on "go" => :build
 
   def install
