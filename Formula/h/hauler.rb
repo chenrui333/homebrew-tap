d13979561930bd9c69fbf5503059d65862d4bba0
Rui Chen <rui@chenrui.dev>
2025-03-06 21:25:17 -0500

    hauler 1.2.1 (new formula)
    Closes #496.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/h/hauler.rb
@@ -0,0 +1,30 @@
+class Hauler < Formula
+  desc "Airgap Swiss Army Knife"
+  homepage "https://docs.hauler.dev/docs/intro"
+  url "https://github.com/hauler-dev/hauler/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "ea875e49968ca4bdd0f5ac2a78c3f6932b75619b647232a554b5b973e42d3317"
+  license "Apache-2.0"
+  head "https://github.com/hauler-dev/hauler.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X hauler.dev/go/hauler/internal/version.gitVersion=#{version}
+      -X hauler.dev/go/hauler/internal/version.gitCommit=#{tap.user}
+      -X hauler.dev/go/hauler/internal/version.gitTreeState=clean
+      -X hauler.dev/go/hauler/internal/version.buildDate=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags:), "./cmd/hauler"
+
+    generate_completions_from_executable(bin/"hauler", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/hauler version")
+
+    assert_match "REFERENCE", shell_output("#{bin}/hauler store info")
+  end
+end
