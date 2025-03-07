7c8a4c8ecd82796cbca2dcadd287ba7594ebbcb0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 03:41:19 +0000

    ips: add 0.3.4 bottle.

--- Formula/i/ips.rb
+++ Formula/i/ips.rb
@@ -6,6 +6,14 @@ class Ips < Formula
   license "Apache-2.0"
   head "https://github.com/sjzar/ips.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cebee234c59559a8337a9fad8938fbb847d63c5f6968961e0796046330c6bf21"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9b0b736eb02c3c35d01b02cd9b22bf8d3feb2b3eeb5b67e090c982a20f214a49"
+    sha256 cellar: :any_skip_relocation, ventura:       "da3286b1dba6efec63f7c90e575faa6b4e088f152ad2e6b86bba50852f502041"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f72952d27065f3958e244c636d4e4ecdd0178d7d20361ef9af17fcae721f821d"
+  end
+
   depends_on "go" => :build
 
   patch :DATA
