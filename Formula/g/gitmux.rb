dd264a26dc14345c05d757b075d4c31b718ba3e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-07 04:19:05 +0000

    gitmux: add 0.11.2 bottle.

--- Formula/g/gitmux.rb
+++ Formula/g/gitmux.rb
@@ -6,6 +6,14 @@ class Gitmux < Formula
   license "MIT"
   head "https://github.com/arl/gitmux.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b3904c2c16c1e9e336be608d92659457a5e64a2cc41ee12c6f13d4ba763bd797"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5aa7d73fd8e126b3f974f3fecc79987dd30ec448f2690cc542a79e8f461ad9f9"
+    sha256 cellar: :any_skip_relocation, ventura:       "ecd846c151fbed6791ea4a6ea822c58cef1929a1b897fc8b1cc3e58734372f12"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3d405a9a546b5c8a101845f992610c77433b617bf0e231c051c6b6bffa40c0e"
+  end
+
   depends_on "go" => :build
 
   def install
