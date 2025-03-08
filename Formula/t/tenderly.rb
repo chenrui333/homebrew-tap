fe00162ece923961c3339b0a50a4e84eb3a499a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-08 19:31:18 +0000

    tenderly: add 1.6.5 bottle.

--- Formula/t/tenderly.rb
+++ Formula/t/tenderly.rb
@@ -6,6 +6,14 @@ class Tenderly < Formula
   license "GPL-3.0-only"
   head "https://github.com/Tenderly/tenderly-cli.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3a854f8bcc7c67f70cc1de763d2dc6102ce3e80ad09d1d04894374235070502f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "273889cdb8b91972f3fa12858cce07d8c6fc6b879c4e61a8d140024bee6bfbb0"
+    sha256 cellar: :any_skip_relocation, ventura:       "3397cf1fc9c1e6a995ba3ddce96868b54c3c5c771d610b4775d1abfcbf877947"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "11fed6f21ad789587de0c109c3c42c03e01221b81132c684cc5fdff7bb170558"
+  end
+
   depends_on "go" => :build
 
   def install
