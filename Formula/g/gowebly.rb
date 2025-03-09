97c39c0f0d649237b91679ca6d7c38eb2369af23
Rui Chen <rui@chenrui.dev>
2025-03-09 15:56:53 -0400

    gowebly 3.0.0 (new formula)
    Closes #530.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/g/gowebly.rb
@@ -0,0 +1,21 @@
+class Gowebly < Formula
+  desc "Next-generation CLI tool to easily build amazing web applications"
+  homepage "https://gowebly.org/"
+  url "https://github.com/gowebly/gowebly/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "4f12b7bbbef2cdd16c37748bc75aff2362987c93eb26f4dfc4dcfe2689811274"
+  license "Apache-2.0"
+  head "https://github.com/gowebly/gowebly.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gowebly doctor")
+
+    output = shell_output("#{bin}/gowebly run 2>&1", 1)
+    assert_match "No rule to make target", output
+  end
+end
