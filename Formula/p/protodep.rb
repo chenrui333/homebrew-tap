f09b4a042ec93d7215aa5604bb883a468f4b09f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 19:03:55 +0000

    protodep: add 0.1.8 bottle.

--- Formula/p/protodep.rb
+++ Formula/p/protodep.rb
@@ -5,6 +5,14 @@ class Protodep < Formula
   sha256 "0dd26260f604955209c856c179e225cdc956fe7ba7f92f33a4f1d5d8d86d30aa"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6ffb701c7fc8d041d2e20990b3a7b6d891d8ea31110b7d8dbff84ed60786b43e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00de15ca4cce4708369b303681146e21df1d79d84ff8a301e0b6782a1fafca8f"
+    sha256 cellar: :any_skip_relocation, ventura:       "5403f3af0bd42e59feab3a238b1544faae26ef9d41d79414da2d4502a27ee299"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "74b100e1dcdd457a8a19e52205ec3c5b3a31bd589e5afdadbf2293a40f52b5a1"
+  end
+
   depends_on "go" => :build
 
   def install
