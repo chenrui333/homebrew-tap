4c24c4defc59b9b892b1830a477ac357cbb3c1bc
Rui Chen <rui@chenrui.dev>
2025-03-09 01:48:06 -0500

    jenkins-cli: rename from jcli
    Closes #522.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/j/jenkins-cli.rb
+++ Formula/j/jenkins-cli.rb
@@ -1,18 +1,10 @@
-class Jcli < Formula
+class JenkinsCli < Formula
   desc "CLI for jenkins"
   homepage "https://github.com/jenkins-zh/jenkins-cli"
   url "https://github.com/jenkins-zh/jenkins-cli/archive/refs/tags/v0.0.47.tar.gz"
   sha256 "4e78600e214c357c08a0a83fe9cc59214b0d050de07dbb469d9f226c8c37eabc"
   license "MIT"
   head "https://github.com/jenkins-zh/jenkins-cli.git", branch: "master"
-
-  bottle do
-    root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2fc5480f2cc3a5146c364d7bae202e707bf603e9b1ec7e40bcc0face7879954a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3bce35a45c7628d0852ccb8d3cbb3aa6655654845b5d73b335186ef8b0ca890c"
-    sha256 cellar: :any_skip_relocation, ventura:       "d7c405f750f5d242f5aca75d85fbede18b4075caa71413f8c6192b0ae407119c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cc40ba1263549e6ad1319a04b18bad9a51b22ca982eb7c35f421bb88b835045f"
-  end
 
   depends_on "go" => :build
 
@@ -23,7 +15,7 @@ class Jcli < Formula
       -X github.com/linuxsuren/cobra-extension/version.commit=#{tap.user}
       -X github.com/linuxsuren/cobra-extension/version.date=#{time.iso8601}
     ]
-    system "go", "build", *std_go_args(ldflags:)
+    system "go", "build", *std_go_args(ldflags:, output: bin/"jcli")
 
     generate_completions_from_executable(bin/"jcli", "completion")
   end
