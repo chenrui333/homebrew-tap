38ec7bf8b0550c91f78d5a54c04964a69f8e48d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 18:31:49 +0000

    jenkins-cli: add 0.0.47 bottle.

--- Formula/j/jenkins-cli.rb
+++ Formula/j/jenkins-cli.rb
@@ -6,6 +6,14 @@ class JenkinsCli < Formula
   license "MIT"
   head "https://github.com/jenkins-zh/jenkins-cli.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ad5b764f0d0973ffe2331e853687250714451a10d66a6c113aec69ecc0d2675f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "34841cd262b9b0d5ea55c4f7643c29a827dfe580fc1152c8de06b72f7094b610"
+    sha256 cellar: :any_skip_relocation, ventura:       "9074b2643178057df4cbe4d01c306cbeddfa79b43e0822afcdc9a5d56d89aba6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e42cf81b71187ee8b8966bc35b021fd4e5b0d1f3d94f2e7427cf51b527b034c"
+  end
+
   depends_on "go" => :build
 
   def install
