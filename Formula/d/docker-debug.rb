13fca0b21652e50d14f7e6c5ec17d0c19f726f2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-09 20:00:10 +0000

    docker-debug: add 0.7.10 bottle.

--- Formula/d/docker-debug.rb
+++ Formula/d/docker-debug.rb
@@ -6,6 +6,14 @@ class DockerDebug < Formula
   license "Apache-2.0"
   head "https://github.com/zeromake/docker-debug.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bb38bc8dd204bebeb1e7884035b7a2e3052793b8dea43daf65aed7b2d12e213d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "447b10635ca042525a1590e41f44dfa1a11180f1e2bbaaa66176e7c5f102bb10"
+    sha256 cellar: :any_skip_relocation, ventura:       "c6afafa1dd8e74cf37428ac5ada20d97b97e400db98cefee89f23508dad5b68c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "816443d8ad4003344ca3db2508ad8b52673138bb7b84d0452a6fd7d51b8ae9e3"
+  end
+
   depends_on "go" => :build
 
   def install
