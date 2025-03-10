21f9cecff03304c9570a192a3460ba53e11caa01
Rui Chen <rui@chenrui.dev>
2025-03-09 21:44:48 -0400

    ziglint: rename from zlint and bump to 0.7.0
    Closes #541.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/z/ziglint.rb
+++ Formula/z/ziglint.rb
@@ -1,17 +1,9 @@
-class Zlint < Formula
+class Ziglint < Formula
   desc "Linter for the Zig programming language"
   homepage "https://github.com/DonIsaac/zlint"
-  url "https://github.com/DonIsaac/zlint/archive/refs/tags/v0.6.1.tar.gz"
-  sha256 "d8f5e1a6a5eaa6896d3a5586ea7182473590165640e859f9542c8b3efcadc67a"
+  url "https://github.com/DonIsaac/zlint/archive/refs/tags/v0.7.0.tar.gz"
+  sha256 "95294e81e5a02ecae0c55d851a14cfe9cc687fc50a439f797ac0ad1cc9e19c27"
   license "MIT"
-
-  bottle do
-    root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8019602cf52ae0eba0f69db85b1baa915423ed856d66d046c7c18ff1540d20af"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "717ce2e9e8ec4185216bce0523f219fb8f1a6e68c9b898f22e1773b43cc24635"
-    sha256 cellar: :any_skip_relocation, ventura:       "7db39349cd3bd2f4d7e252e89daebbe7aebbc9a03c8e270eb3b8255b6b74dc5b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a227290b147d46626dc5334b8a2075457b7039c30b0af2bc787d0620d99705ea"
-  end
 
   depends_on "zig" => :build
 
