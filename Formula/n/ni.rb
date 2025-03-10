39c2d41679aa5c97ab302ef36860347cdcb1b4d4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-10 08:25:55 +0000

    ni 24.0.0

--- Formula/n/ni.rb
+++ Formula/n/ni.rb
@@ -1,8 +1,8 @@
 class Ni < Formula
   desc "Use the right package manager"
   homepage "https://github.com/sindresorhus/ni"
-  url "https://registry.npmjs.org/@antfu/ni/-/ni-23.3.1.tgz"
-  sha256 "90b7dfb524cfa3c3400cc0c6870a4ce16000e74ed279b5de2a10050444565a9b"
+  url "https://registry.npmjs.org/@antfu/ni/-/ni-24.0.0.tgz"
+  sha256 "bc23cc18f58563f8defb0f84715c6c1473549463891130a2c662466edf782a67"
   license "MIT"
 
   bottle do
