18ba584ed50036cae6ca4fe4fa1c085e9e2d28fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-13 08:29:18 +0000

    yuque-dl 1.0.75
    Closes #557.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/y/yuque-dl.rb
+++ Formula/y/yuque-dl.rb
@@ -1,8 +1,8 @@
 class YuqueDl < Formula
   desc "Knowledge base downloader for Yuque"
   homepage "https://github.com/gxr404/yuque-dl"
-  url "https://registry.npmjs.org/yuque-dl/-/yuque-dl-1.0.74.tgz"
-  sha256 "479067e978a98c8063584d9b9916ea8ade4f559fd23557a66a07e7014c03c090"
+  url "https://registry.npmjs.org/yuque-dl/-/yuque-dl-1.0.75.tgz"
+  sha256 "39d00f2bb4c8aada7dda61b0f799186fb025c025f5aaacf4551024617ade56f8"
   license "ISC"
 
   bottle do
