774d083ef60b5cceb4c373e997a45e13f7ba7926
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 14:33:04 +0000

    goboscript: update 3.1.1 bottle.

--- Formula/g/goboscript.rb
+++ Formula/g/goboscript.rb
@@ -7,10 +7,10 @@ class Goboscript < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7d34918ff0577c7cbaaf91602621d5915d8771bd429eb14bf45a3cb547cc888f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4250a44406801ff3e04e0e05e07b522e9ccd855118ca771725595e0cc973af84"
-    sha256 cellar: :any_skip_relocation, ventura:       "b48055107da2111fd734574e3b5b5ae454a98addb59307fdf775d9a70cd034ba"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48ff41e3586e420cb39eb574e850d1ca6196ab3f9307a489e9eb75ddc2eb993d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d964aa661c8e62b3ed0a82c03c890ec939758ce582575e3cbe7209a95639e170"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a429dc8cc4e9d92c29bdcf6cfcbd39ca6651e03a3a319ee0c752aca00291feae"
+    sha256 cellar: :any_skip_relocation, ventura:       "c7b224b38ce00d3c47d8893cfce62cf61679fe63dd71756c618fbf10cd98d0a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d20ba3eed47a7a0c32c67eef116a6f68afc36927819bd4c090c8a27e9ac8ccef"
   end
 
   depends_on "rust" => :build
