c5423266f670fe110df23bbb841fac8133abb71e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 20:13:01 +0000

    kbst: add 0.2.1 bottle.

--- Formula/k/kbst.rb
+++ Formula/k/kbst.rb
@@ -6,6 +6,14 @@ class Kbst < Formula
   license "Apache-2.0"
   head "https://github.com/kbst/kbst.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ca67de1d4e643de0f8c4eb2f43edbe9324c845e4643b44780bdc3874eb6e068"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "42707d6ac672b9ba3cb1815205ef399ddcd5ac6e5adf9aab22a5f52a6be94ec6"
+    sha256 cellar: :any_skip_relocation, ventura:       "9606425accd39e2fcf0cbb47cc320ae91a29a134543c7f20e3b06e1cc13b2f2f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97d3bc137d0e58a1fd07934dd1aafaddf0522ec6af9ade7a2e7e4242bd091d9b"
+  end
+
   depends_on "go" => :build
 
   def install
