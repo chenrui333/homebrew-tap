700608483e2390a830471bf45c1ecb0364c72c38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 16:36:59 +0000

    jsonl-graph: add 1.2.3 bottle.

--- Formula/j/jsonl-graph.rb
+++ Formula/j/jsonl-graph.rb
@@ -6,6 +6,14 @@ class JsonlGraph < Formula
   license "MIT"
   head "https://github.com/nikolaydubina/jsonl-graph.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "64bbb371ef954ef091f804f5a0e3478b3f20948f63266d5ee3a1da86e98757f7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "80c52de7a2d9397bf9ce7463b63e063cc7fdecf9c3031e9def510d9402d31fc7"
+    sha256 cellar: :any_skip_relocation, ventura:       "ae3e1838f9ae00cbd3384e0a60050d4d20f9be241a393fc1c80bb7010c369ce5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b53974e89f26d6536c6ccfdc136e136245c18da34be1acc0641df1b5fcb6e3f"
+  end
+
   depends_on "go" => :build
 
   def install
