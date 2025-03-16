1069b695859199ad367c79de12f70c519be5e9c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 15:27:28 +0000

    wedl: add 0.1.11 bottle.

--- Formula/w/wedl.rb
+++ Formula/w/wedl.rb
@@ -6,6 +6,14 @@ class Wedl < Formula
   license "Unlicense"
   head "https://github.com/gnojus/wedl.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73ab2463cce3c2bcd30ccf5443d5fc1d14a4279a606702a25bd88ecd14162f67"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "af1029478115f5cf6fd7f6559fd014dbc698849d35be4316151f59893010bb1b"
+    sha256 cellar: :any_skip_relocation, ventura:       "ea7f04ace575e2694376b059dd6e8fe69790fca7c5c71e1dda83059f1c65e382"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d9f50dac139f66fa0edaf27b7d2cb100d8af1871b99b455378c6b70e2cf42610"
+  end
+
   depends_on "go" => :build
 
   def install
