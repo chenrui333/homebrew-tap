b495012f6c328483ebdf2fb0583ebab2baa82380
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 16:09:52 +0000

    scholar: add 0.1.1 bottle.

--- Formula/s/scholar.rb
+++ Formula/s/scholar.rb
@@ -6,6 +6,14 @@ class Scholar < Formula
   license "MIT"
   head "https://github.com/cgxeiji/scholar.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9d6e6d52946934b414e9653087f39a0580ef97fd1288df8d46bdffd4e9efe2f8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ce364e6cb53e6a388b2986cf4efce165c2f2eda00702666366e94af81b8db817"
+    sha256 cellar: :any_skip_relocation, ventura:       "c44d8060f7588e9689ed4a8eb9eef1782f94b65dcd66ce541c8862cacd1da125"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06187267c60e87d8312e28ac0135175528fe77c1a213eaa2e50acaebe2e620d9"
+  end
+
   depends_on "go" => :build
 
   def install
