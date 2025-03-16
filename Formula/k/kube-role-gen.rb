9631fbfc8f289704a34ef3f888633fda0f555421
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 19:33:11 +0000

    kube-role-gen: add 0.0.7 bottle.

--- Formula/k/kube-role-gen.rb
+++ Formula/k/kube-role-gen.rb
@@ -6,6 +6,14 @@ class KubeRoleGen < Formula
   license "Apache-2.0"
   head "https://github.com/coopernetes/kube-role-gen.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3da3d8576c8996c80c941767a860d4d1fde2d4528fb53442bd30d17daf3a37e1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7aca501644d5cf5911283aac8184bdcd3f26f2c5473835d81d2a252aec156bfb"
+    sha256 cellar: :any_skip_relocation, ventura:       "c79d037d9fb77f1dba89ccdebdac81737610c44c0963434c5a5db971e44b32da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7437e852e9ff4ec2e91f3bee74a3ce36ecf0e3d9ba85129046c6e66f998207b5"
+  end
+
   depends_on "go" => :build
 
   def install
