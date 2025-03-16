308aa7b5149d282a96d7ff41bc704583f2b2716c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 22:13:15 +0000

    yajsv: add 1.4.1 bottle.

--- Formula/y/yajsv.rb
+++ Formula/y/yajsv.rb
@@ -6,6 +6,14 @@ class Yajsv < Formula
   license "MIT"
   head "https://github.com/neilpa/yajsv.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2c5b35d39aadd3955d5f54eae35742847157131722c7ac6c5ac620a44a3bc465"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aa006249351af05a9c51b44108c5ff4469ae5cd90b9aa9189bca123d08d78d02"
+    sha256 cellar: :any_skip_relocation, ventura:       "aa1487aabc9a56a3c3db74d26aa62b6927a5d09ef5438fea6f3b8741a1c2f995"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b506da088d05fa14ea1b0a4280a37770fccb2f637783972f07c1a7e6ddc211dd"
+  end
+
   depends_on "go" => :build
 
   def install
