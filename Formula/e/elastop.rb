8d7a77bc89557e141ee1ac5efeeab84400d1f47b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 21:24:26 +0000

    elastop: add 1.0.5 bottle.

--- Formula/e/elastop.rb
+++ Formula/e/elastop.rb
@@ -6,6 +6,14 @@ class Elastop < Formula
   license "ISC"
   head "https://github.com/acidvegas/elastop.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fc2c8e8f926f49d966dc86eca4fe573c9e9195525f5ee906c1530b925c6acd99"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d3424d31a2be9527b97cd9f4ed76ddc34878dcd5b9a8e012815af98c4f194921"
+    sha256 cellar: :any_skip_relocation, ventura:       "c3e596a8314f4608fd0d0f0ff0afb05b1e899b87a994cb0cf4bad348b1ccb9fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3e05c857c6de3ae3f9b16bc4e194c84f184299e3e228d3f06f105cbccefa1aa"
+  end
+
   depends_on "go" => :build
 
   def install
