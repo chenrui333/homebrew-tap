d1537db1eb056a62115b3185c56fbf50fdac9569
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 19:12:00 +0000

    gut: add 0.3.1 bottle.

--- Formula/g/gut.rb
+++ Formula/g/gut.rb
@@ -6,6 +6,14 @@ class Gut < Formula
   license "MIT"
   head "https://github.com/julien040/gut.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9b34527e00b35f463a2f446eba3fe141b9c8d81e5660f1bf90b0484962e587d8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0eb9af5cdae874aa73d9b8f7bc1fde63d6f873284c2710eef743853103c6c4ee"
+    sha256 cellar: :any_skip_relocation, ventura:       "3cdbced839805f312ff52814264b2ce35d38311efc30888514b9bedea1a9ad9e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0387f7fd8b1c32811d28753a93043e1542119e2258252055e15293f5dc01eeb0"
+  end
+
   depends_on "go" => :build
 
   def install
