86c24f34a7cef5f9384ef39b6c5bf01655f9e62d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 19:27:23 +0000

    narr: add 0.2.0 bottle.

--- Formula/n/narr.rb
+++ Formula/n/narr.rb
@@ -6,6 +6,14 @@ class Narr < Formula
   license "Unlicense"
   head "https://github.com/IljaN/narr.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "55588064fdcf6855d96007fe0fe028f0b3cfa2015f06738d39f508f9418e55e9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "171cef9aab1471cb03a6143040b07b8a48a95189cc225f4257ec924bee45730f"
+    sha256 cellar: :any_skip_relocation, ventura:       "df721cd5f0041c11cc23776db898cf445c7413868cd43229089ce17588287725"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0c619e145de302f2587927209edadbbabb6ddde348d8e8a1cd80babaeaba12bd"
+  end
+
   depends_on "go" => :build
 
   def install
