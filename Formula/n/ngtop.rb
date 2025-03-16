eefa7d711cb007a4dcd77dbcded87559354c758d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 19:53:28 +0000

    ngtop: add 0.4.6 bottle.

--- Formula/n/ngtop.rb
+++ Formula/n/ngtop.rb
@@ -6,6 +6,14 @@ class Ngtop < Formula
   license "GPL-3.0-only"
   head "https://github.com/facundoolano/ngtop.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "877c77ad4afbd58ffd2558d52c5fa36043223d556671190e88069332215c24ed"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d415101bb9fa8c9f88ceae9e1d9754874a6c1d12f055bdc82d33d28d7a872e92"
+    sha256 cellar: :any_skip_relocation, ventura:       "ee9ab1880869282a49929646c8a3356ec962a20c75f39f564e56631a17c536f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "243e99221bdb92864259fd8dc444bdda6dc413e9fe69a570e3f074917da7224c"
+  end
+
   depends_on "go" => :build
 
   def install
