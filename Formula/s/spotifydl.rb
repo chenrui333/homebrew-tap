05c72a82aa18c62863e0fd8b1780952d78c2c245
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-16 18:56:34 +0000

    spotifydl: add 0.1.1 bottle.

--- Formula/s/spotifydl.rb
+++ Formula/s/spotifydl.rb
@@ -6,6 +6,14 @@ class Spotifydl < Formula
   license "Apache-2.0"
   head "https://github.com/BharatKalluri/spotifydl.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb9e8bc1c347437c11a9df7b78e0381ecb7cc63fa52a5083dcbc53dc374ae108"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aea796d3cd70f99286c31fb92a22b68cb7544672e6748777d2553f5905bd8a35"
+    sha256 cellar: :any_skip_relocation, ventura:       "c1a5956413e050059f8120936c890f3d456e5c23f95ba6f6c621c0fc7234b92b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0fe5faa92f07eaa7f0067fde37868dbdba2d69035f6023f9c123de4d60b290ee"
+  end
+
   depends_on "go" => :build
 
   def install
