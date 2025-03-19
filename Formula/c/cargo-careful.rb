1dc1e922ce27e533eef4dbdf3e1ca3248e88308d
Rui Chen <rui@chenrui.dev>
2025-03-19 13:10:31 -0400

    cargo-careful 0.4.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/cargo-careful.rb
@@ -0,0 +1,41 @@
+class CargoCareful < Formula
+  desc "Execute Rust code carefully, with extra checking along the way"
+  homepage "https://github.com/RalfJung/cargo-careful"
+  url "https://github.com/RalfJung/cargo-careful/archive/refs/tags/v0.4.3.tar.gz"
+  sha256 "de50fe97e4f3569dd1c739ba5347e78967c1f24ac6e2bdfb0a935ce2e6d8f4a1"
+  license any_of: ["Apache-2.0", "MIT"]
+  head "https://github.com/RalfJung/cargo-careful.git", branch: "master"
+
+  depends_on "rust" => :build
+  depends_on "rustup" => :test
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # Show that we can use a different toolchain than the one provided by the `rust` formula.
+    # https://github.com/Homebrew/homebrew-core/pull/134074#pullrequestreview-1484979359
+    ENV.prepend_path "PATH", Formula["rustup"].bin
+    system "rustup", "default", "beta"
+    system "rustup", "set", "profile", "minimal"
+    system "rustup", "toolchain", "install", "nightly"
+
+    (testpath/"src/main.rs").write <<~RUST
+      fn main() {
+        println!("Hello, world!");
+      }
+    RUST
+
+    (testpath/"Cargo.toml").write <<~TOML
+      [package]
+      name = "test-careful"
+      version = "0.1.0"
+      edition = "2021"
+    TOML
+
+    system "cargo", "careful", "setup"
+    output = shell_output("cargo careful run")
+    assert_match "Hello, world!", output
+  end
+end
