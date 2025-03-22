fa009330d8ceb00baac397c8af4f7adfdc8cd5ae
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-22 12:39:31 +0000

    yew-fmt: update 0.6.1 bottle.

--- Formula/y/yew-fmt.rb
+++ Formula/y/yew-fmt.rb
@@ -7,10 +7,10 @@ class YewFmt < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f545f09b8ecd38d533183e0443d7ef4135d15295dd49fcdc62f23379212d7917"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d009f700abc39cd8081ee54e51474da27d2532359e34f468c3fb26cfc43b8be6"
-    sha256 cellar: :any_skip_relocation, ventura:       "d5ec632f5f9a18840ee83fb0c61d2a078d0b9a4229241f1091735777e489a411"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1af2faeafb73a113a8766012bd6164b35ca4a5a67116496a2ae169567e318fb0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a061c0da4d6b7ed1de91398a8f5cd1f958cb1b52692ef7dcdf2cbb2364f40faf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5ccb42ae706116e84779c56db8a5f0f3d5a5b471c8dcc8a0c6075400a8e60d7c"
+    sha256 cellar: :any_skip_relocation, ventura:       "128ebb4fde08370938f01e992589ea42c7d0eeded21c5b7f387b28bc332dac4a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55817fd96c47a8e1b8089eab7a9217bb8e4f26207cccc8d78126fecb72580847"
   end
 
   depends_on "rust" => :build
