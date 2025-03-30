5a0c9ac642f049bd020085418c5e28d1ff946782
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-30 08:31:00 +0000

    go-zzz: update 1.0.41 bottle.

--- Formula/z/go-zzz.rb
+++ Formula/z/go-zzz.rb
@@ -9,10 +9,10 @@ class GoZzz < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a30a2c3ab842f93307ad91e2863cfa364a2be8b26a73c4006736d4ed93cff6dc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cd8e1aca5997517fa797bbb9e2b824a14b45caf1b8fb2633ad8d8626f67d96ac"
-    sha256 cellar: :any_skip_relocation, ventura:       "eced6a13676bff0658472d660199e19bc9a50243fb90af55697f03068669bc60"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cb87b8e6103a7a6256f5bd0ffd16cb21ae3c4c5b78b238b2fced2e96bcc3d2a4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "639361c15b8ca05669dcbd7807969ebfe9a1a074cd8c139ab451b66a5e5535e4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "79b8eba5c41ea03208d1f92ababaaaccd2e4f0a7275b63e0db1e78b84035e6fa"
+    sha256 cellar: :any_skip_relocation, ventura:       "8e5544c435bcd403643e21c6f047ae3b43f21cfc601e651bf854ccc6b2d7217d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3a225b35a72e80e086c8457664aaa0d454302eb0bc039c092f323a952fe64689"
   end
 
   depends_on "go"
