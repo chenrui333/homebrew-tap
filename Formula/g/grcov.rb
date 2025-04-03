81d3d0f83b5c9b2416aab33ad9adf86e3898d6f0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-03 12:26:37 +0000

    grcov 0.8.24
    Closes #678.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/grcov.rb
+++ Formula/g/grcov.rb
@@ -1,8 +1,8 @@
 class Grcov < Formula
   desc "Rust tool to collect and aggregate code coverage data for multiple source files"
   homepage "https://github.com/mozilla/grcov"
-  url "https://github.com/mozilla/grcov/archive/refs/tags/v0.8.23.tar.gz"
-  sha256 "c9e630349a5b1cbd1e6568fd200e233e3bd63a48b5360e744f017de344dc385f"
+  url "https://github.com/mozilla/grcov/archive/refs/tags/v0.8.24.tar.gz"
+  sha256 "c277df7dbbaf6e3c45cbfa5a3392fad264abe04bf43ff29822a150f1f85dba45"
   license "MPL-2.0"
 
   bottle do
