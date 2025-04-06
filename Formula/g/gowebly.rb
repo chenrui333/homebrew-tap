af4489121777c785d00cb1d5fe41f97c2f90d077
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-06 10:24:21 +0000

    gowebly 3.0.1

--- Formula/g/gowebly.rb
+++ Formula/g/gowebly.rb
@@ -1,8 +1,8 @@
 class Gowebly < Formula
   desc "Next-generation CLI tool to easily build amazing web applications"
   homepage "https://gowebly.org/"
-  url "https://github.com/gowebly/gowebly/archive/refs/tags/v3.0.0.tar.gz"
-  sha256 "4f12b7bbbef2cdd16c37748bc75aff2362987c93eb26f4dfc4dcfe2689811274"
+  url "https://github.com/gowebly/gowebly/archive/refs/tags/v3.0.1.tar.gz"
+  sha256 "1d2f0f67c965affafad456294cae30d060de978fda30802dfd5fa40a6fcca347"
   license "Apache-2.0"
   head "https://github.com/gowebly/gowebly.git", branch: "main"
 
