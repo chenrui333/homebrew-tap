fb09b4eeb397b3702e3fccfe0c8cd9d90cd1e409
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-08 08:26:37 +0000

    omnictl 0.48.0

--- Formula/o/omnictl.rb
+++ Formula/o/omnictl.rb
@@ -1,8 +1,8 @@
 class Omnictl < Formula
   desc "CLI for the Sidero Omni Kubernetes management platform"
   homepage "https://omni.siderolabs.com/"
-  url "https://github.com/siderolabs/omni/archive/refs/tags/v0.47.1.tar.gz"
-  sha256 "8abcd55b5c4bc30627184ccf3688215b9c1beea00d6f71dda7bd867ee7133d65"
+  url "https://github.com/siderolabs/omni/archive/refs/tags/v0.48.0.tar.gz"
+  sha256 "7ac1ee11f38728103bfabd74a90120341d6ac98e24846d5ef4b26d67d9514bfe"
   # license "BSL-1.1"
   head "https://github.com/siderolabs/omni.git", branch: "main"
 
