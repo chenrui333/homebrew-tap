437c6460a7a16e1c3c14b93b223fdfead2627659
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-09 14:04:38 +0000

    mdslw: update 0.15.2 bottle.

--- Formula/m/mdslw.rb
+++ Formula/m/mdslw.rb
@@ -9,10 +9,10 @@ class Mdslw < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dea0ca4cf76b286caf5c7e5b07b0b6ec9aad0a5a690bc0a51862f419c42dcaf5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d76e2ae1d003b196f040ff368cbfb7d8403d469c7c750f1c2b5dff4aef1b6274"
-    sha256 cellar: :any_skip_relocation, ventura:       "72007b1ca8eac9f50efa24418cb5a5f1cdeb6b1294b70724f984ca06bb605a5a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d408ef91a48faf8b70f154f81f0c8fa1d5db139c64192d2cb2af1e44281cb97"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "90bdb497dbb85c1fd20e0da3d76acb1bfcaa9559583301f5ff86611b334f9cd5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "27b0e356b1925d512f9ff73ed4d5a1389b2ead0c345c5eacf2e45be700b94e56"
+    sha256 cellar: :any_skip_relocation, ventura:       "b1e0216e051346d006f06fd121224e08db8212a1b9b582f963fd22b0afcef438"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7cd56ce932088a442fb8067b7d0e7c991ad19a04d0196bea9898bee6352375aa"
   end
 
   depends_on "pkgconf" => :build
