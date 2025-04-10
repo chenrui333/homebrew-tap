10381a73afeaf6f45ffab42b104754eed6714cb4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-10 18:40:05 +0000

    tfcmt: update 4.14.4 bottle.

--- Formula/t/tfcmt.rb
+++ Formula/t/tfcmt.rb
@@ -8,10 +8,10 @@ class Tfcmt < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aca117ac1c779186852b35c5712078971a19700d4b44f20af2eaba5dc6e090cd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "066a8d3ee74ffa8d5b119c627fc8335312ba50cc46caad80fc25860ab8f525ab"
-    sha256 cellar: :any_skip_relocation, ventura:       "fda7de44dc44c7dec46a3f5c55b93e7d4fefd27ea1ef9389f526a743bb216937"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d346495e4338c7d204c966bae545f89304dc8c9438f6078d2ce6bca72d2f7692"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3fdcd2618f67015028f778585314d4582e0ef80c75de14fc3ee4804634d81627"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "faa14a722c00d6c30212d3c89ed704ce64e1588c6acc6f4ad11e5013d6500f27"
+    sha256 cellar: :any_skip_relocation, ventura:       "4cd1e9d16d511e34ebae58752e76a0eb3d2fed5816ac6ec0cd932c2251daa8b8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5c04d5a3cb3551519e32f7936382931fe2a68fd10a8d3adabd0b73d86c19d1e4"
   end
 
   depends_on "go" => :build
