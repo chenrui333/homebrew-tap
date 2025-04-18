175238341bd681bff49398abf0cefc94d0219b54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-18 20:56:49 +0000

    protoc-gen-doc: add 1.5.1 bottle.

--- Formula/p/protoc-gen-doc.rb
+++ Formula/p/protoc-gen-doc.rb
@@ -6,6 +6,14 @@ class ProtocGenDoc < Formula
   license "MIT"
   head "https://github.com/pseudomuto/protoc-gen-doc.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "492f4ffdfedd168536904d5f45db3f2f1f3e0b75367e984a519e4f63b684c912"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4f007ef1922b072fcfc23dec50ea516e05075ad8608ea484b02bc0cff408f242"
+    sha256 cellar: :any_skip_relocation, ventura:       "5ec3d9cc8d07d8599e5236a7bcd68eb530b8ecd69d2cb1ab1cf8b8422f0ab1a0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c4ba6e5e8bf0caab5c6965ceccb63320a31259ffd99706231bbc306aac0e48b4"
+  end
+
   depends_on "go" => :build
   depends_on "protobuf"
 
