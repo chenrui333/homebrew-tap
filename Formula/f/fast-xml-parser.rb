3194ddbae30b058fc951b3c4086d39715b1668b8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-22 12:26:00 +0000

    fast-xml-parser 5.2.1
    Closes #758.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/f/fast-xml-parser.rb
+++ Formula/f/fast-xml-parser.rb
@@ -1,8 +1,8 @@
 class FastXmlParser < Formula
   desc "Validate XML, Parse XML and Build XML rapidly"
   homepage "https://naturalintelligence.github.io/fast-xml-parser/"
-  url "https://registry.npmjs.org/fast-xml-parser/-/fast-xml-parser-5.2.0.tgz"
-  sha256 "3dd6e135710c80664a1d097d8cd3620e44a595d8d2049a0d071754b3ec78bfea"
+  url "https://registry.npmjs.org/fast-xml-parser/-/fast-xml-parser-5.2.1.tgz"
+  sha256 "5dccc3ee2cc07b6d36bd6c1d43bdf3500039466b944ce1a35f85c44be46d1f58"
   license "MIT"
 
   bottle do
