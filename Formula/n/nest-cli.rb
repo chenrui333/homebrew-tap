2946a0003cd79b7e1d6ba0d64de54737f62420ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-23 08:34:54 +0000

    nest-cli: update 11.0.7 bottle.

--- Formula/n/nest-cli.rb
+++ Formula/n/nest-cli.rb
@@ -7,10 +7,10 @@ class NestCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e01a3452485ceba99f7cf6bce3e793a94172ee217d97114a0a7ff2fa6ab2c910"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3eaf4a0827fcbe2329b7691d77ad1b883fd818f8211080804f31edabadd534a1"
-    sha256 cellar: :any_skip_relocation, ventura:       "8588f5a115f41cc8e0c866733ed5d86f7d9fa451a14563559f23aedc644a2050"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "91f9e57c8b25d4330f0e8b93d71efab6a7620e6bff749302b89e895b62e5a66d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec9b60bd462058c143546cc2b4c8d5107f611f1ab36066bb2cbda5b3a863b7fe"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "328a9c0eed8af32446d3afd8e1b0ee3cbd2b65f5f376e0541bbc93346dbee623"
+    sha256 cellar: :any_skip_relocation, ventura:       "571ba1117487feba079957d3a3d3d4b35d2b6f4e51c264449757d7e86b237963"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "847279d91b5278957b4a619942926a2597e655a9eb6853de368069caa49ff370"
   end
 
   depends_on "node"
