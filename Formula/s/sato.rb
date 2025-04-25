1f32b4caebf59f410728ac7cabea6646c4d1300c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-25 10:25:51 +0000

    sato 0.1.34
    Closes #768.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s/sato.rb
+++ Formula/s/sato.rb
@@ -2,8 +2,8 @@
 class Sato < Formula
   desc "Tool to convert ARM or CFN into Terraform"
   homepage "https://github.com/JamesWoolfenden/sato"
-  url "https://github.com/JamesWoolfenden/sato/archive/refs/tags/v0.1.33.tar.gz"
-  sha256 "aec1b117d4aaaf0037d97ea8da4b8ef20514a16d55bb19a49ecd4a882a91dd32"
+  url "https://github.com/JamesWoolfenden/sato/archive/refs/tags/v0.1.34.tar.gz"
+  sha256 "6e9be240d6fbdd747886ec4cdcc02677cf5e36a725e3f642e89696cca7152902"
   license "Apache-2.0"
   head "https://github.com/JamesWoolfenden/sato.git", branch: "master"
 
