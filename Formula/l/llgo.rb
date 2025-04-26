63a6fdc795048ad01cd1e5d0ac27da1db436cd78
Rui Chen <rui@chenrui.dev>
2025-04-26 08:39:14 -0400

    llgo: fix missing RPATH for libgc and libunwind on Linux
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/l/llgo.rb
@@ -0,0 +1,136 @@
+class Llgo < Formula
+  desc "Go compiler based on LLVM integrate with the C ecosystem and Python"
+  homepage "https://github.com/goplus/llgo"
+  url "https://github.com/goplus/llgo/archive/refs/tags/v0.11.1.tar.gz"
+  sha256 "abbd9b8b16c1f5273db86a6970c0ffa333b9aec7d5644d296a10686564822f1c"
+  license "Apache-2.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
+  bottle do
+    sha256 cellar: :any, arm64_sequoia: "a96f779a5389a9fcdb130cd9101d35fb2b2d1d3776368969b0993a8a0c867912"
+    sha256 cellar: :any, arm64_sonoma:  "564c695388e1174c54b7ec84fc1baf746dc83c6b39e4b13d2dfbb1f08542dac7"
+    sha256 cellar: :any, arm64_ventura: "5a6d47e546bec68fe6408efa6aa94f43ad13fd782a381d771f3d6643d0f53c51"
+    sha256 cellar: :any, sonoma:        "77ff6916be6051de0c73b92f332cac15decb446345176c48053081e6795d1572"
+    sha256 cellar: :any, ventura:       "19124f9d591258fb823d379730dedbbdd3f2e806bb04cafef1c41150934b722d"
+    sha256               x86_64_linux:  "e39a853e8b70f3f11ca64d3b99a44c4d4c70d06d1d3b472e93f4a8c7186f8d04"
+  end
+
+  depends_on "bdw-gc"
+  depends_on "go"
+  depends_on "libffi"
+  depends_on "libuv"
+  depends_on "lld@19"
+  depends_on "llvm@19"
+  depends_on "openssl@3"
+  depends_on "pkgconf"
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "libunwind"
+  end
+
+  def find_dep(name)
+    deps.map(&:to_formula).find { |f| f.name.match?(/^#{name}(@\d+)?$/) }
+  end
+
+  def install
+    llvm = find_dep("llvm")
+    ldflags = %W[
+      -s -w
+      -X github.com/goplus/llgo/internal/env.buildVersion=v#{version}
+      -X github.com/goplus/llgo/internal/env.buildTime=#{time.iso8601}
+      -X github.com/goplus/llgo/xtool/env/llvm.ldLLVMConfigBin=#{llvm.opt_bin/"llvm-config"}
+    ]
+    tags = nil
+    if OS.linux?
+      ENV.prepend "CGO_CPPFLAGS",
+        "-I#{llvm.opt_include} " \
+        "-D_GNU_SOURCE " \
+        "-D__STDC_CONSTANT_MACROS " \
+        "-D__STDC_FORMAT_MACROS " \
+        "-D__STDC_LIMIT_MACROS"
+      ENV.prepend "CGO_LDFLAGS", "-L#{llvm.opt_lib} -lLLVM"
+      tags = "byollvm"
+    end
+
+    system "go", "build", *std_go_args(ldflags:, tags:), "-o", libexec/"bin/", "./cmd/llgo"
+
+    libexec.install "LICENSE", "README.md", "go.mod", "go.sum", "runtime"
+
+    path_deps = %w[lld llvm go pkgconf].map { |name| find_dep(name).opt_bin }
+    script_env = { PATH: "#{path_deps.join(":")}:$PATH" }
+
+    if OS.linux?
+      libgc = find_dep("bdw-gc")
+      libunwind = find_dep("libunwind")
+      script_env[:CFLAGS] = "-I#{libunwind.opt_include} -I#{libgc.opt_include} $CFLAGS"
+      script_env[:LDFLAGS] = "-L#{libunwind.opt_lib} -L#{libgc.opt_lib} " \
+                             "-Wl,-rpath,#{libgc.opt_lib} " \
+                             "-Wl,-rpath,#{libunwind.opt_lib} $LDFLAGS"
+    end
+
+    (libexec/"bin").children.each do |f|
+      next if f.directory?
+
+      cmd = File.basename(f)
+      (bin/cmd).write_env_script libexec/"bin"/cmd, script_env
+    end
+  end
+
+  test do
+    goos = shell_output("go env GOOS").chomp
+    goarch = shell_output("go env GOARCH").chomp
+    assert_equal "llgo v#{version} #{goos}/#{goarch}", shell_output("#{bin}/llgo version").chomp
+
+    (testpath/"hello.go").write <<~GO
+      package main
+
+      import (
+          "fmt"
+
+          "github.com/goplus/lib/c"
+      )
+
+      func Foo() string {
+        return "Hello LLGO by Foo"
+      }
+
+      func main() {
+        fmt.Println("Hello LLGO by fmt.Println")
+        c.Printf(c.Str("Hello LLGO by c.Printf\\n"))
+      }
+    GO
+    (testpath/"hello_test.go").write <<~GO
+      package main
+
+      import "testing"
+
+      func Test_Foo(t *testing.T) {
+        got := Foo()
+        want := "Hello LLGO by Foo"
+        if got != want {
+          t.Errorf("foo() = %q, want %q", got, want)
+        }
+      }
+    GO
+    (testpath/"go.mod").write <<~GOMOD
+      module hello
+    GOMOD
+    system "go", "get", "github.com/goplus/lib"
+    # Test llgo run
+    assert_equal "Hello LLGO by fmt.Println\n" \
+                 "Hello LLGO by c.Printf\n",
+                 shell_output("#{bin}/llgo run .")
+    # Test llgo build
+    system bin/"llgo", "build", "-o", "hello", "."
+    assert_equal "Hello LLGO by fmt.Println\n" \
+                 "Hello LLGO by c.Printf\n",
+                 shell_output("./hello")
+    # Test llgo test
+    assert_match "PASS", shell_output("#{bin}/llgo test .")
+  end
+end
