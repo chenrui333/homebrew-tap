f411947a3264de66e3912bc83a58187101970eee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-04-30 13:23:47 +0000

    termsvg: add 0.9.2 bottle.

--- Formula/t/termsvg.rb
+++ Formula/t/termsvg.rb
@@ -6,6 +6,14 @@ class Termsvg < Formula
   license "GPL-3.0-only"
   head "https://github.com/MrMarble/termsvg.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dd201ab00b12deca51a81f223aba28c3e57b144a3026e1b8bc24b863ab2185be"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b6ca7f7d7145a5ec0fe0416a14c9b534891c776ab9f20aa83f7ba6f07aa32b9"
+    sha256 cellar: :any_skip_relocation, ventura:       "33ab6541a2af90c7e2ae38a1644a301bbd8f00928283c0bd3f56f04bcb0b1d7d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9d1c39a28440144510d0d4568cec69307dad6c1cbe408140db405a4df28c0f1d"
+  end
+
   depends_on "go" => :build
 
   def install
