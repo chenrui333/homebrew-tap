826a479e1ef3c0255623460cb0a09ebdbf4a92e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-02 18:26:09 +0000

    plandex 2.1.1

--- Formula/p/plandex.rb
+++ Formula/p/plandex.rb
@@ -1,8 +1,8 @@
 class Plandex < Formula
   desc "AI driven development in your terminal. Designed for large, real-world tasks"
   homepage "https://plandex.ai/"
-  url "https://github.com/plandex-ai/plandex/archive/refs/tags/cli/v2.1.0.tar.gz"
-  sha256 "8ab1cbd53ca1adc3fd6999a820bd81e51f19f0c832b37b8cc7215138c55d3c3d"
+  url "https://github.com/plandex-ai/plandex/archive/refs/tags/cli/v2.1.1.tar.gz"
+  sha256 "53d2f9007c32872a116a2344661951b46b04febd1ff6eedd5aac8e1761f6591d"
   license "MIT"
   head "https://github.com/plandex-ai/plandex.git", branch: "main"
 
