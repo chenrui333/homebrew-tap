4fc4358f709f6f384477e429cf7d45d4300fb3c1
Rui Chen <rui@chenrui.dev>
2025-05-02 17:50:33 -0400

    got 0.111
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/g/got.rb
@@ -0,0 +1,43 @@
+class Got < Formula
+  desc "Version control system"
+  homepage "https://gameoftrees.org/"
+  url "https://gameoftrees.org/releases/portable/got-portable-0.111.tar.gz"
+  sha256 "d096f76e91a700dd0d22fbaf9641c2b94f8a6de16f09b0f4939c9b96a9d878ce"
+  license "ISC"
+
+  livecheck do
+    url "https://gameoftrees.org/releases/portable/"
+    regex(/href=.*?got-portable[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "bison" => :build
+  depends_on "pkgconf" => :build
+  depends_on "libevent"
+  depends_on "libretls"
+  depends_on "ncurses"
+  depends_on "openssl@3"
+
+  uses_from_macos "zlib"
+
+  on_linux do
+    depends_on "libbsd"
+    depends_on "libmd"
+    depends_on "util-linux" # for libuuid
+  end
+
+  def install
+    ENV["LIBTLS_CFLAGS"] = "-I#{Formula["libretls"].opt_include}"
+    ENV["LIBTLS_LIBS"] = "-L#{Formula["libretls"].opt_lib} -ltls"
+    system "./configure", "--disable-silent-rules", *std_configure_args
+    system "make", "install"
+  end
+
+  test do
+    ENV["GOT_AUTHOR"] = "Flan Hacker <flan_hacker@openbsd.org>"
+    system bin/"gotadmin", "init", "repo.git"
+    mkdir "import-dir"
+    %w[haunted house].each { |f| touch testpath/"import-dir"/f }
+    system bin/"got", "import", "-m", "Initial Commit", "-r", "repo.git", "import-dir"
+    system bin/"got", "checkout", "repo.git", "src"
+  end
+end
