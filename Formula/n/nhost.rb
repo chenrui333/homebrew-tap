2bbe2671ee3f1bdf0754f0a3470ded69e04c8f83
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-06 08:33:17 +0000

    nhost: update 1.29.6 bottle.

--- Formula/n/nhost.rb
+++ Formula/n/nhost.rb
@@ -8,10 +8,10 @@ class Nhost < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2bba686f3c2f6653dcfd2094b435cab782c8d116a34ab2196425a7c7299fc4d3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "302e240766038ef3c33fc63e8a0c1d4b48d98fbcf834ea33fd5ed7747fb670c1"
-    sha256 cellar: :any_skip_relocation, ventura:       "39ceba8efeef2c6f899b3acc0771c9351e4c378a36d3bbad34bc940fb661012a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61144e53d043bfc97df33ffb6309709c7a6a6af0b86f32fa945bcddee9589018"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1763fd49e89802629ef4e24f6e0310679d8ea2ed230a889a261dbf8983386da4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3186ef39653ce3eab23df5ea27a98406122ff62e050b3d39e1e4f007bc1fbbfb"
+    sha256 cellar: :any_skip_relocation, ventura:       "ba2aebf60ecfeec6143c9e18a5c5028fca460d9d0267f6c2d579d9935403eb29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0fe2a266305e08ce018275ba6e50dc67c4c76602ea95281d3167e3c19307bad9"
   end
 
   depends_on "go" => :build
