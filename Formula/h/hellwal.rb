021e58b3754002abef6821b4903bec18c9215d8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-08 12:26:51 +0000

    hellwal 1.0.4
    hellwal: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/hellwal.rb
+++ Formula/h/hellwal.rb
@@ -1,8 +1,8 @@
 class Hellwal < Formula
   desc "Pywal-like color palette generator, but faster and in C"
   homepage "https://github.com/danihek/hellwal"
-  url "https://github.com/danihek/hellwal/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "eff95d7a0403fc06d57917e4b4d4dbf897a6f0300104592cf95be7b54c83bc90"
+  url "https://github.com/danihek/hellwal/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "a33d1c5257fe4b42e92cac7f055c6ed1a3e857fe52ab435924b316947d55e200"
   license "MIT"
 
   bottle do
@@ -13,8 +13,6 @@ class Hellwal < Formula
   end
 
   depends_on macos: :sonoma # failed on ventura
-
-  patch :DATA
 
   def install
     system "make"
@@ -34,22 +32,3 @@ class Hellwal < Formula
     system bin/"hellwal", "--help"
   end
 end
-
-__END__
-diff --git a/Makefile b/Makefile
-index c19c28f..88be1cf 100644
---- a/Makefile
-+++ b/Makefile
-@@ -6,10 +6,10 @@ LDFLAGS = -lm
- DESTDIR = /usr/local/bin
-
- hellwal: hellwal.c
--	$(CC) $(CFLAGS) $(LDFLAGS) hellwal.c -o hellwal
-+	$(CC) $(CFLAGS) hellwal.c $(LDFLAGS) -o hellwal
-
- debug: hellwal.c
--	$(CC) $(CFLAGS) -ggdb $(LDFLAGS) hellwal.c -o hellwal
-+	$(CC) $(CFLAGS) -ggdb hellwal.c $(LDFLAGS) -o hellwal
-
- clean:
- 	rm hellwal
