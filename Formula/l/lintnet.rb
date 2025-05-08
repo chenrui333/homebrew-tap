4675e186f1f40763ee7d123c6059e9dfd3999224
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-08 00:40:19 +0000

    lintnet 0.4.10
    lintnet: remove build patch
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/l/lintnet.rb
+++ Formula/l/lintnet.rb
@@ -2,8 +2,8 @@
 class Lintnet < Formula
   desc "General purpose linter for structured configuration data powered by Jsonnet"
   homepage "https://lintnet.github.io/"
-  url "https://github.com/lintnet/lintnet/archive/refs/tags/v0.4.9.tar.gz"
-  sha256 "c0022f31f7c789e2a7fe07a0ea174669bce2b87d6bd398d5448e00b8d15699a9"
+  url "https://github.com/lintnet/lintnet/archive/refs/tags/v0.4.10.tar.gz"
+  sha256 "d66371354e7d5918a15e486aa7bcbc678080cdaedaf3325778f3a37e6a523739"
   license "MIT"
   head "https://github.com/lintnet/lintnet.git", branch: "main"
 
@@ -16,12 +16,6 @@ class Lintnet < Formula
   end
 
   depends_on "go" => :build
-
-  # fix version typo
-  patch do
-    url "https://github.com/lintnet/lintnet/commit/ed1fea6ae5ed0e2a92d4ab085772cd9a667359b5.patch?full_index=1"
-    sha256 "234457c4b374471221c75270a32cdcea892d0ea11f7247aadc81102ed5dc4e8f"
-  end
 
   def install
     ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
