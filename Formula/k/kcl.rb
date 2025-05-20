042f6e5cb3a177d01485a42bbecbd86ffad09ae3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-20 08:33:14 +0000

    kcl: update 0.11.2 bottle.

--- Formula/k/kcl.rb
+++ Formula/k/kcl.rb
@@ -8,10 +8,10 @@ class Kcl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73cffb1c7f0f73c5fec0d9beab91eb9a57262bacf79c85865e1212d75adbad2d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7397fe6e6ae57c383be682401ee299ab09f3c91be53fc4e7db4db11ee77458d6"
-    sha256 cellar: :any_skip_relocation, ventura:       "7985e82a3a43d715e52d924038b2602fc94557eadf2f9dc28dc8e85dd916b6fb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1127a6c7eaf3048a1842b2b0b07f14308873ed110fb13653f8bee96b9eaade2a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3210183b28b514bdc0b2f9678b8063fcd72cd79a522b4814b0ac3dc07f0dd14b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1745f5878661ac5e496d48ff43bcf02db8eebf1f2a5d624c763d82c2f1190dae"
+    sha256 cellar: :any_skip_relocation, ventura:       "0bfcca9f44ad265a39cdea3746b63e703a1339929fee65870ec55fb214b18eae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dfbf6785011686b51ec80dc8297a58b16ef45a21df470d84668498004ed1b9a1"
   end
 
   depends_on "go" => :build
