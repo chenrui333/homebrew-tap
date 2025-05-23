001574cf86f4ca4ef72591bb11bf805d4f705276
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-23 14:30:53 +0000

    addons-linter: update 7.13.0 bottle.

--- Formula/a/addons-linter.rb
+++ Formula/a/addons-linter.rb
@@ -7,10 +7,10 @@ class AddonsLinter < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3a26c7e674e497cffc3c7495915bbf6907cb336f94a3c3b5ab472539a7c0ff56"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dcd92342685367f3ca6b1fb011e02a2ade132c07b53e4aa0d6ed367836a6995e"
-    sha256 cellar: :any_skip_relocation, ventura:       "6a5c2fd81c2449abd4d4e48617ef847e66bf7668ecbddd3b16bdb71d650d7808"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "192b9013ba8f46c95acf9d489908234c445bf6128ad897048443cc0592595e47"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78e0a367502d5e03e2c3cc0ce705ecde9bcc87e6a4b34239ffce810411b646bc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "075c2cc90dc9258821045e59bd1d1428e19155ca3cc394d00eadf352b8a744ba"
+    sha256 cellar: :any_skip_relocation, ventura:       "b15db64d26b4eced26aa3cae69aa84988734c6a440e0f72965c8c33077151c95"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e517e4d2cd85e2792dfb9e91aac6cb07a2d339c2447688234bef1ed62006c7cd"
   end
 
   depends_on "node"
