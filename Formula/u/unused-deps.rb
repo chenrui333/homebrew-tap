4b424e22d45d566737e1f99d4ee0e9b3e25fa674
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-26 01:56:57 +0000

    unused-deps: add 8.2.0 bottle.

--- Formula/u/unused-deps.rb
+++ Formula/u/unused-deps.rb
@@ -6,6 +6,14 @@ class UnusedDeps < Formula
   license "Apache-2.0"
   head "https://github.com/bazelbuild/buildtools.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d940dd0dd960b9e4a850bee268b6bd75d3f5a0f3629192c75f30e1c1231e0d02"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8b0dc33e4645928fd510857fd3f8d73f6c13dd4f72af23245e2d7b9e4f495af"
+    sha256 cellar: :any_skip_relocation, ventura:       "4167007b61ce6ea680843aae3aa146073caf74abd65102fbb86d413a5294d9ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f3af5db6f0d547ec3528b3428de51b87c67800dbb79c9bee13dc4380dad25e5"
+  end
+
   depends_on "go" => :build
 
   def install
