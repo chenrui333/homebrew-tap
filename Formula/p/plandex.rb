8df104d44158905776521e00c9a16a9208e8155d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-27 00:41:08 +0000

    plandex 2.1.6
    Closes #893.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/p/plandex.rb
+++ Formula/p/plandex.rb
@@ -1,8 +1,8 @@
 class Plandex < Formula
   desc "AI driven development in your terminal. Designed for large, real-world tasks"
   homepage "https://plandex.ai/"
-  url "https://github.com/plandex-ai/plandex/archive/refs/tags/cli/v2.1.5.tar.gz"
-  sha256 "7f6be0d7798eaaab888683fc377c9a9a4f4ce4b1161dd49ae44a9cf7483f4562"
+  url "https://github.com/plandex-ai/plandex/archive/refs/tags/cli/v2.1.6.tar.gz"
+  sha256 "32a6e59957935dfdfd118fd91c7ac57b0175a0334ac5d847e17cc29231b0f8cb"
   license "MIT"
   head "https://github.com/plandex-ai/plandex.git", branch: "main"
 
