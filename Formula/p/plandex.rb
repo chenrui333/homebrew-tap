373d02812b47306063700d058683138434586f73
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-05-27 00:47:21 +0000

    plandex: update 2.1.6 bottle.

--- Formula/p/plandex.rb
+++ Formula/p/plandex.rb
@@ -13,10 +13,10 @@ class Plandex < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c463e4213b059b88f2a002e26decd9e96118560c40edcf4d3ac28ad3db6e1cf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "39fec68e545e8a8133346f116ee5e5bdfb7cccc4845e1fb99cf916bdb4aabd85"
-    sha256 cellar: :any_skip_relocation, ventura:       "7379e0769970dac977ffa8d968c32c7731daa49dd1a1f4cff13c619a38d94d26"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a901d533e1f45d99f706806848d0f5dd5b2f88ff0db4125a86e305a509c8b2fe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8d262bd21d3acda3e556a841afae11ca180a1c10b19b95f3c6e30d90332be09"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "03dedbcb851c9669171a53b253f36bbb9b11694903ca3b302d98bbd177b01658"
+    sha256 cellar: :any_skip_relocation, ventura:       "11463a7da31c6488cfd006371fabf4bfdad41d2039995e24df1de64790ba9f01"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e8471494a2e29cab3d3cbdfd62f7ec115a9683b13cdd4db4bfb31d5c68e951f"
   end
 
   depends_on "go" => :build
