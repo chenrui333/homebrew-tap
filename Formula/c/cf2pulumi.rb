8e75a9858c52dd4a63513c7cf412e2946ef1a4e4
Rui Chen <rui@chenrui.dev>
2025-05-30 14:10:02 -0400

    cf2pulumi 1.29.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/cf2pulumi.rb
@@ -0,0 +1,71 @@
+class Cf2pulumi < Formula
+  desc "Convert CloudFormation Templates to Pulumi programs"
+  homepage "https://github.com/pulumi/pulumi-aws-native"
+  url "https://github.com/pulumi/pulumi-aws-native.git",
+      tag:      "v1.29.0",
+      revision: "f45078a274ff7d7958e9356e7ffe1eac7e7828b1"
+  license "Apache-2.0"
+  head "https://github.com/pulumi/pulumi-aws-native.git", branch: "master"
+
+  depends_on "go" => :build
+  depends_on "pulumictl" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/pulumi/pulumi-aws-native/provider/version.Version=#{version}
+    ]
+    cd "provider" do
+      system "go", "build",
+            *std_go_args(ldflags:, output: buildpath/"bin/pulumi-gen-aws-native"),
+            "./cmd/pulumi-gen-aws-native"
+    end
+
+    system "make", "generate_schema"
+    cd "provider" do
+      system "go", "build", *std_go_args(ldflags:), "./cmd/cf2pulumi"
+    end
+  end
+
+  test do
+    (testpath/"test.yaml").write <<~YAML
+      AWSTemplateFormatVersion: '2010-09-09'
+      Resources:
+        MyS3Bucket:
+          Type: 'AWS::S3::Bucket'
+          Properties:
+            BucketName: my-test-bucket
+            AccessControl: Private
+        MyEC2Instance:
+          Type: 'AWS::EC2::Instance'
+          Properties:
+            InstanceType: t2.micro
+            ImageId: ami-0c55b159cbfafe1f0
+    YAML
+
+    assert_match <<~TYPESCRIPT, shell_output("#{bin}/cf2pulumi nodejs #{testpath}/test.yaml")
+      import * as pulumi from "@pulumi/pulumi";
+      import * as aws_native from "@pulumi/aws-native";
+
+      const myS3Bucket = new aws_native.s3.Bucket("myS3Bucket", {
+          bucketName: "my-test-bucket",
+          accessControl: aws_native.s3.BucketAccessControl.Private,
+      });
+      const myEC2Instance = new aws_native.ec2.Instance("myEC2Instance", {
+          instanceType: "t2.micro",
+          imageId: "ami-0c55b159cbfafe1f0",
+      });
+    TYPESCRIPT
+
+    assert_match <<~PYTHON, shell_output("#{bin}/cf2pulumi python #{testpath}/test.yaml")
+      import pulumi_aws_native as aws_native
+
+      my_s3_bucket = aws_native.s3.Bucket("myS3Bucket",
+          bucket_name="my-test-bucket",
+          access_control=aws_native.s3.BucketAccessControl.PRIVATE)
+      my_ec2_instance = aws_native.ec2.Instance("myEC2Instance",
+          instance_type="t2.micro",
+          image_id="ami-0c55b159cbfafe1f0")
+    PYTHON
+  end
+end
