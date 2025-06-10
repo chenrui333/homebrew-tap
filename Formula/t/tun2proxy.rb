2d89a5b674baf874dfb8c5622a3e6c1e23bc8cac
Rui Chen <rui@chenrui.dev>
2025-06-09 23:52:51 -0400

    tun2proxy 0.7.10
    Closes #937.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/t/tun2proxy.rb
+++ Formula/t/tun2proxy.rb
@@ -1,10 +1,15 @@
 class Tun2proxy < Formula
   desc "Tunnel (TUN) interface for SOCKS and HTTP proxies"
   homepage "https://github.com/tun2proxy/tun2proxy"
-  url "https://github.com/tun2proxy/tun2proxy/archive/refs/tags/v0.6.6.tar.gz"
-  sha256 "d2177c609dc77d8e79f6c051080b28c3fc718e6b9155ddc19d5a7e997d5c063e"
+  url "https://github.com/tun2proxy/tun2proxy/archive/refs/tags/v0.7.10.tar.gz"
+  sha256 "792b1267d47380289745dca1300d2e42a1c6f3f33af475a54031b0b56c4ff61c"
   license "MIT"
   head "https://github.com/tun2proxy/tun2proxy.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
@@ -23,7 +28,7 @@ class Tun2proxy < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/tun2proxy-bin --version")
-    output = shell_output("#{bin}/tun2proxy-bin --proxy socks5://127.0.0.1:1080 --tun utun4 2>&1")
+    output = shell_output("#{bin}/tun2proxy-bin --proxy socks5://127.0.0.1:1080 --tun utun4 2>&1", 1)
     assert_match "Operation not permitted (os error 1)", output
   end
 end
