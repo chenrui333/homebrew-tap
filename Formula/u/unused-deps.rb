43b6da2ddc0aae942b51412bcc8daf5ec1329349
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-10 12:28:36 +0000

    unused-deps 8.2.1
    Closes #944.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/u/unused-deps.rb
+++ Formula/u/unused-deps.rb
@@ -1,8 +1,8 @@
 class UnusedDeps < Formula
   desc "Determine any unused dependencies in java_library rules"
   homepage "https://github.com/bazelbuild/buildtools"
-  url "https://github.com/bazelbuild/buildtools/archive/refs/tags/v8.2.0.tar.gz"
-  sha256 "444a9e93e77a45f290a96cc09f42681d3c780cfbf4ac9dbf2939b095daeb6d7d"
+  url "https://github.com/bazelbuild/buildtools/archive/refs/tags/v8.2.1.tar.gz"
+  sha256 "53119397bbce1cd7e4c590e117dcda343c2086199de62932106c80733526c261"
   license "Apache-2.0"
   head "https://github.com/bazelbuild/buildtools.git", branch: "main"
 
