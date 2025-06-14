d6e4cf9b3f0af7d846c31f9612fc792e09248b0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-14 23:26:53 +0000

    gobgp: add 3.37.0 bottle.

--- Formula/g/gobgp.rb
+++ Formula/g/gobgp.rb
@@ -6,6 +6,14 @@ class Gobgp < Formula
   license "Apache-2.0"
   head "https://github.com/osrg/gobgp.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "805c0518e92140a1830dbd179f31c6bcfd931bee6ad692b85341111fa1beb1a6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ba44bb3a810e0fd4fea3f9a08334021c04b64f3acb663fa95dfba3f3db1a9a0"
+    sha256 cellar: :any_skip_relocation, ventura:       "cbb2e34491d70cd4d809ad8cd10bffe492abd0e2e5b9698a3e8961173be3fc22"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "46c6fc4bedf08ac641bb37fac7fead51117901023320fac0a8c10c8ff9c836c4"
+  end
+
   depends_on "go" => :build
 
   def install
