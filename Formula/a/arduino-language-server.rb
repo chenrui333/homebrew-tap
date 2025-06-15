01a0741a3857f21efbe4cc807b10c4f4430c1052
Rui Chen <rui@chenrui.dev>
2025-06-15 00:15:36 -0400

    arduino-language-server: update desc
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/arduino-language-server.rb
+++ Formula/a/arduino-language-server.rb
@@ -1,5 +1,5 @@
 class ArduinoLanguageServer < Formula
-  desc "Simplistic command runner and build system"
+  desc "Language server for Arduino development"
   homepage "https://github.com/arduino/arduino-language-server"
   url "https://github.com/arduino/arduino-language-server/archive/refs/tags/0.7.7.tar.gz"
   sha256 "ac9e63e3bd971ba85cdd33e19d736effde804114156b4bf3c0a969ba2d7395c3"
