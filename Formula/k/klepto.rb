c21f0445af57cab6ffafd393e9ecbf3079619b6f
Rui Chen <rui@chenrui.dev>
2025-06-15 00:13:17 -0400

    klepto: update desc
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/klepto.rb
+++ Formula/k/klepto.rb
@@ -1,5 +1,5 @@
 class Klepto < Formula
-  desc "Protocol Buffer companion tool"
+  desc "Tool for copying and anonymising data"
   homepage "https://github.com/hellofresh/klepto"
   url "https://github.com/hellofresh/klepto/archive/refs/tags/v0.4.5.tar.gz"
   sha256 "f62bc59204db301f0f1122e4a9429019afdbe23ac91903252c0b4cf78309507d"
