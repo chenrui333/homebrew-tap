3d530d7cdb46a75d4e5789ac51e48d97b48662ec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-15 03:59:45 +0000

    mpremote: add 1.25.0 bottle.

--- Formula/m/mpremote.rb
+++ Formula/m/mpremote.rb
@@ -7,6 +7,14 @@ class Mpremote < Formula
   sha256 "d0dcd8ab364d87270e1766308882e536e541052efd64aadaac83bc7ebbea2815"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "820a6f816a48d4c754d6da316586d0f962912dc3d963cec9cc06c12d80d22282"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0c5082f733baf556a081d342f23c15d5640d12cc2c4ec1b089250edc2b53bcd9"
+    sha256 cellar: :any_skip_relocation, ventura:       "aafc672fcefd1c8899be354bf12a3805c6d3fd4cebaa2b6540c9bc4f621666d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d304561e9eaeb3230627b044d31026322b3580beecafd4b06b1f7690823b492"
+  end
+
   depends_on "python@3.13"
 
   resource "pyserial" do
