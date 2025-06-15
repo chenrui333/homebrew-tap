be6fe5b97708e8d15a868faa1eb9ae5683d875a4
Rui Chen <rui@chenrui.dev>
2025-06-15 00:10:06 -0400

    pyp: fix homepage copy paste error
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pyp.rb
+++ Formula/p/pyp.rb
@@ -2,7 +2,7 @@ class Pyp < Formula
   include Language::Python::Virtualenv
 
   desc "Easily run Python at the shell! Magical, but never mysterious"
-  homepage "https://papis.readthedocs.io/en/latest/"
+  homepage "https://github.com/hauntsaninja/pyp"
   url "https://files.pythonhosted.org/packages/0c/65/c275ff380e4412438577eab23810dd7a1ba2cf54a6ba558a3d22cf0fb68b/pypyp-1.3.0.tar.gz"
   sha256 "97c78f8fd6d4550bf67bb5001a4c5c1fa58184d9bd8256abac3e240fa38aa05c"
   license "MIT"
