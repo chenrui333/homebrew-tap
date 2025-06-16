4ed2921cae4568652a1ed955666b2eb905bee97e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-16 23:40:27 +0000

    ddev: add 1.24.6 bottle.

--- Formula/d/ddev.rb
+++ Formula/d/ddev.rb
@@ -6,6 +6,14 @@ class Ddev < Formula
   license "Apache-2.0"
   head "https://github.com/ddev/ddev.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e4f644eeb3ed8bae8e5a4670d0763ab7c3ec3b8d14ca967683e3b8e70f4ef8f1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a72cc99acc202f5ddf881befdca8a43aae38580fd9d009a6ef6ed7eff80ca647"
+    sha256 cellar: :any_skip_relocation, ventura:       "2458bf1a49c50e329854fdb9bf619242dfd95ed23df8273806c9da1a6e7e473e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a3f898ee25a6808025a7531594c5e5cb528069226f0b47afa1c6a8042344ee1"
+  end
+
   depends_on "go" => :build
   depends_on "docker" => :test
 
