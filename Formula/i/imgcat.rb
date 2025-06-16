0a2ec6406423b00b144554c43b71680c03a3c02e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-16 02:22:32 +0000

    imgcat: add 2.6.0 bottle.

--- Formula/i/imgcat.rb
+++ Formula/i/imgcat.rb
@@ -5,6 +5,14 @@ class Imgcat < Formula
   sha256 "1e7e69670ad73e36ba1a9f0a09b6a787cf4e141dfe7885ae7ad77c293fb999a6"
   license "ISC"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9883b173598546d481336a75f5f933b2b42602f6e557ffe0e5d98402220db1bb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "371b83117237c20f7e6f1141f4124d88ad3ec487cefa4a263ae261a9a47c1fb1"
+    sha256 cellar: :any_skip_relocation, ventura:       "4d8111925cf72cee1f3931aaae913ac8f958f3d519013084620262947890d9e4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de7aadac53f2e43aea7fec7da3675cb1a40e66ab2b9b468c9866a3ee983429ad"
+  end
+
   depends_on "jpeg-turbo"
   depends_on "libpng"
 
