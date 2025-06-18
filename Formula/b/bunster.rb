7ce8a643fcc6c6cff2555730a9ccc1f930b14ff1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-18 05:49:23 +0000

    bunster: add 0.13.0 bottle.

--- Formula/b/bunster.rb
+++ Formula/b/bunster.rb
@@ -6,6 +6,14 @@ class Bunster < Formula
   license "BSD-3-Clause"
   head "https://github.com/yassinebenaid/bunster.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "51d508a75d0a9da15a212c1bcad2d80d0f593c30395b3cc29ee7f0956df5ccc8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b4fb83ac844d8253058517c855700cd67774c027588ce32cf5f925d097a7e561"
+    sha256 cellar: :any_skip_relocation, ventura:       "623564ada6731ff8eed8b78778ee485d2aa3744da4127038acb87bc66c1d38d5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "99247861b891bc1bde0cc6236358bfb75863a601a429a2dd28f56d414522f55f"
+  end
+
   depends_on "go"
 
   def install
