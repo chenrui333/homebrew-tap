fc4b0ed19e17804f170cb5e66dabb66f06d395b6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-19 18:29:24 +0000

    xcpkg: add 0.30.0 bottle.

--- Formula/x/xcpkg.rb
+++ Formula/x/xcpkg.rb
@@ -6,6 +6,13 @@ class Xcpkg < Formula
   sha256 "69ed7002aa49d9ece103c54f7483ab010bf33aceb5e6a98ab32728b2cbd87434"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any, arm64_sequoia: "0970a19468329f62e8190c64e89a2612a0d4ac165ce191499bb7c32bba2ae3b2"
+    sha256 cellar: :any, arm64_sonoma:  "fd0b4d32bfbb712d8cc014c886ece29477c3ec93d6d153362e4b371f24c602ae"
+    sha256 cellar: :any, ventura:       "4d5284d977d565e3404e895780d73f15bf13f4e15f95dfd8dfb476e064a1bc27"
+  end
+
   depends_on "cmake" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
