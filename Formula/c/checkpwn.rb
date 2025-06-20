09804adf02ab0ea18efa96833e06bc9636af5f05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-20 04:34:05 +0000

    checkpwn: add 0.5.4 bottle.

--- Formula/c/checkpwn.rb
+++ Formula/c/checkpwn.rb
@@ -6,6 +6,14 @@ class Checkpwn < Formula
   license "MIT"
   head "https://github.com/brycx/checkpwn.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8e561ed33f90144ef5e3924ff0b1d67db160b39351992be1adc2674da2c06a6a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f1ac035448c27c94e808a8aa3d06859c7c87480582915c419f5165012bf67d12"
+    sha256 cellar: :any_skip_relocation, ventura:       "e10810958c78a64eb2e5e311e1fe24242dd7d7dd65556df6addf1ab14a656c8a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "74d69458fd30f4d2467e78de1c8a00a18db486b510eb7670138eb1c1c6c4ecc1"
+  end
+
   depends_on "rust" => :build
 
   def install
