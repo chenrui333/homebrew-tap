6e7fb5ba89c6c64c351933d9ba3176a4a2825aab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-20 05:01:02 +0000

    hygg: add 0.1.15 bottle.

--- Formula/h/hygg.rb
+++ Formula/h/hygg.rb
@@ -6,6 +6,14 @@ class Hygg < Formula
   license "AGPL-3.0-only"
   head "https://github.com/kruserr/hygg.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0750d6b3fd6e17b70404d5bd554fab423f4bbd4c5434a07421f59e3616b9ff60"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cdc3f7de14778553a89b33c6ded0f7347d5c0081fa4db7046e2a0af881255456"
+    sha256 cellar: :any_skip_relocation, ventura:       "7b38c9956761723ea7436a0f24d5a1b1bf3425b035db89adb87d6ba43f2ae40c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6216c3e025d53eef0156297734868bd031da72c524cdaf6d7375ecb8bc8e1947"
+  end
+
   depends_on "rust" => :build
   depends_on "ocrmypdf"
 
