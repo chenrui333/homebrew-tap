d09bdccf27a478b6245cb3641b9caba47f1fefa0
Rui Chen <rui@chenrui.dev>
2025-06-23 10:02:46 -0400

    nhost: revert to 1.29.9
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/n/nhost.rb
+++ Formula/n/nhost.rb
@@ -1,18 +1,22 @@
 class Nhost < Formula
   desc "Developing locally with the Nhost CLI"
   homepage "https://docs.nhost.io/development/cli/overview"
-  url "https://github.com/nhost/cli/archive/refs/tags/v1.29.9-jovermier.tar.gz"
-  version "1.29.9-jovermier"
-  sha256 "c95df107ce96962294d737d45960b514a3c4fcacf5cf82ff94aab413456daed7"
+  url "https://github.com/nhost/cli/archive/refs/tags/v1.29.9.tar.gz"
+  sha256 "d104ae870f3afc2f94bd453423b1e5cf1b4b292a63bf4368a46f559c0ad3b0f6"
   license "MIT"
   head "https://github.com/nhost/cli.git", branch: "main"
 
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
+
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ad5ba09f8c95ca79621528907a1481444bfac6aef432ea0c3dadb8485005cb1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd3b7790a8a78bb37b3e7f8068b1da2fd6a2a833cf8c272f12500960b494fd11"
-    sha256 cellar: :any_skip_relocation, ventura:       "45e8cf6156c84a278cb5fca73f92e55b4bdb0ee1778d92513de986da74e96279"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3fba651b0bab56ff3a3e6354cf52ee39129fa7d06590c932dee0620790b4aed6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c7cc5e4104b684248570cde5fe7d87bbf89132b30f234ec06d21561cc833fa54"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9ec47f6068d24f2dd3aad4314e3bf5fc343b765249048f1617caccb5bd90d37b"
+    sha256 cellar: :any_skip_relocation, ventura:       "c2e22e90fb8a9552122060e2b3cf5ca02933ebb1bc30309f2f34857c2cda6a0c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "218c910cfc9aedfd15ce985c6c0f10b3cd4b36a4e8e86aed5ce32df75a2585dd"
   end
 
   depends_on "go" => :build
