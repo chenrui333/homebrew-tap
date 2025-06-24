ddb34ec502e4602e84408914800d4a9a4e5b0426
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-24 14:25:06 +0000

    meteor 0.28.1

--- Formula/m/meteor.rb
+++ Formula/m/meteor.rb
@@ -2,8 +2,8 @@
 class Meteor < Formula
   desc "Highly configurable CLI tool for writing conventional commits"
   homepage "https://github.com/stefanlogue/meteor"
-  url "https://github.com/stefanlogue/meteor/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "3c6f0bafcf27d26a3ef369084630488a4ca05324cb11e61b2e020d827bec51d1"
+  url "https://github.com/stefanlogue/meteor/archive/refs/tags/v0.28.1.tar.gz"
+  sha256 "aae2d28803c778c0dc8dbfdf1d5961dc06fefb11ee8b8531f71a7804ec35fef2"
   license "MIT"
   head "https://github.com/stefanlogue/meteor.git", branch: "main"
 
