44dcdf9dd95694fc9cb424a8d1d0f03a901aa6d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-24 14:25:17 +0000

    mqtt-cli 4.40.2

--- Formula/m/mqtt-cli.rb
+++ Formula/m/mqtt-cli.rb
@@ -1,8 +1,8 @@
 class MqttCli < Formula
   desc "CLI for connecting various MQTT clients supporting MQTT 5.0 and 3.1.1"
   homepage "https://hivemq.github.io/mqtt-cli/"
-  url "https://github.com/hivemq/mqtt-cli/archive/refs/tags/v4.40.0.tar.gz"
-  sha256 "2a91d6719d99ef3598a53be3a9174f0cbda3f8399e368bb31365baa540b8445c"
+  url "https://github.com/hivemq/mqtt-cli/archive/refs/tags/v4.40.2.tar.gz"
+  sha256 "f87ea8a24939d9c69f831f20712b16df213f9ba1dae4c693d1c2798696bc6db4"
   license "Apache-2.0"
   head "https://github.com/hivemq/mqtt-cli.git", branch: "master"
 
