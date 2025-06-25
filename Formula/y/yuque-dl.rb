7c5f80657bd3b215d25b88212a11b4d44a615e72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-25 18:36:22 +0000

    yuque-dl: update 1.0.80 bottle.

--- Formula/y/yuque-dl.rb
+++ Formula/y/yuque-dl.rb
@@ -7,10 +7,10 @@ class YuqueDl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_sequoia: "2c37a4efff1d09a7053f1366e716c63bacfe2e13942ea693250c48caadd4b05b"
-    sha256 cellar: :any,                 arm64_sonoma:  "aece2e0bd9e2e7060da082a41ab897ed0d09d493dee10c626f8925ef0c5ac14b"
-    sha256 cellar: :any,                 ventura:       "e33803cba743b9b25a709f2ebc0615ecf63eba9b095b2994fb593cb0815a576e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "41a0d2c8b088e0b8ea64f99225925cf1cfb9892451ef5da9c7501cf1c8cfa39f"
+    sha256 cellar: :any,                 arm64_sequoia: "276ca23021dd9fed6648a8ea7bb511d82cafc9a5e7d008da551291340ac080c2"
+    sha256 cellar: :any,                 arm64_sonoma:  "770eec23a229705293bab23d3f9b50a2bd871b03b1d9cef17e92c7fee401c59a"
+    sha256 cellar: :any,                 ventura:       "ce289160e3ea0e93684acaba7132935f291b3035944a29df683d5e7cb0ca6ae5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9cf6aa39d8962a91741c714188221ac529a6180891fba3d35d9f9a2ac5dad381"
   end
 
   depends_on "node"
