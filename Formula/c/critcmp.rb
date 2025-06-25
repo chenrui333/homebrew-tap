76ee057af278c2404d161a18548cad03e7ba91fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-06-25 02:51:17 +0000

    critcmp: add 0.1.8 bottle.

--- Formula/c/critcmp.rb
+++ Formula/c/critcmp.rb
@@ -6,6 +6,14 @@ class Critcmp < Formula
   license any_of: ["Unlicense", "MIT"]
   head "https://github.com/BurntSushi/critcmp.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4c44476df526e3d81bdb9861eaa34ef46334ade6c0e8588e4d7079c2dd0a25fc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43f34408ad5da29259e0010ff5e52a3231ffa9e5911ab59ad83ad98e1754e275"
+    sha256 cellar: :any_skip_relocation, ventura:       "734c260f105c3d442ea79fcf85642421697c368e35de34daa2d6e2389c91953a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b5ec9cfb442a982487a8bf58b560e411e4518a836d440ac2d16f3f29db02f481"
+  end
+
   depends_on "rust" => [:build, :test]
 
   def install
