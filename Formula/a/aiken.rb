03cdd8ac028c6636c4d893ab26f485981a1e4b05
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-28 18:37:12 +0000

    aiken: update 1.1.19 bottle.

--- Formula/a/aiken.rb
+++ Formula/a/aiken.rb
@@ -8,10 +8,10 @@ class Aiken < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73cb7ae5466b09c1202728f4e73403b45658d347ee9e9a120b448551e2dd29c5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fb00b90907ce1ab7237eed919c9dbad022e8da83e8dc50ab5bd14fce109dea87"
-    sha256 cellar: :any_skip_relocation, ventura:       "eaf0466ed4907c53db9fb7bbfd4e40002bf0422330dbe53cfb6b315a12be57b2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2a8518e207f037398d83517977f3352608cbab070d33587e7bb1cd5de5cd6f2e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "901d949f776a96b980dd7b4317f3dd12db121ad4ed5e6ef324a928156b7efe83"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8a87c4508b84e75428566589065a6bb42d04f0d74ab1608a3703c56c97ad1bf5"
+    sha256 cellar: :any_skip_relocation, ventura:       "c8e2faa5f78a0590c8abbce86f9e7a171968379e3f3c07f68936fd78fb7691ea"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c5ba9a14b6a2543edce4ac0938a8aebd86d469f5dd87a4dd2fe979e95bfa74a"
   end
 
   depends_on "pkgconf" => :build
