8c62af703d93123c1673666fbeb134e41c31340f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-29 23:26:46 +0000

    mcphost: add 0.26.1 bottle.

--- Formula/m/mcphost.rb
+++ Formula/m/mcphost.rb
@@ -6,6 +6,14 @@ class Mcphost < Formula
   license "MIT"
   head "https://github.com/mark3labs/mcphost.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "979746664673fb2bf5a330f25c21468c5f537bd7a6d04a6d7298521417daaaf2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "edcc3870fdf02c72dc623cb5fec0bebf41fef60b3dbb0b27fccf9a6e9d2f92f0"
+    sha256 cellar: :any_skip_relocation, ventura:       "45100e0c7c63a17053ddd187b542970ef88e97e3cb58d108342498e260e0e9d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86d1bfd41095ee0fd93e54977c31848b057e785e662d98ebd561498e3a555929"
+  end
+
   depends_on "go" => :build
 
   def install
