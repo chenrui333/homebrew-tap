7ad7249b690fb6759e9d154fcc7e0bf8711ef621
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-29 08:24:51 +0000

    ai-context 1.13
    Closes #1103.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/a/ai-context.rb
+++ Formula/a/ai-context.rb
@@ -2,8 +2,8 @@
 class AiContext < Formula
   desc "CLI tool to produce MD context files from many sources"
   homepage "https://github.com/Tanq16/ai-context"
-  url "https://github.com/Tanq16/ai-context/archive/refs/tags/v1.10.tar.gz"
-  sha256 "d776109efd9c4fb942252ec30fcc93a38045046cafc4c1208d398a5f14374881"
+  url "https://github.com/Tanq16/ai-context/archive/refs/tags/v1.13.tar.gz"
+  sha256 "5d544171aaa0f9ef7520230e154cc6856026b54e6eb499b97b8ffe9902b5c487"
   license "MIT"
   head "https://github.com/Tanq16/ai-context.git", branch: "main"
 
