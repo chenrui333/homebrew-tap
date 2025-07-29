90fd842c4cad85567745ab9a04b506377b0d9fe2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-07-29 23:38:51 +0000

    gitea-mcp-server: add 0.3.0 bottle.

--- Formula/g/gitea-mcp-server.rb
+++ Formula/g/gitea-mcp-server.rb
@@ -6,6 +6,14 @@ class GiteaMcpServer < Formula
   license "MIT"
   head "https://gitea.com/gitea/gitea-mcp.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff0326bbc334fab7a9433070febbe617f1aec4816ab7ffbec458172e88ece86d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b1d2f742544b1fec28f56b18762fcd445efd49eb7e11034f9c2be24f4dd29f43"
+    sha256 cellar: :any_skip_relocation, ventura:       "00b06073ae7455214e65a6f13c90653a373c6d7c58db7dc1221802e015f0e2de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d70c553310649c2f60d9264907ed6a2ac4c983f07c0717451fcbdfa9ae192394"
+  end
+
   depends_on "go" => :build
 
   def install
