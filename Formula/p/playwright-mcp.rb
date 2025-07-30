f8411f7c025347f08fc8b0f1879104cd62f1786c
Rui Chen <rui@chenrui.dev>
2025-07-30 05:52:52 -0400

    playwright-mcp 0.0.32 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/playwright-mcp.rb
@@ -0,0 +1,25 @@
+class PlaywrightMcp < Formula
+  desc "MCP server for Playwright"
+  homepage "https://github.com/microsoft/playwright-mcp"
+  url "https://registry.npmjs.org/@playwright/mcp/-/mcp-0.0.32.tgz"
+  sha256 "b76ad8444ca62e0aa793e1a46632816063abb79a935580b0b11e9c584d22fdfb"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/mcp-server-playwright --version")
+
+    json = <<~JSON
+      {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2025-03-26"}}
+      {"jsonrpc":"2.0","id":2,"method":"tools/list"}
+    JSON
+
+    assert_match "browser_close", pipe_output(bin/"mcp-server-playwright", json, 0)
+  end
+end
