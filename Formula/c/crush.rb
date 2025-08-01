070db7e138b1ad557d19ab0a2fc44e8025e82817
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-01 23:43:11 +0000

    crush: add 0.1.11 bottle.

--- Formula/c/crush.rb
+++ Formula/c/crush.rb
@@ -6,6 +6,14 @@ class Crush < Formula
   # license "FSL-1.1-MIT"
   head "https://github.com/charmbracelet/crush.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "39cbbdd9ca0add7dcab53a425e20209a5c0649173e97fade1ab171a35c9885ce"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "312cdbeb90f45e5d3ff182fb360d09fbd91f934057025f29dd9112f1da51e480"
+    sha256 cellar: :any_skip_relocation, ventura:       "1f6f16c89519671cf2b5e38b3996e79c2aab9537db5e10f50799f85af049dfee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d4aea6e5dd797f341aa2342ac5b69415ff55f8902aa0c03f17930a7b51bea4a"
+  end
+
   depends_on "go" => :build
 
   def install
