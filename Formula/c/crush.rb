c8b6819277864097b771f9e7d0c14fe61899c5fa
Rui Chen <rui@chenrui.dev>
2025-08-01 19:34:49 -0400

    crush 0.1.11 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/crush.rb
@@ -0,0 +1,24 @@
+class Crush < Formula
+  desc "Glamorous AI coding agent for your favorite terminal"
+  homepage "https://github.com/charmbracelet/crush"
+  url "https://github.com/charmbracelet/crush/archive/refs/tags/v0.1.11.tar.gz"
+  sha256 "809edd8e05b54f265ba1b8f8a0364fb65e923a56e70eb25c301c71e212838088"
+  # license "FSL-1.1-MIT"
+  head "https://github.com/charmbracelet/crush.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/charmbracelet/crush/internal/version.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"crush", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/crush --version")
+
+    output = shell_output("#{bin}/crush run 'Explain the use of context in Go' 2>&1", 1)
+    assert_match "no providers configured", output
+  end
+end
