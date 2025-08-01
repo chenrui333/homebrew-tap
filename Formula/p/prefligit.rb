7bdc70567b642c2248690c0c4745452c9bbd9a58
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-01 04:53:16 +0000

    prefligit: update 0.0.14 bottle.

--- Formula/p/prefligit.rb
+++ Formula/p/prefligit.rb
@@ -9,10 +9,10 @@ class Prefligit < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a14fee2ae7c41a42a2c012488501b00ac18bed523a5714d484c81df93f15809c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "21ad140af0df6c799db2563ad5f46e67f57d435a2410c5a230b709fff420047f"
-    sha256 cellar: :any_skip_relocation, ventura:       "c06e10080adc8798365038947a45a2a5082f69280fd2d2c51767c60a16c867a2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55595a2092c16c911d130c0f5bac1bb7d8586f7911c0c5f90ce4eefc025290ef"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c075d745129f863d8fac191e48c013cab33a6d0a42194e0e9993e655bba9a760"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a953cd1d94778da20f9b36e5a513986baff583f7951cbf7499f87a2c11a7c01"
+    sha256 cellar: :any_skip_relocation, ventura:       "56bc31a2e12587cb9197bb5fbd5d024b067436809d420f7edaf1bd62a01fbb52"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "21c56619acbd459f6d45f23384f5a5fb4e63399736c4c7cbb90bf1e4c5cb05cc"
   end
 
   depends_on "rust" => :build
