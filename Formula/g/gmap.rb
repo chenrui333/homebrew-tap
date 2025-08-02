3079fce0b7692b52c70ed8fa6cd43ec431d399cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-02 21:37:52 +0000

    gmap: add 0.3.3 bottle.

--- Formula/g/gmap.rb
+++ Formula/g/gmap.rb
@@ -6,6 +6,14 @@ class Gmap < Formula
   license "MIT"
   head "https://github.com/seeyebe/gmap.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a26539820b19c8a99a921dffe82a0ef8a9168cd76957d0bd76f0c5780e99abb2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14bb167ef9c86ed4a3e44276f76adfaeb7b590b6bba05032afd142ba31609c01"
+    sha256 cellar: :any_skip_relocation, ventura:       "8b6789d14df44871ad9f7a4b054f704f467f101e7ded4c7124d376e44d9558f0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d81e41b65956a5426e3b08230dd4d7d87e6ad8e42ffbaec5e0984922b58c50f"
+  end
+
   depends_on "rust" => :build
 
   def install
