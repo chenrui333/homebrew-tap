6c81f4bcf9b64a249d3a30dbda77f03d316bf154
Rui Chen <rui@chenrui.dev>
2025-08-02 17:17:20 -0400

    dory-cli 0.20.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    dory-cli: update build
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/d/dory-cli.rb
@@ -0,0 +1,25 @@
+class DoryCli < Formula
+  desc "Lightweight static site generator for technical documentation"
+  homepage "https://docucod.com/"
+  url "https://registry.npmjs.org/@clidey/dory/-/dory-0.20.0.tgz"
+  sha256 "78adc10d611ec60fc1b8a6645845d222d35150855401375becc249c4268fabd8"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    if OS.linux?
+      (libexec/"lib/node_modules/@clidey/dory/node_modules")
+        .glob("sass-embedded-linux-musl-*")
+        .each(&:rmtree)
+    end
+  end
+
+  test do
+    output = shell_output("#{bin}/dory build 2>&1", 1)
+    assert_match "Dory is ready to build your docs", output
+  end
+end
