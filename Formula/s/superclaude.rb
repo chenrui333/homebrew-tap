e73ba35a7f9dfbf327e8f9878e1555b13c53f196
Rui Chen <rui@chenrui.dev>
2025-08-03 12:58:54 -0400

    superclaude 1.0.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/s/superclaude.rb
@@ -0,0 +1,22 @@
+class Superclaude < Formula
+  desc "AI-powered development toolkit"
+  homepage "https://www.superclaude.sh/"
+  url "https://registry.npmjs.org/superclaude/-/superclaude-1.0.3.tgz"
+  sha256 "f292b6c18f01985382f51f40ad5c2fe0e91cf2f3121a7295b8f2294610b02312"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/superclaude --version")
+
+    output = shell_output("#{bin}/superclaude readme 2>&1", 1)
+    assert_match "System dependencies check...", output
+    assert_match "Claude Code not found", output
+  end
+end
