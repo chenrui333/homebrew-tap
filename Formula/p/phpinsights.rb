ac59ba67d87a03b54b91ec9041a0efbb0dd17680
Rui Chen <rui@chenrui.dev>
2025-08-03 07:26:05 -0400

    phpinsights 2.13.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/phpinsights.rb
@@ -0,0 +1,33 @@
+class Phpinsights < Formula
+  desc "Instant PHP quality checks from your console"
+  homepage "https://github.com/nunomaduro/phpinsights"
+  url "https://github.com/nunomaduro/phpinsights/archive/refs/tags/v2.13.1.tar.gz"
+  sha256 "27d5c2c84c6bb04b9c0dd0228073c40a79e206734a645a546dabcbc53a3fb8ab"
+  license "MIT"
+
+  depends_on "composer" => :build
+  depends_on "php"
+
+  def install
+    system "composer", "install", "--no-dev", "--prefer-dist"
+    libexec.install Dir["*"]
+
+    (bin/"phpinsights").write <<~EOS
+      #!/bin/bash
+      exec php "#{libexec}/bin/phpinsights" "$@"
+    EOS
+  end
+
+  test do
+    # assert_match version.to_s, shell_output("#{bin}/phpinsights --version")
+    system bin/"phpinsights", "--version"
+
+    (testpath/"test.php").write <<~PHP
+      <?php
+      echo "Hello, World!";
+    PHP
+
+    output = shell_output("#{bin}/phpinsights analyse --summary #{testpath}/test.php")
+    assert_match "[ARCHITECTURE] 100 pts within 1 files", output
+  end
+end
