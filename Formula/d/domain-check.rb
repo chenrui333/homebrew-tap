00cbdea4a09da1402d7f1ba3887e43a31d8927a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-03 15:07:50 +0000

    domain-check: add 0.6.0 bottle.

--- Formula/d/domain-check.rb
+++ Formula/d/domain-check.rb
@@ -6,6 +6,14 @@ class DomainCheck < Formula
   license "Apache-2.0"
   head "https://github.com/saidutt46/domain-check.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a53f9f03e6cf1e212fbdbfdce9a7d264952eaf3904702d81695be5deecc4935a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4ab218fd23460bd9d0bc5ea07a4bad94bf34372301187cbae8410a5758158b78"
+    sha256 cellar: :any_skip_relocation, ventura:       "455b05d4441724eaf98ab39ddeb51c816f6bf8f0cb8dec4b6b71e9806d5c995b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "30107dd9aaccb48023532708d594809bd8506a6f1288395f35ae2f837a93218a"
+  end
+
   depends_on "rust" => :build
 
   def install
