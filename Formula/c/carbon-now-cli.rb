1d7cd8f0b8b1323f0e15472076dba81d9ada0660
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-03 22:34:18 +0000

    carbon-now-cli: add 2.1.0 bottle.

--- Formula/c/carbon-now-cli.rb
+++ Formula/c/carbon-now-cli.rb
@@ -5,6 +5,14 @@ class CarbonNowCli < Formula
   sha256 "7ad51db24caf6b79d77d9ee4407bee6a5ed0c6677014ae5ba0fb955fb7770083"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ba895228750739acb0fb6be8f492b7a68b851929dad5824d22bb3e3a7207d6fc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6090b83ca2f3cd6a39f257e9b9a2103af9e28e52907a71920739ae10991a6811"
+    sha256 cellar: :any_skip_relocation, ventura:       "bc711cb928cbd6fc99a7c9699d507a71c13a68e90d68a4d54096a6e105c29606"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ff99900454c51e78daf4369548ad6546b7da9845dccec55c365ef2fc7f5cbfc"
+  end
+
   depends_on "node"
 
   on_linux do
