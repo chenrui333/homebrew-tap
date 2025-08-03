b9aa0b22bc69981d4b3417b360b3a94a16a3b2a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-03 22:24:34 +0000

    agl: add 0.0.1 bottle.

--- Formula/a/agl.rb
+++ Formula/a/agl.rb
@@ -7,6 +7,14 @@ class Agl < Formula
   license "MIT"
   head "https://github.com/alaingilbert/agl.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "171230527311d2d6d3cce50155976a56b5c8b6b959e8cffc9601151c8a3c1f3a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b82f4d04d60ea0f35b45a1e4a9d67990cee1b175ae52a48c19946c3f84de64e4"
+    sha256 cellar: :any_skip_relocation, ventura:       "e41a684a3032421e4a29d6753062bd8618e32aade8c3b69c32bf695338ef9e84"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc1b814b49c997b561ca5ff5dfbd9cf96bc4f25fc434991a82287de04f15c770"
+  end
+
   depends_on "go"
 
   def install
