825b05f68154fb01927e90f7c49e48c12ca79141
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-03 22:38:54 +0000

    decktape: add 3.15.0 bottle.

--- Formula/d/decktape.rb
+++ Formula/d/decktape.rb
@@ -5,6 +5,14 @@ class Decktape < Formula
   sha256 "9ef30a860f2b9a89a3f7143cf4961a8887302d8a53b44cc69958254e44b5eb33"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_sequoia: "f4d1d01055da09f051c28dbbfe7730146278e150a8756c090f0552bdd027b7de"
+    sha256 cellar: :any,                 arm64_sonoma:  "8e4a84624f60136a0df2eb4ffc343bdb8cf0c63d9506e25c0d11eb10e14e1bc0"
+    sha256 cellar: :any,                 ventura:       "1b983b544ff7521567edaf475982a556166ee50b605221a2c5e11f7f4df05949"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "74b8ab8c438064289bf0cc37827763b0ca3023a79b19306947f2d8f100a89cbb"
+  end
+
   depends_on "node"
 
   def install
