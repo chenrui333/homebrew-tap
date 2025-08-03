95dee7d2a00e6b4477a433c50ab559bddb30728f
Rui Chen <rui@chenrui.dev>
2025-08-03 10:43:42 -0400

    octelium 0.15.0 (new formula)
    Closes #1147.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/o/octelium.rb
@@ -0,0 +1,37 @@
+class Octelium < Formula
+  desc "Next-gen FOSS zero-trust platform—self-hosted VPN, ZTNA, API gateway & homelab"
+  homepage "https://octelium.com/docs/octelium/latest/overview/intro"
+  url "https://github.com/octelium/octelium/archive/refs/tags/v0.15.0.tar.gz"
+  sha256 "d93e56cb80d2550d3f4444a4360eb842f4cd0ff6c459f4c0a0dd2cfa84e58c95"
+  license "Apache-2.0"
+  head "https://github.com/octelium/octelium.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/octelium/octelium/pkg/utils/ldflags.GitCommit=#{tap.user}
+      -X github.com/octelium/octelium/pkg/utils/ldflags.GitTag=#{version}
+      -X github.com/octelium/octelium/pkg/utils/ldflags.SemVer=#{version}
+      -X github.com/octelium/octelium/pkg/utils/ldflags.GitBranch=main
+    ]
+
+    %w[octelium octeliumctl octops].each do |cli|
+      system "go", "build", *std_go_args(ldflags:, output: bin/cli), "./client/#{cli}"
+      generate_completions_from_executable(bin/cli, "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    end
+  end
+
+  test do
+    %w[octelium octeliumctl octops].each do |cli|
+      assert_match version.to_s, shell_output("#{bin}/#{cli} version")
+    end
+
+    output = shell_output("#{bin}/octelium status 2>&1", 1)
+    assert_match "Error: The Cluster domain is not set.", output
+
+    output = shell_output("#{bin}/octops init example.com --bootstrap #{testpath}/bootstrap.yaml 2>&1", 1)
+    assert_match "try setting KUBERNETES_MASTER environment variable", output
+  end
+end
