7e6b057e1ff0f703cfbec7c788ec1b616fcd3f2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-04 03:18:01 +0000

    seastar: add 0.1.0-alpha.1 bottle.

--- Formula/s/seastar.rb
+++ Formula/s/seastar.rb
@@ -6,6 +6,14 @@ class Seastar < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/nonscalar/Seastar.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_sequoia: "38e7441a8e803f03fc8f6777b288ec3340a584f3cec5967c45b0d0b3b9443c52"
+    sha256 cellar: :any,                 arm64_sonoma:  "ec7b693ba0c7d583797a28557719a123f6c24358581c2b6dce947c85ecf57745"
+    sha256 cellar: :any,                 ventura:       "f378e209055cd7ddc37770204a6ebf35d8f3bad4dff441d53cbac5fc732d3a17"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "45708f62c13c30114dc8f5db9a15a692a935f5251ba593604ff973a45e731d63"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
