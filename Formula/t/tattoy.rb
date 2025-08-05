2c6ba77cf276afce89fc31dd3b8cf9aa8a619041
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 20:55:06 +0000

    tattoy: add 0.1.8 bottle.

--- Formula/t/tattoy.rb
+++ Formula/t/tattoy.rb
@@ -5,6 +5,14 @@ class Tattoy < Formula
   sha256 "bc8a1fbab1870b64faea6494ab202c08c57b062be17fab11d488ecd312963c46"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f1546206318764a093a577261d18e8b6dc98db28aa1326333779d067c6f9997b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "345130e7efcf3e69a645b38bd7f767535413ba4bde95df4ee7a57dd636e688d1"
+    sha256 cellar: :any_skip_relocation, ventura:       "7b7179676032dd13cc7b7fd50cf225b8d255e9a3f78028056349a987f68ec36e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf2b5d9f1ee1cecb71b037c21d13f4ca56f94beb0d446f6b417abe6fce377551"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
