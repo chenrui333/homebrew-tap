b64d1495c6090c7bb24fcdd9e4cbe81392d43515
Rui Chen <rui@chenrui.dev>
2025-08-05 16:42:50 -0400

    notion-mcp-server 1.8.1 (new formula)
    Closes #1186.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/n/notion-mcp-server.rb
@@ -0,0 +1,23 @@
+class NotionMcpServer < Formula
+  desc "MCP Server for Notion"
+  homepage "https://github.com/makenotion/notion-mcp-server"
+  url "https://registry.npmjs.org/@notionhq/notion-mcp-server/-/notion-mcp-server-1.8.1.tgz"
+  sha256 "34cf7128d29d0d2dcbf412eeafacf164120a3232c630dd14c137ffb5b923b229"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    json = <<~JSON
+      {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2025-03-26"}}
+      {"jsonrpc":"2.0","id":2,"method":"tools/list"}
+    JSON
+
+    assert_match "Identifier for a Notion database", pipe_output(bin/"notion-mcp-server", json, 0)
+  end
+end
