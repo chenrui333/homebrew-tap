118408875bfffac61f95cb69bb670f82ee7a85c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 20:59:08 +0000

    vectorize-mcp-server: add 0.4.3 bottle.

--- Formula/v/vectorize-mcp-server.rb
+++ Formula/v/vectorize-mcp-server.rb
@@ -5,6 +5,14 @@ class VectorizeMcpServer < Formula
   sha256 "610ae57ca32cf79ee6a3cd1b47fb624b1e36529b8ba2ee68c389e25d2aac65b2"
   license "ISC"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78e0dfbb68dc805ccf04a550625e6575f944f3a7da18b5320d4da50c598f7f63"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "60e16d5a504b02f3a20bcdd180ff6f71e05d78e0d5be633727e76b80743f2a9f"
+    sha256 cellar: :any_skip_relocation, ventura:       "b9ce70edbcf2d5afa600f82f55d943de67063b7827e24255042f73881858a846"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0804686c79a2e1959651b03c57d19e4806351d36ea03ea3943c93f8de466ecad"
+  end
+
   depends_on "node"
 
   def install
