5494791d0a9a01354a3084b7fcce91c39879a655
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 23:53:08 +0000

    apify-actors-mcp-server: add 0.2.3 bottle.

--- Formula/a/apify-actors-mcp-server.rb
+++ Formula/a/apify-actors-mcp-server.rb
@@ -5,6 +5,14 @@ class ApifyActorsMcpServer < Formula
   sha256 "55ccedb709cde73ab1e94c1b9e4d862c6fad2e58d91edfa81f349e00aa0111b0"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "586e96c8beb91aee70e6d7b3c509ab18c3abd948dfca66c3af772cfd08beb8b8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "161875739460dfeac78d50378c11332f012e0fd7d22d0b398463342edb75ef21"
+    sha256 cellar: :any_skip_relocation, ventura:       "1616b9393c06c727d39c951229d4561617c343ae061ac530fad3fb080bf67200"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5ae01b1d727660a812518c50267a44e8f2d7b2e6f968861908b771e726f597ea"
+  end
+
   depends_on "node"
 
   def install
