c3f8f39ef58b3c1fe3759653aa6bf0025df0d7b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 17:06:36 +0000

    fli: add 0.1.0 bottle.

--- Formula/f/fli.rb
+++ Formula/f/fli.rb
@@ -5,6 +5,14 @@ class Fli < Formula
   sha256 "4b3c7c454fef762209b4fe879e205ecbc51126625172675ad1ea8f60dc47df82"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2002c05ee259620176bcd617f56ef1bb333d9c0d0b1acbc30e3f8ba4e06c7e56"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c13146b44f0be81632a2562106e2eeb45565b272a3fc77d514a3e7c598dce876"
+    sha256 cellar: :any_skip_relocation, ventura:       "d1537a0187a381e9344a9a6dc61628692c2d1bccc044e519364e327a39756314"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f0260b04cca06b68369c3af2df6037580f2f1caca580783fcc8d910642f5674e"
+  end
+
   depends_on "go" => :build
 
   def install
