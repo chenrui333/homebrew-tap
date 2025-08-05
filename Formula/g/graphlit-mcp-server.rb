3b101a35650bfd3c0cd5bdd98902a66e78dc2758
Rui Chen <rui@chenrui.dev>
2025-08-05 14:55:43 -0400

    graphlit-mcp-server 1.0.20250730002 (new formula)
    Closes #1185.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/g/graphlit-mcp-server.rb
@@ -0,0 +1,24 @@
+class GraphlitMcpServer < Formula
+  desc "Cloudflare MCP Server"
+  homepage "https://www.graphlit.com/"
+  url "https://registry.npmjs.org/graphlit-mcp-server/-/graphlit-mcp-server-1.0.20250730002.tgz"
+  sha256 "a6d760b231777411f72c94ba4e5973c45759511529061045b47a0ecff08b195e"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    json = <<~JSON
+      {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2025-03-26"}}
+      {"jsonrpc":"2.0","id":2,"method":"tools/list"}
+    JSON
+
+    output = pipe_output(bin/"graphlit-mcp-server", json, 0)
+    assert_match "Prompts an LLM conversation about your entire Graphlit knowledge base", output
+  end
+end
