68153f6a99a938c75a84463f93bad9694405b303
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 16:20:16 +0000

    tooka: add 1.0.5 bottle.

--- Formula/t/tooka.rb
+++ Formula/t/tooka.rb
@@ -6,6 +6,14 @@ class Tooka < Formula
   license "GPL-3.0-only"
   head "https://github.com/tooka-org/tooka.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "edd389fbd44f78af6c7bb9128f242cb6000e18612d197947a7849b5b2e8a7f50"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "99727d35605d3bb42cb089183847708db7bb09cb09b81d680c5134c5a0a1cad2"
+    sha256 cellar: :any_skip_relocation, ventura:       "fd522539b8281b016dede754e9168132c2bb4ce321feca886929867c7513b00e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "909f3173bebba9297386cb06b76b484bb79acee30f779f2eb051d59f2cb847a2"
+  end
+
   depends_on "rust" => :build
 
   def install
