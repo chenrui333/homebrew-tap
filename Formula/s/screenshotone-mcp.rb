8e760202d021d6c3151cdb26012ca32611f6f050
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 21:23:07 +0000

    screenshotone-mcp: add 1.0.0 bottle.

--- Formula/s/screenshotone-mcp.rb
+++ Formula/s/screenshotone-mcp.rb
@@ -5,6 +5,14 @@ class ScreenshotoneMcp < Formula
   sha256 "31aa26fc5161fd369723d8c4962bbfc282fb52aa8bc06f602bbe5dfd8026999f"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "72324b2bcac75f496570d2185538fe9c46762d9e026ed952bbf81b96f7ffbc29"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e93fc6527f5e00834d15b635da1b2e6c1442aab74b0249ebedd46138ff488efe"
+    sha256 cellar: :any_skip_relocation, ventura:       "8f729ceb7148d116fdd058d74f1c055486a896764cdb938f5863a4038266467e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f229bfaea1e863d20794aa6f09d0daa5c8a50fca522604afa10b2a5545638f7f"
+  end
+
   depends_on "node"
 
   def install
