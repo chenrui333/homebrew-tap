3351563f7a8aa75b194fe14c934e419ef3313edc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-05 20:51:17 +0000

    notion-mcp-server: add 1.8.1 bottle.

--- Formula/n/notion-mcp-server.rb
+++ Formula/n/notion-mcp-server.rb
@@ -5,6 +5,14 @@ class NotionMcpServer < Formula
   sha256 "34cf7128d29d0d2dcbf412eeafacf164120a3232c630dd14c137ffb5b923b229"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d900d9708df30211efb480a14cca8d4da284d15565f2d9254835c32c5cdb6bc2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "63bceb19c435774bdedc02d243f47ddbd202d337184b0863edcf8cffcd875c9a"
+    sha256 cellar: :any_skip_relocation, ventura:       "98ac3e7661ee7d284628f1d68c5467202de8ae38e17b7fc5ab80bba6e2bebaaa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a145afae10509c11f534d0186f1ad8f39738bb156336f157f0bc8a79e061cffd"
+  end
+
   depends_on "node"
 
   def install
