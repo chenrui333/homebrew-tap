88bcca074a294c4df8ba24294b183e4a5e45f263
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-06 20:36:14 +0000

    tdd-guard: update 0.9.0 bottle.

--- Formula/t/tdd-guard.rb
+++ Formula/t/tdd-guard.rb
@@ -7,10 +7,10 @@ class TddGuard < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9a5a45907399b161bc6401aa9d4810bd90db75a54ae49ba2a6dda719216ee271"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4b6c4a1da321f9c29b105838024496adb4f28b317c5f1e495a01b961712377c"
-    sha256 cellar: :any_skip_relocation, ventura:       "1138e138e987255444d15d80a3c3ed0ca0109b4f27fd1022ab82e2d1d1385dfb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "907f3a4c092f993457df1f8c3f6dfcdc94bd20254101c08944d1b2e0c4eafbcd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cd2e23c52df1e4582ab4d1d59a59c0301f439b57e82726c89db46faba8b555c4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "57f01541b64e63507b8499c5030ae747900e39b6b0286097db014ab432fb83c0"
+    sha256 cellar: :any_skip_relocation, ventura:       "cf66dae39d58313d079894d2badc211eec2860bc7c24ceab76b64b86d3fb210c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1e2b52adee3bd939b97a3a9a0b1c87237d029a3b7f8ff6ba339b050112b32b78"
   end
 
   depends_on "node"
