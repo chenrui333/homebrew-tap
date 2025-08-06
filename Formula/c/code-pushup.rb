14b33a8cb1b42112f6312297f768c68d4aa9f208
Rui Chen <rui@chenrui.dev>
2025-08-06 14:16:59 -0400

    code-pushup 0.72.1 (new formula)
    Closes #1216.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/c/code-pushup.rb
@@ -0,0 +1,67 @@
+class CodePushup < Formula
+  desc "CLI to run all kinds of code quality measurements"
+  homepage "https://code-pushup.dev/"
+  url "https://registry.npmjs.org/@code-pushup/cli/-/cli-0.72.1.tgz"
+  sha256 "f23172e8d1be621e63e2d648ae60d149289477356451a4db9db68745dd6f103e"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/code-pushup --version")
+
+    (testpath/"code-pushup.config.ts").write <<~TS
+      import { dirname } from 'node:path';
+      import { fileURLToPath } from 'node:url';
+
+      const config = {
+        plugins: [
+          {
+            slug: 'ts-migration',
+            title: 'TypeScript migration',
+            icon: 'typescript',
+            audits: [
+              {
+                slug: 'ts-files',
+                title: 'Source files converted from JavaScript to TypeScript',
+              },
+            ],
+            runner: async () => {
+              const jsPaths = paths.filter(path => path.endsWith('.js'));
+              const tsPaths = paths.filter(path => path.endsWith('.ts'));
+              const jsFileCount = jsPaths.length;
+              const tsFileCount = tsPaths.length;
+              const ratio = tsFileCount / (jsFileCount + tsFileCount);
+              const percentage = Math.round(ratio * 100);
+              return [
+                {
+                  slug: 'ts-files',
+                  value: percentage,
+                  score: ratio,
+                  displayValue: `${percentage}% converted`,
+                  details: {
+                    issues: jsPaths.map(file => ({
+                      message: 'Use .ts file extension instead of .js',
+                      severity: 'warning',
+                      source: { file },
+                    })),
+                  },
+                },
+              ];
+            },
+          },
+        ],
+      };
+
+      export default config;
+    TS
+
+    output = shell_output("#{bin}/code-pushup print-config --config code-pushup.config.ts 2>&1")
+    assert_equal "TypeScript migration", JSON.parse(output)["plugins"][0]["title"]
+  end
+end
