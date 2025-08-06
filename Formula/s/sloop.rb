c5f826c2923085621deb05da47e29b507d81b385
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-06 14:09:22 +0000

    sloop: add 1.2 bottle.

--- Formula/s/sloop.rb
+++ Formula/s/sloop.rb
@@ -5,6 +5,14 @@ class Sloop < Formula
   sha256 "ff5b6e91ab56ab534dfa967fa560fd9c2c4a95e63c38c19d1574c4c37645dbd5"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a47ac3282c25f0fa68733b6370ab45a0cbcbd91e10fb782d3593e8eacde79b49"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1d607d931e6882a6e306e225c96b680a3981ccd3f1602bb1e91de7bba1c3c478"
+    sha256 cellar: :any_skip_relocation, ventura:       "9780c8d1695275245841dc2a13062f8dff8957b0304746213b4a9d4fe7e249e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "61e0d7553a2d6d9b29fb8500d9454c011019ae0e30dca9aa7f46ae7e2675769d"
+  end
+
   depends_on "go" => :build
 
   def install
