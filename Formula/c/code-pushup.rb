48af7f249d518626f76f225caaf052df9293709b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-06 18:26:48 +0000

    code-pushup: add 0.72.1 bottle.

--- Formula/c/code-pushup.rb
+++ Formula/c/code-pushup.rb
@@ -5,6 +5,14 @@ class CodePushup < Formula
   sha256 "f23172e8d1be621e63e2d648ae60d149289477356451a4db9db68745dd6f103e"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b15c77ced008f3fe698012ad95cfca5d4dbbe00b2bbcc130bc8e0630014dedb6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "01597eef7bfd458a085fb987e99eeb06977bed6f7c560c3708cec96e4ebb83db"
+    sha256 cellar: :any_skip_relocation, ventura:       "e0746a83254170610047addc42755121fa3db981cdaa94bb581d6f0385284b29"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3e4582cf7a409c041b946168056c5ecc61bb1d33e3e6fc77e1956c4fab35ad3"
+  end
+
   depends_on "node"
 
   def install
