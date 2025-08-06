c7e42224f5790318e4d6298fc4390bec06ff0067
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-06 13:08:54 +0000

    htvend: add 0.0.1 bottle.

--- Formula/h/htvend.rb
+++ Formula/h/htvend.rb
@@ -6,6 +6,14 @@ class Htvend < Formula
   sha256 "79890c69f1c16e2b16c4f12633dfc652f6e3c8f5a0480be941903d5a5ac5d0ff"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4d21feff68dd2bd803f7b6acd3fecbf713bf99a4aff20280c1a4407cc94c3a57"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ac11524bd74e66c4c3ac2742795e76b37e8ea58d3baa3d23131234a3b9d90d9a"
+    sha256 cellar: :any_skip_relocation, ventura:       "a91edc93b5a2eebf2c8ecbef8fd06c74638013b0baf6d541cc07533f7c5d138d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "66861c9e3b5a92ce82e585203fcd1aedc01ef67749daac5571d0ec2db588af81"
+  end
+
   depends_on "go" => :build
 
   def install
