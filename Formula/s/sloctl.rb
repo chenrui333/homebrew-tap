e169ac8824e97537acbfb377601f9807b1f576d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-07 10:35:15 +0000

    sloctl: update 0.15.0 bottle.

--- Formula/s/sloctl.rb
+++ Formula/s/sloctl.rb
@@ -8,10 +8,10 @@ class Sloctl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "376e043c11b323f8dcc9480cc14ce1722c4bf8f75f650cc141f631d8404f4bb2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ea52f7b87575bff5bd8be17eb7a08d1665c630e1963a780eeed64292f3535a8"
-    sha256 cellar: :any_skip_relocation, ventura:       "39ec30ba16e66290211b98a9cbdd6056e04c34297b1f08ce64d9c1b636c1fa92"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d953d86a956f65f7d6d29bca1b878c70b7452467091b2e8ee27867c552c08352"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "35ca65045226d77fb509b047c809354065eea774af132b9044560d852b54b0a1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1cb2a32bde658ca3a6999f3c5c28d841d30784976175da9167429642f83b3e8e"
+    sha256 cellar: :any_skip_relocation, ventura:       "51180c7d6c45089677ddb195bdbe5530f13c51f54a4f7f2c7a63395a8019ec85"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "13079678a7078d5a3e3a651ed43a0a044661f556a8c112f3245456d5a73824b5"
   end
 
   depends_on "go" => :build
