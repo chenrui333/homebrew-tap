5481ac56287b4d3cb02dc125625cd0d3aeb0ae49
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-15 19:19:27 +0000

    code-pushup: update 0.74.0 bottle.

--- Formula/c/code-pushup.rb
+++ Formula/c/code-pushup.rb
@@ -7,10 +7,10 @@ class CodePushup < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ded6bd6328f8ea793ad5779cc317c251b691d44a57b14fba644ad407bdefc8b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ec739cf50276c05f6cf206cae227b9ba398b279c5de095cbb2028aac29e9ac85"
-    sha256 cellar: :any_skip_relocation, ventura:       "466d88c656325fbef6602c45ebabe5a7579c1a6568199e102733cd532149f083"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76af5e427a42ce8308fe691cf234c7c5aeef0c974fdbfbe68519b87faef93ede"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ce54e3e740c1d462e2cc974924618194fc59122717691edb5e612225c9265b4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cd9fa3e85d2c75612a4c01860658c7aa2ac2d063e60cddf3e7431756c0469550"
+    sha256 cellar: :any_skip_relocation, ventura:       "10f76034a3cf6c858a3b1220db80d003ae4c4d534f5a31f55d8d639fe527eb10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "850ca7a7bb2627c4334648cf0c297f7f4849cb3ec6eb5bcaa934ad3f8c3e0ca5"
   end
 
   depends_on "node"
