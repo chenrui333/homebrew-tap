70c1585af378d527901a449ef26fe6d1c17f53b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-17 23:11:01 +0000

    rkik: add 0.6.0 bottle.

--- Formula/r/rkik.rb
+++ Formula/r/rkik.rb
@@ -6,6 +6,14 @@ class Rkik < Formula
   license "MIT"
   head "https://github.com/aguacero7/rkik.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d1019bae5fe9d05ca5b180f560f666e794d0c2eb6323cc72d599a0ce34e66188"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14a4abff7aa6c0603c4bed5234312411a0e883f5d97f7c41979d2ec92a49044e"
+    sha256 cellar: :any_skip_relocation, ventura:       "32822f5c91354e884e9eb1770b5b753c84bd55da38ba829312a58ae5be977266"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1c541449db2df61fdebea81ebf9215c91e03ce727fd6e414b5e0b1b41a814375"
+  end
+
   depends_on "rust" => :build
 
   def install
