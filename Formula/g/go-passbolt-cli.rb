da517095e151518130dcf8a1f5fe72c0e76c26b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-18 00:23:20 +0000

    go-passbolt-cli: add 0.3.2 bottle.

--- Formula/g/go-passbolt-cli.rb
+++ Formula/g/go-passbolt-cli.rb
@@ -5,6 +5,14 @@ class GoPassboltCli < Formula
   sha256 "4e88aa088b68a7101ca89a97184eb5427aa9b70d52df077f7d56c2ff3672fe06"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e40e152a12d2858735e5cb845532f0c9cb29d33bacbf3669370ecdc8de83b621"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a55151e1580c0fe4c1b3441102acedda019e6b4e777addacf52854fb4989c33"
+    sha256 cellar: :any_skip_relocation, ventura:       "2fa976a8733ad6380434f6c67e7b3185a967a65d9669a0a072db7fd3bfe872f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eca7ed6c35d1be4fb5c72ec9b38776306a806d1230b723b595bc7297e8150dd3"
+  end
+
   depends_on "go" => :build
 
   def install
