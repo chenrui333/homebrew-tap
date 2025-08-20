116c68baf2d9ca351a529d3590165a4e3d400dd4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-20 15:46:04 +0000

    hyperbolic: add 0.0.3 bottle.

--- Formula/h/hyperbolic.rb
+++ Formula/h/hyperbolic.rb
@@ -6,6 +6,14 @@ class Hyperbolic < Formula
   license "MIT"
   head "https://github.com/HyperbolicLabs/hyperbolic-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07d4c1ba271d80449a25d75c2295ae6904a3ec05f814406478d488a07307015a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "256d55ef6c1eb1ca9b24e4cfe5a121e2ec3c672c755ef0fcd2687b201f8e59d4"
+    sha256 cellar: :any_skip_relocation, ventura:       "347bbb11a0c9e3a8d3d79274bf14b2e320fe471eba537f16e3e00b2a8e94b4c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48852823a72aa3af4376d49eb00e04c12a3518226f2110fa6cdf831a581ca807"
+  end
+
   depends_on "go" => :build
 
   def install
