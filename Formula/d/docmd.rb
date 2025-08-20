afc5c129456299d5d207c88b48115bf732071268
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-20 16:50:53 +0000

    docmd: update 0.2.2 bottle.

--- Formula/d/docmd.rb
+++ Formula/d/docmd.rb
@@ -7,10 +7,10 @@ class Docmd < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "18193e7669d7225f15dd3ace5541e8904b7362f0064ab3b4834608e9a8c91409"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f94a5d5745e91da8fdce7b719b0d452e3dd2a058d5eae3b702244d14fab42dac"
-    sha256 cellar: :any_skip_relocation, ventura:       "c42db72bfe0d4ddf00bd9c5b0f92a89fe12a97f671c85120cf67c91a0a427d43"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d2a2f7f192c9f892e0beae43f7fa6be65815194b58517b7735f5ff3d08ed646"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c4d767f441c8a3a3f42754894be0dbee4714b33291524c49f2224704ea4b5972"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "41b3c6185b34d39b31ae45e9d64165fd35a0e4f58ed23cb6c388f0808b2dc1ca"
+    sha256 cellar: :any_skip_relocation, ventura:       "6c18b3483c857da3fb9f0dca84b9a8a95c3449b8c55e4b2255f2389b2269cdfc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e2e67ceb81fb1360cb3e1efe7cc95294ae38501546ea5a17abec993204f5800"
   end
 
   depends_on "node"
