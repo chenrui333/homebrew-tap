f493a391e096951501b51c1bb2c08fe8716e340c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-20 00:49:49 +0000

    dg: add 3.1.5 bottle.

--- Formula/d/dg.rb
+++ Formula/d/dg.rb
@@ -5,6 +5,14 @@ class Dg < Formula
   sha256 "1b5eb887fbbff8488d3f19f0b8a5954265a6ad20515d064eda5c18ee9701d66f"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_sequoia: "b11d8fca9ed79ec30882e4f816e42525d4652f8741aef03e7e1b09bae0cf8a57"
+    sha256                               arm64_sonoma:  "f295b06e219a2d41888a87f58f0d98e48e2f7de16eeaa3c878c431ea8a94d279"
+    sha256                               ventura:       "86382fb5157f546d06a397607055cb744939930ffc686649f57cff608d6c3830"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc13bcca752fc16f33005b095e011acc146df74f8a239dc9c3513176b86e1785"
+  end
+
   depends_on "node"
 
   def install
