2ccd7af029b3675c05e0c80b733c3bedd024f707
Rui Chen <rui@chenrui.dev>
2025-08-20 12:16:08 -0400

    tantivy-cli: remove build patch
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tantivy-cli.rb
+++ Formula/t/tantivy-cli.rb
@@ -15,12 +15,6 @@ class TantivyCli < Formula
 
   depends_on "rust" => :build
 
-  # bump traitobject to 0.1.1 to build against rust 1.87, upstream pr ref, https://github.com/quickwit-oss/tantivy-cli/pull/112
-  patch do
-    url "https://github.com/quickwit-oss/tantivy-cli/commit/e0d4877947134ab8a9429a2a08375e0a14c5bfcf.patch?full_index=1"
-    sha256 "abe9bb230580591e61e627f650914134bd2b9b2abaf4aa8775525044f7d9b2e4"
-  end
-
   def install
     system "cargo", "install", *std_cargo_args
   end
