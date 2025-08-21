32491257ac0ae7239d9888038cb4397b3ad152d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 00:47:45 +0000

    paq: add 1.1.1 bottle.

--- Formula/p/paq.rb
+++ Formula/p/paq.rb
@@ -6,6 +6,14 @@ class Paq < Formula
   license "MIT"
   head "https://github.com/gregl83/paq.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a3206073c69d3d2ebdddb98b878c46a9ab76b21414f26bb830b033e2c640ccb6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "433ce75a596b7f2571f352d219660a90bb97ec2144ac698ed45b27ee75abbcbb"
+    sha256 cellar: :any_skip_relocation, ventura:       "682512529ad2db6b6febdf0f16214d5b2f1ba6301b9d6f05700dd494dd8d823b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "afbfb6a0c05902263f64a54d8cce1d5b320ead37da7e51f0a352bb1be7d7acc6"
+  end
+
   depends_on "rust" => :build
 
   def install
