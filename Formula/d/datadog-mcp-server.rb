44c03cbe69321f82331c21042dd3bb605c42388b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 15:38:48 +0000

    datadog-mcp-server: add 1.6.0 bottle.

--- Formula/d/datadog-mcp-server.rb
+++ Formula/d/datadog-mcp-server.rb
@@ -5,6 +5,14 @@ class DatadogMcpServer < Formula
   sha256 "e376bea68a2dc26ccf675a39f08f95c9c9fb0b4a44764c76e9762c53c9bbfcb9"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f44140b73a5c4f046ae4a8d89028c96997cabc3f8195c7ad919b7488ffb7f5b7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4f3ed19548dc9e51119fb47681913f9050cd2bd0f1ab1a4b7d9d7f9fca8c36e"
+    sha256 cellar: :any_skip_relocation, ventura:       "851624de7eabbcaf60a9161e9bd2d4c5326902647a741f165340620f942c73a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a1b735edd54ef54720e7835aa49b9b464f517d3a09b05613c0829c16278b676"
+  end
+
   depends_on "node"
 
   def install
