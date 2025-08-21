49de23020ad1d50b685f0d1e0cf153ac038481db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 17:59:46 +0000

    mongodb-mcp-server: add 0.2.0 bottle.

--- Formula/m/mongodb-mcp-server.rb
+++ Formula/m/mongodb-mcp-server.rb
@@ -5,6 +5,14 @@ class MongodbMcpServer < Formula
   sha256 "6821ad58c5c9f07f878a0fb8bfdbe4e34c8a867386981973e1c67a44a2924a28"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_sequoia: "6160c102944769ac0d0cf54c2625206d61172242c2583fb4d48145b36b2bb969"
+    sha256                               arm64_sonoma:  "3562a99b6e6a30b156ab695d2b6866843af1ffc7abc3cf3d939bb5d7defd5cf2"
+    sha256                               ventura:       "d783b28f414cb012543209632ef6f085158d4b71626edbdce37e6f94cc551d53"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5b735316d4b0a721fbb1cfaa57ecc41610bb314522819b3c094e20f706a961d"
+  end
+
   depends_on "node"
 
   def install
