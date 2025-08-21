07dd53bddd1c434fc5621574dbd0f8609c8deb24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-21 21:31:09 +0000

    twilio-mcp-server: add 0.7.0 bottle.

--- Formula/t/twilio-mcp-server.rb
+++ Formula/t/twilio-mcp-server.rb
@@ -5,6 +5,14 @@ class TwilioMcpServer < Formula
   sha256 "7ff791d1023cad6372496d8aba1c49bc3ffbcf0989b9f96b3a83d2df9d6af755"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "93cf6ff5f954d129a441da08bb3bb879eec7cc48bfdf96248602c0dea42b1c70"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "47426b4c8447bf2f416536043c2aa0064cd4ebce93d0944b1f81b92feba0082b"
+    sha256 cellar: :any_skip_relocation, ventura:       "fa7499032bc70b7c64165f0039c62528cfd7e4e6c4a838bbd6624a699c1309bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d748a97429a450d0d6ed39e3f434790327e57ee4694d7487cebc81c272685ae"
+  end
+
   depends_on "node"
 
   def install
