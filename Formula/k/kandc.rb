458e227b302e4126cd41aaa8835774b820b45ae8
Rui Chen <rui@chenrui.dev>
2025-08-22 15:10:03 -0400

    kandoc 0.0.28 (rename from chisel-cli)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/k/kandc.rb
@@ -0,0 +1,45 @@
+class Kandc < Formula
+  include Language::Python::Virtualenv
+
+  desc "Accelerate your Python functions with cloud GPUs"
+  homepage "https://github.com/Herdora/chisel"
+  url "https://files.pythonhosted.org/packages/ee/3c/cc1a24bfef5f1d36e3fb1deefc3c91ca67802bfe0ae0dec531061e65698e/kandc-0.0.28.tar.gz"
+  sha256 "e8b7246ab26ff5ccda0e6a0de054d3cad4088970bc4415635ef850dc4af80228"
+  license "MIT"
+
+  depends_on "python@3.13"
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/dc/67/960ebe6bf230a96cda2e0abcf73af550ec4f090005363542f0765df162e0/certifi-2025.8.3.tar.gz"
+    sha256 "e564105f78ded564e3ae7c923924435e1daa7463faeab5bb932bc53ffae63407"
+  end
+
+  resource "charset-normalizer" do
+    url "https://files.pythonhosted.org/packages/83/2d/5fd176ceb9b2fc619e63405525573493ca23441330fcdaee6bef9460e924/charset_normalizer-3.4.3.tar.gz"
+    sha256 "6fce4b8500244f6fcb71465d4a4930d132ba9ab8e71a7859e6a5d59851068d14"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/f1/70/7703c29685631f5a7590aa73f1f1d3fa9a380e654b86af429e0934a32f7d/idna-3.10.tar.gz"
+    sha256 "12f65c9b470abda6dc35cf8e63cc574b1c52b11df2c86030af0ac09b01b13ea9"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4/requests-2.32.5.tar.gz"
+    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/15/22/9ee70a2574a4f4599c47dd506532914ce044817c7752a79b6a51286319bc/urllib3-2.5.0.tar.gz"
+    sha256 "3fc47733c7e419d4bc3f6b3dc2b4f890bb743906a30d56ba4a5bfa4bbff92760"
+  end
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    output = shell_output("#{bin}/kandc config")
+    assert_match "Keys & Caches Configuration:", output
+  end
+end
