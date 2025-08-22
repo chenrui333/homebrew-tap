689a5af3e129a7cb83ab04444efbbc2bef70c27f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-22 20:02:17 +0000

    doxx: add 0.1.1 bottle.

--- Formula/d/doxx.rb
+++ Formula/d/doxx.rb
@@ -6,6 +6,14 @@ class Doxx < Formula
   license "MIT"
   head "https://github.com/bgreenwell/doxx.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c82ed0e4f49a339e07fc79fa60002623c2f4a3150d539bd3bc059aaf6700e4b7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "66edfa3de70b54cf136f9794a02679e076103bf0b320ffee012d28dccc17ff44"
+    sha256 cellar: :any_skip_relocation, ventura:       "0009f5070467bd8e1b1dfb79050da99ee533061f77fb10621907ac0d22ca03bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bcf385395455232512ee22095cccaf8d729c3bf80560834bb6c193fc8a341ae1"
+  end
+
   depends_on "rust" => :build
 
   def install
