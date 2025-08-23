cfa79839fbc4fe936e1c4a7ede238060b406bc3c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-23 21:33:18 +0000

    typtea: add 0.1.6 bottle.

--- Formula/t/typtea.rb
+++ Formula/t/typtea.rb
@@ -6,6 +6,14 @@ class Typtea < Formula
   license "MIT"
   head "https://github.com/ashish0kumar/typtea.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "680f0bc7b600ee6c3d8c36eb77b450101d9cb72064c1c4f45d65177c34e428ee"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1e06efd66216b8a4224aff7754e843b4a2503b67e9d3a6c96ff50b9f27e722be"
+    sha256 cellar: :any_skip_relocation, ventura:       "35acbb6cec00e001ea128c7f8105a9f06816f9cb83d64ffa00640038e94675c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "24bafa9f239461bf93cb00db472fd8a8684478143d13067c137c144d678a5ba0"
+  end
+
   depends_on "go" => :build
 
   def install
