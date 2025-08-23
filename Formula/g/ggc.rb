50441971224f4b0da1037533d1f9cda28794c051
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-23 20:30:51 +0000

    ggc: update 3.2.1 bottle.

--- Formula/g/ggc.rb
+++ Formula/g/ggc.rb
@@ -8,10 +8,10 @@ class Ggc < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "44928a50854f52c9f4c54346c6849793b8c6b643b01a6449e62858c319aef979"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aab89d548fc9889179b61ae383f88198bdafe96882bbc4383d7d4b7e76d2eb90"
-    sha256 cellar: :any_skip_relocation, ventura:       "f8d23e97f70c2b1f2e0cb579216d3acab6a50d8b6babf5dd261e7ecb6753d3ef"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acb7d44eb25002e631e937a11416f37ebcf462942d40cc49ec9f2274dc7340c3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6f6156f185d75df9e12ed6a6d04d98595f9e6fc968f9de10d56a5f0294d8403a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4d02272cbb983a7d7b218c89cae3bbcd2af86c38acdfb3b2882df5b67715f977"
+    sha256 cellar: :any_skip_relocation, ventura:       "f3bb9144c0ecd0b7c09ccc66e735bff93b895a5bc950b373d71a1a93a417b321"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c26da81e781f93f0ecff0cb0204d424320fea1658ecbda734c3a78cdcefc5ab"
   end
 
   depends_on "go" => :build
