f52e5dfff148dca2b35c221c933cff184b9031d4
Rui Chen <rui@chenrui.dev>
2025-08-23 15:40:00 -0400

    ggc 3.2.0 (new formula)
    Closes #1323.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/g/ggc.rb
@@ -0,0 +1,20 @@
+class Ggc < Formula
+  desc "Modern Git CLI"
+  homepage "https://github.com/bmf-san/ggc"
+  url "https://github.com/bmf-san/ggc/archive/refs/tags/3.2.0.tar.gz"
+  sha256 "72609300de8dec2f0cfdd9f94591440975591f4f939e9b2b788c3117128fbcfd"
+  license "MIT"
+  head "https://github.com/bmf-san/ggc.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user}"
+    system "go", "build", *std_go_args(ldflags:)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ggc version")
+    assert_equal "main", shell_output("#{bin}/ggc config get default.branch").chomp
+  end
+end
