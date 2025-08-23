e08b2f5c87f5e477770930061c0a2d5f6e9556a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-23 04:56:18 +0000

    cloudflare-ddns: add 1.15.1 bottle.

--- Formula/c/cloudflare-ddns.rb
+++ Formula/c/cloudflare-ddns.rb
@@ -6,6 +6,14 @@ class CloudflareDdns < Formula
   license "Apache-2.0" => { with: "LLVM-exception" }
   head "https://github.com/favonia/cloudflare-ddns.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e8438ef7b6e83af7446aa80b466c6a71660797033c35647adb549c4540bc7187"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6023b280ac9d28aa7715132213c48eef55b67bf081828fb0488d0b318079a995"
+    sha256 cellar: :any_skip_relocation, ventura:       "f4a843c0b11fab977a27f435ff3c5686860b35b63aaf40afb4216f7d2c0b5743"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "74b9947ea6c56dab19f14324354770a7345102bb121a9d15084fc69d000f411d"
+  end
+
   depends_on "go" => :build
 
   def install
