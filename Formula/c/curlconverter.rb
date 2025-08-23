3df50b7d08d805ff3cb91d25ec27f9bfded45494
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-23 23:12:41 +0000

    curlconverter: add 4.12.0 bottle.

--- Formula/c/curlconverter.rb
+++ Formula/c/curlconverter.rb
@@ -5,6 +5,14 @@ class Curlconverter < Formula
   sha256 "16b6edc240fc096f09d4bcedf1358c74fb2ea1fd94f18820ef429af98acb49d3"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b5df8c0ae872dc300a5701f98e9e7fb357967bab58c0cf423b5b05e20c8a7507"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6c3c09d194bfb7d7524a6416fd9b1b504333aecfab5a2ffe51bc660fdbb33b5e"
+    sha256 cellar: :any_skip_relocation, ventura:       "dd8ed3609fd323f2cf51599dbdd052ba718eb2ece2c77a80e1dd9df66f36f8f4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "935bfb5f05622835c2fd6917482c3bd4abf663d2523020ca4eb7b2f7a1fdccc1"
+  end
+
   depends_on "node"
 
   def install
