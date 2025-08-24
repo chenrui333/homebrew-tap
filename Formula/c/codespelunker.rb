df3a20a87545dd1a2fa1f4c0e227875bae3c83c0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-24 04:36:38 +0000

    codespelunker: add 1.4.0 bottle.

--- Formula/c/codespelunker.rb
+++ Formula/c/codespelunker.rb
@@ -6,6 +6,14 @@ class Codespelunker < Formula
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/boyter/cs.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff00ed4736af32a933cb38970d76d9bac55385d1bfdd3438fbec703762661a7e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "94041d324afd1ed452c6f30c0e7397824994cfad5d5c66bd1b59255a83df9ce6"
+    sha256 cellar: :any_skip_relocation, ventura:       "3984fa6c86d0bde89b43685f7cec771f40c24da6930a7981d8757990109e31cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "60f8ec92b22c83f8580243ad51c5ce783f21bd2888a2160d1968233544e1135e"
+  end
+
   depends_on "go" => :build
 
   def install
