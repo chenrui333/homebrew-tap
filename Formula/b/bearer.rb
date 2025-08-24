b51f209158e43c13301a94dc3407b090e71e98a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-24 21:38:03 +0000

    bearer: add 1.50.2 bottle.

--- Formula/b/bearer.rb
+++ Formula/b/bearer.rb
@@ -5,6 +5,14 @@ class Bearer < Formula
   sha256 "e5b8d0bc52644cb16ea6ee2e628b3064fbb0acc4e52f502e95137bacdde60a67"
   license "Elastic-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fd5c93e5f017e0f27671cce10da84fb3e98f1f26a75839a8565d30b101605694"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d440d7fcdb558f58e9823f41d25454ebacd4837955812a2fd874e0157f386b53"
+    sha256 cellar: :any_skip_relocation, ventura:       "7833eb0d01bd06d5d35087ff154446cb090881bbe4eb64488997e72ad10b044c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e92b5b9488e884343f25319c676ecb6b5e1b21d622b9e059c9e0494d70f57f66"
+  end
+
   depends_on "go" => :build
 
   def install
