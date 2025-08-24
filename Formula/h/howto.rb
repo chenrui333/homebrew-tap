7d7a4220e380a4539d621c6bb309966499d314c7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-24 22:05:40 +0000

    howto: add 0.2.1 bottle.

--- Formula/h/howto.rb
+++ Formula/h/howto.rb
@@ -6,6 +6,14 @@ class Howto < Formula
   license "MIT"
   head "https://github.com/nalgeon/howto.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fdc89c905937ea1c38457236b7757e00de07887a1924aa52891508f5ea36ef2a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7f03d6629b6b44b2913897f0371df075da0228c87d72e1f2c7b7563872696e7c"
+    sha256 cellar: :any_skip_relocation, ventura:       "87b285f9134998f24430dfdaf584b50fde1f13ddc1fa055cfcf36b77f4df3a7f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "39e5e8c67011145741dae6a8a899bee7be77d61313f0b67ff7b8b4201837d317"
+  end
+
   depends_on "go" => :build
 
   def install
