fd15ceb6705d34f55c34a89433862ba108f6b7f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 14:30:03 +0000

    weekly-git-summary 1.1.0
    Closes #1352.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/w/weekly-git-summary.rb
+++ Formula/w/weekly-git-summary.rb
@@ -1,8 +1,8 @@
 class WeeklyGitSummary < Formula
   desc "跨平台 CLI 工具，用于生成 Git 提交周报，支持文本、JSON、Markdown"
   homepage "https://github.com/yinzhenyu-su/weekly-git-summary"
-  url "https://registry.npmjs.org/weekly-git-summary/-/weekly-git-summary-1.0.5.tgz"
-  sha256 "48d0a75c559f8bd425e18bd30304e5fa8621d393f2efedb892039b299552cd0e"
+  url "https://registry.npmjs.org/weekly-git-summary/-/weekly-git-summary-1.1.0.tgz"
+  sha256 "96397088089c164823530facece2af86c35c5b29c68ce92ea836a53e6c5f006a"
   license "MIT"
 
   bottle do
