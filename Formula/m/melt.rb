d5ce02f733199fa1ded73d0ac983169fe1617e0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 00:39:00 +0000

    melt: add 0.6.2 bottle.

--- Formula/m/melt.rb
+++ Formula/m/melt.rb
@@ -6,6 +6,14 @@ class Melt < Formula
   license "MIT"
   head "https://github.com/charmbracelet/melt.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8f772707832f579bbd69f13d7a9a42c59df4c3ac0ea3f5598189ead8a369b00"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f70fc9b30392758dc8ac1ad33e9a00d168d75047dbf553c973b0641375c28e9b"
+    sha256 cellar: :any_skip_relocation, ventura:       "ebf430c11e2ce70c4b74d6e5bc2f914ed6f2fca36dcc47130f6afb7c646f47e6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d08cf2fc29d082ccff9ae4c830c4a0deda41ae3f12dd40d3d28e0f8ad142c39"
+  end
+
   depends_on "go" => :build
 
   def install
