97bf99c7a019106deba62987ad3ec6f89eac5c30
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 14:45:08 +0000

    mcpd: add 0.0.7 bottle.

--- Formula/m/mcpd.rb
+++ Formula/m/mcpd.rb
@@ -6,6 +6,14 @@ class Mcpd < Formula
   license "MIT"
   head "https://github.com/mozilla-ai/mcpd.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "029b4b7383d007609d829bacd0938474bdbd1155d7e609abcb615f44e21eb1eb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b614bc267d070f94c4583eba9989d9e4183543c3555015912026951b81e65b72"
+    sha256 cellar: :any_skip_relocation, ventura:       "df70511c3c673c56622f52c88a5b3b68ed6311883ef98ddb8a1aa93b2e1bb57d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4cb3e19201fc0eeb8afd3d9723599a3706bf3c0fdd5535554a4fb2299870bb74"
+  end
+
   depends_on "go" => :build
 
   def install
