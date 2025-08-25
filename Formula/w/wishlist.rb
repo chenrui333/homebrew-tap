f8536d6fa18df0a437d68634124a00719f43d413
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 00:29:34 +0000

    wishlist: add 0.15.2 bottle.

--- Formula/w/wishlist.rb
+++ Formula/w/wishlist.rb
@@ -6,6 +6,14 @@ class Wishlist < Formula
   license "MIT"
   head "https://github.com/charmbracelet/wishlist.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "19f2e828cfc443fc1eaa25198dfd5d8f5c10e0a447acc2d94183596dc44f3e89"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7fdef2477c6bd357cfc7c3cd39725406c2dae12ac5a3af1844b7290e8c062276"
+    sha256 cellar: :any_skip_relocation, ventura:       "ec951814616abad4f50eea9f1bfa83018cdb34c2949a484bb65db2a53143a869"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e97aa4142df88ea3901995c1e9d63ce7d1ade1155fe86e4b14cebb6ab591172"
+  end
+
   depends_on "go" => :build
 
   def install
