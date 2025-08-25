418b9d2d0e331bcfc059c003c6abdef7052c1ae5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 01:48:56 +0000

    tparse: add 0.18.0 bottle.

--- Formula/t/tparse.rb
+++ Formula/t/tparse.rb
@@ -6,6 +6,14 @@ class Tparse < Formula
   license "MIT"
   head "https://github.com/mfridman/tparse.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "743bc92e051a1c986aec44c6e54e3fb39c1528bba10cce742fd1ec1689756880"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bedb76dce08a7b250a24b82f38882dbe3d7364205c16576637c44a03b8ec6d5c"
+    sha256 cellar: :any_skip_relocation, ventura:       "f637c6e042945a6d29ae567bf71dd83f772d56fa6439749ecf66a288b45efa0e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f77be2f1e1d99601c95ac5dc9705688b9ea1d534538d2a853c887de4dcc34264"
+  end
+
   depends_on "go" => [:build, :test]
 
   def install
