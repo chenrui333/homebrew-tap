a3c851d1825f6af9c5465d9e861d40d8c569a658
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-25 18:10:41 +0000

    terrafetch: add 0.4.1 bottle.

--- Formula/t/terrafetch.rb
+++ Formula/t/terrafetch.rb
@@ -6,6 +6,14 @@ class Terrafetch < Formula
   license "Apache-2.0"
   head "https://github.com/RoseSecurity/terrafetch.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a180b7f2d6c8d8fb05dcc20caaa6321b8bdb72a8160900d3630d8a2dff092595"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d74c84434e47540f15894d7938c3f9fe47ce1a14d4dd63c342f891f775d99c92"
+    sha256 cellar: :any_skip_relocation, ventura:       "0b6fc23aee1f7fec40d53afcb195e5fff3f4b41e8de79c46f75b8b1e3b6dde59"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "16c45ef1cb2789a06f076d00ac43b4796f0e260e7b7e06998efd331b5277abdd"
+  end
+
   depends_on "go" => :build
 
   def install
