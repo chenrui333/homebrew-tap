542b23c5017e6fcc6f3802e2b11456681da069cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-26 13:46:41 +0000

    brave-search-mcp-server: add 1.3.6 bottle.

--- Formula/b/brave-search-mcp-server.rb
+++ Formula/b/brave-search-mcp-server.rb
@@ -5,6 +5,14 @@ class BraveSearchMcpServer < Formula
   sha256 "3acc2c4d61cdaf2c604fb838b9843805b16780d287f2540f6874311b67c107eb"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1c893b31a758222e17ce98f1544ce96ee5733b84f0c3a55eb475a1b9a98fc806"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "462b925518d21e41d55a8ba53feda43494d1cfae4da72acb07d70b059ce529ec"
+    sha256 cellar: :any_skip_relocation, ventura:       "09c029d384b5fff685f7c69f00519fa179472f413e187c660383624bbdf91f38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a40901b5b5536ecde8ac6b8d5833373c588c34da3bff3558be40cfa2af4879da"
+  end
+
   depends_on "node"
 
   def install
