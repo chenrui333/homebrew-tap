29f5d18760e1c1decf24879614145931b49699b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-28 00:49:17 +0000

    debugg-ai-mcp: add 1.0.16 bottle.

--- Formula/d/debugg-ai-mcp.rb
+++ Formula/d/debugg-ai-mcp.rb
@@ -5,6 +5,14 @@ class DebuggAiMcp < Formula
   sha256 "3c52ed2403196b68520ea2c3f03986b27ebbb0d38361babc5237c426fd43d2dd"
   license "Apache-2.0" # license fix PR, https://github.com/debugg-ai/debugg-ai-mcp/pull/4
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "66488e6768c8a75fb8e5f5fef1d1b94bd50bf42a7281481102ce4f63b574acd7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c49e49cbd3dd01035a51983b2132ecf9fe9c199c47d2792b60bcb684d592d30c"
+    sha256 cellar: :any_skip_relocation, ventura:       "9aca20da0cc9e39966664e32a5b95bcc63f249b4efac939036109d3db2c55e22"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b339ec10d9856b524929effd22f9fc11ba99c1996e41a1bcedc92413d4e70e04"
+  end
+
   depends_on "node"
 
   def install
