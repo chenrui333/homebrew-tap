5f50d7a190353211ecf04e77db197d7615226a52
Rui Chen <rui@chenrui.dev>
2025-08-27 20:47:14 -0400

    cloud-run-mcp 1.3.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/cloud-run-mcp.rb
@@ -0,0 +1,27 @@
+class CloudRunMcp < Formula
+  desc "MCP server to deploy code to Google Cloud Run"
+  homepage "https://github.com/googlecloudplatform/cloud-run-mcp"
+  url "https://registry.npmjs.org/@google-cloud/cloud-run-mcp/-/cloud-run-mcp-1.3.0.tgz"
+  sha256 "17fe9083eedcfc3f6d18a36665b51b48b50072ef7e6da2f4fc6744aade2d8177"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = testpath/"credentials.json"
+    (testpath/"credentials.json").write ""
+
+    json = <<~JSON
+      {"jsonrpc":"2.0","id":1,"method":"initialize","params":{"protocolVersion":"2025-03-26"}}
+      {"jsonrpc":"2.0","id":2,"method":"tools/list"}
+    JSON
+
+    output = pipe_output("#{bin}/cloud-run-mcp 2>&1", json, 0)
+    assert_match "Lists Cloud Run services in a given project and region", output
+  end
+end
