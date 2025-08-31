6548d9f573ae4d5cb9e738eca009113765c592bf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-31 02:49:59 +0000

    mamediff 0.4.1
    Closes #1428.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/m/mamediff.rb
+++ Formula/m/mamediff.rb
@@ -1,8 +1,8 @@
 class Mamediff < Formula
   desc "TUI editor for managing unstaged and staged Git diffs"
   homepage "https://github.com/sile/mamediff"
-  url "https://github.com/sile/mamediff/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "9fa56ecdd593506021c57e32182dfa8a92850aa766db9d5779059268ce7e819a"
+  url "https://github.com/sile/mamediff/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "fde4529baf2040a411f736805ce844c260fc3f43a50677fa9edf48bc9dea7222"
   license "MIT"
   head "https://github.com/sile/mamediff.git", branch: "main"
 
