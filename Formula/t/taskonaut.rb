f121113f06cc82f46697109af70919c827beacc8
Rui Chen <rui@chenrui.dev>
2025-08-31 18:18:20 -0400

    taskonaut 1.7.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/taskonaut.rb
@@ -0,0 +1,22 @@
+class Taskonaut < Formula
+  desc "Interactive CLI tool for exec into AWS ECS tasks"
+  homepage "https://github.com/SchematicHQ/taskonaut"
+  url "https://registry.npmjs.org/@schematichq/taskonaut/-/taskonaut-1.7.1.tgz"
+  sha256 "8611eebb7070146c503285b544bf01748b878f461e394909ccecc1e74ca2c89a"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    output = shell_output("#{bin}/taskonaut config show")
+    assert_match "\"awsProfile\": \"default\"", output
+
+    output = shell_output("#{bin}/taskonaut doctor 2>&1")
+    assert_match "❌ AWS profile 'default' is not configured", output
+  end
+end
