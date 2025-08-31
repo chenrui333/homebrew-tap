110abb1499e5beae7ee9e226a9a9434cd83dcc9e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-08-31 22:28:25 +0000

    taskonaut: add 1.7.1 bottle.

--- Formula/t/taskonaut.rb
+++ Formula/t/taskonaut.rb
@@ -5,6 +5,14 @@ class Taskonaut < Formula
   sha256 "8611eebb7070146c503285b544bf01748b878f461e394909ccecc1e74ca2c89a"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34d303264f9d62b70d12a03360799ea8296dd36f40749243ae2d6233231e3751"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ef4397d8f5f54a671aa4e46783b3000e3ba8f80bc737c41c5810db45075eccb1"
+    sha256 cellar: :any_skip_relocation, ventura:       "4e1a97c44fbf68c7f02153d3fafe2bc2f795c977c55ece63f971bad750b57a51"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d6fc48cbcfbe69b63de16b89b64055537acbf2110f39d4547e9f74cc83bef0b0"
+  end
+
   depends_on "node"
 
   def install
