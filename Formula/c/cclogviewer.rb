5a09b85e14031390f2cbac7d7174b3499b93158c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 18:54:49 +0000

    cclogviewer: add 1.2.0 bottle.

--- Formula/c/cclogviewer.rb
+++ Formula/c/cclogviewer.rb
@@ -6,6 +6,14 @@ class Cclogviewer < Formula
   license "MIT"
   head "https://github.com/Brads3290/cclogviewer.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "67c0442d845a905f1779bd206de87a66a7fe091864b26ecf89d522099b5a19ba"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8b7462a6c9bc52721d9600feedf0baba8760c80c89e99b7e903cd40f38cc6ddb"
+    sha256 cellar: :any_skip_relocation, ventura:       "45f0d47139f5902ccbea5217aad0eaffbfccab20c4926d7f00e533c2369011a7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dd492b16dc5decb4f6a724cf86a67265bc3da9f3657f3da9794d584845668bb6"
+  end
+
   depends_on "go" => :build
 
   def install
