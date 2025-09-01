a22b145e0bdff10720ce77c5e946a72745a85246
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 23:00:15 +0000

    di-tui: add 1.11.2 bottle.

--- Formula/d/di-tui.rb
+++ Formula/d/di-tui.rb
@@ -6,6 +6,14 @@ class DiTui < Formula
   license "BSD-2-Clause"
   head "https://github.com/acaloiaro/di-tui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "791ee9480c3501468b6cc7a845c38d79fdd2718efc176a74d19129243cc0f216"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43b441a27d24fc48d52a83c4c53a50b8d733b8d83a43bad19e30d4af8e1743f0"
+    sha256 cellar: :any_skip_relocation, ventura:       "12a1ee11006d1a43b992f7da3fa7e26f0b1e91c6c36790ee948018487679b721"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "53c7f7d6f75bc7a62c89a08005c1a9e55bb1446b4c456eea94dc0744ad206841"
+  end
+
   depends_on "go" => :build
 
   def install
