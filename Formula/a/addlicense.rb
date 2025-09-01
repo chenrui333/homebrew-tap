ec5ac9c9f128f382f66f52123ab9c6e23c5888a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 22:43:05 +0000

    addlicense: add 1.2.0 bottle.

--- Formula/a/addlicense.rb
+++ Formula/a/addlicense.rb
@@ -6,6 +6,14 @@ class Addlicense < Formula
   license "Apache-2.0"
   head "https://github.com/google/addlicense.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d104563710829813f23c36e0d39f3fe8e271318dbf3ebaf295de72bd2ba6b7bd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dce435e663081292d2a3bec2b5c5f87904264cbb6d08c721414a8fbf9762be56"
+    sha256 cellar: :any_skip_relocation, ventura:       "2423e34bc109aa95579f84bbebc25751533762e782e475e5a70ba296bfafb95e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78829b5991f8ae8eb18b5f984e2d950e2508bcf9d87f30905f190383bd41c8be"
+  end
+
   depends_on "go" => :build
 
   def install
