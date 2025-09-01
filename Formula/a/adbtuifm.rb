dc2f7712bf98404799fe3cbb0c67c15f064573c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 22:27:09 +0000

    adbtuifm: add 0.5.8 bottle.

--- Formula/a/adbtuifm.rb
+++ Formula/a/adbtuifm.rb
@@ -6,6 +6,14 @@ class Adbtuifm < Formula
   license "MIT"
   head "https://github.com/darkhz/adbtuifm.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ba495be52869769df731a9869a941657bc9f5ca937b392648905c5abbe9af81"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a3c5a6211667d84228832ec7e008799ebfd7181dac427425e809a234f841f34d"
+    sha256 cellar: :any_skip_relocation, ventura:       "e5eb118922e2a01e743d288a42e482e480da1bdfda497392b3978d25109cd52f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8084e18b4b741e531dd44b55b815e0e957673a2ad266e72c411ca590c44be6cc"
+  end
+
   depends_on "go" => :build
 
   def install
