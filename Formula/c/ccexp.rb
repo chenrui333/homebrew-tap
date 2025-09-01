d4ec58748d737f98aa0853b228c5e7c64a9bca1b
Rui Chen <rui@chenrui.dev>
2025-09-01 14:56:05 -0400

    ccexp: vendored xsel for linux build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/ccexp.rb
+++ Formula/c/ccexp.rb
@@ -7,9 +7,22 @@ class Ccexp < Formula
 
   depends_on "node"
 
+  on_linux do
+    depends_on "xsel"
+  end
+
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    clipboardy_fallbacks_dir = libexec/"lib/node_modules/#{name}/node_modules/clipboardy/fallbacks"
+    rm_r(clipboardy_fallbacks_dir) # remove pre-built binaries
+    if OS.linux?
+      linux_dir = clipboardy_fallbacks_dir/"linux"
+      linux_dir.mkpath
+      # Replace the vendored pre-built xsel with one we build ourselves
+      ln_sf (Formula["xsel"].opt_bin/"xsel").relative_path_from(linux_dir), linux_dir
+    end
   end
 
   test do
