f859a42a0e52f03849f49053b6a4f40ec4020ad5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 19:05:46 +0000

    ccexp: add 4.0.0 bottle.

--- Formula/c/ccexp.rb
+++ Formula/c/ccexp.rb
@@ -5,6 +5,14 @@ class Ccexp < Formula
   sha256 "4050d11d372d06adeafe48da1ca7b2201b4e442fe4b8d6aa1943a17883e758c9"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d2a69d223e6199a6b7cc864d34c237686164c6dd65e7a9ab4106360efb5f7e7b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b41737a667b78a769a92bf720399e8964605066111344deebed925a9005c1ccb"
+    sha256 cellar: :any_skip_relocation, ventura:       "d105bc0ee98534da2ed073a0b62c9e6eb52fbd25e24d7943197c66dbc1d510ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "62889610ad9eef0a0f1ccb26d98ec885bb12ff060374203b9fa6c35ae4fa2534"
+  end
+
   depends_on "node"
 
   on_linux do
