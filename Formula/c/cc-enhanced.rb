a6137e1ad716983b71438eff9a1b34c01295a714
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 01:16:34 +0000

    cc-enhanced: add 1.0.2 bottle.

--- Formula/c/cc-enhanced.rb
+++ Formula/c/cc-enhanced.rb
@@ -6,6 +6,14 @@ class CcEnhanced < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/melonicecream/cc-enhanced.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "79c1b2eddf48c456a9001c5ad39b3d49a09755377fb9a851d8a4602a86c46da9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1a4d1fad5baa3f57bb6c043681eefe421c6693bd23d21d6945f94f7992f5fa19"
+    sha256 cellar: :any_skip_relocation, ventura:       "df9b41d6c2b3f95e0c56a17998654470715ed50e044a7069ba35b555fcea30a3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70ec20c0baa6aa0308515f4c139c4f9e5da46e1c17b1f184f5603f6de6d3db47"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
