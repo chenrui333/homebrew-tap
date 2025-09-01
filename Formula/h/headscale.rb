c14f9262f9e167f3941f85b821501d5ded725654
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 23:25:21 +0000

    headscale: add 0.26.1 bottle.

--- Formula/h/headscale.rb
+++ Formula/h/headscale.rb
@@ -6,6 +6,14 @@ class Headscale < Formula
   license "BSD-3-Clause"
   head "https://github.com/juanfont/headscale.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b4fdac09e2fd3525e5ee2c6d0bcb20f24c521d04da9cd2ffae5739a4dbc2aac0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9c3f6cdb74a6b0530b5dcf829d0a026c881efe25400dfe4f181e3012beff0f48"
+    sha256 cellar: :any_skip_relocation, ventura:       "19ebfb927544491413f730982e8b61d899255004af20190832033603acc2e98e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acf4012403e094eebd33ffcabe26ac308f6de5e0ee6a78ae7fcfe49b6c43ff37"
+  end
+
   depends_on "go" => :build
 
   def install
