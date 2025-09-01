c2e58cc2edee640f829f941f718dfb632b607713
Rui Chen <rui@chenrui.dev>
2025-09-01 15:50:38 -0400

    claude-composer: use `xsel` from Homebrew on Linux
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/claude-composer.rb
+++ Formula/c/claude-composer.rb
@@ -13,6 +13,10 @@ class ClaudeComposer < Formula
     depends_on "terminal-notifier"
   end
 
+  on_linux do
+    depends_on "xsel"
+  end
+
   def install
     system "npm", "install", *std_npm_args
     bin.install_symlink Dir["#{libexec}/bin/*"]
@@ -36,6 +40,16 @@ class ClaudeComposer < Formula
       # replace vendored `terminal-notifier` with our own
       terminal_notifier_app = Formula["terminal-notifier"].opt_prefix/"terminal-notifier.app"
       ln_sf terminal_notifier_app.relative_path_from(terminal_notifier_dir), terminal_notifier_dir
+    end
+
+    # use `xsel` from Homebrew on Linux
+    clipboardy_fallbacks_dir = libexec/"lib/node_modules/#{name}/node_modules/clipboardy/fallbacks"
+    rm_r(clipboardy_fallbacks_dir) # remove pre-built binaries
+    if OS.linux?
+      linux_dir = clipboardy_fallbacks_dir/"linux"
+      linux_dir.mkpath
+      # Replace the vendored pre-built xsel with one we build ourselves
+      ln_sf (Formula["xsel"].opt_bin/"xsel").relative_path_from(linux_dir), linux_dir
     end
   end
 
