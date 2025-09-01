c76e36f99ad46cfaff870b7fbcd549a06c7f691b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 19:30:49 +0000

    gritql: add 0.1.0-alpha.17 bottle.

--- Formula/g/gritql.rb
+++ Formula/g/gritql.rb
@@ -7,6 +7,14 @@ class Gritql < Formula
   license "MIT"
   head "https://github.com/getgrit/gritql.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da42f4f8c14409bcd378af659fb666a2eaae686384e45ca9b48e6539104f12c7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c788c59d0f1bbfda8ef4ce9c7017449f3c4e6fc82943823bdf0eaad33811265"
+    sha256 cellar: :any_skip_relocation, ventura:       "04935941062b0622d388c7ee2335d01ccfb4b1ac517617dc979a849d86ec18ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b652a906f5a1f716360ff56f8fa68b1e650c45b6a67791ccf4b81b2b410cd53"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
