89bae9d1d24abba2f25a78d49611ae1ea612e96b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-01 23:33:55 +0000

    npm-np: add 10.2.0 bottle.

--- Formula/n/npm-np.rb
+++ Formula/n/npm-np.rb
@@ -5,6 +5,14 @@ class NpmNp < Formula
   sha256 "e89644fe0a2100f468f3159f402256fa4392cd5782f28fa174b42e5fd46d30ee"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e24a914c884dacb60056579dabab20d93b2c71604cc92dbedfc73ad04f4de340"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fe86e72bb4d31053ab0c9f9cf6174a44250ea9f04c419c375d736a5fb10e5f64"
+    sha256 cellar: :any_skip_relocation, ventura:       "6e55b25ee7d1b6519c5b1192bcf8c0168e5880aca9f33d05be5c4f977d2ea459"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9a8453ccd9e9441780b06afa0e9ea44a4295cdcffcaced89a57d5257fe59163a"
+  end
+
   depends_on "node"
 
   def install
