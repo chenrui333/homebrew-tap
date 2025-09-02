db745c633588c19ebd1f91f12389a46de9cb448e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-02 00:18:58 +0000

    oui: add 0.1.8 bottle.

--- Formula/o/oui.rb
+++ Formula/o/oui.rb
@@ -6,6 +6,14 @@ class Oui < Formula
   license "BSD-3-Clause-Clear"
   head "https://github.com/thatmattlove/oui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5bc187889b787d025018c8b4cd46eaf3ecdccecc921edbc22b8b9a3cb107705d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "37da5916eb4bb261e347e9b3075c3aa1a699b2ebeb163a147870fca6951e84a0"
+    sha256 cellar: :any_skip_relocation, ventura:       "fbfb0a1e53cc7faa2cc3d28c64cd68bd88d848fabd95a38cbd36ee4aa9ae6051"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ef3cbac92db804e53b88455d462236ff891d83ce548f6322317e57d8070f3c1"
+  end
+
   depends_on "go" => :build
 
   def install
