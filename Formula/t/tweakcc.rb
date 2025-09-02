9bf6c190ced7aa5f0a29621dcd38d2acea0880e2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-02 06:40:48 +0000

    tweakcc: update 1.3.0 bottle.

--- Formula/t/tweakcc.rb
+++ Formula/t/tweakcc.rb
@@ -7,10 +7,10 @@ class Tweakcc < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b946eb840a8d3ef265e9493734c741669200f7b59e9a19a619d2aad8f6ff7ed8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6b69fc99f8d26e88bb6530ed8078e30120a9982e23afcc4e50ce10615a2a967c"
-    sha256 cellar: :any_skip_relocation, ventura:       "1bb8ace999151e3548ea37b84da546909fc4603e5029657c9d833684da3cc753"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "207512a3b32cbc15fbb28ea399d9524da472a4e7e76623800de1862c76114a46"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71d4b45b8866be28ae0e481229713bf522995f1ba701257a5540fa7970d09f31"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d3ca20d70fa38a4e3415a23e860b6f0ef20492c96af41184c1c60a8640bf91b"
+    sha256 cellar: :any_skip_relocation, ventura:       "6501062fbd8d237b163d744eef2eb7e5bea4b475a4d071a89c7ab91ad46f92ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b3c56650cf60062d45ffce1ee04668ad0630938c2fed292b93d52d62eb36409f"
   end
 
   depends_on "node"
