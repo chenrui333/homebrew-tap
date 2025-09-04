fd0fe398e6409dbdeef3ee9b5de5098cd45a313e
Rui Chen <rui@chenrui.dev>
2025-09-03 23:58:53 -0400

    envie 0.0.7 (new formula)
    Closes #1483.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/e/envie.rb
@@ -0,0 +1,20 @@
+class Envie < Formula
+  desc "MCP server for Apify"
+  homepage "https://github.com/ilmari-h/envie"
+  url "https://registry.npmjs.org/@envie/cli/-/cli-0.0.7.tgz"
+  sha256 "a55785d2739866b49adb95b9d33e6f9e78418e870f07bff978665019a5a60292"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match "Error: No authentication token found.", shell_output("#{bin}/envie environment list 2>&1", 1)
+    output = shell_output("#{bin}/envie login 2>&1", 1)
+    assert_match "Login failed: Please specify which keypair to use", output
+  end
+end
