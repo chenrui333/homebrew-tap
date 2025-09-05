0a2f3e85963292e04539533b76314cff1e9ce580
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-05 04:10:53 +0000

    localports: add 0.1.0 bottle.

--- Formula/l/localports.rb
+++ Formula/l/localports.rb
@@ -6,6 +6,14 @@ class Localports < Formula
   license "MIT"
   head "https://github.com/diegoholiveira/localports.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8ef1eb58fd4325733756b6285cdfce8cf5022224a97afabbeb917364aac10ae3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9bd99fdf4c544ff6c59f9ad4c492a9be4a1032619ec651afaddeab1856d060a9"
+    sha256 cellar: :any_skip_relocation, ventura:       "e5d6f219999e0b342c0cfa8730bce8e9b5ff8d17fbd154db2026a131d9b8edf8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "976f51a110cb8d58183169be8239b1e513c757e8adecc503e73c6421b49d1838"
+  end
+
   depends_on "rust" => :build
 
   def install
