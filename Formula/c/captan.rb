b58a9e657e8639b6df9b012203c12ff70fce6835
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-05 03:54:03 +0000

    captan: add 0.4.0 bottle.

--- Formula/c/captan.rb
+++ Formula/c/captan.rb
@@ -5,6 +5,14 @@ class Captan < Formula
   sha256 "7736b6329ebbd5cf266d7c8451394b7c41ecef47cb98377a351613e1966453b4"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "079c192625795f97066d28ce8264437d1cc8de0b9a07f04bcf43a7c3a9f5130e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4086dbeb34ac94397bc54dfa31f1e0b2446bc10edaa27ce7ec34c668883f31b8"
+    sha256 cellar: :any_skip_relocation, ventura:       "246acfa5c8284f8063801cb1a3a0a2141fb38998778b4937a67692735fed1e90"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c73aa450bd544319a9151211f39e79bc6144d445ede2dd436d4d25d790a5f72"
+  end
+
   depends_on "node"
 
   def install
