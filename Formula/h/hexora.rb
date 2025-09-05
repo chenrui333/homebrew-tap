6c4f69bd434594648045c6afdbe7f676bafd370e
Rui Chen <rui@chenrui.dev>
2025-09-05 09:39:14 -0400

    hexora 0.1.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/h/hexora.rb
@@ -0,0 +1,29 @@
+class Hexora < Formula
+  desc "Static analysis of malicious Python code"
+  homepage "https://github.com/rushter/hexora"
+  url "https://github.com/rushter/hexora/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "86de5c5ea4a7300876d22b79d4ce48e30b9ef2f537bf95c75df476ed540b24dd"
+  license "MIT"
+  head "https://github.com/rushter/hexora.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    output = shell_output("#{bin}/hexora rules")
+    assert_match "| HX2000 | ClipboardRead | Reading from the clipboard. |", output
+
+    # Create a minimal Python file that should trigger HX2000 (clipboard read)
+    (testpath/"bad.py").write <<~PY
+      import pyperclip
+      data = pyperclip.paste()
+    PY
+
+    out = shell_output("#{bin}/hexora audit --output-format terminal bad.py")
+    assert_match "HX2000", out
+    assert_match "clipboard access can be used to exfiltrate sensitive data", out.downcase
+  end
+end
