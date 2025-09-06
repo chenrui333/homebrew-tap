2ea8b52ec10d2d91f6cfdfa1559b53078a6c3418
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-06 22:15:59 +0000

    git-x: add 1.1.0 bottle.

--- Formula/g/git-x.rb
+++ Formula/g/git-x.rb
@@ -6,6 +6,14 @@ class GitX < Formula
   license "MIT"
   head "https://github.com/simeg/git-x.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "62822ca3ac077e0ecac1544264dc6b6f53eaea620ffeecdacdcbace655690d4c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a57cc648556d5922d3c352cb8969e17f52b7ea3f3a7b32a8fad565f56daee3c7"
+    sha256 cellar: :any_skip_relocation, ventura:       "bd7514a6f02876a32f02d3dcf62822e2b567972c7824fea2917ecddb6495c99a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b3e5889c9fa102c631fbba46777903e3ade322212b3068963b9dbf845d5c8b14"
+  end
+
   depends_on "rust" => :build
 
   def install
