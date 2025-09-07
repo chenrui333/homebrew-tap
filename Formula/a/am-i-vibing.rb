e2b091f44a770cf7181edab3fc96164743a6454f
Rui Chen <rui@chenrui.dev>
2025-09-07 16:39:44 -0400

    am-i-vibing 0.1.0 (new formula)
    Closes #1527.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/a/am-i-vibing.rb
@@ -0,0 +1,22 @@
+class AmIVibing < Formula
+  desc "Detect agentic coding environments and AI editors"
+  homepage "https://github.com/ascorbic/am-i-vibing"
+  url "https://registry.npmjs.org/am-i-vibing/-/am-i-vibing-0.1.0.tgz"
+  sha256 "830ce89a5c6863cd8712f83b26b9924485789fdc3a8359a6ef2872cfe85981a7"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    output = shell_output("#{bin}/am-i-vibing 2>&1", 1)
+    assert_match "✗ No agentic environment detected", output
+
+    output = shell_output("#{bin}/am-i-vibing --format json 2>&1", 1)
+    assert_equal false, JSON.parse(output)["isAgentic"]
+  end
+end
