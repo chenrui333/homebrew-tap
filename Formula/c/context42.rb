b6b8674170f84eedd7413327fab81d5720a0e28c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-07 20:59:08 +0000

    context42: add 0.3.3 bottle.

--- Formula/c/context42.rb
+++ Formula/c/context42.rb
@@ -5,6 +5,14 @@ class Context42 < Formula
   sha256 "48534ea885d58e2b50727f5f142df76eceffc35c690b38230b89955b88edddca"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e858e7745c04074f5099c97e4a95a25a23c227a65d04edd42b5a9249350baa6f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8d71f9ed53f6df8010318efdf6c15758fdd7cd82dac62dea660c254b97760629"
+    sha256 cellar: :any_skip_relocation, ventura:       "2efeeb92392017e60d603296e3e6eb2c1d86cbff61f764ff1ab26e0c0be15774"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eed18dd1126123d50e2d52ed3c8a798b179bcf1fe1bf7518d0ff4706f49a3755"
+  end
+
   depends_on "node"
 
   def install
