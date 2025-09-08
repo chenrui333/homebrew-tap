ea3304cfa2c7058075fc4a01031ee2aeae3f0ece
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 13:46:59 +0000

    lazycli: add 0.1.15 bottle.

--- Formula/l/lazycli.rb
+++ Formula/l/lazycli.rb
@@ -5,6 +5,14 @@ class Lazycli < Formula
   sha256 "66f4c4c5bedf4d3ceb35aebc1d7f18663c7250ac47241fea18108c0741bf2019"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "990118766cfd3ec37e9c44f50d1c64b5330001e19fd1309c5d287c5eb2e9eb95"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "43e22c022fa5c60d9e73144e4f060b433304cabdbbaa3338d1cdfd59d771bd1b"
+    sha256 cellar: :any_skip_relocation, ventura:       "a5404c8f84df2bde35819f09c52d3f8c718817c17d740fab8438b358fd91e27c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ef4af19ed42b1a37cacbb1daa9c8cdad4724d61c3471751c6b45334eab93db24"
+  end
+
   depends_on "rust" => :build
 
   def install
