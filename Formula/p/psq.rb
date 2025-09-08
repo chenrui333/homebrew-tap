c2f43afa53dbd5499e886be5fc8f127a3fdd0509
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 13:44:00 +0000

    psq: add 1.3.1 bottle.

--- Formula/p/psq.rb
+++ Formula/p/psq.rb
@@ -5,6 +5,14 @@ class Psq < Formula
   sha256 "c8e2959f7bfa526ddcaf67e1da1b8fd0b17005a31c71992122bd10fc39754c7e"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "166c5af7380831965294984ef83241d81b264ed1c41973e58e2f964a904c37d1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b227ff4e08169d1489a7ef7c243bfae452e958c55a3b5f42cb4bcee962346976"
+    sha256 cellar: :any_skip_relocation, ventura:       "6990aa4166c293a90486e0b5c60ef73d2ee139cbcc3720e75f65c4905f1e662e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8b009695f2d453300e4a28f55cd9052039e67af7730031c735e1049a10eb1d6"
+  end
+
   depends_on "go" => :build
 
   def install
