a66734c74ed8a76995fe253f3c8105484bd180b8
Rui Chen <rui@chenrui.dev>
2025-09-08 09:25:50 -0400

    psq 1.3.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/psq.rb
@@ -0,0 +1,23 @@
+class Psq < Formula
+  desc "Lightweight postgres monitor for the terminal"
+  homepage "https://github.com/benjaminsanborn/psq"
+  url "https://github.com/benjaminsanborn/psq/archive/refs/tags/v1.3.1.tar.gz"
+  sha256 "c8e2959f7bfa526ddcaf67e1da1b8fd0b17005a31c71992122bd10fc39754c7e"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    output_log = testpath/"output.log"
+    pid = spawn bin/"psq", testpath, [:out, :err] => output_log.to_s
+    sleep 1
+    assert_match "Initializing", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
