288b10d16ec85408d3b5289b510b30e76ca3e233
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 19:08:40 +0000

    psq: update 1.3.2 bottle.

--- Formula/p/psq.rb
+++ Formula/p/psq.rb
@@ -7,10 +7,10 @@ class Psq < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "166c5af7380831965294984ef83241d81b264ed1c41973e58e2f964a904c37d1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b227ff4e08169d1489a7ef7c243bfae452e958c55a3b5f42cb4bcee962346976"
-    sha256 cellar: :any_skip_relocation, ventura:       "6990aa4166c293a90486e0b5c60ef73d2ee139cbcc3720e75f65c4905f1e662e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b8b009695f2d453300e4a28f55cd9052039e67af7730031c735e1049a10eb1d6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "018607b1c6d363629fae8c5d02f4905f9981f1d8efdcb476e478716d9aab6efa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dcd5424ba01ccbe1e747ce293ad63ce9f821241c16ca2b9722bcd3dc9414ecb9"
+    sha256 cellar: :any_skip_relocation, ventura:       "ea6257d28c6a9dd2636ca2521d8f91bdd2664813fe815501a3a4ba3c61308009"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2eaf5708a89d195d138d3e7aaf53ee5bc0f19743ffb773cf4d34e8957bab6a78"
   end
 
   depends_on "go" => :build
