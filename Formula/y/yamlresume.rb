5caf98c76067587836e834ffa2f903f381647fc8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-08 13:02:15 +0000

    yamlresume: add 0.7.5 bottle.

--- Formula/y/yamlresume.rb
+++ Formula/y/yamlresume.rb
@@ -5,6 +5,14 @@ class Yamlresume < Formula
   sha256 "77a1ff0ffa031a3e335d0eb453c12439c90dd54273889ef3434667b5539e52d3"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "46964ff30672ce2c4667cb562a3b250ce6e3d38f3d423c2bd0362ce49a29dab1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c1ced93fa949051b30e40fbd14217fd5fac099a102bd99e0e0d78eccae3d7af0"
+    sha256 cellar: :any_skip_relocation, ventura:       "33ca76cfef0ab1b4b53de69c240377a2fa22346830f20b0c2fcf731dd7e37884"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40339c948527e18d6a36831a8667fbb59a34c27f6e16571f1728b066f18d337a"
+  end
+
   depends_on "node"
 
   def install
