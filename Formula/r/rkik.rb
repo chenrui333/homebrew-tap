432ba336e2ead5725bee0179f8098df71e0128ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-09 16:41:05 +0000

    rkik: update 1.1.0 bottle.

--- Formula/r/rkik.rb
+++ Formula/r/rkik.rb
@@ -8,10 +8,10 @@ class Rkik < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "deca30ecd3ad78a2dbb1fef05bc5478c2b520c4b591586db6582fb033e685b14"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "814b23b08e3f0343191f24c518510fc90efd5a84ebd6cb0d40e8c0020c7f4ee2"
-    sha256 cellar: :any_skip_relocation, ventura:       "5267a72af482cd91bbf9bb426e646b3d14a6405d42e446b929ed12f229d43346"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "57d51eb8f27d01941662cab1cc0bef7ed5de98e2afce9077f4ba32716d1c7d1d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "77e756accb32852b477278f4e6e30a6d4cde538766fdaaa4a09e889b21efbca8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2622426e4c062268f941c488d4a665382abdcb3f003c607d1cb47d235dae1932"
+    sha256 cellar: :any_skip_relocation, ventura:       "de099f3ccf1520e62e314c252a49982fe0137023b73c37f3cfef17280c43ac71"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e786266857d46b63bf83000bc973784f83dba06f97f8ac458a3140233ce4dbd2"
   end
 
   depends_on "rust" => :build
