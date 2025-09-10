b2820fa54394fb01350756f7c5e8eb7a3a4f9a3a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 22:37:17 +0000

    railway-mcp-server: update 0.1.5 bottle.

--- Formula/r/railway-mcp-server.rb
+++ Formula/r/railway-mcp-server.rb
@@ -7,10 +7,10 @@ class RailwayMcpServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe5489da212883d5bd1f89a85d8eac7324d7980acaa4e1056ea854cc3f725ab6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e10edbb8c8432f245df8eada778f1e4ab3791517da80f17173c1be382b5bfb7c"
-    sha256 cellar: :any_skip_relocation, ventura:       "b5e7882977973bad6ef26d290410b30d83cb002695cbcf5303048512d543f592"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "97896bb33e150c78a450a4b46f518132cf6f06c88c13bb40b8fb5c30277f7c5a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7af5a4cab08f2fceff732c690fa4dbcfab98d98e93d641d5a1440e826425433d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e6bc89da6d2ed3653909df9b6aa71dde70144b74e3b5d09fde67580cc5bd66fa"
+    sha256 cellar: :any_skip_relocation, ventura:       "c88ea9091bc863628baddfd610ab87080ccfa4f4b0d22d246441011271dda355"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4ae38ac6df0dcd5e71f78d571e05f9b134b809049bc1e3f80c462d1d15a73e5"
   end
 
   depends_on "node"
