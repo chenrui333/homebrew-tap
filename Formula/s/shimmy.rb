507fbcd02d6adc0a03004fcd252cd55165df23c8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-10 16:45:57 +0000

    shimmy: update 1.2.0 bottle.

--- Formula/s/shimmy.rb
+++ Formula/s/shimmy.rb
@@ -8,10 +8,10 @@ class Shimmy < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d542a27b0ba117939f1f1df0278c0a70aef78464a330a83b84faca1b78b70a68"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e9d8208c7aee2b9938bfa0a26f555ee6ebe295a6e2653704421e6423f2c94cfc"
-    sha256 cellar: :any_skip_relocation, ventura:       "648277b570e0cc128f18709ccebb222495780f655cdf2b33ad64da407e98d3c1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8e912fc3c00805ee2e5c991fe58122c5e19c55b61b9741ac6843d5a33b8fd48a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f33137c93b6689fb2e12edad8cb97962767a3c9e7f31443312776d5a5b54c072"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "91418e5b65104c01dba6292a9a6c30445c196082952896392eb4e76f87eabea9"
+    sha256 cellar: :any_skip_relocation, ventura:       "9d8dbf1ff3430fa281b793f24ba485cb102e7f84cd859e1b0e0ebee6fc6a00b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe8542f5360920666ca3d4d7c646e79cafda7094fa20dd046d484f08f709a1dd"
   end
 
   depends_on "cmake" => :build # for llama-cpp-sys-2
