b92f2fb638ee4d66b0fbda867f06accfc83ba5a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-12 14:42:35 +0000

    brighterscript-formatter: update 1.7.18 bottle.

--- Formula/b/brighterscript-formatter.rb
+++ Formula/b/brighterscript-formatter.rb
@@ -7,10 +7,10 @@ class BrighterscriptFormatter < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9f94cf220f4be30a715e0c0f21acdd2b0be3865cc6fd0c0425fd6d0af914cb34"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00b84005e1e403b02a7f8d37ba2031fa2eaa94543c6ac58ea2eb08147840bac8"
-    sha256 cellar: :any_skip_relocation, ventura:       "fed58aa1f1316e4c797db21a7b0f85ec72ba6d126338c04a1d3a7067ed397f8b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2255d42a67c5c79a98335c8838c1fa965a51277d71d0a1abbe6cbdd8ead98bca"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c53dcd7fd72b9a3a24a87ba99453c2d302c5f571900e55b72b89cc6fe7dd5944"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "039fc7ec568ddd3242855dff35759b1552327465de304604216b3560ac7bdac3"
+    sha256 cellar: :any_skip_relocation, ventura:       "1dda2de35a0d7e6eb96fe63f5e6839f1df5df461e6b27339d859d5a891d84391"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b51ed6e14ee0174c2d9fe4e50eb5c1af21d9739fd9923df012d90456f9399268"
   end
 
   depends_on "node"
