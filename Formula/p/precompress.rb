1bb28c765932d2015c2d615c6262aaf3c438d1a4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-14 08:27:39 +0000

    precompress 13.0.1

--- Formula/p/precompress.rb
+++ Formula/p/precompress.rb
@@ -1,8 +1,8 @@
 class Precompress < Formula
   desc "CLI to compress files to gzip and brotli"
   homepage "https://github.com/silverwind/precompress"
-  url "https://registry.npmjs.org/precompress/-/precompress-12.0.7.tgz"
-  sha256 "9b9dc0c631c2dd3683fe9b0adfd78df5408c263bc07236e00f8ce0eb8c66697d"
+  url "https://registry.npmjs.org/precompress/-/precompress-13.0.1.tgz"
+  sha256 "437f70b6df157a191b2235da4606996aa03750f6ac3ff55ab713b8c7a3f46db4"
   license "BSD-2-Clause"
 
   bottle do
