a5b46e2fa7ea80c954652edf3510488387731e99
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-15 01:22:07 +0000

    attempt-cli: add 1.1.0 bottle.

--- Formula/a/attempt-cli.rb
+++ Formula/a/attempt-cli.rb
@@ -6,6 +6,13 @@ class AttemptCli < Formula
   license "Unlicense"
   head "https://github.com/MaxBondABE/attempt.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b7484f260885dd7b4674546f536556bff37c6d732d47b432c02c580162bb4424"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "89844b908082bc944ac87822cddc290736427c9270ac510e9f21deb981ac2bc6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9bd7caf40cc52a26353e40d5c0413b84b03623e2456866cbb53f7d103a9a9f9e"
+  end
+
   depends_on "rust" => :build
 
   def install
