4d4fb6591d251a6c9b8bc7ff89cf24cd85380ef3
Rui Chen <rui@chenrui.dev>
2025-09-15 10:55:38 -0400

    claude-code 1.0.113 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/c/claude-code.rb
@@ -0,0 +1,29 @@
+class ClaudeCode < Formula
+  desc "Agentic coding tool that lives in your terminal"
+  homepage "https://github.com/anthropics/claude-code"
+  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-1.0.113.tgz"
+  sha256 "7dfa04ede8c19e08273f555d4663094f4387dcff606cdff2e94d1df5a1cce89b"
+  # license :unfree
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # remove non-native architecture pre-built libraries
+    # https://github.com/anthropics/claude-code/blob/main/CHANGELOG.md?plain=1#L79 (since 1.0.84)
+    os = OS.kernel_name.downcase
+    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
+    ripgrep_vendor_dir = libexec/"lib/node_modules/@anthropic-ai/claude-code/vendor/ripgrep"
+    ripgrep_vendor_dir.children
+                      .each { |dir| rm_r(dir) if dir.basename.to_s != "#{arch}-#{os}" }
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/claude --version")
+
+    output = shell_output("#{bin}/claude config list")
+    assert_equal false, JSON.parse(output)["hasTrustDialogAccepted"]
+  end
+end
