324e64f84992b8642f6c0ec7d6e70fbe49816592
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-15 01:18:30 +0000

    pr-desc-cli: add 2.0.1 bottle.

--- Formula/p/pr-desc-cli.rb
+++ Formula/p/pr-desc-cli.rb
@@ -5,6 +5,13 @@ class PrDescCli < Formula
   sha256 "e5df3d3f50921236302ba2af33b417ea281edebd1881eda8d55db5f7fc24e4a9"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c9bc2eaecfe7a39bd33dfd5bfeb2cd8486e213eccde971e1695eadc6c1eb263"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f85e8cdf125377730756da887d0ca9693350f94d8abe059fd3a78c1ecd025624"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51493fd7063255db4c38a47b97be3456082c93721bf7cabbd0783fc8c09bd040"
+  end
+
   depends_on "node"
 
   def install
