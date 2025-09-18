cbefa8b596aed233a8af82f492a0e08b93a4a74f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 20:37:36 +0000

    wuchale: update 0.16.3 bottle.

--- Formula/w/wuchale.rb
+++ Formula/w/wuchale.rb
@@ -7,9 +7,9 @@ class Wuchale < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "829ae9f03fb8d7c97bedaf498a64dcf7e7fed813190500e4c9fbb06e2fce2947"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a7b4a5dfb612c29c3fddacb9122a067d781713e5de44b99ba2c8123b5232f24d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "938a4e35817eb3a38298a7ff6179678d68d2e005b9f57f8234e1c977b37669e0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5839de3ca2c90b2c213b55c613c6c79a52781cbd12941fa19c73173d9406fc6e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00c5aeb5b4c1a3b68c80fc13bd99a37dd127a51ad095e9b170281791a0775ac3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ad07c746b63bce0bfd8ced559d4155042d939deb847e9314e8e489de9faee755"
   end
 
   depends_on "node"
