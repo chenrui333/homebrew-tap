701fe45bd002ae25be6df756d78456e5c64b5bde
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 20:34:44 +0000

    pr-desc-cli: update 2.0.2 bottle.

--- Formula/p/pr-desc-cli.rb
+++ Formula/p/pr-desc-cli.rb
@@ -7,9 +7,9 @@ class PrDescCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9c9bc2eaecfe7a39bd33dfd5bfeb2cd8486e213eccde971e1695eadc6c1eb263"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f85e8cdf125377730756da887d0ca9693350f94d8abe059fd3a78c1ecd025624"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51493fd7063255db4c38a47b97be3456082c93721bf7cabbd0783fc8c09bd040"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "35c21837954cd721dd3a54ec9ef496b76fd863f2b5e58ce412c7108128eca340"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9ee603fd7ba4a9cb53f47fef74864356242370c18539c5dfe35623931920c2cf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a9c9c7371bfbca0b1cff29c6c9fffef20c7575a7ad4cdea6547f9d5c2b4bbaeb"
   end
 
   depends_on "node"
