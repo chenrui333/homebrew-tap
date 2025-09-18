ce853a4424350f8a60b0f66cc48c1840dd024d8c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-18 13:12:46 +0000

    ccapi: update 1.0.7 bottle.

--- Formula/c/ccapi.rb
+++ Formula/c/ccapi.rb
@@ -7,9 +7,9 @@ class Ccapi < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_sequoia: "c9cbaa68274a23e5e6b14241fb3804025f8b06c19b8bd4406ca122e39d332e68"
-    sha256 cellar: :any,                 arm64_sonoma:  "69f6173b67b6dd1c2ad9928df9e6c8ddd1e1bb060ebbe3309e47e42b049dffae"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0834573e8a6651b68127946bf05c47cd97eced58b316f64c12426e056ac1853a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f3e6a9bd9932de47bc5d00ffa3f9aefd3d4a2239694e4c4fdcb0bf1992ffbcb6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "34ae584e4d769139b468416f1ec32a250ca9030f368b582e8bc4b5735a5ed75a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "03d1680dc50bb6e1e6a556b5e774e88f4d94f618d78980ff6735d6a64ec5cea2"
   end
 
   depends_on "node"
