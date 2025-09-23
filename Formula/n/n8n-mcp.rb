45da31c41690fa6bbe024994cd311579231267a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-23 23:39:29 +0000

    n8n-mcp: add 2.12.2 bottle.

--- Formula/n/n8n-mcp.rb
+++ Formula/n/n8n-mcp.rb
@@ -5,6 +5,13 @@ class N8nMcp < Formula
   sha256 "c6796d047792883416dd9bb4a8543501dcf3f5324c5b4d6c82d0fcec82b9de84"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "edc3e637db93bd0b8f0df134a9895e3fdfae3c0d046bc5837abb295fd5563cc0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "789736249bf726ae34bf55b22b09d251a70abe512c345b69033f0b0c2dd89b8e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "75ec739e93b06fc59354d182d6f263a457b3225110351d91e40069d7b61680e8"
+  end
+
   depends_on "node"
 
   def install
