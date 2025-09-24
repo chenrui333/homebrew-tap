4f3ccc2b20326c1fe429c7f77e84f7bf263fe68b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 13:50:07 +0000

    salesforce-mcp: add 0.21.2 bottle.

--- Formula/s/salesforce-mcp.rb
+++ Formula/s/salesforce-mcp.rb
@@ -5,6 +5,13 @@ class SalesforceMcp < Formula
   sha256 "24a8296a1837a20c3b13dd610a048ceeeb25d57e9e6fe215f5d8dd9d48494cdc"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9b2c78179c8fbf7e64f3b4b32f5e4fa82b043e3f853d6345bbe403df1d127aff"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2e16498807c8e9a849db5f1b71ce280f936a01949b4a884bddf0e0df89c6c67e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f8790c5f891c1a53674b1a27e09e3f27afae22e29040aa86d68bb6d6c12c7455"
+  end
+
   depends_on "node"
 
   def install
