ac38397d18694fbd6be9545ccaf5c09af70bd9f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 15:25:04 +0000

    tfstate-lookup: add 1.7.1 bottle.

--- Formula/t/tfstate-lookup.rb
+++ Formula/t/tfstate-lookup.rb
@@ -5,6 +5,13 @@ class TfstateLookup < Formula
   sha256 "25cdce913064998704a8b5d26ff01a572564531999211e6c05b382f20df03f7e"
   license "MPL-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f8dea786597ed9b424cb3b642971005f6121edcb0be5210f5b171d2b93c51a83"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5769b9e2644e72b933284cb9445729568e144ffcc34cf18ab664482280d39dc7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e2196e5fe4249cae04a1795930e41b4a122ef1acb666f51f6720f84dfc415d1"
+  end
+
   depends_on "go" => :build
 
   def install
