ada2adcd72e7135c45f9c80e8d65878ff9933a3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 15:34:42 +0000

    tfclean: add 0.0.11 bottle.

--- Formula/t/tfclean.rb
+++ Formula/t/tfclean.rb
@@ -5,6 +5,13 @@ class Tfclean < Formula
   sha256 "df058b6b1ac4e71fd4669f6e509e20e08f9223eece1082d9c0c908f83b074d4e"
   # license bug report, https://github.com/takaishi/tfclean/issues/70
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2d099ed0dfe55a322a38e79fd6654d995e2917a55e28cac4dffb95c91cbf6dcd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c163ad14cc44bf2906d2f231197e7fd2422c4b7d3dc365546b6f73526565fa6d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8637d71f77ad66e1457196b16f07f2a157406a76336bbb7e4ad000cbf95bb09"
+  end
+
   depends_on "go" => :build
 
   def install
