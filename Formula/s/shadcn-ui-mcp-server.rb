1b3959be58b97ddc9128a9b2d5f1057c46bdeea1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-24 16:01:15 +0000

    shadcn-ui-mcp-server: update 1.1.0 bottle.

--- Formula/s/shadcn-ui-mcp-server.rb
+++ Formula/s/shadcn-ui-mcp-server.rb
@@ -7,10 +7,10 @@ class ShadcnUiMcpServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d361aa38c1e4cdce6c3572fcb3a5c0a90db39604eb1ab4f157fcba94d3a5d8bc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "08d6911b877e565edd923788518f88a9df9a21cdefd0a1b0a302351a4275674b"
-    sha256 cellar: :any_skip_relocation, ventura:       "079852ce17585df177887f963658874f3e3469b00b885fb4f0b8de3c3401e52c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fdef32102a261fd88db2601d2af3e05d230cecd46bce21b59afc09949164fd7f"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9f4c7539944de67cde50bdec90b8ac1a5d04a6adbba37b89730a764d74f79fbf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5bca8452613ca08f2cbdd65081aa16e07e699c403834d60dfb8755723d7f064c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3195fb9a65b4f7c270c78e9cef07dc7b49918be4a7cc0582e401f0c39c72851f"
   end
 
   depends_on "node"
