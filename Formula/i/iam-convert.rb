76781a972506fb9722be96058173382b7dfff94f
Rui Chen <rui@chenrui.dev>
2025-09-24 11:35:35 -0400

    iam-convert 0.1.35 (new formula)
    Closes #1712.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/i/iam-convert.rb
@@ -0,0 +1,28 @@
+class IamConvert < Formula
+  desc "Convert JSON IAM Policies to other formats"
+  homepage "https://iam.cloudcopilot.io/tools/iam-convert"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-convert/-/iam-convert-0.1.35.tgz"
+  sha256 "e35061262a2f4d7fb2cfa9ab577ce0569e65b3d150a0a76ef9baf885aae703c2"
+  license "AGPL-3.0-only"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    resource "test_policy_json" do
+      url "https://government-secrets.s3.amazonaws.com/secret-policy.json"
+      sha256 "6348c4060ad668b8cc14aaad21fd426ca2bffb3002fc11abb89ab4279439b409"
+    end
+
+    testpath.install resource("test_policy_json")
+
+    output = shell_output("#{bin}/iam-convert --file #{testpath}/secret-policy.json")
+    assert_match "data \"aws_iam_policy_document\" \"policy\"", output
+
+    assert_match version.to_s, shell_output("#{bin}/iam-convert --version")
+  end
+end
