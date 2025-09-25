c53b1e330d2d4d5e83e1cce9492ebd535caaecc2
Rui Chen <rui@chenrui.dev>
2025-09-25 12:14:04 -0400

    fastmcp: needs uv at runtime
    Closes #1719.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/f/fastmcp.rb
+++ Formula/f/fastmcp.rb
@@ -12,6 +12,7 @@ class Fastmcp < Formula
   depends_on "cryptography"
   depends_on "libyaml"
   depends_on "python@3.13"
+  depends_on "uv"
 
   resource "annotated-types" do
     url "https://files.pythonhosted.org/packages/ee/67/531ea369ba64dcff5ec9c3402f9f51bf748cec26dde048a2f973a4eea7f5/annotated_types-0.7.0.tar.gz"
