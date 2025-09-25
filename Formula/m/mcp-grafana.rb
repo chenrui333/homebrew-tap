53c8f2084ce41f6564cca866ea0773097e41a910
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-25 14:33:11 +0000

    mcp-grafana: update 0.7.0 bottle.

--- Formula/m/mcp-grafana.rb
+++ Formula/m/mcp-grafana.rb
@@ -8,9 +8,9 @@ class McpGrafana < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "524fb4b8308eba9afafc18e8466dadffaac380ab4aaa9902571fcde6231217ea"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2aef1716be645e2701bdfe4a697216fc15d3a67c1339a73534c6b63c164a8447"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f6b26d30ff5240896d79e23390259cb2869e4581aa5cd81e9d06e38d78716bfe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb82098bbe62e55ac077fefdc5531022b59872e61139dc334fa59ebdb82cf0de"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bf90033d8d05bf594f7d8299b0618ae28219f5d9d2fcd1ff90f2d00d6424d335"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e6e5e84195a3b205c9786031ea28a672a8c6472d72cfdab3b2341d75e766916"
   end
 
   depends_on "go" => :build
