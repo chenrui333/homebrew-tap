6727e92ed6d5c255dbe10df537f374c380ec0cab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-25 20:32:44 +0000

    browserbase-mcp-server: update 2.1.2 bottle.

--- Formula/b/browserbase-mcp-server.rb
+++ Formula/b/browserbase-mcp-server.rb
@@ -7,10 +7,9 @@ class BrowserbaseMcpServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_sequoia: "987a64aaae3f128f4bf4fbb06d70273c740d933e844888772762df9136ca3b4c"
-    sha256 cellar: :any,                 arm64_sonoma:  "65890fd9a93ad6f8806f9c97d56dd0d6235edaa2276ebf1ead3724b4ef47887a"
-    sha256 cellar: :any,                 ventura:       "2a2f57ddb06d6a898598ac412d8c1ee3cbdecb52b8c25ae2b32068e91fbbceb2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dfa1bddb0e41362c6c669ab4ce9cde668c915081709b8a82c5c40895699cee6f"
+    sha256 cellar: :any,                 arm64_sequoia: "a97f40afb65061c86c3d1331ab177ae7d9bafca882b4599d6116123d865b95fc"
+    sha256 cellar: :any,                 arm64_sonoma:  "ec1327ec054ad64a03904040480948120b1bea7bf5162f4cd82fde3770120dfd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fecbae97537e847fb8d9e6b347494607fd3a87a6bf088acd018e0def99ea33f9"
   end
 
   depends_on "node"
