ae0495dcb46e91f3a380823362dfe3264634dd12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-26 23:10:22 +0000

    slack-mcp-server: add 1.1.24 bottle.

--- Formula/s/slack-mcp-server.rb
+++ Formula/s/slack-mcp-server.rb
@@ -6,6 +6,13 @@ class SlackMcpServer < Formula
   license "MIT"
   head "https://github.com/korotovsky/slack-mcp-server.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "91247ff79bb02f573aa2414b8304197fa96b95b1cbcb101789f25dabc5021cab"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5c199a813874bc4e00bb9fb98fd71173786c6aeb3a16e2d9c8ab1e7578958add"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0666437f7838e2d7c9597422c9110db6789b9998a7119926a715abf3b6db1813"
+  end
+
   depends_on "go" => :build
 
   def install
