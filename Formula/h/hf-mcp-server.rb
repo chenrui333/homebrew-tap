bb5bdaf95fab607fc31b274cd0e82a15e2e84c39
Rui Chen <rui@chenrui.dev>
2025-09-26 21:13:30 -0400

    hf-mcp-server 0.2.28 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/h/hf-mcp-server.rb
@@ -0,0 +1,27 @@
+class HfMcpServer < Formula
+  desc "MCP Server for Hugging Face"
+  homepage "https://github.com/evalstate/hf-mcp-server"
+  url "https://registry.npmjs.org/@llmindset/hf-mcp-server/-/hf-mcp-server-0.2.28.tgz"
+  sha256 "e9706cd54acd7d80c1656640662598314432eca847b858571df5ab4dc9d914cc"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    ENV["TRANSPORT"] = "stdio"
+    ENV["DEFAULT_HF_TOKEN"] = "hf_testtoken"
+
+    output_log = testpath/"output.log"
+    pid = spawn bin/"hf-mcp-server", [:out, :err] => output_log.to_s
+    sleep 1
+    assert_match "STDIO transport initialized", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
