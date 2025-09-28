1d16670a5fc7f0527ae3c7396b03aae649d637d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-28 02:49:20 +0000

    iam-convert 0.1.36

--- Formula/i/iam-convert.rb
+++ Formula/i/iam-convert.rb
@@ -1,8 +1,8 @@
 class IamConvert < Formula
   desc "Convert JSON IAM Policies to other formats"
   homepage "https://iam.cloudcopilot.io/tools/iam-convert"
-  url "https://registry.npmjs.org/@cloud-copilot/iam-convert/-/iam-convert-0.1.35.tgz"
-  sha256 "e35061262a2f4d7fb2cfa9ab577ce0569e65b3d150a0a76ef9baf885aae703c2"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-convert/-/iam-convert-0.1.36.tgz"
+  sha256 "11ed81853d8ccad4645cf46f5c08ca709e9b0e1c11bfe28f0e8afdbe9302c7e0"
   license "AGPL-3.0-only"
 
   bottle do
