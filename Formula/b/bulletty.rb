5a4a8597af59f9f5287546d1ffffa9931282e4a0
Rui Chen <rui@chenrui.dev>
2025-09-30 19:32:53 -0400

    bulletty: needs openssl for linux build
    Closes #1815.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/b/bulletty.rb
+++ Formula/b/bulletty.rb
@@ -6,7 +6,12 @@ class Bulletty < Formula
   license "MIT"
   head "https://github.com/CrociDB/bulletty.git", branch: "main"
 
+  depends_on "pkgconf" => :build
   depends_on "rust" => :build
+
+  on_linux do
+    depends_on "openssl@3"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
