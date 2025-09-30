d7afff718a94d21ce969fe28ea57317580c5742c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 06:33:47 +0000

    travelgrunt: update 0.7.0 bottle.

--- Formula/t/travelgrunt.rb
+++ Formula/t/travelgrunt.rb
@@ -8,9 +8,9 @@ class Travelgrunt < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "04e5ad4e9898105207d03e13b4a1c59790c701609638ff400f8f8a5969f7521b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "28c7cb0af0d3f41b4f8105bc118760cf895bedbc6364b5dfb01aef49e16e7e23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0eeca9a7a2c714cae8c24ffd794b9110b7465a2e26cfa4a5b08560e48947354"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6b654f65eaf2bf94bdb667d8757678eb82157e58fbc9749bdac7d46f3c270fe"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "53a682ca425c13a65c10d26dd15b141abe237d95ef4eafbe0dd617f52732f18c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d58313e098341e3a0812b3138af4a3f2aad916df7d964d5f8496827f7f8b54f2"
   end
 
   depends_on "go" => :build
