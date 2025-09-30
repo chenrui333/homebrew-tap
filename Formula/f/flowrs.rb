0b5f9fb661b049519eb875612d2aad3db9d83fa6
Rui Chen <rui@chenrui.dev>
2025-09-30 18:31:28 -0400

    flowrs 0.1.14 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/f/flowrs.rb
@@ -0,0 +1,19 @@
+class Flowrs < Formula
+  desc "TUI application for Apache Airflow"
+  homepage "https://github.com/jvanbuel/flowrs"
+  url "https://github.com/jvanbuel/flowrs/archive/refs/tags/v0.1.14.tar.gz"
+  sha256 "f2cde1dc37dc047af175f03eed85dee68b4446b8cbadd54deb8a6d8d59823cd5"
+  license "MIT"
+  head "https://github.com/jvanbuel/flowrs.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/flowrs --version")
+    assert_match "No servers found in the config file", shell_output("#{bin}/flowrs config list")
+  end
+end
