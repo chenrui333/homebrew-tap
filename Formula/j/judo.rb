a69b4c9eb3ecdb03ba8f573a476765b1991a53af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 23:03:21 +0000

    judo: add 1.1.3 bottle.

--- Formula/j/judo.rb
+++ Formula/j/judo.rb
@@ -6,6 +6,13 @@ class Judo < Formula
   license "MIT"
   head "https://github.com/giacomopiccinini/judo.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4b3f37b5c30aa4e942f2a13049e66219ad24b0a79a9b4f3b49fadbfb1c35264b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "193cd7da925bc3e06436a911a13c259b1faebcdf4853547af5988c49b1701abf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "04242488968bc5c755eaf7037729d02d846843e83a64cffd2ed0926874938861"
+  end
+
   depends_on "rust" => :build
 
   def install
