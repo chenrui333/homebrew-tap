c0d1ecb356f8a5c9240e9b51e4ad7e6aa2ae924d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-09-30 22:53:19 +0000

    flowrs: add 0.1.14 bottle.

--- Formula/f/flowrs.rb
+++ Formula/f/flowrs.rb
@@ -6,6 +6,13 @@ class Flowrs < Formula
   license "MIT"
   head "https://github.com/jvanbuel/flowrs.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bafea6a755a962fca460ee9946034f5ca9392b7e0607adba1496e17f1af04f23"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aab5fbddd2b5da9d9b4ab087d9db44757c8fab6659affa8b5bfb712520594cb9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "37e5651bb2ab629ecf723b2794951b5ffaf4afdec83deaa2afd985e696a8fed4"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
