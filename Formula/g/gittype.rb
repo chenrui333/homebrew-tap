c43ede3fa74c6135f8bca5128d433a08585f733c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-01 00:02:18 +0000

    gittype: add 0.8.0 bottle.

--- Formula/g/gittype.rb
+++ Formula/g/gittype.rb
@@ -6,6 +6,13 @@ class Gittype < Formula
   license "MIT"
   head "https://github.com/unhappychoice/gittype.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a6bed04f6d2b0b26278a25ab92279726cbc4440baefe6900c657db3dbc1b5511"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "85b89d9559891dfeb0465561a9d766b14cc4114ee08f7c087a65b8c67a11cda1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4fdd8c7e6651cd704c9dcf9836a64fe07cd7f547c00a8a6352dfb93e2525062"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
