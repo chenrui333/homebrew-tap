c82cd83c4b0b0aa825e2843a2d0a94edddec0223
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-01 15:08:07 +0000

    aim: add 1.8.6 bottle.

--- Formula/a/aim.rb
+++ Formula/a/aim.rb
@@ -6,6 +6,13 @@ class Aim < Formula
   license "MIT"
   head "https://github.com/mihaigalos/aim.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d88b7c28e9a12ab6a5caf14ae75db4402369ecd52658ee50c03150c78de03b28"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "620a26324ad68e10b1a0f6745f3502d882fc320f3b2ec1386d7cded265fe8bd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f7610add3e929c90810bf41898222f2f8f51ffc947ffb6b3a8d13a52faa92854"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
