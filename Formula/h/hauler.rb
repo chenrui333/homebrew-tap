710e50f690ce595f9c1419440328335bc6c993b7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-03 02:41:59 +0000

    hauler 1.2.6

--- Formula/h/hauler.rb
+++ Formula/h/hauler.rb
@@ -1,8 +1,8 @@
 class Hauler < Formula
   desc "Airgap Swiss Army Knife"
   homepage "https://docs.hauler.dev/docs/intro"
-  url "https://github.com/hauler-dev/hauler/archive/refs/tags/v1.2.5.tar.gz"
-  sha256 "03066f806b02f0565a1c99f2dfe98423824f48b285e0850dc3de4e4dc1aeccc5"
+  url "https://github.com/hauler-dev/hauler/archive/refs/tags/v1.2.6.tar.gz"
+  sha256 "2ca93c4bdb9bd536c5bc08fdca0ec35ccf6db2561ffe2d465ab0041295a91334"
   license "Apache-2.0"
   head "https://github.com/hauler-dev/hauler.git", branch: "main"
 
