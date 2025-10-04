9b21b2782702a1159b8b5b338019ccdcd459ddbd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-04 00:40:33 +0000

    brave-search-mcp-server: update 2.0.26 bottle.

--- Formula/b/brave-search-mcp-server.rb
+++ Formula/b/brave-search-mcp-server.rb
@@ -7,9 +7,9 @@ class BraveSearchMcpServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "63f1f697ebf3d68e55d360a1257384cc66702d1287b29a45db05861bf9edbe4c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9559d260994fc1ffcb35d8a708906836c80d3e0ca6476da44b63b25d57284655"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d182a42ea090c9285fbb1fbe616ab2e66f307dc56d6ae64281b8f354d2d18810"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "22747e8ff3c9f95bb8773a93fbe41742ca09aea431f256051727d90cb1002d04"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4948ad2b2d94ef6823cad85a5b0530b1372a600e3f31bb46c54110342df03ecf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c55707d4494eb8e52a998795e5efa33482a250dbe2278f6ee1b54bb05c9d63f"
   end
 
   depends_on "node"
