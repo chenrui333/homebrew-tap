080b114df15e9b795a0ea4ba122c7d8685f6ca47
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-05 23:35:05 +0000

    hygg: update 0.1.19 bottle.

--- Formula/h/hygg.rb
+++ Formula/h/hygg.rb
@@ -8,9 +8,11 @@ class Hygg < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9e7033bb305f4d3119cb465ed7b89616c1348442cf0ada5af92f11169d5aa14d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62188dfee58bd7f29bb7ec12c28a4af7bcdc09b31b9ea97b6de51b5ec87ba1bb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f92416fe55928ac40babcabc9bab7eaef397ea278e80b26eea9cb37918ab5537"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "458622fe4d29e2deae57ebf8fc7cdfaf79f2f0f50e6871778358d0515636af6f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2d4ea5cd556bdf6c7812fa5c964a2e254745aebe730953d455ae0ac95d92bc3d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c90ce560983e0744153961c41ddb8bc6d5ceabfb82da7789ec6a7b7b0188a648"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d8f29d8687e1e74d04a3f314663586240ef4985622b0757fc0d973ab68165138"
   end
 
   depends_on "rust" => :build
