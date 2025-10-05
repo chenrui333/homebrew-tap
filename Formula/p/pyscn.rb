977132e8e3bbb713d8cc719e8544d3e7570e76e6
Rui Chen <rui@chenrui.dev>
2025-10-05 16:25:03 -0400

    pyscn 1.0.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/pyscn.rb
@@ -0,0 +1,36 @@
+class Pyscn < Formula
+  desc "Intelligent Python Code Quality Analyzer"
+  homepage "https://github.com/ludo-technologies/pyscn"
+  url "https://github.com/ludo-technologies/pyscn/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "938efa562ef23f5a15117316e8ae05c94421e3b50275ccd5798a8c42d25bdd73"
+  license "MIT"
+  head "https://github.com/ludo-technologies/pyscn.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/ludo-technologies/pyscn/internal/version.Version=#{version}
+      -X github.com/ludo-technologies/pyscn/internal/version.Commit=#{tap.user}
+      -X github.com/ludo-technologies/pyscn/internal/version.Date=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/pyscn"
+
+    generate_completions_from_executable(bin/"pyscn", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pyscn version")
+
+    (testpath/"test.py").write <<~PY
+      def add(a, b):
+          return a + b
+
+      print(add(2, 3))
+    PY
+
+    output = shell_output("#{bin}/pyscn analyze #{testpath}/test.py 2>&1")
+    assert_match "Health Score: 98/100 (Grade: A)", output
+  end
+end
