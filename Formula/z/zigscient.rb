66545b1c6f1bf557b409717f2b7d8b3770cf4952
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-05 15:43:27 +0000

    zigscient: update 0.14.15 bottle.

--- Formula/z/zigscient.rb
+++ Formula/z/zigscient.rb
@@ -7,10 +7,10 @@ class Zigscient < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ccb030bae951bdfc010c187fdd6a962229cf1b7cd7d9f354f5f3c917fa8db47f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3e01cd59adf44eb50767a86afefb91ef7e6d91dff100c39235207c842094b5c1"
-    sha256 cellar: :any_skip_relocation, ventura:       "25f181cc4aa0dc4518deca96a369cf85c055310b919ff3cf6436153d3d40c133"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9fef0474aee30c32cb4dc3a77a7e882d9c7e49792b76580a0154e5e62abc46a8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8d3f0e23aec22feb5eacc03b18d62ab65ac0e742a4747e9365e7f60f409b5854"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8d7c611cdafcc8dbdf930863af88596ce398010d91f0ea52c5f3aea489722a80"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "599fe46013efaa290fb55f160db05fe5779e54946a3ba83a9e246ef50b9cb701"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6b470c4aa8ec82df9cdec15820dcbb26e3a0fc0fb3f10219a9db9b669237a907"
   end
 
   depends_on "zig@0.14" => :build
