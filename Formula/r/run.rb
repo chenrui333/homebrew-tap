c5171c52984ad47263a374d5d0d429a21e1818e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-05 20:45:49 +0000

    run: add 0.1.1 bottle.

--- Formula/r/run.rb
+++ Formula/r/run.rb
@@ -6,6 +6,14 @@ class Run < Formula
   license "Apache-2.0"
   head "https://github.com/Esubaalew/run.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c09101cd9545d6d33cf04e7f62bbc4b9fb200a9f0c45736bc1fdb241bb4db790"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "15d4a044f9189b03239f02e2bd697038c854d564eb709f39fc143f577e0e6e78"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ac95a034bc8dae0d26bc9db3e2c2ea45de4e7f8f5af069241577e7c4b20211af"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "73767818c17903ba7e225e3db5a1f37a6f2f76e37e0d7fbcd3fdb656a8b9b380"
+  end
+
   depends_on "rust" => :build
 
   def install
