b48eadd21a6e902e8d48da8925327b518f45f2dd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-05 05:20:21 +0000

    wassette: add 0.3.0 bottle.

--- Formula/w/wassette.rb
+++ Formula/w/wassette.rb
@@ -6,6 +6,14 @@ class Wassette < Formula
   license "MIT"
   head "https://github.com/microsoft/wassette.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "039aeeb7f8125fbe1d6d2a3c646b72c29c967cfe057cbbb4e00a5631ebb2742d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3e693a2fa94e618bf8c115e591924c770a2c36234e5d2af196d1eb0ccc616f1b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7311116aba8769c1f30bfd2f8c95f2bd8d4ff6e56bb7e092809c745896232606"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ecfe1b4c8f33de429847c81eceddd5c86432f40b6d4f926d246978517999562c"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
