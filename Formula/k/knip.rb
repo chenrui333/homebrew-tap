deac85520c422b0dbf92e563561d25fa50a0ddb5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 08:34:05 +0000

    knip: update 5.64.2 bottle.

--- Formula/k/knip.rb
+++ Formula/k/knip.rb
@@ -7,9 +7,10 @@ class Knip < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_sequoia: "384d8c73a12f0ef368050b6296b0573193d867619340b3fda8f676225cd9e61e"
-    sha256 cellar: :any,                 arm64_sonoma:  "75a9863fff2ca91e32b7f921a02ed049e1cd0f994ef615a5c6a95e3521fc592a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9721335578ebdd39b600e6fd4a0f2943d580259406f954279fe93f84e2b3709f"
+    sha256 cellar: :any,                 arm64_tahoe:   "6a74379866451f3e95057120aeba8420d5c691a8c565b607d74f69861dcb1947"
+    sha256 cellar: :any,                 arm64_sequoia: "a58f1adb2cb3fdcc7d0b08aa70fc01812380bb9c4d5e40eac24454149c3c7125"
+    sha256 cellar: :any,                 arm64_sonoma:  "abd126204484b4d7f5b880b51aa9431251bbeea0095f06eafdc5d1dd99b5ffdd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "870c0f2940a18bf8c37d3ed841a060aabb1836a491b1519181a81740ca225c9f"
   end
 
   depends_on "node"
