f2f2704f0e81d50e52d61111bfb73bab88ef2a68
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 14:23:54 +0000

    bulletty 0.1.8
    Closes #1929.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/b/bulletty.rb
+++ Formula/b/bulletty.rb
@@ -1,8 +1,8 @@
 class Bulletty < Formula
   desc "Pretty feed reader (ATOM/RSS) that stores articles in Markdown files"
   homepage "https://bulletty.croci.dev/"
-  url "https://github.com/CrociDB/bulletty/archive/refs/tags/v0.1.7.tar.gz"
-  sha256 "20905059d056d0b347b862c6c62ea58d38ec8e45297ffc3b4321929eb23706b1"
+  url "https://github.com/CrociDB/bulletty/archive/refs/tags/v0.1.8.tar.gz"
+  sha256 "5e53ff81665c179485ceb778be41c456c929d0a60bd436e84461b4cac1597334"
   license "MIT"
   head "https://github.com/CrociDB/bulletty.git", branch: "main"
 
