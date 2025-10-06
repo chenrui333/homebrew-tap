a63666e4a24fa5020028f0da7293f72821a06892
Rui Chen <rui@chenrui.dev>
2025-10-06 00:41:56 -0400

    rawhide 3.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/r/rawhide.rb
@@ -0,0 +1,24 @@
+class Rawhide < Formula
+  desc "Find files using pretty C expressions"
+  homepage "https://raf.org/rawhide/"
+  url "https://github.com/raforg/rawhide/releases/download/v3.3/rawhide-3.3.tar.gz"
+  sha256 "a51a1360ce4763838b29a548a8e49dcf47a93a922bf1df05c8b7d50ecb9ab09d"
+  license "GPL-3.0-or-later"
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+
+    inreplace "Makefile" do |s|
+      s.gsub!(/^PREFIX\s*=\s*.*$/, "PREFIX = #{prefix}")
+      s.gsub!(/^ETCDIR\s*=\s*.*$/, "ETCDIR = #{etc}")
+    end
+
+    system "make"
+    system "make", "install-bin"
+    system "make", "install-man"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/rh --version")
+  end
+end
