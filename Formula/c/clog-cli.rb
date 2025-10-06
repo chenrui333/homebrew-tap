242245a862cdba04b55d4d4a5a0112455bb9404d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 04:04:19 +0000

    clog-cli: add 0.10.0 bottle.

--- Formula/c/clog-cli.rb
+++ Formula/c/clog-cli.rb
@@ -5,6 +5,14 @@ class ClogCli < Formula
   sha256 "9508313c4df5a08a63a9bb8718c2d72125f9dec0cec350532d7b770083e70a71"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7d54471ab1623e8c3733f3809734e000687b3bde8b150a2a63e1e038067c8b87"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f40006c4b88ec190e23a2631a51522d5572c7bf2929a4385a2e55f842c458e99"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b56a61f5c80c9024c715b101a6e4f0d791146b0cc5e1742108d7521b55724e31"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ab5c928d4874973cfbc9e840b05bc1bde4a6f55b2ce76c57b714d52a5aef59c0"
+  end
+
   depends_on "rust" => :build
 
   def install
