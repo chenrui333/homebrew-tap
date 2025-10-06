7c885ebab81209ea3610cd2b02ce65c0baa1ba6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 04:47:24 +0000

    get-port-cli: add 3.0.0 bottle.

--- Formula/g/get-port-cli.rb
+++ Formula/g/get-port-cli.rb
@@ -5,6 +5,14 @@ class GetPortCli < Formula
   sha256 "42bda9c04261747640267510fdaaeffc6cd712a3c54d675e43dc932cc0a0222f"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6190a1ea582e56aab0a819d18d819985cfc5eec95b6b63f3d1fa1568bc1f29cc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "56d5d03308b8c19f6dd7ff779e586b90f659dbacd5e2c4b09f1508c066069c09"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "60d8a1abfcb91ebe61d41372fd7d85345d42c5d7dd34ec47770e02cfa3ca94b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0e61f926d735d4d0d81c1e2e224d38d45af877fa196f208c2a4849518019757b"
+  end
+
   depends_on "node"
 
   def install
