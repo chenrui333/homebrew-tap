c21f4572e5934970fb96d8434648ba645fecbcd0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 14:39:46 +0000

    stree: add 0.0.21 bottle.

--- Formula/s/stree.rb
+++ Formula/s/stree.rb
@@ -6,6 +6,14 @@ class Stree < Formula
   license "MIT"
   head "https://github.com/orangekame3/stree.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "62103d1d5feec28603f6529e537827f00b9980c53f0ce55e71329427cea8d398"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a9fe2c200e7756432f704bccfd3db5e7db6d4e74ee18574b2938d205e80afed4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6699b3806a5ba37ceb361383bc3d67e94fe208fcaf6a6752868ff344de0718a8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bc519645ed53abe7d7f57f59ec19f9d4f31f66efad2ae7361a7b6a7a920f989a"
+  end
+
   depends_on "go" => :build
 
   def install
