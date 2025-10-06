79c93365f87013b9632dc65ec81ee085426a7092
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 10:40:33 +0000

    tsuki: update 0.2.0 bottle.

--- Formula/t/tsuki.rb
+++ Formula/t/tsuki.rb
@@ -8,9 +8,10 @@ class Tsuki < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "29fdb17f63afb67c672ea8e6d6ae0ac4c30e3fbac8b352c06d7b0fcba7d65b83"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3a1b3c37dbfc62b8f4ba5f01bf81deee078806e1e2654f0748f52d1e8417b14d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "270a1ab8fa0292cfa3f317f581d5b5d77415376be7a464253b7fd28b08f613b5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "46770c1363af0d33594f701f60e2e25b6e883118a4de3010ce6e9261e337b08e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a3a4b5dde84864f2a1ce1dcadf4261e54540be91a30c2cafa24f2302d09889a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "99a5791cce677329455f42e30c5a2ceb4e3149ebd2144363a219880b7a99660f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b23ab9c4924d05afe232e1bdc456eef1a1b2df8275c7dc39f1b600e3a8531f6f"
   end
 
   depends_on "rust" => [:build, :test]
