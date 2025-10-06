e082b480c35ac56f31b4873380458cee18b53cf2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-06 14:18:44 +0000

    av: add 0.1.12 bottle.

--- Formula/a/av.rb
+++ Formula/a/av.rb
@@ -6,6 +6,14 @@ class Av < Formula
   license "MIT"
   head "https://github.com/aviator-co/av.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4671c87c9e6557f5eb4ef3f437714c996a4996a4f112e1740f7eccea4d8790c7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0b22b9828279957d43aee093f9086ee476b3a65b16e4ce6da77cceb148a194c6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc2a648121ae252adc9109ab07efa146fffa68d7de5ecc9c3f0ea8da5dd7cd5e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e46bdb981e7dd66effa244b4dcf916943f0d3343a34f9f491cbadf3a4a136a32"
+  end
+
   depends_on "go" => :build
 
   def install
