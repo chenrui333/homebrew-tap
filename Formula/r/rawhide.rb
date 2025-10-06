33145ce11673756d2c98393aa0793ca6f58a6183
Rui Chen <rui@chenrui.dev>
2025-10-06 10:06:57 -0400

    rawhide: needs pcre2 for linux build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/rawhide.rb
+++ Formula/r/rawhide.rb
@@ -5,6 +5,10 @@ class Rawhide < Formula
   sha256 "a51a1360ce4763838b29a548a8e49dcf47a93a922bf1df05c8b7d50ecb9ab09d"
   license "GPL-3.0-or-later"
 
+  on_linux do
+    depends_on "pcre2"
+  end
+
   def install
     system "./configure", "--prefix=#{prefix}"
 
