a1f7bf6a91b5d3cab998d8f0e6a71a6566451f54
Rui Chen <rui@chenrui.dev>
2025-10-08 11:30:37 -0400

    awless: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/awless.rb
+++ Formula/a/awless.rb
@@ -24,6 +24,8 @@ class Awless < Formula
   end
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     system "go", "build", *std_go_args(ldflags: "-s -w"), "-mod=readonly"
   end
 
