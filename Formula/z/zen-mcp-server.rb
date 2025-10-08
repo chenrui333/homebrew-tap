0deb4c4c4aac37d30ee63b457fb8b1b975a37cbe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-08 04:28:22 +0000

    zen-mcp-server 8.0.2

--- Formula/z/zen-mcp-server.rb
+++ Formula/z/zen-mcp-server.rb
@@ -3,8 +3,8 @@ class ZenMcpServer < Formula
 
   desc "Many Workflows. One Context"
   homepage "https://github.com/dagger/container-use"
-  url "https://github.com/BeehiveInnovations/zen-mcp-server/releases/download/v8.0.0/zen_mcp_server-8.0.0.tar.gz"
-  sha256 "6b35b2c852d0b2ff61a83048d24e9e039669d4b8f3b413ae1822e51603d9e309"
+  url "https://github.com/BeehiveInnovations/zen-mcp-server/releases/download/v8.0.2/zen_mcp_server-8.0.2.tar.gz"
+  sha256 "389054fb781b8459e88f0101770ab6c87a76634e9896961bab98b64d2113e034"
   license "Apache-2.0"
 
   bottle do
