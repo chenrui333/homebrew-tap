426acb92bece84cecc93f0aa5c6608da817b2dfb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-08 04:53:26 +0000

    hello: update 2.12.2 bottle.

--- Formula/h/hello.rb
+++ Formula/h/hello.rb
@@ -7,10 +7,12 @@ class Hello < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c74d64c261b96e420941288d5544d61b09ea7ecc57ee2a8407e8a33b29ee1af8"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "637b1501231f7b87fc57c702c1d71b2d05164497c97234009c759d29215b8e12"
-    sha256 cellar: :any_skip_relocation, ventura:       "06c3f733cf558c777668187e474ca623a9f78a676b9dc01937bdfb3eb3fb6cef"
-    sha256                               x86_64_linux:  "f7ef8c930676eb016c3dfca282b827def727f4753e794d31e89aa68f356ee859"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b6b2b5f2873475045a47632fbee2cb76779567457d654b78900b57700d48d5f8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d95e1acbb0ecf3a5ce5f2f7f4eea2eef482779faded5769b4caa6682934bfc95"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "344c7911711052d344d00a4694801430a61d16c8f88bb9ce6501b6bda3fe8927"
+    sha256 cellar: :any_skip_relocation, sequoia:       "3612597f201149e81fd079cf0bf0b8edda0fec1d0ca429bdf0d040d397fde304"
+    sha256                               arm64_linux:   "18e1c66df070b89a338200ffd212eacb416eef1956a9943e834fd913091dfe83"
+    sha256                               x86_64_linux:  "6db5b4fa760fb87b88b98022e0e1404d13808c60c88ab4877cbbd43ec59bec9d"
   end
 
   def install
