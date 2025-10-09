cde0b238f4a3663607e86c38c181d0ea8d25232b
Rui Chen <rui@chenrui.dev>
2025-10-08 23:48:24 -0400

    tpm: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/tpm.rb
+++ Formula/t/tpm.rb
@@ -19,6 +19,8 @@ class Tpm < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
     generate_completions_from_executable(bin/"tpm", "completion")
