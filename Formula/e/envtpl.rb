e39968ec24d42256e5b6a8db643a89bddce95a68
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-09 00:22:30 +0000

    envtpl: update 2.0.5_1 bottle.

--- Formula/e/envtpl.rb
+++ Formula/e/envtpl.rb
@@ -9,10 +9,11 @@ class Envtpl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ff1339f6c830fe082d9a00c93e56471e04a282eeb731c3f1af5819e4cb0f651"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "631ac3be37626fcb7585be22d899a698e26c858950dd3f9028ca8a79b0a5669b"
-    sha256 cellar: :any_skip_relocation, ventura:       "309d5c82c62099c93d4dd07e436039353417f504c1291cb152d94b79dbdffb4c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "30cb6947c567984dbde034e779b8b456374e00183f012add816fb95b41758327"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6025ef60071f6f4a00fde5c2228472bd51059e6d77095f024656ce2686713e19"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6025ef60071f6f4a00fde5c2228472bd51059e6d77095f024656ce2686713e19"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6025ef60071f6f4a00fde5c2228472bd51059e6d77095f024656ce2686713e19"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "89128c77763d1382c29c090b96995d4fcfee0fa17b151acb629db2446972c7ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "987f58362319dccf2c05866fade24a032c2bdb766fa8f86ba80b5844ed5c3402"
   end
 
   depends_on "go" => :build
