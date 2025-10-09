dfd3b9be6454ba1af025ee1c0a4b3cf296e1b99b
Rui Chen <rui@chenrui.dev>
2025-10-08 23:55:17 -0400

    klepto: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/klepto.rb
+++ Formula/k/klepto.rb
@@ -18,6 +18,8 @@ class Klepto < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     ldflags = "-s -w -X github.com/hellofresh/klepto/cmd.version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
