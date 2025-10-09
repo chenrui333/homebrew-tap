8e24dfae09381c208b39742dda0feb2fc2b96525
Rui Chen <rui@chenrui.dev>
2025-10-08 23:55:16 -0400

    horusec: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/horusec.rb
+++ Formula/h/horusec.rb
@@ -18,6 +18,8 @@ class Horusec < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     ldflags = %W[
       -s -w
       -X github.com/ZupIT/horusec/cmd/app/version.Version=#{version}
