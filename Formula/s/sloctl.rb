9cfcd3b142150b7b1d050293477e8d7349363cdd
Rui Chen <rui@chenrui.dev>
2025-10-08 23:48:22 -0400

    sloctl: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/sloctl.rb
+++ Formula/s/sloctl.rb
@@ -17,6 +17,8 @@ class Sloctl < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     ldflags = %W[
       -s -w
       -X github.com/nobl9/sloctl/internal.BuildVersion=#{version}
