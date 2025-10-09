c13287d0e05d321aa219d5deac517f40eb6be8f4
Rui Chen <rui@chenrui.dev>
2025-10-08 23:55:17 -0400

    kube2pulumi: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/kube2pulumi.rb
+++ Formula/k/kube2pulumi.rb
@@ -18,6 +18,8 @@ class Kube2pulumi < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     ldflags = %W[
       -s -w
       -X github.com/pulumi/kube2pulumi/pkg/version.Version=#{version}
