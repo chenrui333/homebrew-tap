bac36072aa42260ae2990c5d34ea4461de814dd1
Rui Chen <rui@chenrui.dev>
2025-10-08 23:48:24 -0400

    vgo: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/v/vgo.rb
+++ Formula/v/vgo.rb
@@ -18,6 +18,8 @@ class Vgo < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
     generate_completions_from_executable(bin/"vgo", "completion")
