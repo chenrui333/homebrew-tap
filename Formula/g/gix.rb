01ec0134f366f602b7bba5a113a42814b1de89b6
Rui Chen <rui@chenrui.dev>
2025-10-08 23:55:15 -0400

    gix: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gix.rb
+++ Formula/g/gix.rb
@@ -17,6 +17,8 @@ class Gix < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     ldflags = "-s -w -X github.com/ademajagon/gix/cmd.version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
