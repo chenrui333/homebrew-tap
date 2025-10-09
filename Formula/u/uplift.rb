e189700836640f8ee5c61c3dddabc611c7ab8afd
Rui Chen <rui@chenrui.dev>
2025-10-08 23:48:24 -0400

    uplift: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/u/uplift.rb
+++ Formula/u/uplift.rb
@@ -18,6 +18,8 @@ class Uplift < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     ldflags = %W[
       -s -w
       -X github.com/gembaadvantage/uplift/internal/version.version=#{version}
