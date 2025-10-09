fd988ac7317e39910c72047247cc53b99aebe271
Rui Chen <rui@chenrui.dev>
2025-10-08 23:48:22 -0400

    spok: disable cgo for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/spok.rb
+++ Formula/s/spok.rb
@@ -23,6 +23,8 @@ class Spok < Formula
   depends_on "go" => :build
 
   def install
+    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
+
     ldflags = %W[
       -s -w
       -X go.followtheprocess.codes/spok/cli/cmd.version=#{version}
