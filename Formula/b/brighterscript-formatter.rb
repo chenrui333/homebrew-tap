43a0625bf610fe9762943994d3693de92ff5ce93
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-10 18:25:03 +0000

    brighterscript-formatter 1.7.19

--- Formula/b/brighterscript-formatter.rb
+++ Formula/b/brighterscript-formatter.rb
@@ -1,8 +1,8 @@
 class BrighterscriptFormatter < Formula
   desc "Code formatter for BrighterScript (and BrightScript)"
   homepage "https://github.com/rokucommunity/brighterscript-formatter"
-  url "https://registry.npmjs.org/brighterscript-formatter/-/brighterscript-formatter-1.7.18.tgz"
-  sha256 "5f84c67f03d8a0606f1ca50bf2c2d24372b55a428294555010ab9bbc03f3b65c"
+  url "https://registry.npmjs.org/brighterscript-formatter/-/brighterscript-formatter-1.7.19.tgz"
+  sha256 "57329cad7d8692b230e9702b7099926f88d1b2b087d30aefb02d744f1266683c"
   license "MIT"
 
   bottle do
