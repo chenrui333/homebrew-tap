2d284049c6b43b530f3e30a614ef6eac97156be5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-10 18:35:04 +0000

    brighterscript-formatter: update 1.7.19 bottle.

--- Formula/b/brighterscript-formatter.rb
+++ Formula/b/brighterscript-formatter.rb
@@ -7,10 +7,7 @@ class BrighterscriptFormatter < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c53dcd7fd72b9a3a24a87ba99453c2d302c5f571900e55b72b89cc6fe7dd5944"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "039fc7ec568ddd3242855dff35759b1552327465de304604216b3560ac7bdac3"
-    sha256 cellar: :any_skip_relocation, ventura:       "1dda2de35a0d7e6eb96fe63f5e6839f1df5df461e6b27339d859d5a891d84391"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b51ed6e14ee0174c2d9fe4e50eb5c1af21d9739fd9923df012d90456f9399268"
+    sha256 cellar: :any_skip_relocation, all: "3a0f9a9ed89421e1ab40caff83bf1c42c480da25b582514579f76ef525559223"
   end
 
   depends_on "node"
