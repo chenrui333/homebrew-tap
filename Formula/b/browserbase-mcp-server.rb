bec42d9c3fb21abca1f0678d4d827a0bf754b49e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-13 22:23:42 +0000

    browserbase-mcp-server 2.1.3

--- Formula/b/browserbase-mcp-server.rb
+++ Formula/b/browserbase-mcp-server.rb
@@ -1,8 +1,8 @@
 class BrowserbaseMcpServer < Formula
   desc "MCP server for AI web browser automation using Browserbase and Stagehand"
   homepage "https://github.com/browserbase/mcp-server-browserbase"
-  url "https://registry.npmjs.org/@browserbasehq/mcp-server-browserbase/-/mcp-server-browserbase-2.1.2.tgz"
-  sha256 "cc7b64c781947425865b81d70a03e2a9d826c6c0dd4740de5b241ad0b7a40357"
+  url "https://registry.npmjs.org/@browserbasehq/mcp-server-browserbase/-/mcp-server-browserbase-2.1.3.tgz"
+  sha256 "35a47d60a656b044512560914a4504318a8e13195a337d7fce54d9962f403ccc"
   license "Apache-2.0"
 
   bottle do
