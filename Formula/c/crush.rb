badff79f7f1645ab1f51289ea99e8e4a6b5917ea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-14 20:24:39 +0000

    crush 0.11.1

--- Formula/c/crush.rb
+++ Formula/c/crush.rb
@@ -1,10 +1,9 @@
 class Crush < Formula
   desc "Glamorous AI coding agent for your favorite terminal"
   homepage "https://github.com/charmbracelet/crush"
-  url "https://github.com/charmbracelet/crush/archive/refs/tags/v0.10.4.tar.gz"
-  sha256 "24074bbf98871966061ca364458d6dfa0c319dcd7910437cc3dcc9911e90113b"
+  url "https://github.com/charmbracelet/crush/archive/refs/tags/v0.11.1.tar.gz"
+  sha256 "be792b90bdf8a16fc31f7d9be6cd2608220e0b9bf2728a28494f14cafd1d990d"
   # license "FSL-1.1-MIT"
-  revision 1
   head "https://github.com/charmbracelet/crush.git", branch: "main"
 
   bottle do
