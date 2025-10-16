0b8135f224bd2a1060a9bd385a5efe03b3c13abd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-16 12:38:45 +0000

    mongodb-mcp-server: update 1.1.0 bottle.

--- Formula/m/mongodb-mcp-server.rb
+++ Formula/m/mongodb-mcp-server.rb
@@ -7,10 +7,11 @@ class MongodbMcpServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "ffc6287c4ab11846e69a9696c0685a309840e21dfc7fccc6a8c1adf004faca99"
-    sha256                               arm64_sequoia: "74aa066976bd73d849cce0bae79c07952db9b6c0f9f98b46aeba0babe6af19ef"
-    sha256                               arm64_sonoma:  "8e6bca1a76323248f71092d45d55c1a5bd13db0fd2670ae8ea6826cf28041752"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ecfc6823a82f24bc368f9e512bc23ad55d37f7ca6db51edc178197498f894b33"
+    sha256                               arm64_tahoe:   "844b9614b3604ce8e2d995dc759a56465d0c1dd215d7308be8557bdb8fadd09f"
+    sha256                               arm64_sequoia: "3d4760364b870ff160442d74dc73042cc5f056211772b5b20b90262aa982ae7f"
+    sha256                               arm64_sonoma:  "043824e78796dfcc9d84a2b7684595bb3a64d33e8b4e0ab2206fd4317c0aadd1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3d3724545bb772d525e666d01d34d57a2bfe67b5b3701ca74b302cf2370c8cae"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1fe4a6db2b91abdba0c86e11b6dce587631d4b30d7b6409575cbbdb7f776b817"
   end
 
   depends_on "node"
