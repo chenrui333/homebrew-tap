058c4ab38ff09c2c251df708d0c2a89ca4b0ffc1
Rui Chen <rui@chenrui.dev>
2025-10-16 18:53:15 -0400

    autotag: Remove CGO_ENABLED workaround for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/autotag.rb
+++ Formula/a/autotag.rb
@@ -19,8 +19,6 @@ class Autotag < Formula
   depends_on "go" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
-
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./autotag"
   end
 
