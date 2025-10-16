51ec23ba0b762c4e95cbaceeef24484b2b6ba790
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-16 23:32:42 +0000

    hf-mcp-server: update 0.2.32 bottle.

--- Formula/h/hf-mcp-server.rb
+++ Formula/h/hf-mcp-server.rb
@@ -7,11 +7,11 @@ class HfMcpServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "cd391c43c5e92bb6c017c867e4ac28ede63e6d541b25929dcca9053d1d1d982b"
-    sha256 cellar: :any,                 arm64_sequoia: "1bbea151b472c6fbe6076d171eb86af455c4e2ae3e16aa6f1385fd0e0e84d5e9"
-    sha256 cellar: :any,                 arm64_sonoma:  "1bbea151b472c6fbe6076d171eb86af455c4e2ae3e16aa6f1385fd0e0e84d5e9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "5db99d5cf0eb7ce4484b2feb5a3167d9fe36ec022c7e77372c24b2e0d098ca97"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b4288c953ae9dd3487bb72a866107e394e2bbc23a99e1aba6dd25957a1834609"
+    sha256 cellar: :any,                 arm64_tahoe:   "ef23969e089be9d2866185a141698c4cfb08f19f5229e1be5a5de5233f1497ea"
+    sha256 cellar: :any,                 arm64_sequoia: "88450d15868861ebdf2ff872db38675dbdf8280c4962e71a20b0aee37834ca7d"
+    sha256 cellar: :any,                 arm64_sonoma:  "88450d15868861ebdf2ff872db38675dbdf8280c4962e71a20b0aee37834ca7d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7137d1ab9942e223f877160734954054d913586575a245a8db1b811103c7cffb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bd330611282ba5aaa491e7d8b9b7f12619e38caee3a1819a5e301e372e3d5cc3"
   end
 
   depends_on "node"
