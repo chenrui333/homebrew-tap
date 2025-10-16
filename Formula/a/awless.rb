80a10375867238d686b3fff56e582d565d6ff06d
Rui Chen <rui@chenrui.dev>
2025-10-16 18:53:15 -0400

    awless: Remove CGO_ENABLED workaround for linux arm build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/a/awless.rb
+++ Formula/a/awless.rb
@@ -25,8 +25,6 @@ class Awless < Formula
   end
 
   def install
-    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
-
     system "go", "build", *std_go_args(ldflags: "-s -w"), "-mod=readonly"
   end
 
