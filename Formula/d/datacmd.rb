1e2c7a98555be251267f06e8aafe1a5ce7d14082
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 23:36:22 +0000

    datacmd: update 0.0.3.1_1 bottle.

--- Formula/d/datacmd.rb
+++ Formula/d/datacmd.rb
@@ -9,11 +9,12 @@ class Datacmd < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9f1f08c3c942e167642efd382542487fb8671d37ec40cde54dc0242cd1d70da6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1978aa5c6acff5167527f5c148c39f0ef14cac5d80730b36612daa575f706e0d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "88cea9dd3e34045563194ebbb4deffee40f185f45ef04aad2e267e1afa16d7bb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "915b7c142f21782fed51f16d7402665fa21f9457f422cd8bd5837d3e6428dc72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aad098cdecdec9a87974a3419999d0286cbf685f75cd0e1c53d28b7e020eda58"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bccaa631915e1fcfe34722002052465f19bd81479591333f926f7f2e4d406d1a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0224d203b385351957c9b7bec4414351761d2b7c8a2f3dd9f3664139e08f5731"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ee6bce9b575a026302c2315ca972d1148533f0beb61809cd0aaee0888ffbb1a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "636cff30e12cff6f3186af1930752446d59ed859078678e844282dbc3ae98629"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4a292a2b95f9ff97f715947a910aa739efd1bb7cd285b23c6a758c7ac7baeaa7"
   end
 
   depends_on "go" => :build
