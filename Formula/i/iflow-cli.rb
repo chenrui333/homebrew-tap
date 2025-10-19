e0c97a83d3b3f53ced6fd394e02b087bf1f55809
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 21:36:29 +0000

    iflow-cli: add 0.3.5 bottle.

--- Formula/i/iflow-cli.rb
+++ Formula/i/iflow-cli.rb
@@ -5,6 +5,15 @@ class IflowCli < Formula
   sha256 "b3cb6878bfc5c6912a6a4375fba5c38bc0e2e289f2b44d847b0b7b0fd3260098"
   license "Apache-2.0" # derived from LICENSE, but no source code for the project in https://github.com/iflow-ai/iflow-cli
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_tahoe:   "6b4485e59b04b8a455c9e5fee1e8f5e4f11b46716280ead90df4e27259436d01"
+    sha256                               arm64_sequoia: "3912b01d72efae08540ea6ad3b06d38b320e382f7121b596d0450f5f6cf768fe"
+    sha256                               arm64_sonoma:  "0a8e127ec2ecd56acc918a48ccd73f51dcf12db928cbad4138133a086f36a189"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "37f28a1b71d0b28207b4280815601dea35fb7cd4d27d16ee8c07316762379681"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e5bac34a788879c22b29747fcb24f35485c7037a2d680d588bb288e9f8c3963"
+  end
+
   depends_on "node"
 
   def install
