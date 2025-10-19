b77f84a4c7d2dff82e1df6417a58a01da4c1e40e
Rui Chen <rui@chenrui.dev>
2025-10-19 18:19:06 -0400

    container2wasm: Remove CGO_ENABLED workaround for linux arm build
    Closes #2154.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/container2wasm.rb
+++ Formula/c/container2wasm.rb
@@ -19,8 +19,6 @@ class Container2wasm < Formula
   depends_on "go" => :build
 
   def install
-    ENV["CGO_ENABLED"] = "0" if OS.linux? && Hardware::CPU.arm?
-
     ldflags = %W[
       -s -w
       -X github.com/ktock/container2wasm/version.Version=#{version}
