2168caab719753cca17e8fea994e6c3d9212ab14
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 23:29:39 +0000

    bin-fq: update 0.15.1 bottle.

--- Formula/b/bin-fq.rb
+++ Formula/b/bin-fq.rb
@@ -8,11 +8,12 @@ class BinFq < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0e10571e06f0676cdc614302172b87a2562e061f444238d6712d57df724a6f25"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0e10571e06f0676cdc614302172b87a2562e061f444238d6712d57df724a6f25"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0e10571e06f0676cdc614302172b87a2562e061f444238d6712d57df724a6f25"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6c08a74799531419d24c5e09815203ddc882cb47b06c59f6f61209da3ea6256f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f411a4b2f1fa0eff803912a1d8f6e09ca7c1d8f5056fc68d7ee3625c5e53929"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "87bec6a5b897bc083684c779e6726a1aa6aea71d6b119fef69caf2e7c303413c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "87bec6a5b897bc083684c779e6726a1aa6aea71d6b119fef69caf2e7c303413c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "87bec6a5b897bc083684c779e6726a1aa6aea71d6b119fef69caf2e7c303413c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3cc0d463d88c7b4b2268c80fa8eb6c5518eb897a749dcfab4144f72c4a05e24c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3ff8ec3c9ac09920c68cbaa20d6a6aa9fd68a63f7abeaa4951d57bfc7a9bf45c"
   end
 
   depends_on "go" => :build
