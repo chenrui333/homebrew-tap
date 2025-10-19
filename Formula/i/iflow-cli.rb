11f7b4e0fcb6aebabcf7af81bbb740bcdc707b65
Rui Chen <rui@chenrui.dev>
2025-10-19 17:10:46 -0400

    iflow-cli 0.3.5 (new formula)
    Closes #2130.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/i/iflow-cli.rb
@@ -0,0 +1,23 @@
+class IflowCli < Formula
+  desc "AI assistant that runs directly in your terminal"
+  homepage "https://cli.iflow.cn/"
+  url "https://registry.npmjs.org/@iflow-ai/iflow-cli/-/iflow-cli-0.3.5.tgz"
+  sha256 "b3cb6878bfc5c6912a6a4375fba5c38bc0e2e289f2b44d847b0b7b0fd3260098"
+  license "Apache-2.0" # derived from LICENSE, but no source code for the project in https://github.com/iflow-ai/iflow-cli
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+
+    # Remove incompatible pre-built binaries
+    ripgrep_vendor_dir = libexec/"lib/node_modules/@iflow-ai/iflow-cli/vendors/ripgrep"
+    rm_r(ripgrep_vendor_dir)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/iflow --version")
+    assert_match "No local commands found", shell_output("#{bin}/iflow commands list")
+  end
+end
