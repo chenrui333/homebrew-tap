f672a52cafb55323e4b3aa57ec2ee4ab921e4d6b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-19 22:04:19 +0000

    adbtuifm: update 0.5.8_1 bottle.

--- Formula/a/adbtuifm.rb
+++ Formula/a/adbtuifm.rb
@@ -9,11 +9,12 @@ class Adbtuifm < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "45e4ee75df4fd3eaad37e1b9efef474e11c0c6d3e66ff582212da6fb3761ce30"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45e4ee75df4fd3eaad37e1b9efef474e11c0c6d3e66ff582212da6fb3761ce30"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "45e4ee75df4fd3eaad37e1b9efef474e11c0c6d3e66ff582212da6fb3761ce30"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a72cfad8603ec0bba4c2dfea775c8cfd15330d32bac3b9e0f1ca6cf139fc1000"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8aa1de2bea0c4f2309f4ffae15a96e14f6f82855f1e50cbcf580c3ae14d4e29a"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "34db19f48a06c4f274b02926cc25ac68d93658200ec190242d1398f75d5063c9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "34db19f48a06c4f274b02926cc25ac68d93658200ec190242d1398f75d5063c9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "34db19f48a06c4f274b02926cc25ac68d93658200ec190242d1398f75d5063c9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9bbd220b67545fd975f2c378993ac2c040c4e6a81ccc2c3eb1462697de05d6c8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "729c394611325b75c7c4dd14c34d6c400e5634644ec00492f61c9f5bf94338d1"
   end
 
   depends_on "go" => :build
