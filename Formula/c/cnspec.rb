cd6ab29ce2a4fc25420f947f3a186198a6444ec5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-21 14:53:26 +0000

    cnspec: add 12.6.0 bottle.

--- Formula/c/cnspec.rb
+++ Formula/c/cnspec.rb
@@ -6,6 +6,15 @@ class Cnspec < Formula
   license "BUSL-1.1"
   head "https://github.com/mondoohq/cnspec.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4b1b3206c9c45ed2130c893e1a4df0f9be1826a4912c9d23fecc089a1f7cb828"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2815d7dd6169b6deabbfa26a7f107779dc1dca6693344e6adc2afd27117dd7b6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "55552541547ac9032b99aea0bfdc39ef738a1c09007912f384ecbbba03a28c41"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "13e19f85bc0772d0a30d203be5e645ea53b87a29e8615e2fccbb4824a29fa36e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3db6073470793ce72fe152825014431d3c8b5cbaa3ec46a92e5dd72bc6a42ba6"
+  end
+
   depends_on "go" => :build
 
   def install
