395c7368d2fca6724c145c5aba030c0e811a2e53
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-21 10:37:14 +0000

    pphack: update 0.1.1 bottle.

--- Formula/p/pphack.rb
+++ Formula/p/pphack.rb
@@ -8,10 +8,11 @@ class Pphack < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4a1ee0c35d2d86cc22bc9e7285372998a025ca10c41fde9cf4d2bfd2e559d29b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "513965d438e36c82848c4fcf1c9dd7a9274f2856d34760f4011d65eca90595ad"
-    sha256 cellar: :any_skip_relocation, ventura:       "c7b802811f6861b02f66c75a4c393e8fc935dadad2fcdae8cfe501b1fb8b7e02"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1ba867feff32e93c2984c437d361b05447abe3caccda7fe48cf440bf46c3ff7d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "20b10dd802bc560a074cf9c8fad76b4ea4957b91d45a5c5f02643a806d01e8fb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "20b10dd802bc560a074cf9c8fad76b4ea4957b91d45a5c5f02643a806d01e8fb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "20b10dd802bc560a074cf9c8fad76b4ea4957b91d45a5c5f02643a806d01e8fb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2f4817ebed122cecc7a5c0741b54e675f28c34c530a2f39e159a8afb018f5cf8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4fce1bd363cd07fb72f5f6b00006b38c153d9dc74d568ad07ffbaa1a9ee3788b"
   end
 
   depends_on "go" => :build
