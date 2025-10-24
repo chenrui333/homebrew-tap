9a511a5d17ff7f32910b9763ff777ae6ae6bf0d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-24 22:31:46 +0000

    kekkai: add 0.1.5 bottle.

--- Formula/k/kekkai.rb
+++ Formula/k/kekkai.rb
@@ -6,6 +6,15 @@ class Kekkai < Formula
   license "Apache-2.0"
   head "https://github.com/catatsuy/kekkai.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6961ab43a3a2e44eb2f2013e7fbcf9b8159761f6b090b0829c2c19281985864f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6961ab43a3a2e44eb2f2013e7fbcf9b8159761f6b090b0829c2c19281985864f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6961ab43a3a2e44eb2f2013e7fbcf9b8159761f6b090b0829c2c19281985864f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d532c6ebb871c64a8a82c571244ef7c239d9c45469b60411e6d749c91a18d595"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0edf7c2c6e2f8c137eda9b9b366104c234ee71826785574348280d91da23e85f"
+  end
+
   depends_on "go" => :build
 
   def install
