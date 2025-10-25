c2c815a2b4aa5742b38bbfa78995b16facb3ddeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-25 23:46:35 +0000

    shortest: add 0.4.9 bottle.

--- Formula/s/shortest.rb
+++ Formula/s/shortest.rb
@@ -5,6 +5,15 @@ class Shortest < Formula
   sha256 "0e239bceeda97a65178b7e4b6be16cc88c67e047eeb71bfd2da04441156180f8"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cb3317b16f32964930b0dc0800926f935f8b4b8e43587b6c307218000ff8bdb0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb3317b16f32964930b0dc0800926f935f8b4b8e43587b6c307218000ff8bdb0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cb3317b16f32964930b0dc0800926f935f8b4b8e43587b6c307218000ff8bdb0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "91ca37e8ada7364de9a79f531762d0ef3abbfca5117c5de66efa702816c18bde"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dab31d829a0e0de264d83636e041d73e5c8e01c9f7e6081921a26905509a75f0"
+  end
+
   depends_on "node"
 
   def install
