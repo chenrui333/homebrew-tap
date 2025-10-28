4566e19b798dd2a57f498485158b480f47e7e888
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-28 17:15:50 +0000

    jules: add 0.1.38 bottle.

--- Formula/j/jules.rb
+++ Formula/j/jules.rb
@@ -5,6 +5,15 @@ class Jules < Formula
   sha256 "5a7ef1c25e3e58ecb5518c866f644ca4bcf6fda312688c20d7fbc5adfc79cba6"
   # license :unfree
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4881053ca8217520d8815742cfb5338843ad2724e3474459e5151fdad3b0ba1e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4881053ca8217520d8815742cfb5338843ad2724e3474459e5151fdad3b0ba1e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4881053ca8217520d8815742cfb5338843ad2724e3474459e5151fdad3b0ba1e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3d742a2a133601d6a527b5490fa2198e2bf5f524cd0cd9fecaca35db2a4131df"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "91e9ff35ec0a7e2913bcefbdc869888d29c635d8b53e3b4b9882664b13d98a1d"
+  end
+
   depends_on "node"
 
   def install
