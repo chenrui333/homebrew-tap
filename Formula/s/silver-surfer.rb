f3e203c2f116442691afef14f789d22b06dad8c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-10-30 23:26:46 +0000

    silver-surfer: add 0.1.4 bottle.

--- Formula/s/silver-surfer.rb
+++ Formula/s/silver-surfer.rb
@@ -5,6 +5,15 @@ class SilverSurfer < Formula
   sha256 "3ce8a7fe5754078a9d34f7018a0f99cccca37f423a0b9719d3f33570e58130b7"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "824f9641d763487393d27de9ee7ba457bd96a8ad1fbff87aeb3644e24763baad"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "824f9641d763487393d27de9ee7ba457bd96a8ad1fbff87aeb3644e24763baad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "824f9641d763487393d27de9ee7ba457bd96a8ad1fbff87aeb3644e24763baad"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d7680840f933a78d9c4b62edbec0ad92c72eabe31332cd69835bd7be89132cd5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ff73b7e663fefefff804fb015637db0a2499547677f1d3c796922e2ed717550"
+  end
+
   depends_on "go" => :build
 
   def install
