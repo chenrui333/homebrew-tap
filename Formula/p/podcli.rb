5ef8af979281af3ab6094b8be9bf7e3dd2ce8635
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-01 23:33:41 +0000

    podcli: add 6.9.2 bottle.

--- Formula/p/podcli.rb
+++ Formula/p/podcli.rb
@@ -6,6 +6,15 @@ class Podcli < Formula
   license "Apache-2.0"
   head "https://github.com/stefanprodan/podinfo.git", branch: "dev"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "17470b98ae072e203f462719e76f66df8a29d9c4e060b0dfa47d4321ee89df62"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "17470b98ae072e203f462719e76f66df8a29d9c4e060b0dfa47d4321ee89df62"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "17470b98ae072e203f462719e76f66df8a29d9c4e060b0dfa47d4321ee89df62"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a5a79dcff09aa0af69988221e4a10e0a6afc7ea91bfa0b20a34c46d8b426eff2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3227c131129cd7c982f76453b0fdfc8b4f16a4c982d7419710456e83fde9e363"
+  end
+
   depends_on "go" => :build
 
   def install
