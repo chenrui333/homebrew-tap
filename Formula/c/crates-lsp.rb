f67dee9dc4dd842425354daa9993c3dd203dd57f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-01 20:50:11 +0000

    crates-lsp: add 0.4.3 bottle.

--- Formula/c/crates-lsp.rb
+++ Formula/c/crates-lsp.rb
@@ -6,6 +6,15 @@ class CratesLsp < Formula
   license "MIT"
   head "https://github.com/MathiasPius/crates-lsp.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "89cdc28e5060d974245648cd9a77d307c03ec04cca061f0e26b591499e281e71"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d3368f85935e646cf90326149aea4461535b43c0bcab6bc739b584f7e678be37"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9f1dc4274ce622669a991eda110a6ab7b603f36ea6b679c9e40b588b30f7a023"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "371cecf44fd0459ac27ddef1e535c8498bd3852d4eaca20a9c368b58c32fd429"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8d2a7807af17b1857a9a231af18944f4f66e0cdf13b8ac6c995681152ffb8f9b"
+  end
+
   depends_on "rust" => :build
 
   def install
