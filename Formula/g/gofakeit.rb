5394a31088d9eaf132a8307b772461515d0b15e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 18:57:03 +0000

    gofakeit: update 7.8.2 bottle.

--- Formula/g/gofakeit.rb
+++ Formula/g/gofakeit.rb
@@ -8,11 +8,11 @@ class Gofakeit < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9515980384e04ed57cf5758bf8c71a3dbeeceb5f4616788e252cb50c6f310148"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9515980384e04ed57cf5758bf8c71a3dbeeceb5f4616788e252cb50c6f310148"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9515980384e04ed57cf5758bf8c71a3dbeeceb5f4616788e252cb50c6f310148"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "727e059e9d717c63471b01bdfc20edc6dee4f645103cb6fa406faba4ca097c6a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b7fbb4336b2fb2d1a76b8e0f22f10c1274ace85b3950401f31b2db330efe79a8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "08e3ba25faf039e7fb6484b607c1ccf975372c96153586df94221478ffb3cac3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "08e3ba25faf039e7fb6484b607c1ccf975372c96153586df94221478ffb3cac3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "08e3ba25faf039e7fb6484b607c1ccf975372c96153586df94221478ffb3cac3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d1ff9f6e6d96d458ad55b2be4fe1c7d0fea3f34264fd9e7dfda3938eea445550"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cfe189a14be7da38ab6bf19c17d2491c3e2dc656d5f532b293c60a1001387078"
   end
 
   depends_on "go" => :build
