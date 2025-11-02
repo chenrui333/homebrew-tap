bd6b4fcf95bc4c63899b14bf81508e76cde00127
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 16:27:04 +0000

    goclone: add 1.2.2 bottle.

--- Formula/g/goclone.rb
+++ Formula/g/goclone.rb
@@ -6,6 +6,15 @@ class Goclone < Formula
   license "MIT"
   head "https://github.com/goclone-dev/goclone.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9d78e58f4a67331af19802810ea3945b58f364f1d798f4fca84806d3d9b82140"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9d78e58f4a67331af19802810ea3945b58f364f1d798f4fca84806d3d9b82140"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d78e58f4a67331af19802810ea3945b58f364f1d798f4fca84806d3d9b82140"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e0d981cf95a540abe9f8f1070a8795dfd1d76a64e5c49bd891ce37e768db3a92"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9db3a5fad6c21fd5a251b10bbb699861498148216561868ef42327590f0ce64a"
+  end
+
   depends_on "go" => :build
 
   def install
