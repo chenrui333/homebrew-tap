19f146ffb8a4835f2f3dccaebcec25e3511b3411
Rui Chen <rui@chenrui.dev>
2025-11-02 13:20:55 -0500

    teldrive 1.7.0 (new formula)
    Closes #2341.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/t/teldrive.rb
@@ -0,0 +1,43 @@
+class Teldrive < Formula
+  desc "Utility to organize, manage, and sync Telegram files locally"
+  homepage "https://teldrive-docs.pages.dev/"
+  url "https://github.com/tgdrive/teldrive/archive/refs/tags/1.7.0.tar.gz"
+  sha256 "f91c88bc34d9eb43416b4965cfa2fff311a743352a4284a54046e695a5191d43"
+  license "MIT"
+  head "https://github.com/tgdrive/teldrive.git", branch: "master"
+
+  depends_on "go" => :build
+
+  resource "ui_assets" do
+    url "https://github.com/tgdrive/teldrive-ui/releases/download/latest/teldrive-ui.zip"
+    sha256 "332846ee65061adca2a7213fbe01ec1f182e20bde9db498407061c6d4e4228b5"
+  end
+
+  def install
+    (buildpath/"ui/dist").install resource("ui_assets")
+
+    # generate API code
+    system "go", "generate", "./..."
+
+    ldflags = %W[
+      -s -w
+      -X github.com/tgdrive/teldrive/internal/version.Version=#{version}
+      -X github.com/tgdrive/teldrive/internal/version.CommitSHA=#{tap.user}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"teldrive", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/teldrive version 2>&1")
+
+    (testpath/".teldrive/config.toml").write <<~TOML
+      [db]
+      data-source = "postgres://user:password@localhost/dbname"
+    TOML
+
+    output = shell_output("#{bin}/teldrive check 2>&1", 1)
+    assert_match "database: failed to connect to `user=user database=dbname`", output
+  end
+end
