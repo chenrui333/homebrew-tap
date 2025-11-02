6c98a602c289b6447638ee2a11a6b357b48c06ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 00:09:10 +0000

    vuls: add 0.35.1 bottle.

--- Formula/v/vuls.rb
+++ Formula/v/vuls.rb
@@ -6,6 +6,15 @@ class Vuls < Formula
   license "GPL-3.0-only"
   head "https://github.com/future-architect/vuls.git", branch: "dev"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "983698a8258ea5599fbb152f653d9982b8e9ea642b65a728002dcbdc665e1341"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5fdd0822f34835aa180661cebeee4fad79b37ae7ba891f7dd0b67c10b75b6301"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa05941aa05c957aac67b4b14c6e51e0eddc00b49e7d3ab13985a350d40c922b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a655850afe1706d4ff732e661d1c3154ac5d6ccf29a8e102563dfb696373b218"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2052389f16295c6eca389202a5365f30b369186b5f2ee31c566d2d5a9f0977e2"
+  end
+
   depends_on "go" => :build
 
   def install
