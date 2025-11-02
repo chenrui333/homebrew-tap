3db897a0e5190da660f70366d64df6de162e0f52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 00:27:18 +0000

    sish: add 2.20.0 bottle.

--- Formula/s/sish.rb
+++ Formula/s/sish.rb
@@ -6,6 +6,15 @@ class Sish < Formula
   license "MIT"
   head "https://github.com/antoniomika/sish.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3b85125ac79ed6a664541a132063b3fa8a171d173fc1582816fa94f31f7bdea0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3b85125ac79ed6a664541a132063b3fa8a171d173fc1582816fa94f31f7bdea0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3b85125ac79ed6a664541a132063b3fa8a171d173fc1582816fa94f31f7bdea0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8fe208da73e56eff67655d43ae33ec4d1ba289a975090ab51881b20456fa2c65"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d10e33f9a9362e83d102c32a0474ea1e249f01e51a0a698a22f4b38eb4ec9744"
+  end
+
   depends_on "go" => :build
 
   def install
