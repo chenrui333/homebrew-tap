f93950c68e1b287f99d563f4dbafffcd8a6e7606
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 02:20:35 +0000

    semaphore: add 2.16.37 bottle.

--- Formula/s/semaphore.rb
+++ Formula/s/semaphore.rb
@@ -6,6 +6,15 @@ class Semaphore < Formula
   license "MIT"
   head "https://github.com/semaphoreui/semaphore.git", branch: "develop"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5067f1100b459e234921335634626c8c43c21d18871d97c13ed07a265edc97e6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5067f1100b459e234921335634626c8c43c21d18871d97c13ed07a265edc97e6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "55e459e394c872c2e8c95cf30feddd95b29d637d08c97b22393611b59b4560dc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9c455b67c1db965354aba11248b41fdc361e9e8c32d5c4ae3cfd8a9881a1a7b3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ff7f4c77eb912d809aae95d61be997299b9fab9fd7875f42d699fd2d264d48e"
+  end
+
   depends_on "go" => :build
   depends_on "go-task" => :build
   depends_on "node" => :build
