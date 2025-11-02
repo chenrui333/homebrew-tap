44a29b50da39897e1d7a62247eb3a29e1a394475
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 00:35:43 +0000

    pet: add 1.0.1 bottle.

--- Formula/p/pet.rb
+++ Formula/p/pet.rb
@@ -6,6 +6,15 @@ class Pet < Formula
   license "MIT"
   head "https://github.com/knqyf263/pet.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "13a1fbee67bd40c496f65ad2f3e95f5bf04cdf7b0b9d943abec5b78817b98b6b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "13a1fbee67bd40c496f65ad2f3e95f5bf04cdf7b0b9d943abec5b78817b98b6b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "13a1fbee67bd40c496f65ad2f3e95f5bf04cdf7b0b9d943abec5b78817b98b6b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "df85db82914d3b487a4c1c7b1c05e1880b57667f949e21ac31f9c1729ed29f1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b36a7d69c3290c458b8c7a7f824f1e4cc063ed8d2677d86de2dc0ed666e6140"
+  end
+
   depends_on "go" => :build
 
   def install
