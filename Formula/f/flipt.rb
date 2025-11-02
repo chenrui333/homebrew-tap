efd153c57d26842786dfc347e959f4b7af6d9fec
Rui Chen <rui@chenrui.dev>
2025-11-01 21:01:48 -0400

    flipt 2.3.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/f/flipt.rb
@@ -0,0 +1,23 @@
+class Flipt < Formula
+  desc "Enterprise-ready, Git native feature management solution"
+  homepage "https://flipt.io/"
+  url "https://github.com/flipt-io/flipt/archive/refs/tags/v2.3.1.tar.gz"
+  sha256 "4ec42d28fdb43aefa1a38c8ab0503312dd3736d46d949a4ef8dbd56b5a05f06f"
+  # license "FCL-1.0-MIT" # Fair Core License, Version 1.0, MIT Future License
+  head "https://github.com/flipt-io/flipt.git", branch: "v2"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/flipt"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/flipt --version")
+
+    cfg = testpath/"config.yml"
+    system bin/"flipt", "config", "init", "--force", "--config", cfg
+    assert_match "diagnostics:\n  profiling:\n    enabled: true", cfg.read
+  end
+end
