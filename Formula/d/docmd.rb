36d573d409b7d50ce9417b8afbe8f66a6b91131b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-02 22:16:03 +0000

    docmd: update 0.2.5 bottle.

--- Formula/d/docmd.rb
+++ Formula/d/docmd.rb
@@ -7,10 +7,7 @@ class Docmd < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6da3af0ffdc96a9a9ea0b4a3e093df946460056629414b90d18cacc6a32ca4fc"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "878b4093259cfe46cd3febdc8a16733c223d27bd810d729024509ff7ab1703e5"
-    sha256 cellar: :any_skip_relocation, ventura:       "a3552fac437ca76a0a1fc77564ed42ae9a4b4454b5a28cc0cd0d487e31205242"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "47fc3f74dcc22378688ea9ec161a2b962272ae0c5cd7732cf188233647301eed"
+    sha256 cellar: :any_skip_relocation, all: "24267b6f7303375c652bfa77b8a1267b21d4f137ed2accd0e7b559ea878eab14"
   end
 
   depends_on "node"
