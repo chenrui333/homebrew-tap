89776baf7599d079a9b4e34b815da2b9e01fdf19
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-03 00:11:23 +0000

    ninjabot: add 0.4.1 bottle.

--- Formula/n/ninjabot.rb
+++ Formula/n/ninjabot.rb
@@ -6,6 +6,15 @@ class Ninjabot < Formula
   license "MIT"
   head "https://github.com/rodrigo-brito/ninjabot.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1595a85737f1ed805d68987be2519f976f00bbc8cb4e6dcf7b45725434926cef"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1595a85737f1ed805d68987be2519f976f00bbc8cb4e6dcf7b45725434926cef"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1595a85737f1ed805d68987be2519f976f00bbc8cb4e6dcf7b45725434926cef"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ba988d57f59265ec893482776f1a60b25f0d4db4d1d3d5a9862d04c212e1994c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a66805f02726c85c7856eb214055de3d2d9fa74ea0d8ca201ad5df80d5e48c8a"
+  end
+
   depends_on "go" => :build
 
   def install
