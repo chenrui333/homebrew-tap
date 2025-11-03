6095e97ffcfcb861799265b51c83e4df13c8a536
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-03 00:56:16 +0000

    urlhunter: add 0.2.0 bottle.

--- Formula/u/urlhunter.rb
+++ Formula/u/urlhunter.rb
@@ -6,6 +6,15 @@ class Urlhunter < Formula
   license "MIT"
   head "https://github.com/utkusen/urlhunter.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b2abd85bbc5473b757770234f2af3b926c2c334e77cfe8fe03b8e1660062b4c3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b2abd85bbc5473b757770234f2af3b926c2c334e77cfe8fe03b8e1660062b4c3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b2abd85bbc5473b757770234f2af3b926c2c334e77cfe8fe03b8e1660062b4c3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "209283d7bfb2b722332f9626e4527eef339904ab6a820e7c7242f2ddb5422ac9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cda3e49859b899c15f0b18a6444b78f80105440b1d5cdeb8de2ac57d0ddafe1a"
+  end
+
   depends_on "go" => :build
 
   def install
