bd6d8d09c0cc8b24e992d38e3c9e3b28e1f1f06c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-03 01:02:07 +0000

    starcharts: add 1.10.0 bottle.

--- Formula/s/starcharts.rb
+++ Formula/s/starcharts.rb
@@ -6,6 +6,15 @@ class Starcharts < Formula
   license "MIT"
   head "https://github.com/caarlos0/starcharts.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "767d9ed0e5839c461577d52d7a1a0645760ebee052483d01628ffdd3231ffbe1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7db0026adac837e6fe6240876d004ea0eab1f23b6fdd5c06a718aedae93f5e02"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2b5991aa577c3cd075abd843d0d6973e43f4ffbf411d05137fe7aac7176eae7c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f7bfb9de236effd7665c0aa0ff2c37da2b2e8538921e18ca9ae3a111e9d5561d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3b4a179efcaea6778758497adbe1a8ab78b6049bd69f6161789c5a50f7b7027"
+  end
+
   depends_on "go" => :build
 
   def install
