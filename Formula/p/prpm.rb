6d1c6371932e2e3fb848d813ba47954890e1fdd7
Rui Chen <rui@chenrui.dev>
2025-11-03 09:30:44 -0500

    prpm 0.1.3 (new formula)
    Closes #2377.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/p/prpm.rb
@@ -0,0 +1,24 @@
+class Prpm < Formula
+  desc "Universal registry for AI coding tools"
+  homepage "https://prpm.dev/"
+  url "https://registry.npmjs.org/prpm/-/prpm-0.1.3.tgz"
+  sha256 "bc8ccfcde9df37d1e81270b105ceadca1ea437c3c054eabc802af88890e35349"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/prpm --version")
+
+    system bin/"prpm", "init", "--yes"
+    assert_path_exists testpath/"prpm.json"
+    assert_match "# README.md", (testpath/"README.md").read
+
+    assert_match "No packages installed", shell_output("#{bin}/prpm list")
+  end
+end
