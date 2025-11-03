ff6da2af0a36fc5ad21a35b4d17c578bc9558002
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-03 00:24:17 +0000

    inbucket: add 3.1.0 bottle.

--- Formula/i/inbucket.rb
+++ Formula/i/inbucket.rb
@@ -6,6 +6,15 @@ class Inbucket < Formula
   license "MIT"
   head "https://github.com/inbucket/inbucket.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4b39a8bfbcbfb459368bee731be9787b814ffc817caeeceaae35a48ed8595985"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4b39a8bfbcbfb459368bee731be9787b814ffc817caeeceaae35a48ed8595985"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4b39a8bfbcbfb459368bee731be9787b814ffc817caeeceaae35a48ed8595985"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "64228e2461b71b1b1a7b312120914f70cab9288b309dd2f076568b2d283e94de"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "278c751b0dc48193e60c2b46b46304de30fc01ad87bb5b80d358f5a752338e35"
+  end
+
   depends_on "go" => :build
 
   def install
