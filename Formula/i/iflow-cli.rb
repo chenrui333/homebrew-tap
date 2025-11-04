be45f520048f458bc1864bcfa03dc6563dbde9fc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 12:31:04 +0000

    iflow-cli 0.3.14
    Closes #2398.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/i/iflow-cli.rb
+++ Formula/i/iflow-cli.rb
@@ -1,8 +1,8 @@
 class IflowCli < Formula
   desc "AI assistant that runs directly in your terminal"
   homepage "https://cli.iflow.cn/"
-  url "https://registry.npmjs.org/@iflow-ai/iflow-cli/-/iflow-cli-0.3.13.tgz"
-  sha256 "7ddb6a6e304df6ad306462a7b76bfcc064e9715fc701475443de2a9a9e7b62e4"
+  url "https://registry.npmjs.org/@iflow-ai/iflow-cli/-/iflow-cli-0.3.14.tgz"
+  sha256 "1977d5266cccf00153602ed20627a783ca40f47803df7fc7fb1c0936643cc1ac"
   license "Apache-2.0" # derived from LICENSE, but no source code for the project in https://github.com/iflow-ai/iflow-cli
 
   bottle do
