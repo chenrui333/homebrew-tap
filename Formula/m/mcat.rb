acd2d30187a3b8626dcc023237b4be1f5332f0df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 19:24:36 +0000

    mcat: add 0.4.4 bottle.

--- Formula/m/mcat.rb
+++ Formula/m/mcat.rb
@@ -6,6 +6,15 @@ class Mcat < Formula
   license "MIT"
   head "https://github.com/Skardyy/mcat.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "effc6a0ac604da68eddd27ea2271b9d9d6dd0febbae9225d6ec3507206e827ad"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "98ad9dee750fbec6c8936dfd0b17e6eff44bf4d95bc779a5bafb4141093b8180"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a86636cba461774e67bc92b814d88ea603880ba6111af132f33d0f11d1c76a60"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e2d359f0ab21ec1ca0b9c142d00c9d5bcb4a83bb74a1ed88f81d0602c9fd13dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f6b94ea86f67a71a199cf895c28df95649fd69a33ef2adcecabc2a88f83ea504"
+  end
+
   depends_on "rust" => :build
 
   def install
