abdf868daf0d91b53b2b9fb75f2782314eaf5e33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 23:02:48 +0000

    tinifier: add 5.1.1 bottle.

--- Formula/t/tinifier.rb
+++ Formula/t/tinifier.rb
@@ -6,6 +6,15 @@ class Tinifier < Formula
   license "MIT"
   head "https://github.com/tarampampam/tinifier.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5e500802eb5442399fd0edaa608553a7a846ba1e19ba9015bebe71c87b152f4e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e500802eb5442399fd0edaa608553a7a846ba1e19ba9015bebe71c87b152f4e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5e500802eb5442399fd0edaa608553a7a846ba1e19ba9015bebe71c87b152f4e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cff814dd19b6ef650674e9c315998f569667c330880f0888602693a7d34b3a38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fcfe27b748a55e6d52a433cb9a0d294f1e17de5e271b1c69f80201d0e81ac9a5"
+  end
+
   depends_on "go" => :build
 
   def install
