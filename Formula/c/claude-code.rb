e8fe078e7e3ba64746218724d6e91598337b4e88
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 00:39:32 +0000

    claude-code 2.0.32

--- Formula/c/claude-code.rb
+++ Formula/c/claude-code.rb
@@ -1,8 +1,8 @@
 class ClaudeCode < Formula
   desc "Agentic coding tool that lives in your terminal"
   homepage "https://github.com/anthropics/claude-code"
-  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.0.31.tgz"
-  sha256 "6b67f14f030554cf055f030c82de64955438ed8c7225f96b1576a15c2be15fa4"
+  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.0.32.tgz"
+  sha256 "f0a95b505a73d00c075d1b0845a8d77a0bee5d56659d3b28a1db06c5cce42643"
   # license :unfree
 
   bottle do
