639d6cd4f4d02519506ed462d6170b8667704d94
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 19:34:50 +0000

    resterm: add 0.6.3 bottle.

--- Formula/r/resterm.rb
+++ Formula/r/resterm.rb
@@ -6,6 +6,15 @@ class Resterm < Formula
   license "Apache-2.0"
   head "https://github.com/unkn0wn-root/resterm.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0a764ce74160544a8360803a778ce7edc6865e397d17ced9fd93df040ae6eb84"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0a764ce74160544a8360803a778ce7edc6865e397d17ced9fd93df040ae6eb84"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0a764ce74160544a8360803a778ce7edc6865e397d17ced9fd93df040ae6eb84"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b63d222dd8dfa25969984d88b55394fe3d7a60f5b18ea70443f13049a02bdd56"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e40ee4df8f57d0dc16236b76c704757ddefba118ee8f28a00424cdcb21d3e815"
+  end
+
   depends_on "go" => :build
 
   def install
