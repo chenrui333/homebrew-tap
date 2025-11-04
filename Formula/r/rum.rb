9828e2b04ae3a959a48e2af27ee02acd84b625c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-04 19:45:23 +0000

    rum: add 1.0.0 bottle.

--- Formula/r/rum.rb
+++ Formula/r/rum.rb
@@ -6,6 +6,15 @@ class Rum < Formula
   license "MIT"
   head "https://github.com/thekarel/rum.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a2bcd864da4d35d8d2ab4c04f615aa3a9f3205b585745487a7bc1cd22d7df4c9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a2bcd864da4d35d8d2ab4c04f615aa3a9f3205b585745487a7bc1cd22d7df4c9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a2bcd864da4d35d8d2ab4c04f615aa3a9f3205b585745487a7bc1cd22d7df4c9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2bf6a118e14e7be2583144d2462d61c059588e7ad9dfa205766a29d6e4516fec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25fbf0e379b4f74c7acbd3fdd821f9ce511de1cb37a62870417ffe1ad56e4bc4"
+  end
+
   depends_on "go" => :build
 
   def install
