c422202e16b5cce41fde921886bcb1f8a23a65d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-05 13:50:38 +0000

    ktea: add 0.6.1 bottle.

--- Formula/k/ktea.rb
+++ Formula/k/ktea.rb
@@ -6,6 +6,15 @@ class Ktea < Formula
   license "Apache-2.0"
   head "https://github.com/jonas-grgt/ktea.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a0a56fd31ddaf1b12f0523e0d8ba94b6c7eb64381484dca2798685f843088ce5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a0a56fd31ddaf1b12f0523e0d8ba94b6c7eb64381484dca2798685f843088ce5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a0a56fd31ddaf1b12f0523e0d8ba94b6c7eb64381484dca2798685f843088ce5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "552c9f4531c9b49b04c7c0bf8b60b30762d5dc8db9004c6eca45cbc4e84c6599"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b76c378660d25ca93aace6c0ea1205bf7af7cbb11458b7b70c1ed3f957349361"
+  end
+
   depends_on "go" => :build
 
   def install
