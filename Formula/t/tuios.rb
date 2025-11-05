c68d3a0ff69e2a79eec046b564cfd670998ec5af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-05 18:47:30 +0000

    tuios: update 0.3.2 bottle.

--- Formula/t/tuios.rb
+++ Formula/t/tuios.rb
@@ -8,11 +8,11 @@ class Tuios < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f8f0678847675ca50d40f88765f7094d5cfaef695d96b8e7b22573008265d42d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b2936a4ab2acbbd1de6be8aa8a9a667ef393afdc0ea003a681e59ab68864cf0c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "634947415bea0b18b8c290de78a3e44e4ec78f9267a4b2f63b526964b82fa888"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "164037673f8a62695d2583aaf0f77295c2002b14ee0e4da1ae848a29e7c49828"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1d32390de66717e15188043e2efdd321d26ee0272bd1b8b30e6f65ae6cfbc56d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5901a53cf233125c25cb48a947d497e4656951014adc4e326d22e0d09b5c5896"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0d3e0b9da8798646340dfc8e662243575ca2f6665d2e66165387fdba990b0574"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "94e62b72fd6e8f3fe792b2c8a0aad803cdebca3f287e691877be061208a6394a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a9657470cc981e86525714f4c810b5587442d37ef6116ba3f8c4fd42b525fdf2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "de2b15b891179a77ace17767d82197ec176ee4e5ed5240810beed49380d73ce5"
   end
 
   depends_on "go" => :build
