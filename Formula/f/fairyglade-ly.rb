0f0d4ffb7e413a254f0d67bbb9351cc2eeea77c8
Rui Chen <rui@chenrui.dev>
2025-11-09 12:29:31 -0500

    fairyglade-ly 1.2.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/f/fairyglade-ly.rb
@@ -0,0 +1,33 @@
+class FairygladeLy < Formula
+  desc "TUI (ncurses-like) display manager for Linux and BSD"
+  homepage "https://codeberg.org/fairyglade/ly"
+  url "https://codeberg.org/fairyglade/ly/archive/v1.2.0.tar.gz"
+  sha256 "ad5157ed81783c15244d4f67d1ee88528562176f4822df086295a93e960df720"
+  license "WTFPL"
+  head "https://codeberg.org/fairyglade/ly.git", branch: "master"
+
+  depends_on "pkgconf" => :build
+  depends_on "zig" => :build
+  depends_on "libxcb"
+  depends_on :linux
+  depends_on "linux-pam"
+
+  def install
+    # Fix illegal instruction errors when using bottles on older CPUs.
+    # https://github.com/Homebrew/homebrew-core/issues/92282
+    cpu = case ENV.effective_arch
+    when :arm_vortex_tempest then "apple_m1" # See `zig targets`.
+    when :armv8 then "xgene1" # Closest to `-march=armv8-a`
+    else ENV.effective_arch
+    end
+
+    args = []
+    args << "-Dcpu=#{cpu}" if build.bottle?
+
+    system "zig", "build", *std_zig_args, *args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ly --version 2>&1")
+  end
+end
