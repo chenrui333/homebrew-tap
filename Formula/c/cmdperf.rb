195d6722efe8f5e1bbe71874ac4c6fbb5b026d01
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 14:13:20 +0000

    cmdperf: add 0.1.3 bottle.

--- Formula/c/cmdperf.rb
+++ Formula/c/cmdperf.rb
@@ -5,6 +5,15 @@ class Cmdperf < Formula
   sha256 "11add8c4ee1744a0408ffb1e3e4a41d66985c563ebe8b873ea17748a60e7dbbe"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5c9ad948d3c3116d6fb1a1d316cb2605f1b30e08d41cad0add308f2f8546a840"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c9ad948d3c3116d6fb1a1d316cb2605f1b30e08d41cad0add308f2f8546a840"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5c9ad948d3c3116d6fb1a1d316cb2605f1b30e08d41cad0add308f2f8546a840"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d0c6e85acb118b9475faaab690f231f4b41568d9854ea1ceb0f3c89b8ba8e7cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "75a653cde61ed3768acea4373110dc02143d2c122566575bf97d5632deea0449"
+  end
+
   depends_on "go" => :build
 
   def install
