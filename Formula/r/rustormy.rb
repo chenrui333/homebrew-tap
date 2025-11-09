6089f95bba80863aa184faa029f5a60625a58286
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 15:57:16 +0000

    rustormy: add 0.4.1 bottle.

--- Formula/r/rustormy.rb
+++ Formula/r/rustormy.rb
@@ -6,6 +6,15 @@ class Rustormy < Formula
   license "MIT"
   head "https://github.com/Tairesh/rustormy.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1ffdd8399af3f2150286a3f04206949cbaaebeb67aa37f8c1e1769ea9241cfc2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bb8eb376a730dfa5197bca78e63178f9c73ce825e34aad18d654a4b58a24f4f9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "496ca5d453e101a246ae6587c913f30704ea9d93aa96020c250128b8e70fb128"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0bbd0690530e5695383c0b0a39d6fc5dbd1fe39ad43a74de622fc79664237ce9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3da7dfc0170161786f8803d32ccf2d7d4d9994ed72d4889a2035f729468d9fc"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
