044385291602cf859a7046665a7f42c9dd195d8e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 00:23:29 +0000

    ugdb: add 0.1.12 bottle.

--- Formula/u/ugdb.rb
+++ Formula/u/ugdb.rb
@@ -6,6 +6,15 @@ class Ugdb < Formula
   license "MIT"
   head "https://github.com/ftilde/ugdb.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2be2c8c5e99ea1e1dd5a09bf127d74e2a660582dce2f598b20bb5192871793e9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b9597685eb95f3cb30348729cb159b20c56f575b6f7a6acd16a56c6779d2cafd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2004e6594f128b0a9ee108ed6e6af1fa492f2ac6b4b7679063aba44e344ad8d4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "54683faa00286efb96250d170fe299eac10d543781dbbac1abcbd408d912915d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "def31c405c6e039c10baad4cf46c4b3b00ee98d8f1446a9acb85a8483850fb15"
+  end
+
   depends_on "rust" => :build
 
   def install
