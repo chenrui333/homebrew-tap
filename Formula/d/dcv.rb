e9050d445f65d12e4e9992e8744dd987ebe7c4fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 16:32:43 +0000

    dcv: add 0.3.1 bottle.

--- Formula/d/dcv.rb
+++ Formula/d/dcv.rb
@@ -5,6 +5,15 @@ class Dcv < Formula
   sha256 "3a97f480df7466b8bf0fc50e3bfb643d768245156738bc1858de8cd2032582e8"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "27f5bb8769cea8b36598a70ff0d531795e64d983f3a4828f3f03e65a8a4faf84"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "27f5bb8769cea8b36598a70ff0d531795e64d983f3a4828f3f03e65a8a4faf84"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "27f5bb8769cea8b36598a70ff0d531795e64d983f3a4828f3f03e65a8a4faf84"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ccf931651abd540a3b37d900647877c3a493a73416aefda975e4e08e41d00313"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "17341a5704a51b3b535d3f9f616f90d7d4ffc96c4720122dbd2b62b33426a02d"
+  end
+
   depends_on "go" => :build
 
   def install
