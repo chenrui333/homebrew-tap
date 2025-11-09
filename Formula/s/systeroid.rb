7760944af1780679409a2b75a042a74006b8bf64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 17:28:26 +0000

    systeroid: add 0.4.6 bottle.

--- Formula/s/systeroid.rb
+++ Formula/s/systeroid.rb
@@ -5,6 +5,12 @@ class Systeroid < Formula
   sha256 "756b341dc86553ce8df583d55e6d01517bf52721a556713a4fb6056c0f823f3b"
   license any_of: ["Apache-2.0", "MIT"]
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "c1fc7b3d0f147bcc0da379e284411462ff8a44b64258cb12fda31bca0fc54fa2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3aaa97eef8dcf6091f0c37aff3868b988ce15d06ebcc1fcf2b772746c8393ecc"
+  end
+
   depends_on "rust" => :build
   depends_on :linux
 
