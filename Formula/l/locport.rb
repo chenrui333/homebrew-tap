547b30cf5075d2bef3823c70f803e649a9e1ab1f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 14:26:22 +0000

    locport 1.2.0

--- Formula/l/locport.rb
+++ Formula/l/locport.rb
@@ -1,8 +1,8 @@
 class Locport < Formula
   desc "Manage local ports across projects"
   homepage "https://github.com/klevo/locport"
-  url "https://github.com/klevo/locport/archive/refs/tags/v1.1.3.tar.gz"
-  sha256 "81ba77accf8ac659a42a4dffd4a6c573a004299d7f0888036afc1fc896c32b4c"
+  url "https://github.com/klevo/locport/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "e8bc684ee239ddef9c2d98f24f34a9b716e5f539a96300959db278a8e156a56b"
   license "MIT"
 
   bottle do
