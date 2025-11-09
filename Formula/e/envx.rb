8b6a4f24075ea93a68c9c18a852e573a83bbe075
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-09 16:01:03 +0000

    envx: add 0.6.2 bottle.

--- Formula/e/envx.rb
+++ Formula/e/envx.rb
@@ -6,6 +6,15 @@ class Envx < Formula
   license "MIT"
   head "https://github.com/mikeleppane/envx.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "488870b521a979ab9bfeddee6795c0787f5eb97e021510ca2213f649b7cf79b2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aaa740ebfc2b2d724ba4455c7c12227b216a190291e2524e50af03194c4f35f2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4454fb1da4f404b054e023a0903b999cf8c963bd8fb6407de034c1f73b692f26"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1a1ca7678d0882f9839d340e4c509b77a3301950b1ea52a24d443b6fecb12b05"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d854eb7b17372d6909859e415b92aa399a914313ae3ec3941c502c20ef76a6d6"
+  end
+
   depends_on "rust" => :build
 
   def install
