34621f676eed607c8a6d67408751c576077653c4
Rui Chen <rui@chenrui.dev>
2025-11-09 10:41:30 -0500

    rustormy: needs openssl for linux build
    Closes #2521.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/r/rustormy.rb
+++ Formula/r/rustormy.rb
@@ -6,7 +6,12 @@ class Rustormy < Formula
   license "MIT"
   head "https://github.com/Tairesh/rustormy.git", branch: "main"
 
+  depends_on "pkgconf" => :build
   depends_on "rust" => :build
+
+  on_linux do
+    depends_on "openssl@3"
+  end
 
   def install
     system "cargo", "install", *std_cargo_args
