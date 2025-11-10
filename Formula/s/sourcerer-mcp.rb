dfc8255084dadc1b9f19ad649353a24357c8523f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-10 14:56:28 +0000

    sourcerer-mcp: update 0.5.4 bottle.

--- Formula/s/sourcerer-mcp.rb
+++ Formula/s/sourcerer-mcp.rb
@@ -7,10 +7,10 @@ class SourcererMcp < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "266b8056d242264de392125ee3fafda78c25d33f327b27bf7c76e353d88a9a9f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "26aeec947b36389f2c56657900d935975d8d5e3fe7dac205f98d827ba3055d6c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "48aff0d5e43630f461df0a76a95e6da975bf13e1ce2c47c0aacdb4acef856de6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4f8f0250283a7ae2642a1c3b559cbf849c4fc2c1456f9c4e6980464bb4d5910b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a1b9644fc3c413986d01d3783f8bb9b86769ffb67e95d4e976d5f97b089d8195"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5668a854753499f521fa80d696a601d32c1b87e750bf71dbb389c3d7dc674c95"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ef5266a79b5f15d85ef24a7c9b2ea6712c7d0086eea15110a168f71e7e164e58"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7523c8d441f239b999220cd18da4f9d87459cf3a957265c786bbd396cf053f3c"
   end
 
   depends_on "go" => :build
