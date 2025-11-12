9bc6c5efa6cad572e143c491b352f72e967caf3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-12 14:31:24 +0000

    prpm 0.1.16

--- Formula/p/prpm.rb
+++ Formula/p/prpm.rb
@@ -1,8 +1,8 @@
 class Prpm < Formula
   desc "Universal registry for AI coding tools"
   homepage "https://prpm.dev/"
-  url "https://registry.npmjs.org/prpm/-/prpm-0.1.15.tgz"
-  sha256 "52287bbebd0233a14204a7e5b70576bfb15792bd1febbbd1ad7c4f069cd7be4b"
+  url "https://registry.npmjs.org/prpm/-/prpm-0.1.16.tgz"
+  sha256 "b867777e9579287cb466edb615a6d0bd69e757dd1c3f24a6aea970b2cd815500"
   license "MIT"
 
   bottle do
