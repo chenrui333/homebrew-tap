c8ad2cfb08be21c4ccc06b3b2e588fb19f1a85d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-14 06:30:40 +0000

    prpm 0.1.17
    Closes #2637.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/p/prpm.rb
+++ Formula/p/prpm.rb
@@ -1,8 +1,8 @@
 class Prpm < Formula
   desc "Universal registry for AI coding tools"
   homepage "https://prpm.dev/"
-  url "https://registry.npmjs.org/prpm/-/prpm-0.1.16.tgz"
-  sha256 "b867777e9579287cb466edb615a6d0bd69e757dd1c3f24a6aea970b2cd815500"
+  url "https://registry.npmjs.org/prpm/-/prpm-0.1.17.tgz"
+  sha256 "89d8a442853e26e73e3c29f1d745ecb3101dd5e89628cbbc0ba11e5e43ae1b20"
   license "MIT"
 
   bottle do
