34e5c34e675103b23e61a5eeaee3c9b89eac38dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 20:27:54 +0000

    e1s: add 1.0.51 bottle.

--- Formula/e/e1s.rb
+++ Formula/e/e1s.rb
@@ -6,6 +6,15 @@ class E1s < Formula
   license "Apache-2.0"
   head "https://github.com/keidarcy/e1s.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "124254b8071bd2c12d297884f09847f9d9ea5ac99c090292128d5c1394d2af37"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "124254b8071bd2c12d297884f09847f9d9ea5ac99c090292128d5c1394d2af37"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "124254b8071bd2c12d297884f09847f9d9ea5ac99c090292128d5c1394d2af37"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9acf669670678bc540c572e29bb975cbb4736ac31dfc31181b7f3dc6d86deff1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "475f4d937ebd795ed25402325f04b04493d060431ebc3d8dfb2a8a9bf2b0b715"
+  end
+
   depends_on "go" => :build
 
   def install
