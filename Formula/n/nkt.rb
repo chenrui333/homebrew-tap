d928d6002aebd9f0aa2ec398f135e7809f16cc92
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 19:22:10 +0000

    nkt: add 0.3.1 bottle.

--- Formula/n/nkt.rb
+++ Formula/n/nkt.rb
@@ -6,6 +6,15 @@ class Nkt < Formula
   license "GPL-3.0-only"
   head "https://git.sr.ht/~fjebaker/nkt", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f7a83a41ac29910fd0e1056261c1cb98168e7a190025bd0577c650b084f5843a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "91ef94fb9cf73bf80c8da465f452e03c902d6df0f04c4a8c4700f10fda52361a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c006e2a1e3291d94e0dbe54c0b93927a695b4dd41f5a1af7a9aeb11743e129e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e644bab9bd1d5b494d72806c73d0bcc36da68e8c85170cd76f6a50e55deefae4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f3025c359b5955ada789071bed44c3ad913f5534a81acf99ea4c36b18e4321af"
+  end
+
   depends_on "zig" => :build
 
   def install
