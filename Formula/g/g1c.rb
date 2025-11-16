8e7e1d23a7344a56bb6f0d315e7173928df5e348
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 20:24:25 +0000

    g1c: add 0.2.0 bottle.

--- Formula/g/g1c.rb
+++ Formula/g/g1c.rb
@@ -6,6 +6,15 @@ class G1c < Formula
   license "Apache-2.0"
   head "https://github.com/nlamirault/g1c.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "83e3e8e7b18683ea019f745cc8ec2ee58d048b80dce9015864b16a13772ca4fe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "56fef57c840df090da8def99f9e2694d4070e887f65408c91a81cc9d8d8fe278"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "48589a8738d5643c1bf28c49093662f5331d49c16c290eb74e7eff40406307ff"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "10640ea46602d50109904d8071e6893656bd9fc935dafb1cd4a4879a1b6f3f0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "201257cf23d343981560bf91c5dc95d4c3545ef485fa13d2940a8e8e5279384d"
+  end
+
   depends_on "rust" => :build
 
   def install
