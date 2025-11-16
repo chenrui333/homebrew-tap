7f6eff734f168ad62430bf6d76c8cc0f1af05ffe
Rui Chen <rui@chenrui.dev>
2025-11-16 13:27:20 -0500

    bibiman 0.17.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/b/bibiman.rb
@@ -0,0 +1,32 @@
+class Bibiman < Formula
+  desc "TUI for fast and simple interacting with your BibLaTeX database"
+  homepage "https://codeberg.org/lukeflo/bibiman"
+  url "https://codeberg.org/lukeflo/bibiman/archive/v0.17.1.tar.gz"
+  sha256 "2aa25f6260581957d6d53896ca140eae8e7237be06abff0f972f03097efe8e44"
+  license "GPL-3.0-or-later"
+  head "https://codeberg.org/lukeflo/bibiman.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/bibiman --version")
+
+    test_file = testpath/"test.bib"
+    test_file.write("")
+
+    test_config = testpath/".config/bibiman/bibiman.toml"
+    test_config.write("")
+
+    output_log = testpath/"output.log"
+    pid = spawn bin/"bibiman", "--config-file", test_config.to_s, test_file.to_s, [:out, :err] => output_log.to_s
+    sleep 1
+    assert_match "Bibliographic Entries", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
