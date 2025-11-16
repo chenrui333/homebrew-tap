aaf04fc8aec02207df0e058bea00f3c5e1e6d4fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 18:27:22 +0000

    oatmeal: add 0.13.0 bottle.

--- Formula/o/oatmeal.rb
+++ Formula/o/oatmeal.rb
@@ -6,6 +6,15 @@ class Oatmeal < Formula
   license "MIT"
   head "https://github.com/dustinblackman/oatmeal.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "71afa42d950702d109a5e6a07aa72e0c85022f11feb5bd9822226ca5f1f8a8c6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ca662830c08fe280aed3ff5a4416932bb545a5c6482206ee5287ba7a5e308270"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "920878067d9b1448645c43ee3611ad08f00c47e9506cc69caead33183a5bb0e7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0f366ecd59256ea9d905f0955f62315175f1028573638a450732e9ce8d52a9b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8834fd16d76f34f2d9114e020d4005d5b55c1d17754133bbea734c4356e6b85b"
+  end
+
   depends_on "rust" => :build
 
   def install
