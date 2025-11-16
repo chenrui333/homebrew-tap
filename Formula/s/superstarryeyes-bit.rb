ce6ca4806f20bfcb37be2a9c0a0622d66fab4996
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 20:05:56 +0000

    superstarryeyes-bit: add 0.1.0 bottle.

--- Formula/s/superstarryeyes-bit.rb
+++ Formula/s/superstarryeyes-bit.rb
@@ -6,6 +6,15 @@ class SuperstarryeyesBit < Formula
   license "MIT"
   head "https://github.com/superstarryeyes/bit.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "895a8937eaa9e490b83aea8d061ee5e998926c3bae7e371d42d474e49299d52e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "895a8937eaa9e490b83aea8d061ee5e998926c3bae7e371d42d474e49299d52e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "895a8937eaa9e490b83aea8d061ee5e998926c3bae7e371d42d474e49299d52e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "872cdfcb92040098b458bdec59ffffbd45cac7f82c6c8a3e066984644a6e6d69"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9aa02fd33ba486beb4f31df124a9c2588b45c418387a215d03b21936b9f583dc"
+  end
+
   depends_on "go" => :build
 
   def install
