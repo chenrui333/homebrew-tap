02d80abb24726d7dedcfd2fca7c25f993ff35edc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-16 19:12:59 +0000

    bibiman: add 0.17.1 bottle.

--- Formula/b/bibiman.rb
+++ Formula/b/bibiman.rb
@@ -6,6 +6,15 @@ class Bibiman < Formula
   license "GPL-3.0-or-later"
   head "https://codeberg.org/lukeflo/bibiman.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d5a9d6cce1a6c4baf992f28d49393398584f4e144df9c7edc6fa76ba075faec1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f23fcca2d407cc17e29720c2a05222e49406cdce0c412892044b05c6a523a4fa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f0c894d33bec48c3bbe35b2ac6490c042eaebf4cc4c823d64d1f632b4fd7b56e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6e61e60bedf3cf327ce2cdd7fdb1ab9024c9d433682db9123f5be809c79f0cee"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "814517526cc2c12d2881806aa905b38b13db06c445975e0e44e4c819b5ae5496"
+  end
+
   depends_on "rust" => :build
 
   def install
