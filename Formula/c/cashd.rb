9d359b3fad033cff35f8062ab1a28c8e66e277e1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-17 01:00:12 +0000

    cashd: add 0.1.5 bottle.

--- Formula/c/cashd.rb
+++ Formula/c/cashd.rb
@@ -6,6 +6,15 @@ class Cashd < Formula
   license "MIT"
   head "https://github.com/hzqtc/cashd.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "56b5afe219494982016a8ff4f35491c8e95f6657df95dde682464faf36531c1d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "56b5afe219494982016a8ff4f35491c8e95f6657df95dde682464faf36531c1d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "56b5afe219494982016a8ff4f35491c8e95f6657df95dde682464faf36531c1d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1f64f7659449d699a579af5a2b0ed34debc9a73da44cbc2156c156f1eab0e49c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1a58cb6190c667bb80035d4f31f4ad4e7c431e62990e2442a1e09fe2d83844fa"
+  end
+
   depends_on "go" => :build
 
   def install
