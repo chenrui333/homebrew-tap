6c1f2aaf6e5e0240aea6487efef1bb28b2a41b53
Rui Chen <rui@chenrui.dev>
2025-11-20 21:58:18 -0500

    devcockpit 1.0.8 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/d/devcockpit.rb
@@ -0,0 +1,24 @@
+class Devcockpit < Formula
+  desc "TUI system monitor for apple silicon"
+  homepage "https://devcockpit.app/"
+  url "https://github.com/caioricciuti/dev-cockpit/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "a1ce6d16d46da379d88ca579f24d9d16c542b047c6dd3005637c2d45cf7c49e7"
+  license "MPL-2.0"
+  head "https://github.com/caioricciuti/dev-cockpit.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on arch: :arm64
+
+  def install
+    ENV["CGO_ENABLED"] = "1"
+
+    cd "app" do
+      system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}"), "./cmd/devcockpit"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/devcockpit --version")
+    assert_match "Log file location:", shell_output("#{bin}/devcockpit --logs")
+  end
+end
