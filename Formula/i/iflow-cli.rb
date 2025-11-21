cfddef26dd5ab84305a812f19a6cddc1899c74de
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-21 23:55:06 +0000

    iflow-cli: update 0.3.26 bottle.

--- Formula/i/iflow-cli.rb
+++ Formula/i/iflow-cli.rb
@@ -7,11 +7,11 @@ class IflowCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "64d13c481569b843e23600fbe8bff94286387d2e99f8d82ed9a0d72d90c28993"
-    sha256                               arm64_sequoia: "03027610a0f25058857dcac1a7f889cc3d9433120cc4965ff4196d30b16215a5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "76c81507df9236e6569b73d803d3bb2abba33c3bcc56ef4da05b60eaa91bde89"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "60347e82a98affe786dd7c8f4eaff8bc2e5753199f9033cb1ad9bac6f20b94af"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "afdc07831d9503effc1e59cd8120655ed785b5a86ce133429326d8ac54af3d70"
+    sha256                               arm64_tahoe:   "0bbc8481cacf61e5398ea28cba97f95cd902cf9fde18f23b36bcac1292e32503"
+    sha256                               arm64_sequoia: "1e0a70fb7413caa60ce815e098f5a930c46edd415a010c09ff45ff29f23f4c18"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "51e9beb1b2084669661eb8aea4a462b36b89913a424832398259e1a16d58cce5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e09815c5a69b14c4cec14774828289d4c415cfee4bcdffe3f8470ce0bd358d9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e9c9f4a6713380ec1850cca6f0955bdfcd1bbd7a830e5ed353c55c6aea230bb"
   end
 
   depends_on "node"
