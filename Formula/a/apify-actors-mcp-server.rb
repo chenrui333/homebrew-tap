befd2121179b3c4b9b77752e601143260e4301df
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-21 22:42:35 +0000

    apify-actors-mcp-server: update 0.5.6 bottle.

--- Formula/a/apify-actors-mcp-server.rb
+++ Formula/a/apify-actors-mcp-server.rb
@@ -7,7 +7,7 @@ class ApifyActorsMcpServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "103b0dcc98afcf4af0b7519bf699dc72ad103c3c1907ea003a91774200702718"
+    sha256 cellar: :any_skip_relocation, all: "3ee146814a41f05936e77762fcffb3d2c8c661b77bdf667bfecc00b08a609e61"
   end
 
   depends_on "node"
