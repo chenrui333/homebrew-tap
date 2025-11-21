47d242d0b937b395ed75ab5ff037529854f111d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-21 23:29:32 +0000

    cozypkg: update 1.4.0 bottle.

--- Formula/c/cozypkg.rb
+++ Formula/c/cozypkg.rb
@@ -8,11 +8,11 @@ class Cozypkg < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d832a106205cd49171cff789d96ad937a1ac60581a02f993917e56632a7a2053"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1324999ab50221cb460c0ba7a5dbce33ccf6ee2ac3d1c9588a0751a68ec7e3fd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ae44bd6d5257d089c37f6bc9f78b57e30a504ef6eb2b6e4ac06fe52bfdd06283"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7edc7bfb1e836dddfd4c6bd913b22d75dce4ace467e3ac03d09cdd8b18d5191a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1637ada418e8081f48b76f6695099d91aa837b5fccdd0fcd82ad6ba2cbe93280"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b65253e975bdb409687f7690b4cd6530f918ec82b8b6fce9cd3b591cf1793e13"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "84153ba6099285721318dade9062d1fbee83effe51a9354a5ed8dafe797b15ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b304cbb40a753342a2297b8a0d15e22d8fccf5898b48a59158aa260dc74b023c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "722aca783d8929594e26f6d4de67ca70971b041c748184e2fb630ed71cd5c394"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "32099d0cca88f5da529c0fb77f3e29700a50c293b18ad084954bc4c853585993"
   end
 
   depends_on "go" => :build
