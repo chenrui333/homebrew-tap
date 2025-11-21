9b11240b9996953ce1c354d1c9c1ab2bca78cd0f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-21 02:01:22 +0000

    cgpu: add 0.1.3 bottle.

--- Formula/c/cgpu.rb
+++ Formula/c/cgpu.rb
@@ -5,6 +5,11 @@ class Cgpu < Formula
   sha256 "1716420d6b30833402a3371986490791f26afce6fc128edc35000c53a0d835b6"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, all: "4632e1395cdda80a24d602bc4ab35e9c8b89ef96521b2fea0f46a7994e7fe210"
+  end
+
   depends_on "node"
 
   def install
