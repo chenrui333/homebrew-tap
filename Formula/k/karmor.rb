72a8edbef7dc9f02ec53d7be4fa75cd627fb1398
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-22 00:18:27 +0000

    karmor: update 1.4.6 bottle.

--- Formula/k/karmor.rb
+++ Formula/k/karmor.rb
@@ -8,10 +8,11 @@ class Karmor < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ec070ab9778cdb7dfaa3a7466d6037268509053015840aa579ad73d34d1623c5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b4e66c497b3653fccb8022cc0275909f8dda747a2ad7f7a1310753ec89f86056"
-    sha256 cellar: :any_skip_relocation, ventura:       "d013d6733d24943988a803c628f7123e5a0bc2717e9b426efd710ef0d0e1957b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "acde295f99275e2064df2d511427f7ddbe2e513a635fa5f71b630e8676b24f0e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fe7587835673493d6f9ff5a8c01b12b992bfd293d94b07922cde7df2d3d8c43d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "318685091a580da8832eccdebca24e88fa3b0478b18ec96e81c9af0eefe40bd5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c81ce17eb38d8aaa34c2dcdf6ff68fdbaf156443fbf6994650b78e9567ee543"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "caff006684e1afee57a9cf3719c661bc923882f55d78bd73e143269ee999c2dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f9af6ac0e3fa6ff2f3fd34168eda4b0ef4cdb682bfc2fb5aa35a98dd4d1f19fe"
   end
 
   depends_on "go" => :build
