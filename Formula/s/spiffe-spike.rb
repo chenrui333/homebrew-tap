4e4ec5ce380d7781c4bea7594c1e1baea9a8bc33
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-22 23:06:52 +0000

    spiffe-spike: add 0.4.2 bottle.

--- Formula/s/spiffe-spike.rb
+++ Formula/s/spiffe-spike.rb
@@ -6,6 +6,15 @@ class SpiffeSpike < Formula
   license "Apache-2.0"
   head "https://github.com/spiffe/spike.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "399987b12064686b81a71fdfb2a11c1ba5617a752430ca6d3e105561eee0248f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f5a36422414eaa99b1ba0f1e3f36bed70eac358f0d8de7fd54d7ac4c6ac2ea8e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fe7c790d2cfff9f85d9eabf50145636ee87493d5e07475eaaaa6712a05284edb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "23ec523b1216cb184c7797c26750b074d372fa397fbf4f2552a59ea173619f9d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9466e87f70a90c98362ca244a5b883eb4e579cd5008c00f0ace97d409bec657"
+  end
+
   depends_on "go" => :build
   uses_from_macos "sqlite"
 
