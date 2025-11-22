3eb684214a7535de642e575fedd2f469feee0fb5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-22 05:24:07 +0000

    qqqa: add 0.10.0 bottle.

--- Formula/q/qqqa.rb
+++ Formula/q/qqqa.rb
@@ -6,6 +6,15 @@ class Qqqa < Formula
   license "MIT"
   head "https://github.com/iagooar/qqqa.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "144c701460c95624c95192be206ba8d53d6f358ccb62f382d9ef5c706cc70267"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0200f138ddfbc86987e44b0e9d66520e5f8c0652c479320696a4ae9bb4a9adf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3029c4a1788f7f1277f8e5a9cae9aab5ca8ce3bc96f3ef5554bc7369aa848f5b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7ed7f2b32a72993867baaeb09cca84c2c882f79bb029d0777acc8529b36c3341"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c049a93fce5221fd6faf8c92915ee8250d9fa9665c3063f8f5c58b45ea456c04"
+  end
+
   depends_on "rust" => :build
 
   def install
