348a49dc6fe28612fe95095b2e03317fad151f1a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-22 00:23:49 +0000

    flipt: update 2.4.0 bottle.

--- Formula/f/flipt.rb
+++ Formula/f/flipt.rb
@@ -8,11 +8,11 @@ class Flipt < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "629b4bcdbe61fe380c2f45b6a49f04a464d9c263a2a52065b1f12156cf93f788"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80cc727d5ae784b3d1e734d7b8ef2a0493beaeb011323e78f33379d6b76830a6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "23801c6b3a5ed6febdf9eb74881eef21c928877086e62111ef671f8cb38da978"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "72e9a5354aae6e462f8f00e71659f0f231da178ee589031fedd267b4c940ce41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "676f9c48bc9b5fca90e8f7644714e5d4835aa9f3c27ba00decdb46eec5048583"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "695f2c76ade42696d1ade08aae093ba0b7e764e3f86c97fa41de8e0a7351bd47"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1de87686cf40293eeb5a011d990cd35acf3e679a239bdc493a27e70b01909fa9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "168391f138b172811de2db12cd6d1b15d9db1d4307f1494461a3b73f776a6635"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c2acd992bf8666f247b81e77134930642d851962647d94a6a73da20f70b4418b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "904bab0a13a86ab46ad3604ef613f8af177fe8820a1bc59edab29b34a7e08526"
   end
 
   depends_on "go" => :build
