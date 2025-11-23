cff45abcd34dae6af584a4037f4fc53d7ba5c720
Rui Chen <rui@chenrui.dev>
2025-11-23 15:26:05 -0500

    relay 1.0.4 (new formula)
    Closes #2803.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/r/relay.rb
@@ -0,0 +1,27 @@
+class Relay < Formula
+  desc "Simple tunneling with random 3-word subdomains; a self-hosted ngrok alternative"
+  homepage "https://github.com/talyuk/relay"
+  url "https://registry.npmjs.org/@talyuk/relay/-/relay-1.0.4.tgz"
+  sha256 "ca49e43fe8f334a037448ab8bdcc7bb0351aee7fceebc9eac8fde6807be2049b"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/relay --version")
+
+    output_log = testpath/"output.log"
+    pid = spawn bin/"relay", "3000", "--server", "tunnel.example.com",
+                    "--secret", "your-secret", [:out, :err] => output_log.to_s
+    sleep 1
+    assert_match "WebSocket error: getaddrinfo ENOTFOUND tunnel.example.com", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
