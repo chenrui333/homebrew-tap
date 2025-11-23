658d2ecf44ed45a2b403d1ec62c89d777299c74c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-23 03:10:07 +0000

    spiffe-spike: update 0.6.0 bottle.

--- Formula/s/spiffe-spike.rb
+++ Formula/s/spiffe-spike.rb
@@ -8,11 +8,11 @@ class SpiffeSpike < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "399987b12064686b81a71fdfb2a11c1ba5617a752430ca6d3e105561eee0248f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f5a36422414eaa99b1ba0f1e3f36bed70eac358f0d8de7fd54d7ac4c6ac2ea8e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fe7c790d2cfff9f85d9eabf50145636ee87493d5e07475eaaaa6712a05284edb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "23ec523b1216cb184c7797c26750b074d372fa397fbf4f2552a59ea173619f9d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e9466e87f70a90c98362ca244a5b883eb4e579cd5008c00f0ace97d409bec657"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "88070e72195e8a59aa485585a37a8b9b49745d331b5281136fea459f2e0922b4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "55252aafff9b5c7061defacaac15c18d02ffee74c167dfa86d44196b5648e6fa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8296e12ac9d8dce771f0bc7fbf807a2d10916cc1f083b504e485481d2feb0b71"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "78e831986cd7d7b30f835697f4b17f101f72cebbcd0286b5fe21f7cfbab7ed52"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c832d28e1ede5eb4b43c292b395008f699ce003fb78987b08b29bdd52d31d9a"
   end
 
   depends_on "go" => :build
