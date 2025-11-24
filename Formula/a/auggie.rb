351f0a8eb8721c7313af216aaaac088653a54774
Rui Chen <rui@chenrui.dev>
2025-11-24 00:39:06 -0500

    auggie 0.9.1 (new formula)
    Closes #2813.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/a/auggie.rb
@@ -0,0 +1,21 @@
+class Auggie < Formula
+  desc "All the power of Augment Code in your terminal"
+  homepage "https://www.augmentcode.com/product/CLI"
+  url "https://registry.npmjs.org/@augmentcode/auggie/-/auggie-0.9.1.tgz"
+  sha256 "62abb347a91eabca63bee85e460378d12a7c69f0a238dff138253c865ae5e505"
+  # license :unfree
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/auggie --version")
+
+    assert_match "Total: 14 tools", shell_output("#{bin}/auggie tools list")
+    assert_match "You are not currently logged in to Augment", shell_output("#{bin}/auggie model list 2>&1", 1)
+  end
+end
