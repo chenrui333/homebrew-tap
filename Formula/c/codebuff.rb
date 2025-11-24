6ff2a933f6cac3d438688feec8331f9d0411ba39
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-24 04:32:35 +0000

    codebuff: add 1.0.527 bottle.

--- Formula/c/codebuff.rb
+++ Formula/c/codebuff.rb
@@ -5,6 +5,11 @@ class Codebuff < Formula
   sha256 "1206ed26a548641717f57c542943758a9d7cbfbd5526b7a84e7d93ac4ec68932"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, all: "dc044b79576bfc8adcc913190f65d5cc54c70b0605aeefcd0866beb803524a9d"
+  end
+
   depends_on "node"
 
   def install
