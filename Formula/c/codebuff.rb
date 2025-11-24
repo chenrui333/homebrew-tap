7c0a5e2d4d01f1a4d327af11f98252c7f5ad03f4
Rui Chen <rui@chenrui.dev>
2025-11-23 23:20:13 -0500

    codebuff 1.0.527 (new formula)
    Closes #2808.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/c/codebuff.rb
@@ -0,0 +1,18 @@
+class Codebuff < Formula
+  desc "Generate code from the terminal"
+  homepage "https://www.codebuff.com/"
+  url "https://registry.npmjs.org/codebuff/-/codebuff-1.0.527.tgz"
+  sha256 "1206ed26a548641717f57c542943758a9d7cbfbd5526b7a84e7d93ac4ec68932"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/cb --version")
+  end
+end
