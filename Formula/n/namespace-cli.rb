debee38a66f81dae01afe5547302016d65434951
Rui Chen <rui@chenrui.dev>
2025-11-23 22:59:34 -0500

    namespace-cli 0.0.452 (new formula)
    Closes #2806.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/n/namespace-cli.rb
@@ -0,0 +1,23 @@
+class NamespaceCli < Formula
+  desc "Command-line interface for the Namespaces platform"
+  homepage "https://github.com/namespacelabs/foundation"
+  url "https://github.com/namespacelabs/foundation.git",
+      tag:      "v0.0.452",
+      revision: "011665d6e5f41457d5d45384201e1df42fe57cfc"
+  license "Apache-2.0"
+  head "https://github.com/HarishChandran3304/better-env.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X namespacelabs.dev/foundation/internal/cli/version.Tag=#{version}"
+    system "go", "build", *std_go_args(ldflags:, output: bin/"nsc"), "./cmd/nsc"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/nsc version")
+
+    assert_match "not logged in", shell_output("#{bin}/nsc list 2>&1", 1)
+    assert_match "failed to get authentication token", shell_output("#{bin}/nsc registry list 2>&1", 1)
+  end
+end
