aba79ef2b54c768e50a7435b0253d2b3e367b190
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-26 02:29:24 +0000

    treemd: add 0.3.2 bottle.

--- Formula/t/treemd.rb
+++ Formula/t/treemd.rb
@@ -6,6 +6,15 @@ class Treemd < Formula
   license "MIT"
   head "https://github.com/epistates/treemd.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "34ff6b60ffa5eb24e7134f7bfc8c0a2d31e1d3bcd6831d8bd25a6ebb3fd1879b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41d1c1c1123f1972fb352f2be9617dd2bea8ee162e5ffd623b59c0d9202e4f7e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1747d6a904e0e0e412f394c155c52b074d66494ca0063e6736ef688f3dbb5bb1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "55ab78843d6b8cd53e8d1ec686881c459f8c8bbaa32dd85ad147b0a7b218d0b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f79afeb7416830b0d144050b0a0ac5caf78f52acfdd20117239e7ff66ab4a080"
+  end
+
   depends_on "rust" => :build
 
   def install
