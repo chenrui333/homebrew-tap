cd300c822f39940638d097869abb57c6a83b69ac
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 22:03:25 +0000

    bbrew: add 2.1.1 bottle.

--- Formula/b/bbrew.rb
+++ Formula/b/bbrew.rb
@@ -6,6 +6,15 @@ class Bbrew < Formula
   license "MIT"
   head "https://github.com/Valkyrie00/bold-brew.git", branch: "trunk"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7909609b570748693d73b01f3df7274cfa2ec8200161612d5580e083a30454b6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7909609b570748693d73b01f3df7274cfa2ec8200161612d5580e083a30454b6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7909609b570748693d73b01f3df7274cfa2ec8200161612d5580e083a30454b6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5b34fb33416e1c75bfaeacbc7f17d4885d16395d94d2af3802df3dc2a201b191"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6757ef20a9c19f4720b5bca25d3c0479d10c3512344d9be84b94770bef0ca418"
+  end
+
   depends_on "go" => :build
 
   def install
