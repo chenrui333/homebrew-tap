58efe81e739210fdde138446eb29cee99450d129
Rui Chen <rui@chenrui.dev>
2025-11-27 19:41:39 -0500

    qmassa 1.1.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/q/qmassa.rb
@@ -0,0 +1,21 @@
+class Qmassa < Formula
+  desc "TUI for displaying GPUs usage stats on Linux"
+  homepage "https://github.com/ulissesf/qmassa"
+  url "https://github.com/ulissesf/qmassa/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "a65763543858b7042d91756bbd9afe45a3c3dddea7b845115e9e8ba4625044f3"
+  license "Apache-2.0"
+  head "https://github.com/ulissesf/qmassa.git", branch: "main"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+  depends_on :linux
+  depends_on "systemd" # for `libudev`
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/qmassa --version")
+  end
+end
