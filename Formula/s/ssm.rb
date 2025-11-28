1c76fc431e7fcf5d840a6c916adbade6766c8f4a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 21:52:38 +0000

    ssm: add 0.4.0 bottle.

--- Formula/s/ssm.rb
+++ Formula/s/ssm.rb
@@ -6,6 +6,15 @@ class Ssm < Formula
   license "BSD-3-Clause"
   head "https://github.com/lfaoro/ssm.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5f2d9819122f708355e8bfb2bcdfb451ec63868be70705bfd632c82b44732cbc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5f2d9819122f708355e8bfb2bcdfb451ec63868be70705bfd632c82b44732cbc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5f2d9819122f708355e8bfb2bcdfb451ec63868be70705bfd632c82b44732cbc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e5d9c2be4582ce6eea973c0317b0e6da1a572df5559eecfcca0b518d3934e8d1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1186f8ca5236dfee2170f37ae7d8037103f26077fe8e299889f079edd0bee7cb"
+  end
+
   depends_on "go" => :build
 
   def install
