7e4dcd9fc2ad4591f2f5533480c5e33e23df26f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 20:05:38 +0000

    awsesh: add 0.1.11 bottle.

--- Formula/a/awsesh.rb
+++ Formula/a/awsesh.rb
@@ -6,6 +6,15 @@ class Awsesh < Formula
   license "MIT"
   head "https://github.com/elva-labs/awsesh.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "87086541027d05c6a72a5d6bbc425446c46c7ebb8190d9812e23cbaac1471072"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "87086541027d05c6a72a5d6bbc425446c46c7ebb8190d9812e23cbaac1471072"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "87086541027d05c6a72a5d6bbc425446c46c7ebb8190d9812e23cbaac1471072"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9d8d0e214c4914cc89cbae9b13a038433154e6b12f15306f1d1005c67a45ca0f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "40dc8ad43b322ffd9e6929de3af0c0eaaf436c86d80ef16384ae226312b4fc8f"
+  end
+
   depends_on "go" => :build
 
   def install
