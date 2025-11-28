100ddcfaa312e347dd311f3a12c659edbe46b187
Rui Chen <rui@chenrui.dev>
2025-11-27 20:49:36 -0500

    yatto 0.21.7 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/y/yatto.rb
@@ -0,0 +1,44 @@
+class Yatto < Formula
+  desc "Interactive VCS-based todo-list for the command-line"
+  homepage "https://github.com/handlebargh/yatto"
+  url "https://github.com/handlebargh/yatto/archive/refs/tags/v0.21.7.tar.gz"
+  sha256 "153f0ab84959489cd24e798b345469f613742fc0f30ae459cbd4559e02cff544"
+  license "MIT"
+  head "https://github.com/handlebargh/yatto.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/handlebargh/yatto/internal/version.version=#{version}
+      -X github.com/handlebargh/yatto/internal/version.revision=#{tap.user}
+      -X github.com/handlebargh/yatto/internal/version.revisionDate=#{time.iso8601}
+    ]
+
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"yatto", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+  end
+
+  test do
+    # assert_match version.to_s, shell_output("#{bin}/yatto version")
+    # Version:	(devel)
+    # Revision:	chenrui333
+    # RevisionDate:	2025-11-17T17:30:31Z
+    # GoVersion:	go1.25.4
+    system bin/"yatto", "version"
+
+    (testpath/".config/yatto/config.toml").write <<~TOML
+      [git]
+      default_branch = 'main'
+
+      [git.remote]
+      enable = true
+      name = 'origin'
+      url = 'chenrui333/homebrew-tap'
+    TOML
+    output = shell_output("#{bin}/yatto config show")
+    assert_match "[git]\ndefault_branch = 'main'", output
+  end
+end
