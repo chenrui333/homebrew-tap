4e867bc0b56bdb91cc6b92d3eb69f24cf3205788
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 22:51:32 +0000

    jwt-ui: add 1.3.0 bottle.

--- Formula/j/jwt-ui.rb
+++ Formula/j/jwt-ui.rb
@@ -6,6 +6,15 @@ class JwtUi < Formula
   license "MIT"
   head "https://github.com/jwt-rs/jwt-ui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "82724ff9b5253a63ea575b3774b4ed9d01e093f0e96dafeef30e3f0ddfd3fccd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "16a04fd709300ed69b14391cf9752b062b2534a905e05539b033166339cab2e3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "19c6315347fba7ec1dbd8f1703efe36b8104361f4aa61596abf130d74386b765"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "442467f0409bae4636383f2008eafc824d25883fafe8e55f78815527653d9c13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "682df4fbdefce58c74e7605e4358f20b97c1d84d2df44a0d67dc80f99198c151"
+  end
+
   depends_on "rust" => :build
 
   def install
