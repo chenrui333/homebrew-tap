85f240a23f40443cf3b7f1e1e9e2f6c7a15ca06a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 01:01:38 +0000

    qmassa: add 1.1.0 bottle.

--- Formula/q/qmassa.rb
+++ Formula/q/qmassa.rb
@@ -6,6 +6,12 @@ class Qmassa < Formula
   license "Apache-2.0"
   head "https://github.com/ulissesf/qmassa.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "c345181198ce564414e823649c0c07e85e68f67c183d8b82472c504809005de9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cd023225c2e3f973e379307487febde9651f285d2e82c59b1e608b6ba7971b49"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on :linux
