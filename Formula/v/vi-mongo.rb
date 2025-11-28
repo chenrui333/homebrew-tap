06895be9f6a24617f4c3ab66d31611ec7e8dceda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 20:14:02 +0000

    vi-mongo: add 0.1.31 bottle.

--- Formula/v/vi-mongo.rb
+++ Formula/v/vi-mongo.rb
@@ -6,6 +6,15 @@ class ViMongo < Formula
   license "Apache-2.0"
   head "https://github.com/kopecmaciej/vi-mongo.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c58a3d62502c383799f8048409ad0a3aae691ef18321d9a742f22d151e1ae33d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c58a3d62502c383799f8048409ad0a3aae691ef18321d9a742f22d151e1ae33d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c58a3d62502c383799f8048409ad0a3aae691ef18321d9a742f22d151e1ae33d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ad4bf855f834b331e0baa80fb4b2f918c53ed07af41e724cde53d1c85ac74a10"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a3a6d16d1a62b7f7f9d93d50cf87cd5216d9ae3aba252ac5cc01c869a1d5775f"
+  end
+
   depends_on "go" => :build
 
   def install
