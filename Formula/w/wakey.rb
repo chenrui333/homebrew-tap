668f3bc9686ea2166cb8c8846582f3152af751d5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 21:00:45 +0000

    wakey: add 1.1.0 bottle.

--- Formula/w/wakey.rb
+++ Formula/w/wakey.rb
@@ -6,6 +6,15 @@ class Wakey < Formula
   license "MIT"
   head "https://github.com/jonathanruiz/wakey.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6e1559874e2ca5862cafa2177f623b89867df2735408486d3552c91479c94bca"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6e1559874e2ca5862cafa2177f623b89867df2735408486d3552c91479c94bca"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6e1559874e2ca5862cafa2177f623b89867df2735408486d3552c91479c94bca"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2382f01cf4fb79aa99658bf20c64ecbb03b92c923f169562aedf6e1660f9b836"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e6650d2e84c102bec787ce43112b736a941d285682019b04d62420889e4218a"
+  end
+
   depends_on "go" => :build
 
   def install
