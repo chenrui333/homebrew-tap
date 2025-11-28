ac8fadf39eeeda0edf794429a9137218ba7c1fbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-28 01:47:37 +0000

    apisnip: add 1.4.59 bottle.

--- Formula/a/apisnip.rb
+++ Formula/a/apisnip.rb
@@ -6,6 +6,15 @@ class Apisnip < Formula
   license "MIT"
   head "https://github.com/Tuurlijk/apisnip.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e40b12efe931be6bb01b43bb7462ff7922914d83e2306537433f967e472976f8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "15d356a3981d5792b95cf6346deb973e21dee4e03bb70a428af51f3602f93ca5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1216562f5b4bc26b56961eeee9313f0207f725928b0b9c26d5d73bd6ec506eb5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "21aaf8e5338f235c9686071830039bbe6b174912dc765e0594c872025d1a39be"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c1173c83d9d0171ff5146cc5766bd385e194e00dbcbf84178c3f44be9a7e1565"
+  end
+
   depends_on "rust" => :build
 
   def install
