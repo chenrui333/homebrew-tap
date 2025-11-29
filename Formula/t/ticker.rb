d66ace9381b79317b1e4a57a3eef1918e0ad695d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 15:40:13 +0000

    ticker: add 5.1.0 bottle.

--- Formula/t/ticker.rb
+++ Formula/t/ticker.rb
@@ -6,6 +6,15 @@ class Ticker < Formula
   license "GPL-3.0-only"
   head "https://github.com/achannarasappa/ticker.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "badd168f5c4c64cf020baf69d723f259d8e39f9de873717f0c9a63631b46d1da"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "badd168f5c4c64cf020baf69d723f259d8e39f9de873717f0c9a63631b46d1da"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "badd168f5c4c64cf020baf69d723f259d8e39f9de873717f0c9a63631b46d1da"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7e5aa78ab67943b18461bb5ad7bdc1b75c5bf1f69472637a7a0d87ca7a5b5991"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8235949a32788a76abfb164387d3c581751c875305c909902ed9f78101b9b45b"
+  end
+
   depends_on "go" => :build
 
   def install
