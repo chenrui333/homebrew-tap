2d1ecfd4bc3c0277256ef65d9b67ac01d2007c2f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 00:43:17 +0000

    spiffe-spike 0.8.0
    Closes #2944.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s/spiffe-spike.rb
+++ Formula/s/spiffe-spike.rb
@@ -1,8 +1,8 @@
 class SpiffeSpike < Formula
   desc "Lightweight secrets store using SPIFFE as its identity control plane"
   homepage "https://spike.ist/"
-  url "https://github.com/spiffe/spike/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "649a3dd1df79f4f1952d93d65c73a070b946c74847e92be6287b1ec09cfa6a14"
+  url "https://github.com/spiffe/spike/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "6cc31ed9b8b9890e83deb280065ed5d247562aab6b7e88e659bd66548ced5b4a"
   license "Apache-2.0"
   head "https://github.com/spiffe/spike.git", branch: "main"
 
