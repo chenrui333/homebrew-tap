4ccbd3077babfc67e66ad602037105d4fa4f4b7e
Rui Chen <rui@chenrui.dev>
2025-11-29 00:33:15 -0500

    passepartui 0.1.7 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/passepartui.rb
@@ -0,0 +1,28 @@
+class Passepartui < Formula
+  desc "TUI for pass"
+  homepage "https://github.com/kardwen/passepartui"
+  url "https://github.com/kardwen/passepartui/archive/refs/tags/v0.1.7.tar.gz"
+  sha256 "a0f518ff699a88f721ac9d90646aa3e8c82f99acdb58d915dada317d8fd1fa95"
+  license "GPL-3.0-only"
+  head "https://github.com/kardwen/passepartui.git", branch: "main"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+  depends_on "gpgme"
+  depends_on "libgpg-error"
+  depends_on "pass"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    output_log = testpath/"output.log"
+    pid = spawn bin/"passepartui", [:out, :err] => output_log.to_s
+    sleep 1
+    assert_match "Password file", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
