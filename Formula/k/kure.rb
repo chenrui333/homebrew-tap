2fb27d0b43d0b214198ea891c3fe892f02bb1881
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 05:42:26 +0000

    kure: add 1.3.0 bottle.

--- Formula/k/kure.rb
+++ Formula/k/kure.rb
@@ -6,6 +6,15 @@ class Kure < Formula
   license "Apache-2.0"
   head "https://github.com/GGP1/kure.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "435986ffb19fd6aae6bb90dcd949131d1f29d1b4045ae3a3b46584bbd0aa490f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "435986ffb19fd6aae6bb90dcd949131d1f29d1b4045ae3a3b46584bbd0aa490f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "435986ffb19fd6aae6bb90dcd949131d1f29d1b4045ae3a3b46584bbd0aa490f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ef79f2a234ad96affcfcd374bfcdb5db8905a8b80dd079fc2771c2df4d580e64"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "73247f2c2f8b605733164585b3eea475e4d5d8f9e43f729a91d78aef9dbfb7c0"
+  end
+
   depends_on "go" => :build
 
   def install
