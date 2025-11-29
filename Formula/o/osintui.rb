d118e09a611ff20cefa271485d26ff0218b3d060
Rui Chen <rui@chenrui.dev>
2025-11-29 00:08:52 -0500

    osintui 0.1.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/o/osintui.rb
@@ -0,0 +1,24 @@
+class Osintui < Formula
+  desc "Open Source Intelligence Terminal User Interface"
+  homepage "https://github.com/wssheldon/osintui"
+  url "https://static.crates.io/crates/osintui/osintui-0.1.1.crate"
+  sha256 "732444225882845e6148e0fcc1ab4351454180014eb605f2133c490a1314b703"
+  license "MIT"
+  head "https://github.com/wssheldon/osintui.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    output_log = testpath/"output.log"
+    pid = spawn bin/"osintui", [:out, :err] => output_log.to_s
+    sleep 1
+    assert_match "Integrations", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
