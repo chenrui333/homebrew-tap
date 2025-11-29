e2c737c3fda6d514a23c7441b60437fc5c9a2a00
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 01:50:45 +0000

    kdash: add 0.6.2 bottle.

--- Formula/k/kdash.rb
+++ Formula/k/kdash.rb
@@ -6,6 +6,15 @@ class Kdash < Formula
   license "MIT"
   head "https://github.com/kdash-rs/kdash.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "97fed814d759ea4b2411b8ec752e0145bf081befbe47e0dd588236d6b01510e3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "43a8a1d30b7165f911c99e08ea5507f2d1627474237f81a8c737911f011a7e58"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "850acfeb8746910cac7c8b1313ac981691b6ab4f4223bbab146dcb8c8965d5dc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c233584395f8f07316fa57c24b670bf0c0008e2ce08ac946f49eee8c7a7ebd0a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b4c3ddeaef8e73bf96d5db16e19298b1bf42b04fff6b54f9c9e29e9b86b730eb"
+  end
+
   depends_on "rust" => :build
 
   def install
