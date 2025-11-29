6abf117fce1b823eaf79a24104d3c7f3b710104e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-11-29 05:30:55 +0000

    pwdsafety: add 0.4.1 bottle.

--- Formula/p/pwdsafety.rb
+++ Formula/p/pwdsafety.rb
@@ -6,6 +6,15 @@ class Pwdsafety < Formula
   license "GPL-3.0-only"
   head "https://github.com/edoardottt/pwdsafety.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5ecf822ea3c2567858ba10f3fcba23b0dc2da652b7d63a3bbd7edcdf220cfa99"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5ecf822ea3c2567858ba10f3fcba23b0dc2da652b7d63a3bbd7edcdf220cfa99"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5ecf822ea3c2567858ba10f3fcba23b0dc2da652b7d63a3bbd7edcdf220cfa99"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "eddb9a1c9bb4b4504382f942da9cf683b5ae3546c9e8f3c68095d242adf998a5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e8ae2e0631ce9bf5c1b4e09d45d468c2309537f9ab0ff3e09b811a2a32b85759"
+  end
+
   depends_on "go" => :build
 
   def install
