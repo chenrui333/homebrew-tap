ac0fbc7da23564da3fde86a8ee28cd9757ae0161
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-03 18:39:40 +0000

    astro-language-server: update 2.16.2 bottle.

--- Formula/a/astro-language-server.rb
+++ Formula/a/astro-language-server.rb
@@ -7,11 +7,7 @@ class AstroLanguageServer < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "613096984512800f598bd0f838c8a5f0ae800bb31dedf60cea1b99f84a700315"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "569cab91532ce7ee6dd80514a7adb5fbf5f6fc5b63e0f7d686b20ac66bff9c5f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f9da93bca140ca6c1af64eb601ec283f7411ceee8d908aff935a7e05b9b55cca"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0b89d4081532e22de83a2453957a8cc0c6838938214977e2cd3b0e6ef784d9d8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "76cf5505db3d05408b636407fbf5a484ef7823c9b81bdc5fb9d8705832f3f243"
+    sha256 cellar: :any_skip_relocation, all: "3509d17655db4ca34f6322a135b90932a89486daaa22b1b225e83c2da0ac9c07"
   end
 
   depends_on "node"
