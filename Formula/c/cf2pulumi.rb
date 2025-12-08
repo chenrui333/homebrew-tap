87194e45b8b952e1ca5e875f6408a79a425bee11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-08 12:30:22 +0000

    cf2pulumi 1.39.0
    Closes #3103.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/cf2pulumi.rb
+++ Formula/c/cf2pulumi.rb
@@ -2,8 +2,8 @@ class Cf2pulumi < Formula
   desc "Convert CloudFormation Templates to Pulumi programs"
   homepage "https://github.com/pulumi/pulumi-aws-native"
   url "https://github.com/pulumi/pulumi-aws-native.git",
-      tag:      "v1.38.0",
-      revision: "56aabc05231e1b19260b7e2f436812ca1b3777d4"
+      tag:      "v1.39.0",
+      revision: "7fe3a5d467ea89fa048997511273fc129685a531"
   license "Apache-2.0"
   head "https://github.com/pulumi/pulumi-aws-native.git", branch: "master"
 
