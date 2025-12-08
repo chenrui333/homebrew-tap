5dc1b0cefbc5f1e69596789a9ede7d91f658e4bf
Rui Chen <rui@chenrui.dev>
2025-12-07 20:04:33 -0500

    iam-expand 0.11.44 (new formula)
    Closes #3089.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/i/iam-expand.rb
@@ -0,0 +1,26 @@
+class IamExpand < Formula
+  desc "Expand IAM Actions with Wildcards"
+  homepage "https://iam.cloudcopilot.io/tools/iam-expand"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-expand/-/iam-expand-0.11.44.tgz"
+  sha256 "32ae36c16e40d59ac9c3e8edc18dd471d6c34816f09e934d7819d6e67d2bfe2c"
+  license "AGPL-3.0-or-later"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/iam-expand --version")
+
+    assert_equal <<~EOS, shell_output("#{bin}/iam-expand 's3:*Object'")
+      s3:DeleteObject
+      s3:GetObject
+      s3:PutObject
+      s3:ReplicateObject
+      s3:RestoreObject
+    EOS
+  end
+end
