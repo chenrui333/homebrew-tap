11716d20607941b6e8bf1edd52406f64c89b6bd5
Rui Chen <rui@chenrui.dev>
2025-12-07 19:59:03 -0500

    iam-lens 0.1.67 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/i/iam-lens.rb
@@ -0,0 +1,26 @@
+class IamLens < Formula
+  desc "Google Maps for AWS IAM"
+  homepage "https://github.com/cloud-copilot/iam-lens"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-lens/-/iam-lens-0.1.67.tgz"
+  sha256 "912773d6ed90a676b32b1cef0ffc9bd1b0363cb009cf234a8994811288367014"
+  license "AGPL-3.0-or-later"
+
+  depends_on "iam-collect"
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/iam-lens --version")
+
+    system "iam-collect", "init"
+
+    output = shell_output("#{bin}/iam-lens simulate --principal arn:aws:iam::123456789012:role/ExampleRole " \
+                          "--resource arn:aws:s3:::example-bucket/secret-file.txt " \
+                          "--action s3:GetObject 2>&1", 1)
+    assert_match "Unable to find account ID for resource", output
+  end
+end
