d83d15802002959e15c146a72bfabd7fb61963ad
Rui Chen <rui@chenrui.dev>
2025-12-07 19:54:44 -0500

    iam-collect 0.1.160 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/i/iam-collect.rb
@@ -0,0 +1,23 @@
+class IamCollect < Formula
+  desc "Collect IAM information from all your AWS organization, accounts, and resources"
+  homepage "https://github.com/cloud-copilot/iam-collect"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-collect/-/iam-collect-0.1.160.tgz"
+  sha256 "c16d9a45a08c8db9bc17e30a9af52d7f6e737a28a7a2471315df0a5fb65186a2"
+  license "AGPL-3.0-or-later"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/iam-collect --version")
+
+    system bin/"iam-collect", "init"
+    assert_path_exists testpath/"iam-collect.jsonc"
+
+    assert_match "Could not load credentials from any providers", shell_output("#{bin}/iam-collect download 2>&1", 1)
+  end
+end
