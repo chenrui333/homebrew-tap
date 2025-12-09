d7d510b9c2ced9033958f0e7bf11c24a77a6378c
Rui Chen <rui@chenrui.dev>
2025-12-08 22:05:42 -0500

    tcpterm 0.0.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/tcpterm.rb
@@ -0,0 +1,18 @@
+class Tcpterm < Formula
+  desc "Terminal-based TCP dump viewer"
+  homepage "https://github.com/sachaos/tcpterm"
+  url "https://github.com/sachaos/tcpterm/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "c4c0c536ddd12509f1fa2607d84a1e2a002a5a82269ade98a9da4933bc8bd020"
+  license "MIT"
+  head "https://github.com/sachaos/tcpterm.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    system bin/"tcpterm", "--version"
+  end
+end
