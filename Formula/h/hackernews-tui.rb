e035eefbd376a7f23c23a61dc5898c65ae3cb7e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-09 03:32:59 +0000

    hackernews-tui: add 0.13.5 bottle.

--- Formula/h/hackernews-tui.rb
+++ Formula/h/hackernews-tui.rb
@@ -6,6 +6,15 @@ class HackernewsTui < Formula
   license "MIT"
   head "https://github.com/aome510/hackernews-TUI.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a7233abdf4bfc70f2ba133dd184b2d3dc061e81a611cf50b58369d359cd9ad82"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7f800a4a46b8d9a59c13aaa403d33a53a0b0cc1129ef8bef571050d5be63a980"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "21ab115de1d726922731addc9dff3644e52d85679525b24b92cb7019e5c0c886"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2ba0993e3973e6092401f75581f1696e2af55659bd928304f0a58ccb12cdb38b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c07ce9c86a45a0c47b6c08b026d86e0ca3abd6321724f33c421dbbe96c64c145"
+  end
+
   depends_on "rust" => :build
 
   def install
