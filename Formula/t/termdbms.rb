f351df62a04163eaba86a7c9bbbc81ab89add921
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-09 03:59:18 +0000

    termdbms: add 0.9-alpha bottle.

--- Formula/t/termdbms.rb
+++ Formula/t/termdbms.rb
@@ -6,6 +6,15 @@ class Termdbms < Formula
   license "MIT"
   head "https://github.com/mathaou/termdbms.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8b5f33d62003cdfebc5f04c266e7fbeea40161e65390b780e0fa543989d5dbb4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8b5f33d62003cdfebc5f04c266e7fbeea40161e65390b780e0fa543989d5dbb4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8b5f33d62003cdfebc5f04c266e7fbeea40161e65390b780e0fa543989d5dbb4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "005d382f49c1732e6e60fd4cb7354e7f11df666e43c9c1177d3e9ae62fd15228"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "408994632cca6b5089dfe1dd0678897ef0cb80330c43f97c9538e80fb37d88e7"
+  end
+
   depends_on "go" => :build
 
   def install
