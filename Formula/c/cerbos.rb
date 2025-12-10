4b29e0af09e94c50d27945577c4897508085150a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-10 10:42:47 +0000

    cerbos: update 0.49.0 bottle.

--- Formula/c/cerbos.rb
+++ Formula/c/cerbos.rb
@@ -8,11 +8,11 @@ class Cerbos < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eb5a4169da7f714f0c526378a075645ee337c15c3b647206bde8c1d402b2c1ab"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1c9d791a028080255098fa627e1e8656f38a8371fc892c5131bab222901d918b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "50e24fc2de935c4568b4155c88b999b3c99498127a1b76ef080fcc01b002a1f1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1634a5e1b8ecd87e0576b77b9ddd67c54bdba4bfcc3bc7da428680b65ab97a9d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c32d9dd87733542e389a71575a25baa0723ff81cdf257503b271f66541e85873"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cf325c66ff69ec4f35e192f4ebe8b8f85c71382076289ea361b69868e2053e19"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e705a6cad627fe76596c064522fe2d4182993e3d6989f11f23accc4a73e77d83"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "514932a74eeb49fef0d77493bbad2e4df723fa4c67476cfe065fd3f8363c8dc5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f7bbcbe6bc6a425e45f498b144c94bc689dc135eb1d62c8c6d12f472298eaa47"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3d0554fe5fb52af882ce70cb9b30ffee7b1497701c917b32e95adaeace75400"
   end
 
   depends_on "go" => :build
