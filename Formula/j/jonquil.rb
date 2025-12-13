85e705cd53a93a456775cf463e58d013e544dadc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-13 04:27:32 +0000

    jonquil: add 0.3.0 bottle.

--- Formula/j/jonquil.rb
+++ Formula/j/jonquil.rb
@@ -6,6 +6,15 @@ class Jonquil < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/toml-f/jonquil.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_tahoe:   "377d5d5d1309e545d18951f66b51dba871200a6d6ee58aca35c560d2e3691fcb"
+    sha256 cellar: :any,                 arm64_sequoia: "e6c03f53f4f5a8829215f0a5887f109b2a147a789b24b5863da7e71340c28fb0"
+    sha256 cellar: :any,                 arm64_sonoma:  "7c76070aa34a47b240403855542bf56b3a2195c481d80062af3c350a270c7990"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "84f50b1ee056ad2d93e5541eb7cd74534c3a447c661fcc07ae7198bc060c43d8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe3fced4178b3e8e4707d1173ea0bb12807756668553d074a3812bae34744014"
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => [:build, :test]
