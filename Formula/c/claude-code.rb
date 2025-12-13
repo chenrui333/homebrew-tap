374ed4d1e011f7ed353658c0bb6fb90866829a1b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-13 00:53:16 +0000

    claude-code: update 2.0.68 bottle.

--- Formula/c/claude-code.rb
+++ Formula/c/claude-code.rb
@@ -7,11 +7,11 @@ class ClaudeCode < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "2d168e74eb64e5f3f4b6b341fa8e1f958a4b9f7e421af06dc8f6ca007121352a"
-    sha256 cellar: :any,                 arm64_sequoia: "c90e051dbe9c0f9172fd1b6f00ae91100e8ad6150f4864ae39b8539cfff44ee6"
-    sha256 cellar: :any,                 arm64_sonoma:  "c90e051dbe9c0f9172fd1b6f00ae91100e8ad6150f4864ae39b8539cfff44ee6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "14107b875801433e2e52cce3cc2080495d3d6dfcc65a0308381f92cfb71e0fde"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a4b4888ef58f5a578c2087820aceff5da95f931876f5ade19ebc3a7dd1e9472"
+    sha256 cellar: :any,                 arm64_tahoe:   "b4bdb7b08969d68df9bee6e59379b3ab172c15b35324b3660a4689645dda2c43"
+    sha256 cellar: :any,                 arm64_sequoia: "54046b4ff488f7fd626dd43a0fe36772c8df225b0605ce2657687be426d70d56"
+    sha256 cellar: :any,                 arm64_sonoma:  "54046b4ff488f7fd626dd43a0fe36772c8df225b0605ce2657687be426d70d56"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7d6b0268197ecb541f7b7f418c0d69e4ff0b514644be3c9d49c3df421c5d7d9f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "979b365395593a95ebb8486aeeb3f85c17689379b79b60d2ca2ff37584533404"
   end
 
   depends_on "node"
