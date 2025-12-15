7e9a11422b9f76567f2d89884f8ac7f72a5727e0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-15 12:48:26 +0000

    shopify-cli: update 3.88.1 bottle.

--- Formula/s/shopify-cli.rb
+++ Formula/s/shopify-cli.rb
@@ -7,11 +7,11 @@ class ShopifyCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "5b6f9eb7fc61ec7dde0981460f9ffe48952d9b4c3aae2b6de7b304c2aa128b49"
-    sha256 cellar: :any,                 arm64_sequoia: "99eab92a284dcaccbf9dd956704843b593a8a52067e1f0abb699f1ea39e8d804"
-    sha256 cellar: :any,                 arm64_sonoma:  "99eab92a284dcaccbf9dd956704843b593a8a52067e1f0abb699f1ea39e8d804"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "18102765bcdfc10b7e2035ace9f770be173c5b7a66f2a1a0db997abe01e4b9b3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ad53ad145759dfa10e351fcc061b29136a248054fea5c47517fcc22d88cafe0"
+    sha256 cellar: :any,                 arm64_tahoe:   "e30162b47f48f343fc36994cc35024d15af52d936b9f22b6ab0c2548ed8d3c2e"
+    sha256 cellar: :any,                 arm64_sequoia: "a1fef2558547fa6d7a874f663ac623d5f15c9560ac2547a1d44e82c9d8282946"
+    sha256 cellar: :any,                 arm64_sonoma:  "a1fef2558547fa6d7a874f663ac623d5f15c9560ac2547a1d44e82c9d8282946"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dacec346d21d154e3f93115db53bc5304c60de2a1c6cf9c27e15191a569d7cc7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b07cdea604b456b86fec101b3678f13e8b4b7140cccc8ac15be5b2089b257ccf"
   end
 
   depends_on "node"
