b8ca6e1dcf4dfb189a941b6a8e369cdf52fa6aba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-18 14:58:06 +0000

    flowrs: update 0.7.3 bottle.

--- Formula/f/flowrs.rb
+++ Formula/f/flowrs.rb
@@ -8,11 +8,11 @@ class Flowrs < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1294734f756bb2ed9f338459b428f41316107b77d0161b7b94880ede38c929e9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e4156b96ef0b93d02ddda64c6b14c6d5ca2bc8303d892a0b1aab0437d35ba7ab"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "44a665715363ee0d78d9267c59aea3dee10d58dfae5aafda5741a7013a79ce82"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9e11ce1a8c6831baac7459a32d38146f3e1f2b4beab798f064bd8ae5b878062e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "158b068993d22e90b4e92438a5f75cdf0b27d30c3f48fc403c7827bf735c2da1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "008d3cf6ad59589f00e2b373e8a253ae37566509ed1ee3370283e9fba0626e26"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8fb384f43ab6f9b4f38caab54c2178c3f6d1b7636a7619c6e4fa7b0e4ac43572"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "311f9b0e9ea6c448525841ed62fd79318976ffc358c394612452306e381d559a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6552644918130fdd663252a8c44e759f7947e084686fd233ba7fd6885647c543"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "62c193d6fe17a5fe7280b5a438d16aa1ec728f9126a8a7fca97522fde1da9d82"
   end
 
   depends_on "pkgconf" => :build
