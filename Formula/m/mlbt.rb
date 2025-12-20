806d59e935269e928c4dc5a5e0aee52bc6ceaf27
Rui Chen <rui@chenrui.dev>
2025-12-19 21:34:50 -0500

    mlbt 0.0.19 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/m/mlbt.rb
@@ -0,0 +1,24 @@
+class Mlbt < Formula
+  desc "TUI for MLB stats API"
+  homepage "https://github.com/mlb-rs/mlbt"
+  url "https://github.com/mlb-rs/mlbt/archive/refs/tags/v0.0.19.tar.gz"
+  sha256 "7d61181f1e622e8d801f448bdc4282ae788cb118548fdb9069cce471ca47b6dc"
+  license "MIT"
+  head "https://github.com/mlb-rs/mlbt.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    output_log = testpath/"output.log"
+    pid = spawn bin/"mlbt", testpath, [:out, :err] => output_log.to_s
+    sleep 1
+    assert_match "Gameday │ Stats │ Standings", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
