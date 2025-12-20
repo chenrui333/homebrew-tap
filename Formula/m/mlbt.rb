ed4636c3f33ac6b940af039dab17c5513b4eb7da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-20 23:11:26 +0000

    mlbt: add 0.0.19 bottle.

--- Formula/m/mlbt.rb
+++ Formula/m/mlbt.rb
@@ -6,6 +6,15 @@ class Mlbt < Formula
   license "MIT"
   head "https://github.com/mlb-rs/mlbt.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2d2e7097a300ca16658b16b442bc4004a00ffd3c30039bc9b52224ec3f98d05a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "840161ccdeb2c270e7a26a54b2c4467da6c678b82f550e93cd618eeadbee0127"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "66cde2f02d07c190be82af879733f0c1aa21dc23fa84dddc0d67a2d1d891853d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "70785d1a80a8fd96535a445041588c0ce3bed91e4a516d4875b0fc59be551795"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7196d693497d13182e114cebc719f660d93eb6904ec806ceaae1c06c2e545145"
+  end
+
   depends_on "rust" => :build
 
   def install
