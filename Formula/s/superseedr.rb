7f9bc052e5face966a6414c758dd3e85932c09a6
Rui Chen <rui@chenrui.dev>
2025-12-19 21:46:20 -0500

    superseedr: fix desc
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/s/superseedr.rb
+++ Formula/s/superseedr.rb
@@ -1,5 +1,5 @@
 class Superseedr < Formula
-  desc "Build your Docker images in the cloud"
+  desc "BitTorrent Client in your Terminal"
   homepage "https://github.com/Jagalite/superseedr"
   url "https://github.com/Jagalite/superseedr/archive/refs/tags/v0.9.28.tar.gz"
   sha256 "496d081ca1bcd7df05dfc4b2532a0fd8fb0b0e92a67f28f1a3277892fe6334fb"
