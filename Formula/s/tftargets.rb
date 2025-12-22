cf0b5cfb463fa14358aabecead828323205b4f07
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 04:54:27 +0000

    tftargets: add 0.0.7 bottle.

--- Formula/s/tftargets.rb
+++ Formula/s/tftargets.rb
@@ -6,6 +6,15 @@ class Tftargets < Formula
   license "MIT"
   head "https://github.com/takaishi/tftargets.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c687c539aa83cec0bbb30f31d4dcf479abbf1704fe79a9c418497b5225c1df14"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c687c539aa83cec0bbb30f31d4dcf479abbf1704fe79a9c418497b5225c1df14"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c687c539aa83cec0bbb30f31d4dcf479abbf1704fe79a9c418497b5225c1df14"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "41c1c711e201e66fad1fd58e75bb312545ba95cefccfa366a73d60f3653e1933"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d0605e619eaa853280dc572ba740ce0a9edf7ad00eaf40955cbcd73b72884635"
+  end
+
   depends_on "go" => :build
 
   def install
