d8f5e46ef8915671f5e16d3f655a63e8ed522978
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 08:28:19 +0000

    ergo 2.17.0

--- Formula/e/ergo.rb
+++ Formula/e/ergo.rb
@@ -1,8 +1,8 @@
 class Ergo < Formula
   desc "Modern IRC server (daemon/ircd) written in Go"
   homepage "https://github.com/ergochat/ergo"
-  url "https://github.com/ergochat/ergo/archive/refs/tags/v2.16.0.tar.gz"
-  sha256 "85b3f3e83d9f2076e7e719b22b8e8fabd647ab746cc4d8ab945ea88cebb42065"
+  url "https://github.com/ergochat/ergo/archive/refs/tags/v2.17.0.tar.gz"
+  sha256 "bfda2be82aa133ddd7a03c2121d6807c8a1b9f5c055f0bbb90451baa2a249ce4"
   license "MIT"
   head "https://github.com/ergochat/ergo.git", branch: "master"
 
