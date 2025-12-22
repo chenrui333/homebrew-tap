73bb1a690f1102b565f148aa86dc6bf0074fee7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-22 03:37:22 +0000

    rbac-lookup: add 0.10.3 bottle.

--- Formula/r/rbac-lookup.rb
+++ Formula/r/rbac-lookup.rb
@@ -6,6 +6,15 @@ class RbacLookup < Formula
   license "Apache-2.0"
   head "https://github.com/FairwindsOps/rbac-lookup.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "22697b7d33bd8cd8fcc5abd7429e03f117698336e143003974d27813d39ca305"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "22697b7d33bd8cd8fcc5abd7429e03f117698336e143003974d27813d39ca305"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "22697b7d33bd8cd8fcc5abd7429e03f117698336e143003974d27813d39ca305"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b73e11b8a3bda51cf325708b7cefc76d5548b00f0fb4afdd9c543fa59783748b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e6fdffce1c6219411666fc7c9f42ca4c358b8d9750fbe4733600ceeb459ab246"
+  end
+
   depends_on "go" => :build
 
   def install
