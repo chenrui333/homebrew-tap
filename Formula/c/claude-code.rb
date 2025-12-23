ff1c66061912cbaa80cb1cf2a3bdbbf4227d98c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-23 02:56:30 +0000

    claude-code 2.0.76
    Closes #3361.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/claude-code.rb
+++ Formula/c/claude-code.rb
@@ -1,8 +1,8 @@
 class ClaudeCode < Formula
   desc "Agentic coding tool that lives in your terminal"
   homepage "https://github.com/anthropics/claude-code"
-  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.0.75.tgz"
-  sha256 "810cdff8b1bfc1d21db164887cd912ae4f8d41823d21364a02cd1478d13b8362"
+  url "https://registry.npmjs.org/@anthropic-ai/claude-code/-/claude-code-2.0.76.tgz"
+  sha256 "fca39936ff8e9310c8e4c6833d6455341b92acd92317785f0f773ee74391b9d9"
   # license :unfree
 
   bottle do
