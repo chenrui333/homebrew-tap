91b0410a10532f99005f323b504fcf3569f898e1
Rui Chen <rui@chenrui.dev>
2025-12-25 10:22:28 -0500

    hapi 0.1.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/h/hapi.rb
@@ -0,0 +1,19 @@
+class Hapi < Formula
+  desc "Agentic coding - access coding agent anywhere"
+  homepage "https://github.com/tiann/hapi"
+  url "https://registry.npmjs.org/@twsxtd/hapi/-/hapi-0.1.3.tgz"
+  sha256 "47735f4a563af71cd965dd703ca4788547a687954acdb859795abb34f3242aad"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/hapi --version")
+    assert_match "📋 Basic Information", shell_output("#{bin}/hapi doctor")
+  end
+end
