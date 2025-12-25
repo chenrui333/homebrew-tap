607a2c8217b9cd89988229c5a7b6259ecf938185
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-25 15:35:07 +0000

    hapi: add 0.1.3 bottle.

--- Formula/h/hapi.rb
+++ Formula/h/hapi.rb
@@ -5,6 +5,15 @@ class Hapi < Formula
   sha256 "47735f4a563af71cd965dd703ca4788547a687954acdb859795abb34f3242aad"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_tahoe:   "a09bbe81fb17116d4175655808c62e3c7e3205747c0d57eb916e80c3b4303cfc"
+    sha256                               arm64_sequoia: "a09bbe81fb17116d4175655808c62e3c7e3205747c0d57eb916e80c3b4303cfc"
+    sha256                               arm64_sonoma:  "a09bbe81fb17116d4175655808c62e3c7e3205747c0d57eb916e80c3b4303cfc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e4672d050e946b7c73c877e2db497c5087db68659486288dcd00693d63fdc46e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "58b237370a963d26ae002e07879fac879e3e8ad2d03330e34cad0c8b2b22c3db"
+  end
+
   depends_on "node"
 
   def install
