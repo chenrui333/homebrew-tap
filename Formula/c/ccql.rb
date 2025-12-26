f9eed8160baa808779d5b9aa3d5e4d4c0b3ad6d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-26 03:39:34 +0000

    ccql: add 0.1.0 bottle.

--- Formula/c/ccql.rb
+++ Formula/c/ccql.rb
@@ -6,6 +6,15 @@ class Ccql < Formula
   license "MIT"
   head "https://github.com/douglance/ccql.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "85db98d3419cf797d28e33e8aa0048d8d7d9062ebeeb211b38d84c1829216005"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fc874fbddb3e974d2ec25a1a20b78ed5d05f856a1830df18f1bbbf9ebdc3c98e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d9e10cb21fe7c4d0b54538684447e8a2bdd9b88f7aca1fe1fa564a3ffabdb2b4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c0948762dbc0d5d3723432105c77a6b3bf8313bc70486bc284adf358b221bff6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "626ebcc1f1d69972427abbc1caf5558dbef4aaaa06bba343d1d35e862dca38d9"
+  end
+
   depends_on "rust" => :build
 
   def install
