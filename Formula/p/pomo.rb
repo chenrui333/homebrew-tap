f6765427354f11a253f68941705d6b9cf39d2616
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-26 05:08:48 +0000

    pomo: add 0.8.1 bottle.

--- Formula/p/pomo.rb
+++ Formula/p/pomo.rb
@@ -6,6 +6,15 @@ class Pomo < Formula
   license "MIT"
   head "https://github.com/Bahaaio/pomo.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b6b06e073794d6a6791238cf2e8df52e80399ceb219027433f7870cf7b85fada"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b6b06e073794d6a6791238cf2e8df52e80399ceb219027433f7870cf7b85fada"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b6b06e073794d6a6791238cf2e8df52e80399ceb219027433f7870cf7b85fada"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f6173c80e8ba03c735bf12f79bac194bff75fb239dcd15f85fe609ee7b904e3f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e553c780e59912445b664dab08fcbed114af35fca35592f67462011ec5633c4"
+  end
+
   depends_on "go" => :build
 
   def install
