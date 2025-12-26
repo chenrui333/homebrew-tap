4d35fa994764c8c5ab3a1195eccc52eea465843d
Rui Chen <rui@chenrui.dev>
2025-12-25 23:42:48 -0500

    pomo 0.8.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/pomo.rb
@@ -0,0 +1,27 @@
+class Pomo < Formula
+  desc "Terminal Pomodoro Timer"
+  homepage "https://github.com/Bahaaio/pomo"
+  url "https://github.com/Bahaaio/pomo/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "f758f6d1849c143accaf776979b2354f7ef0ca4b9f1f7244d75afd597248428c"
+  license "MIT"
+  head "https://github.com/Bahaaio/pomo.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+    generate_completions_from_executable(bin/"pomo", shell_parameter_format: :cobra)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pomo --version")
+
+    output_log = testpath/"output.log"
+    pid = spawn bin/"pomo", [:out, :err] => output_log.to_s
+    sleep 1
+    assert_match "work session", output_log.read
+  ensure
+    Process.kill("TERM", pid)
+    Process.wait(pid)
+  end
+end
