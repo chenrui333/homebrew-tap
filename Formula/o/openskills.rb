b2aab637b22b0b4077d6aeb1e30f7490ce441463
Rui Chen <rui@chenrui.dev>
2025-12-28 18:26:57 -0500

    openskills 1.3.0 (new formula)
    Closes #3509.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/o/openskills.rb
@@ -0,0 +1,21 @@
+class Openskills < Formula
+  desc "Universal skills loader for AI coding agents"
+  homepage "https://github.com/numman-ali/openskills"
+  url "https://registry.npmjs.org/openskills/-/openskills-1.3.0.tgz"
+  sha256 "88ab379000b618e3a460305e306cd2260eca4618ac65c50345e56ae77059f917"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    # version report has issue, https://github.com/numman-ali/openskills/pull/27
+    # assert_match version.to_s, shell_output("#{bin}/openskills --version")
+    system bin/"openskills", "--version"
+    assert_match "No skills installed", shell_output("#{bin}/openskills list")
+  end
+end
