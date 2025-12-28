3e7c1e44baf8b1e797650a892a6001786b38a2eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-28 17:00:10 +0000

    venom: update 1.2.0 bottle.

--- Formula/v/venom.rb
+++ Formula/v/venom.rb
@@ -9,10 +9,12 @@ class Venom < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e0c116950e759523bb1a478e818e1894b7d0fed64a958f42e8499a257a7aef38"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c45bf0f445dc8a3c6062e2013cc8e615539b45f5cf44146e2fb2cd85e5c9c338"
-    sha256 cellar: :any_skip_relocation, ventura:       "a2a68ace298cf3934a0df8c77c252b81f2f7a90d3b152d0de51a0f15c5c1b08f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf148470ac8eb306b770119f2738fbb262995832f1395387a0fe87ebdb0780d2"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2e8db527425ec5376f5b442cfdea883535aee5a941aea031a404aaa34d52f450"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "831fd809ab5f8b0ee736463089ce8af3b9bf17211963649d7922f947559b0997"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5e6b93a2c858af520853f11a9e922b5ec2d828272cda1514401706a9477ef68b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "51c28ec00409ba8e8b07b86f8841ba468ac721cec5817c38f9a3f2a899734aec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7ce6cc8097b008307085a6ec95dbc1eea22263f10d3eb41155bb4ab0d12d1a5c"
   end
 
   depends_on "go" => :build
