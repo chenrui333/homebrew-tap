b27b386b036b0030d5cf18a4cc99ebbda9714ca4
Rui Chen <rui@chenrui.dev>
2025-12-28 10:11:04 -0500

    timetrace: update completion
    Closes #3489.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/t/timetrace.rb
+++ Formula/t/timetrace.rb
@@ -19,7 +19,7 @@ class Timetrace < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
 
-    generate_completions_from_executable(bin/"timetrace", "completion")
+    generate_completions_from_executable(bin/"timetrace", shell_parameter_format: :cobra)
   end
 
   test do
