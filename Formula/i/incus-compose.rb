5b1ed0334cf011078a73e8908744fb8c3e0dcec2
Rui Chen <rui@chenrui.dev>
2025-12-28 09:49:02 -0500

    incus-compose: update completion
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/incus-compose.rb
+++ Formula/i/incus-compose.rb
@@ -27,7 +27,7 @@ class IncusCompose < Formula
     ]
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"incus-compose", "completion")
+    generate_completions_from_executable(bin/"incus-compose", shell_parameter_format: :cobra)
   end
 
   test do
