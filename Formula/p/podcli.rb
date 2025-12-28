32e7112ea40e299037191b83a1361fa9d129f22d
Rui Chen <rui@chenrui.dev>
2025-12-28 10:01:15 -0500

    podcli: update completion
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/podcli.rb
+++ Formula/p/podcli.rb
@@ -21,7 +21,7 @@ class Podcli < Formula
     ldflags = "-s -w -X github.com/stefanprodan/podinfo/pkg/version.REVISION=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cmd/podcli"
 
-    generate_completions_from_executable(bin/"podcli", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"podcli", shell_parameter_format: :cobra)
   end
 
   test do
