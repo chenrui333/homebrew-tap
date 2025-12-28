60a063272ee234a1f21af5efbf16a9d41a142d1e
Rui Chen <rui@chenrui.dev>
2025-12-28 10:10:26 -0500

    sgpt: update completion

--- Formula/s/sgpt.rb
+++ Formula/s/sgpt.rb
@@ -27,7 +27,7 @@ class Sgpt < Formula
 
     system "go", "build", *std_go_args(ldflags:), "./cmd/sgpt"
 
-    generate_completions_from_executable(bin/"sgpt", "completion")
+    generate_completions_from_executable(bin/"sgpt", shell_parameter_format: :cobra)
   end
 
   test do
