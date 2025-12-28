3b38d986aa59586a9e7138c335ca80aac491ac15
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-28 00:33:01 +0000

    checksec: update 3.1.0 bottle.

--- Formula/c/checksec.rb
+++ Formula/c/checksec.rb
@@ -8,11 +8,12 @@ class Checksec < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4a948d4ea23454cfe9b577afee433ab4b6f30fcc7d3c04da472d28e4aac5d8b8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "50044c6e6ff760a6510076305f2b1ba8230c338e2c6877b172af5c70a003cb81"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00f09c0ee43b03b9ab1ea55d6024024eed3af379ac6e31b65871d6da6120dc3b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3712996861760cad5d65faa79dbaeb6bcf5da3e0862ca2f4ea0d855d530e8753"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e14e9dd4cc3720d03689806728b18933be83611df5b7763d1db70fbd18ddcc9"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "993131876eef90a8c62834707ecec1aabf129b1ac917226dda2dfc9368b0df80"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0cae962674fa2917ba5b5feb4dbb71c1874e1700713838367c8f2687c3bcef05"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "404f94518d6a91da817e8fd7d50fd3b0d0822ea3697314ca0f278af9789e998d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "713cf28b0ef8b4f8b9e7dba2d6d1ba0da9bf8aef99a212e57f6b1b44cbd88c3e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc18ac82fccd7281fdb7ca2ddfe9a0955fc4256c665e744db19bb0c709a1cadd"
   end
 
   depends_on "go" => :build
