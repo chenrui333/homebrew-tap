e9f16bf932bfa309fc87ade6061e2a89a60aaec8
Rui Chen <rui@chenrui.dev>
2025-12-28 10:10:45 -0500

    teldrive: update completion

--- Formula/t/teldrive.rb
+++ Formula/t/teldrive.rb
@@ -35,7 +35,7 @@ class Teldrive < Formula
     ]
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"teldrive", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"teldrive", shell_parameter_format: :cobra)
   end
 
   test do
