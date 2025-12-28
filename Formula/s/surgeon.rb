1e40a43316278ee33d530032e399290e728e11d0
Rui Chen <rui@chenrui.dev>
2025-12-28 10:10:41 -0500

    surgeon: update completion

--- Formula/s/surgeon.rb
+++ Formula/s/surgeon.rb
@@ -28,7 +28,7 @@ class Surgeon < Formula
     ]
     system "go", "build", *std_go_args(ldflags:), "./cmd/surgeon"
 
-    generate_completions_from_executable(bin/"surgeon", "completion")
+    generate_completions_from_executable(bin/"surgeon", shell_parameter_format: :cobra)
   end
 
   test do
