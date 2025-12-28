20285ca5f470c92f45800d4f71bb85365439cb07
Rui Chen <rui@chenrui.dev>
2025-12-28 10:11:17 -0500

    tpm: update completion

--- Formula/t/tpm.rb
+++ Formula/t/tpm.rb
@@ -20,7 +20,7 @@ class Tpm < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
-    generate_completions_from_executable(bin/"tpm", "completion")
+    generate_completions_from_executable(bin/"tpm", shell_parameter_format: :cobra)
   end
 
   test do
