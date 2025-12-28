faa13db29cc673c10166748a3ddde7073bf80248
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-28 18:34:59 +0000

    lintnet: update 1.0.0 bottle.

--- Formula/l/lintnet.rb
+++ Formula/l/lintnet.rb
@@ -9,11 +9,12 @@ class Lintnet < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "06986cc119c669794b3588ab64edb3d2630330461698afe71f8fb95aa8266e50"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "06986cc119c669794b3588ab64edb3d2630330461698afe71f8fb95aa8266e50"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "06986cc119c669794b3588ab64edb3d2630330461698afe71f8fb95aa8266e50"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3b6be77d2094de45fc580ea291bd9e890167180041d612dbe7a69888b9b16e33"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bf5cf195825ca510fbdd5a2489186bf1f69524b2a5eed04ce2900c8319d8b1a9"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d09a7901bc0a704968e96a2fc2b76fbb7c1d6626c844a931de361f1102ba9b18"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d09a7901bc0a704968e96a2fc2b76fbb7c1d6626c844a931de361f1102ba9b18"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d09a7901bc0a704968e96a2fc2b76fbb7c1d6626c844a931de361f1102ba9b18"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1064ab257f255e7e033d2d71009a8e1ddd8a14f2c47426514c6d8594156db265"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b1aecae7027fb4fe0ae581cbcefae5dd90985c4fb613f1d1605fc8b9a6787481"
   end
 
   depends_on "go" => :build
