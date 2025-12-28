e7537a1f6d46d6a8f16b93cd52f0f26070400b00
Rui Chen <rui@chenrui.dev>
2025-12-28 10:11:00 -0500

    ticker: update completion
    Closes #3488.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/t/ticker.rb
+++ Formula/t/ticker.rb
@@ -21,7 +21,7 @@ class Ticker < Formula
     ldflags = "-s -w -X github.com/achannarasappa/ticker/v5/cmd.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"ticker", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"ticker", shell_parameter_format: :cobra)
   end
 
   test do
