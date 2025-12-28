9c2cb53dc7be5c50785fdf2e004b4ea3ef12e851
Rui Chen <rui@chenrui.dev>
2025-12-28 10:10:56 -0500

    tftarget: update completion
    Closes #3487.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/t/tftarget.rb
+++ Formula/t/tftarget.rb
@@ -21,7 +21,7 @@ class Tftarget < Formula
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
 
-    generate_completions_from_executable(bin/"tftarget", "completion")
+    generate_completions_from_executable(bin/"tftarget", shell_parameter_format: :cobra)
   end
 
   test do
