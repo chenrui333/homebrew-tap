a79ae18f77db20c9edb3e74c2bb84f3c6ad68766
Rui Chen <rui@chenrui.dev>
2025-12-27 19:48:46 -0500

    crush: update completion
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/crush.rb
+++ Formula/c/crush.rb
@@ -21,7 +21,7 @@ class Crush < Formula
     ldflags = "-s -w -X github.com/charmbracelet/crush/internal/version.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"crush", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"crush", shell_parameter_format: :cobra)
   end
 
   test do
