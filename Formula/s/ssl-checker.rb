2ff2159269626354979fa4ac0c6ec10e4381a93e
Rui Chen <rui@chenrui.dev>
2025-12-28 10:10:37 -0500

    ssl-checker: update completion

--- Formula/s/ssl-checker.rb
+++ Formula/s/ssl-checker.rb
@@ -21,7 +21,7 @@ class SslChecker < Formula
     ldflags = "-s -w -X github.com/fabio42/ssl-checker/cmd.Version=#{version}"
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"ssl-checker", "completion", shells: [:bash, :zsh, :fish, :pwsh])
+    generate_completions_from_executable(bin/"ssl-checker", shell_parameter_format: :cobra)
   end
 
   test do
