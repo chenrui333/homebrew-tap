d9b6db375fec1ea986c2a8f683960e9530f0f0d4
Rui Chen <rui@chenrui.dev>
2025-12-28 09:56:03 -0500

    mfa: update completion
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mfa.rb
+++ Formula/m/mfa.rb
@@ -26,7 +26,7 @@ class Mfa < Formula
     ]
     system "go", "build", *std_go_args(ldflags:)
 
-    generate_completions_from_executable(bin/"mfa", "completion")
+    generate_completions_from_executable(bin/"mfa", shell_parameter_format: :cobra)
   end
 
   test do
