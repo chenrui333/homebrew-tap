9915b7a0524b6a3fc770351dbf99b0d59bb0a4b5
Rui Chen <rui@chenrui.dev>
2025-12-28 09:52:21 -0500

    kcl: update completion
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/k/kcl.rb
+++ Formula/k/kcl.rb
@@ -21,7 +21,7 @@ class Kcl < Formula
     ldflags = "-s -w -X kcl-lang.io/cli/pkg/version.version=#{version}"
     system "go", "build", *std_go_args(ldflags:), "./cmd/kcl"
 
-    generate_completions_from_executable(bin/"kcl", "completion")
+    generate_completions_from_executable(bin/"kcl", shell_parameter_format: :cobra)
   end
 
   test do
