22dc99024c7aeca6cf8266e8321664fb1863bde5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-29 01:34:58 +0000

    lsv: add 0.1.11 bottle.

--- Formula/l/lsv.rb
+++ Formula/l/lsv.rb
@@ -6,6 +6,15 @@ class Lsv < Formula
   license "MIT"
   head "https://github.com/SecretDeveloper/lsv.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a0dc1e530f98c56c14c4e9673732ff06e62983db5f23b383ec6a34cf862adc05"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3136f0b930dae2d54fa912c70397453e62572f01aa12c0bf373c4256d70f845c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4f5eeb9a7f2ac1d7dbd482c116e357df8bf9871107ffbb4683d78cd9da9faa87"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c44500bb20069f8a1c0d23cd0f9c7a90413b09234605589c99ae23af41739a4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "059333008546452e9f46c0869f6f8121adfe237c2276a72d04e5d2881ba32aba"
+  end
+
   depends_on "rust" => :build
 
   def install
