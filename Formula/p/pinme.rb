0171e0909fc723002504680d6e08ef9d961a236b
Rui Chen <rui@chenrui.dev>
2025-12-28 20:26:36 -0500

    pinme 1.2.1 (new formula)
    Closes #3517.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/p/pinme.rb
@@ -0,0 +1,20 @@
+class Pinme < Formula
+  desc "Deploy Your Frontend in a Single Command"
+  homepage "https://pinme.eth.limo/"
+  url "https://registry.npmjs.org/pinme/-/pinme-1.2.1.tgz"
+  sha256 "065fa4f23364757a618b73db0f72b887feaaa9613144119631cca9a8cc2af92a"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/pinme --version")
+    assert_match "Failed to fetch domains: Auth not set", shell_output("#{bin}/pinme domain")
+    assert_match "No upload history found", shell_output("#{bin}/pinme ls")
+  end
+end
