6f2634691c7fe3e2ea23e8f1c75529401225a769
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-29 04:54:38 +0000

    easy-llm-cli: add 0.1.11 bottle.

--- Formula/e/easy-llm-cli.rb
+++ Formula/e/easy-llm-cli.rb
@@ -5,6 +5,11 @@ class EasyLlmCli < Formula
   sha256 "f8f2adf430a2034e8991017538ef77d618d1af5a025ada3139787f24d6a27d83"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, all: "de2f61d16526c7cce3ab2d3d65b33b3431ab5af854958c2f1077e897fd9b2e1b"
+  end
+
   depends_on "node"
 
   def install
