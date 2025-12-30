dcc38dfbe2cb79f42987d6d7a30ef1d24c91063e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-30 22:25:55 +0000

    codebuff 1.0.565
    Closes #3544.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/codebuff.rb
+++ Formula/c/codebuff.rb
@@ -1,8 +1,8 @@
 class Codebuff < Formula
   desc "Generate code from the terminal"
   homepage "https://www.codebuff.com/"
-  url "https://registry.npmjs.org/codebuff/-/codebuff-1.0.564.tgz"
-  sha256 "6e99e3199c8614aa7d2897cad4a0658bf68cf42a38d37f11d8969e0a217c6931"
+  url "https://registry.npmjs.org/codebuff/-/codebuff-1.0.565.tgz"
+  sha256 "732c17a2dd3d70ee59f8d6a3301814beb9700c78c6aa3c8507d2236b164dc125"
   license "MIT"
 
   bottle do
