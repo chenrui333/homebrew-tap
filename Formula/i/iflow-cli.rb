520f037191ab7df89cdae3a36fa7b1586dad4520
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-12-31 10:27:49 +0000

    iflow-cli 0.4.14

--- Formula/i/iflow-cli.rb
+++ Formula/i/iflow-cli.rb
@@ -1,8 +1,8 @@
 class IflowCli < Formula
   desc "AI assistant that runs directly in your terminal"
   homepage "https://cli.iflow.cn/"
-  url "https://registry.npmjs.org/@iflow-ai/iflow-cli/-/iflow-cli-0.4.8.tgz"
-  sha256 "d71ef391ff7f6f58b428177a5c9a2faaee4c569385ec9c88420e6f35cf74f8bc"
+  url "https://registry.npmjs.org/@iflow-ai/iflow-cli/-/iflow-cli-0.4.14.tgz"
+  sha256 "4289fe9448ad6b687d26c4acc5fc44b6763075415265a7496e3d138de51725c6"
   license "Apache-2.0" # derived from LICENSE, but no source code for the project in https://github.com/iflow-ai/iflow-cli
 
   bottle do
