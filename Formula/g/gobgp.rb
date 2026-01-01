b405e86697d75fbe8ffeaf4f44489c641602fa75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-01 14:38:09 +0000

    gobgp: update 4.2.0 bottle.

--- Formula/g/gobgp.rb
+++ Formula/g/gobgp.rb
@@ -8,11 +8,11 @@ class Gobgp < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e95bb13d24b9d359bd35fde0c218542b60308ef6d548934cbb474d3e3dae367f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fc9a29ff53873291d3cebc30e9e7991064cb5228da2c4585de8d8373f2cb3133"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f790b4b6ad66ce09970de15b6d2293575bcdaeda56fc1de751ebf01718b99e3a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "edcb0db09b2a0cb3f996f773fcc1b2ed406c1239899d8a9a9da7acaa735ccef5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "778d002320f106862953791c297736d2e4b7cc931fa4b05d602f1287e0bb7eb2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a5ad1cf087392f3532bdf31b056f4a3a4eafd7c991800fcc8dff3f86b67d9fa3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5dc583914ee0a7643af5d30e16fef8591b5259ecbee48b51c4efbb7791eb4a0e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0d295275825358d8c295d91eed35edccd0175986fa8730c74afc07cf13b63ca2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a3baa037d963f1ac36a8b2dcb17f891532542fa8bb37a0f1da436c48e5ee308b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c5c7261bd5535bd7bdae20f18cf7dd2e0210d90dab60a3371178bc9f46678be2"
   end
 
   depends_on "go" => :build
