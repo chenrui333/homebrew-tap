ebacc40f36ffd2522aed18eda910ea5c32fccae5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-01 22:42:17 +0000

    prpm: update 2.1.21 bottle.

--- Formula/p/prpm.rb
+++ Formula/p/prpm.rb
@@ -7,7 +7,7 @@ class Prpm < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "bdf1df6d2e074b26f77c4f738e3c1ef3c88137122e007f22569064af6595c4cb"
+    sha256 cellar: :any_skip_relocation, all: "503accd8784bbe0312d87fe6670645f721772fbf32e82421f5af98062d0608e0"
   end
 
   depends_on "node"
