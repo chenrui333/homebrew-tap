04e8a62cef7aa416e787b2063e186ffac0b66a6d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-06 04:31:22 +0000

    go-getter 1.8.4
    Closes #3617.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/go-getter.rb
+++ Formula/g/go-getter.rb
@@ -1,8 +1,8 @@
 class GoGetter < Formula
   desc "Package for downloading things from a string URL using a variety of protocols"
   homepage "https://github.com/hashicorp/go-getter"
-  url "https://github.com/hashicorp/go-getter/archive/refs/tags/v1.8.3.tar.gz"
-  sha256 "480e8106fe50127a4e5329cc5d09909cc2d7b48dfee80d6681679f483b3c560e"
+  url "https://github.com/hashicorp/go-getter/archive/refs/tags/v1.8.4.tar.gz"
+  sha256 "fb4d42583398e09715c44f208cd11bb45489d670577d1f2aa6974d029ba6accd"
   license "MPL-2.0"
   head "https://github.com/hashicorp/go-getter.git", branch: "main"
 
