619ddf0375210489a8e8a202003eb39e9050dfb2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-07 16:52:23 +0000

    wisu: update 0.1.4 bottle.

--- Formula/w/wisu.rb
+++ Formula/w/wisu.rb
@@ -8,11 +8,11 @@ class Wisu < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0e10a829cc771ed83d4af111ebc202a93d5d9a0465fdb4c2439623ee793f5be9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ae0914ce7925b0543f6d623435eddbcaf3afe5a7c54b477a02adee6b4727b6c3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d8841d8c7120d36d7963580834893b5313a6c168954945faa4b2340dda3e787d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8784fcf1d8b891b55757bc13477191e5e1ddd9aea5efcdf14326edba003cd288"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "95c7f144c087429687c4b5059bb3b7e4e4ab592ba5ee58c805a2993ca49c1557"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c405593b47a22b32a908e94a6772370c746b1f4df4aa046042bf55d90fd119c9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "944d81038ee5fed57d10a0a316ae5739bc8edc1fd09869071efe84038fbc770a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7f4c09a4164f2a5fc26040e948881517de207459bbf779e5be297d8572d2ed78"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2b3b9ae3dec228e5baddad206c6f8dca36666dc6e0f37cb5d32d248cee2a6abb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "741d365a048f314fe01eabc21aba6a900cfeea940a00691a1f5a426541b9b438"
   end
 
   depends_on "rust" => :build
