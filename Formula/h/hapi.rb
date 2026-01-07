ef7c49c59ff07ad5ffda12f08f713f4407d7f8a6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-07 12:43:08 +0000

    hapi: update 0.7.0 bottle.

--- Formula/h/hapi.rb
+++ Formula/h/hapi.rb
@@ -7,11 +7,11 @@ class Hapi < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "44917dec0a629befcef0e2d042eaaf0512b731dccef9b82cb2347b419ce402dd"
-    sha256                               arm64_sequoia: "44917dec0a629befcef0e2d042eaaf0512b731dccef9b82cb2347b419ce402dd"
-    sha256                               arm64_sonoma:  "44917dec0a629befcef0e2d042eaaf0512b731dccef9b82cb2347b419ce402dd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c23efa1d4159c09a1a53705e62528a928922f728952a715934a6b0051a55711"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dd456920e51cf1a63974d90f3307a2d2c9fcb5a7312b8b57004760b29fa50440"
+    sha256                               arm64_tahoe:   "ab2d657c6b1731735aeb66f4b627b92ea5a3c622c67ba6eb4a5df440fc326a7d"
+    sha256                               arm64_sequoia: "ab2d657c6b1731735aeb66f4b627b92ea5a3c622c67ba6eb4a5df440fc326a7d"
+    sha256                               arm64_sonoma:  "ab2d657c6b1731735aeb66f4b627b92ea5a3c622c67ba6eb4a5df440fc326a7d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a22e242673fb4a68997f65c1dcb4a0b1353565af087bde7e9822ac203c46afa3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89e55b7d320fd06fec57b4420abeecf44fd928a2237ebff7c87579e6c482f916"
   end
 
   depends_on "node"
