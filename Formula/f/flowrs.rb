66d5500afdea7285d6f361dad1881cb6f4119167
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-08 10:27:35 +0000

    flowrs 0.8.1

--- Formula/f/flowrs.rb
+++ Formula/f/flowrs.rb
@@ -1,8 +1,8 @@
 class Flowrs < Formula
   desc "TUI application for Apache Airflow"
   homepage "https://github.com/jvanbuel/flowrs"
-  url "https://github.com/jvanbuel/flowrs/archive/refs/tags/v0.8.0.tar.gz"
-  sha256 "d6be1acb58c18f875ee0c6cd8e24d16729ce96d89ee864198fe7ab767f127631"
+  url "https://github.com/jvanbuel/flowrs/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "ec2f5f04ec77d7c63cf40da03570d2593ba23a60e835c94102efa9b2caaa9aa5"
   license "MIT"
   head "https://github.com/jvanbuel/flowrs.git", branch: "main"
 
