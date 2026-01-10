ea5d9d7b5926816868c4d1dd112706b3c91f48ff
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-10 04:41:26 +0000

    codebuff: update 1.0.579 bottle.

--- Formula/c/codebuff.rb
+++ Formula/c/codebuff.rb
@@ -7,7 +7,7 @@ class Codebuff < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "afacff4662d35b370eede9aadcaef07b1d0595bf335b4f907bcde4379f9b944e"
+    sha256 cellar: :any_skip_relocation, all: "ad8705e19eea4950da02e25c4e89da127546b2e71a28a2639d5382a34319b288"
   end
 
   depends_on "node"
