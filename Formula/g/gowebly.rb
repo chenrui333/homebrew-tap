2ec5628e18e3916b26a6935c8f2813a3192a08fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-12 14:39:53 +0000

    gowebly: update 3.0.6 bottle.

--- Formula/g/gowebly.rb
+++ Formula/g/gowebly.rb
@@ -8,10 +8,11 @@ class Gowebly < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "af897b390d3488ddcddec45461973bcd869bc9b1bb42bba571a11b8e5df6d952"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5b018a9cc5b5c56caadeb27b0a5d04033993be3d752675b408b1e54d7ea17cae"
-    sha256 cellar: :any_skip_relocation, ventura:       "1cbed5ed2542630e88dfc3ebd425f9ad628b3a7ddd14d363261ea63c93d0d134"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d17d34bc250cae232adc87ea05d1320127a3632fa5b3f43e2e0ece58d8fd9b60"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dff27a4bfc117cc6494753935f2e28429eab44ee2c2a3fcebae291080377f43e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dff27a4bfc117cc6494753935f2e28429eab44ee2c2a3fcebae291080377f43e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dff27a4bfc117cc6494753935f2e28429eab44ee2c2a3fcebae291080377f43e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0a96144204166b8c1ab75b1191e7a329cbe84f2130aae6ac7bf3219607334ad2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cbb061940b4287dcb7231da3dc3378e08e5a975481eb6db3323f7cb185e3501e"
   end
 
   depends_on "go" => :build
