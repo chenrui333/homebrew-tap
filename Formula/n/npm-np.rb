6a15ee0222b7a5b38f61e03a07c360d78fe4ac81
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-18 14:28:40 +0000

    npm-np 10.3.0

--- Formula/n/npm-np.rb
+++ Formula/n/npm-np.rb
@@ -1,8 +1,8 @@
 class NpmNp < Formula
   desc "Better `npm publish`"
   homepage "https://github.com/sindresorhus/np"
-  url "https://registry.npmjs.org/np/-/np-10.2.0.tgz"
-  sha256 "e89644fe0a2100f468f3159f402256fa4392cd5782f28fa174b42e5fd46d30ee"
+  url "https://registry.npmjs.org/np/-/np-10.3.0.tgz"
+  sha256 "48b0e2aafce05698249adb66842a4f671e168f823ed2c7f5a0989fe4178a52bf"
   license "MIT"
 
   bottle do
