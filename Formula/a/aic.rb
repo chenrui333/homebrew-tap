732e8fec8e983507f753622daca93eb9527614d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-20 21:00:00 +0000

    aic: add 2.2.1 bottle.

--- Formula/a/aic.rb
+++ Formula/a/aic.rb
@@ -5,6 +5,15 @@ class Aic < Formula
   sha256 "1179c3d3910399aa096c4531e65bb99acf020698ae44927b1fbccea9f5167c81"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "58ff358802e52ea75e4351c186c52c7671cccc38c2cefbecba05582a6284967a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "58ff358802e52ea75e4351c186c52c7671cccc38c2cefbecba05582a6284967a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "58ff358802e52ea75e4351c186c52c7671cccc38c2cefbecba05582a6284967a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8c77aa5c0ee25abeac5364fefb916a91bb76ad58bbba92d8bc08247fc91d0f24"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f0582fd897a5416e85cb992e3d984c6cc63b24a8e71a827b292012adc084d9b"
+  end
+
   depends_on "go" => :build
 
   def install
