539de00fdaff8cfb26131185a79fb8f97aa40f36
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-21 10:38:22 +0000

    cmdperf: update 0.1.4 bottle.

--- Formula/c/cmdperf.rb
+++ Formula/c/cmdperf.rb
@@ -7,11 +7,11 @@ class Cmdperf < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5c9ad948d3c3116d6fb1a1d316cb2605f1b30e08d41cad0add308f2f8546a840"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5c9ad948d3c3116d6fb1a1d316cb2605f1b30e08d41cad0add308f2f8546a840"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5c9ad948d3c3116d6fb1a1d316cb2605f1b30e08d41cad0add308f2f8546a840"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d0c6e85acb118b9475faaab690f231f4b41568d9854ea1ceb0f3c89b8ba8e7cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "75a653cde61ed3768acea4373110dc02143d2c122566575bf97d5632deea0449"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "893f3e4f1e52e128258a526c194f2c7a4bc9023111fc27ccf13e53248741ef74"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "893f3e4f1e52e128258a526c194f2c7a4bc9023111fc27ccf13e53248741ef74"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "893f3e4f1e52e128258a526c194f2c7a4bc9023111fc27ccf13e53248741ef74"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "161b3c366089d7a4b319e52f1799a2e902a67e243ac0af1d09d07bbb522abd3b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49082bff4e90a9112d86d0acffcbed0d0fe358bfac93166317639dea4ca1f47e"
   end
 
   depends_on "go" => :build
