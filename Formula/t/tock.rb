f8f8ca9662a0fda1051059b70cde84e4c94bd179
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 12:48:43 +0000

    tock: update 1.6.3 bottle.

--- Formula/t/tock.rb
+++ Formula/t/tock.rb
@@ -8,11 +8,11 @@ class Tock < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "eca4efcc09418546a77344a6ecdf31ae54f2a7d0992164b961db2819795b71a5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eca4efcc09418546a77344a6ecdf31ae54f2a7d0992164b961db2819795b71a5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eca4efcc09418546a77344a6ecdf31ae54f2a7d0992164b961db2819795b71a5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "98200a37c7b84160b2832213337e59614089174a1c33a82c247f18ad0df1ada5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0c9c9b2dcb9d269afcb738fa4addccaaad1c40975c09a7c792b16aee91185da"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "98354377fe83c8b4f23f88a9e82616d034269762f37982bd6ef7eb5b41238416"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "98354377fe83c8b4f23f88a9e82616d034269762f37982bd6ef7eb5b41238416"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "98354377fe83c8b4f23f88a9e82616d034269762f37982bd6ef7eb5b41238416"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5885161d2b9f332ec0449cbf2127bd435d8e98d5d53fc2127af82b85b679700f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a032aedc0395f43cad65eae6d81ce9be805724da121b0443226ef806f6fbefce"
   end
 
   depends_on "go" => :build
