0a76ca2f9545740141eacb1461541ccbd6117ed5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 06:29:45 +0000

    mcp-use 2.12.1

--- Formula/m/mcp-use.rb
+++ Formula/m/mcp-use.rb
@@ -1,8 +1,8 @@
 class McpUse < Formula
   desc "CLI for mcp-use"
   homepage "https://mcp-use.com/"
-  url "https://registry.npmjs.org/@mcp-use/cli/-/cli-2.6.1.tgz"
-  sha256 "fea61b9a5b825fafbd330d6762f26410eb44cbf7b8435bade2c2125b282324f7"
+  url "https://registry.npmjs.org/@mcp-use/cli/-/cli-2.12.1.tgz"
+  sha256 "e990d3b5e6d262d601eeb7b0ccfc9809fc84158bce9da72a14470e04be55f637"
   license "MIT"
 
   bottle do
