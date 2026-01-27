a62db4b5d4673dfc1d2b6d6028c450676b377658
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-01-27 14:30:08 +0000

    hapi 0.14.0
    Closes #3922.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/h/hapi.rb
+++ Formula/h/hapi.rb
@@ -1,8 +1,8 @@
 class Hapi < Formula
   desc "Agentic coding - access coding agent anywhere"
   homepage "https://github.com/tiann/hapi"
-  url "https://registry.npmjs.org/@twsxtd/hapi/-/hapi-0.13.0.tgz"
-  sha256 "1338cd2acfc267138258036d6e1fc643ee787211fd560ca92357ec95e285dd78"
+  url "https://registry.npmjs.org/@twsxtd/hapi/-/hapi-0.14.0.tgz"
+  sha256 "f887334187afdad53bade93dc95d0ed69319d6fcb9773dc71686e1c95baafc9d"
   license "MIT"
 
   bottle do
