10b9b1799c82c4d6df786c894bf6d46773dd7fb8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-04 16:31:27 +0000

    code-pushup 0.113.0

--- Formula/c/code-pushup.rb
+++ Formula/c/code-pushup.rb
@@ -1,8 +1,8 @@
 class CodePushup < Formula
   desc "CLI to run all kinds of code quality measurements"
   homepage "https://code-pushup.dev/"
-  url "https://registry.npmjs.org/@code-pushup/cli/-/cli-0.93.0.tgz"
-  sha256 "36bb31adc49e122cd006e6d89dce6ae8aaa0064e6f5f50c62600a8f07f68606f"
+  url "https://registry.npmjs.org/@code-pushup/cli/-/cli-0.113.0.tgz"
+  sha256 "6e974d3ab85d6699d49a00b0e453c3a0633af353fab2004b613fb36d67b57fa9"
   license "MIT"
 
   bottle do
