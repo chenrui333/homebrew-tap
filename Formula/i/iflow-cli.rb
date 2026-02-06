54ffdee4860b650fd4aaed4a2e2344d40fc82fcd
Rui Chen <rui@chenrui.dev>
2026-02-06 03:26:37 -0500

    iflow-cli: add terminal-notifier update
    Closes #4058.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/i/iflow-cli.rb
+++ Formula/i/iflow-cli.rb
@@ -16,13 +16,30 @@ class IflowCli < Formula
 
   depends_on "node"
 
+  on_macos do
+    depends_on "terminal-notifier"
+  end
+
   def install
-    system "npm", "install", *std_npm_args
+    system "npm", "install", *std_npm_args(ignore_scripts: false)
     bin.install_symlink Dir["#{libexec}/bin/*"]
 
     # Remove incompatible pre-built binaries
     ripgrep_vendor_dir = libexec/"lib/node_modules/@iflow-ai/iflow-cli/vendors/ripgrep"
     rm_r(ripgrep_vendor_dir)
+
+    # Remove vendored pre-built binary `terminal-notifier`
+    node_notifier_vendor_dir = libexec/"lib/node_modules/@iflow-ai/iflow-cli/node_modules/node-notifier/vendor"
+    rm_r(node_notifier_vendor_dir) # remove vendored pre-built binaries
+
+    if OS.mac?
+      terminal_notifier_dir = node_notifier_vendor_dir/"mac.noindex"
+      terminal_notifier_dir.mkpath
+
+      # replace vendored `terminal-notifier` with our own
+      terminal_notifier_app = Formula["terminal-notifier"].opt_prefix/"terminal-notifier.app"
+      ln_sf terminal_notifier_app.relative_path_from(terminal_notifier_dir), terminal_notifier_dir
+    end
   end
 
   test do
