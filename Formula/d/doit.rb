5827cc1ab539234aac8c62cfd12768ce19a1038f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-09 08:36:10 +0000

    doit 0.37.0
    Closes #4090.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/d/doit.rb
+++ Formula/d/doit.rb
@@ -3,8 +3,8 @@ class Doit < Formula
 
   desc "CLI task management & automation tool"
   homepage "https://pydoit.org/"
-  url "https://files.pythonhosted.org/packages/5a/36/66b7dea1bb5688ba0d2d7bc113e9c0d57df697bd3f39ce2a139d9612aeee/doit-0.36.0.tar.gz"
-  sha256 "71d07ccc9514cb22fe59d98999577665eaab57e16f644d04336ae0b4bae234bc"
+  url "https://files.pythonhosted.org/packages/35/f6/3a817d438799bda4d4e5fd136175cf7c328c074fadc1422dec3b374907e7/doit-0.37.0.tar.gz"
+  sha256 "d3c72e0e46a8fa1ddabea8f830762402dee090caf33c30c2295ac7010db8f09c"
   license "MIT"
 
   bottle do
@@ -14,21 +14,6 @@ class Doit < Formula
   end
 
   depends_on "python@3.14"
-
-  resource "cloudpickle" do
-    url "https://files.pythonhosted.org/packages/52/39/069100b84d7418bc358d81669d5748efb14b9cceacd2f9c75f550424132f/cloudpickle-3.1.1.tar.gz"
-    sha256 "b216fa8ae4019d5482a8ac3c95d8f6346115d8835911fd4aefd1a445e4242c64"
-  end
-
-  resource "importlib-metadata" do
-    url "https://files.pythonhosted.org/packages/76/66/650a33bd90f786193e4de4b3ad86ea60b53c89b669a5c7be931fac31cdb0/importlib_metadata-8.7.0.tar.gz"
-    sha256 "d13b81ad223b890aa16c5471f2ac3056cf76c5f10f82d6f9292f0b415f389000"
-  end
-
-  resource "zipp" do
-    url "https://files.pythonhosted.org/packages/e3/02/0f2892c661036d50ede074e376733dca2ae7c6eb617489437771209d4180/zipp-3.23.0.tar.gz"
-    sha256 "a07157588a12518c9d4034df3fbbee09c814741a33ff63c05fa29d26a2404166"
-  end
 
   def install
     virtualenv_install_with_resources
