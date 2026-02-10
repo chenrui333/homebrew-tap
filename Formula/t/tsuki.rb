01aa505e79018b126f5814476858856cf15fd971
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-10 13:02:30 +0000

    tsuki: update 0.4.6 bottle.

--- Formula/t/tsuki.rb
+++ Formula/t/tsuki.rb
@@ -8,7 +8,7 @@ class Tsuki < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "c3f7d294caf1b3d9b1a69ca93132b873a3e83b14fba6768b42dae9b45066877f"
+    sha256 cellar: :any_skip_relocation, all: "cab4d3f24003bb0f8bd0bf6d19069539981850e56e8a991423492d00c17e982a"
   end
 
   depends_on "rust" => [:build, :test]
