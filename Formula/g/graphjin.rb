6b23691f89aca5f47a9865daeb6ed19ab707d766
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-10 10:55:20 +0000

    graphjin: update 3.7.0 bottle.

--- Formula/g/graphjin.rb
+++ Formula/g/graphjin.rb
@@ -8,11 +8,11 @@ class Graphjin < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a9c412fdc3bce5faf903d8015ee96b028fd50d866881d5029d52004a23f6cf40"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2dfff754bc9e9d26612eac41d10fd0a2016899018bedb81c21b462d300225292"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b95ae9fb94cf33cd7900dfe813ba1f5c85deb7341f4d161fbcd92e0e93bea37d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "33f13fffb7298538183ea683a91751afe5e6275ae8ca134a5cf50833d63399c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2896d99c2645180f3b4e49d69be943269fdc9487158ceb2795fc012a055bd543"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "912fae7fc1e999d625529fb3252942e6efc2c844e13b3c48fea262bfb2847bbe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "453c973fe0da42c18bb4c9d8704799d893d468a9d5dd8b19e3b17e1f72f2b5ca"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d1128cf8baafffd7264c4e6b90440656ea341f5faa9a350cd4b833ec355711de"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "45a1a4c10cfcf029f5e6cdad7918a854c055f4b8af988fa2cb42a37d64306eaa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6a5f6d8f15385c30e33540ca8066b6305fa7fd4b357e5e0378f7af3d8e088c6b"
   end
 
   depends_on "go" => :build
