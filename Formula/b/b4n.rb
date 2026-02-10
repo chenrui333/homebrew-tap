a6d177d915337f7241f28f73339bfd793df60de8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-10 22:28:30 +0000

    b4n 0.4.1
    Closes #4112.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/b/b4n.rb
+++ Formula/b/b4n.rb
@@ -1,8 +1,8 @@
 class B4n < Formula
   desc "Terminal user interface (TUI) for Kubernetes API"
   homepage "https://github.com/fioletoven/b4n"
-  url "https://github.com/fioletoven/b4n/archive/refs/tags/v0.3.9.tar.gz"
-  sha256 "cc1267903e89b653ff9d903f349cd73e9108c632b0e703bd1401b239ab95ff79"
+  url "https://github.com/fioletoven/b4n/archive/refs/tags/v0.4.1.tar.gz"
+  sha256 "9d435e3205d5acf39d4b3c67d2e3ec65437a1407ac0573015fe2b35a73e5a3ab"
   license "MIT"
   head "https://github.com/fioletoven/b4n.git", branch: "main"
 
