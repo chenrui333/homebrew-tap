43247b576a26fa0651414361fe10947e70f5c41a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-12 17:27:33 +0000

    gittype: update 0.9.0 bottle.

--- Formula/g/gittype.rb
+++ Formula/g/gittype.rb
@@ -8,9 +8,11 @@ class Gittype < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a6bed04f6d2b0b26278a25ab92279726cbc4440baefe6900c657db3dbc1b5511"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "85b89d9559891dfeb0465561a9d766b14cc4114ee08f7c087a65b8c67a11cda1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d4fdd8c7e6651cd704c9dcf9836a64fe07cd7f547c00a8a6352dfb93e2525062"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4ed131e1bc7e6571f4bb559086155ebf09524c2c9fe8c443951b9a5bd3ab15b1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e399a2c84b8ebfae90114c9ef0f685f8c518c49f523bed4980f4b1b741f2bd33"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "90f79a2e6b95667414ebb61d72cf1988f3fffc85a983acbdabb58663964f4617"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1563da30de7b89d8b0116d3db0b4386aef71372f3bb405d86d644f08636ed6dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "42980aa4c99e7fd9b3cc7407d4c0e18c5198bc5d3b18249977cf974a9deb0135"
   end
 
   depends_on "pkgconf" => :build
