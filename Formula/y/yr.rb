358e223f8cc11e9d6d5df2e2c024b9ebcc53ee11
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 22:33:39 +0000

    yr 1.1.0
    yr: update test
    
    Closes #4172.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/y/yr.rb
+++ Formula/y/yr.rb
@@ -1,8 +1,8 @@
 class Yr < Formula
   desc "Get the weather delivered to your command-line"
   homepage "https://git.sr.ht/~timharek/yr"
-  url "https://git.sr.ht/~timharek/yr/archive/v1.0.0.tar.gz"
-  sha256 "bc8980f06abc23b80fa2e8a8adcfbeeb3fa98b35d4ae883540c9e3c6cb627cba"
+  url "https://git.sr.ht/~timharek/yr/archive/v1.1.0.tar.gz"
+  sha256 "cf7b92d980f74278623306f4b715acfd69c629266849f61999570005b3f2cc7e"
   license "GPL-3.0-only"
   head "https://git.sr.ht/~timharek/yr", branch: "main"
 
@@ -27,7 +27,6 @@ class Yr < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/yr --version")
 
-    output = shell_output("#{bin}/yr now nyc")
-    assert_match "City of New York", output
+    assert_match "New York", shell_output("#{bin}/yr now nyc")
   end
 end
