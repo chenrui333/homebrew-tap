06f25faec71095f45bbc8e5b7cee5a8d4496b8e6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-13 20:08:59 +0000

    repology: add 1.9.0 bottle.

--- Formula/r/repology.rb
+++ Formula/r/repology.rb
@@ -6,6 +6,15 @@ class Repology < Formula
   license "ISC"
   head "https://github.com/ibara/repology.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0fd47ba1124fa91872d5490f078d8ab79a396a99e30ccdd58e4c57160ee7ac57"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80f86287084b9b8ebcdee3293ab69da653706fe571b7ddb34801d5056b8810cf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e6e668bb54e070597e77492c322ce3f1b7c90c38b65111cfed33207107427c58"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0ce2e2827d3dd333c98a27474ac44d832787bfffef1d86c7c17ee1801ed0e69d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3cf7cabe083cd20aae08720dcbd67c4dd9d23fb886a1752cb8921a54a3cf964f"
+  end
+
   depends_on "ldc" => :build
 
   def install
