3383755150a45da8978691b7589a04047eef97fc
Rui Chen <rui@chenrui.dev>
2026-02-13 16:44:32 -0500

    dominds 0.8.11 (new formula)
    Closes #4168.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/d/dominds.rb
@@ -0,0 +1,21 @@
+class Dominds < Formula
+  desc "AI-driven DevOps framework with persistent memory"
+  homepage "https://github.com/longrun-ai/dominds"
+  url "https://registry.npmjs.org/dominds/-/dominds-0.8.11.tgz"
+  sha256 "39ba2c00b0785fd479b9a3df8d02ed529c94d341ac5849ea6162f54abff8eeef"
+  license "LGPL-3.0-or-later"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dominds --version")
+
+    output = shell_output("#{bin}/dominds manual ws_read --lang en --all")
+    assert_match "Toolset manual: ws_read", output
+  end
+end
