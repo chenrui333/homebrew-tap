c2c631ffe84350aeb6faa34522972dae770a1868
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-15 10:44:27 +0000

    yatto: update 1.0.0 bottle.

--- Formula/y/yatto.rb
+++ Formula/y/yatto.rb
@@ -8,11 +8,11 @@ class Yatto < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d17039a92c3b76fad6edb8fcd620f218d91b4ddb8afd29a25a1b005501c7decf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d17039a92c3b76fad6edb8fcd620f218d91b4ddb8afd29a25a1b005501c7decf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d17039a92c3b76fad6edb8fcd620f218d91b4ddb8afd29a25a1b005501c7decf"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ad07f871216df42150b7703686ba1bb48013242e5d6a44b50f974d8dba2f0ab8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "80fa0d4378749d240c31fd6e664649e516dcf3c1df3dc1997c2216b3588dd556"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bbffe5c5bde47421c75197edd837cc2f117b190a3181c3332dd536d82704b2dc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bbffe5c5bde47421c75197edd837cc2f117b190a3181c3332dd536d82704b2dc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bbffe5c5bde47421c75197edd837cc2f117b190a3181c3332dd536d82704b2dc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a005e76bafa531e68e7de2db9e1819221328f60873808c5b4167004a0a41b240"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "91a644acb704a4e01b93ea2b31e0ea89553f1a781260e5b3420266d8d03168cb"
   end
 
   depends_on "go" => :build
