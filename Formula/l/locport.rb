9bfdaeb0450864d91329f148b03e17cc2bafb107
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-15 10:29:37 +0000

    locport 1.3.0
    locport: bottle for each os
    
    Closes #4205.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/l/locport.rb
+++ Formula/l/locport.rb
@@ -1,14 +1,9 @@
 class Locport < Formula
   desc "Manage local ports across projects"
   homepage "https://github.com/klevo/locport"
-  url "https://github.com/klevo/locport/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "5c30f9930c45ce4c8052235f1d8c2df3741e20f56a864be53ae1fcc8aee3ddfa"
+  url "https://github.com/klevo/locport/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "bbcf5132d77fc1f058df061881a8f098ad31f050298bb3d00d1ab59e75dcde37"
   license "MIT"
-
-  bottle do
-    root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "cbbb2e9c9366740f77900f56fd7c0e95139795226e868c926450e2e0cd846394"
-  end
 
   depends_on "ruby"
 
