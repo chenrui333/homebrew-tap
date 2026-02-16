fe9e83987469347d017532f4639b0cb65476257d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-16 22:42:12 +0000

    create-mcp-use-app: update 0.13.0 bottle.

--- Formula/c/create-mcp-use-app.rb
+++ Formula/c/create-mcp-use-app.rb
@@ -7,7 +7,7 @@ class CreateMcpUseApp < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "be1043d577c9fcaff78c463a737adfb8b95e28074e746024ae9e9f984304f6b1"
+    sha256 cellar: :any_skip_relocation, all: "8f11b9b743bd044eb1b84ed0d69306071129ba354a3ec6b76fb8b6d81a4206d7"
   end
 
   depends_on "node"
