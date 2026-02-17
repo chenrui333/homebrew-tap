cddec3aef84354ee7d0f2ec8ceb2216156b0fbc0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 00:07:47 +0000

    locport: add 1.3.0 bottle.

--- Formula/l/locport.rb
+++ Formula/l/locport.rb
@@ -5,6 +5,15 @@ class Locport < Formula
   sha256 "bbcf5132d77fc1f058df061881a8f098ad31f050298bb3d00d1ab59e75dcde37"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c169631562bd7c78236ed13d065f0cfbfb330aaa9ed62323c5879bda4185851a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c169631562bd7c78236ed13d065f0cfbfb330aaa9ed62323c5879bda4185851a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c169631562bd7c78236ed13d065f0cfbfb330aaa9ed62323c5879bda4185851a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "25e681b10888d0ddcf590be35163067224e38b61acfcfaca8b8cce717eeb6ff5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25e681b10888d0ddcf590be35163067224e38b61acfcfaca8b8cce717eeb6ff5"
+  end
+
   depends_on "ruby"
 
   def install
