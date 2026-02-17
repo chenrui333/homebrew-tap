d6f5e85a077ce020b27cbb1eef87c5f382d60183
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-17 18:44:45 +0000

    rtk: add 0.20.1 bottle.

--- Formula/r/rtk.rb
+++ Formula/r/rtk.rb
@@ -6,6 +6,15 @@ class Rtk < Formula
   license "MIT"
   head "https://github.com/rtk-ai/rtk.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7b4a578c65c42909f11423319a393d0ee97b3383cc8ed584a767a8d6a1322bd9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "30a74f201c82e81175cdf86bbec9b1c9c988e9feabf52df6765ed6310cd5cb4f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3e09f201d2afa52c927699421617bc80cd003c2f0489a134e006a62debc78152"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "aad01dab9dc63dc995d71f68ca214c9adb152460a608100ec3414e3cee7b8364"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5d49b98f11bf1444f7acd039cbd288d9e6f443afd2cc054a5b079535eee56c2e"
+  end
+
   depends_on "rust" => :build
 
   def install
