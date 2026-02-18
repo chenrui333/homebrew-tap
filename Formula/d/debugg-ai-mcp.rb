6a4f5e8a38a84a5f8e25f3fc790dce5db9ff19a8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-18 03:19:40 +0000

    debugg-ai-mcp 1.0.18

--- Formula/d/debugg-ai-mcp.rb
+++ Formula/d/debugg-ai-mcp.rb
@@ -2,8 +2,8 @@
 class DebuggAiMcp < Formula
   desc "MCP Server for Debugg AI"
   homepage "https://debugg.ai/"
-  url "https://registry.npmjs.org/@debugg-ai/debugg-ai-mcp/-/debugg-ai-mcp-1.0.16.tgz"
-  sha256 "3c52ed2403196b68520ea2c3f03986b27ebbb0d38361babc5237c426fd43d2dd"
+  url "https://registry.npmjs.org/@debugg-ai/debugg-ai-mcp/-/debugg-ai-mcp-1.0.18.tgz"
+  sha256 "9a822d81ba8f41a20ed00e67944da96746a013fd2087821042a3cbafa199ae01"
   license "Apache-2.0" # license fix PR, https://github.com/debugg-ai/debugg-ai-mcp/pull/4
 
   bottle do
