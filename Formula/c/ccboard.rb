aaf61648fa2ee3bfe03b3cd15aeccf3661a4e055
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-18 22:19:52 +0000

    ccboard: add 0.8.0 bottle.

--- Formula/c/ccboard.rb
+++ Formula/c/ccboard.rb
@@ -6,6 +6,15 @@ class Ccboard < Formula
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/FlorianBruniaux/ccboard.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ccf4fa7ecd62e92988daeaf42e4c370a46e75b6a1270cade512cc142f0c37a1e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d058a57191c976380482934439ebcddd7d633b9fa7333265c9329f71e59e6b89"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ec11815dcd1ea292e62499c24aa09334c7158f9d7d6cf0c3bf5b83431391c859"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "937d59e42fff1489614bf328c443e39d3f9ee9216199f4bc0573158cbe3771d0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "85939bd0a62277532c510dd400f50b4f60a4a1680d5eb956777d658077a3d74b"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
