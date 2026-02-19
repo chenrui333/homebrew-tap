fbc5c81e4e59b4d1a029de865d0eedee7ad9f0a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 21:47:35 +0000

    oeis-tui: add 1.0.0 bottle.

--- Formula/o/oeis-tui.rb
+++ Formula/o/oeis-tui.rb
@@ -6,6 +6,15 @@ class OeisTui < Formula
   license "MIT"
   head "https://github.com/hako/oeis-tui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2d222e916ffa8f58a6f2219073c74fd37bedfaf733f7665886b35be21be8bd7e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bb5fd2e917bb3dcd0a55cdf7cb93affac8f32ccc6aa329b5391fc396bb60be48"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e6b0846d4e523589ee1b7076983c671fd9dee20a23cfbc74599e33dbfd1910e6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2b92fb1aa8835daabb51a90476beb4fa4299ba0ac056586dc12a6004ad5e9652"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f24df548103ce53702886ec909b26c130d5617b25c397ff91c24e8005fc7d5c6"
+  end
+
   depends_on "rust" => :build
 
   def install
