78c648729833cec81dfe7a78954aaa5a4be2365e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 10:51:42 +0000

    dory-cli: update 0.42.0 bottle.

--- Formula/d/dory-cli.rb
+++ Formula/d/dory-cli.rb
@@ -7,11 +7,11 @@ class DoryCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "d5b45c01565db683755b0d7ed0689a6eb0a8693427d47b0eeba35b25decb60d6"
-    sha256 cellar: :any,                 arm64_sequoia: "e8d8bbc02037888df57e48fdd9b3dae45da02570b3a7f8b2bf304bf88047f0ed"
-    sha256 cellar: :any,                 arm64_sonoma:  "e8d8bbc02037888df57e48fdd9b3dae45da02570b3a7f8b2bf304bf88047f0ed"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e6da1c21846669b56f22bfeb5161da838d2562b2351a8ac180f28a543d9a80ee"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c1c7eb69bac04dbd89961230958f719ca769d42c4f86b788cfa2104b30886c53"
+    sha256 cellar: :any,                 arm64_tahoe:   "68b560dcc1ad29a38e3bafa57c8b689e521a0bbdaa8e415e93de59c23d389003"
+    sha256 cellar: :any,                 arm64_sequoia: "9fa70fee4c2d0640df91670dfaa273bc04c03c8224c84ecbf3e1f76cfeaa0ded"
+    sha256 cellar: :any,                 arm64_sonoma:  "9fa70fee4c2d0640df91670dfaa273bc04c03c8224c84ecbf3e1f76cfeaa0ded"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c86f4a8acfe0ea1fc81c41f925f897f60f34b6bc9144d3f0f12deb408f6d700e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "323b267dabcdb1085a48304630974f29965c41db3091aabc87754cbc22cb042f"
   end
 
   depends_on "node"
