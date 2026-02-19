3bf6eb414b28fa68988f1f2e3844cf1d78174423
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 22:00:22 +0000

    hexowl: add 1.5.1 bottle.

--- Formula/h/hexowl.rb
+++ Formula/h/hexowl.rb
@@ -6,6 +6,15 @@ class Hexowl < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/dece2183/hexowl.git", branch: "dev"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f531da5e0122c1e048ceb06e955e0a115fe30a161b0d2f0a7b1a19bd2e08915a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "46062207f884edf40609e61b4469c1594c525f1393e5a1a679d5a4d905897329"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "33567f50dc9605fa11660dcf8cb48bbf50656b82e546005f104301b12a25a248"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d0810928411493b9fb8547af79dc00fbc2aedde8b559803d147f771d90bf5aff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bdd3578212563b9d15e51890d78c3ef3e8eac0ab9b554f44c6086d948b92c6c5"
+  end
+
   depends_on "go" => :build
 
   def install
