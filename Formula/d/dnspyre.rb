9114968609b5222373c45479cc2d0c56898ad2f1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 21:43:46 +0000

    dnspyre: add 3.10.0 bottle.

--- Formula/d/dnspyre.rb
+++ Formula/d/dnspyre.rb
@@ -6,6 +6,15 @@ class Dnspyre < Formula
   license "MIT"
   head "https://github.com/Tantalor93/dnspyre.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "38103548e5934019968ff918097cdb250640c4c5c46fa087155b8734b5fa780c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73d62392d220f058ff68a98caebcaab76760379afcdaed616e42a03443f5731e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "091a001f15af1c01553764f536f6940efa46eef958a84cf6566520f7569cccc4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8a1618c24775541dd2fc64bbd9df740fa4564f67400db803b94986fb828d0d93"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "627bfd9473830c1ffd464d96cdb80dde3aa2a1409d473d05d535131339800644"
+  end
+
   depends_on "go" => :build
 
   def install
