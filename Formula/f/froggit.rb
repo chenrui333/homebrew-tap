fd662069e8a0868764431218d8654cece81b528c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-19 22:17:41 +0000

    froggit: add 1.2.0-beta bottle.

--- Formula/f/froggit.rb
+++ Formula/f/froggit.rb
@@ -6,6 +6,15 @@ class Froggit < Formula
   license "MIT"
   head "https://github.com/thewizardshell/froggit.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2f8ac9946d5285e0d34d380ca2598e1f760bbd314730aad1c4bc99fef831a3bd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2f8ac9946d5285e0d34d380ca2598e1f760bbd314730aad1c4bc99fef831a3bd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2f8ac9946d5285e0d34d380ca2598e1f760bbd314730aad1c4bc99fef831a3bd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5f66ced7923d6085a46579e4f5a19a6e9bdd3e600239261bd2bb0b3282dfa6a6"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3f504b9d10b4276a83a81b75bfd753af627613e2b58deda5347f24fa19827635"
+  end
+
   depends_on "go" => :build
 
   def install
