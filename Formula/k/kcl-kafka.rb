d07ff730da2c18e2386f86f14064bbe39beccca8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 18:38:56 +0000

    kcl-kafka: add 0.17.0 bottle.

--- Formula/k/kcl-kafka.rb
+++ Formula/k/kcl-kafka.rb
@@ -6,6 +6,15 @@ class KclKafka < Formula
   license "BSD-3-Clause"
   head "https://github.com/twmb/kcl.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4f8346d8e9b93c8d5329bae91516eaf0803513ead24dd888665bab1805e56172"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4f8346d8e9b93c8d5329bae91516eaf0803513ead24dd888665bab1805e56172"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4f8346d8e9b93c8d5329bae91516eaf0803513ead24dd888665bab1805e56172"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2008fb57d1ef764a049c0b26b67917c541a2f959ecd0553540ff8c5ee229d2e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "89a9bba7b1cf2ced98a8ab991378b0d063df217b519cd290fb39945eaf211d39"
+  end
+
   depends_on "go" => :build
 
   def install
