32e3b58d3e7b5141527155e59993621fd0561c9c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 10:46:43 +0000

    semaphore: update 2.17.12 bottle.

--- Formula/s/semaphore.rb
+++ Formula/s/semaphore.rb
@@ -8,11 +8,11 @@ class Semaphore < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "66a7bb1b9ac67f6b70d4bcb2c97379f9af0dee78e10a3a83b775b682667c4b7c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "66a7bb1b9ac67f6b70d4bcb2c97379f9af0dee78e10a3a83b775b682667c4b7c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "66a7bb1b9ac67f6b70d4bcb2c97379f9af0dee78e10a3a83b775b682667c4b7c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e9edbcc76f14733bdb4101403878553697699fadec7729bd0afd88e93cc25da8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7e1bfc4542a715f432f037fca90694f770d3157431a2704c931af6d08f0c0c30"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "00cef120512dfac13b0afeadfca2244c0e1a61fbb7cdb2fda8f3718d8644f560"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "00cef120512dfac13b0afeadfca2244c0e1a61fbb7cdb2fda8f3718d8644f560"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "00cef120512dfac13b0afeadfca2244c0e1a61fbb7cdb2fda8f3718d8644f560"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bd2d049fe6956ae9dcd3fc025222c602299c9fd1abfda000ce7385ee896fc150"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2dc6ca61cc586552e38021999d768990c7a1267b94c9d60c5b3571d5837776b9"
   end
 
   depends_on "go" => :build
