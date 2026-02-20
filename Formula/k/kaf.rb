9aabb313c6e23864cde6c4d252c5ed40c933e24c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-20 18:25:38 +0000

    kaf: add 0.2.13 bottle.

--- Formula/k/kaf.rb
+++ Formula/k/kaf.rb
@@ -6,6 +6,15 @@ class Kaf < Formula
   license "Apache-2.0"
   head "https://github.com/birdayz/kaf.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "12b0f51e64ce92e90765846a82af44511676e9c60fa92031b0cc4b4f7635052c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "12b0f51e64ce92e90765846a82af44511676e9c60fa92031b0cc4b4f7635052c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "12b0f51e64ce92e90765846a82af44511676e9c60fa92031b0cc4b4f7635052c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9b8047dc8f4c415901514d77ec0a457734b6064fd56ce286c41df6488770bc62"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c5edb4ca249160285dc9d8dbad2510d19c0fb4f42734565c2654fb9d24b9178"
+  end
+
   depends_on "go" => :build
 
   def install
