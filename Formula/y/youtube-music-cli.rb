31c2ff7d44fa3cfb3ceb509fd35480c7ef6d5688
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 19:46:52 +0000

    youtube-music-cli: add 0.0.27 bottle.

--- Formula/y/youtube-music-cli.rb
+++ Formula/y/youtube-music-cli.rb
@@ -6,6 +6,15 @@ class YoutubeMusicCli < Formula
   license "MIT"
   head "https://github.com/involvex/youtube-music-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "345e0ac8d17088e7f91f094ea17aeee9f567a003323d7862a5ef6ec71f00ee9d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3de0cfbcdc107576ad9465a3aed056adfe2376314a052d518df70d899b498d04"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1898924f697f9edd7cfd1cd799b23e1c204345abac569f86055ffb4d51d2152e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3d35020790177ce20985593e0cf332053ae2472048fd24b9ba8348d64d5ececc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5bb99b8dd8d742f64b2dfd201fd6218c9e3c4d2ec66b4a8ec2f7aef8d7cdc09b"
+  end
+
   depends_on "mpv"
   depends_on "node"
   depends_on "yt-dlp"
