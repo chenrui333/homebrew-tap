f990cdb6accff1faaf9c394ff67a367a11f23d51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 06:56:14 +0000

    gokin: add 0.44.0 bottle.

--- Formula/g/gokin.rb
+++ Formula/g/gokin.rb
@@ -6,6 +6,15 @@ class Gokin < Formula
   license "MIT"
   head "https://github.com/ginkida/gokin.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e871c86c2fac706ab1b86af1342fb8b8b8f236154ea70f72a1d2ffec7c969643"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e871c86c2fac706ab1b86af1342fb8b8b8f236154ea70f72a1d2ffec7c969643"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e871c86c2fac706ab1b86af1342fb8b8b8f236154ea70f72a1d2ffec7c969643"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "213edda8ce30f59da825cf5ec43947b2b3e3c61ae91110da46b43f7ee536fb7e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "af6358ab4edc075173192e8139e9522122ba76b569fc9dab3a954cd6c940ea20"
+  end
+
   depends_on "go" => :build
 
   def install
