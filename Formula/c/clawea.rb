3379ee28186610ad5a37eba020cdd8b1f203a68a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 19:05:30 +0000

    clawea: add 1.2.0 bottle.

--- Formula/c/clawea.rb
+++ Formula/c/clawea.rb
@@ -6,6 +6,15 @@ class Clawea < Formula
   license :cannot_represent
   head "https://github.com/Cladamos/clawea.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a0d23b1df41ec244c84e1b88790b03eeeb964d2033c18c73fc0f5e10fc931d04"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a0d23b1df41ec244c84e1b88790b03eeeb964d2033c18c73fc0f5e10fc931d04"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a0d23b1df41ec244c84e1b88790b03eeeb964d2033c18c73fc0f5e10fc931d04"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "58b18d684b45bdef076b51b0b86f1cc4a83ea36f652f019f13f17f187efd3983"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "25f5c2cb5ddad7994cc020c1a2e4171a0cf5f42c00c3f143879528b8cebf3923"
+  end
+
   depends_on "go" => :build
 
   def install
