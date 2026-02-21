40b799742f2cfabea9d345cc325f1e48f283e6e3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 20:29:41 +0000

    auox: add 0.0.1 bottle.

--- Formula/a/auox.rb
+++ Formula/a/auox.rb
@@ -7,6 +7,15 @@ class Auox < Formula
   license "MIT"
   head "https://github.com/sverrejb/auox.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7a2af2e567e11f896c3c1ba7ed57954c01e40106f8a0f5271a32d60015bddefc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5699bbd5efed65615743f352c2047fbb783428f2c6acb327e397cde872d112ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "45378be52ca031bb6dd4e21f7562e89d7a18009237419b9dae15e9b1b2a41271"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9e5a2d85f92ce2d8a0b398e85383c443e96aab77ebaedb6212247c3001be632f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e7742a74c053e5554fb247dc2d5b02ef740b57e79325ffd24733ad7099d0590b"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
