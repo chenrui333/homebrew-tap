85c9a799131ea16954218441d16d5ee420659573
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 07:20:26 +0000

    sqd: add 0.1.8 bottle.

--- Formula/s/sqd.rb
+++ Formula/s/sqd.rb
@@ -6,6 +6,15 @@ class Sqd < Formula
   license "MIT"
   head "https://github.com/albertoboccolini/sqd.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "867c3cd35c98f355ba7824b8a5fc99d4796cb9cb560b6d431a9c26ff6782caff"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "867c3cd35c98f355ba7824b8a5fc99d4796cb9cb560b6d431a9c26ff6782caff"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "867c3cd35c98f355ba7824b8a5fc99d4796cb9cb560b6d431a9c26ff6782caff"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e5c30f761ef759ef2e6d17ebd88c71ce59c4d011a9cddcfa6d4e8d36d96ab2a1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3a2f6ae4e16d0e70473782a19c682d322aa85738cc4bdeb6301c881191ae8da"
+  end
+
   depends_on "go" => :build
 
   def install
