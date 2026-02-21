cc6066bf67c6e9ab70cb865c113bc50ed04f1a59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 18:57:28 +0000

    iam-collect: update 0.1.170 bottle.

--- Formula/i/iam-collect.rb
+++ Formula/i/iam-collect.rb
@@ -7,7 +7,7 @@ class IamCollect < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "f5c79f5de9197d4598e851e4e4694549e32221d3ed87a71e512767dc153fc18a"
+    sha256 cellar: :any_skip_relocation, all: "c88aaa8912f742008ab7e3829ffbbd8c17c0e4285bab7764e73a6fb9d92d1c1a"
   end
 
   depends_on "node"
