013759ca662fe00eb0242715e60d9c56ee56fbea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 07:09:15 +0000

    macime: add 4.2.0 bottle.

--- Formula/m/macime.rb
+++ Formula/m/macime.rb
@@ -6,6 +6,14 @@ class Macime < Formula
   license "MIT"
   head "https://github.com/riodelphino/macime.git", branch: "4.x"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_tahoe:   "0bf26fb9a6af7dcdb846da1fb416f6d4c2a2bad5cf6015c031d431a4696c19c7"
+    sha256                               arm64_sequoia: "6b39155bae9d6d59e62fb557a05756bd981ee8d43c6e492edd3f253926a5f95d"
+    sha256                               arm64_sonoma:  "cdfa5ae4041d05ca76e57afad9daed0bf8d2e409b799a5101e504fcd70eb9419"
+    sha256 cellar: :any_skip_relocation, sequoia:       "782688a18df4b2045b9cb9f3e2e0dfdf247c598f4caaa8b5b8fcc2a403087562"
+  end
+
   depends_on :macos
 
   def install
