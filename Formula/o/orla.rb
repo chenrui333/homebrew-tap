55cc5312c4d858280c389da85e70fdf850fe47db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 07:23:24 +0000

    orla: add 1.1.1 bottle.

--- Formula/o/orla.rb
+++ Formula/o/orla.rb
@@ -6,6 +6,15 @@ class Orla < Formula
   license "MIT"
   head "https://github.com/dorcha-inc/orla.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "35bd70b1d255ce1bb14a71116b49b7dc88d7d6986c8aac23b59fd927de1c626b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "35bd70b1d255ce1bb14a71116b49b7dc88d7d6986c8aac23b59fd927de1c626b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "35bd70b1d255ce1bb14a71116b49b7dc88d7d6986c8aac23b59fd927de1c626b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "052778f3f3960f7659c7f4b53a109a546846e7918774a27e0d7fb65b5e319078"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3be116493889a24f21ff32ddfebb45edcec31e1eeaac59885c52d56fbf9e88e9"
+  end
+
   depends_on "go" => :build
 
   def install
