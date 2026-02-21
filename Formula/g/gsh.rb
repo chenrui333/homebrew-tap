a7bb954a5f2caba0c55f757ca183a9a16a6de4dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 07:24:26 +0000

    gsh: add 1.7.0 bottle.

--- Formula/g/gsh.rb
+++ Formula/g/gsh.rb
@@ -6,6 +6,15 @@ class Gsh < Formula
   license "GPL-3.0-only"
   head "https://github.com/atinylittleshell/gsh.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f577365d4f38eee3a212f62920e02ebdbe1f1c74eea4f3d99867eda5061ec923"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f577365d4f38eee3a212f62920e02ebdbe1f1c74eea4f3d99867eda5061ec923"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f577365d4f38eee3a212f62920e02ebdbe1f1c74eea4f3d99867eda5061ec923"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5a4da3a4df09b06c044e5134752532ba1cbd778285c290d1cb94d5f85f7fe294"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c4bc6d9653ca9ccef1f521f1b37548e6cf36080c35cc50588d225e460711b5af"
+  end
+
   depends_on "go" => :build
 
   def install
