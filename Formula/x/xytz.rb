6a3e0ecd652f607f22b0af651c79638504688f12
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 06:18:45 +0000

    xytz: add 0.8.4 bottle.

--- Formula/x/xytz.rb
+++ Formula/x/xytz.rb
@@ -6,6 +6,15 @@ class Xytz < Formula
   license "MIT"
   head "https://github.com/xdagiz/xytz.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "010bf94041ad06540988d5f3f9839159a4da02b5fe02e14e4ebfdaf15fdf85ee"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "010bf94041ad06540988d5f3f9839159a4da02b5fe02e14e4ebfdaf15fdf85ee"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "010bf94041ad06540988d5f3f9839159a4da02b5fe02e14e4ebfdaf15fdf85ee"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8b1e7aa3266e6c11d15c4937e6c5dfd6fabbbb5bab7a82e792ed28779823c3b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2d66389a541a22c37dfd27093d209c2c12571e93afa4a3317b49760af27788db"
+  end
+
   depends_on "go" => :build
   depends_on "ffmpeg"
   depends_on "yt-dlp"
