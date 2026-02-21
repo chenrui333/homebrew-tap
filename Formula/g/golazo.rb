f1397b38d811eb2dfc0f68f0884c498f304545d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 20:31:27 +0000

    golazo: add 0.22.0 bottle.

--- Formula/g/golazo.rb
+++ Formula/g/golazo.rb
@@ -6,6 +6,15 @@ class Golazo < Formula
   license "MIT"
   head "https://github.com/0xjuanma/golazo.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3a0a6b187885d5dcc7b08223e12d9d721219f6c26f8c91d8e008da0be1719c7b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3a0a6b187885d5dcc7b08223e12d9d721219f6c26f8c91d8e008da0be1719c7b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3a0a6b187885d5dcc7b08223e12d9d721219f6c26f8c91d8e008da0be1719c7b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "065b5deaf6a00f5b480ff65355a3569d09d92902734a415a5f841e7a2725a47d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d3b8279a4527c933f66f159161e30cb35b3016e442d582bee40b7125cf0b0fec"
+  end
+
   depends_on "go" => :build
 
   def install
