c2e71916e189c198cb314a21047f00d1a2ceda37
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-21 19:00:50 +0000

    rt: add 0.1.8 bottle.

--- Formula/r/rt.rb
+++ Formula/r/rt.rb
@@ -6,6 +6,15 @@ class Rt < Formula
   license "MIT"
   head "https://github.com/unvalley/rt.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "09cf97269ac25645e61746cf9e2618459018f57a101fc16b7eb43e3d922a2716"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a63931f0fe7f82a4dfe1e7383f8c3a79cc76d41e1278cd22f29ce95550882768"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "315ca561be406ecfd6a53fcb992b2421cf026b9ddb852d584585df3aa1e71811"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "54df232fbc8c9278a8fb1a00f76509211cd1070310e02b93a98f4718371820b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6ba6ff599f3ef5e6952ed8f016b6f9b691043eea0c4a8fcf216e0143c0002496"
+  end
+
   depends_on "rust" => :build
 
   def install
