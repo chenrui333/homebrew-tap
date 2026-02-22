770512b3b6cff11c8697ef91a90ad9acbccd23d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 01:43:37 +0000

    parm: add 0.1.6 bottle.

--- Formula/p/parm.rb
+++ Formula/p/parm.rb
@@ -6,6 +6,15 @@ class Parm < Formula
   license "GPL-3.0-only"
   head "https://github.com/alxrw/parm.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e50be298202b624738d5ebe6f25fa275d22ebf029c68ed2cbe4082a68e543ed4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c77c77675ba72fda0671b750fcb71aab81aaa0ca2d119848589cb96129261092"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "685e703ca5890063801fee44d4eafa3d4fdbc21b5230a38675897761a66c09a3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "988d9ee21e894a804edb30e3805ef9ec7aa036a671d3180ddc552fa8a486a479"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9750e08e56e93f6c45e5d85947a47a11a105bc4bd771e2d38ef207d9b763300b"
+  end
+
   depends_on "go" => :build
 
   def install
