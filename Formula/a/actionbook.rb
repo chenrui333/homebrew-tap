08a4f4097b509aa8a5daa05540c20b74182b3552
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 18:13:58 +0000

    actionbook: add 0.7.3 bottle.

--- Formula/a/actionbook.rb
+++ Formula/a/actionbook.rb
@@ -5,6 +5,15 @@ class Actionbook < Formula
   sha256 "eb8ba6df41c00819f937082e08758987aa18341e76cd62defb03ec0ce3f70433"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c55fc553735c0dcc72724c27d95eca0507f17b1e72b5e859836649da678b4cc8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96f376af481a2edf74b711e0749cc3a851b43260512ec0b9880cafb04d6d28f4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd33a5d16918a4a19cc569ea56d251c21335d5d13b593b0d62de4919927afcaf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1fd7556ec1b02d3ccf31e14cfdf0bcfdd6e610a02ea0a7da51409b0963377e2c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "19f5a31369e487de66518c0077e839c1deb3f00c22df2be99da9a0b1d8000c9c"
+  end
+
   depends_on "rust" => :build
 
   def install
