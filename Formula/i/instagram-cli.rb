d7029d20e34b6dd9355cde72847ed31c44d5deb3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 06:30:03 +0000

    instagram-cli: add 1.4.3 bottle.

--- Formula/i/instagram-cli.rb
+++ Formula/i/instagram-cli.rb
@@ -6,6 +6,15 @@ class InstagramCli < Formula
   license "MIT"
   head "https://github.com/supreme-gg-gg/instagram-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_tahoe:   "1e66f76efe293276c4367aee7b4b1cb3405f5d22cb28cadb07ee828cb77611b5"
+    sha256 cellar: :any,                 arm64_sequoia: "61e13afe2e3d4b7fa8dc69886c5cb687e1f47a5e0a7dc787c59e4702d88603d0"
+    sha256 cellar: :any,                 arm64_sonoma:  "61e13afe2e3d4b7fa8dc69886c5cb687e1f47a5e0a7dc787c59e4702d88603d0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f3ea723d7a875a1603fb6ab9d6ba17af8088caa6ac7adb736917d558a02f71ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fad8b6eec7e7ec4232355fef9efe4701ed96c0cb2b083fef5909cec824d9644a"
+  end
+
   depends_on "node"
 
   def install
