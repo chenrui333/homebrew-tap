0a95b6ba80da6464f92c7fec3d6439df61198ee0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 00:54:50 +0000

    gix 0.2.7

--- Formula/g/gix.rb
+++ Formula/g/gix.rb
@@ -1,8 +1,8 @@
 class Gix < Formula
   desc "Git, but with superpowers"
   homepage "https://github.com/ademajagon/gix"
-  url "https://github.com/ademajagon/gix/archive/refs/tags/v0.2.6.tar.gz"
-  sha256 "bc8301ebb6b6b83445d4b8f99ed451c7a68b396bac8bc7059fe5ea5497881535"
+  url "https://github.com/ademajagon/gix/archive/refs/tags/v0.2.7.tar.gz"
+  sha256 "4436cb0f26c608bbf4774210422a66a25b27ea8775260373b6b81a52e97bf94a"
   license "MIT"
 
   bottle do
