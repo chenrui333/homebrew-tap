d164e56160e1126ae59e9c28bd248970dc87b90b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 02:36:56 +0000

    envlens: add 0.1.2 bottle.

--- Formula/e/envlens.rb
+++ Formula/e/envlens.rb
@@ -6,6 +6,15 @@ class Envlens < Formula
   license "MIT"
   head "https://github.com/craigf-svg/envlens.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "14fe3c5e56db4d4989d6e72e45f33734eb64354842c2e2344242349b2ba0456d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "14fe3c5e56db4d4989d6e72e45f33734eb64354842c2e2344242349b2ba0456d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14fe3c5e56db4d4989d6e72e45f33734eb64354842c2e2344242349b2ba0456d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ff8369f66304f5adb097437b284f96aeaa5f2dc77ab3c50bbfe020f9b5c27663"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d44533b22b8781f2976157352e6d865b066faeb3496ac92eaa576014f9d909a2"
+  end
+
   depends_on "go" => :build
 
   def install
