ee2f9d82fca7d5ee5e036ade5ded89138a232763
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 01:48:34 +0000

    codemap: add 4.0.1 bottle.

--- Formula/c/codemap.rb
+++ Formula/c/codemap.rb
@@ -6,6 +6,15 @@ class Codemap < Formula
   license "MIT"
   head "https://github.com/JordanCoin/codemap.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "07a00fadd4b3bf7e66c8b65a77dcf8166117f3c23835838a7b08c7320d466a84"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07a00fadd4b3bf7e66c8b65a77dcf8166117f3c23835838a7b08c7320d466a84"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "07a00fadd4b3bf7e66c8b65a77dcf8166117f3c23835838a7b08c7320d466a84"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5bd8638a9f76a1bdc001c3e742c08d0fd7614ca50a3f97c4bc1b7ba832049668"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "26f533b494034a4a3c242b21ac5782e624704a37fc0f6db5fafd0c95b56e35ca"
+  end
+
   depends_on "go" => :build
 
   def install
