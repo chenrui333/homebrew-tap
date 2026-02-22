73822d14ebc6c0904e5ba26a7cd7239bb0663ebb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 05:45:16 +0000

    chiko: add 0.1.3 bottle.

--- Formula/c/chiko.rb
+++ Formula/c/chiko.rb
@@ -6,6 +6,15 @@ class Chiko < Formula
   license "MIT"
   head "https://github.com/felangga/chiko.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ea2383370eefd2e562688d827ca902f463db2fcdd450541722e0b5cecd2f4c87"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ea2383370eefd2e562688d827ca902f463db2fcdd450541722e0b5cecd2f4c87"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ea2383370eefd2e562688d827ca902f463db2fcdd450541722e0b5cecd2f4c87"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d43bc656d1799480eef4574ef1a1024f18a7ca09d8010ddd7b0f95ecbf02f553"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2631235ade9dac17fae60884adc865ac2ae4fb6590c4ebbc91dec120ad4932d3"
+  end
+
   depends_on "go" => :build
 
   def install
