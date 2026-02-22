b1b27a35f66dc7a2ac8db903ef5f63f399cdd809
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 02:35:37 +0000

    rendy: add 0.0.0 bottle.

--- Formula/r/rendy.rb
+++ Formula/r/rendy.rb
@@ -7,6 +7,15 @@ class Rendy < Formula
   license "MIT"
   head "https://github.com/tokyohardrock/rendy.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6e40d00ecc7a8c545da9bbf53641560aeb9482f746fdcbcd21e82be00880a8f1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6e40d00ecc7a8c545da9bbf53641560aeb9482f746fdcbcd21e82be00880a8f1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6e40d00ecc7a8c545da9bbf53641560aeb9482f746fdcbcd21e82be00880a8f1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d4fc8807b5c4d31f9cd860ea3ce184c11ad6bca81dfdabb490716cfbbd9a8deb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4e79a1c93eb783fb510af844d64ec929b04c44f9eeb17d237b7c8ad8d7555356"
+  end
+
   depends_on "go" => :build
 
   def install
