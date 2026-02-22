128b8d7a3d44ee227187b3c049df4e74e5932c5d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-22 03:02:48 +0000

    dotr: add 1.0.0 bottle.

--- Formula/d/dotr.rb
+++ Formula/d/dotr.rb
@@ -6,6 +6,15 @@ class Dotr < Formula
   license "MIT"
   head "https://github.com/uroybd/DotR.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e9ba0f470c4ac2e2f1eea83db35c8830e30d50f101647ffdb5fde4eb9f41f67f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a7fb4b31774fb6c906e06a7964e4efdcb7e3a88a0924e2193240719ed054b557"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5752c9d251b28255e5ca0ab97360f4f19dad04773d214cab7f6fc60d0e4ff989"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c0bdaf6939593cfc9c523074d0501544b1ebd2b8d2715e2d7fa045e28ed700d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c68f875cf284bb30840911a900e238b5208a1246ddafe7d967a3d7ef5a69d9b2"
+  end
+
   depends_on "rust" => :build
 
   def install
