2cc8a1551389c95dce1f568779a21b17cc4dea93
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-23 14:42:58 +0000

    zeptoclaw 0.5.4
    Closes #4579.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/z/zeptoclaw.rb
+++ Formula/z/zeptoclaw.rb
@@ -1,8 +1,8 @@
 class Zeptoclaw < Formula
   desc "Lightweight personal AI gateway with layered safety controls"
   homepage "https://zeptoclaw.com/"
-  url "https://github.com/qhkm/zeptoclaw/archive/refs/tags/v0.5.3.tar.gz"
-  sha256 "a74a66d3bbc79b05633c85b39146c1d8e79a3713616ca19213f60a3d23f87535"
+  url "https://github.com/qhkm/zeptoclaw/archive/refs/tags/v0.5.4.tar.gz"
+  sha256 "db273080064dc41640fe70213c5cdf931cb58d761c104f66e5fd62b48117d304"
   license "Apache-2.0"
   head "https://github.com/qhkm/zeptoclaw.git", branch: "main"
 
