5ac5c483af1dd33d35d6b58a95a0ad176a136584
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-23 15:27:28 +0000

    zeptoclaw: update 0.5.4 bottle.

--- Formula/z/zeptoclaw.rb
+++ Formula/z/zeptoclaw.rb
@@ -8,11 +8,11 @@ class Zeptoclaw < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2ad8726a171036d69f2d26806804ff9c88731c2c297966c886034a8acf887468"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "165927e6b7e0bafa164d1cf2ed780657d4ab9bfdabd46018817bfccb7b7644c6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d9359ccf5ddc91f47be3d975492998a6ff1e932fc5e18277850e73a9b13f5627"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "821406867d70f13720770a94f3efccf7d0f467a450229c56c8a2a411049d91a9"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7c0493ccd7f44b33e3a7965d9bfad1d2602466724270b6ee5b229b8b8ceb6e11"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "10474e09560d4346f4dfa8827d3e3842e576096e304fde3e6d45ebaf0e99ff10"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f325d69814e19717975830404033e69a7f1c058a81010d709259126bd24031f5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0cd2ded7a4e1819a49eac75b2f3ccada2db1791a32991565df2504c64de03ebb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2397c708c277f8d51f1c06352b7c71152fc95d9b72601d3cfce977b3087df209"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "643472f5aa5d5c935472ef33bfbf70352c97bda7cf06c74337ae0635e0f17b43"
   end
 
   depends_on "rust" => :build
