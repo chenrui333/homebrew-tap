0922cb42d882825c1bab763f79260efb7c2e8dd8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-23 16:40:41 +0000

    mongodb-mcp-server 1.7.0

--- Formula/m/mongodb-mcp-server.rb
+++ Formula/m/mongodb-mcp-server.rb
@@ -1,8 +1,8 @@
 class MongodbMcpServer < Formula
   desc "MCP Server to connect to MongoDB databases and MongoDB Atlas Clusters"
   homepage "https://github.com/mongodb-js/mongodb-mcp-server"
-  url "https://registry.npmjs.org/mongodb-mcp-server/-/mongodb-mcp-server-1.3.1.tgz"
-  sha256 "6b029745275d87088fe5a127725b0536193a678c54d692308718d4d985fb0286"
+  url "https://registry.npmjs.org/mongodb-mcp-server/-/mongodb-mcp-server-1.7.0.tgz"
+  sha256 "0e2e2f0f38f3404eec2b884a83a4edb3517329539873e58287979e739963c029"
   license "Apache-2.0"
 
   bottle do
