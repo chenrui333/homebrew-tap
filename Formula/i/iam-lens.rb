142626130d6f04bebfa5504669ad95389458b1e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-24 00:54:09 +0000

    iam-lens 0.1.88

--- Formula/i/iam-lens.rb
+++ Formula/i/iam-lens.rb
@@ -1,8 +1,8 @@
 class IamLens < Formula
   desc "Google Maps for AWS IAM"
   homepage "https://github.com/cloud-copilot/iam-lens"
-  url "https://registry.npmjs.org/@cloud-copilot/iam-lens/-/iam-lens-0.1.87.tgz"
-  sha256 "64745c8af9452c73911f060fdbab5f02ec8f5ef4118c52d0245dfd9bdbd16fc2"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-lens/-/iam-lens-0.1.88.tgz"
+  sha256 "5cd83286a4a5866114e81072835801ab5d03cccd8a292d0cdefadb3ddaaa13dd"
   license "AGPL-3.0-or-later"
 
   bottle do
