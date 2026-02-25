75d86149fcda4eb77ae94658139db618bbb2e9a1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-25 21:02:00 +0000

    aic: update 2.7.0 bottle.

--- Formula/a/aic.rb
+++ Formula/a/aic.rb
@@ -7,11 +7,11 @@ class Aic < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "31d1aac985f7ef5742af1f917ee446d70f0c8f5a7d14c07bdd21a655c24fc8b7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "31d1aac985f7ef5742af1f917ee446d70f0c8f5a7d14c07bdd21a655c24fc8b7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "31d1aac985f7ef5742af1f917ee446d70f0c8f5a7d14c07bdd21a655c24fc8b7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b57e03d7d0102fadddaaa44f9f3cd6596ca3aef096b1dbe12e4bef21f64d8c1b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ddd35c3e6923dd17a221026e21ebf53ab2a8aa5aa880b62c01f6e2d2fde26162"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5eb51131c079fcfc58eb754bc2a2284aed10dd91eb7b9a7eeb68ec18b865303a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5eb51131c079fcfc58eb754bc2a2284aed10dd91eb7b9a7eeb68ec18b865303a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5eb51131c079fcfc58eb754bc2a2284aed10dd91eb7b9a7eeb68ec18b865303a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ccd8fa4ce61f654c209622a1f7cfb24963a1e2e02473550957ff6fbb2aad7f8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4601b3b66cde1a4e12eceabce1adb698028120859ab18707e8d028736e04bba3"
   end
 
   depends_on "go" => :build
