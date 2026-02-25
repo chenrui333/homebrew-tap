6a3939ac8973560bada651c1b155635cf0b6072e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-25 21:42:03 +0000

    orla: update 1.2.3 bottle.

--- Formula/o/orla.rb
+++ Formula/o/orla.rb
@@ -8,11 +8,11 @@ class Orla < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "35bd70b1d255ce1bb14a71116b49b7dc88d7d6986c8aac23b59fd927de1c626b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "35bd70b1d255ce1bb14a71116b49b7dc88d7d6986c8aac23b59fd927de1c626b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "35bd70b1d255ce1bb14a71116b49b7dc88d7d6986c8aac23b59fd927de1c626b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "052778f3f3960f7659c7f4b53a109a546846e7918774a27e0d7fb65b5e319078"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3be116493889a24f21ff32ddfebb45edcec31e1eeaac59885c52d56fbf9e88e9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8fe3178a31cffa58a2325b1a93d959a8dd19048ace8aa5138eb4e3b8fe79c021"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8fe3178a31cffa58a2325b1a93d959a8dd19048ace8aa5138eb4e3b8fe79c021"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8fe3178a31cffa58a2325b1a93d959a8dd19048ace8aa5138eb4e3b8fe79c021"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a891b7567d3fbb14719bfafb79373c41674b1d5165e5a0168d150e8dd300c2bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2371ae3b1f809a4dd72216aff3630809584e93987a09a8c7be1fa1fc913eab42"
   end
 
   depends_on "go" => :build
