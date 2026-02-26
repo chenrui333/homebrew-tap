cddb1e9646f6d23d8595986247c2a06444faf147
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-26 13:47:52 +0000

    terradozer: add 0.2.1 bottle.

--- Formula/t/terradozer.rb
+++ Formula/t/terradozer.rb
@@ -6,6 +6,15 @@ class Terradozer < Formula
   license "MIT"
   head "https://github.com/chenrui333/terradozer.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d7bc1c3691b26fe7223f6ada64836e64983681ed42c940f16129473d15ddd770"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d7bc1c3691b26fe7223f6ada64836e64983681ed42c940f16129473d15ddd770"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7bc1c3691b26fe7223f6ada64836e64983681ed42c940f16129473d15ddd770"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5eea18a2d70b80eb8f824f6d141fb9ebdb255023cbe8afa57756ecb62b51faca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d5c4bae4669dc2770fe1cdb7a5a1196151d142fbd4b5c3e2f6f39d38fb5da9ba"
+  end
+
   depends_on "go" => :build
 
   def install
