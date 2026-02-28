cc60cd4f387d6dd66f57def2ba386e76df67149b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-02-28 13:09:20 +0000

    beelzebub: update 3.6.5 bottle.

--- Formula/b/beelzebub.rb
+++ Formula/b/beelzebub.rb
@@ -8,11 +8,11 @@ class Beelzebub < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "38f7d2005d0662b82c8e636aad105b997fec6342d3a149604da100d064b7333d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "40f712c386be29ef38ec8f194a4527337ad0dcb8928c7324eacd4d1781fdbbf1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7ea420456a1bd2b9f17d17953d21733700e97f60c6d5bc996cbb7eb1a460eca7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4997a425b82aa020263921f2b61a6c88f7866b1876130240a1f3416771b2c919"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fdfd67bf728234d712d7cbb06a0db745102ae9e41d69de43312ad6bc8c555653"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "36118cce42b2a8711bb248526250b0efe489e68533ea6107d7817825091ffd98"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "686313e0313593154e3b779369d503138b059e440076f9092651a2dbb026e4e5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1890fa15b27324049ab43fe3aa6e7cc821f7f3e2eb02e39ef03d473ae8067244"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "876638b91003ce7c3c44a0e97c930ee52994640a15db58e78dd8f1cdb8365621"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "aae2e0a790c773d3ea880951a289c7a5f265e3608c67f71bf831876ec339404f"
   end
 
   depends_on "go" => :build
