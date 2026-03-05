4b4508cfae0a18912be87fd2499439bcc6cf2d7d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-05 00:57:49 +0000

    vi-mongo 0.1.35

--- Formula/v/vi-mongo.rb
+++ Formula/v/vi-mongo.rb
@@ -1,8 +1,8 @@
 class ViMongo < Formula
   desc "MongoDB TUI designed to simplify data visualization and quick manipulation"
   homepage "https://github.com/kopecmaciej/vi-mongo"
-  url "https://github.com/kopecmaciej/vi-mongo/archive/refs/tags/v0.1.34.tar.gz"
-  sha256 "8c2686b4d3890e90be2bb80326d93d1081ed4078deaae63b8d04be0e5dba1c75"
+  url "https://github.com/kopecmaciej/vi-mongo/archive/refs/tags/v0.1.35.tar.gz"
+  sha256 "a231ce251f25ddef6727f6d057169516f7a310386e42b69e6d13fb4a51b0f7b0"
   license "Apache-2.0"
   head "https://github.com/kopecmaciej/vi-mongo.git", branch: "master"
 
