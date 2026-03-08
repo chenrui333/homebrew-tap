2f7e007acd8e42156f2e9651e2ed4c6339a40923
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 05:37:52 +0000

    tredis: add 1.2.0_1 bottle.

--- Formula/t/tredis.rb
+++ Formula/t/tredis.rb
@@ -6,6 +6,15 @@ class Tredis < Formula
   license "MIT"
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "05bf446f030331b0c7756f952729167ed66ec29066118578ef428a8acbf5caa8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e39cefe1e31fbaa00ade80656c00e3b9c15a6e10c4056e32ef9ad8962c8d5527"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f13c5fc83d25e41220858eb3911a3e6ee87e66c52d303cd45e7d7b7d71c2aded"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "427f717c3519c48904f91a196d5486bacc93f32b2167f1ec1628549178a66ab7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ce66aaa530230664b63b736b7c6c248585264781b252925954906264737f4d64"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
