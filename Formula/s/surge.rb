0bdf59da6ee3de9ed0fe7d63f07f6760c9727fab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 06:50:25 +0000

    surge: add 0.6.10_1 bottle.

--- Formula/s/surge.rb
+++ Formula/s/surge.rb
@@ -7,6 +7,15 @@ class Surge < Formula
   revision 1
   head "https://github.com/surge-downloader/Surge.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7ca2cd544f890cb39ae896d2ad3c28d2ddd033fede567aedd60a2f0cf54b59f5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ca2cd544f890cb39ae896d2ad3c28d2ddd033fede567aedd60a2f0cf54b59f5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7ca2cd544f890cb39ae896d2ad3c28d2ddd033fede567aedd60a2f0cf54b59f5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ec1d86f79a41e71e13b48a2c81d471dd1184375e9667ffa6f0dbd207f0ae4f07"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e763101467458d6de7d155cc4d845d479d2126ea39aa131b1aa800e1507ce571"
+  end
+
   depends_on "go" => :build
 
   def install
