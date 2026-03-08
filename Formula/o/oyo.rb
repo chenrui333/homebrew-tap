d0a1974de77f171bdc199c1469684a6590f2c33e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 03:56:02 +0000

    oyo: add 0.1.24_1 bottle.

--- Formula/o/oyo.rb
+++ Formula/o/oyo.rb
@@ -7,6 +7,15 @@ class Oyo < Formula
   revision 1
   head "https://github.com/ahkohd/oyo.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "00a74f7913efbb889dc5cefe96a8b4c89d4c1637e28495d40ef828a9841f6590"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78572db2028765541f494d004a6a6f48dabacd98aa8401cb00618eccdea4543d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2345a3b03f5e9543a076bf40f253cd9abdd88a40f8a047342342fa33eadc6160"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c79e8b7a9e7c99b5bb859337b69e3d578d8997cebb396e88ffffedb1db1180a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "50dbceb8211d3b5f668b9838f50fa065f51a47b2b28ab1f94893a2cb563e53bf"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "oniguruma"
