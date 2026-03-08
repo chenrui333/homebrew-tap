424543e373d219f11cf8de76799589ddd7a7c2c4
Rui Chen <rui@chenrui.dev>
2026-03-07 20:27:18 -0500

    jiq 3.21.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/j/jiq.rb
@@ -0,0 +1,23 @@
+class Jiq < Formula
+  desc "Interactive JSON query tool with real-time output and AI assistance"
+  homepage "https://github.com/bellicose100xp/jiq"
+  url "https://github.com/bellicose100xp/jiq/archive/refs/tags/v3.21.0.tar.gz"
+  sha256 "fc03c02bacb74573fb9a446d7809c5ae7d2e9ef1cc1f849d24a40683173b23f6"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: ".")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/jiq --version")
+
+    (testpath/"data.json").write("{}\n")
+    empty_path = testpath/"empty"
+    empty_path.mkpath
+    output = shell_output("PATH=#{empty_path} #{bin}/jiq #{testpath}/data.json 2>&1", 1)
+    assert_match "jq binary not found in PATH.", output
+  end
+end
