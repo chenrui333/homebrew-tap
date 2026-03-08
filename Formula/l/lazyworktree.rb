b1823db4f2eb0b07b3e236fe6c573f38b151fd47
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 04:54:42 +0000

    lazyworktree: add 1.41.0_1 bottle.

--- Formula/l/lazyworktree.rb
+++ Formula/l/lazyworktree.rb
@@ -7,6 +7,15 @@ class Lazyworktree < Formula
   revision 1
   head "https://github.com/chmouel/lazyworktree.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d7fcb7cdf0957cc9bbde9ef96ce5b5012c2e987ed529d0c0ddc9f162d7479f66"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d7fcb7cdf0957cc9bbde9ef96ce5b5012c2e987ed529d0c0ddc9f162d7479f66"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7fcb7cdf0957cc9bbde9ef96ce5b5012c2e987ed529d0c0ddc9f162d7479f66"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e97c575fa5426f6ce673ca0702dbfa3b7508d9d1e1d78bb06184d481f0ee9bc3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c2190cd99649437c09ce4f0124f38d7ddd42439b4ab71496d7a1b7a476a1931c"
+  end
+
   depends_on "go" => :build
 
   def install
