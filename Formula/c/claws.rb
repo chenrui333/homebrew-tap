b05f9eb4a2cd7e012d353a6afb8efebc4444c484
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 15:42:01 +0000

    claws: add 0.15.2 bottle.

--- Formula/c/claws.rb
+++ Formula/c/claws.rb
@@ -6,6 +6,15 @@ class Claws < Formula
   license "Apache-2.0"
   head "https://github.com/clawscli/claws.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "033c89a5a649ca719aaff50dfe8fb19074d5dc105529730d17d4a336e4602bdf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "033c89a5a649ca719aaff50dfe8fb19074d5dc105529730d17d4a336e4602bdf"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "033c89a5a649ca719aaff50dfe8fb19074d5dc105529730d17d4a336e4602bdf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cd70d2fbc93f288594235ea376ed77ec960018ba32e3e3822cf96c48efdaa4bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "52d0754510e8afb9549434f2f099c6cd81e0582f9d80e767f81b2ddb0b39d293"
+  end
+
   depends_on "go" => :build
 
   def install
