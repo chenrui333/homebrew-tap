9732a55288db76b9600eb463c9d66315e99ee4be
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 06:13:40 +0000

    climp: add 0.3.1_1 bottle.

--- Formula/c/climp.rb
+++ Formula/c/climp.rb
@@ -6,6 +6,14 @@ class Climp < Formula
   license "Apache-2.0"
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0aeb71c249c21df9aef4865f1ad21744fda67335ea685e47aedc35693997de8b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4f00338c26baa5797abead1726a46846e9c1928504062dc6b1aee7cb096cf944"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c0c142f344299a0d1089f2f7ef6069190ff4e2be96de84ee37f6429132ee915f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed122419b387d6f08293425f0bdef0e50539f87b6c1916f61344ac776feea8a4"
+  end
+
   depends_on "go" => :build
   depends_on "pkgconf" => :build
 
