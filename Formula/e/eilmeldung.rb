2438ebeaf06f17dc578132d79bb4f219afede480
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 06:01:24 +0000

    eilmeldung: add 1.0.0_1 bottle.

--- Formula/e/eilmeldung.rb
+++ Formula/e/eilmeldung.rb
@@ -7,6 +7,15 @@ class Eilmeldung < Formula
   revision 1
   head "https://github.com/christo-auer/eilmeldung.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0ba46fb402575232a75e2de3cdac920c995232afefe4ab57cfddca0579850ba8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e0ddf8abca45730a0c5bf701ccc6bc7416d2db827403278497c48da72765145a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c4eb6b7269b89db0433f4c3d9f215faaa8eee07dfe6f4828375e2bc49ca61a99"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1840a7156d0648cfe9e48f7c1f4d8cc6654e4b84f747a7721d4da4690eb67e80"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e56041ad2099009113c5a462511a5ba1db9a860ea0ced7fffb8949453f2bdb71"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "libxml2"
