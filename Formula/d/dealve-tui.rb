9915284afd718876c0f183b815a242e67770b568
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 07:07:35 +0000

    dealve-tui: add 1.0.2_1 bottle.

--- Formula/d/dealve-tui.rb
+++ Formula/d/dealve-tui.rb
@@ -6,6 +6,15 @@ class DealveTui < Formula
   license any_of: ["MIT", "Apache-2.0"]
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "917519a7fa63532d426624fe4154380fe6abf79187f1617fe5ff0936114db224"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2350e5546000070d6ef25a97bda5bc9c6922820c603aaa98fc3939e2b8a85dfb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5caace23e7966575906929118684b34bd5ac6778387ebe6c5453cfd56125eae4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7eb31523e97a8d89fea0a357a6a597a3f58ec8267d83abb35229a8ebd6226cc8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63175a7ce7094b8f5d9cd525800008ed0253a0b95e8ca2e304642caa135762ad"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
