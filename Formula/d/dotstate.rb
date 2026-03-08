92af807a455c603a80084487c24412138ced5c72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 15:44:57 +0000

    dotstate: add 0.3.1 bottle.

--- Formula/d/dotstate.rb
+++ Formula/d/dotstate.rb
@@ -6,6 +6,15 @@ class Dotstate < Formula
   license "MIT"
   head "https://github.com/serkanyersen/dotstate.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c087a88f6461d5f868ec126e4d4fad6a58a6a9297179b85bd42a9ca00f36d034"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "81d73022c13b097dc93a45d6d4c9c5ca8bf1ec4c37c77a6e2c65d5b495b01991"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1f1b4d745ca3d7a9158fad2b5eda69b5c38f8926ef3a61aea96787fb5919aa18"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1eb84a319bf97ccb81e9bfdc8ecb3956ac147a8605b844e493e43322349fa48e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "00d5233bff1e6f4b7c6665ddd4926846d636636c10479d8212309a239804b3f4"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
