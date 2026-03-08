f3541975248098b2993b8b1a955167844abef37d
Rui Chen <rui@chenrui.dev>
2026-03-07 21:09:41 -0500

    datui 0.2.49 (new formula) (#4876)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/d/datui.rb
@@ -0,0 +1,22 @@
+class Datui < Formula
+  desc "Data exploration in the terminal"
+  homepage "https://derekwisong.github.io/datui/"
+  url "https://github.com/derekwisong/datui/archive/refs/tags/v0.2.49.tar.gz"
+  sha256 "461640173fccaedeb3a140ea15f3beb739b394271a2514bd9aa789a04a44f8c4"
+  license "MIT"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+  depends_on "fontconfig"
+
+  def install
+    system "cargo", "install", "--bin", "datui", *std_cargo_args(path: ".")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/datui --version")
+
+    output = shell_output("HOME=#{testpath} #{bin}/datui --generate-config")
+    assert_match "Configuration file written to:", output
+  end
+end
