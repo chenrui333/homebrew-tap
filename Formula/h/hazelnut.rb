0f475675758b54e0500f12a51624ede7e848b060
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 05:47:50 +0000

    hazelnut: add 0.2.49_1 bottle.

--- Formula/h/hazelnut.rb
+++ Formula/h/hazelnut.rb
@@ -7,6 +7,15 @@ class Hazelnut < Formula
   revision 1
   head "https://github.com/ricardodantas/hazelnut.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bfe57b65d77b5518e0e0aa1f39f6cc39d7a205532879c1280cb338e75ce31983"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45217d5d6f7c700414bd928e7e5e4978b3ca266138b0a2f7a276ab28f8383912"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b5da23818eff4dd006f1e09c0548bdde33d335cbf38695eddae96c31aecf1763"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c3606d2caae9ab4020cea826af580735f5298dbeecc98b766e0dc4209f3fc331"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e0275632e91375fa7852815f4fb153ec1309c3990c195cb38dfc63e882a5b129"
+  end
+
   depends_on "rust" => :build
 
   def install
