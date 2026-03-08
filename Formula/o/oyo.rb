900f918be4ac5d09a088e9de91274408f09cddac
Rui Chen <rui@chenrui.dev>
2026-03-07 21:10:53 -0500

    oyo 0.1.24 (new formula) (#4889)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/o/oyo.rb
@@ -0,0 +1,21 @@
+class Oyo < Formula
+  desc "Step-through diff viewer"
+  homepage "https://github.com/ahkohd/oyo"
+  url "https://github.com/ahkohd/oyo/archive/refs/tags/v0.1.24.tar.gz"
+  sha256 "5283d39438fa71e25a096ade1c755ca80f3eb3f2adbadd7b56936e1e83f3f197"
+  license "MIT"
+  head "https://github.com/ahkohd/oyo.git", branch: "main"
+
+  depends_on "pkgconf" => :build
+  depends_on "rust" => :build
+  depends_on "oniguruma"
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/oyo")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/oy --version")
+    assert_match "github", shell_output("#{bin}/oy themes")
+  end
+end
