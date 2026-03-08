2eaf88ea5726ec9a2f3497382efdf48890d7e32b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 00:10:15 +0000

    tori: add 0.3.1 bottle.

--- Formula/t/tori.rb
+++ Formula/t/tori.rb
@@ -6,6 +6,15 @@ class Tori < Formula
   license "MIT"
   head "https://github.com/thobiasn/tori-cli.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4fcc286ed9ea96baed1c80f1e0b2dc573459c5e7adff9d9904a9679831aa9beb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4fcc286ed9ea96baed1c80f1e0b2dc573459c5e7adff9d9904a9679831aa9beb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4fcc286ed9ea96baed1c80f1e0b2dc573459c5e7adff9d9904a9679831aa9beb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "98313758f78181bb5e40cff12ee01d894046c4a08c4a14ae1c30c492a6a800ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e3500c81d0f0df166b73b8fd6e10e8bb987a4d50d3e0403345e5c44021985936"
+  end
+
   depends_on "go" => :build
 
   def install
