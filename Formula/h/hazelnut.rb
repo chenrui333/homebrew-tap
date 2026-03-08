bb9c89ad105d7e9ceab06027e771f702599af41d
Rui Chen <rui@chenrui.dev>
2026-03-08 00:10:40 -0500

    hazelnut: use bare std_cargo_args

--- Formula/h/hazelnut.rb
+++ Formula/h/hazelnut.rb
@@ -9,7 +9,7 @@ class Hazelnut < Formula
   depends_on "rust" => :build
 
   def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+    system "cargo", "install", *std_cargo_args
   end
 
   test do
