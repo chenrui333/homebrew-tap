d639584250a0ef06dbe7d49f1cec3c568616fb49
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 05:52:44 +0000

    models: add 0.9.7_1 bottle.

--- Formula/m/models.rb
+++ Formula/m/models.rb
@@ -7,6 +7,15 @@ class Models < Formula
   revision 1
   head "https://github.com/arimxyer/models.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1d0e0ee99bffa9919461166aa0bb9364f0c8352ff183068f5d080de264842d6b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ad2b036d4b4e0ed464c7c4722110d1b2ff2ff5c804c0e7f96d7d8ad6c10f4612"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3ddd43e2633f6402c8bc1dd7754d6d9ad899b8398768c522948bd14095c01a52"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "54a0e6a4814810a38f604bc65ef23b43dfaf5bfe383ec42861f86732800967f9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "62aab26e164245659b6d423219ec3bc69a5d69a91c21d0853628f0e1fe47c76a"
+  end
+
   depends_on "rust" => :build
 
   def install
