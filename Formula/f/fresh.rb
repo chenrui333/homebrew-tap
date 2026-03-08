78372f0952f26c354bd5ca08c57eb36c906e8882
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 16:01:46 +0000

    fresh: add 0.2.14 bottle.

--- Formula/f/fresh.rb
+++ Formula/f/fresh.rb
@@ -6,6 +6,15 @@ class Fresh < Formula
   license "GPL-2.0-only"
   head "https://github.com/sinelaw/fresh.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "400e41199bd5ba0678e602342027816f61c0723472976e533536173c5149eb7a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9e8a05e7b033e939faa51b12e40ada6ec48a5325fa9d25b0657f93a0404d62bb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd68ba8e09099012ea1846c78e53879f06ca3d8c9c77fa491380f8e58e6eb2a6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8974a9af2837c05e48d8a9b80c4cec502c8d83a6a521e8c62cd208c6bdceb3be"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63b37bd2decf6b7e9ae7fe8908d8c91322890369504247ab3b26e9c7c5b0f840"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "oniguruma"
