15e81607bd1af74bc7e7f8e5ed58782607aaff87
Rui Chen <rui@chenrui.dev>
2026-03-07 21:39:27 -0500

    oyo: add revision for bottles
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/o/oyo.rb
+++ Formula/o/oyo.rb
@@ -4,6 +4,7 @@ class Oyo < Formula
   url "https://github.com/ahkohd/oyo/archive/refs/tags/v0.1.24.tar.gz"
   sha256 "5283d39438fa71e25a096ade1c755ca80f3eb3f2adbadd7b56936e1e83f3f197"
   license "MIT"
+  revision 1
   head "https://github.com/ahkohd/oyo.git", branch: "main"
 
   depends_on "pkgconf" => :build
