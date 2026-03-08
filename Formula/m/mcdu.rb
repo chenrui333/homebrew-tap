99b9637dace270f511f6818d0e0fe8cff23aecd9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 00:07:18 +0000

    mcdu: add 0.5.0 bottle.

--- Formula/m/mcdu.rb
+++ Formula/m/mcdu.rb
@@ -6,6 +6,15 @@ class Mcdu < Formula
   license "MIT"
   head "https://github.com/mikalv/mcdu.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3d1e4dd05ffd8e5901017fcc262457af04ec9734bbbee74849213829672aa38b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fe7598ba2b9e93e5bcac2355fa717c3d2e462e0faabde205e5c93020babdbbae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4bc9bbe133ea4e1c6b489de5e495e609a56a8702948451ebd2d7d5cd46135d88"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2fc87d6655b0d8d53e487842ad9cc14bd5ee6c644b190a5f639fd73df52a9cb4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "84eecabef1e31f505b5f6b3d9dc9dd4179141887b57bd0b9fe9e1ed2312ea473"
+  end
+
   depends_on "rust" => :build
 
   def install
