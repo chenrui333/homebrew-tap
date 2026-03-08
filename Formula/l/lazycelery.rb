e84f1e17870cafab26aec8c89ccdef4c353e0ac0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 17:12:09 +0000

    lazycelery: update 0.8.3 bottle.

--- Formula/l/lazycelery.rb
+++ Formula/l/lazycelery.rb
@@ -8,11 +8,11 @@ class Lazycelery < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bd2015f625d5ab57ad25b8b9c4bb48b3a138198f0b18aecad985e8806d1b251e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cf23f9e4ad89e26c8940aa07a6a4cfd2ec673af4eaab4dabe4d7b8e9e281956a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0f8c7d221d96c23be9be166a5f5ea2b0da6e6632e32865acf01bdaf91631f6dc"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c465e4f360ab6b6e98b97bbffa5f12dcef818bd7087f012a7ef0fc31e2f2866d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ddb43529c3c76e74198374310649995f39fa0eda0ed48783b7eeec9d41a5784e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c5edd804874a42a0cfff589aaaf2932543f8952a81b423adf4249fbf0b05b80a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b143851d6d13d9df60716a938559f3c940c12bd6f884ce65d85ecd43977f0310"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "197a73f993a3305e056f7dd413661142495bd2d714375d97d02022e5f5eaf9f1"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a01c9cf37c38bee11f8c29ec49cf18fc98869ceb62489b4990e6ef32e5821d20"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "dc413b1dbc4a7247c1c90272a2d62058124451555ae7b9b7a3337a034344e84e"
   end
 
   depends_on "rust" => :build
