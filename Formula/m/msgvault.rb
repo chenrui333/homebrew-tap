04cb1df60636fe34316b96700145273a4fc6b435
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 00:11:16 +0000

    msgvault: add 0.9.0 bottle.

--- Formula/m/msgvault.rb
+++ Formula/m/msgvault.rb
@@ -6,6 +6,15 @@ class Msgvault < Formula
   license "MIT"
   head "https://github.com/wesm/msgvault.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1dc6b68dac921230f098e6f92566eef8bc12b8200d11f0c250b6a099f35cbd91"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "89d5786561fc10db7eb14c3d3163564d26e8ea36f4612789e06fa3537a3adfd2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f1039375ad0d409962bfb4ee9e47c7c3a2ed0be01973c921667c5f2219af1503"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "aab2b086c50f8bdea06f21dfc6fcc63b42689ab89d19a3275bee9e45f6a0a5d3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56c5f173393d43b3d7aad3219209654ad79ed5725d1b8a42647a59dc02ce34b3"
+  end
+
   depends_on "go" => :build
 
   def install
