3d9ec2ba26ea9fdb7d605032bfd9e6cc47ff529f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 05:34:48 +0000

    pam: add 0.3.0-beta_1 bottle.

--- Formula/p/pam.rb
+++ Formula/p/pam.rb
@@ -7,6 +7,15 @@ class Pam < Formula
   revision 1
   head "https://github.com/eduardofuncao/squix.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5371b035cff26bb7f019a16114e4427219f053becd1aad5635fc86249f5dd1c6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2a5bb0421f7671d2ee4897a2f7fb25179a38b4f727151301b81c34d7e8521826"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f209ee20577b45d96acf15a8b1f6df920830491aff4e368eb7b6adf828a18aae"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bffd043d4c244bdba3eb1bb19977f29399b76926ac369ae394fc7d2963afa2bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "69f44376666d2c2f23fea37f7db03a78da660383b7eb3808fea4b385a448c766"
+  end
+
   depends_on "go" => :build
 
   def install
