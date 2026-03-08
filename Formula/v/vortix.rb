895ff521709324bef1b02a8269bb8243e2f0383d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-08 04:44:00 +0000

    vortix: add 0.1.5_1 bottle.

--- Formula/v/vortix.rb
+++ Formula/v/vortix.rb
@@ -7,6 +7,15 @@ class Vortix < Formula
   revision 1
   head "https://github.com/Harry-kp/vortix.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4119fec6400948b762504a00deacb5c07e72b8bb176f122023baf5a68f01fb31"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "79d3579472cc922fd5efdc20f7e97d0ad3359159aef7393b505ad20781e1680e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7e96511b76742c745a44888dc81395088aeee0c735a167701598fe99b905d138"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3aba4f034efc3d4d3a3acfd6f1f20b25b84c29fa66a05a068cd2af01afdd97aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e96bb6c57e0704cb366338783f96f5da326d070e57e4458304d3c898c746260e"
+  end
+
   depends_on "rust" => :build
   depends_on "openvpn"
   depends_on "wireguard-tools"
