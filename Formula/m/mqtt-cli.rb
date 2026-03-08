59bc6c0fe06d7f46151bc8a8e722c559c4bd79c8
Rui Chen <rui@chenrui.dev>
2026-03-08 10:11:26 -0400

    mqtt-cli: avoid network test

--- Formula/m/mqtt-cli.rb
+++ Formula/m/mqtt-cli.rb
@@ -27,29 +27,120 @@ class MqttCli < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/mqtt --version")
 
-    require "timeout"
-    output = ""
-    # spellchecker:ignore-next-line
-    io = IO.popen("#{bin}/mqtt sub -t test/brewtest -h test.mosquitto.org")
-    begin
-      sleep 1
+    require "socket"
 
-      # spellchecker:ignore-next-line
-      system bin/"mqtt", "pub", "--verbose", "-t", "test/brewtest", "-m", "Hello, World!", "-h", "test.mosquitto.org"
+    decode_remaining_length = lambda do |io|
+      multiplier = 1
+      value = 0
+      bytes = []
 
-      Timeout.timeout(5) do
-        while (line = io.gets)
-          output << line
-          break if output.include?("Hello, World!")
-        end
+      loop do
+        byte = io.read(1)&.ord
+        raise "Failed to read MQTT remaining length" if byte.nil?
+
+        bytes << byte
+        value += (byte & 0x7f) * multiplier
+        break if byte.nobits?(0x80)
+
+        multiplier *= 128
       end
-    ensure
-      # Terminate the subscriber process
-      Process.kill("TERM", io.pid)
-      io.close
+
+      [value, bytes]
     end
 
-    assert_match "Hello, World!", output
+    read_packet = lambda do |io|
+      header = io.read(1)
+      raise "Failed to read MQTT packet type" if header.nil?
+
+      remaining_length, remaining_length_bytes = decode_remaining_length.call(io)
+      payload = io.read(remaining_length)
+      raise "Failed to read complete MQTT packet" if payload.nil? || payload.bytesize != remaining_length
+
+      header + remaining_length_bytes.pack("C*") + payload
+    end
+
+    parse_connect_protocol_level = lambda do |packet|
+      index = 1
+      loop do
+        byte = packet.getbyte(index)
+        index += 1
+        break if byte.nobits?(0x80)
+      end
+
+      protocol_name_length = packet.byteslice(index, 2).unpack1("n")
+      index += 2 + protocol_name_length
+      packet.getbyte(index)
+    end
+
+    parse_publish_packet = lambda do |packet, protocol_level|
+      index = 1
+      loop do
+        byte = packet.getbyte(index)
+        index += 1
+        break if byte.nobits?(0x80)
+      end
+
+      topic_length = packet.byteslice(index, 2).unpack1("n")
+      index += 2
+      topic = packet.byteslice(index, topic_length)
+      index += topic_length
+      qos = (packet.getbyte(0) >> 1) & 0x03
+      index += 2 if qos.positive?
+
+      if protocol_level == 5
+        property_length = 0
+        multiplier = 1
+
+        loop do
+          byte = packet.getbyte(index)
+          index += 1
+          property_length += (byte & 0x7f) * multiplier
+          break if byte.nobits?(0x80)
+
+          multiplier *= 128
+        end
+
+        index += property_length
+      end
+
+      payload = packet.byteslice(index, packet.bytesize - index)
+
+      [topic, payload]
+    end
+
+    server = TCPServer.new("127.0.0.1", 0)
+    port = server.addr[1]
+    published_packet = nil
+    protocol_level = nil
+
+    server_thread = Thread.new do
+      client = server.accept
+      connect_packet = read_packet.call(client)
+      protocol_level = parse_connect_protocol_level.call(connect_packet)
+      connack = if protocol_level == 5
+        [0x20, 0x03, 0x00, 0x00, 0x00].pack("C*")
+      else
+        [0x20, 0x02, 0x00, 0x00].pack("C*")
+      end
+      client.write(connack)
+      published_packet = read_packet.call(client)
+    ensure
+      client&.close
+      server.close unless server.closed?
+    end
+
+    message = "hello-from-brew"
+    shell_output("#{bin}/mqtt pub -h 127.0.0.1 -p #{port} -t test/brewtest -m #{message}")
+
+    server_thread.join(15) || raise("Timed out waiting for mqtt-cli to publish")
+    server_thread.value
+
+    refute_nil published_packet
+    assert_equal 3, published_packet.getbyte(0) >> 4
+
+    topic, payload = parse_publish_packet.call(published_packet, protocol_level)
+    assert_equal "test/brewtest", topic
+    assert_equal message, payload
   end
 end
 
