e1567e5c6710d81b25af9f3bcacfd0579e75655e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 14:55:27 +0000

    ccboard: update 0.11.2 bottle.

--- Formula/c/ccboard.rb
+++ Formula/c/ccboard.rb
@@ -8,11 +8,11 @@ class Ccboard < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "13acd32f14c5c247d8e27a8d3dea440fa77aea5dd3dfdc4880cab828088d5761"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6c00537a9a2fa31095828b61b053abc6ecd9c622738e7ef07f29f13fa7b1583e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "31709713ff0f2e06c223b2cd94f8c30da0f1f99e01bbc612e0d66577f1e2766f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3abcdcee57d4b3fb22f3aff26a1805df4fb6ffaaa19eda839da2af1efb865a91"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e52f7ba0201906cbe488e2caa75867f5aff183c8d4db14274c7a7fdf395f870"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1518d511a0b4b4f024087b4bdcb21f6ef41897d5a4831d9af5badb419ed870f7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3cbe349707eeb22b2a8a87a84fb5c48a11e03274a67bbc9d3d848f0c9a849eaa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b5888326251d7328b940b9ffc339f2ccaf094055303bd86fd43a4596c4d444a6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a661619604af7dd80dc0210fd32a39c5dd2e724ac3a0108f80138ee8631d8b2b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0451213abc1b7834ec28b422e31e471c5b48a8baa85003bc6b6e9fbd97cc595d"
   end
 
   depends_on "pkgconf" => :build
