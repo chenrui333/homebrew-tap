736f477c323fcdfcfde68ed6f5b790b9dbdc13ad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 03:29:33 +0000

    sem-cli: add 0.3.7 bottle.

--- Formula/s/sem-cli.rb
+++ Formula/s/sem-cli.rb
@@ -6,6 +6,15 @@ class SemCli < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/Ataraxy-Labs/sem.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d98ff1d3c0b494ae2fd1e579b7eaf449fe5cb3746711755d55238a2cb01c6962"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d8eec2c32cb26721db721814070d46f0074a161833dfd9e01090d121d35b5c7c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "21b7bf35dbd0bf81fce2d3c046c1882e8d9bec02796f51e1aa7a9eda88111736"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "77d5f584cf25d632ace19aa4fc106d9d94cf60f0d5f9ff5cf2f32e16d479329a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f56a4ea6ab2d0429dbe0b6dac54ba8b2c46dc04f4ca83d6da25709e380357baf"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
