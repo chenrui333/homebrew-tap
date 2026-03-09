f787524c23dcadaf127ba869c9e194116dfd482c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 10:56:38 +0000

    sem-cli: update 0.3.8 bottle.

--- Formula/s/sem-cli.rb
+++ Formula/s/sem-cli.rb
@@ -8,11 +8,11 @@ class SemCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d98ff1d3c0b494ae2fd1e579b7eaf449fe5cb3746711755d55238a2cb01c6962"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d8eec2c32cb26721db721814070d46f0074a161833dfd9e01090d121d35b5c7c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "21b7bf35dbd0bf81fce2d3c046c1882e8d9bec02796f51e1aa7a9eda88111736"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "77d5f584cf25d632ace19aa4fc106d9d94cf60f0d5f9ff5cf2f32e16d479329a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f56a4ea6ab2d0429dbe0b6dac54ba8b2c46dc04f4ca83d6da25709e380357baf"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "144e3da5a586b07622aeafbde9a75c706c6f47837186d6c84f449aecd1f9a4da"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c9d16468250dafe9e6e6259a1d045446bba4273abb231be3b2401655d6835ec4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "311b2c2be859939974e4af5ee112d7493bfde47f9852718b1c19758e90a8179e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bded86a5ccbbae4d19a7b7d49f4123fb07f9673f65b99c43d751bb78d0fc2f43"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3a1f2a67c4108bfafe0a34795d13a34e6d2658dc62afa42a44f51a6da0c06d95"
   end
 
   depends_on "pkgconf" => :build
