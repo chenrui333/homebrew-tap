8095e4b3facf58de6c710d0807f61f54eaa105f9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 03:12:59 +0000

    jarl: update 0.4.0 bottle.

--- Formula/j/jarl.rb
+++ Formula/j/jarl.rb
@@ -8,11 +8,12 @@ class Jarl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6c96fb59291aaeb584b3c9abba74d91da6c9daf00c8b4f8c81cedc2c5f8b3586"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e65d20a6816116fecb4fbeabf49b6009ee8d35a23f6485ba71b4666cb475c361"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "376ff4505c97ef7fcb46861a0acb8abe639665ed5cb593b3143b9b31cb356d44"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8e275c19c171ea4b22e5c7d50ada360a33f1fec9cc88c8dd4c4143b57ffe2b7f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5e92c5216e89be5e110844814ee2743a8cb7b9dc560fd410664a34ac211eaf57"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "923d8f158510f528a359acc13a72f19ee361f1b7555dc9d02209d09f933f3bde"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aeb955d30766cae889d708dedd6452ef9344d0def8dbb85a67c447f80be13ef7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb72931d1996d5cdf0ff11a85d5f0a697a8a522377003d12843025f522d0e31c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3369a0b3a0c1e665b053c18e40572d8f420479b97b6e01f0480e64f6b0f72f27"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "98a4cfcd3fdd010118bafcc43b27af8bf24c61bf80caca7f03b449ef1714d954"
   end
 
   depends_on "rust" => :build
