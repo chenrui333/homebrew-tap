4e2c089817e45f6e1f72c04f0a5befc3acf0b4cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 02:14:00 +0000

    motus: add 0.4.0 bottle.

--- Formula/m/motus.rb
+++ Formula/m/motus.rb
@@ -6,6 +6,15 @@ class Motus < Formula
   license "AGPL-3.0-only"
   head "https://github.com/oleiade/motus.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7598c46b813e3705d78fbc6c9b53ed0d97d4ee3519c51e8518a3aa65af26a6c4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "234192f8f44bec41f461c85594d342c69529d10f911b4f0224832d2c8431dc04"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "208ed5ebe18819c514e08f7b8a54d5dd789b64dd2801020a586caf2a99ff35e8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b94fecef2102a730443f6abc46f498fdc11fdaad599b40413db7e5524402e0b4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "646350e598b76a31fea15fdf4b7306b25f71e5053fe45f0d18a746fbd5274e20"
+  end
+
   depends_on "rust" => :build
 
   def install
