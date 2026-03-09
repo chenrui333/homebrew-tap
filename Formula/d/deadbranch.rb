d4d3901d659186a575882bca67fd298ed31d9bfc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 10:47:21 +0000

    deadbranch: update 0.2.0 bottle.

--- Formula/d/deadbranch.rb
+++ Formula/d/deadbranch.rb
@@ -8,11 +8,11 @@ class Deadbranch < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fb89e17f1e08f6e5c55bc6d27bdd2f19589fc851146e2b1caa0defdb29c30ea3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f6717cef533aca4416426bd8f538a3ec9d18f415993905678ba3b52ab64a7394"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8bf63f59c2d5bb629a0d1ce683e791e4999075ad00413424c5b3a1a41c11bcee"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "896d2f3ba9cdd739f200e5f6fd43f874b95ff99b90ebd067b7cbd692e55fc3de"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fbd31dd75e9b237c069a7182798a22db4e7c10ddfd50b47df0626764fc8b56a3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fa00922b103c429ee26a768bd2624972251a2f1d577fa0cb1b1b2f39efa2b62a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d99b7709061bf3acc94f0bb537f40b539c4db1444126b31c489398948fbc6a83"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2123128303a1e9d16748d3e44f4aa317a3121cb3c253c20c7a90c5500bae48da"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4ca179c87fd359b3c59f40489dd20259ab56b76660d22127eabde1a56a0ad850"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "36960ca14642d9d0fe8f2f5d7144fbdbec05a92f7abe9b20f9fb9c371ce230ab"
   end
 
   depends_on "rust" => :build
