06e76be63c5dd71de4029bf5dd3dcb0c74a5da5f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 20:32:04 +0000

    youtube-music-cli 0.0.57

--- Formula/y/youtube-music-cli.rb
+++ Formula/y/youtube-music-cli.rb
@@ -1,8 +1,8 @@
 class YoutubeMusicCli < Formula
   desc "Terminal user interface music player for YouTube Music"
   homepage "https://involvex.github.io/youtube-music-cli/"
-  url "https://github.com/involvex/youtube-music-cli/archive/refs/tags/v0.0.47.tar.gz"
-  sha256 "b000a61677361af3b7b8b155c5b1f104b07700792e6408306086aaf4402ae18d"
+  url "https://github.com/involvex/youtube-music-cli/archive/refs/tags/v0.0.57.tar.gz"
+  sha256 "00f2cdcfbe8b8c248b0bc1a9f5717301f2f85f4fe2c428d1ef934fdd38e48617"
   license "MIT"
   head "https://github.com/involvex/youtube-music-cli.git", branch: "main"
 
