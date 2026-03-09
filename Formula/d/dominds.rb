34a54886da8ee1441cfc2c6b634b60e578bbe8eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-09 10:33:13 +0000

    dominds 1.5.1

--- Formula/d/dominds.rb
+++ Formula/d/dominds.rb
@@ -1,8 +1,8 @@
 class Dominds < Formula
   desc "AI-driven DevOps framework with persistent memory"
   homepage "https://github.com/longrun-ai/dominds"
-  url "https://registry.npmjs.org/dominds/-/dominds-1.4.2.tgz"
-  sha256 "8b9590e816d88707b1fc91b6d3f4d0ec26868cc09ce5cf54a949240e4f242423"
+  url "https://registry.npmjs.org/dominds/-/dominds-1.5.1.tgz"
+  sha256 "4271fa435ebf9b3892cbd42b9146d1802589327dc587784d0560e65db206cd3d"
   license "LGPL-3.0-or-later"
 
   bottle do
