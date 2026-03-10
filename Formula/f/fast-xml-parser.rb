6259b784a93e9b74f529e105c401af9efbb6d442
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 14:34:49 +0000

    fast-xml-parser 5.5.0

--- Formula/f/fast-xml-parser.rb
+++ Formula/f/fast-xml-parser.rb
@@ -1,8 +1,8 @@
 class FastXmlParser < Formula
   desc "Validate XML, Parse XML and Build XML rapidly"
   homepage "https://naturalintelligence.github.io/fast-xml-parser/"
-  url "https://registry.npmjs.org/fast-xml-parser/-/fast-xml-parser-5.4.2.tgz"
-  sha256 "09d788b6db3039f6fcdb6174047424cc65aa39bb38b394e9d071aad49c459af8"
+  url "https://registry.npmjs.org/fast-xml-parser/-/fast-xml-parser-5.5.0.tgz"
+  sha256 "0f0ae31bf27b098e898f04e95bd2bba572369992733ef8d387ccf130242ddd20"
   license "MIT"
 
   bottle do
