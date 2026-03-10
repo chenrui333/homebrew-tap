d9dfc3c5f3c58f9b24987c2e8f2b9457e714cf04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-10 17:11:14 +0000

    xfr: update 0.9.3 bottle.

--- Formula/x/xfr.rb
+++ Formula/x/xfr.rb
@@ -8,11 +8,11 @@ class Xfr < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "74fc5c49f39b43cc7aa59e2e0c8085069b2ab849eef11ece8e4c28289762ea19"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb342f6b26e23c04f3ad5242e80d1d92ea15afaf38c7ff7417825518e6d91441"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a6206fa8704391aaca8754d3ba1dae2f8ea2c60f1dcad60e16ba6ae5613246ba"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "af083af65d80135a7830d63793cfc5419f3062680b52d060f48fc32fc6468185"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bca63adff6a14ed9e3699f7cf6efb967e4dfcbe60b00af4a85d044756cec926a"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c8ecfd4e4cbbe53c2df261c1a19ce4f5eb13a943ebee314415634c42736bbf61"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b5118e1b7416cd501cc573d844ecfee7187a643e2ffc2ec3d30cbe441fa45b3d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6e76d278f5212592dd3fc52565c5c102f211c6a0f3dad3e38627b4d35fe5efdc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9c766231f30ee565c1dda70610dd370f343feabbdcc811f6e77c20534df12cbf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "574f98da210f5b419fad14a0838a0d3b9680e6c5cda92adb8868fddcfaf67168"
   end
 
   depends_on "rust" => :build
