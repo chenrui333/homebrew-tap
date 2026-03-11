b19d18c19638580a65830349db0cb71a7569d1a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 18:59:52 +0000

    vortix: update 0.1.7 bottle.

--- Formula/v/vortix.rb
+++ Formula/v/vortix.rb
@@ -8,11 +8,11 @@ class Vortix < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "569e1a0a13e1e161569316190064d4dd9ef3aa68dfe19e8a8f57362f24c6a1e8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e41e04dcf7913541c902d4db9b2e7e3ff987fe493233b01f838dd2f1fc1d1aea"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8cf8f9d2bc398e02cedaf3f9c5c0e04c4373d34f929062b264d5fe02951698c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "418ccb7fa442dcadf22651f304973f5fd31217f45fbeb14ccc6c26a973429e41"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "39e77b0f471603ebf63f1cfa3611a7b10faff44d13ba0dd8b12790b159e6c429"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "273c4c219a501463fd9f96eda115c183281855e651aaecdcbc8cac001f21ebe3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "615ee83b86c3cfaa9131073adda3ee590f695bd26df8ecfbd783293699a0fd8c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "621713791b7ddef81d3f86279482e50c35605bfefab9490370b7b7fa92adce3a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f94b0393f852a337c1aa937fb8dfc541a3e479a454f675f23014a8a1a6f1e4aa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "18c37b3577810fc7f48d8b3c7489f850c9e88c17267025b22657d698a28b8f94"
   end
 
   depends_on "rust" => :build
