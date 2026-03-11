3fcf1d70a0b4ffcac980cfe1d66d6468ee9c8ae6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-11 20:45:23 +0000

    surge: update 0.7.0 bottle.

--- Formula/s/surge.rb
+++ Formula/s/surge.rb
@@ -8,11 +8,11 @@ class Surge < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7ca2cd544f890cb39ae896d2ad3c28d2ddd033fede567aedd60a2f0cf54b59f5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ca2cd544f890cb39ae896d2ad3c28d2ddd033fede567aedd60a2f0cf54b59f5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7ca2cd544f890cb39ae896d2ad3c28d2ddd033fede567aedd60a2f0cf54b59f5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ec1d86f79a41e71e13b48a2c81d471dd1184375e9667ffa6f0dbd207f0ae4f07"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e763101467458d6de7d155cc4d845d479d2126ea39aa131b1aa800e1507ce571"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "56946be5e69d7b56def07573f0c0a831ef60639862a94ca7d64e5e2138b56266"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "56946be5e69d7b56def07573f0c0a831ef60639862a94ca7d64e5e2138b56266"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "56946be5e69d7b56def07573f0c0a831ef60639862a94ca7d64e5e2138b56266"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c5c81a1b234afbda353989c5676c71b114e7715e2f41f57075f6be061f378d8f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0f38f30182cfc9b5152b3a5f0049c5160b47709dead96679f7231b9f9bb97d3d"
   end
 
   depends_on "go" => :build
