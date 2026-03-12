45c146f49fb518d6c1df2854f4ff997635fa74f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-12 12:43:16 +0000

    go-getter: update 1.8.5 bottle.

--- Formula/g/go-getter.rb
+++ Formula/g/go-getter.rb
@@ -13,11 +13,11 @@ class GoGetter < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7e131d538bffe0bf7cdbac20df240ba0bd38800617ad668dbe921459fc374c9d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7e131d538bffe0bf7cdbac20df240ba0bd38800617ad668dbe921459fc374c9d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7e131d538bffe0bf7cdbac20df240ba0bd38800617ad668dbe921459fc374c9d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cd7aa2f7a3dd665fda399f2e929f60cf2271d1d069ac29a03fdffae183e45cb1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7593799783fc512432e12a5acf5ea2e576a26f0f152bc3c12b09d3c8ea4ff3d8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cc69b75d576ad1f40b11fc429d351b3085c9a2894b80e8dd6c73332230f46e9d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cc69b75d576ad1f40b11fc429d351b3085c9a2894b80e8dd6c73332230f46e9d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cc69b75d576ad1f40b11fc429d351b3085c9a2894b80e8dd6c73332230f46e9d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dc824089b841deecc96e0f1c558597579edebe91d87305f9c958bb4ac1f0f89c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "464dfc82274e6e3aa9f94ceabee154d3c0e689fc8c237c1db4c9c1ce1a7425a6"
   end
 
   depends_on "go" => :build
