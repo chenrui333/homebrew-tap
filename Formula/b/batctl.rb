2848b190be12591effcf81bceb87a16b5203a433
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 04:30:36 +0000

    batctl: add 2026.3.13 bottle.

--- Formula/b/batctl.rb
+++ Formula/b/batctl.rb
@@ -6,6 +6,12 @@ class Batctl < Formula
   license "MIT"
   head "https://github.com/Ooooze/batctl.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "90bf721bc42ba3c08862738309c36cce559f0b0594e8ae651004a200203999cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "deeb511ae35f17d7779150180e274435f5ceea913985399cb5f2d5c267acca3f"
+  end
+
   depends_on "go" => :build
   depends_on :linux
 
