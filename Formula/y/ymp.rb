13391311ddae93047f49db80d2b4c4bdb7e2b71d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 04:11:43 +0000

    ymp: add 0.1.0 bottle.

--- Formula/y/ymp.rb
+++ Formula/y/ymp.rb
@@ -6,6 +6,15 @@ class Ymp < Formula
   license "MIT"
   head "https://github.com/trap251/ymp.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "48d67de63c48ca5667bf619ff657fd5cb6f7f13f9a079e21e5769cacbd57b71b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6f26d7aee3e9d8d7c7b39734de02d5c7bd98adedad534d262a7ad356165137aa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e57578bdd926f84457676d163fd63020e7b2a235a66d663c79a6a5e21af3a2e9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a23cae0a9f8e7d20bd800fd3b2e3ce664d7e06ce985b985b2764bb643e70b6dc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "eee2fe9e55b494a414539840963391167c7785910bd31c9e74631dc2424dd16b"
+  end
+
   depends_on "rust" => :build
   depends_on "mpv"
   depends_on "yt-dlp"
