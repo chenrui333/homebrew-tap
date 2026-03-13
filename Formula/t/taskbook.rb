bc406ada8f950cbef59e624a679a22dce722114b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-13 04:26:47 +0000

    taskbook: add 1.3.2 bottle.

--- Formula/t/taskbook.rb
+++ Formula/t/taskbook.rb
@@ -6,6 +6,15 @@ class Taskbook < Formula
   license "MIT"
   head "https://github.com/taskbook-sh/taskbook.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "93fe0d44336e478239370dde7164b3c9e56a5445f7d7484e1129f91274b4b7e7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "17e89ad275c07427d4fc05eb5ce63822692a91adbea5c77cf0d7b2a8c6d72cfc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c70e8e5b71bacb6d18a06c4bdb2ca5244b9ffda3460894196fb230d2315ac0b5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bb6752580a7162fd7203d2f4ac50f7ba335ab5bec788153c273b71b59f5bc684"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "28eb041ce26cede98e201977b0b97a9027b0004d360d40549aad4a6e74bf6691"
+  end
+
   depends_on "rust" => :build
 
   def install
