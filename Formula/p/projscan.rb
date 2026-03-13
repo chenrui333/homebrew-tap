3c524f6ffcd27e119aa8dd4334a630343ca32899
Rui Chen <rui@chenrui.dev>
2026-03-12 23:47:34 -0400

    projscan 0.1.13 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/p/projscan.rb
@@ -0,0 +1,35 @@
+class Projscan < Formula
+  desc "Instant codebase insights for any repository"
+  homepage "https://github.com/abhiyoheswaran1/projscan"
+  url "https://github.com/abhiyoheswaran1/projscan/archive/refs/tags/v0.1.13.tar.gz"
+  sha256 "7a0726fa357365833bb8f4e1bd19097575a41d2e8c08289829112f7deb6923b5"
+  license "MIT"
+  head "https://github.com/abhiyoheswaran1/projscan.git", branch: "main"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", "--include=dev", *std_npm_args(prefix: false, ignore_scripts: false)
+    system "npm", "run", "build"
+    system "npm", "install", *std_npm_args
+
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/projscan --version")
+
+    (testpath/"package.json").write <<~JSON
+      {
+        "name": "projscan-test",
+        "version": "1.0.0"
+      }
+    JSON
+    (testpath/"src").mkpath
+    (testpath/"src/index.js").write "console.log('hello');\n"
+
+    output = shell_output("cd #{testpath} && #{bin}/projscan doctor --format json")
+    assert_match "\"health\"", output
+    assert_match "\"score\"", output
+  end
+end
