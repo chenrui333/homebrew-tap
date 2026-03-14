702da2c27afd8e5b1e4c548f8203b48306c26c35
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 20:55:44 +0000

    tortuise: add 0.1.1 bottle.

--- Formula/t/tortuise.rb
+++ Formula/t/tortuise.rb
@@ -6,6 +6,15 @@ class Tortuise < Formula
   license "MIT"
   head "https://github.com/buildoak/tortuise.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "733edededc3fd2e22dbf0c2be55ee25348186c1ae54850239e08f8fb8d30014d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0acdaad66fa8782e8bcb5351ab78ca706651be6b36026aa2239ad7c383f35045"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9423b88cfd6b79de2a9fb848406061e7dc71b7a3ee3f1894101b6f3e784bbc32"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "101e327ed9f3358c78dd8d99ace3ea784bba2e2b9bb5a8152170945265017a42"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "845541a99717c926f7717afccf4d32918442122bf5ef55119ce2a483cfd86403"
+  end
+
   depends_on "rust" => :build
 
   def install
