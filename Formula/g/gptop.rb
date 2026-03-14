e1d5eb994883e8886d12093d5656a2a3eb195670
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-14 21:25:24 +0000

    gptop: add 0.2.0 bottle.

--- Formula/g/gptop.rb
+++ Formula/g/gptop.rb
@@ -6,6 +6,15 @@ class Gptop < Formula
   license "GPL-3.0-only"
   head "https://github.com/evilsocket/gptop.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "358c92a5e9f93d680c0eea179470da4e12680d26db9b6948b3d64ced7cd5fe9f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa15a887dc2dc27b40707280689f40c6cfbabb881900cdb8b18b0693dcbf3cfe"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9d9de35b156f44f8727edcdfb5eb1861b221b06ea0a95762bf481c62569a31f8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4befb730e2f406081edd97852c7a7f872fe0fc87ad907961779fda8c7d09e56c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4ef388ed52db87fc8751bb9ea7c09bc659e3470c723347e844ae9bdb4eeb222f"
+  end
+
   depends_on "rust" => :build
 
   def install
