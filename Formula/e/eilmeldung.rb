cf8844f296a639af2adbfbe8f31f0356d2c691b4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-15 17:00:11 +0000

    eilmeldung: update 1.2.0 bottle.

--- Formula/e/eilmeldung.rb
+++ Formula/e/eilmeldung.rb
@@ -8,11 +8,11 @@ class Eilmeldung < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "acb87a8b7327ec0da79c00ed3b50114aa0e4a3867885c57bd80b025404732c35"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "898079ce5035e68a9d0e97f1b278a2dc75c25490b548435e481d0988c660c5c4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9ea4575eef89c7022c241c22c78b998375c4f6c475fa044d4d8a1a0096c90559"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7512447492d219b8a096a785bfb5707c693747ba2ddd425cf6bf196374a058a3"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "841f445a2996a5696a8d52003d7489b43d9d58bc97ec1e9a8aa43c658d328a33"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a670e189f1652bc7a1d022d071f6e9cd0631be1607d778bc54109fe09b4aa21a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff8af5bfb58afb46ba7afda9d7fd0dcab75efcf114d9a3be7ea5e400a68407e9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "80fefd5b9ac87e2b9d3721cacb71fc888e65eef72baae0bacc2135d70eac1b8e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3168b67f1720f10108d8aaa7e0ff9b7a44051d0acc5de87ba95434fd2dfc764a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5846c65002e97112c61080ab1015d6ef087abc613b1f1518ab91ca5fb55a0b6e"
   end
 
   depends_on "pkgconf" => :build
