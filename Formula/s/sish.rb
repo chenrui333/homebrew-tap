7b2d529b3051f6e9700f362c1c6d1fd441d70bca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-16 03:30:15 +0000

    sish 2.22.1
    Closes #5193.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s/sish.rb
+++ Formula/s/sish.rb
@@ -1,8 +1,8 @@
 class Sish < Formula
   desc "HTTP(S)/WS(S)/TCP Tunnels to localhost using only SSH"
   homepage "https://docs.ssi.sh/"
-  url "https://github.com/antoniomika/sish/archive/refs/tags/v2.21.1.tar.gz"
-  sha256 "c69a7542ad4135cf5f408f533e4767e6a6e8e6ab4cdbd97e3dc004f7dec3b045"
+  url "https://github.com/antoniomika/sish/archive/refs/tags/v2.22.1.tar.gz"
+  sha256 "f23def84d89d9ae0fd52dd997aaeb4311c4c36144b2435e68f8543fdc762bc3a"
   license "MIT"
   head "https://github.com/antoniomika/sish.git", branch: "main"
 
