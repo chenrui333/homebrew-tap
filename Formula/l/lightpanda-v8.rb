44b7b5d7c1af5e05825d9fc44b935a4976c2edbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-17 01:12:02 +0000

    lightpanda-v8: add 0.3.3 bottle.

--- Formula/l/lightpanda-v8.rb
+++ Formula/l/lightpanda-v8.rb
@@ -6,6 +6,16 @@ class LightpandaV8 < Formula
   license "MIT"
   head "https://github.com/lightpanda-io/zig-v8-fork.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "861ae77853918b2644498e787248f0d80fe2da9d767d1cf0ff3225fa74121f2b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "645a28c8aab12bf5ede73942b29ce9d6d114fe1bda5eb6474b0e75b5385ec90d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "686a72dcabe98ce3f28e4f20844908aedc898c021312e00ac687f6146dd8072e"
+    sha256 cellar: :any_skip_relocation, sequoia:       "eb995d4f4cfc264dd38b8d344b9524ed4c5a72ce8e4a91a51728d8fabe4781dd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "87d935b8af27832c49e0866a44682c4681c859b3bb600f0ce6bcdc5249067f0f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "896e19c4712f06a1ee1dbc66611c20dbf246f5f79d001b2cefa7ff4553213130"
+  end
+
   if OS.mac? && Hardware::CPU.arm?
     resource "libc_v8" do
       url "https://github.com/lightpanda-io/zig-v8-fork/releases/download/v0.3.3/libc_v8_14.0.365.4_macos_aarch64.a"
