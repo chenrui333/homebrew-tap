a4091a2df854de347056a1b71a963c7f9ee08537
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 16:40:31 +0000

    chatuino 1.2.0
    Closes #5266.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/chatuino.rb
+++ Formula/c/chatuino.rb
@@ -1,8 +1,8 @@
 class Chatuino < Formula
   desc "Feature-rich TUI Twitch chat client"
   homepage "https://chatuino.net"
-  url "https://github.com/julez-dev/chatuino/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "a6a232bd47772514bead2fe14da51db588bfd2c45abc943dd6f46c97ec5dbd38"
+  url "https://github.com/julez-dev/chatuino/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "f2f2ad43978fc6a2da4325eed299b0c80265145fbef2bd94f74deb3396a3bd6d"
   license "MIT"
   head "https://github.com/julez-dev/chatuino.git", branch: "main"
 
