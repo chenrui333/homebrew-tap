df71ccebeef1f89532aca36d721e5e04e674d90d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-18 18:57:30 +0000

    fresh: update 0.2.16 bottle.

--- Formula/f/fresh.rb
+++ Formula/f/fresh.rb
@@ -8,11 +8,11 @@ class Fresh < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "400e41199bd5ba0678e602342027816f61c0723472976e533536173c5149eb7a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9e8a05e7b033e939faa51b12e40ada6ec48a5325fa9d25b0657f93a0404d62bb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd68ba8e09099012ea1846c78e53879f06ca3d8c9c77fa491380f8e58e6eb2a6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8974a9af2837c05e48d8a9b80c4cec502c8d83a6a521e8c62cd208c6bdceb3be"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "63b37bd2decf6b7e9ae7fe8908d8c91322890369504247ab3b26e9c7c5b0f840"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ed9890f6e7a17180507299ff9b4ce76aa15cc81911769f79a2c36b9bc8b8eda0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ee8a2e0d06343a72acf8af9d3d75a4c024806d27fcc7363f8aae50237772ca1e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "04f94087aad9d791fd72b025fead9a75b1650b5735e166e63412702caac79fe8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2973fd92a07507374673a66ded28b5cd4ca48c5d829f93ee29b58e933def065f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5bfbb4dc98303fa1cdd59c6cc230c519abfd140479929f466632d417022f00fa"
   end
 
   depends_on "pkgconf" => :build
