494ad22f78a4dda5a3c2812d79977d9649d73f31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-20 14:35:07 +0000

    gitv 0.4.0
    Closes #5323.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/gitv.rb
+++ Formula/g/gitv.rb
@@ -1,8 +1,8 @@
 class Gitv < Formula
   desc "Terminal-based viewer for GitHub issues"
   homepage "https://github.com/JayanAXHF/gitv"
-  url "https://github.com/JayanAXHF/gitv/archive/refs/tags/gitv-tui-v0.3.4.tar.gz"
-  sha256 "9cdff28b648df73187adb1aec09fb87dbbcce1f590ffee85884b39f48366107f"
+  url "https://github.com/JayanAXHF/gitv/archive/refs/tags/gitv-tui-v0.4.0.tar.gz"
+  sha256 "d9374f0ebaab223d16099ad47a55b9f1510c23bf4c89203f35fa12c21116748c"
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/JayanAXHF/gitv.git", branch: "main"
 
