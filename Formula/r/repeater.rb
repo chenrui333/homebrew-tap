16a5af0205b424570cbd32537db90429f8ae619a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 23:41:05 +0000

    repeater: add 0.1.7 bottle.

--- Formula/r/repeater.rb
+++ Formula/r/repeater.rb
@@ -6,6 +6,15 @@ class Repeater < Formula
   license "Apache-2.0"
   head "https://github.com/shaankhosla/repeater.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "30ef0aa4c672a7a3e02f995c1fb6b82de27b404a9244eaf4049c1a565fd51c29"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2e5be71f526fc7ee653451dba6021b59e3e93e7452a921b8f6381976fbfeaf05"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "025e5d9a4007f61ab589c4e62e1bd410ff439d8e1415d19ed599ab3b381a744f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "47a20627f756b23a53cac9ab2393eb98eb1db7dfb6fcedffed4db52ec6d93aba"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4548e5a05481f15540a66f68574b7bc3a903502edd70edf96f7d933105d138ea"
+  end
+
   depends_on "rust" => :build
 
   def install
