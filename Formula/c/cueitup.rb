c27705ef09bc3da3bc5872abc4a2ccd81c0d4beb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 23:41:14 +0000

    cueitup: add 1.0.0 bottle.

--- Formula/c/cueitup.rb
+++ Formula/c/cueitup.rb
@@ -6,6 +6,15 @@ class Cueitup < Formula
   license "MIT"
   head "https://github.com/dhth/cueitup.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "07ff9028af8985437bc8d07c851b66c6102db44a623f088b0d276aaa36837f4b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07ff9028af8985437bc8d07c851b66c6102db44a623f088b0d276aaa36837f4b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "07ff9028af8985437bc8d07c851b66c6102db44a623f088b0d276aaa36837f4b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "033fb6e50bd3af57d0ba8d85d483000b8dda8bc8e4b0dbdee47dca0b7cd63678"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9765495de94fec047cb16aa33931cf73b4717d16e16d46c11ba693f107a09c8a"
+  end
+
   depends_on "go" => :build
 
   def install
