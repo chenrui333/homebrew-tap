f7b393f83e75374799523f59ba61ac8a550342eb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 20:49:56 +0000

    ctxmv: update 0.5.0 bottle.

--- Formula/c/ctxmv.rb
+++ Formula/c/ctxmv.rb
@@ -8,9 +8,9 @@ class Ctxmv < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8578a3f72e0129336c37c77bf5ceb65b5c4eb7c3d4bd556c1777042c22f1257b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1a68f20f759f60d8ecbbfe5a32b2385c441a34707a507e6eecaf2262a1a7ba39"
-    sha256 cellar: :any_skip_relocation, sequoia:       "be2887e62d4c4289e45e26f95ddf771ca8512ee87a5b402435f6132f2875aa19"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c2c7b0f8fac67a7748449eb1d8391294bbd0741934e50c2d37cc0824d06cf8bc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dd72023f8bf153c2859c0618a7484873da2f45c7502dc206baef5b51e2b1beab"
+    sha256 cellar: :any_skip_relocation, sequoia:       "caf5dff13120acc5edb707bc5d6a8a3f59a0bae284316de185c2a5a1d2552400"
   end
 
   depends_on xcode: ["16.0", :build]
