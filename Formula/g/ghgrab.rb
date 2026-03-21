423e48d5b673cb9e2ea044c9e530dc644b8e42cd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-21 18:47:10 +0000

    ghgrab: add 1.1.0 bottle.

--- Formula/g/ghgrab.rb
+++ Formula/g/ghgrab.rb
@@ -6,6 +6,15 @@ class Ghgrab < Formula
   license "MIT"
   head "https://github.com/abhixdd/ghgrab.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ee0c4673e68ce42e7f1da2599dc0de633d7eb4723c07d49a04eec1f2272b1d83"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "631fb279fa04fa9b67576e4b878f5760a0ca21f2f408a5a059af5acf7781c6aa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "45194eae6f7c7829871b08ca464a00b3066fd484f9894e7d135c7e1f35e8a3a8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8eee6f8e269fee0fb7353e823341cb027b5af6c101c6fac35f291ecb6d5e7213"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "accf81c69a1ea8a7a200f2031d5b2624635fecb2c501a6e235311128d9975cb8"
+  end
+
   depends_on "rust" => :build
 
   def install
