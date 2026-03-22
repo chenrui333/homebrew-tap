509f44fea5a1a3d61890f62d8e5b7a79437ec165
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 00:40:30 +0000

    csvi: add 1.23.1 bottle.

--- Formula/c/csvi.rb
+++ Formula/c/csvi.rb
@@ -6,6 +6,15 @@ class Csvi < Formula
   license "MIT"
   head "https://github.com/hymkor/csvi.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b858d0bfc779e9d0602ab6cb7ab258f009f8244c227de24a0d366c23643785fd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b858d0bfc779e9d0602ab6cb7ab258f009f8244c227de24a0d366c23643785fd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b858d0bfc779e9d0602ab6cb7ab258f009f8244c227de24a0d366c23643785fd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4967fa6289a84b92c5b5066d72091798aa0c594603f1cf87a55ff0ca80db4102"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a1b697aa85ae83bc35c79ef0afe0b61e235a56a0a926077300399813058f2273"
+  end
+
   depends_on "go" => :build
 
   def install
