7246de5d74d5134f2e55a388c0d73468bfcd7c22
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 17:11:43 +0000

    tui-banner: add 0.2.2 bottle.

--- Formula/t/tui-banner.rb
+++ Formula/t/tui-banner.rb
@@ -6,6 +6,15 @@ class TuiBanner < Formula
   license "Apache-2.0"
   head "https://github.com/coolbeevip/tui-banner.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3b656c79961764b58b9675078d818e037eee3a37f7c323a262850f123cc91bbd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2bf706bc603b3a262e929d8685fc223376f4f6b1468356c46df519171a1945ad"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "184591f320aed2548bfe45642a40f8d8129ea645af8ace516b7788d4dcff9289"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a9e5432c4ef268db85922b98c175800c29366f424bfb9eb206bae389c51e90f2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "261d7f78b38f19d6a1fdf997f4a96244636d2a259f5ad8b6d42a8e3476b18fff"
+  end
+
   depends_on "rust" => :build
 
   def install
