e27fff7be550aef76919cb9c84700aca01d03ea0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 18:53:09 +0000

    tui-banner: update 0.2.3 bottle.

--- Formula/t/tui-banner.rb
+++ Formula/t/tui-banner.rb
@@ -8,11 +8,11 @@ class TuiBanner < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3b656c79961764b58b9675078d818e037eee3a37f7c323a262850f123cc91bbd"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2bf706bc603b3a262e929d8685fc223376f4f6b1468356c46df519171a1945ad"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "184591f320aed2548bfe45642a40f8d8129ea645af8ace516b7788d4dcff9289"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a9e5432c4ef268db85922b98c175800c29366f424bfb9eb206bae389c51e90f2"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "261d7f78b38f19d6a1fdf997f4a96244636d2a259f5ad8b6d42a8e3476b18fff"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7708b53ddf635abc9a047cf8b53c97e439c3168a9c5acc13388e7bb100c566c4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "edd5d3afce4d5b69f48bdd01424203920ba5b2b857763d9994638da458af35a2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e4a83a56ee315fabc4d34709d69a71f7f07e0c07f924e21cdedc2e8059288264"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7c4c01a5167d211235fb1bc993f7a96f6b51638a771e247cec8684e983ff7f98"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b193b1304796b1f584a80b4440b02cd203c4a492e6c58c481a01cacf132772bf"
   end
 
   depends_on "rust" => :build
