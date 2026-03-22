d31ea403bbb9a946e1723a01331719bfce06c13f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 03:52:24 +0000

    revanced-cli: update 6.0.0 bottle.

--- Formula/r/revanced-cli.rb
+++ Formula/r/revanced-cli.rb
@@ -7,10 +7,7 @@ class RevancedCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5b9ac4805e3d33efc5b31612ec5334dd4f0707480d71d9db3ea3695d1912ce55"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0f7a67ec3dd3ca7ff4b65eb519619162a28917768671365f03c264e9fd7ba9f6"
-    sha256 cellar: :any_skip_relocation, ventura:       "da6f819f45b115e1cdc5f4124c546aa1d2e15629ec021fad3ed556e952aa2f2b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b6bc3e3453b44c0a055c76f7e6095fe92baa3cf2277d3f7f47a2df6277818ae"
+    sha256 cellar: :any_skip_relocation, all: "10be9e8fdb56ef526c2f66ed4d3f20e6dcf7a1caf8aa26037a01c7e4a1159a3f"
   end
 
   depends_on "openjdk"
