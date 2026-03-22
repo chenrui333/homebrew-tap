799430886ebd03a84bd487cf7ff34712444bb26d
Rui Chen <rui@chenrui.dev>
2026-03-22 01:51:09 -0400

    mqtt-cli 4.49.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mqtt-cli.rb
+++ Formula/m/mqtt-cli.rb
@@ -1,15 +1,10 @@
 class MqttCli < Formula
   desc "CLI for connecting various MQTT clients supporting MQTT 5.0 and 3.1.1"
   homepage "https://hivemq.github.io/mqtt-cli/"
-  url "https://github.com/hivemq/mqtt-cli/archive/refs/tags/v4.48.0.tar.gz"
-  sha256 "5e32a9c0ed5ef775bee98b00c8d73c64b4a599935b62838a9d293e1a795ce814"
+  url "https://github.com/hivemq/mqtt-cli/archive/refs/tags/v4.49.0.tar.gz"
+  sha256 "613a67e2df1588595a58c29e9e993ebc16db1a5b493ab9087d3930bef5a79d2c"
   license "Apache-2.0"
   head "https://github.com/hivemq/mqtt-cli.git", branch: "master"
-
-  bottle do
-    root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "eb10addc1649576e6be84505e4289e670d4031a2eabbeae85e28fb3a296e560e"
-  end
 
   depends_on "openjdk@21"
 
@@ -21,35 +16,130 @@ class MqttCli < Formula
 
     system "./gradlew", "shadowJar", "--no-daemon", "-x", "test"
     libexec.install "build/libs/mqtt-cli-#{version}.jar" => "mqtt-cli.jar"
-    bin.write_jar_script libexec/"mqtt-cli.jar", "mqtt"
+    java = Formula["openjdk@21"].opt_bin/"java"
+    (bin/"mqtt").write <<~SH
+      #!/bin/bash
+      exec "#{java}" -jar "#{libexec}/mqtt-cli.jar" "$@"
+    SH
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/mqtt --version")
 
-    require "timeout"
-    output = ""
-    # spellchecker:ignore-next-line
-    io = IO.popen("#{bin}/mqtt sub -t test/brewtest -h test.mosquitto.org")
-    begin
-      sleep 1
+    require "socket"
 
-      # spellchecker:ignore-next-line
-      system bin/"mqtt", "pub", "--verbose", "-t", "test/brewtest", "-m", "Hello, World!", "-h", "test.mosquitto.org"
+    decode_remaining_length = lambda do |io|
+      multiplier = 1
+      value = 0
+      bytes = []
 
-      Timeout.timeout(5) do
-        while (line = io.gets)
-          output << line
-          break if output.include?("Hello, World!")
-        end
+      loop do
+        byte = io.read(1)&.ord
+        raise "Failed to read MQTT remaining length" if byte.nil?
+
+        bytes << byte
+        value += (byte & 0x7f) * multiplier
+        break if byte.nobits?(0x80)
+
+        multiplier *= 128
       end
-    ensure
-      # Terminate the subscriber process
-      Process.kill("TERM", io.pid)
-      io.close
+
+      [value, bytes]
     end
 
-    assert_match "Hello, World!", output
+    read_packet = lambda do |io|
+      header = io.read(1)
+      raise "Failed to read MQTT packet type" if header.nil?
+
+      remaining_length, remaining_length_bytes = decode_remaining_length.call(io)
+      payload = io.read(remaining_length)
+      raise "Failed to read complete MQTT packet" if payload.nil? || payload.bytesize != remaining_length
+
+      header + remaining_length_bytes.pack("C*") + payload
+    end
+
+    parse_connect_protocol_level = lambda do |packet|
+      index = 1
+      loop do
+        byte = packet.getbyte(index)
+        index += 1
+        break if byte.nobits?(0x80)
+      end
+
+      protocol_name_length = packet.byteslice(index, 2).unpack1("n")
+      index += 2 + protocol_name_length
+      packet.getbyte(index)
+    end
+
+    parse_publish_packet = lambda do |packet, protocol_level|
+      index = 1
+      loop do
+        byte = packet.getbyte(index)
+        index += 1
+        break if byte.nobits?(0x80)
+      end
+
+      topic_length = packet.byteslice(index, 2).unpack1("n")
+      index += 2
+      topic = packet.byteslice(index, topic_length)
+      index += topic_length
+      qos = (packet.getbyte(0) >> 1) & 0x03
+      index += 2 if qos.positive?
+
+      if protocol_level == 5
+        property_length = 0
+        multiplier = 1
+
+        loop do
+          byte = packet.getbyte(index)
+          index += 1
+          property_length += (byte & 0x7f) * multiplier
+          break if byte.nobits?(0x80)
+
+          multiplier *= 128
+        end
+
+        index += property_length
+      end
+
+      payload = packet.byteslice(index, packet.bytesize - index)
+
+      [topic, payload]
+    end
+
+    server = TCPServer.new("127.0.0.1", 0)
+    port = server.addr[1]
+    published_packet = nil
+    protocol_level = nil
+
+    server_thread = Thread.new do
+      client = server.accept
+      connect_packet = read_packet.call(client)
+      protocol_level = parse_connect_protocol_level.call(connect_packet)
+      connack = if protocol_level == 5
+        [0x20, 0x03, 0x00, 0x00, 0x00].pack("C*")
+      else
+        [0x20, 0x02, 0x00, 0x00].pack("C*")
+      end
+      client.write(connack)
+      published_packet = read_packet.call(client)
+    ensure
+      client&.close
+      server.close unless server.closed?
+    end
+
+    message = "hello-from-brew"
+    shell_output("#{bin}/mqtt pub -h 127.0.0.1 -p #{port} -t test/brewtest -m #{message}")
+
+    server_thread.join(15) || raise("Timed out waiting for mqtt-cli to publish")
+    server_thread.value
+
+    refute_nil published_packet
+    assert_equal 3, published_packet.getbyte(0) >> 4
+
+    topic, payload = parse_publish_packet.call(published_packet, protocol_level)
+    assert_equal "test/brewtest", topic
+    assert_equal message, payload
   end
 end
 
