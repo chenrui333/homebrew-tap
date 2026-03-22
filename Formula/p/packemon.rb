db4aadc15ac5e02e0e6f2f954fa6af31aa589dc2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 14:13:17 +0000

    packemon: add 1.8.21 bottle.

--- Formula/p/packemon.rb
+++ Formula/p/packemon.rb
@@ -6,6 +6,15 @@ class Packemon < Formula
   license "BSD-2-Clause"
   head "https://github.com/ddddddO/packemon.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0065efa4c202077bc12b925452e422a6152bfb43fdb000fbed0a2efc0486a9f3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "23c312fa724e0a37dbfcff6e3f73a79f6b65b71053e1298c7a1692d97ce7d4f6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1d50f6de3bc1355f224f8445d67a5abdcc6735db3f6b406a33b6ac793c2ef758"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8ded1ed5d7c3ad64b4b411f05994e83dd55f92675db5007c40e8880934751904"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "943cdf8a42a33549c9e068d77b389d47de72503f0048af5ced1cd353dc77f2d0"
+  end
+
   depends_on "go" => :build
 
   def install
