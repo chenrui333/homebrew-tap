438664dd3438c1d3b78be12869c6b48919838e9a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 05:40:48 +0000

    satview: add 0.1.18 bottle.

--- Formula/s/satview.rb
+++ Formula/s/satview.rb
@@ -6,6 +6,15 @@ class Satview < Formula
   license "Apache-2.0"
   head "https://github.com/ShenMian/tracker.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c8bf3b67939c21a69784de722e1aba0e043ef5570cff8c9d7d392ddf5e94051a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "69480d53790ad8519b5a3363fa662e9d29d816a1f9ccb2ab7e2d5bb8f38b7d35"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d41ede1bad7ee7a7be0295a767898791edae07310dcd4f692815918d4fb59ad7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a3c7e7bed9ac43be75646c03d72ed6aa4948675b2c1815657734be57196bd8b2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9134a7bb28bfb5b515075feeec02f160f52c98f644a88f509faaaaca528b0378"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
