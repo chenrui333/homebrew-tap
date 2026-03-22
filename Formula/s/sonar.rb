3bb46c454c9b0898f2c5adbd10b8a0388e1e5e24
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 20:30:53 +0000

    sonar 0.2.1

--- Formula/s/sonar.rb
+++ Formula/s/sonar.rb
@@ -1,8 +1,8 @@
 class Sonar < Formula
   desc "CLI tool for inspecting and managing localhost ports"
   homepage "https://github.com/raskrebs/sonar"
-  url "https://github.com/raskrebs/sonar/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "9f6f6dc68497196275a1aecd15622837b22319a561989bfdb71ab6e536ba3657"
+  url "https://github.com/raskrebs/sonar/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "52c4e3fe9df9f0dffaba5668f5a93dc5235e900aa2a3aaa2e86e5ad916df8f47"
   license "MIT"
   head "https://github.com/raskrebs/sonar.git", branch: "main"
 
