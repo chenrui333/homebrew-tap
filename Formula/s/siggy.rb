e51b7b2cc2796ebb3d2a05159e09185cd8ff0eb1
Rui Chen <rui@chenrui.dev>
2026-03-22 18:11:20 -0400

    siggy 1.5.1 (new formula)
    Closes #5422.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/s/siggy.rb
@@ -0,0 +1,45 @@
+class Siggy < Formula
+  desc "Terminal-based Signal messenger client with vim keybindings"
+  homepage "https://github.com/johnsideserf/siggy"
+  url "https://github.com/johnsideserf/siggy/archive/refs/tags/v1.5.1.tar.gz"
+  sha256 "84105115bbada8fd4002077330953586fcae77afb4437d5ab121526a88daccdd"
+  license "GPL-3.0-only"
+  head "https://github.com/johnsideserf/siggy.git", branch: "master"
+
+  depends_on "rust" => :build
+  depends_on "signal-cli"
+
+  on_linux do
+    depends_on "pkgconf" => :build
+    depends_on "dbus"
+    depends_on "libxcb"
+    depends_on "libxkbcommon"
+  end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    output = shell_output("#{bin}/siggy --help 2>&1")
+    assert_match "siggy - Terminal Signal client", output
+    assert_match "--demo", output
+
+    log = testpath/"siggy-demo.log"
+    in_r, in_w = IO.pipe
+    script_args = if OS.mac?
+      ["script", "-q", log, bin/"siggy", "--demo"]
+    else
+      ["script", "-q", "-c", "#{bin}/siggy --demo", log]
+    end
+
+    pid = spawn({ "TERM" => "xterm-256color" }, *script_args, in: in_r, out: File::NULL, err: File::NULL)
+    in_r.close
+    sleep 2
+    in_w.write("\u0003")
+    in_w.close
+    Process.wait(pid)
+
+    assert_match "siggy (4)", log.read
+  end
+end
