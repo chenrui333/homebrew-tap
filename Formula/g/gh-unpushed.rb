0d33f3ec9574df338a37297349e25b365113d35e
Rui Chen <rui@chenrui.dev>
2026-03-21 20:46:55 -0400

    gh-unpushed 0.1.1 (new formula)
    Closes #5371.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/g/gh-unpushed.rb
@@ -0,0 +1,37 @@
+class GhUnpushed < Formula
+  desc "GitHub CLI extension that shows your unpushed Git commits"
+  homepage "https://github.com/achoreim/gh-unpushed"
+  url "https://github.com/achoreim/gh-unpushed/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "adbb1ad2a7650a22e1e0bf6f7bc07a1fa6c5ec0a64bfe5436e8be843f68c3b11"
+  license "MIT"
+  head "https://github.com/achoreim/gh-unpushed.git", branch: "main"
+
+  depends_on "gh"
+
+  def install
+    libexec.install "gh-unpushed", "VERSION"
+    bin.write_exec_script libexec/"gh-unpushed"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin/"gh-unpushed"} --version")
+
+    system "git", "init", "-b", "main"
+    system "git", "config", "user.name", "Homebrew"
+    system "git", "config", "user.email", "brew@example.com"
+    (testpath/"README.md").write("first\n")
+    system "git", "add", "README.md"
+    system "git", "commit", "-m", "first"
+
+    system "git", "init", "--bare", testpath/"remote.git"
+    system "git", "remote", "add", "origin", testpath/"remote.git"
+    system "git", "push", "-u", "origin", "main"
+
+    (testpath/"README.md").append_lines("second")
+    system "git", "commit", "-am", "second"
+
+    output = shell_output(bin/"gh-unpushed")
+    assert_match "Unpushed commits on 'main'", output
+    assert_match "second", output
+  end
+end
