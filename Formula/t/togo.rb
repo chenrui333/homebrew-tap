63c28c3555f32f9fb8f369d79123fd4ca7bf3dbb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 01:00:48 +0000

    togo: add 1.1.0 bottle.

--- Formula/t/togo.rb
+++ Formula/t/togo.rb
@@ -7,6 +7,15 @@ class Togo < Formula
   license "MIT"
   head "https://github.com/prime-run/togo.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "96934431cbe074cd7397b6adbeb09c0e1b6c0ad2d3680c311de58fcc62f0e748"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96934431cbe074cd7397b6adbeb09c0e1b6c0ad2d3680c311de58fcc62f0e748"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "96934431cbe074cd7397b6adbeb09c0e1b6c0ad2d3680c311de58fcc62f0e748"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c116fe1067f96ac31c9e70e8b1ec5a415ddc6b74743bc32dd1d5e90fa8088108"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "bbae20d9130231b8dc057336e81a1c70ba0bd1f8d29e1a5e90c61ea678d9ecd2"
+  end
+
   depends_on "go" => :build
 
   def install
