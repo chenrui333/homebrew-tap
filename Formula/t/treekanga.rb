71fbb88689de187f803e04bca44d314c553e274c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-22 00:29:17 +0000

    treekanga: add 1.1.3 bottle.

--- Formula/t/treekanga.rb
+++ Formula/t/treekanga.rb
@@ -6,6 +6,16 @@ class Treekanga < Formula
   license :cannot_represent
   head "https://github.com/garrettkrohn/treekanga.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "113f89d1e4d4e2a0578beced9fbaae8db94728b134c0d137192fa40ead6f9b5c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "113f89d1e4d4e2a0578beced9fbaae8db94728b134c0d137192fa40ead6f9b5c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "113f89d1e4d4e2a0578beced9fbaae8db94728b134c0d137192fa40ead6f9b5c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7f229856e28ec3df0fcb84f081da9d5b54539c2daac50680e074ff3a964c5d01"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "708d0fb55d15a0d0de5776d90cd445bec1dcfde92f9341edd9b5cf38c95e8d17"
+  end
+
   depends_on "go" => :build
 
   def install
