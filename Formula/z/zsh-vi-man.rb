a00724653f7098d709ef4906686ebbba5d38a375
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 00:40:47 +0000

    zsh-vi-man: add 0.1.0 bottle.

--- Formula/z/zsh-vi-man.rb
+++ Formula/z/zsh-vi-man.rb
@@ -6,6 +6,16 @@ class ZshViMan < Formula
   license "MIT"
   head "https://github.com/TunaCuma/zsh-vi-man.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "04a3696e9d35853a9dbfb5f5bd8823eb9e8525a66044aa56c22311de91e86755"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "04a3696e9d35853a9dbfb5f5bd8823eb9e8525a66044aa56c22311de91e86755"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "04a3696e9d35853a9dbfb5f5bd8823eb9e8525a66044aa56c22311de91e86755"
+    sha256 cellar: :any_skip_relocation, sequoia:       "04a3696e9d35853a9dbfb5f5bd8823eb9e8525a66044aa56c22311de91e86755"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "79cdaeafd91b0221355ba10b14878da7552fa6c723de7e1940bf124233dbda41"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "79cdaeafd91b0221355ba10b14878da7552fa6c723de7e1940bf124233dbda41"
+  end
+
   def install
     pkgshare.install "zsh-vi-man.plugin.zsh", "zsh-vi-man.zsh", "lib"
 
