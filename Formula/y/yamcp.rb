ae5353704ed3f7d2a88f6638a286f32e6efdbe68
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 01:07:35 +0000

    yamcp: add 0.2.3 bottle.

--- Formula/y/yamcp.rb
+++ Formula/y/yamcp.rb
@@ -5,6 +5,15 @@ class Yamcp < Formula
   sha256 "ed23201e068cd001dc49a837d881a44e61b4f5527dd74b735ba2ebb6c2db662d"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d7b86a46d0250d9e14bda023910cf3f69e8e4f88bb7eec4dc9968cd1e36168e9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "39e2cd06cd95a0bb5a01f4f740ef286da0f41518805b9c045812a0bdd53963c3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3d12aaeb306743bbf62f732e651b20dfc78f8fcd41d950e48996fa23b531df3b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "17e75405310a61b0adb2f2b7659708a7b226f1f460af0c7460164a5a51255446"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0b12c445c02aff67924c3df1df979d79dedcd0681aa5d85c4232302581db030b"
+  end
+
   depends_on "node@24"
 
   def install
