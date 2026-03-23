b4bb18ce08af0042c60bd1de6149612144a32eec
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-23 00:54:22 +0000

    httpreplay: add 0.6.3 bottle.

--- Formula/h/httpreplay.rb
+++ Formula/h/httpreplay.rb
@@ -4,6 +4,15 @@ class Httpreplay < Formula
   url "https://github.com/roy2220/httpreplay/archive/refs/tags/v0.6.3.tar.gz"
   sha256 "6630a42cd3d30465970bedb5b4501ea7f60b30df36791eb72d84f1dedafe3b71"
   license "MIT"
+
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2963065ccc8dc88f63bed0c96d92df4fe50f85d19799b0425f0801ad5871d2e6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2963065ccc8dc88f63bed0c96d92df4fe50f85d19799b0425f0801ad5871d2e6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2963065ccc8dc88f63bed0c96d92df4fe50f85d19799b0425f0801ad5871d2e6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "277167907278e510583356c253745c5054e4e65f176476dce3060de831911ccd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "295d7e3a6b9522791e8e297a4aeaa6f9448c5bb5c6fb48a354aae5319464b2a4"
+  end
   depends_on "go" => :build
 
   def install
