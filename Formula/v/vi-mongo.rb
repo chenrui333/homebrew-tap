9548ac7bcfe92c83fd8d452d8794cff8ed5cdb09
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-24 18:46:27 +0000

    vi-mongo 0.2.1
    Closes #5500.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/v/vi-mongo.rb
+++ Formula/v/vi-mongo.rb
@@ -1,8 +1,8 @@
 class ViMongo < Formula
   desc "MongoDB TUI designed to simplify data visualization and quick manipulation"
   homepage "https://github.com/kopecmaciej/vi-mongo"
-  url "https://github.com/kopecmaciej/vi-mongo/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "298b6553a576716086def2828588552cfd11c4ca783b53b5c35659c5e73fb06c"
+  url "https://github.com/kopecmaciej/vi-mongo/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "f22fb09b6f5d8439663142dcc02039b788c59aa9ffdeb32a96c2e2dddc360ffe"
   license "Apache-2.0"
   head "https://github.com/kopecmaciej/vi-mongo.git", branch: "master"
 
