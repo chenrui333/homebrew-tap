9dccc471cd3de41fdbbdb9c4dae47d2a966b5eaf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-25 01:10:51 +0000

    msgvault: update 0.11.0 bottle.

--- Formula/m/msgvault.rb
+++ Formula/m/msgvault.rb
@@ -8,11 +8,11 @@ class Msgvault < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6f9f1b3da9ddfccbf19259c2907ef17709e6a6f2f6beed50b919df95bee96c12"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "baff3340225c67261f87cbbaf320d53b7c8f6b7948ef4868131d1e624c8cb7f1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3637cf040771f096fa8203aa62e9a5cfe03b6c0458606b6368288c86b3763a0d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ca473ffa4738480c307a31fd73e7cafd5e3e6ef99d0ee5e2c6c9442b7d7f5e00"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8afc238f94e359e6a0a7b4af6c4938ef494486038037a0851a283228d7359332"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0e3075b401cc4b4a9849a8173f28433dcfe0f3d40eb0f118635e851f380e30c2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6baa61fbe74da99853a7e7015e449f917f9beb9c13030b1b76c7e8e88a270359"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "89f326bb7266a16223627be7d633b66a842b68e7f2a357bd14b95c22f4f5f8a3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bb45bec4cf1a25cb4a46169d0d43928f782a39833430a8e961d15a5f52221c37"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "75d6f8ed9bf977f1b1b6368f7469bf7ef1cf5bdd5299444e13442b25e40c4830"
   end
 
   depends_on "go" => :build
