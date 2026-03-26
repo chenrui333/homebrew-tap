370eab4ad87f4aa1a207cf26bd54a806a1630d5a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-26 16:58:05 +0000

    ghgrab: update 1.2.0 bottle.

--- Formula/g/ghgrab.rb
+++ Formula/g/ghgrab.rb
@@ -8,11 +8,11 @@ class Ghgrab < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ee0c4673e68ce42e7f1da2599dc0de633d7eb4723c07d49a04eec1f2272b1d83"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "631fb279fa04fa9b67576e4b878f5760a0ca21f2f408a5a059af5acf7781c6aa"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "45194eae6f7c7829871b08ca464a00b3066fd484f9894e7d135c7e1f35e8a3a8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8eee6f8e269fee0fb7353e823341cb027b5af6c101c6fac35f291ecb6d5e7213"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "accf81c69a1ea8a7a200f2031d5b2624635fecb2c501a6e235311128d9975cb8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "47cb48a9b8934d7988045bd427b646de08acf33fd43be23d7d4d570ffb3e1e34"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b71f9192d7b104cbfdf04a2d3dce20d186101e62e1f407c6bfbfec52f87fca71"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ab4c36d0abd84ae6795f3cbcf8180e6a06787ef4f7e131ce70a2279e2a119670"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c982b7bc60f0156de91a00b65035464f72961180601de31de3fcf68a32d4ef5b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78fe41c3cbae466a5139f1307af7c02c6edc3d4a320890ad9d6606eb2031d622"
   end
 
   depends_on "rust" => :build
