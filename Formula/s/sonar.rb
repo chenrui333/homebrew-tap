928db0c5af2980674c339abdecf42e55f87fbbf5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-27 10:58:34 +0000

    sonar: update 0.2.6 bottle.

--- Formula/s/sonar.rb
+++ Formula/s/sonar.rb
@@ -8,11 +8,11 @@ class Sonar < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4284239fcb0ee7c1fa95122ff1bb43e8554e4fbc433a463432bbd1fe35c921da"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4284239fcb0ee7c1fa95122ff1bb43e8554e4fbc433a463432bbd1fe35c921da"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4284239fcb0ee7c1fa95122ff1bb43e8554e4fbc433a463432bbd1fe35c921da"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b8b667ac365f77591a9e06ae73bdc8abe3a5fe611d56d28cb11a0d102be6216d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a047b5552c739df42101d483deabe7ecc921970594f59576d114bbfbe68f73c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5d669514be8262f5f6c35d1fbbaf5054116213f9a73ecc433d628628e54f766b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5d669514be8262f5f6c35d1fbbaf5054116213f9a73ecc433d628628e54f766b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5d669514be8262f5f6c35d1fbbaf5054116213f9a73ecc433d628628e54f766b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c507eaec939451a3e83e70db330029e6296bc29b8ee156e7841a0ac0fc5097f8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e617e25c6366c9bfb29870e0035a6c3a7f3c8184c54ce066eb59fbd0aed5902b"
   end
 
   depends_on "go" => :build
