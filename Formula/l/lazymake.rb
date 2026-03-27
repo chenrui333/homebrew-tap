11c2c81601f64a1a04f5b0173e0b83cc91a5008f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-27 08:51:15 +0000

    lazymake: update 0.4.1 bottle.

--- Formula/l/lazymake.rb
+++ Formula/l/lazymake.rb
@@ -8,11 +8,11 @@ class Lazymake < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2b8158b6ae7363292cff2d20390d6a984d1a1d6caa9d83478eb22be2b669828f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b8158b6ae7363292cff2d20390d6a984d1a1d6caa9d83478eb22be2b669828f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2b8158b6ae7363292cff2d20390d6a984d1a1d6caa9d83478eb22be2b669828f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "cf0eac6a153925c07fb1de458b5ec5e82b048130304384f3d8072ce5504cdedc"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a348e2af6560e727683cd6ea0e03db17461eec39929125da5e2c1979f6004cc1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2961317d772740cb5880be209571336077671aec95dde6be4783624ccfd86917"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2961317d772740cb5880be209571336077671aec95dde6be4783624ccfd86917"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2961317d772740cb5880be209571336077671aec95dde6be4783624ccfd86917"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "75c0703c9c79a026ac11dd2c62452341e37a4dc97a2dd76183d56c783dcae553"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fbc998d1ee856f1ed39f91fd84352d74e4894850ab3ba3c7b789025002e904e9"
   end
 
   depends_on "go" => :build
