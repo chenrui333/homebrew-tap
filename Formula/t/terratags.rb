f6b3c83de624e9f5adb07c4e08ff08e9af2bc322
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-29 18:47:51 +0000

    terratags: update 0.8.3 bottle.

--- Formula/t/terratags.rb
+++ Formula/t/terratags.rb
@@ -7,11 +7,11 @@ class Terratags < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7ab96f12583100e2a233e9d5c57550dc89f40bcf15cf14a8f3f9be681713f55d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ab96f12583100e2a233e9d5c57550dc89f40bcf15cf14a8f3f9be681713f55d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7ab96f12583100e2a233e9d5c57550dc89f40bcf15cf14a8f3f9be681713f55d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "28c37d5b2fce06e7e016fd8f7fb79447e3e0a0a9df51a1cd41e8bddee0857c8e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e5b345c6fa82616871ce5977c9d68481b7bbeb8c5afc696506c78461bc86e261"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f4c4fcecf9b65231888a7f723035df27b445648912d8aa05d28ffac9a05e53e9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f4c4fcecf9b65231888a7f723035df27b445648912d8aa05d28ffac9a05e53e9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f4c4fcecf9b65231888a7f723035df27b445648912d8aa05d28ffac9a05e53e9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1392b03631435216448470876535b07e84ef768d1da2af366d5890d8d69a8a21"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2183c08663f11dcc2bfdac25f46f7727e34ac03e39cd7bd7a75098be1cbc9a99"
   end
 
   depends_on "go" => :build
