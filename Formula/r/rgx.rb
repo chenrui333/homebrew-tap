ae5caa5f4f311a894e80fd8601db5068cc463d72
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-30 03:48:03 +0000

    rgx: add 0.8.1 bottle.

--- Formula/r/rgx.rb
+++ Formula/r/rgx.rb
@@ -6,6 +6,15 @@ class Rgx < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/brevity1swos/rgx.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ea345172bb1da0ae3702d568ed51a4c7a49e626609324486e2dc4a914975fb30"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c69957455d63d10c5426dfc59a2352dee3055713e3474ebc80e8eb0ff5464281"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3def2d41c3995b0bd0020d86946120f62b4e653b7e494671cf89be1b2316acd3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "da69c9f32210727c80c413f497943d9367386cc457f68b5658ab8057af13c35f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "faa33e1ee537854c71b0891c99e3413a8d4a45cc5f62e858f10c3a04f1c3a306"
+  end
+
   depends_on "rust" => :build
 
   def install
