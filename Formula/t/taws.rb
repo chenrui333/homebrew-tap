c2a3c138d1cd86c2c9aaed54a9fa5e915c91bf41
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-30 01:50:10 +0000

    taws: add 1.3.0-rc.7 bottle.

--- Formula/t/taws.rb
+++ Formula/t/taws.rb
@@ -5,6 +5,15 @@ class Taws < Formula
   sha256 "c6bd15c5541a4b6a4accb780128642f0cca78c43c741adfbade48062a8f96b51"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "be3e4f1a9d22b9713603282d7d057f4ec9a4b3e80185ecb0e3e0b50ab726c051"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1b9007db6dbf2c4266db254cf3382a42e8e63842efa018f5dd0ff95473cd9c09"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c7b098bed9af832921e45a96f64fa65e8713d9735c0f380e0b1a9de77264aa80"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8f33aab69b78f18a6b8973edad39550f2eeb3f19f5b3d133661c08f93b198c53"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "860c8b0e5d5b11c61977a9abb43e0c8baeedbb0786c67ed0e6860979693cc2b9"
+  end
+
   depends_on "rust" => :build
 
   def install
