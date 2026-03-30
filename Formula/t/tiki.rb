bb8af9144193771d8c28774026b2376ef5e03183
Rui Chen <rui@chenrui.dev>
2026-03-29 21:22:09 -0400

    tiki 0.3.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/tiki.rb
@@ -0,0 +1,35 @@
+class Tiki < Formula
+  desc "Markdown-based git-versioned documentation and issue management"
+  homepage "https://github.com/boolean-maybe/tiki"
+  url "https://github.com/boolean-maybe/tiki/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "f4542065925a331b91cd6758b3fc98cfc87048a5d08b8c0092c2832e41fe730a"
+  license "Apache-2.0"
+
+  depends_on "go" => :build
+  depends_on "git"
+
+  def install
+    ldflags = %W[
+      -s
+      -w
+      -X github.com/boolean-maybe/tiki/config.Version=#{version}
+      -X github.com/boolean-maybe/tiki/config.GitCommit=Homebrew
+      -X github.com/boolean-maybe/tiki/config.BuildDate=unknown
+    ]
+
+    system "go", "build", *std_go_args(ldflags:), "."
+  end
+
+  test do
+    output = shell_output("#{bin}/tiki sysinfo")
+    assert_match "System Information", output
+    assert_match "OS:", output
+    assert_match "Project Root:", output
+
+    assert_match version.to_s, shell_output("#{bin}/tiki --version")
+
+    system "git", "init"
+    usage = shell_output("#{bin}/tiki")
+    assert_match "Run 'tiki init' to initialize this repository.", usage
+  end
+end
