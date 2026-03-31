da345a0309ad5dca7066bcca08b4e513d4168ee7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-31 10:59:43 +0000

    mqtt-cli: update 4.50.0 bottle.

--- Formula/m/mqtt-cli.rb
+++ Formula/m/mqtt-cli.rb
@@ -8,11 +8,11 @@ class MqttCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "75a2ee74ca92935ff224aa0b2f0ee75e957a82535b87bd445cd3b707f76ac9ef"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "75a2ee74ca92935ff224aa0b2f0ee75e957a82535b87bd445cd3b707f76ac9ef"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "75a2ee74ca92935ff224aa0b2f0ee75e957a82535b87bd445cd3b707f76ac9ef"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ae1bbe5e130ad545ddd7ea79c9d119a6a3dfab5d70fecf4c94272eb9136a4f00"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ae1bbe5e130ad545ddd7ea79c9d119a6a3dfab5d70fecf4c94272eb9136a4f00"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "78086e80a4697244f2f5bd23f4df3f383c65745dced375ca872e3f15d4683a4f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "78086e80a4697244f2f5bd23f4df3f383c65745dced375ca872e3f15d4683a4f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "78086e80a4697244f2f5bd23f4df3f383c65745dced375ca872e3f15d4683a4f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ca83f6db27065c2c5f145f242829193f7bdd5a512a5f22ed630db4919b5cb965"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ca83f6db27065c2c5f145f242829193f7bdd5a512a5f22ed630db4919b5cb965"
   end
 
   depends_on "openjdk@21"
