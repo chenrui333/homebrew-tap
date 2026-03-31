33fb3458b08049bd3b74d468006e399893250884
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-03-31 08:40:21 +0000

    cliamp 1.31.6
    Closes #5642.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/cliamp.rb
+++ Formula/c/cliamp.rb
@@ -1,8 +1,8 @@
 class Cliamp < Formula
   desc "Retro terminal music player inspired by Winamp"
   homepage "https://www.cliamp.stream"
-  url "https://github.com/bjarneo/cliamp/archive/refs/tags/v1.31.5.tar.gz"
-  sha256 "196cd573a67613335c180bbcbec7f320642d347048097e7004313921cd19d7e8"
+  url "https://github.com/bjarneo/cliamp/archive/refs/tags/v1.31.6.tar.gz"
+  sha256 "2b214ca358a62c11f54e89e0c3e220d89060de4d3039622d9393eee5e75caf81"
   license "MIT"
   head "https://github.com/bjarneo/cliamp.git", branch: "main"
 
