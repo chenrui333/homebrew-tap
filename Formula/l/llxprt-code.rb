d2a5fa82556c3c0b4e9a917d4ffcb4c2c42596ca
Rui Chen <rui@chenrui.dev>
2026-04-01 09:21:08 -0400

    llxprt-code 0.9.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/l/llxprt-code.rb
@@ -0,0 +1,31 @@
+class LlxprtCode < Formula
+  desc "Open-source multi-provider AI assisted CLI development tool"
+  homepage "https://github.com/vybestack/llxprt-code"
+  url "https://registry.npmjs.org/@vybestack/llxprt-code/-/llxprt-code-0.9.3.tgz"
+  sha256 "fd673a16c4f9706936d8f181a7ec83347d764830237f3c842ae7e21f98bc271b"
+  license "Apache-2.0"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+
+    # Remove prebuilds for non-native architectures and problematic .so files
+    nm = libexec/"lib/node_modules/@vybestack/llxprt-code/node_modules"
+    if Hardware::CPU.arm?
+      nm.glob("**/prebuilds/darwin-x64").each(&:rmtree)
+      nm.glob("**/prebuilds/linux-x64").each(&:rmtree)
+    else
+      nm.glob("**/prebuilds/darwin-arm64").each(&:rmtree)
+      nm.glob("**/prebuilds/prebuild-macOS-ARM64").each(&:rmtree)
+      nm.glob("**/prebuilds/linux-arm64").each(&:rmtree)
+    end
+    # Remove @ast-grep prebuilds that fail dylib ID rewriting
+    nm.glob("**/@ast-grep/*/prebuilds").each(&:rmtree)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/llxprt --version")
+  end
+end
