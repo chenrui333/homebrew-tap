f02c4d0c1e5eab9719c40ea2b95ab724202ff4d9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-01 15:25:54 +0000

    squads-cli: add 0.2.2 bottle.

--- Formula/s/squads-cli.rb
+++ Formula/s/squads-cli.rb
@@ -5,6 +5,11 @@ class SquadsCli < Formula
   sha256 "a0265064e888ec09c29747e2979eb9f91c14ab7db2248c67d2934aa2106024b2"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, all: "c3575c5284265db23c54af4b1e9687de8f399668979bd01a3cca6b506779cb8a"
+  end
+
   depends_on "node"
 
   def install
