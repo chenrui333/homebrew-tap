d8ecca6278fa8cae35085cbe8c244022c2a5383c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-01 15:26:09 +0000

    wolfpack: add 1.6.0 bottle.

--- Formula/w/wolfpack.rb
+++ Formula/w/wolfpack.rb
@@ -5,6 +5,15 @@ class Wolfpack < Formula
   sha256 "b04c7807d9fb4f0d5058a3b83a64d1d35e565bad6ee0ca2d1af8426531b79317"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_tahoe:   "a0d4d5a47986c63d2599a3b5f03cdcb8e3a17da2ec1ff97627cb1358bfafbba3"
+    sha256                               arm64_sequoia: "a0d4d5a47986c63d2599a3b5f03cdcb8e3a17da2ec1ff97627cb1358bfafbba3"
+    sha256                               arm64_sonoma:  "a0d4d5a47986c63d2599a3b5f03cdcb8e3a17da2ec1ff97627cb1358bfafbba3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1ad84f847230557265f8ae914a75255e038447f52bf7a59a6b181fa214e85e97"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "828e081df7c5181609c403cf069ff951a91712cc44b3b6012d89200bda3e7e1d"
+  end
+
   depends_on "node"
 
   def install
