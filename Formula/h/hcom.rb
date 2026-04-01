609e70bf981c44d56de7666e7ad78c8810b75967
Rui Chen <rui@chenrui.dev>
2026-04-01 09:34:52 -0400

    hcom 0.7.7 (new formula)
    Closes #5684.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/h/hcom.rb
@@ -0,0 +1,17 @@
+class Hcom < Formula
+  desc "Let AI agents message, watch, and spawn each other across terminals"
+  homepage "https://github.com/aannoo/hcom"
+  url "https://github.com/aannoo/hcom/archive/refs/tags/v0.7.7.tar.gz"
+  sha256 "7d7c149fc614f00e7b906f7d5844e77d90721e97c909fcd010448f448ce31ba1"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/hcom --version")
+  end
+end
