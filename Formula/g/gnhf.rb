b692c8b311986d6a0ad1394d035a3e38557d2ade
Rui Chen <rui@chenrui.dev>
2026-04-02 12:33:11 -0400

    gnhf 0.1.8 (new formula)
    Closes #5716.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/g/gnhf.rb
@@ -0,0 +1,18 @@
+class Gnhf < Formula
+  desc "Autonomous agent orchestrator for long-running coding tasks"
+  homepage "https://github.com/kunchenguid/gnhf"
+  url "https://registry.npmjs.org/gnhf/-/gnhf-0.1.8.tgz"
+  sha256 "47fa77edb88dbe7ab9f29d3325d64c4f63bad5490d4d2a850f8bac01266e9e0f"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/gnhf --version")
+  end
+end
