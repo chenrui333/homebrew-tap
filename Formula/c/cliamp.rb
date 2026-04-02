62a704466d29b7023b3b32350f7cc4704a262fa8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-02 16:56:11 +0000

    cliamp: update 1.32.4 bottle.

--- Formula/c/cliamp.rb
+++ Formula/c/cliamp.rb
@@ -8,11 +8,11 @@ class Cliamp < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "baf78cba1be41cc3add1c05fd12e4b713aca4dcfec128dff1d81b85a988d73f3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "367a1be995487252616d8d987e76e7e155f0445da30baeb3cc3c8869779293c9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b0add85e78af726702cae5b56d2188e8d340fbc3b4e951afb61a7ca1e9072e36"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fc5a68e78c965988b9250f9de44831d612d637535072bc8ef3b0359b05206f78"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3b5022c38417926cbe010ce72595bb28a4e8183d359f28e419639e8fbf157ca9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6cb3940552f617848d0b58fcc602c6ea4f0a62f7450fafad5f14fe52332f235b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "79d4422f29ad21405d88c2d029fea77dc3b0dafc6b8d2f7c5523282b17136fdb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "97ef86bd80e76d87e2c42c7e81408f6c65db3e0c6635a3ed2fe8a9fd8485362f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f2bf5cc735a370e35493a10689422f8fdfc106a70e0aa70c932275ae17f0628e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8f649667dff5e228940b91aa396a5e3ef77428e0cb7786bb6c68dcfabc7e1729"
   end
 
   depends_on "go" => :build
