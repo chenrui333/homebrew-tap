0696f63e52b2686b011ae70d66587800f11b1f83
Rui Chen <rui@chenrui.dev>
2026-04-02 15:47:40 -0400

    oh-my-codex 0.11.12 (new formula)
    Closes #5723.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/o/oh-my-codex.rb
@@ -0,0 +1,22 @@
+class OhMyCodex < Formula
+  desc "Multi-agent orchestration layer for OpenAI Codex CLI"
+  homepage "https://github.com/Yeachan-Heo/oh-my-codex"
+  url "https://registry.npmjs.org/oh-my-codex/-/oh-my-codex-0.11.12.tgz"
+  sha256 "a0f9ba756d4751d88270526c6cc4bf2f8ba78f4faa3da1fef419e59ef70c6350"
+  license "MIT"
+  head "https://github.com/Yeachan-Heo/oh-my-codex.git", branch: "main"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    pkg = libexec/"lib/node_modules/oh-my-codex/package.json"
+    assert_match version.to_s, shell_output("node -p \"require('#{pkg}').version\"").strip
+
+    assert_match "oh-my-codex", shell_output("#{bin}/omx --help")
+  end
+end
