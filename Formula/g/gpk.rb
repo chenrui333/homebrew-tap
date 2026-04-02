2e1db76b3296c7c4bcb7264a6041e226f94c6a44
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-02 16:42:09 +0000

    gpk: add 0.3.21 bottle.

--- Formula/g/gpk.rb
+++ Formula/g/gpk.rb
@@ -6,6 +6,15 @@ class Gpk < Formula
   license "GPL-3.0-only"
   head "https://github.com/neur0map/glazepkg.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5e0d39e9c0728c7eeb56fb08f4f2aeabcf368d2373274b68ae4cb2c3e27c7dd7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e0d39e9c0728c7eeb56fb08f4f2aeabcf368d2373274b68ae4cb2c3e27c7dd7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5e0d39e9c0728c7eeb56fb08f4f2aeabcf368d2373274b68ae4cb2c3e27c7dd7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "629253ab270fb2e5b77ad61128c463e529d7644d184473b1a9ac5754e0d76cd4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "41dbbe3831115bfe3469b62d51adc552113a7688c8e8929ab936f13204367448"
+  end
+
   depends_on "go" => :build
 
   def install
