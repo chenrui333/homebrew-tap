652eb5cb9afc5fa19175bdf1ca6c5002f63537a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-03 10:34:41 +0000

    meteor 0.31.0
    Closes #5747.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/m/meteor.rb
+++ Formula/m/meteor.rb
@@ -2,8 +2,8 @@
 class Meteor < Formula
   desc "Highly configurable CLI tool for writing conventional commits"
   homepage "https://github.com/stefanlogue/meteor"
-  url "https://github.com/stefanlogue/meteor/archive/refs/tags/v0.30.0.tar.gz"
-  sha256 "b1a9630798a208f1a825656d2325db6576817ccfbc304f7bae8b31305bd713c2"
+  url "https://github.com/stefanlogue/meteor/archive/refs/tags/v0.31.0.tar.gz"
+  sha256 "8c6b5e56ebb31a1ffa94adfa226c970415bae61352699d8849e34773f7e42f91"
   license "MIT"
   head "https://github.com/stefanlogue/meteor.git", branch: "main"
 
