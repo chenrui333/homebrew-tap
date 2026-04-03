4121a865a6e69ce680bcc935d232f9a6b0efd852
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-03 05:14:02 +0000

    tiki: update 0.3.4 bottle.

--- Formula/t/tiki.rb
+++ Formula/t/tiki.rb
@@ -7,11 +7,11 @@ class Tiki < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9e1655b3c46ce6e5b7cc9473901b3988fab324853caf3e07a3c4605e500003a7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9e1655b3c46ce6e5b7cc9473901b3988fab324853caf3e07a3c4605e500003a7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9e1655b3c46ce6e5b7cc9473901b3988fab324853caf3e07a3c4605e500003a7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9c8ec1241f8c4ac834723e5e86bcfd8131c2e0a0c033f6aa11b1d33d99129b6c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a205c213f21714e2f7f4354145ed8da3a29dd5090a282416e5862ea4d98b9a55"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9960190e6f4b255078a61b903a8d178c56b34f915ef9ad16abf64f9a7b855c13"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9960190e6f4b255078a61b903a8d178c56b34f915ef9ad16abf64f9a7b855c13"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9960190e6f4b255078a61b903a8d178c56b34f915ef9ad16abf64f9a7b855c13"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ae2a238d088a3364fc69f383d99fd30b62992f4e5fb1cc765cb6250be0fecaa1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ccd0d7308a958984d35dced1c22d00dc92f263bce8d67f3f75153a2a1ee13348"
   end
 
   depends_on "go" => :build
