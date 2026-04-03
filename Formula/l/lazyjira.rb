f3394c8b1c95bd1375f3484b8a2369b9241309fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-03 12:45:15 +0000

    lazyjira: update 2.7.2 bottle.

--- Formula/l/lazyjira.rb
+++ Formula/l/lazyjira.rb
@@ -8,11 +8,11 @@ class Lazyjira < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "308e71d6b575b35588eb494f87dd11793828bbee1d7e35dc02fad8078ac0c170"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "308e71d6b575b35588eb494f87dd11793828bbee1d7e35dc02fad8078ac0c170"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "308e71d6b575b35588eb494f87dd11793828bbee1d7e35dc02fad8078ac0c170"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "569007290a19fdfb18d6cd536b6c337c385bdd77144ab2026664d86c7f823243"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9e7600b1aa3699ec7e70a122535cbc7c279ec57351f7ea0db50457af0715d769"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c0f3240f80a7bc3e0eeb4cfb6252022bf52fcab318d92f0617744a8a8a85f7f4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c0f3240f80a7bc3e0eeb4cfb6252022bf52fcab318d92f0617744a8a8a85f7f4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c0f3240f80a7bc3e0eeb4cfb6252022bf52fcab318d92f0617744a8a8a85f7f4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "99cac2e9b8281118201e4a3c9249a7ac31eb195f55b962218b88936723cfe4e9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c896e12806dc832d3f77c41e72c38ba29680ae38b4f9f67c1d7245bf6b73efb9"
   end
 
   depends_on "go" => :build
