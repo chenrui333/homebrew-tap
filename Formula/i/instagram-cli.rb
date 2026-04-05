329ea3fd4e2252b99b122306fb10499ba3145727
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-05 18:47:44 +0000

    instagram-cli: update 1.5.0 bottle.

--- Formula/i/instagram-cli.rb
+++ Formula/i/instagram-cli.rb
@@ -8,11 +8,11 @@ class InstagramCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "3af820a5da28348aebee916796aeed6f1fd62d801c22ef9d6687503d53d65722"
-    sha256 cellar: :any,                 arm64_sequoia: "34b96c402bd848d4f734326fb89330ae1d937e8c2c5beae5664e685da69b9353"
-    sha256 cellar: :any,                 arm64_sonoma:  "34b96c402bd848d4f734326fb89330ae1d937e8c2c5beae5664e685da69b9353"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3d4baebeaca1dbbef791dbf9b91321afcfbee2ba300be87e41911caa0f259977"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b812ea5c5d4a73cf6257704569fe942e68100dfef27656c8feea22fb354140c1"
+    sha256 cellar: :any,                 arm64_tahoe:   "a9be0b06d8a5679829fdf6cc7a2907fa92b59573ad46ac5c0ce300192c1a5d99"
+    sha256 cellar: :any,                 arm64_sequoia: "7b7ab4cbeb2ee868848936d72da523356a3607007812c9fd4c500f1315f61c7b"
+    sha256 cellar: :any,                 arm64_sonoma:  "7b7ab4cbeb2ee868848936d72da523356a3607007812c9fd4c500f1315f61c7b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "955301faad92b5c7e5b84efdd36fdb508960560516437df8d1d3cb372f8637c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "771f5ed99fff3b5d1d187a8457cce1446f00e6f8afeefe8a84351961fb9694d7"
   end
 
   depends_on "node"
