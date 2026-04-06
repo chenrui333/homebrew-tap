31a3d70aacbd6fb51c3b61aa8c625fb6e50ffe2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-06 09:09:08 +0000

    oyo: update 0.1.27 bottle.

--- Formula/o/oyo.rb
+++ Formula/o/oyo.rb
@@ -8,11 +8,11 @@ class Oyo < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4418930de7f68ecf3338861e0d136c3d4e42e0757487837a40a725121eb2979e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "47c235a54e892010840bda4ea3d9a9b8de27de7fa0bb3fe34b76fc072cf70886"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4a7bfc45d006bd640f53c12d5b89c5dc83013bef2030d250ceb084a57df7067e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "019fc556a8a75d2fbcf75a114a109d008e80dd74c70225a4a24a04b130b67320"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e19ac2a975d81ff1901223a93db8386884919a9f3a2a4db0ce949b2e2e334ed2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cea5691b42d6204ebcb095982159efb58c433a7f12d3f43361c0d7637942f13d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "032cca9e4eec01a6e1cb96f8fa07670aca510c8d2cf2495d1f5e8291e3523d12"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "394186e93983c0d6814aa91d6d1dd1d74215cc0f73c7338f49b13c55610369e9"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f7cf80ec47efb60499f8670282d970e3dd4c37166062816fa9e1080570f47c91"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac4d8984e13dae220683f4e8271ff57d25e197228d8549efb9d10177377f591e"
   end
 
   depends_on "pkgconf" => :build
