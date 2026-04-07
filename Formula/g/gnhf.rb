c2fdc58d7f6c4c0d99fb4d3dccc7076085942b0b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-07 18:38:33 +0000

    gnhf 0.1.14

--- Formula/g/gnhf.rb
+++ Formula/g/gnhf.rb
@@ -1,8 +1,8 @@
 class Gnhf < Formula
   desc "Autonomous agent orchestrator for long-running coding tasks"
   homepage "https://github.com/kunchenguid/gnhf"
-  url "https://registry.npmjs.org/gnhf/-/gnhf-0.1.13.tgz"
-  sha256 "24da98042b9de2d4177a030e975fa06b06e5cdb240ac34f6e158264dccb27709"
+  url "https://registry.npmjs.org/gnhf/-/gnhf-0.1.14.tgz"
+  sha256 "13b111c8ee27e066183131a95095c4f28aa65fb5ee692e2522199dfbd6dbfb99"
   license "MIT"
 
   bottle do
