32b71450fdd20f3f269699550d8428b3c715e1f3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-08 17:19:35 +0000

    rfc_reader: add 0.11.2 bottle.

--- Formula/r/rfc_reader.rb
+++ Formula/r/rfc_reader.rb
@@ -6,6 +6,15 @@ class RfcReader < Formula
   license "MIT"
   head "https://github.com/ozan2003/rfc_reader.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8d9d861f1b238816578cf18b601497f644b319f941886cc5b986b45a9bbbfd89"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6b7a4563f337cc1e37425548f3196954d99c6d637c7df7b2838457dbeecaecc5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7dce7ff303b7fe44205c380b8af6b7265a321e05fbc5c937db469b47ae961804"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a4c58669d2e98377f370ee2932f3310eaf81f94f33ec3d7d535073681f9efd94"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "418d48ea5f8ac1756a38671c85f96ce54e691f5a675d8515cffd6d70b782d363"
+  end
+
   depends_on "rust" => :build
 
   on_linux do
