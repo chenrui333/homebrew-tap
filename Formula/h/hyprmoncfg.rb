8d0e61ad686a44557f96b3dc08cbb9dc4edff685
Rui Chen <rui@chenrui.dev>
2026-04-08 12:54:11 -0400

    hyprmoncfg 1.2.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/h/hyprmoncfg.rb
@@ -0,0 +1,24 @@
+class Hyprmoncfg < Formula
+  desc "Terminal-first monitor configurator and daemon for Hyprland"
+  homepage "https://hyprmoncfg.dev/"
+  url "https://github.com/crmne/hyprmoncfg/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "8111c2081f1d99105058437eae29e2fb0c6301d16597c00c1cefbef78b7ba86f"
+  license "MIT"
+  head "https://github.com/crmne/hyprmoncfg.git", branch: "main"
+
+  depends_on "go" => :build
+  depends_on :linux
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/crmne/hyprmoncfg/internal/buildinfo.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/hyprmoncfg"
+    system "go", "build", *std_go_args(ldflags:, output: bin/"hyprmoncfgd"), "./cmd/hyprmoncfgd"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/hyprmoncfg version")
+  end
+end
