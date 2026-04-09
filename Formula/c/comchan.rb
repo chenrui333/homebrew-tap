919e46cee56a10a6a484ac6b3969c6558002af7f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-09 18:37:54 +0000

    comchan 0.3.0
    Closes #5906.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/comchan.rb
+++ Formula/c/comchan.rb
@@ -1,8 +1,8 @@
 class Comchan < Formula
   desc "Minimal serial monitor and plotter for embedded applications"
   homepage "https://github.com/Vaishnav-Sabari-Girish/ComChan"
-  url "https://github.com/Vaishnav-Sabari-Girish/ComChan/archive/refs/tags/v0.2.5.tar.gz"
-  sha256 "b4ef7d7dfbf2dcdf2a22ba4f4de356f8237f9252ccb69c7f2aab1e76191a8cb9"
+  url "https://github.com/Vaishnav-Sabari-Girish/ComChan/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "f66667621b7251a92803633e6c2b18747b32a45a04d0d0e892e6483c5e65ea86"
   license "MIT"
   head "https://github.com/Vaishnav-Sabari-Girish/ComChan.git", branch: "main"
 
