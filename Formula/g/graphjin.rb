ba2f6c3bdab65d0724689d0e5215a1d7ef1b43e9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-10 08:51:26 +0000

    graphjin 3.16.2

--- Formula/g/graphjin.rb
+++ Formula/g/graphjin.rb
@@ -1,8 +1,8 @@
 class Graphjin < Formula
   desc "Build NodeJS / GO APIs in 5 minutes not weeks"
   homepage "https://graphjin.com/"
-  url "https://github.com/dosco/graphjin/archive/refs/tags/v3.16.1.tar.gz"
-  sha256 "5714b0cf194b14a6b39d7eaec5c203674c82d712178d43aafe7c07bcf5a90595"
+  url "https://github.com/dosco/graphjin/archive/refs/tags/v3.16.2.tar.gz"
+  sha256 "dcdc552e78ecb2f404502e6f629aea4d68c8351d8d03e07174dff91d33aeb7a4"
   license "Apache-2.0"
   head "https://github.com/dosco/graphjin.git", branch: "master"
 
