3c15b5c2d836775f64b41fdef5efcd755dd81644
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-12 23:56:37 +0000

    kumo: add 0.8.0 bottle.

--- Formula/k/kumo.rb
+++ Formula/k/kumo.rb
@@ -6,6 +6,15 @@ class Kumo < Formula
   license "MIT"
   head "https://github.com/sivchari/kumo.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "73948e7b9cc12cde72828c8c3ed77a2e787a932ec8c00c30e1b302ed76ad37b2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "73948e7b9cc12cde72828c8c3ed77a2e787a932ec8c00c30e1b302ed76ad37b2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "73948e7b9cc12cde72828c8c3ed77a2e787a932ec8c00c30e1b302ed76ad37b2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c66c79dfcfc1b892024489bf934af4ff5bca12be104f9d0f1a3c72dae904324a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "44731234431891e1a1e804a4fb1b118938bd2f7806fefc5c7c83579ad6177695"
+  end
+
   depends_on "go" => :build
 
   def install
