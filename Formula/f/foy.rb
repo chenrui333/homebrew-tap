dbe3699d19890310fa245a0d8381f2e372d08059
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-13 15:02:15 +0000

    foy 1.0.1

--- Formula/f/foy.rb
+++ Formula/f/foy.rb
@@ -1,8 +1,8 @@
 class Foy < Formula
   desc "Simple, light-weight and modern task runner for general purpose"
   homepage "https://zaaack.github.io/foy/"
-  url "https://registry.npmjs.org/foy/-/foy-1.0.0.tgz"
-  sha256 "aeb22884699934f0f3de4517dedb57e8ce3fd08260b4c38542ab3c2065e6ddac"
+  url "https://registry.npmjs.org/foy/-/foy-1.0.1.tgz"
+  sha256 "cbeb9511db1d1aa35c27e3db281637fa70d41065c65579bbc179eaacce351e46"
   license "MIT"
 
   bottle do
