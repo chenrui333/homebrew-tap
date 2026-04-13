e4e0fc08218d00017f6593626660b3f1ba491c54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-13 13:52:15 +0000

    foy: update 1.0.0 bottle.

--- Formula/f/foy.rb
+++ Formula/f/foy.rb
@@ -7,10 +7,7 @@ class Foy < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "048ef2bb88b689f6eed4b1b86d61fff7f0e7f76492d1e92ad7fa9a6511b7c288"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "50d41e8f6a254d8c11bdd616408f4e903eaef44e1204d0781417702041e4ae60"
-    sha256 cellar: :any_skip_relocation, ventura:       "491605b0fce3c12c7827b778e4c45200b0bcec147f704f1003688003281b6e30"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ec1dbb1d34bbb1399db473f86e83dfccaf54f654b773023ff2cb6ab231f76806"
+    sha256 cellar: :any_skip_relocation, all: "586a26cc31aca9a96b584ad4cd3845400d41b54c79cbf6684a548baedb33177a"
   end
 
   depends_on "node"
