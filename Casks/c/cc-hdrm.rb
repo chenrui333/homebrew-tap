520c19d3e037beffd71849c97545de7222eb8735
Rui Chen <rui@chenrui.dev>
2026-04-14 08:54:28 -0400

    cc-hdrm 1.4.9
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/c/cc-hdrm.rb
+++ Casks/c/cc-hdrm.rb
@@ -1,13 +1,24 @@
 cask "cc-hdrm" do
-  version "1.4.2"
-  sha256 "dc0bc570ad7714354083b596aba93c37e7f5297a36f7f558360b73049f34e5ec"
+  version "1.4.9"
+  sha256 "5c2cfb69e19c84f2aab963a47fb37f0e38703ae1d1cc06cd98f46e74ded26ca3"
 
   url "https://github.com/rajish/cc-hdrm/releases/download/v#{version}/cc-hdrm-#{version}.dmg"
   name "cc-hdrm"
   desc "Menu bar utility showing Claude subscription session headroom"
   homepage "https://github.com/rajish/cc-hdrm"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on macos: ">= :sonoma"
 
   app "cc-hdrm.app"
+
+  zap trash: [
+    "~/Library/Application Support/cc-hdrm",
+    "~/Library/Caches/com.cc-hdrm.app",
+    "~/Library/HTTPStorages/com.cc-hdrm.app",
+  ]
 end
