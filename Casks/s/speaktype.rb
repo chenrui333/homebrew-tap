404999e5c2165fa7b4fd0206b732a9e26dd5640b
Rui Chen <rui@chenrui.dev>
2026-04-14 09:10:22 -0400

    speaktype 1.0.29 (#6020)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/s/speaktype.rb
+++ Casks/s/speaktype.rb
@@ -1,6 +1,6 @@
 cask "speaktype" do
-  version "1.0.21"
-  sha256 "e2710780c9621a8111ee46cb58b14b5e73fcd750849c7ee3e9a3fbcb9f75fd79"
+  version "1.0.29"
+  sha256 "c3c3b1c99696bfccd33eff63b08bd07788127b1a00b089c42c0e1afdb3b32f16"
 
   url "https://github.com/karansinghgit/speaktype/releases/download/v#{version}/SpeakType-#{version}.dmg",
       verified: "github.com/karansinghgit/speaktype/"
@@ -8,7 +8,14 @@ cask "speaktype" do
   desc "Open-source offline voice dictation app"
   homepage "https://tryspeaktype.com/"
 
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
   depends_on macos: ">= :sonoma"
 
-  app "SpeakType.app"
+  app "speaktype.app"
+
+  zap trash: "~/Library/Preferences/com.2048labs.speaktype.plist"
 end
