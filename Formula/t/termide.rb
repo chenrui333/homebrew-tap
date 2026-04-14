48444da48a0978461ffe6676b25e6d41d1be89ea
Rui Chen <rui@chenrui.dev>
2026-04-14 10:09:13 -0400

    termide 0.19.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/t/termide.rb
@@ -0,0 +1,17 @@
+class Termide < Formula
+  desc "Cross-platform terminal-based IDE, file manager, and virtual terminal"
+  homepage "https://termide.github.io"
+  url "https://github.com/termide/termide/archive/refs/tags/0.19.0.tar.gz"
+  sha256 "30e2e19ac45f5176c16a010257083362819025eb8b47a8c73f63383602296c9c"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/termide --version")
+  end
+end
