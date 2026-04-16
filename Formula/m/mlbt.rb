86c9456430918d64be180210d850845978f0e233
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 07:15:30 +0000

    mlbt: update 0.3.0 bottle.

--- Formula/m/mlbt.rb
+++ Formula/m/mlbt.rb
@@ -8,11 +8,11 @@ class Mlbt < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ec4b330ab789ac4a2f41b3978f49bfd18a15ec1d50a96af29a8354db6ad070b5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ecb2d7f490748ea93d51b83d4da95596335d992ad3036ef4332fcc9872002b6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "503ac538093a8a65e1444f9e9c5af6287a48a4ec0bd9ab022edc55f7f8c3b86d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "41ba3bb30b2eb016a737b89f04256ef8066fd4377901cc8b0cb10539eb4e547d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c370b5b578a20362d1dba4e6dd06e87c70beff8cfd40b8ad49987892c5e56bc5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dc22e96f7190e865918ce7d91a64ee781129a456a3bc27e316a2634d17a7614b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ef6a0b21fe0fbfc9b433de26d9376200cf0ac90322f04253ea326881db3e399"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0be45eac7e45e3de5e814d43c0285e8ed2fbf80fbfb2727b0a9bda37af06aecf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "095505a1d09f54de9f888947b7c2adcc19d2aae685b9e9a8bc55db12002d7e88"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "016a261be494a4e3f5e5b89255c2e9000b03a7fece2c9377c7d47a22e0ed9f81"
   end
 
   depends_on "rust" => :build
