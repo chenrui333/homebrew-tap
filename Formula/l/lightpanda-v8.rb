5c1ebe2f5e048d227600f310c2b731873edf3f51
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 01:25:47 +0000

    lightpanda-v8: update 0.4.0 bottle.

--- Formula/l/lightpanda-v8.rb
+++ Formula/l/lightpanda-v8.rb
@@ -8,12 +8,12 @@ class LightpandaV8 < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fde6bcbec1da61fc6fa78c1c33409dbbaf7b9caec6e99bd0270f1f8b61ab1d69"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "80a2890e6ede32ec7f680db7b1d7a5319b030f6764d8484161fb2878c35fe805"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f6e2bb7b6b67f1ff2b2c32e4d84d882fee731ea20751cfb9eb5b2a37507b0bb2"
-    sha256 cellar: :any_skip_relocation, sequoia:       "7e97d7aec1159d4ba4bf40d24feefd69eaf4550adffdbc3969dd537de636147c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a1f04df5e40c0f30d56a48ec1ff000a1445a3ffdf5864d59ade2491b771917fa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f38f8125a5d49273beb717974f5bfbc6b7a85ac4553fd08223801c1e0054af71"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "69279afdda9c9b476eaf63c19edcffae76637fd7e1bb992948bb7ff226c2648d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ed1a4a2817b62d5d23864ce71bb93b67e708f36d0a576c1773375259bba89eda"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "70670141b8033b669a04ecbe5d62311ac4f54cf71d84b629ce6a5416b3b85e25"
+    sha256 cellar: :any_skip_relocation, sequoia:       "db2b7b865a84e51934d8b5f2037c052d8fc098eb40d0c921897bf6f37fee7312"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a325c8007abeb273be53d6ec7ebe25ae9de01a332e06532d57181ca85c2c986d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "51696a4a22811f54f8e5b2f5eda8ab592eeec7979944140180e073ce695888b4"
   end
 
   if OS.mac? && Hardware::CPU.arm?
