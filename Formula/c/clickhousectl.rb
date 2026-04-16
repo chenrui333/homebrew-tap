d5647279d70b4c546730fc20d31f91677f54121e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-16 18:51:02 +0000

    clickhousectl: add 0.1.18 bottle.

--- Formula/c/clickhousectl.rb
+++ Formula/c/clickhousectl.rb
@@ -6,6 +6,15 @@ class Clickhousectl < Formula
   license "Apache-2.0"
   head "https://github.com/ClickHouse/clickhousectl.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9f38388bd0c85916dcb3cbf9b5a5375f52da538c2e578827815df247352a7598"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b42666087d9857eee797896703902eb235a1aa896f55283fcb1b1eb73b8a5f4b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "12272454a818ddde384a7e81c83d9c948857d129523a055f03e1930592d55ad2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a791a7432a8f3284bd3e89d5c2dee490d648dfbfc745fe36b6296318c6794331"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "06bd7751e14f30ee4a4dc43a2f668edd5fe7e0fce99618c0fad7bad0e65523bd"
+  end
+
   depends_on "rust" => :build
 
   def install
