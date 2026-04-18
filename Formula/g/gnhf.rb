da4d603a6c217e3aa7a274b30b854d7d002ad289
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-18 08:33:10 +0000

    gnhf 0.1.20
    Closes #6152.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/gnhf.rb
+++ Formula/g/gnhf.rb
@@ -1,8 +1,8 @@
 class Gnhf < Formula
   desc "Autonomous agent orchestrator for long-running coding tasks"
   homepage "https://github.com/kunchenguid/gnhf"
-  url "https://registry.npmjs.org/gnhf/-/gnhf-0.1.19.tgz"
-  sha256 "86f5061ff47241842fe26a7a5df0d648256c62a83b2a431c6a2079b363a25b90"
+  url "https://registry.npmjs.org/gnhf/-/gnhf-0.1.20.tgz"
+  sha256 "e558a9ab5314873c4599944fb83e4c717f5f8c3de55433fec65c1d4228e73777"
   license "MIT"
 
   bottle do
