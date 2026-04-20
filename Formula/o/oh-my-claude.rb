809a156ba9c10f5bcbe2ed9aba32105eb42b157b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-20 09:07:55 +0000

    oh-my-claude 4.13.0
    Closes #6211.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/o/oh-my-claude.rb
+++ Formula/o/oh-my-claude.rb
@@ -1,8 +1,8 @@
 class OhMyClaude < Formula
   desc "Teams-first multi-agent orchestration for Claude Code"
   homepage "https://github.com/Yeachan-Heo/oh-my-claudecode"
-  url "https://registry.npmjs.org/oh-my-claude-sisyphus/-/oh-my-claude-sisyphus-4.12.1.tgz"
-  sha256 "90f8a3837e0b020289b327de858abc0b34dca9e278cf70619d25e8772fa6f571"
+  url "https://registry.npmjs.org/oh-my-claude-sisyphus/-/oh-my-claude-sisyphus-4.13.0.tgz"
+  sha256 "fae627c9ca0424be59526fe7d87840abb9730cf633f1934e717ee8c762103566"
   license "MIT"
   head "https://github.com/Yeachan-Heo/oh-my-claudecode.git", branch: "main"
 
