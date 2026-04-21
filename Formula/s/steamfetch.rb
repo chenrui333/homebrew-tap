9d91d896979390be4d3a723ebb47f557517fc6c2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-21 22:53:48 +0000

    steamfetch: update 0.5.5 bottle.

--- Formula/s/steamfetch.rb
+++ Formula/s/steamfetch.rb
@@ -8,10 +8,10 @@ class Steamfetch < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "2fb66e5f8ed132b171f31433dd996d4be077f7ab38aea15fc04851d5d2d2e9cf"
-    sha256                               arm64_sequoia: "9445b7b23177ec60708f59f506bc5749bb8f49b26a12a01a4662517e9b599d04"
-    sha256                               arm64_sonoma:  "cab1b62189e8aa2aa47bc0c99b56b0ea8ecc534d2608b29025bea6b214beb3c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "82b6707595253022bc9864730dd35053b752ffe7cffd2bd3b7075337636bb01f"
+    sha256                               arm64_tahoe:   "0de60fe4d38d4ed66b9d17424aa7c1b9a0dc5bd15305f4e06bb0fbb2e8b68ec4"
+    sha256                               arm64_sequoia: "d19e95e4b14754b2a41bfb647cc9df50f4bb908348fd592bf300fddea59cd5e4"
+    sha256                               arm64_sonoma:  "9b8ea23671ff00b1ffe2c9ff71e4acf5870f5492ef3c65e8b1e3f585081e02bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ba60b06b8780a26dd12114d4ef9514c38f70868e518d87a39635ac615cf1996"
   end
 
   depends_on "rust" => :build
