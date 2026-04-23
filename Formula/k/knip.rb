4a122f61dab19254397691d49b9785fb98dad930
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-23 07:14:08 +0000

    knip: update 6.6.1 bottle.

--- Formula/k/knip.rb
+++ Formula/k/knip.rb
@@ -7,11 +7,11 @@ class Knip < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "9cc8fc2c43cb4a85a6349307766e47553a402de921f55356b79c9f5220354811"
-    sha256 cellar: :any,                 arm64_sequoia: "4fd2e2f3e830edf4fbc1dba8774fd0da610a1df99fe64eecbcefaab38cccf89e"
-    sha256 cellar: :any,                 arm64_sonoma:  "4fd2e2f3e830edf4fbc1dba8774fd0da610a1df99fe64eecbcefaab38cccf89e"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d982d58e77aee42f825828994196cb6604cd21553178766c6b4cf2a9fc406350"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e1bcae4881ba79f68a77309b1eaf5cb74b54cf9a8cca1fbb310452468a179e09"
+    sha256 cellar: :any,                 arm64_tahoe:   "b8faef95aac3e5aa5ff6aee09a501d36e5ae3cebfa8343559caff33e2e148875"
+    sha256 cellar: :any,                 arm64_sequoia: "c58cf5e22388fc3f3638ebb61d3afbd0e32658cb62c3a0df83a3444de2e94e36"
+    sha256 cellar: :any,                 arm64_sonoma:  "c58cf5e22388fc3f3638ebb61d3afbd0e32658cb62c3a0df83a3444de2e94e36"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "006ae4962ea6fd1ee814677bacf9d950bc39424afec4d4c64230210d52be10e1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e73227e6efa9b09948ef3fb223022ed07840f067cb73335dbfc23cfe33fb7383"
   end
 
   depends_on "node"
