077c20ce8488aefef28392276d46212be887920b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-24 22:53:15 +0000

    splitrail: update 3.5.2 bottle.

--- Formula/s/splitrail.rb
+++ Formula/s/splitrail.rb
@@ -7,11 +7,11 @@ class Splitrail < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bb057608f49929c09a076d4b942b9e1cebf52e145c7ce67f31203619fab63e10"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8c6a2ebd0e891763e102e94852a53bb20d06b8fd4b74d317cfeff8174a8b4ca4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ae1d890c4e22c393acdeba60388442ac7d2ccc63281e8e442178db9ec0c5d727"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c94622b62488542d050c5d0bd336054d82815732ccd284f94e0797a25bd6f35e"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "33ea05736f124d4e05f8816f7dff4afb980ca692eabb6e5c94b879341a37e5b8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cd8406751098ad72c1b3b94910fecd78748db846cc2a0bd4676b85669db99776"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c36ed71597f1aa13cdb042576711b15603bbf3bef83880e4ef38ea6a3956991a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "48f5f055e60361298b16121052a6b304890f8689f99ab063229438550c9e8d30"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "86e64e4bdff8c7f32e8221052bfd2d7f4f625b54fe75bee3259efbd7fba8f9fc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9614b1808ddbef185b14c80bcea0233fe3fac044eefeb4457d4964c2e5555635"
   end
 
   depends_on "rust" => :build
