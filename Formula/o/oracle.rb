61343091fb7a34d70c8cb2132c086c7f0ab293e7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-25 23:57:30 +0000

    oracle: add 1.3.0 bottle.

--- Formula/o/oracle.rb
+++ Formula/o/oracle.rb
@@ -6,6 +6,15 @@ class Oracle < Formula
   license "MIT"
   head "https://github.com/steipete/oracle.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c5b5e1de5656e94294e6794fba935d8873333e5370987052c6b88b5fa91ce336"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8150a56e0a061ad75108e1dd9e5202078a72191e1ff4fe20eed27a77058479ba"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e7c3f427840321ad5a4a6bd25e2535a899121cae05d5b3fa795d811f0cee00bf"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6d4363bd711cd2f3a3b6cae8a528d7921bea077876ff582f52f92a27ec784d67"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b9a1fd734c422fbdcf62124ef39653823df6b71b182edc54dd09f825a0a24eba"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "pnpm" => :build
   depends_on "node"
