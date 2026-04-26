af909e55ea0dda626a78333788c6cf101bb37621
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-26 19:02:51 +0000

    termide: update 0.20.1 bottle.

--- Formula/t/termide.rb
+++ Formula/t/termide.rb
@@ -7,11 +7,11 @@ class Termide < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "b6b8df50f2a995f0f55dd667fd7ec9c36a72454a62303a952930d90c357864bb"
-    sha256                               arm64_sequoia: "d5f0c838e8fc34a541a682a3c7ce5d5d39dce94e32e23ad42d5ace7b50fa7c5f"
-    sha256                               arm64_sonoma:  "3fcc78a582efbd2dfefa442a5550d23cca0f5665e0000ee3e09ab8f14080cbdb"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "741afdb6ef34a46b27e43e33f4386ba03096000a45829919ea7619871f7e5ee7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fe21b332b3eed6857b816f93435b09c4a779b2cb1d1acf249bf0d599a7bb9775"
+    sha256                               arm64_tahoe:   "1a5444c15556c7eacc7d16a015df77fd947b3094e8e58a567072684f52f335e0"
+    sha256                               arm64_sequoia: "21c01a83992381d4bbc518c4b81b05787fb6247bf6d46231bc3fd5c4ba814542"
+    sha256                               arm64_sonoma:  "80ab6308a6188ebdd38c90fc2fa6517916c7f97a8ec88816b6ccfc15ea0965b3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "acc069a3dde5140c1f1ac29fdccb12465bdf18b97515674d555b79a3378aa5d0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7988bbaca4851e392aa91613c50ae4c70a3e81cccddf33ac0d874278fe417307"
   end
 
   depends_on "pkgconf" => :build
