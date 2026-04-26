e2081faf4a1f4a79d8d478cdb16b92137ddbc7fe
Rui Chen <rui@chenrui.dev>
2026-04-26 11:25:33 -0400

    termide: add Linux zlib dependency
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/t/termide.rb
+++ Formula/t/termide.rb
@@ -9,6 +9,10 @@ class Termide < Formula
   depends_on "rust" => :build
   depends_on "openssl@3"
 
+  on_linux do
+    depends_on "zlib-ng-compat"
+  end
+
   def install
     system "cargo", "install", *std_cargo_args
   end
