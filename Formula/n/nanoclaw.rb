696e47fb59f16f6d2fc922b289171f6612e17010
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-26 17:57:28 +0000

    nanoclaw: add 1.1.0 bottle.

--- Formula/n/nanoclaw.rb
+++ Formula/n/nanoclaw.rb
@@ -7,6 +7,15 @@ class Nanoclaw < Formula
   license "MIT"
   head "https://github.com/qwibitai/nanoclaw.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_tahoe:   "5eed76b3f8cdf7cca0448192aef923f4c14c8bb94d3cd1273724e57ea39c4059"
+    sha256 cellar: :any,                 arm64_sequoia: "ee56c0175da9b9654fa642ddee01abf044666ba6552c825f9890978272ae6ae5"
+    sha256 cellar: :any,                 arm64_sonoma:  "3bc56d3a37c14ef206542bf2c6cb285abfbce0b71aaa948a90fe80782c1800bd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "dca19e1d8898a9ebc948cf2b024e217b50e761e06007d256d752d371bf0c9832"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed3e9959a2488037028f8ad7888cf8b8372644384a3dc4ac0f3e76a1510cfbca"
+  end
+
   depends_on "node@24"
 
   def install
