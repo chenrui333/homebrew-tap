c8a1246b7bc0cb6a22eee80bb7dfc8c50893cab9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-27 19:32:01 +0000

    opamui: add 0.2.0 bottle.

--- Formula/o/opamui.rb
+++ Formula/o/opamui.rb
@@ -6,6 +6,15 @@ class Opamui < Formula
   license "Apache-2.0"
   head "https://github.com/nlamirault/opamui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cb40cdafbd7f8b725324d640951c1a9e9a811cc40297ac7fba5e397c19973c48"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e10a37950d63c241be5ec23767f296e3cec79dd72fa2addf591cf680c4ff0503"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d12c658a5bf2a826d035de6f1a1bb08035e5c34da31df8ab5715b4bba1f10f10"
+    sha256                               arm64_linux:   "6ba52c26393fe1a279617dd5bf815a3ea5d159742e24cb21e3e323ded11c7d35"
+    sha256                               x86_64_linux:  "87d788098ece68202e093deebf74e4516ffb8d9acef6d081b713caff1c822501"
+  end
+
   depends_on "dune" => :build
   depends_on "ocamlbuild" => :build
   depends_on "opam" => :build
