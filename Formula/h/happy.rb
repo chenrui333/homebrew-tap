3056fe0901cc56ec96f2ad1cfee1056d1b3fbcc7
Rui Chen <rui@chenrui.dev>
2026-04-26 20:55:23 -0400

    happy 1.1.7 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/h/happy.rb
@@ -0,0 +1,24 @@
+class Happy < Formula
+  desc "Mobile and Web client for Claude Code and Codex"
+  homepage "https://happy.engineering"
+  url "https://registry.npmjs.org/happy/-/happy-1.1.7.tgz"
+  sha256 "dc04cf8b23c9d64324e3ff77591466ed7805b126e453acb8079d675ea67083d3"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match "\"version\": \"#{version}\"", (libexec/"lib/node_modules/happy/package.json").read
+
+    with_env(HAPPY_HOME_DIR: testpath/".happy") do
+      output = shell_output("#{bin}/happy doctor 2>&1")
+      assert_match "Happy CLI Version: #{version}", output
+      assert_match "Doctor diagnosis complete!", output
+    end
+  end
+end
