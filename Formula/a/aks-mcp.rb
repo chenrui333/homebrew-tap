05262c2c70ce5f86c105bc72baa20060c6cbbfc4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-27 20:47:17 +0000

    aks-mcp: add 0.0.17 bottle.

--- Formula/a/aks-mcp.rb
+++ Formula/a/aks-mcp.rb
@@ -6,6 +6,15 @@ class AksMcp < Formula
   license "MIT"
   head "https://github.com/Azure/aks-mcp.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c2926b1298c67e8f9a9c284142a93711532b83e13348babcaa257c05ebcc2b8a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c2926b1298c67e8f9a9c284142a93711532b83e13348babcaa257c05ebcc2b8a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c2926b1298c67e8f9a9c284142a93711532b83e13348babcaa257c05ebcc2b8a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e5e599b10e3ff3d0b38a3659a25ee303f95543bfbf21076519d6788bd8e70e82"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7efa983b6f9de3bff1d165e7e72e67ee9b691bdb81e0cc2e86f09f0d1ad0c67c"
+  end
+
   depends_on "go" => :build
 
   def install
