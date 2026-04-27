9e177a6a542514909ba5d494132cd2b1fddc9efb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-27 01:48:32 +0000

    codemachine: add 0.8.0 bottle.

--- Formula/c/codemachine.rb
+++ Formula/c/codemachine.rb
@@ -5,6 +5,15 @@ class Codemachine < Formula
   sha256 "13b5b78d7e33e1d6733e8dce05e5b4d41173db44465f6ca559172b517890bcdd"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256                               arm64_tahoe:   "adb9560f22748b73256c3fedf967eb1b28442446199ad2d5161f0581f07bd531"
+    sha256                               arm64_sequoia: "adb9560f22748b73256c3fedf967eb1b28442446199ad2d5161f0581f07bd531"
+    sha256                               arm64_sonoma:  "adb9560f22748b73256c3fedf967eb1b28442446199ad2d5161f0581f07bd531"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "299b78fca3b7450098e48db277b0fe5baef9eedff5f4ed860c705e431a73778b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b3563da1d70eaa31fe446995d1b85472e1dad3de43d7b8ee6b1910b007bca8bf"
+  end
+
   depends_on "chenrui333/tap/bun"
   depends_on "node"
 
