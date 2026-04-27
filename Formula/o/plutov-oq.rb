850ae123684a052da19465792af5415ad207dfc3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-27 20:00:35 +0000

    plutov-oq: add 0.0.22 bottle.

--- Formula/o/plutov-oq.rb
+++ Formula/o/plutov-oq.rb
@@ -6,6 +6,15 @@ class PlutovOq < Formula
   license "MIT"
   head "https://github.com/plutov/oq.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "675fb776654f57413074f2b422bf973e58f5bc407962c13cb70cefe1235def69"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "675fb776654f57413074f2b422bf973e58f5bc407962c13cb70cefe1235def69"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "675fb776654f57413074f2b422bf973e58f5bc407962c13cb70cefe1235def69"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3a2ad51f54f8c1b9778d6b6b3751138e36d5ea0b396b9c3f864f6c2bc748e6d7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "446e957d0967425e9005a922653dcc09d3bdb994a7ab771167b86a4ccf750b55"
+  end
+
   depends_on "go" => :build
 
   def install
