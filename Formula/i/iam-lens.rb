2e42afe88ff60af0253a4370d3d306952290906f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-27 17:43:59 +0000

    iam-lens: update 0.1.118 bottle.

--- Formula/i/iam-lens.rb
+++ Formula/i/iam-lens.rb
@@ -7,7 +7,7 @@ class IamLens < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "3fd47c7b38035c75f74b61510b584208a0131115538237a651860e3d5a3148f5"
+    sha256 cellar: :any_skip_relocation, all: "182b957f487c47f7b38ac9a6fe5c281fe15e4f09c7f59abe52945e68d1cf6a60"
   end
 
   depends_on "iam-collect"
