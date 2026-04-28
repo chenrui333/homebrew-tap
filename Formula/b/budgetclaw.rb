64eb9f14b0d3019d36bc7b11e859b40fb945a86a
Rui Chen <rui@chenrui.dev>
2026-04-28 01:16:30 -0400

    budgetclaw 0.1.1 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/b/budgetclaw.rb
@@ -0,0 +1,23 @@
+class Budgetclaw < Formula
+  desc "Local spend monitor for Claude Code"
+  homepage "https://github.com/RoninForge/budgetclaw"
+  url "https://github.com/RoninForge/budgetclaw/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "59f19accd26a759aaf80237258ed7b0b92fe4980e592e36d752eca3458f985c9"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/RoninForge/budgetclaw/internal/version.version=#{version}
+      -X github.com/RoninForge/budgetclaw/internal/version.commit=HEAD
+      -X github.com/RoninForge/budgetclaw/internal/version.buildDate=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/budgetclaw"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/budgetclaw version")
+  end
+end
