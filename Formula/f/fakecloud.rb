aeec8afe41ff3e90e1558d799d662fcc3e5cc9e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-28 15:46:59 +0000

    fakecloud: add 0.13.0 bottle.

--- Formula/f/fakecloud.rb
+++ Formula/f/fakecloud.rb
@@ -8,6 +8,15 @@ class Fakecloud < Formula
   license "AGPL-3.0-or-later"
   head "https://github.com/faiscadev/fakecloud.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "116c825d6e9a0e82c346425bf715532d758b51aa2ad74fd764f3c27b68fee484"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "afad3d74161f1d2e8459572ca15be23e13ce7fd180b397e396492550820a2205"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ae7d8bf40c644d14a341fb21dfe4fe4dc697ffd5465c48e1390016b57c4de359"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "34266b45017866b783577111e3d22caf8111e4b080584a8be0461cc79e9e58ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e4e424ff19c70830650df2fde2aa91a2f6ae9e56a72d552b4e28c4b6632a9328"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
