982d4d2ae4beea0665dc6c87cf2e7c050bbac6ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-28 05:57:39 +0000

    librarian-mcp: add 0.1.0 bottle.

--- Formula/l/librarian-mcp.rb
+++ Formula/l/librarian-mcp.rb
@@ -5,6 +5,15 @@ class LibrarianMcp < Formula
   sha256 "1e93fae534c93621f0e9f298f56241f0ca5998a08f4d25a0975298daadf6ce38"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "734736cddf081fa8b5589fdacc5abdb6b6dce9c48b9d6014d34794e84dcd3054"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eadb559a8e3c328eb167d07f2af74cdf5526eccbd8023c96e90f33c7ff575eb4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ea5ee60c6f7630e1cf906d94fde87321887e2789805d2c9de48924a746959e6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1e5e8c0740e649775fe85dbbef529adab51b6b32e6a0cc975d6756a36f89dd7f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d9e663ffcaa71f77d505a3eb592676a1d5b5a55edad2ad6dfa4239f4c2162e13"
+  end
+
   depends_on "rust" => :build
 
   def install
