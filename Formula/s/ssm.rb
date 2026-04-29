38004c9a208791eefada0e5cf132e4dbc8600798
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-29 13:02:01 +0000

    ssm 0.4.2

--- Formula/s/ssm.rb
+++ Formula/s/ssm.rb
@@ -1,8 +1,8 @@
 class Ssm < Formula
   desc "Terminal Secure Shell Manager"
   homepage "https://github.com/lfaoro/ssm"
-  url "https://github.com/lfaoro/ssm/archive/refs/tags/0.4.0.tar.gz"
-  sha256 "e6be15721429ae654880bc432becc3b80d06cd558e5bea1f2806e408517e5c87"
+  url "https://github.com/lfaoro/ssm/archive/refs/tags/0.4.2.tar.gz"
+  sha256 "6a2226884509377b873831efc4e4103c007e3e59283baf9598c3bfbc4f3e96b8"
   license "BSD-3-Clause"
   head "https://github.com/lfaoro/ssm.git", branch: "main"
 
