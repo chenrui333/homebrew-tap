481bfa90c840fd2d85c757da87d2611ffe02b700
Rui Chen <rui@chenrui.dev>
2026-04-29 22:39:56 -0400

    aube 1.5.1 (new formula)
    Closes #6531.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/a/aube.rb
@@ -0,0 +1,27 @@
+class Aube < Formula
+  desc "Fast Node.js package manager"
+  homepage "https://github.com/endevco/aube"
+  url "https://github.com/endevco/aube/archive/refs/tags/v1.5.1.tar.gz"
+  sha256 "8c631862b8c1b5fd93574d95dd308680e737af098a15c1da94a13ef88386e64c"
+  license "MIT"
+  head "https://github.com/endevco/aube.git", branch: "main"
+
+  depends_on "cmake" => :build
+  depends_on "rust" => :build
+  depends_on "usage" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "crates/aube")
+
+    generate_completions_from_executable(bin/"aube", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/aube --version")
+    assert_match "Usage", shell_output("#{bin}/aubr --help")
+    assert_match "Usage", shell_output("#{bin}/aubx --help")
+
+    (testpath/"package.json").write('{"name":"test","version":"0.0.1"}')
+    system bin/"aube", "install"
+  end
+end
