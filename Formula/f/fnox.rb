8f4e68ed9b09335d93ec60aac62d22c2c597fd34
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-04-30 04:03:53 +0000

    fnox: add 1.23.0 bottle.

--- Formula/f/fnox.rb
+++ Formula/f/fnox.rb
@@ -6,6 +6,15 @@ class Fnox < Formula
   license "MIT"
   head "https://github.com/jdx/fnox.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "490b43718eafacedc31409a3f1007d45315ba558dbaaa98c3763ac6797608134"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "54a3485e5e8049ef2a43032677d9bcd878f70da986d7ea441b336eb395f96926"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d80b9cda955fd8511017ce8d9676048efdc5284400f41fc3b28860e67f40e6b0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "edd90520b061ca0cbe5e81dc17a7711b893a2ec9bd9438f2d0306820931362fb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7a4b459116bcb60bcc82186ae924aa0e960f00ec2ebba5f91acb9d8807fa7cd5"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "usage" => :build
