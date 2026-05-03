14a52256f57fb49e81642c6d565493acd20b6928
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-03 13:51:42 +0000

    mockgen: add 0.6.0 bottle.

--- Formula/m/mockgen.rb
+++ Formula/m/mockgen.rb
@@ -6,6 +6,16 @@ class Mockgen < Formula
   license "Apache-2.0"
   head "https://github.com/uber-go/mock.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6ad1a69790b85b2f1cfa2c9c431fe15119234897422d39a47c8382cd27bcb7c3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6ad1a69790b85b2f1cfa2c9c431fe15119234897422d39a47c8382cd27bcb7c3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6ad1a69790b85b2f1cfa2c9c431fe15119234897422d39a47c8382cd27bcb7c3"
+    sha256 cellar: :any_skip_relocation, sequoia:       "0423d869544eb90c04c16659a81088eb3c5b6c00f04de4f5caedc78dabba964c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6ab9ed1c02f43a6d3f2625d6b972dba5ba701a65e79d2f250a9a6cb346408e86"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "81707a759e11898803d4a2b64428160a096ec33914358f7602bcd805d27996d7"
+  end
+
   depends_on "go" => [:build, :test]
 
   def install
