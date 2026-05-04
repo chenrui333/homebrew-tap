0d64e4e7e7e029feac192cfb45599b74db757961
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 16:21:41 +0000

    unarj: add 2.65 bottle.

--- Formula/u/unarj.rb
+++ Formula/u/unarj.rb
@@ -10,6 +10,16 @@ class Unarj < Formula
     regex(/href=.*?unarj[._-]v?(\d+(?:\.\d+)+[a-z]?)\.t/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3feceaef70cef3d3f54df2941c2b183ccf614cdf0015090d817486b352715b8c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cbf4dbb61c79b57ef9fd60755a62b2e6ed009ae4691bdeffed293e1b05f463fa"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "50661363548d642668fcbd471fcffc7f4c53f908af74a29392df0254a9e70a9b"
+    sha256 cellar: :any_skip_relocation, sequoia:       "ca2fabb243c6c48585774d9d030571ba278fce7fd5a6f77e58fd9aa73933b37d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e1f31155b12dba8ee64007976cf71343a59e4cbca6fa621316180bfd5110434f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "350540b306667b67891a2b9a783b5c4fd5fb269edf7671be75c4eedd994733bc"
+  end
+
   resource "testfile" do
     url "https://s3.amazonaws.com/ARJ/ARJ286.EXE"
     sha256 "e7823fe46fd971fe57e34eef3105fa365ded1cc4cc8295ca3240500f95841c1f"
