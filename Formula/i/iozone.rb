dec478668bbc627b79102c11887dedea22c217b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 14:19:25 +0000

    iozone: add 3.494 bottle.

--- Formula/i/iozone.rb
+++ Formula/i/iozone.rb
@@ -13,6 +13,16 @@ class Iozone < Formula
     end
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e513e62e909eb53493e3a083245acc33e438d33fc6f555a5ec51e34bbb997abe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7b88d6c6ffc786e36008eba18ebb8079d1e5da940fe809b60c9a9aa9e0149fa1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5672c0656860d804a4505c200d93f1ba237727a8249e9c1ce56f463a56bfa7a9"
+    sha256 cellar: :any_skip_relocation, sequoia:       "b1264a3ecb9a3d0e1277c571947868709a5f28a4c4da921974e7371bdfadc2c2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5a9631d16476814aa01fad26179372862da693118855787587334448f825a2ca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e117e0a2430e6577fbf41843707e79b7aa470efe8a4a5054438ccffbdad859cd"
+  end
+
   def install
     cd "src/current" do
       target = OS.mac? ? "macosx" : OS.kernel_name.downcase
