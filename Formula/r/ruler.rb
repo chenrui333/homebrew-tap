96d1d85ca317361e1f0ad7866623a2525c803590
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 14:45:28 +0000

    ruler: add 2.5.0 bottle.

--- Formula/r/ruler.rb
+++ Formula/r/ruler.rb
@@ -6,6 +6,15 @@ class Ruler < Formula
   license "CC-BY-NC-SA-4.0"
   head "https://github.com/sensepost/ruler.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a4e91130fc2a1e9a597504c9220664e52fb89d265f9c5b28bdbffbb2def5a30c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a4e91130fc2a1e9a597504c9220664e52fb89d265f9c5b28bdbffbb2def5a30c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a4e91130fc2a1e9a597504c9220664e52fb89d265f9c5b28bdbffbb2def5a30c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0a70495a89366734ac469f645c1e74f0ead64a947e3335fde4496aec32ac7e2d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "164b8ed8a4715a9670af51b751dd297720f61e4463fc95406648647641490a1b"
+  end
+
   depends_on "go" => :build
 
   def install
