ce9093798c298ab301565dbb260f95b4d7f3b662
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 13:30:11 +0000

    tdd-guard: add 1.1.0 bottle.

--- Formula/t/tdd-guard.rb
+++ Formula/t/tdd-guard.rb
@@ -5,6 +5,15 @@ class TddGuard < Formula
   sha256 "f9da65d258979704097f6646190473a31080e05909ab54c814b8d902a8938087"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_tahoe:   "e6819975f8e39b0c55e8374d02f464492daa0feedc97f70d23e65a47f0ebc5e3"
+    sha256 cellar: :any,                 arm64_sequoia: "ee0e0e55bd08ef48a097875f8155c4bbef36f76b5bb65ecc2252ec8618ccac64"
+    sha256 cellar: :any,                 arm64_sonoma:  "ee0e0e55bd08ef48a097875f8155c4bbef36f76b5bb65ecc2252ec8618ccac64"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8f1934a40ca684bbb527e73814bef355e6e85b404bac6719f9fb2726491f32fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4387aced381ebee74c9430eab942c78f696801c20e0a52756b3e9b18f731c5d6"
+  end
+
   depends_on "node"
 
   def install
