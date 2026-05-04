dcc8e552bf01335ad65822de29c5b8555c76325a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 16:28:54 +0000

    dgen: add 1.33 bottle.

--- Formula/d/dgen.rb
+++ Formula/d/dgen.rb
@@ -5,6 +5,15 @@ class Dgen < Formula
   sha256 "99e2c06017c22873c77f88186ebcc09867244eb6e042c763bb094b02b8def61e"
   license all_of: ["BSD-3-Clause", "LGPL-2.0-or-later", "GPL-2.0-or-later"]
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d8dd287512a2b98448f61b94ccd3f0ac49dd49382ec5dc2b3b9b07322054a701"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "406945a412c01d32c1e0426722159fbd51ab4d291a71a07598a4599b3f4f407d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54939e9a3ad43fb68c7627a93ce61a441912196fb3b9fddff5cdb83cb63db6fd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9906018b434b4c10fb12cd165057759be64bf6e03572baa8a688ac97fd9803b1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c6037e60ecce43f013cf0a0098e4c810350549510e950d70b9f9da98f6cd76ce"
+  end
+
   head do
     url "https://git.code.sf.net/p/dgen/dgen.git", branch: "master"
 
