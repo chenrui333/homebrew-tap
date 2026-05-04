bd094f5e6ac6f8a7d0dea53a42fbee64f762ed52
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 09:29:22 +0000

    gokin: update 0.78.58 bottle.

--- Formula/g/gokin.rb
+++ Formula/g/gokin.rb
@@ -8,11 +8,11 @@ class Gokin < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b8fee52726c20b9dcb7e584152883e57dd9b97cdd7aa4247d51d61c32029aaed"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b8fee52726c20b9dcb7e584152883e57dd9b97cdd7aa4247d51d61c32029aaed"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b8fee52726c20b9dcb7e584152883e57dd9b97cdd7aa4247d51d61c32029aaed"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "103b6f8cd78471dec9863125ee418b9624eff2eead6587ca98db6d7419b6aa0f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "081385fedfe25b6e45cb9a6c1e004035999bbebecd32218e3b5c78f5d04bf9e3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cb44e69eb041076a8abaadc1cb81a8eb9448e16e9e42b87ba6b43139fc53e6e6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cb44e69eb041076a8abaadc1cb81a8eb9448e16e9e42b87ba6b43139fc53e6e6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cb44e69eb041076a8abaadc1cb81a8eb9448e16e9e42b87ba6b43139fc53e6e6"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "202891c942fa8726d26603c33ba1203e6b7b9e45e2094870b4fd637c309a90e7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "345f7b4087c5858deff62270f3930331bb57b36d4b6058f6367834214defc9ae"
   end
 
   depends_on "go" => :build
