17e2fe26f86f21c0d2bf090d0c9c773f4d9cc029
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 15:54:42 +0000

    unrar: add 6.0.2 bottle.

--- Formula/u/unrar.rb
+++ Formula/u/unrar.rb
@@ -10,6 +10,16 @@ class Unrar < Formula
     regex(/href=.*?unrarsrc[._-]v?(\d+(?:\.\d+)+)\.t/i)
   end
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any,                 arm64_tahoe:   "9d62e765e7e4e0c8e8f96bd9aaba72f83ee394cddc70cb5ae2e489e941806fb8"
+    sha256 cellar: :any,                 arm64_sequoia: "c9418a200e643737d27d2545c869ccdc6452a5f995fc34e3e2855180739ef8d4"
+    sha256 cellar: :any,                 arm64_sonoma:  "f83a269042439e5bb1fbceb45ac55941798892429c614d6b151c8ff78e261997"
+    sha256 cellar: :any,                 sequoia:       "c0b3466e937f7a09d0e16c4b1f9e557923458845870afee60063956de662e8df"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5de99403b07b99e6dbdac2653d0d5cd2e8cfbe4e71458c41864402e30732db75"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1b7ab2fd9551471f591e47e8050a6568023f1b2e6cc80e8358d3b9ff1fa23cdb"
+  end
+
   def install
     inreplace "makefile", "libunrar.so", "libunrar.dylib" if OS.mac?
 
