a37c531765487716c6624cbfd76de615c4333086
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 03:24:25 +0000

    nanobrew: add 0.1.192 bottle.

--- Formula/n/nanobrew.rb
+++ Formula/n/nanobrew.rb
@@ -6,6 +6,15 @@ class Nanobrew < Formula
   license "Apache-2.0"
   head "https://github.com/justrach/nanobrew.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 arm64_tahoe:   "593f975132410a47b5812b8104e799ded00881f9547a791e1408f2deb0ddc172"
+    sha256 arm64_sequoia: "f5b68c04a709e785f9a195ffec6b8d9a502dcff8978361e7422b8b7afebd2e2c"
+    sha256 arm64_sonoma:  "5562ccc8b7711e58d684e042c6dc8c61f18a102f43e3e9b32cce2f3abd88635f"
+    sha256 arm64_linux:   "81931a31941e1502662577216f2e2c59d49ddd0c5fe49fcd30ae93df1bb122ab"
+    sha256 x86_64_linux:  "2da22b22a8b15d26e106c2d407c4d30d2653d66f8a7185ac9b7baafade40282c"
+  end
+
   depends_on "zig" => :build
 
   conflicts_with "nb", because: "both install `nb` binaries"
