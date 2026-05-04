e896be930febb44616451c0ba3147ad2f9437778
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 15:44:17 +0000

    pam: update 0.4.0-beta bottle.

--- Formula/p/pam.rb
+++ Formula/p/pam.rb
@@ -13,11 +13,11 @@ class Pam < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5371b035cff26bb7f019a16114e4427219f053becd1aad5635fc86249f5dd1c6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2a5bb0421f7671d2ee4897a2f7fb25179a38b4f727151301b81c34d7e8521826"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f209ee20577b45d96acf15a8b1f6df920830491aff4e368eb7b6adf828a18aae"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "bffd043d4c244bdba3eb1bb19977f29399b76926ac369ae394fc7d2963afa2bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "69f44376666d2c2f23fea37f7db03a78da660383b7eb3808fea4b385a448c766"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e700fe33c47cee1ce2531eaf578db08740c9eceb0ef4ac8ba5d2a3ffd0129b36"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "36aa69235b220cdfb8d68a53a41698f826e57311e6922119bc836d0e51fb8dc5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "23b8137e43f3d412b86c7ff1f0a0acfa8be18a4903c07604d0f2cd3d259bf1bb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5ca5022640df3c0bc703dcaacd25b9aa201d39dbc2c3a4a416fd3d827d8ef236"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c9c4d322993b737dcb020e6a0f794e53dd5a4afb6ab49a968fa7c301b8d8132"
   end
 
   depends_on "go" => :build
