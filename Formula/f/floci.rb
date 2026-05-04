a7caf1d81e544102bd9f396ae0d7bee5fef1a704
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 07:26:20 +0000

    floci 1.5.12
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/f/floci.rb
+++ Formula/f/floci.rb
@@ -1,8 +1,8 @@
 class Floci < Formula
   desc "Open-source local AWS emulator"
   homepage "https://github.com/floci-io/floci"
-  url "https://github.com/floci-io/floci/archive/refs/tags/1.5.11.tar.gz"
-  sha256 "e0458e878b41fb0af31aa8bb16fd0cfd8463c37e4acaa1d3570f813f36b2f237"
+  url "https://github.com/floci-io/floci/archive/refs/tags/1.5.12.tar.gz"
+  sha256 "7edce9f19eb5c60df6e6a080ae7bbc2dfb3a6add3c4050abf65932ab27587ccd"
   license "MIT"
   head "https://github.com/floci-io/floci.git", branch: "main"
 
@@ -66,7 +66,8 @@ class Floci < Formula
       end
 
       output = shell_output("curl -fsS http://127.0.0.1:#{port}/_floci/health")
-      assert_match "\"edition\":\"floci-always-free\"", output
+      assert_match "\"edition\":\"community\"", output
+      assert_match "\"original_edition\":\"floci-always-free\"", output
       assert_match "\"version\":\"#{version}\"", output
       assert_match "\"s3\":\"running\"", output
     ensure
