23fadf9f37e2ac53947fe9c36366b00458b3b665
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-04 17:01:10 +0000

    sonarqube-lts: add 9.9.8.100196 bottle.

--- Formula/s/sonarqube-lts.rb
+++ Formula/s/sonarqube-lts.rb
@@ -5,6 +5,15 @@ class SonarqubeLts < Formula
   sha256 "07d9100c95e5c19f1785c0e9ffc7c8973ce3069a568d2500146a5111b6e966cd"
   license "LGPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "12fb3ed5c742093ada085bf9547cf231451b42b5bf1988dd9b46cefea896a625"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "12fb3ed5c742093ada085bf9547cf231451b42b5bf1988dd9b46cefea896a625"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "12fb3ed5c742093ada085bf9547cf231451b42b5bf1988dd9b46cefea896a625"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d03fdd3c53368ffdce97cddea22a35d260ff0459f3510d73f9eb9ddda850ba38"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d03fdd3c53368ffdce97cddea22a35d260ff0459f3510d73f9eb9ddda850ba38"
+  end
+
   # Upstream no longer provides a Community Build for LTA releases.
   # See: https://www.sonarsource.com/blog/better-free-sonarqube-experience/
   deprecate! date: "2025-03-19", because: :deprecated_upstream
