42d12230e855ead3f1e42dfd3230031b1cc203c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-05 19:13:38 +0000

    aube: update 1.9.0 bottle.

--- Formula/a/aube.rb
+++ Formula/a/aube.rb
@@ -8,11 +8,11 @@ class Aube < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f27d20fc2fa6234becc892ad27ff3e87090a20c6ad1430075514167128acb379"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "eb6008a5a03fb39738e427da567588c36392e37d9fa6a57fbf7c3e37af127112"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d64c439c6460c884392644e2224a578ea6d2cffe687df9e1ab024ac021106e22"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b4058a837b5011cd4b16fc6c633499660b33fa56860ad3274ea140b3892c21aa"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f45d47001b3055a7c3684c1e64bdcaffb3f399ababcb4cf0c2c17feb965eebf5"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "deab923dbfc5161247b14b46463369473cae0babec00ade30d2b14a5a0269da6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f20e751956e20c17df3aa89766025a0844f44bbb3e2c7f92ecdac31ba74a0f88"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eea95eabf3470bfcaa2a7231f6e8f40b6a9cd006b1e47e2d5d295a263d187127"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "430d685d4e845bc4794a0fddb79bf2d7abcec79d78c0a6dc46f627a1c7a25a16"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d709b5bc29f19e333d94db892d259c607e2a8bb25aa12484834aadd1bcd8731c"
   end
 
   depends_on "cmake" => :build
