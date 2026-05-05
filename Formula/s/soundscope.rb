45b816d62751aec17243358c0fa278296a94f4ef
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-05 21:10:52 +0000

    soundscope: update 1.9.1 bottle.

--- Formula/s/soundscope.rb
+++ Formula/s/soundscope.rb
@@ -8,11 +8,11 @@ class Soundscope < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9e3fc79f646a2958c70042a61f668b389719a6e3e64918db60f8681454b4827b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e52503d726859b92178edf43271f2c88ced5f726c0686d0fcc3f56a959a6108"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cebb339543e4f3f435974f8a8f7ffb1cd015d81f5b710b1a4e182bd4ff8afdc9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "9c65bd58844cda0da16e08a90d8d4348eaf84b0d90eca3b339d254f425f3fa7d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "241742c54aa96f22b3762a7a4328bde68c0862cdd2d896faf3dca0d116d652ec"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "30bf995ba8763dfd78434d13322750c148b8adb80b0287eb0a39828b9795d087"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c00cd6e9055f9248036e5ff0110b0ed131f42772ff1354428e0f4155cda8158c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "70b7ae2b5269f0564eb2ece5957546e242d3c68fdb70409a38dc3cb63d26369e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "2e09ce3feb5b4e6c6643cda27ca936e6ad7b5dcf764c744859732cb6bbafdf61"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "676d6e4b6164cc6148320a161c832a89d2fbeef33101303bb0a0261c01a20e9f"
   end
 
   depends_on "pkgconf" => :build
