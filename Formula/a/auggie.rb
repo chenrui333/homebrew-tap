ac80a15fdeae8d5b68890dec8a30d40f075dd4c3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-05 03:36:58 +0000

    auggie: update 0.25.2 bottle.

--- Formula/a/auggie.rb
+++ Formula/a/auggie.rb
@@ -7,7 +7,7 @@ class Auggie < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "021af2a1ca32e65c31d7db546cb45ab03cd105b168775571b3b9aa5bb0c06270"
+    sha256 cellar: :any_skip_relocation, all: "fa3cf2101c7a7d23d29274a45a12fe88032b1bc2f5d815c17badab00c460d822"
   end
 
   depends_on "node"
