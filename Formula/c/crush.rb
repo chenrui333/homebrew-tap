bc486324e8c32b9e90ab03669a54cf298194aa98
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-06 21:07:59 +0000

    crush: update 0.66.0 bottle.

--- Formula/c/crush.rb
+++ Formula/c/crush.rb
@@ -8,11 +8,11 @@ class Crush < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fdc9e3f85c479134348408c3d33584d8ca9224d6eaf941b532c870b5bac680bb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e412fa1a4975def06b0d89fd5f29d263104ab4caeb60cd561192c25c395c54cf"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8072fc30db8c9dda320f7e324ac36b2c67838294b36e4d0f4f70f67d7b0c7dc3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3886c8d3faeefe961f3b28c8eae36ae49f33f6c328c4a56a25f025ae8ba0982c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8af0b1404e42fde4f8a961bf3de29bfb0b14729ee5b6e27a99e52e69267b941e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ac2e2de193dfd284c1c5fab566b637a5db2cd6f0d95772af8bb9432f6ff4c519"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "04b038bac08e8de274175248d81ade94963e768d27a2ca2b236d851b848a0ea7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f602abe140c4270cb37894577b05e6f2c509a200c16c65595d224d88c8fdc421"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1efc8e7c79c63003c7c5c011090cfb41131d43c8077f09624979b6ab76164076"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "70b55ed4e9545b51ae7e2853428af9d397be20a7646692ef71120f5a042d6f29"
   end
 
   depends_on "go" => :build
