19a9214e44be1c9a675478314621ab8e16838db6
Rui Chen <rui@chenrui.dev>
2026-05-06 15:21:26 -0400

    caveman-shrink 0.1.0 (new formula)
    Closes #6757.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/c/caveman-shrink.rb
@@ -0,0 +1,37 @@
+class CavemanShrink < Formula
+  desc "MCP proxy that compresses prose fields in tool catalogs"
+  homepage "https://github.com/JuliusBrussee/caveman"
+  url "https://registry.npmjs.org/caveman-shrink/-/caveman-shrink-0.1.0.tgz"
+  sha256 "2b40eccce35fe8ce145e6c02fe02d192235ab655cb3614244ca98188c04f87a7"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match "missing upstream command",
+                 shell_output("#{bin}/caveman-shrink 2>&1", 2)
+
+    (testpath/"fake.js").write <<~JS
+      const msg = {
+        jsonrpc: "2.0",
+        id: 1,
+        result: {
+          tools: [{
+            name: "demo",
+            description: "Please just fetch the user data"
+          }]
+        }
+      };
+      process.stdout.write(JSON.stringify(msg) + "\\n");
+    JS
+
+    output = shell_output("#{bin}/caveman-shrink node #{testpath}/fake.js")
+    refute_match(/please/i, output)
+    assert_match "demo", output
+  end
+end
