8953b6e147eb56703c3d6c2893df7a8f2a8505a0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-09 05:41:42 +0000

    comchan: update 0.3.6 bottle.

--- Formula/c/comchan.rb
+++ Formula/c/comchan.rb
@@ -8,11 +8,11 @@ class Comchan < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9dc30343c035d76586e46e1a061970eae0d874434cd9763aa11493e6a3dace85"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3feac5a9c48481d563ff98c921586013943b24e1fd79b9dcd69ad3ef1e686374"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a7bbd0ec3c4869b404258632d327ae79f50bd9b1b9e481532dd13c42bbda282f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "405407c63ebe5c7ac1f57760d9f982451f91fdfe7a4727bcb4757752b043fc23"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "091773d0a93d258fc9ca70fd221551d490dd0ecba55ee9f2e604dad1f2304868"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0293019d36568f79c50f3b23b69580511980d4381135f5bf3ec0e1104ef39cf0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "16d0112e6f40ee312c3f9a42679668b524307881b0bb249bc74777bd9ba93f53"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cb3da41f3d38a407713b05b3629e7012055513ba92a6a44e8c046fef6055f33e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "9b513e2c60fc5f192608f2f687104e5f239978ae8fe4e773c723a62fc2b4083a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78972aae2e82693364c56cc0bdf7685aa053c2fb3546f962637b07ed9ab0852d"
   end
 
   depends_on "pkgconf" => :build
