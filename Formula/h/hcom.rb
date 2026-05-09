52467d9cadbb1229d253a640c7f3a2395cfc4bea
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-09 03:45:13 +0000

    hcom: update 0.7.16 bottle.

--- Formula/h/hcom.rb
+++ Formula/h/hcom.rb
@@ -7,11 +7,11 @@ class Hcom < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2abc6acecd80f5490cb4d2947c3a6e09203e5f5f882ee6fa6f967d34d0db524f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1a0e7573b4d3c092d9604d37c27c971832687807a06a00bced47a5204efda896"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e867a4b882623d694085f6e1bd590f0d25e197c107464c5c9be1a87ab749ef5c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a294b1683b801023e2f48f961899495043e9a3e54e6ba1227de0da3ffdf0b0f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5859819efa80c287d621d7161f5c59f3ff33e61917aad072d3f63de0637ec7a1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e9dd94db05d9dec3869e4678f127ca2221fa73848b02f1df735e7f326815a29f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0edc67eec2dd87df0d4098eae87d20e3951aa768b616ebbd10bb3aaa324bfb42"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ef6871b363106472445c40d7116b9d4366c78c6a5a6b40ee674a0582357d144d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "15f2c0a201cab501350e762f03f89bf3cf6f6f731fb479777942c82c36fb5193"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "331f645829d409a33244ef6493bd7d3333bf820368c16ee2b7007d4710c558ed"
   end
 
   depends_on "rust" => :build
