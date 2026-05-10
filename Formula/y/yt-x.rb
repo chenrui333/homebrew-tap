dfadfe71c5535907206403b1469602c2d81aa457
Rui Chen <rui@chenrui.dev>
2026-05-10 15:44:54 -0400

    yt-x: fix Linux test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/y/yt-x.rb
+++ Formula/y/yt-x.rb
@@ -72,7 +72,6 @@ class YtX < Formula
     assert_match "Version=#{version}", desktop_entry
 
     assert_path_exists testpath/"config/yt-x/config"
-    assert_path_exists testpath/"Videos/yt-x"
 
     version_output, status = Open3.capture2e(env, bin/"yt-x", "--version")
     assert_predicate status, :success?
