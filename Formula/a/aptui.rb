5955274d0def293731b71cfe2d9cbeefef5f6853
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-10 14:48:23 +0000

    aptui: update 0.8.1 bottle.

--- Formula/a/aptui.rb
+++ Formula/a/aptui.rb
@@ -8,8 +8,8 @@ class Aptui < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "05fb300a40eada9f1bba57b2c93ec9f00a80492763ea1678e7d42cd2200576d8"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "007a38c54bc4261f6d3e96adc93ecaadca234fa4dbf4a994b70ec0d61f077a50"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "1aae729fd5d43a4b1993ef1f7deefe5b18fa68f817ece14b5fb2d44ba958a1bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "957594f65512f6810b71e5ccf956eff497e07e5910601d2db5e36b95f33c0746"
   end
 
   depends_on "go" => :build
