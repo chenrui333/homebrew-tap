81a49f293c9ee758ff37628289a53791e5cd2f41
Rui Chen <rui@chenrui.dev>
2026-05-10 13:57:31 -0400

    yt-x: fix 0.5.0 build
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/y/yt-x.rb
+++ Formula/y/yt-x.rb
@@ -12,7 +12,6 @@ class YtX < Formula
     sha256 cellar: :any_skip_relocation, all: "67143502e7fca4dfa44e049963721608fc2b2e94ef2f2f967a38b40e43fab63e"
   end
 
-  depends_on "bash"
   depends_on "ffmpeg"
   depends_on "fzf"
   depends_on "jq"
@@ -20,56 +19,61 @@ class YtX < Formula
   depends_on "yt-dlp"
 
   def install
-    inreplace "yt-x", "#!/usr/bin/env bash", "#!#{Formula["bash"].opt_bin}/bash"
-    inreplace "yt-x", 'CLI_VERSION="0.4.0"', %Q(CLI_VERSION="#{version}")
+    inreplace "yt-x", /^CLI_VERSION=.*/, %Q(CLI_VERSION="#{version}")
 
     inreplace "yt-x",
-              'CLI_EXTENSION_DIR="$CLI_CONFIG_DIR/extensions"',
+              'CLI_EXTENSIONS_DIR="$CLI_CONFIG_DIR/extensions"',
               <<~EOS.chomp
-                CLI_EXTENSION_DIR="$CLI_CONFIG_DIR/extensions"
-                CLI_BUNDLED_EXTENSION_DIR="#{pkgshare}/extensions"
+                CLI_EXTENSIONS_DIR="$CLI_CONFIG_DIR/extensions"
+                CLI_BUNDLED_EXTENSIONS_DIR="#{pkgshare}/extensions"
               EOS
 
     inreplace "yt-x",
-              '[ -s "$CLI_EXTENSION_DIR/$ext" ] && . "$CLI_EXTENSION_DIR/$ext"',
+              '[ -s "$CLI_EXTENSIONS_DIR/$ext" ] && . "$CLI_EXTENSIONS_DIR/$ext"',
               <<~EOS.chomp
-                if [ -s "$CLI_EXTENSION_DIR/$ext" ]; then
-                  . "$CLI_EXTENSION_DIR/$ext"
-                elif [ -s "$CLI_BUNDLED_EXTENSION_DIR/$ext" ]; then
-                  . "$CLI_BUNDLED_EXTENSION_DIR/$ext"
+                if [ -s "$CLI_EXTENSIONS_DIR/$ext" ]; then
+                  . "$CLI_EXTENSIONS_DIR/$ext"
+                elif [ -s "$CLI_BUNDLED_EXTENSIONS_DIR/$ext" ]; then
+                  . "$CLI_BUNDLED_EXTENSIONS_DIR/$ext"
                 fi
               EOS
 
     old_extension_dispatch = [
-      "  -x | --extension)",
-      "    [ -n \"$2\" ] || usage 1",
-      "    . \"$CLI_EXTENSION_DIR/$2\"",
-      "    shift",
+      "    -x | --extensions)",
+      "      [ -n \"$2\" ] || _app_usage 1",
+      "      if [ \"\${2#/}\" != \"$2\" ]; then",
+      "        . \"$2\"",
+      "      else",
+      "        . \"$CLI_EXTENSIONS_DIR/$2\"",
+      "      fi",
+      "",
+      "      shift_count=2",
       "    ;;",
       "",
     ].join("\n")
     new_extension_dispatch = [
-      "  -x | --extension)",
-      "    [ -n \"$2\" ] || usage 1",
-      "    if [ \"${2#/}\" != \"$2\" ]; then",
-      "      . \"$2\"",
-      "    elif [ -s \"$CLI_EXTENSION_DIR/$2\" ]; then",
-      "      . \"$CLI_EXTENSION_DIR/$2\"",
-      "    elif [ -s \"$CLI_BUNDLED_EXTENSION_DIR/$2\" ]; then",
-      "      . \"$CLI_BUNDLED_EXTENSION_DIR/$2\"",
-      "    else",
-      "      echo \"Extension '$2' not found in $CLI_EXTENSION_DIR or $CLI_BUNDLED_EXTENSION_DIR\" >&2",
-      "      exit 1",
-      "    fi",
-      "    shift",
+      "    -x | --extensions)",
+      "      [ -n \"$2\" ] || _app_usage 1",
+      "      if [ \"\${2#/}\" != \"$2\" ]; then",
+      "        . \"$2\"",
+      "      elif [ -s \"$CLI_EXTENSIONS_DIR/$2\" ]; then",
+      "        . \"$CLI_EXTENSIONS_DIR/$2\"",
+      "      elif [ -s \"$CLI_BUNDLED_EXTENSIONS_DIR/$2\" ]; then",
+      "        . \"$CLI_BUNDLED_EXTENSIONS_DIR/$2\"",
+      "      else",
+      "        echo \"Extension '$2' not found in $CLI_EXTENSIONS_DIR or $CLI_BUNDLED_EXTENSIONS_DIR\" >&2",
+      "        exit 1",
+      "      fi",
+      "",
+      "      shift_count=2",
       "    ;;",
       "",
     ].join("\n")
     inreplace "yt-x", old_extension_dispatch, new_extension_dispatch
 
     inreplace "yt-x",
-              '--arguments \"(command ls $CLI_EXTENSION_DIR)\"',
-              '--arguments \"(command ls $CLI_BUNDLED_EXTENSION_DIR)\"'
+              'ext_dir="$HOME/.config/$CLI_NAME/extensions"',
+              "ext_dir=\"#{pkgshare}/extensions\""
 
     libexec.install "yt-x"
     pkgshare.install "extensions"
@@ -98,13 +102,13 @@ class YtX < Formula
       "XDG_CONFIG_HOME" => (testpath/"config").to_s,
     }
 
-    desktop_entry, status = Open3.capture2e(env, bin/"yt-x", "-x", "example", "--generate-desktop-entry")
+    desktop_entry, status = Open3.capture2e(env, bin/"yt-x", "--generate-desktop-entry", stdin_data: "\n")
     assert_predicate status, :success?
     assert_match "[Desktop Entry]", desktop_entry
     assert_match "Name=yt-x", desktop_entry
-    assert_match "version=#{version}", desktop_entry
+    assert_match "Version=#{version}", desktop_entry
 
-    assert_path_exists testpath/".config/yt-x/yt-x.conf"
+    assert_path_exists testpath/".config/yt-x/config"
     assert_path_exists testpath/"Videos/yt-x"
 
     fish_completion, status = Open3.capture2e(env, bin/"yt-x", "completions", "--fish")
