89905b3ae34c27b9bf64ccbb7d9ead425a7092cc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-11 15:33:47 +0000

    rgx 0.12.2

--- Formula/r/rgx.rb
+++ Formula/r/rgx.rb
@@ -1,8 +1,8 @@
 class Rgx < Formula
   desc "Terminal regex tester with real-time matching and multi-engine support"
   homepage "https://github.com/brevity1swos/rgx"
-  url "https://github.com/brevity1swos/rgx/archive/refs/tags/v0.12.1.tar.gz"
-  sha256 "0598513279859e2832272d5a3c7f5961a334c59dacf649a9d180a3fb79c53b71"
+  url "https://github.com/brevity1swos/rgx/archive/refs/tags/v0.12.2.tar.gz"
+  sha256 "0b3f7db478d5b3c83e3e51c671b42d339fa48dc23d94d2ef0e87f51dd6c98b9d"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/brevity1swos/rgx.git", branch: "main"
 
