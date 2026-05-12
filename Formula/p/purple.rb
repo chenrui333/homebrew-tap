9aa66d58041acaf5d1005a1d48f7a53bca99a3c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 07:56:35 +0000

    purple: add 3.11.0 bottle.

--- Formula/p/purple.rb
+++ Formula/p/purple.rb
@@ -6,6 +6,15 @@ class Purple < Formula
   license "MIT"
   head "https://github.com/erickochen/purple.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ef10293860edfbae93b62940dc248710a658f292183db39460a37458c17bb2b5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ff7b0a4d7d9935f7da717d6162d3a40d07fbc0a4ee04a13dd2a26686b9b7cf6b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b4577a3219b2080b1fb7b1c7b7c0a8665bff05474655133ed27a5a28166ae527"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0dbb641cfd25a542c8d6a3aa0132791cde91aa2b34c518569d5a03303c7017bd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83f9de3f383f9149ad0dc965b34fa1c662d34e8d2de91d699ab54605db08a370"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 
