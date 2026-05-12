0dfcc2ce4e01ba9f1973bb65aec5bc49c04e36a5
Rui Chen <rui@chenrui.dev>
2026-05-12 02:06:44 -0400

    fix: update sha256 checksum
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/r/rustlens.rb
+++ Formula/r/rustlens.rb
@@ -2,7 +2,7 @@ class Rustlens < Formula
   desc "Blazing-fast Rust code inspector for the terminal"
   homepage "https://github.com/yashksaini-coder/Rustlens"
   url "https://github.com/yashksaini-coder/Rustlens/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "b5dee233fc8f792b0e9577771d2264b7bdc918a7176cf4689ef866c3e963d94d"
+  sha256 "3c5729600ad797b1e9e69cae57ba3b60a617bec3f30c27c975ef9d32b70052e1"
   license "MIT"
   head "https://github.com/yashksaini-coder/Rustlens.git", branch: "main"
 
