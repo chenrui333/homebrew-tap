84d3ed98724912cf582ad96c3508ae3c515d9b39
Rui Chen <rui@chenrui.dev>
2026-05-12 02:06:45 -0400

    fix: update sha256 checksum
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/u/unifly.rb
+++ Formula/u/unifly.rb
@@ -2,7 +2,7 @@ class Unifly < Formula
   desc "CLI/TUI for UniFi network controller management"
   homepage "https://github.com/hyperb1iss/unifly"
   url "https://github.com/hyperb1iss/unifly/archive/refs/tags/v0.9.1.tar.gz"
-  sha256 "7c0742ca5a9b6a8e80ebcd7741acdc86ce23bf3d48eabbc03fd387b25ee4b1b9"
+  sha256 "34a5c73a548270f670b9458f07da3ba1a94f1b9c31831fd4c433d6e01d561330"
   license "Apache-2.0"
   head "https://github.com/hyperb1iss/unifly.git", branch: "main"
 
