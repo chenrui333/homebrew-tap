dbcaeb0dbf3e1345db3ac4c428746c3bcc259880
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 07:13:37 +0000

    gitsocial: add 0.10.2 bottle.

--- Formula/g/gitsocial.rb
+++ Formula/g/gitsocial.rb
@@ -6,6 +6,15 @@ class Gitsocial < Formula
   license "MIT"
   head "https://github.com/gitsocial-org/gitsocial.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5e77d6ebcd98db2e29a1af493f04643a75dd18d13b630643bf1b65e0036bc5fb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5e77d6ebcd98db2e29a1af493f04643a75dd18d13b630643bf1b65e0036bc5fb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5e77d6ebcd98db2e29a1af493f04643a75dd18d13b630643bf1b65e0036bc5fb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e41f692d929955214e490a4dfc3a46585b7e37259fd751acda8ab5553b49edd0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "52bbc47c6de92b2efcd3b24e9b0368a3b23b7fdaa4568b817848dd3281a2d1f9"
+  end
+
   depends_on "go" => :build
 
   def install
