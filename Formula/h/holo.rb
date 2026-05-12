2e73e2659c4a27c7c1f6499566f30135e66078b2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 07:55:26 +0000

    holo: add 0.3.0 bottle.

--- Formula/h/holo.rb
+++ Formula/h/holo.rb
@@ -6,6 +6,15 @@ class Holo < Formula
   license "MIT"
   head "https://github.com/measure-sh/holo.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cbea51ddae81e797c5bbe6faa40af08081ea6a2a25d6b665e8c8534fc484a801"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6155fcf3ce91f71267e25c84666e04ca9a82ada0a25a8b9043fdd22b42ee7ed3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "65cf1cc8f25faa0899ebf1bca72970f688c3120d83813a080d591a29d186f23c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4cc9cbe30aa781f2a247676591238340e374b0d381953b4d6104b929cd44d5bf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8a6377fcdc05304878f9b84be05cf92c942785a68b41ca87d02f09e61e79e896"
+  end
+
   depends_on "rust" => :build
 
   def install
