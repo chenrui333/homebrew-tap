f1c34e284a2da844ab70d9c1d25eb4e7f0130a7b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 08:02:57 +0000

    rustlens: add 0.2.1 bottle.

--- Formula/r/rustlens.rb
+++ Formula/r/rustlens.rb
@@ -6,6 +6,15 @@ class Rustlens < Formula
   license "MIT"
   head "https://github.com/yashksaini-coder/Rustlens.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "34aed5afde476e0002b058bc5b0c45f7a93cc8b096918f09018907ffc790c0ca"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e143879f105d4823091ab7fa0d3745f2d996947dd7924d942005ec98c9d09cfb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62385b001a81ae3088b37a5b39d4bf977f7d37c3fdd3f8b635bdd331cd802ae4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bc3b3e46876330c3561a55077296cb7ba2f0e9c6c71e2d752b2d63fac7a950ce"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "325cb04d59c8edfdb9788da3e8e926f516aa4d5bb7260ccef786c08e3e9c38af"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3"
 
