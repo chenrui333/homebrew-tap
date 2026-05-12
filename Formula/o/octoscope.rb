e5afe115aa7cdbe95968ece02275069075612e75
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 07:10:23 +0000

    octoscope: add 0.11.0 bottle.

--- Formula/o/octoscope.rb
+++ Formula/o/octoscope.rb
@@ -6,6 +6,15 @@ class Octoscope < Formula
   license "MIT"
   head "https://github.com/gfazioli/octoscope.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "824fa6ffa6e7fdebd7bf5f54eb8a51f3ab9c022312efc6f08edce513497496a0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "824fa6ffa6e7fdebd7bf5f54eb8a51f3ab9c022312efc6f08edce513497496a0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "824fa6ffa6e7fdebd7bf5f54eb8a51f3ab9c022312efc6f08edce513497496a0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "259601a0a04a86b32711e373cb9e38aef244ae809777d07a1f1138ed31dc16fa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "10ccca4a19296a8ee44a435257cc4a210e040b388f3250d6969b9513cc301d8d"
+  end
+
   depends_on "go" => :build
 
   def install
