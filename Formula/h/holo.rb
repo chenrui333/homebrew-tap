e6c9fb5e7c65114053a177dbe5e18ef615259102
Rui Chen <rui@chenrui.dev>
2026-05-12 02:06:39 -0400

    fix: update sha256 checksum
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/h/holo.rb
+++ Formula/h/holo.rb
@@ -2,7 +2,7 @@ class Holo < Formula
   desc "Terminal based profiler and app inspector for Android"
   homepage "https://github.com/measure-sh/holo"
   url "https://github.com/measure-sh/holo/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "9abcff896d59012ab74f91df6bf0a2ecc6f33d596d65970a4e1c49a451d595c0"
+  sha256 "fa6cdd87cad8d7336f991fb4d8d8f79164a04e1aaf47268c656e22f588cc113d"
   license "MIT"
   head "https://github.com/measure-sh/holo.git", branch: "main"
 
