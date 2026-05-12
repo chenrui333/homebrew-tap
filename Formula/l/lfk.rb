ccd0be50040b5fcbed12f2f55a964f188639b788
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 07:32:13 +0000

    lfk: add 0.11.2 bottle.

--- Formula/l/lfk.rb
+++ Formula/l/lfk.rb
@@ -6,6 +6,15 @@ class Lfk < Formula
   license "Apache-2.0"
   head "https://github.com/janosmiko/lfk.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "68574737ea37f0ad7d738bb3d8ebf66d5237f0408a55eada16497a34d27affb5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "68574737ea37f0ad7d738bb3d8ebf66d5237f0408a55eada16497a34d27affb5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "68574737ea37f0ad7d738bb3d8ebf66d5237f0408a55eada16497a34d27affb5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "11348632f95385d067ec1fe66194cd5daf0fda08c8b0c85fe4be25ad5b892e99"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "928b1e917def9eb331201c211e3ed29b20d63cdc00f845cd24e83d1655312c7e"
+  end
+
   depends_on "go" => :build
 
   def install
