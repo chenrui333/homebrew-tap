4282629d0864950ca7379f6c1a154da9d4a369d6
Rui Chen <rui@chenrui.dev>
2026-05-12 00:24:33 -0400

    lfk 0.11.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/l/lfk.rb
@@ -0,0 +1,22 @@
+class Lfk < Formula
+  desc "Lightning fast Kubernetes navigator"
+  homepage "https://github.com/janosmiko/lfk"
+  url "https://github.com/janosmiko/lfk/archive/refs/tags/v0.11.2.tar.gz"
+  sha256 "3b7645eca4815b5422f9661eeb1f262d612a01e106d045c041d2cb642c03cf5b"
+  license "Apache-2.0"
+  head "https://github.com/janosmiko/lfk.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = "-s -w -X github.com/janosmiko/lfk/internal/version.Version=#{version}"
+    system "go", "build", *std_go_args(ldflags:), "."
+
+    generate_completions_from_executable(bin/"lfk", "completion", shell_parameter_format: :cobra)
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lfk --version 2>&1")
+    assert_match "#compdef lfk", shell_output("#{bin}/lfk completion zsh")
+  end
+end
