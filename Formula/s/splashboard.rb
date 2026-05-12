21a459c2e93d88458bbfa74579ae51a0654d1868
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 07:58:03 +0000

    splashboard: add 1.5.0 bottle.

--- Formula/s/splashboard.rb
+++ Formula/s/splashboard.rb
@@ -6,6 +6,15 @@ class Splashboard < Formula
   license "ISC"
   head "https://github.com/unhappychoice/splashboard.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8d9d1c823cbee413ae9909c6cd2dd6b96002eb0ebaa1953a35554d4e774c5aff"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45b62fcc54585eabe86416bd6b996a21e83a86af901b1d11e474e0f6e962f617"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c7542769a734e6f6414852059454be30e2c20325e674fae12c5f42babb472eb3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "55eeec46095f6409ad9e9fc1ab64c0769e3323349d9d35077031f6dd8faa377b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23a485059d318cea288bb743f9825bb207b65c5d31038c79b34fecdbc37dc962"
+  end
+
   depends_on "rust" => :build
 
   def install
