9876788ab8e7c8a6a20f75f0dbb56d6ded02c726
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 03:43:19 +0000

    oh-my-codex: update 0.16.4 bottle.

--- Formula/o/oh-my-codex.rb
+++ Formula/o/oh-my-codex.rb
@@ -8,11 +8,11 @@ class OhMyCodex < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "62228dd5e1af2e9f4562c91788492536f32562c6006c6a1a0d38f63ff516c6a2"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "62228dd5e1af2e9f4562c91788492536f32562c6006c6a1a0d38f63ff516c6a2"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62228dd5e1af2e9f4562c91788492536f32562c6006c6a1a0d38f63ff516c6a2"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "915b4acdeef953663c1c6c745740ae4cd7bb84a95f403f2e0dd1be8d9364d9e4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "915b4acdeef953663c1c6c745740ae4cd7bb84a95f403f2e0dd1be8d9364d9e4"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "63453e91c47d6ed4b97a546c0e900069470686d361c8abb33bfe57cd98a487e2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "63453e91c47d6ed4b97a546c0e900069470686d361c8abb33bfe57cd98a487e2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "63453e91c47d6ed4b97a546c0e900069470686d361c8abb33bfe57cd98a487e2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "fa784c399bfb39f691276363dafb6806346281c2fec9f9c7d67e69040896fcb3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa784c399bfb39f691276363dafb6806346281c2fec9f9c7d67e69040896fcb3"
   end
 
   depends_on "node"
