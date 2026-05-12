0e98d50bfd2f043d9c36004dc9c30d11b9688449
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 05:36:14 +0000

    octelium 0.32.0

--- Formula/o/octelium.rb
+++ Formula/o/octelium.rb
@@ -1,8 +1,8 @@
 class Octelium < Formula
   desc "Next-gen FOSS zero-trust platform—self-hosted VPN, ZTNA, API gateway & homelab"
   homepage "https://octelium.com/docs/octelium/latest/overview/intro"
-  url "https://github.com/octelium/octelium/archive/refs/tags/v0.31.0.tar.gz"
-  sha256 "70be5737908d08ea6214bfd3a49a7683b4c1b3f68a1ef141d81d7b04a78e6f09"
+  url "https://github.com/octelium/octelium/archive/refs/tags/v0.32.0.tar.gz"
+  sha256 "8b37d879de9b6c5210a642b6593d8a8829d0cd612803bc18462feb1098ac433e"
   license "Apache-2.0"
   head "https://github.com/octelium/octelium.git", branch: "main"
 
