ea9fc1566d4aed096524b6e7a131d418cc89edad
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 07:40:23 +0000

    threatdeck: add 0.2.0 bottle.

--- Formula/t/threatdeck.rb
+++ Formula/t/threatdeck.rb
@@ -6,6 +6,15 @@ class Threatdeck < Formula
   license "MIT"
   head "https://github.com/gripebomb/threatdeck.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3a4ab6b3025c81b1d38690c7ad598283494f974ef5900b522bf6c73b10e9918f"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "57088eba5405b13a339d9d5778ccbd893e5e8b8e19210168b2c8eb5c1f71d4ec"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5dd4ec32ed456efd5db5783e71313ac693db51a5b46640802459a64ebf813731"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "02ce2578bdefcf8df9195d4001ce2b06432278ff2478f4933ba11f762ab4ca92"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f97212d6dc7006457eaa3e170ea52b961e7f15161d773ff2b443a6fec9dd94f9"
+  end
+
   depends_on "rust" => :build
 
   def install
