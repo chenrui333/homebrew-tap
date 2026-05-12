3167a3278f97a9cc1cf80b14bc80df6ca0b675af
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 17:39:42 +0000

    splashboard: update 1.6.0 bottle.

--- Formula/s/splashboard.rb
+++ Formula/s/splashboard.rb
@@ -8,11 +8,11 @@ class Splashboard < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8d9d1c823cbee413ae9909c6cd2dd6b96002eb0ebaa1953a35554d4e774c5aff"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "45b62fcc54585eabe86416bd6b996a21e83a86af901b1d11e474e0f6e962f617"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c7542769a734e6f6414852059454be30e2c20325e674fae12c5f42babb472eb3"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "55eeec46095f6409ad9e9fc1ab64c0769e3323349d9d35077031f6dd8faa377b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "23a485059d318cea288bb743f9825bb207b65c5d31038c79b34fecdbc37dc962"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c4ceefc060a5f455307310f6d239753f80e384be3e4f68af98835c1f8bc0b003"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "de5446a3fc2ce98cda8109ec9cac387435672409c8798aa6ecbec4f66ca36105"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "82628e4e810c18dca711dc7722437e81adee3ee6403246a657e1a831b23e34b8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "bf69890e73656987b659e2f42f39d454a575cc494cb8c25783f75540b481d955"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "440112d16e73f1840746744c6bfd4aeaa80b80aa9f95d6ff3b3454daa0354cec"
   end
 
   depends_on "rust" => :build
