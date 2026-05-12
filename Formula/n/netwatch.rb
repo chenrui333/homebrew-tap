aa4a10863cce74428c14a874054e520aa74a2030
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-12 07:10:22 +0000

    netwatch: add 0.15.8 bottle.

--- Formula/n/netwatch.rb
+++ Formula/n/netwatch.rb
@@ -6,6 +6,15 @@ class Netwatch < Formula
   license "MIT"
   head "https://github.com/matthart1983/netwatch.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0908b68e444a04f520b6b3b4407c41252a9afe26142c286247c7b4b738cb64a4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b60ad2cb126f8abb4cd876ebe429516edf27480e8f2309b2c46caa9844cf961b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "eb4d203335ac13402b4bef6dd1c5b88cd20e39434dfb4455128ae821f88aba45"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c27682bcd2f649c96081b251174507d1bdbeaf85f543b1b1f8368c7cd0f1076f"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "29c13d8d5df18855d6f367bfa833860c20d2ee41d7728a657b2fb560d9f66e40"
+  end
+
   depends_on "rust" => :build
   uses_from_macos "libpcap"
 
