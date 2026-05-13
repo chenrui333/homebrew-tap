5da89b3bd7c5163be279a52da387463c35875e59
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-13 11:34:27 +0000

    namespace-cli: update 0.0.513 bottle.

--- Formula/n/namespace-cli.rb
+++ Formula/n/namespace-cli.rb
@@ -9,11 +9,11 @@ class NamespaceCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a3be995b0c4046d2a2c291adedf3e71711786ce8f4c1303267062ebe85c0c364"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "417d06405ee4aae643721c2dd018287bc2ceb9ab6c719371a8ea88c52bd454bd"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b8ef3a6b3c703b514970093ed75c2eaa8b54dc2483f90d0b896687e21d1a376d"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "773134a090fdd83bdc1a849baf937ceea4d16d2d3f8d336469c848ad67ab46da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fa2e1612d2c280fcedf2326f7e013f7026f7cbdd4702a723bd0ac589cfc8818d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d602ded14307b84eb90647385f77d1ea988f5e7237d656a505af4aa6492831f0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f6ac5514f7e28adac74f6e309824d8cd88fd7b5310ca14e749e85d4033365d73"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f1f01ca1c04cb34d736e30fa7dbce917d4219ee09d1692b3d47ad571c1edb261"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0accd74784c48d8eb124926f39687a07e4f6918d3c87e008f8ec00cd17d141e2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "cafeebb1f8f890f03241f56ff3caac8cdf3a839d305fcbe6863b19e7dbfa68e3"
   end
 
   depends_on "go" => :build
