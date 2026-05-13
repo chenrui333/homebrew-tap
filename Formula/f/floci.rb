dc91a6e74d2938848132d17e1192e50f0142815b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-13 07:25:43 +0000

    floci 1.5.15
    Closes #6963.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/f/floci.rb
+++ Formula/f/floci.rb
@@ -1,8 +1,8 @@
 class Floci < Formula
   desc "Open-source local AWS emulator"
   homepage "https://github.com/floci-io/floci"
-  url "https://github.com/floci-io/floci/archive/refs/tags/1.5.14.tar.gz"
-  sha256 "6c7382e7776abbee6dd8833d31942fc65ea11e6f52d5ecd307c011456bf8d660"
+  url "https://github.com/floci-io/floci/archive/refs/tags/1.5.15.tar.gz"
+  sha256 "db0c24f038e8d7c5110d439c1c45bbbfa8bd2aba2d51219b3a8ce3d899e6d71b"
   license "MIT"
   head "https://github.com/floci-io/floci.git", branch: "main"
 
