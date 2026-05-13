1da3a697729c84169e4e870f6b304d29ed0ce5dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-13 11:14:19 +0000

    namespace-cli 0.0.513

--- Formula/n/namespace-cli.rb
+++ Formula/n/namespace-cli.rb
@@ -2,8 +2,8 @@ class NamespaceCli < Formula
   desc "Command-line interface for the Namespaces platform"
   homepage "https://github.com/namespacelabs/foundation"
   url "https://github.com/namespacelabs/foundation.git",
-      tag:      "v0.0.512",
-      revision: "a0711b29967c56502db9ea775cfe7d773c6a1056"
+      tag:      "v0.0.513",
+      revision: "507d22be7e32458130f8dbe042aa041f09b70c35"
   license "Apache-2.0"
   head "https://github.com/HarishChandran3304/better-env.git", branch: "main"
 
