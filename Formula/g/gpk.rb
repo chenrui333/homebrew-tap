c9722e1cffd587b7c5154b80d35dc0e19cc1c02c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-15 01:28:10 +0000

    gpk: update 0.5.2 bottle.

--- Formula/g/gpk.rb
+++ Formula/g/gpk.rb
@@ -8,11 +8,11 @@ class Gpk < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3cb46107784f8001cb1b0e1646e5b42fccfc87d37b0cea1b5c0b4cd981ed6859"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3cb46107784f8001cb1b0e1646e5b42fccfc87d37b0cea1b5c0b4cd981ed6859"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3cb46107784f8001cb1b0e1646e5b42fccfc87d37b0cea1b5c0b4cd981ed6859"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8b9406f7285d809b99fed08d9641cbb71ac1faa83ce241b844ae92113e9dcd22"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4db8dfce3fa571a694f095b4672e1e6bae0a50a6058b2d1631e7bc63619c8f66"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cdc7806163c41fdda460d5a4296f1b59b3e2bbedd4e1aa4269b5e8cb3b7e67bd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cdc7806163c41fdda460d5a4296f1b59b3e2bbedd4e1aa4269b5e8cb3b7e67bd"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cdc7806163c41fdda460d5a4296f1b59b3e2bbedd4e1aa4269b5e8cb3b7e67bd"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "8e4b188b461386d471ce0d3d3e26106444389ac22ca5dd0e9cfa80ef944ead70"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2139e7770ed91bb94aa5437ebd8fe9ae898e3e081e8b6be7857d0278d77e272d"
   end
 
   depends_on "go" => :build
