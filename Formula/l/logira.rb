09745552b56e1b123348043c74ac0918ff6b5654
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-16 13:55:10 +0000

    logira: add 0.1.0 bottle.

--- Formula/l/logira.rb
+++ Formula/l/logira.rb
@@ -6,6 +6,12 @@ class Logira < Formula
   license "Apache-2.0"
   head "https://github.com/melonattacker/logira.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "b3fb4d93c7e07c1558e60f0f646dd9d9a0d092f3902650bbe1fc33ca7a141b73"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "f8d9ad278946b654f09fcff70d3d4df7a7bf68315f768e5642fb517ba0d1787d"
+  end
+
   depends_on "go" => :build
   depends_on :linux
 
