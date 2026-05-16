8729cd03d3cd1f1830c0aa7d93b33dad198b912b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-16 13:57:47 +0000

    sakimori: add 0.34.4 bottle.

--- Formula/s/sakimori.rb
+++ Formula/s/sakimori.rb
@@ -6,6 +6,15 @@ class Sakimori < Formula
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/bokuweb/sakimori.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c8969c98b032ebde73e609a44cf8e098fed0d76cc054927a36fc1e65da3dfa98"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7b5de630fda4f844f7f6f1029a3f9f048e1d8bbe87ec8da624167361c8ff09ba"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f1cd7b0a3d03b6963b4c557c90a91c4b69df22f8c03d8ec496e49a38e659037c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "42b2920900a823039ddde5393fa4badb6bf4b59f9d92b27d888f0cbfcf445b06"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ab34e78a6471bc20c8a46472a04133b27da021583c201e473bb328428def811"
+  end
+
   depends_on "rust" => :build
 
   def install
