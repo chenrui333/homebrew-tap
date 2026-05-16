8f3f0a911363d11af36873357154217510cf7231
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-16 15:24:27 +0000

    zerolang: add 0.1.1 bottle.

--- Formula/z/zerolang.rb
+++ Formula/z/zerolang.rb
@@ -6,6 +6,16 @@ class Zerolang < Formula
   license :cannot_represent
   head "https://github.com/vercel-labs/zero.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ff8adbbfdfea7a03ba7a5308198a0e1765f1055be57990e4dd97c265e5b94a43"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9321c31899ca828c4aa02315165ca4ab731c4bd6381d4c7dcc4a5c7370b3494a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c3e0bf1c053d78c2da0a9609f2a8a6da64a255283dc6f750627dc6a789a2612e"
+    sha256 cellar: :any_skip_relocation, sequoia:       "37b5778672a144a4ac1ebc16f92e7893090b71238c147edf16fe25eaa9cd9bf8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "98a6343c0d6017e915881db809754c392d47f234877ac95250d23e9865b85caf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f8bcc12689becf55eac631fe54efa2fc33309717904afca8b4b2a0605c60f3c"
+  end
+
   def install
     bin.mkpath
     cd "native/zero-c" do
