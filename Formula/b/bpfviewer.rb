b7cf3c825b3ee77971d370049645f509e90a5fc5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-16 14:00:39 +0000

    bpfviewer: add 0.1.2 bottle.

--- Formula/b/bpfviewer.rb
+++ Formula/b/bpfviewer.rb
@@ -6,6 +6,15 @@ class Bpfviewer < Formula
   license any_of: ["Apache-2.0", "MulanPSL-2.0"]
   head "https://github.com/tsint/bpfviewer.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c429b3f7b50928c8daaa3fea9932117341b74d367b55de6230fce4e19c3718d4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c429b3f7b50928c8daaa3fea9932117341b74d367b55de6230fce4e19c3718d4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c429b3f7b50928c8daaa3fea9932117341b74d367b55de6230fce4e19c3718d4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "98c62f89751433e7f9424042812ef36dafba512a9ee735ada2b19b5d9f1be2ad"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3656932dd19a918e0c1ef1bdf9f4e48881c06124c4f4666b7167a767a5b407e9"
+  end
+
   depends_on "go" => :build
 
   def install
