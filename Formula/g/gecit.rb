eb5213e7c54f47fdf105f77feccd6bad42f73bd1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-16 14:19:11 +0000

    gecit: add 0.1.4 bottle.

--- Formula/g/gecit.rb
+++ Formula/g/gecit.rb
@@ -6,6 +6,12 @@ class Gecit < Formula
   license "GPL-3.0-only"
   head "https://github.com/boratanrikulu/gecit.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "6465cfffb10d0a0f6c4b31738ee7507ef2fe82bdbc2c0d8fdb90244c8e038aa2"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "9cf58d16d09f44c3d0ac06cd6bb3e7813cbb35d33c34c609bcd273c817b0eec7"
+  end
+
   depends_on "go" => :build
   depends_on :linux
 
