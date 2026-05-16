bd61e742f501455f9eabd7bc4a4a5325899d7656
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-16 20:34:55 +0000

    gitsocial 0.10.4
    Closes #7103.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/gitsocial.rb
+++ Formula/g/gitsocial.rb
@@ -1,8 +1,8 @@
 class Gitsocial < Formula
   desc "Git-native cross-forge collaboration platform"
   homepage "https://github.com/gitsocial-org/gitsocial"
-  url "https://github.com/gitsocial-org/gitsocial/archive/refs/tags/v0.10.3.tar.gz"
-  sha256 "f271e8782b95e0050a57ceb0222f30e66550a49cf9b0d7162dd7d4eb7875ded2"
+  url "https://github.com/gitsocial-org/gitsocial/archive/refs/tags/v0.10.4.tar.gz"
+  sha256 "1f80ccde31e4f29b4e96e0f8ab9c6ad26937b4e32147529800d8a348e1573d27"
   license "MIT"
   head "https://github.com/gitsocial-org/gitsocial.git", branch: "main"
 
