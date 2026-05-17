a5b4e9d684e254c97e78a9e6463b52416f4fd8e5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-17 10:49:20 +0000

    lazyenv 0.8.0
    Closes #7123.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/l/lazyenv.rb
+++ Formula/l/lazyenv.rb
@@ -1,8 +1,8 @@
 class Lazyenv < Formula
   desc "TUI tool for managing multiple .env files in the terminal"
   homepage "https://github.com/lazynop/lazyenv"
-  url "https://github.com/lazynop/lazyenv/archive/refs/tags/v0.7.1.tar.gz"
-  sha256 "8714222bfa945b4f331d29eb6b486b9cdf0d9538650d40445c0575478662f680"
+  url "https://github.com/lazynop/lazyenv/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "62498af6b30c4612b700e8dbc96bd8cbdb5cf951fbda3d624c5df74fe40a2131"
   license "MIT"
   head "https://github.com/lazynop/lazyenv.git", branch: "main"
 
