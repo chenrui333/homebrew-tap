358e5b6d314b451d4baef7597b5aeeabe149646e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-18 20:15:17 +0000

    zerolang: update 0.1.2 bottle.

--- Formula/z/zerolang.rb
+++ Formula/z/zerolang.rb
@@ -8,12 +8,13 @@ class Zerolang < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3b5466f298363ca8a278c76a723a6feb78747f7f508246c2d86c9daa2c8f698d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "62f6544019c4cfeaf792d1a4a1e47a0bf3a9baac57921bb4d7f814e0b9340bd3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d04c7de5cb1b6a9f3ae891c4d197fa30132e323aabb653d624661a12b905f1e4"
-    sha256 cellar: :any_skip_relocation, sequoia:       "d6c59c30cb310e6a19a630aba7832401295c6a6c1c40eaa31e60aee9bcebe2e1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8832ba3c8bba0662f5e3b7cfce2caa498c9634c6553d7747f132a889ea1a3c18"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "05e344faf31a0b4146a3fde53c0f81a1c74e7f7207624f3f5e3c1ee9a5ad7a72"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "05ad97889c643ebd6c4ad18b2dc1725a3b700f94dd518d030107f1b3f065ff44"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37cc59ba7a5d3b79ffa80ff6f48d45a8c71c6a0ed020eeb0a786dba520a73bc5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ca30e1c35d70370f8755d3140c3ecf858b6cd78bedbde5519aad2865a2762a7c"
+    sha256 cellar: :any_skip_relocation, sequoia:       "80ddbc3954655b48d1d75f1b46c6e5165769520879713f32e12d3c2ec449d8fa"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "683925ef21ea5bac7b445185cb97bfddb0993cb705f2dc12fd907866d52b89e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2e58a70f842db49ce029ecc2ba8a0157d186ed9b7158b8e88e4c429ac6a9a9a4"
   end
 
   def install
