e8ec9561e8eb2fc0130c75a76f8c51d99d29a1eb
Rui Chen <rui@chenrui.dev>
2026-05-18 22:23:05 -0400

    multica 0.3.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/m/multica.rb
@@ -0,0 +1,26 @@
+class Multica < Formula
+  desc "Open-source managed agents platform for AI coding agents"
+  homepage "https://github.com/multica-ai/multica"
+  url "https://github.com/multica-ai/multica/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "063ee3fdd6d5f4b16476d7b385ef1d4082140c803db0ec05c330ef77643202d3"
+  license :cannot_represent
+  head "https://github.com/multica-ai/multica.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    cd "server" do
+      ldflags = %W[
+        -s -w
+        -X main.version=#{version}
+        -X main.commit=#{tap.user}
+        -X main.date=#{time.iso8601}
+      ]
+      system "go", "build", *std_go_args(ldflags:), "./cmd/multica"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/multica version")
+  end
+end
