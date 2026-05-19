a5e5cba91ad4e7989fc4d1ce7bb051f6ec81ceb5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-19 17:18:06 +0000

    clickhousectl 0.2.2

--- Formula/c/clickhousectl.rb
+++ Formula/c/clickhousectl.rb
@@ -1,8 +1,8 @@
 class Clickhousectl < Formula
   desc "CLI for ClickHouse: local and cloud"
   homepage "https://github.com/ClickHouse/clickhousectl"
-  url "https://github.com/ClickHouse/clickhousectl/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "3fbc4ca413c76f022e6f79129d3e2d80ff74406082fa505709d724a99a344cd0"
+  url "https://github.com/ClickHouse/clickhousectl/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "677126470b828b471177a16ac519c61b5f185cd23683ef799e9030adca06a975"
   license "Apache-2.0"
   head "https://github.com/ClickHouse/clickhousectl.git", branch: "main"
 
