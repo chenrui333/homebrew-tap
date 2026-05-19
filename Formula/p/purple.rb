69c3617478e5a5502935788e7b3de4eb06bd07a3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-19 16:00:55 +0000

    purple: update 3.15.2 bottle.

--- Formula/p/purple.rb
+++ Formula/p/purple.rb
@@ -8,11 +8,11 @@ class Purple < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5fb138d4c1a2f8f16b0f38949276617edda80d9bebbf42ea2a7de15fdd7774f3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b7f78ab1b7cf471a59022656114f7fb91f644d9ff9bc0a237f2fbc506044db65"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "079f189baaa4108139070a44b3acbcf6bc2a34e510b04c297da34bdf57b64357"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3cce65818fe62d9d8a1c33ceb3934b1b5d6447d69d171cc3feca8e1f4b7c2908"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5881f5e7beca3d715cc044af860149f69b1d0fb2764d4740ebb7ca2f7baf5d6b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "003c5d2a7d76ba4d54a9993fb159e25d12df37e87841c9fd64c0c589b43b7968"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9a73eb3fe1d152cb39570f432703f4fe91eef148632755950e67ff4a5981d42e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "82c0245321260e4fea5fee73dc900a76ba324db8fe35c1973fb1f9c838d55e61"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "11f4e7508cc63915e172ad610cf7f7bd90370da08cdb6ea7974a1f76b66cf374"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "9b031eff3d83bbbe2171b419c9829a201f798b13be048b0caf6a7787f9bbc367"
   end
 
   depends_on "rust" => :build
