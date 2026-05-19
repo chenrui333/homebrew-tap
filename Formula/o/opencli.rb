4b3f66ee9b97a25a3a5373b9b2954591ac60b821
Rui Chen <rui@chenrui.dev>
2026-05-18 22:18:45 -0400

    opencli 1.7.22 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/o/opencli.rb
@@ -0,0 +1,20 @@
+class Opencli < Formula
+  desc "Make any website or Electron App your CLI, AI-powered"
+  homepage "https://github.com/jackwener/opencli"
+  url "https://registry.npmjs.org/@jackwener/opencli/-/opencli-1.7.22.tgz"
+  sha256 "8109a73b3033f47b3d8844caf23c6cbf542fada9ab8746641c3a27c7edfa798f"
+  license "Apache-2.0"
+  head "https://github.com/jackwener/opencli.git", branch: "main"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/opencli --version")
+    assert_match "opencli", shell_output("#{bin}/opencli --help")
+  end
+end
