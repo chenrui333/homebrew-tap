4eb5a3bd6eceb4b6a817c675695b6e4d5f9ef47c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-21 08:04:45 +0000

    proton-pass-cli 2.1.0

--- Casks/p/proton-pass-cli.rb
+++ Casks/p/proton-pass-cli.rb
@@ -2,11 +2,11 @@ cask "proton-pass-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "macos", linux: "linux"
 
-  version "2.0.2"
-  sha256 arm:          "0bfd99c4d8270ce2c2913c93a823c620a8975e3c2f1ce24d234e41706cb5cf47",
-         intel:        "a975ca6af19177a2d274b7a8222c2510d84e13d252fd9c50bf8df67e54b42818",
-         arm64_linux:  "c12c531adb823d4eae4f6e69065c6e767b10ed71328a1b3fa625a4897ae3b38c",
-         x86_64_linux: "fd60a5041e642a7b1135ef6878c3d7ce3523b083210ea8100eee5c3701017ed9"
+  version "2.1.0"
+  sha256 arm:          "e4541297a5ea8a99ba316ab69336b61555d26a35f8c4207882a68079de343682",
+         intel:        "893e77702b24498f3e02f2d759019ca859254092413b577b005304218103965a",
+         arm64_linux:  "e1481c47dc95fe241a42841414345afb2cd208520893f49a3c23897538293102",
+         x86_64_linux: "5a775d5f83affbd8c1bcbf1517c38d1a157c160133e7136b00d5311bafe8ba93"
 
   url "https://proton.me/download/pass-cli/#{version}/pass-cli-#{os}-#{arch}",
       verified: "proton.me/"
