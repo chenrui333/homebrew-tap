ce2618c985ef08a4beec6b6a80762b801ce15815
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-21 17:13:34 +0000

    eas-cli 19.0.4

--- Formula/e/eas-cli.rb
+++ Formula/e/eas-cli.rb
@@ -1,8 +1,8 @@
 class EasCli < Formula
   desc "Fastest way to build, submit, and update iOS and Android apps"
   homepage "https://docs.expo.dev/eas/"
-  url "https://registry.npmjs.org/eas-cli/-/eas-cli-19.0.2.tgz"
-  sha256 "0888c726e9a4ac53cc68e5ec1e14e0d42f9174300a93ba50ecf3b85a19349059"
+  url "https://registry.npmjs.org/eas-cli/-/eas-cli-19.0.4.tgz"
+  sha256 "29f11e7d699c0856f7cd59bc2104ee4af9888e56889569d95f1057820d72f083"
   license "MIT"
 
   bottle do
