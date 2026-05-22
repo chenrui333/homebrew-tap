5612fc6760a1338fdc913f0ac1162f2a2f7443c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-22 15:49:18 +0000

    hyprmoncfg: update 1.5.0 bottle.

--- Formula/h/hyprmoncfg.rb
+++ Formula/h/hyprmoncfg.rb
@@ -8,8 +8,8 @@ class Hyprmoncfg < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "5e3e1fbeb9524d63ba03c09c179bcd6b6396c9f7103a0e207d0abe6da8ee6eb5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "df4ec66ab396e40b5aff243433f9bc78d4376333906a2eb668b9d189319b33a8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "49e43698664a9ff750ac2b32b51ea83c93fd93b9fcb161bac8cc04dd7638be6c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "457781cf89aa00c83fdd73519803cab12968af03757b791025edadc3fcc46257"
   end
 
   depends_on "go" => :build
