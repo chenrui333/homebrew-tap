bd83d232c5854cb42af7ec86510b6a06274deb0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-23 18:43:10 +0000

    purple 3.15.18

--- Formula/p/purple.rb
+++ Formula/p/purple.rb
@@ -1,8 +1,8 @@
 class Purple < Formula
   desc "Terminal SSH config manager and cockpit for your servers"
   homepage "https://github.com/erickochen/purple"
-  url "https://github.com/erickochen/purple/archive/refs/tags/v3.15.15.tar.gz"
-  sha256 "36d9f0c0e4f7e49226724d67bf9c9c17038d7caffac0add6c98a31c37b3d5b06"
+  url "https://github.com/erickochen/purple/archive/refs/tags/v3.15.18.tar.gz"
+  sha256 "19cdb6458130ccc38137bbab0effa8bc08b94af0def03b456ab6dc96d10abc15"
   license "MIT"
   head "https://github.com/erickochen/purple.git", branch: "master"
 
