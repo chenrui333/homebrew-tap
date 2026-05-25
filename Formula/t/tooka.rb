08c6a515db98e518e389c4184a489dade9fa8bd5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-25 19:18:26 +0000

    tooka: update 1.1.1 bottle.

--- Formula/t/tooka.rb
+++ Formula/t/tooka.rb
@@ -8,11 +8,11 @@ class Tooka < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "13b5518dbec07d23870f94f0410d7c4f430f35e0d88103b5561df893adcab9c0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d839e11416d89ce29cfe74f2bc98301e77079d62a6df7fb2ef5948a48053683c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bbe88a94def0b2da78b39cb35b83b333947427b8890935b038d7724ff9ca2eb6"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8944be46ddb57df61386f728a6906641626fc89b5b82557f748e5cb1390903bd"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "0ef52e2373a980588e3f1615cd22c42eb0fdc88d7493e0cef42686d69ebb5530"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7c25ac3aa29d18a5792ec4a6092428e97c21ac257e8deea8f8e94b98d7a76136"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "da3025c26aeda815c231bb7fca53a93dd7aabea878d24d34e44d4cb059696e9e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62b3549685081b36850406b5bb51c5a18194d005259c701898be88cff68c1bb7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "65b90445c5fe0b2d51c007aa6bb4698dc14928e1f84eda5dbb688c705357c5cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "49fb03d95c21e78a34d7b8fe5456734c445f4d98ccc5806271e2b4919c4f6ff2"
   end
 
   depends_on "rust" => :build
