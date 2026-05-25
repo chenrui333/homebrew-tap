43057d18eb45a061bc190375384701ad27e70e3f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-25 17:07:16 +0000

    gitsocial: update 0.11.2 bottle.

--- Formula/g/gitsocial.rb
+++ Formula/g/gitsocial.rb
@@ -8,11 +8,11 @@ class Gitsocial < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3ec2701658cf6c6edf3898a95d4a84b1ace4245db0687da170f0fa22102ae941"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ec2701658cf6c6edf3898a95d4a84b1ace4245db0687da170f0fa22102ae941"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3ec2701658cf6c6edf3898a95d4a84b1ace4245db0687da170f0fa22102ae941"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c4c9d9f6389ab5f9f3f640a4354501a666da8c3f6f10022809d5134b57ee401a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "def607daa03316637cbe1bf981e42419ca64c6eac42d12b2e03ba479c4dea577"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e86ddf02bad43234ccdae937a6b7f65ab02dee45ce05d4a5c17a820c502d02a5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e86ddf02bad43234ccdae937a6b7f65ab02dee45ce05d4a5c17a820c502d02a5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e86ddf02bad43234ccdae937a6b7f65ab02dee45ce05d4a5c17a820c502d02a5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0dfbd2c8551c5c30d604bce5721e77cb6329d50bf0592b7edc9aa0c186db18e5"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c8bd2ffb27e810d91f65656aa1f02a0166bd1931b6f68a60506b4d76b01af9a0"
   end
 
   depends_on "go" => :build
