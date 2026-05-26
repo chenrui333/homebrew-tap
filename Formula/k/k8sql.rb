432021dce906f1c4b861fdbc6c129f60aaafa60c
Rui Chen <rui@chenrui.dev>
2026-05-26 00:08:29 -0400

    k8sql 0.2.4 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/k/k8sql.rb
@@ -0,0 +1,19 @@
+class K8sql < Formula
+  desc "Query Kubernetes clusters using SQL"
+  homepage "https://github.com/ndenev/k8sql"
+  url "https://github.com/ndenev/k8sql/archive/refs/tags/0.2.4.tar.gz"
+  sha256 "459e6e718e783b3b2302b13590c23427d6c285ccf54dc1affca9d9f1b4073f0a"
+  license "BSD-3-Clause"
+  head "https://github.com/ndenev/k8sql.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/k8sql --version")
+    assert_match "query", shell_output("#{bin}/k8sql --help")
+  end
+end
