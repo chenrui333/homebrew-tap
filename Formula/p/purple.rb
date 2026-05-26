4b5df317b263f552419965c3b8e7666f0fc13ab0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-26 21:03:48 +0000

    purple 3.18.3
    Closes #7454.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/p/purple.rb
+++ Formula/p/purple.rb
@@ -1,8 +1,8 @@
 class Purple < Formula
   desc "Terminal SSH config manager and cockpit for your servers"
   homepage "https://github.com/erickochen/purple"
-  url "https://github.com/erickochen/purple/archive/refs/tags/v3.18.1.tar.gz"
-  sha256 "e6ce4ccf168b9fccb1ee18ff0f490ba84e3cbac102e93d0116ce910aa767621c"
+  url "https://github.com/erickochen/purple/archive/refs/tags/v3.18.3.tar.gz"
+  sha256 "7212224a8849ec9c038ad893b8b0e48f4319783cd402cbbcf77c3cbe7f6296e1"
   license "MIT"
   head "https://github.com/erickochen/purple.git", branch: "master"
 
