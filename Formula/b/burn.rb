972ffec47f768513bcd2983563c845159762d201
Rui Chen <rui@chenrui.dev>
2026-05-26 00:08:55 -0400

    burn 0.3.5 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/b/burn.rb
@@ -0,0 +1,25 @@
+class Burn < Formula
+  desc "See what's burning your Kubernetes budget"
+  homepage "https://github.com/tanrikuluozlem/burn"
+  url "https://github.com/tanrikuluozlem/burn/archive/refs/tags/v0.3.5.tar.gz"
+  sha256 "cc4aa76e1c667b9a1f50af73b8638c8a7e9d09d1ac3fa5cb7bed57bd9d78457f"
+  license "Apache-2.0"
+  head "https://github.com/tanrikuluozlem/burn.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X main.version=#{version}
+      -X main.commit=HEAD
+      -X main.date=#{time.iso8601}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/burn"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/burn version")
+    assert_match "burn", shell_output("#{bin}/burn --help")
+  end
+end
