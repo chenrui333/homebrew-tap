1430d6670651f22f1ffc3ecc11ccd1fb9b35b1ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-27 11:48:28 +0000

    dominds: update 1.25.11 bottle.

--- Formula/d/dominds.rb
+++ Formula/d/dominds.rb
@@ -7,7 +7,7 @@ class Dominds < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "ab95b94a1dac1f66106fdfd3a4d700f5c280c0fda115e44b2efb67fd48cb438a"
+    sha256 cellar: :any_skip_relocation, all: "44af17a1e690596f239d76089350c24ba7e8ba22c3ced302992f6d395e281b1c"
   end
 
   depends_on "node"
