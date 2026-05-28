5d8b3a1f1df5e67c3577c87fe386fe073d2957c1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 21:03:57 +0000

    fresh 0.3.10

--- Formula/f/fresh.rb
+++ Formula/f/fresh.rb
@@ -1,8 +1,8 @@
 class Fresh < Formula
   desc "Modern terminal-based text editor with plugin support"
   homepage "https://getfresh.dev/"
-  url "https://github.com/sinelaw/fresh/archive/refs/tags/v0.3.8.tar.gz"
-  sha256 "7c52ec24536b3703b3db9d201601c2ee807156a3f68713b790bb6f55290d2811"
+  url "https://github.com/sinelaw/fresh/archive/refs/tags/v0.3.10.tar.gz"
+  sha256 "1e4592e19cf7a95df5901e21d350f88534caf7fec9348801b76eabbf8d8d9c1d"
   license "GPL-2.0-only"
   head "https://github.com/sinelaw/fresh.git", branch: "master"
 
