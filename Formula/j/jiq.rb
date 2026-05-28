8e944e386584989f09dc09723e8b96c2af703fbf
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-28 21:05:30 +0000

    jiq 3.30.0
    Closes #7529.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/j/jiq.rb
+++ Formula/j/jiq.rb
@@ -1,8 +1,8 @@
 class Jiq < Formula
   desc "Interactive JSON query tool with real-time output and AI assistance"
   homepage "https://github.com/bellicose100xp/jiq"
-  url "https://github.com/bellicose100xp/jiq/archive/refs/tags/v3.29.0.tar.gz"
-  sha256 "ab383577ca94deab1bc1b343778aae1a1752b60115c91042feca9d2ca49c3516"
+  url "https://github.com/bellicose100xp/jiq/archive/refs/tags/v3.30.0.tar.gz"
+  sha256 "319f5b23cdb505c76c01c840386a3b265eeba5e9651645b7508e435a5499d64c"
   license "Apache-2.0"
 
   bottle do
