f7710ce44e834715de36b340e9da2db080380368
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-05-31 07:54:04 +0000

    dotstate: update 0.4.0 bottle.

--- Formula/d/dotstate.rb
+++ Formula/d/dotstate.rb
@@ -8,11 +8,11 @@ class Dotstate < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "7fe113b67b3f0af11bc210fbb98362aff0ba8bec203df8e26374e355140f65dd"
-    sha256                               arm64_sequoia: "13bb6487703549f748d32468c55618a61e778316b173e96cdad4f7afee0d880e"
-    sha256                               arm64_sonoma:  "80283db5bea82aea87618d1aca85ec0cf9eb3a2328345309eb69dcb6d7595b6b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "eab7f153f015b24b8947170ce65e57d45e0318af880a336a05e9adc2ea996994"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2fae8383e980aba9fe1921cc5b32230ba2939b845df380671069eaec8d100635"
+    sha256               arm64_tahoe:   "f9dffd40e2b1292e105f73081cf40cfbe014a6853e4921f302037d899d0b5ebd"
+    sha256               arm64_sequoia: "6e174da8f1a84a26b8ac8df7142b0f469602fe48cd0f3eea08639c4886f1cddb"
+    sha256               arm64_sonoma:  "b5dfcbe295e35d75aa0b72f5345c189c5afde69c28c975180d0a37aa51d2b4df"
+    sha256 cellar: :any, arm64_linux:   "b4d578ea06a754cb3d4d09c4c7d5a15d85b6ff44027b866de72af176d9871189"
+    sha256 cellar: :any, x86_64_linux:  "f76eb10ea62420db2ceb05d7674c362de65bfb1f3a51ad1ba6af9a83fccdd851"
   end
 
   depends_on "pkgconf" => :build
