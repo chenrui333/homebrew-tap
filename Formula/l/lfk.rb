5c1749ae5f66852c639203d9022393ea126aa0d0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-01 21:49:27 +0000

    lfk: update 0.13.3 bottle.

--- Formula/l/lfk.rb
+++ Formula/l/lfk.rb
@@ -8,11 +8,11 @@ class Lfk < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "644254c62ca5beed1543da80bdbeac1fb831b4a31ede3a6387c61f3558151ee5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "644254c62ca5beed1543da80bdbeac1fb831b4a31ede3a6387c61f3558151ee5"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "644254c62ca5beed1543da80bdbeac1fb831b4a31ede3a6387c61f3558151ee5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "48274e0074dbdb653ac386e6f4c860c04d76d55a18e3e741e5fed121d6232b99"
-    sha256 cellar: :any,                 x86_64_linux:  "a52b1915bdd82aeeae2c1bcee3a78d863214c8fe22ca352d0b0c36725671f2a7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "23ce8e46b2018948b7733b1a40c18f3d855faae504ea44fe960920c0a5b45580"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "23ce8e46b2018948b7733b1a40c18f3d855faae504ea44fe960920c0a5b45580"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "23ce8e46b2018948b7733b1a40c18f3d855faae504ea44fe960920c0a5b45580"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "09ec71ee4fde840149728a6cd7f40a94ac1cb503748a10a1479187c20125a505"
+    sha256 cellar: :any,                 x86_64_linux:  "d3c006eab9ef4de24d73c036b33eae19b7e8b07bf32befcfb765f1bd5a14f87c"
   end
 
   depends_on "go" => :build
