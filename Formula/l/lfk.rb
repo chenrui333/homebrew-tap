42d69ef651ad958c000a668cdd00157108eb82f5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-02 17:55:13 +0000

    lfk 0.13.4

--- Formula/l/lfk.rb
+++ Formula/l/lfk.rb
@@ -1,8 +1,8 @@
 class Lfk < Formula
   desc "Lightning fast Kubernetes navigator"
   homepage "https://github.com/janosmiko/lfk"
-  url "https://github.com/janosmiko/lfk/archive/refs/tags/v0.13.3.tar.gz"
-  sha256 "ad3df25b7e2c2addb063ff19f536ea4167f636aec1637d38a38bdf9f39620bc2"
+  url "https://github.com/janosmiko/lfk/archive/refs/tags/v0.13.4.tar.gz"
+  sha256 "0b170a8b632dd4f840e72f7b438fefa206231b9a81bdca82fe48663a55ba938d"
   license "Apache-2.0"
   head "https://github.com/janosmiko/lfk.git", branch: "main"
 
