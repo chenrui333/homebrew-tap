10c5d07a860717d5eb5d8c60924e8c71ea86fed8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 10:11:29 +0000

    multica: update 0.3.15 bottle.

--- Formula/m/multica.rb
+++ Formula/m/multica.rb
@@ -8,11 +8,11 @@ class Multica < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8d81c9a70926af8c945cbbec6a72128920b83b9b7ea5b933ad917f2ca2529619"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8d81c9a70926af8c945cbbec6a72128920b83b9b7ea5b933ad917f2ca2529619"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8d81c9a70926af8c945cbbec6a72128920b83b9b7ea5b933ad917f2ca2529619"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f46059cd0e203032830689aa7349dae668dcc544d8ae18eb48cad881c95f14a7"
-    sha256 cellar: :any,                 x86_64_linux:  "e335e98846cf66698b099ba95949827c0f3c128361329fa66f0dc23527e8837e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "479ca0e177be39583b526d85bd364633d711d005b048c55a8c75b30c2ca0f65d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "479ca0e177be39583b526d85bd364633d711d005b048c55a8c75b30c2ca0f65d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "479ca0e177be39583b526d85bd364633d711d005b048c55a8c75b30c2ca0f65d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "cd3286a0620b11d3df0deb661765098a1a2643f97366c6264b08e282f7c9b63a"
+    sha256 cellar: :any,                 x86_64_linux:  "1476c433c245548c4677556a65a910e36c2abe1a10597bef85c2c70d3094b7ba"
   end
 
   depends_on "go" => :build
