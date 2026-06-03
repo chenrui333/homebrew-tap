2ed8f0afbb886d44966d5c21ce9ab1521252fa04
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-03 16:02:33 +0000

    sloctl 0.23.0
    Closes #7705.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s/sloctl.rb
+++ Formula/s/sloctl.rb
@@ -1,8 +1,8 @@
 class Sloctl < Formula
   desc "CLI for Nobl9 to manage SLOs, Projects or Alert Policies"
   homepage "https://docs.nobl9.com/sloctl-user-guide/"
-  url "https://github.com/nobl9/sloctl/archive/refs/tags/v0.22.1.tar.gz"
-  sha256 "56679e47cc668404db692c1a683610a95374a2f4282c061484fef93df4097998"
+  url "https://github.com/nobl9/sloctl/archive/refs/tags/v0.23.0.tar.gz"
+  sha256 "a196643b7757879d050c9ecd102ed4506990f6d8c7bce4a4ea2422b3084cdfc4"
   license "MPL-2.0"
   head "https://github.com/nobl9/sloctl.git", branch: "main"
 
