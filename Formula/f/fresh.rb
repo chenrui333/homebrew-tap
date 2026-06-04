f0ed206f0a8fff691509b72eb8c28b757215c930
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-04 19:09:40 +0000

    fresh 0.3.11
    Closes #7745.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/f/fresh.rb
+++ Formula/f/fresh.rb
@@ -1,8 +1,8 @@
 class Fresh < Formula
   desc "Modern terminal-based text editor with plugin support"
   homepage "https://getfresh.dev/"
-  url "https://github.com/sinelaw/fresh/archive/refs/tags/v0.3.10.tar.gz"
-  sha256 "1e4592e19cf7a95df5901e21d350f88534caf7fec9348801b76eabbf8d8d9c1d"
+  url "https://github.com/sinelaw/fresh/archive/refs/tags/v0.3.11.tar.gz"
+  sha256 "7255c8ae3a059abda15ad425ac8bfbd65c563a6379446ca1424f633bb5c14e20"
   license "GPL-2.0-only"
   head "https://github.com/sinelaw/fresh.git", branch: "master"
 
