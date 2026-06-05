98e1d35d605f8dd648d1141948eba35404e51a1d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-05 16:35:23 +0000

    aws-doctor: update 2.21.0 bottle.

--- Formula/a/aws-doctor.rb
+++ Formula/a/aws-doctor.rb
@@ -8,11 +8,11 @@ class AwsDoctor < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4fbbb27084e889ea3b11ef66fbb735153dc7785bd7f3c3a054ed99398baedc72"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4fbbb27084e889ea3b11ef66fbb735153dc7785bd7f3c3a054ed99398baedc72"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4fbbb27084e889ea3b11ef66fbb735153dc7785bd7f3c3a054ed99398baedc72"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "56a2ef15048f5dc43a77e2fec12e61e154c9ce1e4abd5a45350f5e4f8a1a08e2"
-    sha256 cellar: :any,                 x86_64_linux:  "1713c3f853fdb84d02cc07e66cc1e69bfd638a790e7f1de043d845ac913acc27"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "06d4458587d54d3d5f84a0b984924eb4c9ed19da2684a15ca319b64c58616aef"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "06d4458587d54d3d5f84a0b984924eb4c9ed19da2684a15ca319b64c58616aef"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "06d4458587d54d3d5f84a0b984924eb4c9ed19da2684a15ca319b64c58616aef"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d561efe34e601918f756a58739792fb43960bda15f84593aa62c9e688d711d1f"
+    sha256 cellar: :any,                 x86_64_linux:  "871fb05a0127abb72981c20cd51e44d0456310017bdfdc2e35743d202818d182"
   end
 
   depends_on "go" => :build
