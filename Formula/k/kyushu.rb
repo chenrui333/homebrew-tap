e3d483b5a3283d28dff687eba2091c0db6bc9902
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 05:44:58 +0000

    kyushu: add 0.0.5 bottle.

--- Formula/k/kyushu.rb
+++ Formula/k/kyushu.rb
@@ -6,6 +6,15 @@ class Kyushu < Formula
   license "MIT"
   head "https://github.com/peterpeterparker/kyushu.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e752d51e8293e1fbc67650bb8e701efc4deb4ded2678ea39432a997650d6eddd"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f438dcac97578efc7873df1e7ee2ca376f1cd18c682bee3dd4a088a12a274c8c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0ed400b5479893e683e09cf7a0cf4e11a25b9a42917c59f0d5a244a38358364f"
+    sha256 cellar: :any,                 arm64_linux:   "a78f69dd65e48d6fe9fa5c63ca79b183094d38a5cc2455db0692fceebe10ad62"
+    sha256 cellar: :any,                 x86_64_linux:  "f976226d83884f5e75870a2d0f8c37ebd88e5fc5f2523ac201594b711d1ae18d"
+  end
+
   depends_on "rust" => :build
 
   def install
