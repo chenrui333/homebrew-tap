380e0db9f510dd57d4ccb14868e868e6249db571
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 05:36:24 +0000

    vaults3: add 4.2.1 bottle.

--- Formula/v/vaults3.rb
+++ Formula/v/vaults3.rb
@@ -6,6 +6,15 @@ class Vaults3 < Formula
   license "AGPL-3.0-only"
   head "https://github.com/Kodiqa-Solutions/VaultS3.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c7c6a7df9f19e707ab8ad477c4ef522a421177615061bfa0017f7ca6227dcdac"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c7c6a7df9f19e707ab8ad477c4ef522a421177615061bfa0017f7ca6227dcdac"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c7c6a7df9f19e707ab8ad477c4ef522a421177615061bfa0017f7ca6227dcdac"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "4c5254a302e5abfa531006a433fa4af4958f598220bc32c855c3dba8a3afc217"
+    sha256 cellar: :any,                 x86_64_linux:  "702e4930a38b9fed33ab6167925fc11a8209346f067ff0fedec242102ddb5417"
+  end
+
   depends_on "go" => :build
   depends_on "node" => :build
 
