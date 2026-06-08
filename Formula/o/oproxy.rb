feab4457f7fedf151ddf5b1abfaa6752a9d2ad3d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 06:04:34 +0000

    oproxy: add 0.1.6 bottle.

--- Formula/o/oproxy.rb
+++ Formula/o/oproxy.rb
@@ -6,6 +6,15 @@ class Oproxy < Formula
   license "MIT"
   head "https://github.com/sauravrao637/oproxy.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "467e79a5b81ab258b77b2195cd747c73fb672b6e8c54b8d563521e13b6e81222"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "275eb25eb0fd55fca027471b53e5a0cc7af7e71423446a81be4b4de3407d75eb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0b42e1702ea032be4ff3278e7ab9d1bd45033ab7703ce8eae7abd50158de333c"
+    sha256 cellar: :any,                 arm64_linux:   "4443c43ee021ebd102bdc221d180c224ec73d5c2a3b576f100d887782996ab94"
+    sha256 cellar: :any,                 x86_64_linux:  "bbfbd0f74e0d20c2fb4ba9a05564fe27db1282fe62619709a9b6db8018ed1b01"
+  end
+
   depends_on "node" => :build
   depends_on "rust" => :build
 
