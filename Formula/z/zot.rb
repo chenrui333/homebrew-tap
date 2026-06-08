b2cd7220035c1aada31b2ced42915a14fca1e586
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 06:06:47 +0000

    zot 0.2.16

--- Formula/z/zot.rb
+++ Formula/z/zot.rb
@@ -1,8 +1,8 @@
 class Zot < Formula
   desc "Lightweight coding agent harness written in Go"
   homepage "https://github.com/patriceckhart/zot"
-  url "https://github.com/patriceckhart/zot/archive/refs/tags/v0.2.15.tar.gz"
-  sha256 "1a6756cb64a9bae31f2769de2b4cf9fa6560b31295eda048a31a447e0c991e11"
+  url "https://github.com/patriceckhart/zot/archive/refs/tags/v0.2.16.tar.gz"
+  sha256 "e7a08efa949506db20e9eededdecc236220e02fd8b0f2d139b4430b704aeaae7"
   license "MIT"
   head "https://github.com/patriceckhart/zot.git", branch: "main"
 
