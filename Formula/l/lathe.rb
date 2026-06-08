10352bf2c4ac12590128646b9671c42ee0f9f3dc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 05:40:10 +0000

    lathe: add 0.3.0 bottle.

--- Formula/l/lathe.rb
+++ Formula/l/lathe.rb
@@ -6,6 +6,15 @@ class Lathe < Formula
   license "MIT"
   head "https://github.com/devenjarvis/lathe.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "89797f577028ffabc9deef8ec4ff4cb19c4ba1954a5fc151f3c5c849791473e4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "89797f577028ffabc9deef8ec4ff4cb19c4ba1954a5fc151f3c5c849791473e4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "89797f577028ffabc9deef8ec4ff4cb19c4ba1954a5fc151f3c5c849791473e4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f0876e40bdfc8e27a5e51d52f89302c8d093db5814392e22eadecf98b7ac9db3"
+    sha256 cellar: :any,                 x86_64_linux:  "d513785a520a18d4bb8b22b093a0cbd6c0c7451b07fc2fb7f814eb7477a692e6"
+  end
+
   depends_on "go" => :build
 
   def install
