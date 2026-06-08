d8a5f9e1cae5aebf96982fe2dae2d312e197715a
Rui Chen <rui@chenrui.dev>
2026-06-08 00:37:55 -0400

    breathe-cli 1.9 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/b/breathe-cli.rb
@@ -0,0 +1,21 @@
+class BreatheCli < Formula
+  include Language::Python::Virtualenv
+
+  desc "Paced resonance breathing in your terminal"
+  homepage "https://github.com/marekkowalczyk/breathe-cli"
+  url "https://github.com/marekkowalczyk/breathe-cli/archive/refs/tags/v1.9.tar.gz"
+  sha256 "8842ea690828e6c4acfd1d0887ed86b7d209d863fc7b4c644581461412b4e2dd"
+  license "MIT"
+  head "https://github.com/marekkowalczyk/breathe-cli.git", branch: "main"
+
+  depends_on "python@3.13"
+
+  def install
+    virtualenv_install_with_resources
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/breathe --version")
+    assert_match "breathe", shell_output("#{bin}/breathe --help")
+  end
+end
