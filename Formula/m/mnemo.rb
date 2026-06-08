69060c1e3ff651255d553b9e1cbb694cbbd2bfeb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-08 05:52:53 +0000

    mnemo: add 0.1.0 bottle.

--- Formula/m/mnemo.rb
+++ Formula/m/mnemo.rb
@@ -6,6 +6,15 @@ class Mnemo < Formula
   license "MIT"
   head "https://github.com/zaydmulani09/mnemo.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "27c80b5c9d139906e71cf80ff4c0372039e484867546f7f27132ee8af6cd4677"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "582ae9a2d0cb5785a5454b80fc75a01232598f9e6f7050c72f45526d8c4a0b27"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1f20e79e47c9e814eeda45e9a01b99bb191e9ffef0771d3c23d1f7e6980ea996"
+    sha256 cellar: :any,                 arm64_linux:   "7b1e43334efadd7c712178d914e38f45bb81168472e3c86ed93c931808e7a24b"
+    sha256 cellar: :any,                 x86_64_linux:  "cb3112833403082b619f85dff6561d22fb43b9bc02dcfbb5690761fb68046fb1"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "openssl@3"
