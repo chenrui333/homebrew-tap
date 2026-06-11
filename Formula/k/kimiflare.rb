cc15a52960e69bcdd563f90bffba7bbf21dbf208
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 17:24:36 +0000

    kimiflare: add 0.60.1 bottle.

--- Formula/k/kimiflare.rb
+++ Formula/k/kimiflare.rb
@@ -5,6 +5,15 @@ class Kimiflare < Formula
   sha256 "dca7e2572562a8250a639c50ad5ab4c76b07cc81399e96158c41452716222c39"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e29e770b96bed251722d043b08d81798a62d2d5b293c17f4f2fa9b2469a241b2"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e29e770b96bed251722d043b08d81798a62d2d5b293c17f4f2fa9b2469a241b2"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e29e770b96bed251722d043b08d81798a62d2d5b293c17f4f2fa9b2469a241b2"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a8e950b09fbf75201f85aa2e4a697cb8a2443d7ae153727bbf17f6340d94d5ff"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "afc949aad10793c5903f8ee6936017893b5008d8b26a0c55e6b4a389f9c9dcb6"
+  end
+
   depends_on "node"
 
   def install
