9b77353d000ed18b361f716e52fc4d82310d015f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 16:37:25 +0000

    pho: add 0.1.38 bottle.

--- Formula/p/pho.rb
+++ Formula/p/pho.rb
@@ -6,6 +6,15 @@ class Pho < Formula
   license "GPL-3.0-only"
   head "https://github.com/utkarsh261/pho.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8a5665b25b45fddea230b7ce2b1a0f18b4124269357270fc982be9f5677128af"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a5665b25b45fddea230b7ce2b1a0f18b4124269357270fc982be9f5677128af"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8a5665b25b45fddea230b7ce2b1a0f18b4124269357270fc982be9f5677128af"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6ca62003a65cc5686a2dd49514ca06fe1ac0e1e936bb658b171be09bf7d397fc"
+    sha256 cellar: :any,                 x86_64_linux:  "67fe098928bfd239780c4bfd77b464f7f2eb8a9f793334f7a24d0c9cc46862c5"
+  end
+
   depends_on "go" => :build
 
   def install
