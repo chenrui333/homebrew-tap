1afa8433a47c008dfe5711f5f1b26587d3bfa3a7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 19:36:26 +0000

    netwatch: update 0.25.6 bottle.

--- Formula/n/netwatch.rb
+++ Formula/n/netwatch.rb
@@ -8,11 +8,11 @@ class Netwatch < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f0e12f957226aa52cdb8729ae8725b670a1bea24985fe218a1cbe628eb4c5ce5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92dafa7817ae9c03806cdfd5d6a264687173b789eae9cf134468644812b4fafb"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2ef0040072da513dc0f92767259a6d73f03c20da1234ad24b046e7ea79f25cdd"
-    sha256 cellar: :any,                 arm64_linux:   "1796de5cb7670a27dcb713e0c3d1be4e42590be030d54b953b5bc469ae3a9b27"
-    sha256 cellar: :any,                 x86_64_linux:  "8681501b0ef806501527cf3a702d0cc2d99b87c19b513aaff32cf7a7a39fb6e8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "62fbe77d8c0a0f2d2d6848d093ed08862cc4e481387e6a5d929d2afaaceadf80"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c2175184f23f4890c9b9dd70b2c1c9d56706975ec3d4a154c4af05a94eac2e26"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4345bfed6fe9e4443098e1b3c2b68faaa2cdd6b0cb04f0f98364b4390cbb4456"
+    sha256 cellar: :any,                 arm64_linux:   "f8d3a59608417df7083163872a6b9f96adbb55738806776fc785df1e4f361d75"
+    sha256 cellar: :any,                 x86_64_linux:  "7b09e493fc91f5abd72c52f2c436a5436033b65f2a808ec997830612343e1fc7"
   end
 
   depends_on "rust" => :build
