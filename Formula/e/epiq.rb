03bf2209eb737a05f880aa680cf96e5e540971b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 05:37:57 +0000

    epiq: add 0.6.20 bottle.

--- Formula/e/epiq.rb
+++ Formula/e/epiq.rb
@@ -5,6 +5,11 @@ class Epiq < Formula
   sha256 "e7b1f77920486997a111db3adc45488a0bce85cbc5cc962b7bb0c9514bc7154e"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, all: "bdf3ed6f90acea16988013d47977ddf24031d8f70b1caf2553ad0f2583b9faf7"
+  end
+
   depends_on "node"
 
   def install
