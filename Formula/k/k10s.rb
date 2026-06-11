75e1b6f1b4f323edf4ab3538d8f6c87cbe05a3ee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 16:30:33 +0000

    k10s: add 0.4.0 bottle.

--- Formula/k/k10s.rb
+++ Formula/k/k10s.rb
@@ -5,6 +5,15 @@ class K10s < Formula
   sha256 "80af292b682d12ecc81eb92638520000d17f1d3ec24244adc26f3809c0bf62b0"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b7c90dcca3fdb4124340de432633b8129021603626bdc6ef4400d07cc01b0b61"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b7c90dcca3fdb4124340de432633b8129021603626bdc6ef4400d07cc01b0b61"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b7c90dcca3fdb4124340de432633b8129021603626bdc6ef4400d07cc01b0b61"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5bb358a356a326ac135f98e09f27b0bd77b692cdcfb7eec3691d09c4b326cb22"
+    sha256 cellar: :any,                 x86_64_linux:  "8a3d55c7264afc2df37a731825c0e46161d71ed4160dff0be7acacfa4a6939da"
+  end
+
   depends_on "go" => :build
 
   def install
