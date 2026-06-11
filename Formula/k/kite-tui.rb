9ef5e77057adc21f14163481ba819cee4282c380
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 05:31:29 +0000

    kite-tui: add 0.1.1 bottle.

--- Formula/k/kite-tui.rb
+++ Formula/k/kite-tui.rb
@@ -6,6 +6,15 @@ class KiteTui < Formula
   license "MIT"
   head "https://github.com/KernelFreeze/kite-tui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "be75f23600d84037d50e155ff7d2fa1636ad87754dfea60a3398e6e20fab54a7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d334e8998d2052ade1624c2e9973e931e25b0e55473678ef786ff720b8aee79b"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "01d208a7578b19d2079c411da1e837c751aab6d74ed543e16fe6349bdc4d8121"
+    sha256 cellar: :any,                 arm64_linux:   "98586973ac78fa91b2112110f0cb652eb7eb2958589fcd703c398560c9197483"
+    sha256 cellar: :any,                 x86_64_linux:  "82cdd97ca07d7e288444ec090e18f004c93c44ddd530b3e1b5d29bd925cfb1a9"
+  end
+
   depends_on "rust" => :build
 
   def install
