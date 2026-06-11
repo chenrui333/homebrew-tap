13f5f4b18e4a767735ed8860b04970ece225d402
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 16:55:44 +0000

    prempti: add 0.2.1 bottle.

--- Formula/p/prempti.rb
+++ Formula/p/prempti.rb
@@ -6,6 +6,15 @@ class Prempti < Formula
   license "Apache-2.0"
   head "https://github.com/falcosecurity/prempti.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "782bb73506376dba7964d9b4393b23192e36bc02adfb950a36ba4482dc577edc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2eb3885d1393bf0f2007d1ea35a720afc51103abc5b8ec3bf434c09472446c4d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e0d8f5dfbb008ae8800cf2fff45611464594a013b3295e66b39e261fd4aaf585"
+    sha256 cellar: :any,                 arm64_linux:   "f53a47819de3b4d4b744ddb6ea632cbf8fb5d60898a1f64b1fdc6f7a457ecc39"
+    sha256 cellar: :any,                 x86_64_linux:  "a18980856acd89ebae2215b05283032e21b54ca9496a46a7b727ffeb41b07f78"
+  end
+
   depends_on "rust" => :build
 
   def install
