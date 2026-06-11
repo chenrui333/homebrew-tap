e327c1fb76dc9c2f747c4bda3ee5cc318edcf15f
Rui Chen <rui@chenrui.dev>
2026-06-11 01:13:41 -0400

    epiq 0.6.20 (new formula)
    Closes #7874.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/e/epiq.rb
@@ -0,0 +1,19 @@
+class Epiq < Formula
+  desc "Distributed terminal-native issue tracker backed by Git"
+  homepage "https://github.com/ljtn/epiq"
+  url "https://registry.npmjs.org/epiq/-/epiq-0.6.20.tgz"
+  sha256 "e7b1f77920486997a111db3adc45488a0bce85cbc5cc962b7bb0c9514bc7154e"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/epiq --version")
+    assert_match "CLI based issue tracker", shell_output("#{bin}/epiq --help")
+  end
+end
