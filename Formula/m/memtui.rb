31dc0c8d64c4dda73e9f9f45188b0d0e53e3c782
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 05:18:07 +0000

    memtui: add 0.0.6 bottle.

--- Formula/m/memtui.rb
+++ Formula/m/memtui.rb
@@ -6,6 +6,15 @@ class Memtui < Formula
   license "MIT"
   head "https://github.com/nnnkkk7/memtui.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "085b7f43352db0b595ddeb1fb18446ee2fa3a0b3025ede868ca562c995241ca4"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "085b7f43352db0b595ddeb1fb18446ee2fa3a0b3025ede868ca562c995241ca4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "085b7f43352db0b595ddeb1fb18446ee2fa3a0b3025ede868ca562c995241ca4"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "76955ac24dd54d376e467bddb606adf60114999f49ec1a0f03e39d700f2ea9b6"
+    sha256 cellar: :any,                 x86_64_linux:  "947c495732af1cd8ac959f0905337aff3092ff0ce284918b7101afa3545f010f"
+  end
+
   depends_on "go" => :build
 
   def install
