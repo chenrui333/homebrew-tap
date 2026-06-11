4c8b34dc6a2e3d1cece7f0ef003d8cd3d72a18c8
Rui Chen <rui@chenrui.dev>
2026-06-11 11:59:01 -0400

    kimiflare 0.60.1 (new formula)
    Closes #7886.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/k/kimiflare.rb
@@ -0,0 +1,28 @@
+class Kimiflare < Formula
+  desc "Terminal coding agent powered by Kimi-K2.6 on Cloudflare Workers AI"
+  homepage "https://github.com/sinameraji/kimiflare"
+  url "https://registry.npmjs.org/kimiflare/-/kimiflare-0.60.1.tgz"
+  sha256 "dca7e2572562a8250a639c50ad5ab4c76b07cc81399e96158c41452716222c39"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    prebuilds = libexec/"lib/node_modules/kimiflare/node_modules/isolated-vm/prebuilds"
+    platform = OS.mac? ? "darwin" : "linux"
+    arch = Hardware::CPU.arm? ? "arm64" : "x64"
+    keep = "#{platform}-#{arch}"
+    if prebuilds.directory?
+      prebuilds.children.each { |dir| rm_r(dir) if dir.basename.to_s != keep }
+      (prebuilds/keep).glob("*.musl.node").each(&:unlink) if OS.linux?
+    end
+
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/kimiflare --version")
+    assert_match "Usage", shell_output("#{bin}/kimiflare --help")
+  end
+end
