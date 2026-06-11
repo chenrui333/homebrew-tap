0705e3f50255802e504b24446764e06f0d8d6698
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-11 05:33:57 +0000

    quokka: add 0.2.7 bottle.

--- Formula/q/quokka.rb
+++ Formula/q/quokka.rb
@@ -6,6 +6,15 @@ class Quokka < Formula
   license "MIT"
   head "https://github.com/dutradotdev/quokka.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0d08a91af04d6ddf6b457f0103ad93ccdcc4163feba4fe9ef615b2defa1b7768"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bde10490a795db74ee45baeba8816028a74fc7ca52cbd39632a6271143a18e9f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e59d4e6e7730749011fde6251caafbb7cccd1275a40a98ad6fa0eb972b0db1ef"
+    sha256 cellar: :any,                 arm64_linux:   "86e9cafe8cab58dc816ef5db2ba17b339de67b2c20907dec7cd4cf5b9388f239"
+    sha256 cellar: :any,                 x86_64_linux:  "7c4a9b8a07b87a0bc76e5d9ad78736efeaf1dbb7392d9c47a7f0daefbcd93f39"
+  end
+
   depends_on "rust" => :build
 
   def install
