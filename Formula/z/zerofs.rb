6511c0e4cb62a7570d786e0423850dd6d53833b3
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 22:30:10 +0000

    zerofs: add 1.2.5 bottle.

--- Formula/z/zerofs.rb
+++ Formula/z/zerofs.rb
@@ -6,6 +6,15 @@ class Zerofs < Formula
   license "AGPL-3.0-only"
   head "https://github.com/Barre/ZeroFS.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e5fd96831213d90054d1a4c1b01a4f1430b8f74058fd9bc309c34164005b4c58"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7405c2ab3d5a84409ba7b11ccc7daa73b995c6742e136ad1157034dfa17a3f8f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "62b6ca761016e74696f6e29a1c17c8c8d3bbb35368d901b6b431904e183d7c8f"
+    sha256 cellar: :any,                 arm64_linux:   "61111b31e2e70e46bb484d44dd310adce7f453c71c260bc5674b97329b07c249"
+    sha256 cellar: :any,                 x86_64_linux:  "4a86ee57f729c8155ca7f73bde4c06c71dea0e8ddc8252f4bf061a739335ec56"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
