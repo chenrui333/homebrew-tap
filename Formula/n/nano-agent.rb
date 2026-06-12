4fbdbc8e3593110187552d91d6c2d295a5753ae9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 22:23:19 +0000

    nano-agent: add 0.2.1 bottle.

--- Formula/n/nano-agent.rb
+++ Formula/n/nano-agent.rb
@@ -6,6 +6,15 @@ class NanoAgent < Formula
   license "MIT"
   head "https://github.com/skorotkiewicz/nano-agent.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "feaf758ebbd5796195b478336654d888545d38803cccf554cd9cf55c23095980"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ece016f7064a47097b7de1fa3d0405d6ac84079fdcb00f02341b786c1eb2aa3e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "056a9d5d706666b50e64c5fac5278d03fc4c7043302e4e1e21b2cc58146e0372"
+    sha256 cellar: :any,                 arm64_linux:   "6cb7eb1f4f6bc11f563d619fde38a93761ee6531ee3cc1a4efe8ad8c1d0b348b"
+    sha256 cellar: :any,                 x86_64_linux:  "cbaca23124b5e69122352c432f2422888699e4384a74cdccdc9375b5c48f687a"
+  end
+
   depends_on "rust" => :build
 
   def install
