3040c92cd31ce24dada61341d4136a9705003b6c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-12 22:33:30 +0000

    bunsen: add 0.0.9 bottle.

--- Formula/b/bunsen.rb
+++ Formula/b/bunsen.rb
@@ -7,6 +7,13 @@ class Bunsen < Formula
   license "MIT"
   head "https://github.com/g4rcez/bunsen.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 arm64_tahoe:   "cd4a076a94e5904bb3d1448e655368d212d8ee9e3de8decefff456ac06585256"
+    sha256 arm64_sequoia: "b4960edf2ccc5966461a126846bb0146fabc7c9a8fce29ee3e26376d35ba7682"
+    sha256 arm64_sonoma:  "0a64890f2cdeafba12571e5879e1e91b7df7d740b81e568a594dc9a344131789"
+  end
+
   depends_on "homebrew/core/bun" => :build
   depends_on :macos
 
