c7f0badb341ab4f60fb8a019758ce7fd446f0f0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-13 18:38:50 +0000

    agentcarousel: add 0.8.1 bottle.

--- Formula/a/agentcarousel.rb
+++ Formula/a/agentcarousel.rb
@@ -6,6 +6,15 @@ class Agentcarousel < Formula
   license "MIT"
   head "https://github.com/agentcarousel/agentcarousel.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "93d5f5a7e7851aa2f2e81f74edf171b03471c9efbbbde6f9ea29da3f70faea94"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d9d17a5bf9d071ba107948c177a7a206aa6001fc60bc6e0b16a5c179922e956d"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1008643547e3f1b2f22d3459bbeafd89312e8c98686ebd261da73e2f620b35cf"
+    sha256 cellar: :any,                 arm64_linux:   "c3190f530aa8241aa05a65fd99786144f59b8f44b5596bcef0b9e045b567c9a3"
+    sha256 cellar: :any,                 x86_64_linux:  "091027a85c89ee73ee9406d3761377ef2ee9eabc4b536b4486c5ec07af37fcfa"
+  end
+
   depends_on "rust" => :build
 
   def install
