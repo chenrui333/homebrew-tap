b6a45f683624045af003f0b9900511cdf7010377
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-13 23:41:58 +0000

    riskkernel: add 0.6.0 bottle.

--- Formula/r/riskkernel.rb
+++ Formula/r/riskkernel.rb
@@ -6,6 +6,15 @@ class Riskkernel < Formula
   license "Apache-2.0"
   head "https://github.com/prashar32/riskkernel.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4089f02b5c2b4d80a67f958de118b79855c8232d83d0fe124711ce25865b1093"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4089f02b5c2b4d80a67f958de118b79855c8232d83d0fe124711ce25865b1093"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4089f02b5c2b4d80a67f958de118b79855c8232d83d0fe124711ce25865b1093"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "04d7c41ec299bc5b4d1535a54d48d004739681031de16a3373dd3e142b89e279"
+    sha256 cellar: :any,                 x86_64_linux:  "48a3777bbdb568f873d7379b47913fe84754292d5b5da1927a13feb91a696450"
+  end
+
   depends_on "go" => :build
   depends_on "pkgconf" => :build
 
