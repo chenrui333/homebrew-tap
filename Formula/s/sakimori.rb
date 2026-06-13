4b42fb474e1f346f130a916ee027cd9cf324660f
Rui Chen <rui@chenrui.dev>
2026-06-13 17:25:44 -0400

    sakimori: improve test
    Closes #8044.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s/sakimori.rb
+++ Formula/s/sakimori.rb
@@ -22,6 +22,11 @@ class Sakimori < Formula
   end
 
   test do
-    assert_match "sakimori", shell_output("#{bin}/sakimori --help")
+    require "open3"
+
+    # FIXME: Upstream does not expose a version command; replace this with a version assertion when available.
+    output, status = Open3.capture2e(bin/"sakimori", "--not-a-real-option")
+    refute_predicate status, :success?
+    assert_match "not-a-real-option", output
   end
 end
