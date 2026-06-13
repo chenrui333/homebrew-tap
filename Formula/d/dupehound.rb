acf7b313435f422db813bf623f079e4a8c503201
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-13 01:18:25 +0000

    dupehound: add 0.1.0 bottle.

--- Formula/d/dupehound.rb
+++ Formula/d/dupehound.rb
@@ -6,6 +6,15 @@ class Dupehound < Formula
   license "MIT"
   head "https://github.com/Rafaelpta/dupehound.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5c265467c65432e3bd4053d0360f21f41a09f8ec8a4b7c465a208113a1cadf4b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "85e1a29d5295225ff19b7170602874dc9d0787b9f0fc05113a51af93f47726e1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c5155c50708149527405d6e6c813f381b072ed4775e301fdcaefd45ca3eecc3c"
+    sha256 cellar: :any,                 arm64_linux:   "43226a959bc4b50082f134ce346c858c4b7695216d3677122ee30c85d7342ef5"
+    sha256 cellar: :any,                 x86_64_linux:  "7d7bab7a302b0bbe15aef21d836dda514b9283f05b830e68d9704094944010a8"
+  end
+
   depends_on "rust" => :build
 
   def install
