129bce717fc2b99d383097704f82b0dd13c2509c
Rui Chen <rui@chenrui.dev>
2026-06-13 17:13:11 -0400

    deface: improve test
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/deface.rb
+++ Formula/d/deface.rb
@@ -66,6 +66,11 @@ class Deface < Formula
   end
 
   test do
-    system bin/"deface", "--help"
+    require "open3"
+
+    # FIXME: Upstream does not expose a version command; replace this with a version assertion when available.
+    output, status = Open3.capture2e(bin/"deface", "--not-a-real-option")
+    refute_predicate status, :success?
+    assert_match "not-a-real-option", output
   end
 end
