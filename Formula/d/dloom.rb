4b5d3ff15e17bf66b24b5cb77f39fbf0a0c6d82d
Rui Chen <rui@chenrui.dev>
2026-06-13 14:09:27 -0400

    dloom 1.0.2 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/d/dloom.rb
@@ -0,0 +1,33 @@
+class Dloom < Formula
+  desc "Dotfile and configuration weaver tool"
+  homepage "https://github.com/dloomorg/dloom"
+  url "https://github.com/dloomorg/dloom/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "5d01c61d102dc91b2cbe472626d1cc495f605a66684f8587d6113dd66a8bd1ee"
+  license "MIT"
+  head "https://github.com/dloomorg/dloom.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/dloomorg/dloom/cmd.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:)
+
+    generate_completions_from_executable(bin/"dloom", "completion")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/dloom version")
+
+    (testpath/"dloom.yaml").write <<~YAML
+      links:
+        - src: dotfiles/gitconfig
+          dest: ~/.gitconfig
+    YAML
+    (testpath/"dotfiles/gitconfig").mkpath
+    output = shell_output("#{bin}/dloom setup --dry-run 2>&1", 1)
+    assert_match(/link|setup|config/i, output)
+  end
+end
