3cf01acd9e3e24c1c2734ceae48e4d9ff102a605
Rui Chen <rui@chenrui.dev>
2026-06-13 16:24:32 -0400

    riskkernel 0.6.0 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/r/riskkernel.rb
@@ -0,0 +1,25 @@
+class Riskkernel < Formula
+  desc "Deterministic cost, loop, and time budgets for AI agents with observability"
+  homepage "https://github.com/prashar32/riskkernel"
+  url "https://github.com/prashar32/riskkernel/archive/refs/tags/v0.6.0.tar.gz"
+  sha256 "16feb81d0f05129113f71d17731525093948a9bbc03d7ebfb92bc0deb447130e"
+  license "Apache-2.0"
+  head "https://github.com/prashar32/riskkernel.git", branch: "main"
+
+  depends_on "go" => :build
+
+  def install
+    ldflags = %W[
+      -s -w
+      -X github.com/prashar32/riskkernel/internal/version.Version=#{version}
+    ]
+    system "go", "build", *std_go_args(ldflags:), "./cmd/riskkernel"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/riskkernel version")
+
+    output = shell_output("#{bin}/riskkernel policy validate /dev/null 2>&1", 1)
+    assert_match "policy", output
+  end
+end
