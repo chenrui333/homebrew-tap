0617de9ba20c819b5f4d85f66d2a8a09ea18db95
Rui Chen <rui@chenrui.dev>
2026-06-13 16:29:15 -0400

    agenthud 0.18.3 (new formula)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Formula/a/agenthud.rb
@@ -0,0 +1,19 @@
+class Agenthud < Formula
+  desc "Claude Code TUI dashboard for parallel sessions and sub-agents"
+  homepage "https://github.com/neochoon/agenthud"
+  url "https://registry.npmjs.org/agenthud/-/agenthud-0.18.3.tgz"
+  sha256 "c854463bb7c376f1b5e789937628c65f543ceb2b046af3ef20f5fd1b50bc2820"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/agenthud --version")
+    assert_match "agenthud", shell_output("#{bin}/agenthud --help")
+  end
+end
