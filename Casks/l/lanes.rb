88555da7bb86ed131b778b3a57df1db6418def94
Rui Chen <rui@chenrui.dev>
2026-06-13 16:24:43 -0400

    lanes 0.43.0 (new cask)
    Signed-off-by: Rui Chen <rui@chenrui.dev>

---
+++ Casks/l/lanes.rb
@@ -0,0 +1,22 @@
+cask "lanes" do
+  version "0.43.0"
+  sha256 "8b884b4c5fb8fa2b04ac3be2dc038f9a7f624cacf3d0e029603740cf411c94ff"
+
+  url "https://github.com/lanes-sh/app/releases/download/v#{version}/Lanes_#{version}_universal.dmg",
+      verified: "github.com/lanes-sh/app/"
+  name "Lanes"
+  desc "Mission control for parallel AI coding agents"
+  homepage "https://lanes.sh/"
+
+  disable! date: "2026-06-14", because: :fails_gatekeeper_check
+
+  depends_on macos: :ventura
+
+  app "Lanes.app"
+
+  zap trash: [
+    "~/Library/Application Support/Lanes",
+    "~/Library/Caches/Lanes",
+    "~/Library/Preferences/sh.lanes.app.plist",
+  ]
+end
