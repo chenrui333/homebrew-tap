be6be0a32d74d7e4c635457c25e0c7b6070c778a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-13 18:19:23 +0000

    claumon: add 0.14.0 bottle.

--- Formula/c/claumon.rb
+++ Formula/c/claumon.rb
@@ -6,6 +6,15 @@ class Claumon < Formula
   license "MIT"
   head "https://github.com/fabioconcina/claumon.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "40dbbb58d09b52227124b0f8ef0d0cba2481b6679c53516b4d971d8c9a81e63c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "40dbbb58d09b52227124b0f8ef0d0cba2481b6679c53516b4d971d8c9a81e63c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "40dbbb58d09b52227124b0f8ef0d0cba2481b6679c53516b4d971d8c9a81e63c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "69e60670756a3869124f5f21e774074b7cd23b6072857185127c8c5aca72df4c"
+    sha256 cellar: :any,                 x86_64_linux:  "f18cc7bfca5a93936c545274b7d9d01fa98bb4b2072ec8c0df3f5be8a35a239a"
+  end
+
   depends_on "go" => :build
 
   def install
