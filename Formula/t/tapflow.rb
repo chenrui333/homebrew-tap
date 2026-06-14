a59304358bcb8d07e92f8c7c509330cb1e61f695
Rui Chen <rui@chenrui.dev>
2026-06-14 12:38:50 -0400

    tapflow 0.8.2 (new formula)
    Closes #8053.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

---
+++ Formula/t/tapflow.rb
@@ -0,0 +1,26 @@
+class Tapflow < Formula
+  desc "Self-hosted iOS and Android simulator streaming for the whole team"
+  homepage "https://github.com/jo-duchan/tapflow"
+  url "https://registry.npmjs.org/tapflow/-/tapflow-0.8.2.tgz"
+  sha256 "54789c27df85922bf4d68e3746fcf1991d503feb03a6f441bc2177bc7e245715"
+  license "MIT"
+
+  depends_on :macos
+  depends_on "node"
+
+  on_macos do
+    depends_on macos: :tahoe
+  end
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tapflow --version")
+
+    output = shell_output("#{bin}/tapflow admin not-a-real-subcommand 2>&1", 1)
+    assert_match "Unknown subcommand: admin not-a-real-subcommand", output
+  end
+end
