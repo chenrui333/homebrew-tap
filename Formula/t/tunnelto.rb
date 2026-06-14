b5370d95fdb14bb9dcb8f2acd22cbae531dc6e31
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 16:49:16 +0000

    tunnelto: update 0.1.18 bottle.

--- Formula/t/tunnelto.rb
+++ Formula/t/tunnelto.rb
@@ -8,11 +8,12 @@ class Tunnelto < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "47488136cdf30e5fe1643e247dda2f11f25bb95ca83531e6620055b8ba2cfa9d"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bea6c8fb06ffe4afbccfa1b9e79fb0e476e612c44dbaacf70f325e4f08f6344a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "772d5b7d4c24a8dcce1dfce159739d541329e904b7f406a20656738b41e5b146"
-    sha256 cellar: :any,                 arm64_linux:   "6971bdfa821755775a13296cc7d81880327a31bf6bb11616b2bd518c5d45721b"
-    sha256 cellar: :any,                 x86_64_linux:  "c05d41afbf2015427dda7b1471cbdffe501b5ee1a757ca0ab379b694087efc53"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4a4265faa920380f656660d0419df796d2e2f0e430da195bf2b4d109f4b93a6e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0357db179c264c4bd202e29c75ad99ae942110a3cbebdb49a08c23fef3d28ecb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c92a0912df2fdf7a643c3d031afdc9de3ec5f31d903e0bfd4d06fa78a2924b8e"
+    sha256 cellar: :any,                 arm64_linux:   "5bf2def15babf76884d52fdf415159ad6e6284d216f0fda4c28b778b946b4906"
+    sha256 cellar: :any,                 x86_64_linux:  "18e54241f09a59a29eec061d2c687f7380c69c48d66b6d792c97fd8835ca018e"
   end
 
   depends_on "pkgconf" => :build
