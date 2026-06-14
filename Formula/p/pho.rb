370e5238772841f33f2cee49b7a30f040a4b223c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 03:29:40 +0000

    pho: update 0.1.38 bottle.

--- Formula/p/pho.rb
+++ Formula/p/pho.rb
@@ -8,11 +8,12 @@ class Pho < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8a5665b25b45fddea230b7ce2b1a0f18b4124269357270fc982be9f5677128af"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a5665b25b45fddea230b7ce2b1a0f18b4124269357270fc982be9f5677128af"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8a5665b25b45fddea230b7ce2b1a0f18b4124269357270fc982be9f5677128af"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6ca62003a65cc5686a2dd49514ca06fe1ac0e1e936bb658b171be09bf7d397fc"
-    sha256 cellar: :any,                 x86_64_linux:  "67fe098928bfd239780c4bfd77b464f7f2eb8a9f793334f7a24d0c9cc46862c5"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f7c9cda3290816207a9912947d105579b82de4895d0f52a8fbc009878a334fd7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f7c9cda3290816207a9912947d105579b82de4895d0f52a8fbc009878a334fd7"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f7c9cda3290816207a9912947d105579b82de4895d0f52a8fbc009878a334fd7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c3b26c787a11bea521bc81a672eb6d945700dc64fe5603261100027bf9be443f"
+    sha256 cellar: :any,                 x86_64_linux:  "d21e877d1732b2e3b39ed8fc08a917bfdd42d9f7db050e1db9eb4ca32089fae5"
   end
 
   depends_on "go" => :build
