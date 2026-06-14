604cc3b78f808093afc2bdbd324efc0717933bcd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 03:25:17 +0000

    hyperbolic: update 0.0.3 bottle.

--- Formula/h/hyperbolic.rb
+++ Formula/h/hyperbolic.rb
@@ -8,10 +8,12 @@ class Hyperbolic < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "07d4c1ba271d80449a25d75c2295ae6904a3ec05f814406478d488a07307015a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "256d55ef6c1eb1ca9b24e4cfe5a121e2ec3c672c755ef0fcd2687b201f8e59d4"
-    sha256 cellar: :any_skip_relocation, ventura:       "347bbb11a0c9e3a8d3d79274bf14b2e320fe471eba537f16e3e00b2a8e94b4c0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "48852823a72aa3af4376d49eb00e04c12a3518226f2110fa6cdf831a581ca807"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "04c76c43eeaed269a0b8715077e4a17f4a4873c7758763cff6403c96c12e6c0e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "04c76c43eeaed269a0b8715077e4a17f4a4873c7758763cff6403c96c12e6c0e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "04c76c43eeaed269a0b8715077e4a17f4a4873c7758763cff6403c96c12e6c0e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "55c9aa18c61dc4cf8af1f2524f624212ee5717303411aae53630151502999714"
+    sha256 cellar: :any,                 x86_64_linux:  "4702ccb31be0b65b33e8c14ed14ef30579f11fa8a5d15e275923bc2563b0c118"
   end
 
   depends_on "go" => :build
