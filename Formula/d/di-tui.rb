1da1b2ce3a335a9f53fd3922cc5f7fd6b677703d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 00:08:30 +0000

    di-tui: update 1.14.0 bottle.

--- Formula/d/di-tui.rb
+++ Formula/d/di-tui.rb
@@ -8,11 +8,12 @@ class DiTui < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7b283938970b6729c0e5a4eb84dbdac78d4ae6b62faccc3e2a82545705e1c7a9"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7b283938970b6729c0e5a4eb84dbdac78d4ae6b62faccc3e2a82545705e1c7a9"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7b283938970b6729c0e5a4eb84dbdac78d4ae6b62faccc3e2a82545705e1c7a9"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2fd12c2c945ced4e2a0afce7e1a0e9667023ff17437a2d36b372e60e0b953403"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86a612b56315709dcfb2586b81c8b4b0c62996c801d665bc08826f2cc9764408"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "44ff29717772528524d3f53c0e4c559ef5f470c963ee5a6e0b2f337be228a63a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "44ff29717772528524d3f53c0e4c559ef5f470c963ee5a6e0b2f337be228a63a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "44ff29717772528524d3f53c0e4c559ef5f470c963ee5a6e0b2f337be228a63a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "1b6d24eacd802bc74361ed4cd67545c53501f361a7c8d5d66c95f41551201111"
+    sha256 cellar: :any,                 x86_64_linux:  "af89c5b48f5a2f088eed701c198c14ad8bcef2b1da565a80673a55b7a267edb8"
   end
 
   depends_on "go" => :build
