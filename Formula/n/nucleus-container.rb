896678d7170514a2b37d85fdfbb6a62c5d8bcac7
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 16:10:38 +0000

    nucleus-container: add 0.3.3 bottle.

--- Formula/n/nucleus-container.rb
+++ Formula/n/nucleus-container.rb
@@ -6,6 +6,12 @@ class NucleusContainer < Formula
   license "Apache-2.0"
   head "https://github.com/sig-id/nucleus.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any, arm64_linux:  "f589b53e0a0242be2939c74acb74ff80df431a8e0e101c57444c3727925719b5"
+    sha256 cellar: :any, x86_64_linux: "8be57cce27cbb8714131fd38822c3dd077b27c76a1724c12849d35dc0d907f6e"
+  end
+
   depends_on "rust" => :build
   depends_on :linux
 
