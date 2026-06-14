35d13a89e780d2104d735d0b291e52f92531caca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 22:23:30 +0000

    unisondb: add 0.0.1 bottle.

--- Formula/u/unisondb.rb
+++ Formula/u/unisondb.rb
@@ -7,6 +7,15 @@ class Unisondb < Formula
   license "Apache-2.0"
   head "https://github.com/ankur-anand/unisondb.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "71b1f8cb02f87d72ad793e7b8a1f78e226657509fe6b2b6e95f01f61bcf96d36"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4d0da115245b27f9d47500be916d127eb99c2c4f6394cd56c5fb4cdeab30e90f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f4d5943567b528e8591510aebe2e673d70e3b33a16bf5506847bc13af62615cd"
+    sha256 cellar: :any,                 arm64_linux:   "b9bfd3438be13b25f8aaa505431b797b214f0aed3e1502e674d926b43ee124ac"
+    sha256 cellar: :any,                 x86_64_linux:  "7227ab7bc3904fb3cc01722f1428119279d1a0f52461a3246e11f46cf8d7447d"
+  end
+
   depends_on "go" => :build
 
   def install
