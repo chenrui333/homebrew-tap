9a4c6a0fcb552d5c31abfa1b1d0f8c6492ae4aee
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 07:10:43 +0000

    cc-bridge: add 0.1.0 bottle.

--- Formula/c/cc-bridge.rb
+++ Formula/c/cc-bridge.rb
@@ -5,6 +5,11 @@ class CcBridge < Formula
   sha256 "803e6d257e03cb628523071070b8f15e3b0687c581ab64897438c0de82b2a087"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, all: "f81e82ea342fa2bdfcedf8315dea20395ec9fb811d7f95ef501b64f5adeaef22"
+  end
+
   depends_on "node"
 
   def install
