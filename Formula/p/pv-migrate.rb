6576cde24ce482d99b7eff423aa511fe6d3a65c2
Rui Chen <rui@chenrui.dev>
2026-06-14 18:53:11 -0400

    pv-migrate 3.5.0
    Closes #8090.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/p/pv-migrate.rb
+++ Formula/p/pv-migrate.rb
@@ -1,8 +1,8 @@
 class PvMigrate < Formula
   desc "CLI tool to migrate or backup/restore Kubernetes persistent volumes"
   homepage "https://github.com/utkuozdemir/pv-migrate"
-  url "https://github.com/utkuozdemir/pv-migrate/archive/refs/tags/v3.4.0.tar.gz"
-  sha256 "b96ad3d4ec78fbf6396a9e4a9d2a5755a65b459e02f02c5b9b766d36d8b44de5"
+  url "https://github.com/utkuozdemir/pv-migrate/archive/refs/tags/v3.5.0.tar.gz"
+  sha256 "a3ddbbbe97376a240ddb37e0bfd1978b291c9a9ba23cd5883433b00dace2ee9c"
   license "Apache-2.0"
   head "https://github.com/utkuozdemir/pv-migrate.git", branch: "master"
 
@@ -21,7 +21,7 @@ class PvMigrate < Formula
     ldflags = "-s -w -X main.version=#{version} -X main.commit=#{tap.user} -X main.date=#{time.iso8601}"
     system "go", "build", *std_go_args(ldflags:), "./cmd/pv-migrate"
 
-    generate_completions_from_executable(bin/"pv-migrate", "completion")
+    generate_completions_from_executable(bin/"pv-migrate", shell_parameter_format: :cobra)
   end
 
   test do
