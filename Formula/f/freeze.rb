91e49e314235418d23a57cd0917ab56a96c1c5fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 23:31:21 +0000

    freeze: add 0.2.2 bottle.

--- Formula/f/freeze.rb
+++ Formula/f/freeze.rb
@@ -6,6 +6,15 @@ class Freeze < Formula
   license "MIT"
   head "https://github.com/charmbracelet/freeze.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "bd8601384df73d54dbe3ac7a029846914414c35cea3eceb905c95500bb7700a5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd8601384df73d54dbe3ac7a029846914414c35cea3eceb905c95500bb7700a5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "bd8601384df73d54dbe3ac7a029846914414c35cea3eceb905c95500bb7700a5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3300c0f52c8a6e6034a372ea0bf07e76472ca2e313dea5fe4e502279c36e533e"
+    sha256 cellar: :any,                 x86_64_linux:  "970fafde519f7fdf0661ce9aee584ab3c7317b618a5453ea61c99438cdfc4056"
+  end
+
   depends_on "go" => :build
 
   def install
