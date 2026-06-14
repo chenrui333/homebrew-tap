6c9674ec1958acf0b3a6d7817c70026c11546fb2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 22:06:28 +0000

    open-code-review: add 1.3.9 bottle.

--- Formula/o/open-code-review.rb
+++ Formula/o/open-code-review.rb
@@ -6,6 +6,15 @@ class OpenCodeReview < Formula
   license "Apache-2.0"
   head "https://github.com/alibaba/open-code-review.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "253514c2d9dbeb486479d19181c372efabf183dd603e7a6af1c19dcb3cc749bc"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "253514c2d9dbeb486479d19181c372efabf183dd603e7a6af1c19dcb3cc749bc"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "253514c2d9dbeb486479d19181c372efabf183dd603e7a6af1c19dcb3cc749bc"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a775a43e146c128b816fba89c312211370bcb7e55aba21ea11ffcf2fdfe898f1"
+    sha256 cellar: :any,                 x86_64_linux:  "9231b43be38b061508f3078084ad4da77929a84db18dccd3fba7de9e7199f470"
+  end
+
   depends_on "go" => :build
 
   def install
