6f64a1ab96650b5154162f2e875c4cb140978948
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 06:56:53 +0000

    oh-my-claude: update 4.14.5 bottle.

--- Formula/o/oh-my-claude.rb
+++ Formula/o/oh-my-claude.rb
@@ -8,11 +8,12 @@ class OhMyClaude < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any, arm64_tahoe:   "dc17c79c33785dbaf79262afbc4c4836e4689ceba9d51191c4581ee7f1e73e5e"
-    sha256 cellar: :any, arm64_sequoia: "199b8832d17734b4567be000ae2862ec92b3ed3c6ea2d8dd84091b0c7bffa5f7"
-    sha256 cellar: :any, arm64_sonoma:  "199b8832d17734b4567be000ae2862ec92b3ed3c6ea2d8dd84091b0c7bffa5f7"
-    sha256 cellar: :any, arm64_linux:   "f66a600894b8097ce81da1815a3d4ce873e9d704c7fc20466f4e5cc169bb6086"
-    sha256 cellar: :any, x86_64_linux:  "fa291ba7fad899a39e81930977134527ff66020837fd1e543fce19e024a03649"
+    rebuild 1
+    sha256 cellar: :any, arm64_tahoe:   "cf9f6be3a74794e4c16fc961df0e72bb4d201728577d61edc540d9ffc1e80afe"
+    sha256 cellar: :any, arm64_sequoia: "df5348d70179333b61531b7be00e929bf7a6c6a98e67dbbbac2df4800e271e0b"
+    sha256 cellar: :any, arm64_sonoma:  "df5348d70179333b61531b7be00e929bf7a6c6a98e67dbbbac2df4800e271e0b"
+    sha256 cellar: :any, arm64_linux:   "90b83bcaafd8ea77195ad3678e1e533f52c94150f433908a1610ec6588fde40f"
+    sha256 cellar: :any, x86_64_linux:  "cc122408d6c2788d0bb9b83efb5f70ebc6bb0fc3d5e08a946fb07cbf972bdff7"
   end
 
   depends_on "node"
