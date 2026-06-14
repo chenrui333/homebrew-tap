d5f9f9a9eddb7236db78dce9003f6d695063128c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-14 06:49:27 +0000

    agent-pd: add 0.2.0 bottle.

--- Formula/a/agent-pd.rb
+++ Formula/a/agent-pd.rb
@@ -8,6 +8,15 @@ class AgentPd < Formula
   license "Apache-2.0"
   head "https://github.com/varmabudharaju/agent-pd.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cd4e8aa6f77d4dc83992ed1da893adb4e71f0ce12ac7a6e909c1485e6085c283"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "796b0de15d7f32f6fedb9663d16cb3b88c155ec93e1a542815c4a93b7adc0654"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14bb7570ceec3293b574440ef23393d057376582fa48ef6c0f43048643581572"
+    sha256 cellar: :any,                 arm64_linux:   "fe32f7217f8ff056ebf9de1d99a804d7539741ac7b9f744f54443e3ccc3db5e3"
+    sha256 cellar: :any,                 x86_64_linux:  "9991012914d6adfaf720309aab95a7444f062a7c4433619d8aa01c463f47f841"
+  end
+
   depends_on "libyaml"
   depends_on "python@3.13"
 
