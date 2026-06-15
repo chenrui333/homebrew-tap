cbc2e7248bb5340459432f44112c45b7f95589ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-15 05:58:19 +0000

    hielo: add 0.5.0 bottle.

--- Formula/h/hielo.rb
+++ Formula/h/hielo.rb
@@ -6,6 +6,15 @@ class Hielo < Formula
   license "MIT"
   head "https://github.com/atcol/hielo.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a2bd7cf5f86757e5981933661e74e00e523bdd2e8c788162a718e2b2e30f2ca9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "300c65ce6803562f317a3da209325bc5cf077fae87211f7fa51ed5f2616fbf8f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a01f43de752ff2086e2d78d2a01afca35954bb9ca832866fceca7bb8c1239b88"
+    sha256 cellar: :any,                 arm64_linux:   "7181fd35ffa3f45cbbbc5879b351a383e7d41c088eca2afeef2f073729a64a6f"
+    sha256 cellar: :any,                 x86_64_linux:  "585405c0f1ff4f6ce4781586282306b4e76a25a63863efdf27bfd7ddc2fa0be6"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
 
