a02c1af0b4dd97f24796077d8e9de8eff5b44fcb
Rui Chen <rui@chenrui.dev>
2026-06-19 12:34:31 -0400

    ci: migrate Linux CI to Ubuntu 24.04 (#8294)
    * ci: migrate Linux CI to Ubuntu 24.04
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * ci: address Ubuntu 24.04 review feedback
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- .github/scripts/environment.js
+++ .github/scripts/environment.js
@@ -21,10 +21,12 @@ module.exports = async ({github, context, core}, formula_detect) => {
         pull_number: context.issue.number
     })).data.labels : []
     const label_names = labels.map(label => label.name)
-    const linux_runner = 'ubuntu-22.04'
-    const linux_arm64_runner = 'ubuntu-22.04-arm'
+    const linux_runner = 'ubuntu-24.04'
+    const linux_arm64_runner = 'ubuntu-24.04-arm'
     const container = {
-        image: 'ghcr.io/homebrew/brew:main',
+        // Keep the generated matrix on Homebrew's main tag so formula CI follows
+        // current test-bot behavior; Renovate does not update JS-embedded digests.
+        image: 'ghcr.io/homebrew/ubuntu24.04:main',
         options: '--user=linuxbrew -e GITHUB_ACTIONS_HOMEBREW_SELF_HOSTED'
     }
 
