2a519bfab7644c2c1789ceb4b69fb0f0bbdd8dbc
Rui Chen <rui@chenrui.dev>
2026-06-19 08:57:14 -0400

    deletor 2.0.0
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/d/deletor.rb
+++ Formula/d/deletor.rb
@@ -1,8 +1,8 @@
 class Deletor < Formula
   desc "TUI and CLI to manage and delete files efficiently"
   homepage "https://github.com/pashkov256/deletor"
-  url "https://github.com/pashkov256/deletor/archive/refs/tags/v1.5.0.tar.gz"
-  sha256 "e4a2a2ca3a1fa420c1962c2d8dd1778a3fbd06dc9a468e6d5f76b9d26971bbeb"
+  url "https://github.com/pashkov256/deletor/archive/refs/tags/2.0.0.tar.gz"
+  sha256 "6bd2c548fc85f0e7896bf2324ffd4b31248a6d78a2ae6d7405f3e229c4fb5f81"
   license "MIT"
   head "https://github.com/pashkov256/deletor.git", branch: "main"
 
