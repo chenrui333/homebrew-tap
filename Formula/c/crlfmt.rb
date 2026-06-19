cf0e223a3c9c942d61d65f8e71bce0d4433ca32a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 12:48:29 +0000

    crlfmt 0.5.2
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/crlfmt.rb
+++ Formula/c/crlfmt.rb
@@ -1,8 +1,8 @@
 class Crlfmt < Formula
   desc "Formatter for CockroachDB's additions to the Go style guide"
   homepage "https://github.com/cockroachdb/crlfmt"
-  url "https://github.com/cockroachdb/crlfmt/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "7145c34ab1b569be92a46c46973fc66f55ea775960395e426031579ffb277d4f"
+  url "https://github.com/cockroachdb/crlfmt/archive/refs/tags/v0.5.2.tar.gz"
+  sha256 "819a564a01d27c2a4033327b4db129dbccea505c6623fe6735e957eef338eb8d"
   license "Apache-2.0"
   head "https://github.com/cockroachdb/crlfmt.git", branch: "master"
 
@@ -15,13 +15,15 @@ class Crlfmt < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "f12403bb0847d76d1f1311a0c4c8fe026976b94d3320d822311e17e62bb6002d"
   end
 
-  depends_on "go" => :build
+  depends_on "go"
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w")
   end
 
   test do
+    # FIXME: Upstream does not expose a version command; replace this with a version assertion when available.
+
     (testpath/"test.go").write <<~GO
       package main
 
