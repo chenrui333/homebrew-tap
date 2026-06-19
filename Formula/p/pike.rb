131c26e55c54b274a5591778d312c474000e97da
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 14:36:51 +0000

    pike 1.0.3

--- Formula/p/pike.rb
+++ Formula/p/pike.rb
@@ -2,8 +2,8 @@
 class Pike < Formula
   desc "Tool for determining the permissions or policy required for IAC code"
   homepage "https://github.com/jamesWoolfenden/pike"
-  url "https://github.com/JamesWoolfenden/pike/archive/refs/tags/v0.3.39.tar.gz"
-  sha256 "728c17762378b5469e30637a06b1921414694199da1dc3c317c14f70061cb740"
+  url "https://github.com/JamesWoolfenden/pike/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "9d56b5e2038180f715d42b2acb8e45ea08ada6475d8afb529601dd96bd9488d5"
   license "Apache-2.0"
   head "https://github.com/jamesWoolfenden/pike.git", branch: "master"
 
