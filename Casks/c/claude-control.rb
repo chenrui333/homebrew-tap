b7ffdc0d72b3f127fb1295c11ecb521415003a6e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 12:41:19 +0000

    claude-control 0.17.0

--- Casks/c/claude-control.rb
+++ Casks/c/claude-control.rb
@@ -1,9 +1,9 @@
 cask "claude-control" do
   arch arm: "-arm64", intel: ""
 
-  version "0.16.0"
-  sha256 arm:   "dfecffe1ec54abdebe5e04db89106529bedfdcfd7591d88996253e6081012dce",
-         intel: "1d11ee0465ef1fce6472711fc581346959f35c9949bf8e89fff5fb55babd29ef"
+  version "0.17.0"
+  sha256 arm:   "16d0b70ba7402c7ad793399d1d79a9bb4e280c1e5c946fb938537cf7e89b50d4",
+         intel: "fb2aa86213b3680b1146017ef8ce54861fe9b6bf8fc0eb431c317ebc9a63cd20"
 
   url "https://github.com/sverrirsig/claude-control/releases/download/v#{version}/Claude.Control-#{version}#{arch}.dmg"
   name "Claude Control"
