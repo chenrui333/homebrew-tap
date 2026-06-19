25c60dbba4af4f2d3c092c125928da7b2a547d9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 12:50:39 +0000

    epiq 0.7.4

--- Formula/e/epiq.rb
+++ Formula/e/epiq.rb
@@ -1,8 +1,8 @@
 class Epiq < Formula
   desc "Distributed terminal-native issue tracker backed by Git"
   homepage "https://github.com/ljtn/epiq"
-  url "https://registry.npmjs.org/epiq/-/epiq-0.6.20.tgz"
-  sha256 "e7b1f77920486997a111db3adc45488a0bce85cbc5cc962b7bb0c9514bc7154e"
+  url "https://registry.npmjs.org/epiq/-/epiq-0.7.4.tgz"
+  sha256 "68162c9e3cc87ca88dbd89ec631e4fa4951f2df3fdb2aa4b7cbb6bf2846bd403"
   license "MIT"
 
   bottle do
