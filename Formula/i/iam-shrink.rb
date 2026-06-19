c54fd7221903ca55ee25e07bb0f0afc8ef69243a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-19 19:55:13 +0000

    iam-shrink: update 0.1.78 bottle.

--- Formula/i/iam-shrink.rb
+++ Formula/i/iam-shrink.rb
@@ -7,7 +7,7 @@ class IamShrink < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "767bea860da916221f474985b7a9010771a963174c0b3cf4f151f48b419da6b5"
+    sha256 cellar: :any_skip_relocation, all: "07809d99234fa16fe097a7311bdd510b388d722933b5961177f83b5c7e044876"
   end
 
   depends_on "node"
