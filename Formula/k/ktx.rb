cb310e9107c21f5cb1f0bb8aec25ee3392da8373
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-20 09:31:26 +0000

    ktx 0.13.0
    Closes #8332.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k/ktx.rb
+++ Formula/k/ktx.rb
@@ -1,8 +1,8 @@
 class Ktx < Formula
   desc "Executable context layer for data and analytics agents"
   homepage "https://github.com/Kaelio/ktx"
-  url "https://registry.npmjs.org/@kaelio/ktx/-/ktx-0.12.0.tgz"
-  sha256 "aa704e407d3f19b7cd7f7669b7be5bd69341114a583963abc2f9db034acde660"
+  url "https://registry.npmjs.org/@kaelio/ktx/-/ktx-0.13.0.tgz"
+  sha256 "5f3bce91f20c3b8c9b13a9a8a4e10102162b61609e8e7b24ed0b88af1fde1816"
   license "Apache-2.0"
 
   bottle do
