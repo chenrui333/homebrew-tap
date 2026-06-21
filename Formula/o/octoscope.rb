507ba3504b821dd1f8abe0f7a3acbcaac374d9d8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-21 08:05:50 +0000

    octoscope 0.20.2

--- Formula/o/octoscope.rb
+++ Formula/o/octoscope.rb
@@ -1,8 +1,8 @@
 class Octoscope < Formula
   desc "Terminal dashboard for your GitHub account"
   homepage "https://github.com/gfazioli/octoscope"
-  url "https://github.com/gfazioli/octoscope/archive/refs/tags/v0.20.1.tar.gz"
-  sha256 "47ed13cc272838192e0ed59bd7fb3441f4d0c1595c1116b00fc69b64b3578a9c"
+  url "https://github.com/gfazioli/octoscope/archive/refs/tags/v0.20.2.tar.gz"
+  sha256 "2ab2ee18f84c992cc1e5d6295962efec2047bc4aa3392420b167246e50929367"
   license "MIT"
   head "https://github.com/gfazioli/octoscope.git", branch: "main"
 
