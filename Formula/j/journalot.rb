88e9a7550ebc993924b6d49c6148821278046a2a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-21 15:10:22 +0000

    journalot 5.6.0
    Closes #8365.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/j/journalot.rb
+++ Formula/j/journalot.rb
@@ -1,8 +1,8 @@
 class Journalot < Formula
   desc "Minimal journaling CLI for developers"
   homepage "https://github.com/jtaylortech/journalot"
-  url "https://github.com/jtaylortech/journalot/archive/refs/tags/v5.2.1.tar.gz"
-  sha256 "1c4f93eda09312891e3c3d8d1f7ed0844df4b7c3399407b99ec47f13dc20a188"
+  url "https://github.com/jtaylortech/journalot/archive/refs/tags/v5.6.0.tar.gz"
+  sha256 "338d4163c13d1178f5588165db7b55a3b88a358233057c06669b3703e8469a38"
   license "MIT"
   head "https://github.com/jtaylortech/journalot.git", branch: "main"
 
