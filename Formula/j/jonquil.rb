6e82075bc72a7963b14cd4223dec2ce540aa1ccd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 11:25:55 +0000

    jonquil 0.3.2
    Closes #8463.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/j/jonquil.rb
+++ Formula/j/jonquil.rb
@@ -1,8 +1,8 @@
 class Jonquil < Formula
   desc "JSON parser on top of TOML implementation (Fortran)"
   homepage "https://github.com/toml-f/jonquil"
-  url "https://github.com/toml-f/jonquil/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "9742a1b15fe4e7c3332f501dcedc4f9559dfa37884ca055ff5b5508ba0901749"
+  url "https://github.com/toml-f/jonquil/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "963f7f12128bc45dc3313df87dd4a3ba4b8ff20f38fdec2408b2a6391cf7aae2"
   license any_of: ["Apache-2.0", "MIT"]
   head "https://github.com/toml-f/jonquil.git", branch: "main"
 
@@ -21,9 +21,12 @@ class Jonquil < Formula
   depends_on "chenrui333/tap/toml-f"
   depends_on "gcc" # for gfortran
 
-  patch :DATA
-
   def install
+    inreplace "meson.build", "description: 'Bringing TOML blooms to JSON land',", <<~MESON.chomp
+      description: 'Bringing TOML blooms to JSON land',
+          requires: 'toml-f',
+    MESON
+
     system "meson", "setup", "build", "-Dtests=false", *std_meson_args
     system "meson", "compile", "-C", "build", "--verbose"
     system "meson", "install", "-C", "build"
@@ -34,33 +37,28 @@ class Jonquil < Formula
 
     (testpath/"t.f90").write <<~F90
       program t
-      print *, "ok"
+        use jonquil
+        implicit none
+
+        class(json_value), allocatable :: data
+        type(json_object), pointer :: object
+        character(:), allocatable :: name
+        integer :: count
+
+        call json_loads(data, '{"name": "Fortran", "count": 42}')
+        object => cast_to_object(data)
+        call get_value(object, "name", name)
+        call get_value(object, "count", count)
+
+        print '(a,a)', "Name: ", name
+        print '(a,i0)', "Count: ", count
       end program t
     F90
-    system Formula["gcc"].opt_bin/"gfortran", "t.f90", "-o", "test"
-    system "./test"
+    cflags = shell_output("pkgconf --cflags jonquil").chomp.split
+    libs = shell_output("pkgconf --libs jonquil").chomp.split
+    system formula_opt_bin("gcc")/"gfortran", "t.f90", *cflags, *libs, "-o", "test"
+    output = shell_output("./test")
+    assert_match "Name: Fortran", output
+    assert_match "Count: 42", output
   end
 end
-
-__END__
-diff --git a/meson.build b/meson.build
-index cbb35f0..2dd1f9c 100644
---- a/meson.build
-+++ b/meson.build
-@@ -77,4 +77,6 @@ if install
- endif
-
- # add the testsuite
--subdir('test')
-+if get_option('tests')
-+  subdir('test')
-+endif
-diff --git a/meson_options.txt b/meson_options.txt
-new file mode 100644
-index 0000000..93d92ff
---- /dev/null
-+++ b/meson_options.txt
-@@ -0,0 +1,3 @@
-+# Build options for toml-f
-+option('tests',
-+  type: 'boolean', value: false, description: 'Build test suite')
