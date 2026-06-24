88c205124a5e2c8e4f59f4b55a6acee15d40cce9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-24 03:36:29 +0000

    tars 1.37.0

--- Formula/t/tars.rb
+++ Formula/t/tars.rb
@@ -1,8 +1,8 @@
 class Tars < Formula
   desc "Local-first autonomous AI supervisor and sidekick powered by Google Gemini"
   homepage "https://github.com/agustinsacco/tars"
-  url "https://registry.npmjs.org/@saccolabs/tars/-/tars-1.31.0.tgz"
-  sha256 "8475585cafc20423728364602b34e3a4c9ebac0dd8ad8858010b3a4107f73895"
+  url "https://registry.npmjs.org/@saccolabs/tars/-/tars-1.37.0.tgz"
+  sha256 "de60654286d1fd2d81a8138fe64f14b282c3d84c5e21b29c509c3d7eeeff317e"
   license "MIT"
 
   bottle do
