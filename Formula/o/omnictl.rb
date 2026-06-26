d0a11974c4ddb12d4afc7f11e1a80d57f9c681cb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-26 08:02:58 +0000

    omnictl: update 1.9.0 bottle.

--- Formula/o/omnictl.rb
+++ Formula/o/omnictl.rb
@@ -8,11 +8,11 @@ class Omnictl < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "29eb8306c49ec2fb7a988d19556ff6a46a3f3dbd7ea50c4dd243d0d71226023a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1fed3044f57b1cb7efb2844e64ba90fedd90858c69f64711243611b83cfe5e9d"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7635ce344817861e9e7234c58ad9eb401b8f5e3229641b36c075dd479cda8cd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "26d628633d3a5ee8a6691ef2aa2d7be9abde8bbbe534fc8fe558a9839d221d2e"
-    sha256 cellar: :any,                 x86_64_linux:  "2ef2cf05846edf1bcd752795567fa1adf41016730cd4e95672d96692e3f82fd0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ab7ffa52481d372ef6ef4de9e4139d59f2f3f0e4a5fc009a51c62b49fe8f2fbe"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cea94fa26fa16427f7ce1882758e8055ba84dd185475b9cef19afb8b46df1cd8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f95349a8e86c1a688a29f7664fd326a5334112193ef3c17d733aaf10686d664f"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "280b879ee4fba7a6a0babd0253c8525de4bbeb32a384b583fa00cc87d5f82a69"
+    sha256 cellar: :any,                 x86_64_linux:  "70a00bfb7cb2bf416dc98a0300f1e48a0aa6c1932be3e0ecf90380621ded41a1"
   end
 
   depends_on "go" => :build
