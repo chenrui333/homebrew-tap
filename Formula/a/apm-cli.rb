0de4dad3ef9a4f8634b1fc4c67f0243b6362d804
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-06-27 09:10:50 +0000

    apm-cli 0.22.0

--- Formula/a/apm-cli.rb
+++ Formula/a/apm-cli.rb
@@ -3,8 +3,8 @@ class ApmCli < Formula
 
   desc "Open-source dependency manager for AI agents"
   homepage "https://github.com/microsoft/apm"
-  url "https://files.pythonhosted.org/packages/94/c9/577024b83129768eec233ee862a4a024793ee0ae48d879c19fe098c8ca9a/apm_cli-0.21.0.tar.gz"
-  sha256 "b3ffd561db22debc5c43f05a775048b9419d49467b99ccf10cc44b4b9b26d780"
+  url "https://files.pythonhosted.org/packages/b7/9b/93da92e753f6e0d0f0aa62ae60be7b03a196c72def0b44ad31ace427b595/apm_cli-0.22.0.tar.gz"
+  sha256 "4a78e51cf0afab0ea88ab42201a78f8738d4d6c4faaa035cf8bddfdb80275566"
   license "MIT"
 
   bottle do
@@ -43,8 +43,8 @@ class ApmCli < Formula
   end
 
   resource "anyio" do
-    url "https://files.pythonhosted.org/packages/1c/b5/001890774a9552aff22502b8da382593109ce0c95314abaebbb116567545/anyio-4.14.0.tar.gz"
-    sha256 "b47c1f9ccf73e67021df785332508f99379c68fa7d0684e8e3492cb1d4b23f89"
+    url "https://files.pythonhosted.org/packages/3b/72/5562aabb8dd7181e8e860622a38bea08d17842b99ecd4c91f84ac95251b0/anyio-4.14.1.tar.gz"
+    sha256 "8d648a3544c1a700e3ff78615cd679e4c5c3f149904287e73687b2596963629e"
   end
 
   resource "attrs" do
@@ -73,8 +73,8 @@ class ApmCli < Formula
   end
 
   resource "click" do
-    url "https://files.pythonhosted.org/packages/9b/98/518d8e5081007684232226f475082b30087d0f585e8457db087298259f49/click-8.4.1.tar.gz"
-    sha256 "918b5633eddf6b41c32d4f454bf0de810065c74e3f7dbf8ee5452f8be88d3e96"
+    url "https://files.pythonhosted.org/packages/76/d4/81420972a676e8ffea40450d8c8c92943e7218a78fe9b64359836cc9876b/click-8.4.2.tar.gz"
+    sha256 "9a6cea6e60b17ebe0a44c5cc636d94f09bd66142c1cd7d8b4cd731c4917a15f6"
   end
 
   resource "click-default-group" do
@@ -173,8 +173,8 @@ class ApmCli < Formula
   end
 
   resource "openai" do
-    url "https://files.pythonhosted.org/packages/f3/fa/88d0c58a0c58df7e6758e66b99c5d028d5e0bb49f8812d7203940cd9dbf1/openai-2.43.0.tar.gz"
-    sha256 "e74d238200a26868977002190fb6631613480a93dfe0c9c982e77021ed60a017"
+    url "https://files.pythonhosted.org/packages/49/f5/7c7cb955305cb41f7f3c5fd7e0e38bf6bbf2658468863d4b7b868a5cb8df/openai-2.44.0.tar.gz"
+    sha256 "68a5a5ffad82b8ff7d451c437529fb64f7c3b8123aaf0c021966a882d9e3947d"
   end
 
   resource "pluggy" do
