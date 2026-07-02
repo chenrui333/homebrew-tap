52aa144b4fb69f0e35fe027d1b4c6fe93b988185
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 14:53:31 +0000

    cnspec: update 13.27.3 bottle.

--- Formula/c/cnspec.rb
+++ Formula/c/cnspec.rb
@@ -8,11 +8,11 @@ class Cnspec < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7bf43b77d1205afbdda80dee68db9a8f8de2f8b952366f0d59b1790b358eef34"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e2e334bf68b3b5a8203c098850ea28b40cf45f3bdfe748f8f771e2a7907524ed"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "19c2c50fd54399bfb427a78556d1aa397053f5b4bfc87b79850912c461091c41"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "29abcaf672b96ba4f57594ee68b1b9816f9102d7987ada7b742cc573b8c8c7e8"
-    sha256 cellar: :any,                 x86_64_linux:  "4f835edbda7f5473aafedb7af36326832ebe035081f27d42e8ce6e49da6b92ce"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6e3eb3bb9387e74d74ea11e1d86fdeb8eb997207ad49bc64c8d3a4d58c09b7b5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c35477ce2ef498149b7b0fff3353911a7deddba5616c9ec0e68738b0065d7c2c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8934f1f32d61d1b767a8fe515c1492a700981cec95bdd43706731427d01b9e08"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "e435453c9ff60de33f2c23bde5365de06c412a2175d18cce000122ca36794bf0"
+    sha256 cellar: :any,                 x86_64_linux:  "b9a6cba8812b8c544ea04ede08e5381ecb821dfb76400330d9f8163338bd7afe"
   end
 
   depends_on "go" => :build
