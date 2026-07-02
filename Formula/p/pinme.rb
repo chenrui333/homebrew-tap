e7f2d7eeb6aa8a70d19aa18722f2b614cd6c9956
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-02 11:01:51 +0000

    pinme 2.0.11

--- Formula/p/pinme.rb
+++ Formula/p/pinme.rb
@@ -1,8 +1,8 @@
 class Pinme < Formula
   desc "Deploy Your Frontend in a Single Command"
   homepage "https://pinme.eth.limo/"
-  url "https://registry.npmjs.org/pinme/-/pinme-2.0.10.tgz"
-  sha256 "2e321c51ea141861bd612195ec2675cd47f2ab9f4d392ddece9087d6979ad6bf"
+  url "https://registry.npmjs.org/pinme/-/pinme-2.0.11.tgz"
+  sha256 "18213b232abcc27f4c86ae20b1546a3ca9a1d3e8b7380e603425201ed910d379"
   license "MIT"
 
   bottle do
