a252d618d15d88aa9cdc53dfd1d039f8b6b7db0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-03 22:01:45 +0000

    mdns-scanner: update 0.27.4 bottle.

--- Formula/m/mdns-scanner.rb
+++ Formula/m/mdns-scanner.rb
@@ -8,11 +8,11 @@ class MdnsScanner < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6e6fba8aec5a83c92228bdd352999be04718f543739da425bffa4376250979ae"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b33d9e355bd8e9d81002951cdddf48c3e96e1543241502a28910f3e0e5b3c551"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "0662ba856d75ea04bf560fd6a5e0f28af32bfc66140991079e25acdbf3014359"
-    sha256 cellar: :any,                 arm64_linux:   "ab9997a0f11cae765197352039e5d78d38b565ef43c2fee24ff9d2a5c79dcbb8"
-    sha256 cellar: :any,                 x86_64_linux:  "127f92ea50bd23618890503a2d0535cd948099de2fed54e3d1b7ce2eeab97142"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4e828cff871e97a064e76a0fbfc77041c0588b88e6818005223b466aca2358ab"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "87b8b7d0adff780b3cebc0edab7236acf2f2f5130bcf3faea9cddf781c633b9a"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "716576d221119d4e54f334f31ab2c6ec467ea2b37e59c201cd1d31f24ec705d0"
+    sha256 cellar: :any,                 arm64_linux:   "b55659e4ac1f2497c67b19466587ab293d6011799e44cb90c6bd2731f3fbacf2"
+    sha256 cellar: :any,                 x86_64_linux:  "fede69596ab281e94762ca875d3a546d84e34cd05b463ab95e9ae6ccc0af1841"
   end
 
   depends_on "rust" => :build
