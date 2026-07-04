b8b3e667a791ca01180dcbb576c0a3095a9017ce
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-04 04:17:22 +0000

    zerofs 2.0.2

--- Formula/z/zerofs.rb
+++ Formula/z/zerofs.rb
@@ -1,8 +1,8 @@
 class Zerofs < Formula
   desc "Serve S3 buckets as POSIX filesystems over NFS, 9P, or as block devices"
   homepage "https://github.com/Barre/ZeroFS"
-  url "https://github.com/Barre/ZeroFS/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "caecbcb8e88534e29c0f125b46668de83851a7843f5d360bc9c73eda07c6bd4c"
+  url "https://github.com/Barre/ZeroFS/archive/refs/tags/v2.0.2.tar.gz"
+  sha256 "5ee6ceb1531f3df268abaa22867008d96ef48f38f462a8a155ceaf53b75715c8"
   license "AGPL-3.0-only"
   head "https://github.com/Barre/ZeroFS.git", branch: "main"
 
