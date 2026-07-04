12c955c805ec61035851f70fa4329e6f6cda4801
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-04 14:03:58 +0000

    gh-dash 4.25.0

--- Formula/g/gh-dash.rb
+++ Formula/g/gh-dash.rb
@@ -1,8 +1,8 @@
 class GhDash < Formula
   desc "Terminal UI for GitHub"
   homepage "https://github.com/dlvhdr/gh-dash"
-  url "https://github.com/dlvhdr/gh-dash/archive/refs/tags/v4.24.1.tar.gz"
-  sha256 "928f39ef26711d49d81e827e9759482add511c5fe792c788214db2fdb09bbe34"
+  url "https://github.com/dlvhdr/gh-dash/archive/refs/tags/v4.25.0.tar.gz"
+  sha256 "cd36a9f004e3a40d6acdea5183429a50e07ebbefad03eb95915d09ac9d37db15"
   license "MIT"
 
   bottle do
