3b91916b2b6d2e6f044b0dae551059fab7cd8bf6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-05 14:14:42 +0000

    iam-shrink 0.1.81

--- Formula/i/iam-shrink.rb
+++ Formula/i/iam-shrink.rb
@@ -1,8 +1,8 @@
 class IamShrink < Formula
   desc "Make AWS IAM policies smaller by adding wildcards to actions"
   homepage "https://iam.cloudcopilot.io/tools/iam-shrink"
-  url "https://registry.npmjs.org/@cloud-copilot/iam-shrink/-/iam-shrink-0.1.80.tgz"
-  sha256 "3f899e8bddcbaba5eb421c727fad79fc19b80526497928fa8ac42b6617d12b88"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-shrink/-/iam-shrink-0.1.81.tgz"
+  sha256 "a9478c5901bf6eefe3bcc3db6b5a77e9071cfad252aaded89c8e9ef7ca6fa35d"
   license "AGPL-3.0-or-later"
 
   bottle do
