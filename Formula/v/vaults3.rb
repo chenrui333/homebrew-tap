1879588400b64f73164636cfac0cb27757912696
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-05 21:38:10 +0000

    vaults3 4.4.4

--- Formula/v/vaults3.rb
+++ Formula/v/vaults3.rb
@@ -1,8 +1,8 @@
 class Vaults3 < Formula
   desc "Lightweight, S3-compatible object storage server with built-in web dashboard"
   homepage "https://github.com/Kodiqa-Solutions/VaultS3"
-  url "https://github.com/Kodiqa-Solutions/VaultS3/archive/refs/tags/v4.4.3.tar.gz"
-  sha256 "a76f085a1710fdc10c8c4415790e9b60ac831804635b2ad15e75fa56ee0b6b5f"
+  url "https://github.com/Kodiqa-Solutions/VaultS3/archive/refs/tags/v4.4.4.tar.gz"
+  sha256 "b383d102a246b1bd4e88f0eeea546716ca0a03d79c4ccf5e3af6017bebf2fdfe"
   license "AGPL-3.0-only"
   head "https://github.com/Kodiqa-Solutions/VaultS3.git", branch: "main"
 
