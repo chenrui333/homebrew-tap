e72e01a61b85c9cd500a7e7ded2ee594ea0ae037
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-09 15:53:05 +0000

    openclacky 1.3.10

--- Formula/o/openclacky.rb
+++ Formula/o/openclacky.rb
@@ -1,8 +1,8 @@
 class Openclacky < Formula
   desc "Token-efficient open-source AI Agent with skill system and IM integrations"
   homepage "https://github.com/clacky-ai/openclacky"
-  url "https://github.com/clacky-ai/openclacky/archive/refs/tags/v1.3.6.tar.gz"
-  sha256 "a94d9deca8f33fe8537ac4ce41a6731947ff8ef142a359ae774e42eb9830d83e"
+  url "https://github.com/clacky-ai/openclacky/archive/refs/tags/v1.3.10.tar.gz"
+  sha256 "3c14c5d356f57bd60ad5474d8a60f8c501e517aa67107700942d7e7131497933"
   license "MIT"
   head "https://github.com/clacky-ai/openclacky.git", branch: "main"
 
