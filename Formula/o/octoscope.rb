848281891ad4734db9b30581ab66088adb05e9b0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-11 20:55:08 +0000

    octoscope: update 0.24.1 bottle.

--- Formula/o/octoscope.rb
+++ Formula/o/octoscope.rb
@@ -8,11 +8,11 @@ class Octoscope < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3746ccc6bd6e29ef3a92bcf474a0af5a48bf09d1efc1921ad5090da435b3698b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3746ccc6bd6e29ef3a92bcf474a0af5a48bf09d1efc1921ad5090da435b3698b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3746ccc6bd6e29ef3a92bcf474a0af5a48bf09d1efc1921ad5090da435b3698b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ed95c290c12fc2ae9d97b853f4eceab26f6be9daa203ae83c182ef94f0e2a104"
-    sha256 cellar: :any,                 x86_64_linux:  "33f062deb38eebec4e6e102e9509a6bf3e3a94884ce197808a97ac410b0ee376"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e6e843d006b62bf0efe3a732a6d43395c105ef2d56df890f72803e437a9f6633"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e6e843d006b62bf0efe3a732a6d43395c105ef2d56df890f72803e437a9f6633"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e6e843d006b62bf0efe3a732a6d43395c105ef2d56df890f72803e437a9f6633"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "71b19e9eae09c7b26f98eb7b9bfc8d77e12d0e481eb66125cf8a42d1399cf3ce"
+    sha256 cellar: :any,                 x86_64_linux:  "c4eeb9b666faf9ca477c521d6726bb802b031f2dcc6abd6a3970d92d4fb5986d"
   end
 
   depends_on "go" => :build
