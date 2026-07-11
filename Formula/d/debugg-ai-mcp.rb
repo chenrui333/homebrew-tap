1724dbac454f1b79cf3f2aa6811537b05d96e804
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-11 19:39:20 +0000

    debugg-ai-mcp 3.5.2
    Closes #9074.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/d/debugg-ai-mcp.rb
+++ Formula/d/debugg-ai-mcp.rb
@@ -2,8 +2,8 @@
 class DebuggAiMcp < Formula
   desc "MCP Server for Debugg AI"
   homepage "https://debugg.ai/"
-  url "https://registry.npmjs.org/@debugg-ai/debugg-ai-mcp/-/debugg-ai-mcp-3.5.1.tgz"
-  sha256 "51b73659401ae1df28090f984178be9afce1a810d8d9b0743d716976a551b147"
+  url "https://registry.npmjs.org/@debugg-ai/debugg-ai-mcp/-/debugg-ai-mcp-3.5.2.tgz"
+  sha256 "7a8bb95330496fb3c10ea526ecfe9df900c458b3f7cab880ca01f20977aac85a"
   license "Apache-2.0" # license fix PR, https://github.com/debugg-ai/debugg-ai-mcp/pull/4
 
   bottle do
