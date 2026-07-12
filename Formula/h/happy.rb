9be5fe395cbb79eac7a44a634e5df54ed70afdda
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-12 10:25:18 +0000

    happy: update 1.2.0 bottle.

--- Formula/h/happy.rb
+++ Formula/h/happy.rb
@@ -7,11 +7,11 @@ class Happy < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "eaa15669ffc0e0a8af4cafe18e5856634950b8cc6bac9542a76000b63fc355d5"
-    sha256                               arm64_sequoia: "eaa15669ffc0e0a8af4cafe18e5856634950b8cc6bac9542a76000b63fc355d5"
-    sha256                               arm64_sonoma:  "eaa15669ffc0e0a8af4cafe18e5856634950b8cc6bac9542a76000b63fc355d5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "225875f8377b99e6abf05ead57eee7e9db82b8c2bdf4a66bf18ec27e5e8d5bce"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "86dc62466c339752612f4be7c36faad9d2bbeb743b2873361bf984b55f213f30"
+    sha256                               arm64_tahoe:   "62a6460b9aabd1e1a88f2e10ebc8657d5c28bd067ad82b563d8683b562c8518b"
+    sha256                               arm64_sequoia: "62a6460b9aabd1e1a88f2e10ebc8657d5c28bd067ad82b563d8683b562c8518b"
+    sha256                               arm64_sonoma:  "62a6460b9aabd1e1a88f2e10ebc8657d5c28bd067ad82b563d8683b562c8518b"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0afbfd96dd9887ae704673f5e287307bdbf7dba081c5ed37215dcc7a682bb687"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ac2860a9ff8b6a810b5404215f17b363c67866b40ec81fb3426c20361a14c5da"
   end
 
   depends_on "node"
