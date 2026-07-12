25de743bcd3442cfb63cb72fc58a17a636a1b520
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-12 20:59:33 +0000

    cmake-init: add 0.41.1 bottle.

--- Formula/c/cmake-init.rb
+++ Formula/c/cmake-init.rb
@@ -8,6 +8,16 @@ class CmakeInit < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/friendlyanon/cmake-init.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8771da8c5a5be962c95d62453b28e26ea1d44ffe4f467dab18689e0b703d3e47"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8771da8c5a5be962c95d62453b28e26ea1d44ffe4f467dab18689e0b703d3e47"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8771da8c5a5be962c95d62453b28e26ea1d44ffe4f467dab18689e0b703d3e47"
+    sha256 cellar: :any_skip_relocation, sequoia:       "8771da8c5a5be962c95d62453b28e26ea1d44ffe4f467dab18689e0b703d3e47"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "0bdf2b7876f51f2e377a8453e17dbefd83398f3f1eae02a6b28720437a06e2da"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34aea663849137c64d219c6059247e9bfd45030a1f9891e6918a9048f0a11903"
+  end
+
   depends_on "python@3.14"
 
   def install
