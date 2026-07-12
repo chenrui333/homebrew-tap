b96424ce489be957679ffa0bbf5c433318aa07f8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-12 14:09:52 +0000

    semaphore 2.18.26

--- Formula/s/semaphore.rb
+++ Formula/s/semaphore.rb
@@ -1,8 +1,8 @@
 class Semaphore < Formula
   desc "Modern UI and powerful API for Ansible, Terraform/OpenTofu/Terragrunt"
   homepage "https://filebrowser.org/"
-  url "https://github.com/semaphoreui/semaphore/archive/refs/tags/v2.18.17.tar.gz"
-  sha256 "f7a3d62e066c951327d5cea1ee42f80663e06d112537586d54c08220e3c4eb3a"
+  url "https://github.com/semaphoreui/semaphore/archive/refs/tags/v2.18.26.tar.gz"
+  sha256 "2c37baafe263ec0139d748344348c92095e6fd915a29f57d3ecc022e69356ec9"
   license "MIT"
   head "https://github.com/semaphoreui/semaphore.git", branch: "develop"
 
