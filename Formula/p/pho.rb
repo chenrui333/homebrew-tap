2c459ca24d654c3039cb58676ec06e6b7d253ea1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 15:19:19 +0000

    pho 0.1.42
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/p/pho.rb
+++ Formula/p/pho.rb
@@ -1,8 +1,8 @@
 class Pho < Formula
   desc "TUI for GitHub Pull Requests"
   homepage "https://github.com/utkarsh261/pho"
-  url "https://github.com/utkarsh261/pho/archive/refs/tags/v0.1.41.tar.gz"
-  sha256 "d411df97c7ac4415c41b799cdf8bfc82bdc34bfc7ec782b073ae43d5703f4c41"
+  url "https://github.com/utkarsh261/pho/archive/refs/tags/v0.1.42.tar.gz"
+  sha256 "acccb2757afaedd2262c5beac611f7406e72db4825f8e0b5b54195c0de6bd6a9"
   license "GPL-3.0-only"
   head "https://github.com/utkarsh261/pho.git", branch: "main"
 
