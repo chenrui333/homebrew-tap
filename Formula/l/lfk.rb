3f334fed2f3ed73d6c3911041c74e0853b5d2520
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 15:16:26 +0000

    lfk 0.15.7
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/l/lfk.rb
+++ Formula/l/lfk.rb
@@ -1,8 +1,8 @@
 class Lfk < Formula
   desc "Lightning fast Kubernetes navigator"
   homepage "https://github.com/janosmiko/lfk"
-  url "https://github.com/janosmiko/lfk/archive/refs/tags/v0.15.6.tar.gz"
-  sha256 "edb605e434a8040275f912e5c977ddc91b8bef1a63d7caf2d77f911b3c85d705"
+  url "https://github.com/janosmiko/lfk/archive/refs/tags/v0.15.7.tar.gz"
+  sha256 "66fb1d267344a87bc97fa3856be9f1a006fc02b03803904ce225a4568720730f"
   license "Apache-2.0"
   head "https://github.com/janosmiko/lfk.git", branch: "main"
 
