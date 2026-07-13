59b1ae5b63c54a8918d60e02d43fa75d60b3bc64
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-13 05:23:27 +0000

    biber-inspector: update 0.2.0 bottle.

--- Formula/b/biber-inspector.rb
+++ Formula/b/biber-inspector.rb
@@ -8,11 +8,11 @@ class BiberInspector < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "baa32a360d30ea4087bbd5a782fb88ad82402269668fa152b46cfe80c1082cdf"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cbf4eca398cedd449166e712822f437f5fa30b27ecd831f82dfc2384d9f2c829"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "950c00e37f91dc83db78d4a7a4b564e800c0ba97ae8d2f8233f1a7019f6543b5"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6ede2bc12e49afce165a2b97e6415e5d5d02aff73528a36a042307f98c343f72"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6345c98b7d847f996c41f4f9a90515344127a82fc574e84ebc346bf27e051e41"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7c5479b26838272980e1806356985cb0e23c1603d58a627107b12f58b826c35b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0e8c8b255c1fe18af00f787221595240b626ebfdd0f9e23065f8884d6599f80c"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "df85be8debd80ad278f37a7593d6ebe82911b9e43053ad2b4825be567b902a23"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "31f9921042653839e2f4526c379b192fc76d826b45e9daaa9595d34c9f6d8d45"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5f1447d6e51352461dd7de9b6878f0e35ebbd834303f4974a07b655192d47890"
   end
 
   depends_on "zig" => :build
