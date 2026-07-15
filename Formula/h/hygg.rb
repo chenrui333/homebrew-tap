c77c6a6a938b490a9f3e0b8888def06dc7857935
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 13:29:54 +0000

    hygg: update 0.1.24 bottle.

--- Formula/h/hygg.rb
+++ Formula/h/hygg.rb
@@ -8,11 +8,11 @@ class Hygg < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "71dc2dea271740426ee001e97061341034e6a1f05d621d6c31e77779e31c746e"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "5afad4606770990cc44b714cca874a65908dc487fd0a8ec6caa5468c2ff64b5a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f61fed44b98010de8dca6fe977b31c45eb6cd548eefc9d2566703423d8fae90f"
-    sha256 cellar: :any,                 arm64_linux:   "06649a690a63d76e8d30303b0fe4b9ede0cdc03e0bd2f0ef0e49d91abfef0f13"
-    sha256 cellar: :any,                 x86_64_linux:  "c299d651bbbaae8cbd17d01493d211ea09799aff5e81967f7257c92a2a6f2519"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7e63e8d13bbae8ede6d1f10c1082bed57eea7ecdbb09122ae4890a61b2632157"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7f651c772dc04106bb6a6b9a41c09a2c11af4a55a0b432d8d2ce08276ca5b340"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7c4c494291f9766d88773f6206582b232cf321c228e40d28564421af66c26361"
+    sha256 cellar: :any,                 arm64_linux:   "fe763bff8218148f6ba2c727827c641c499dfdaa7faec6e5c8ab8011f3ce7326"
+    sha256 cellar: :any,                 x86_64_linux:  "207ad988ba3093a6d801d57fb4324530d7b2e08d1d8d9f2f704a3069307066ec"
   end
 
   depends_on "rust" => :build
