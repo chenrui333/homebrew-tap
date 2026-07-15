8bdf88bbd2c8c93dcbd64013ade8361d1394b88c
Rui Chen <rui@chenrui.dev>
2026-07-15 09:32:52 -0400

    dhictl 0.0.6 (#9174)
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/d/dhictl.rb
+++ Casks/d/dhictl.rb
@@ -2,11 +2,11 @@ cask "dhictl" do
   arch arm: "arm64", intel: "amd64"
   os macos: "darwin", linux: "linux"
 
-  version "0.0.5"
-  sha256 arm:          "460ec15d863188105de9e487b7905900d4d05d568609f52a7d67ce1e05f09b84",
-         intel:        "63fd1c9a4d15ee7e66a762f3bfec68775f875304555d78dff8ec8a764ceb1a5d",
-         arm64_linux:  "6cd76ef527f3f85fa3b27a0f60a602339cfa5d91b8b018174e9ca6a6615b638f",
-         x86_64_linux: "4f6da1177c6e4a8e41e25446bff53d04dd38dba8080ae7b6d5766569d1d4263c"
+  version "0.0.6"
+  sha256 arm:          "8659bec41c9bb25122370e0e570bebdb61df6d76f9690938cae9a5f09539c26b",
+         intel:        "75dcb9ea72daa5225e05eba6bb90d574ef3e2f267067ddc027d6e3b9e3ecabd6",
+         arm64_linux:  "10156fa90c1db17356e219cdcbd9bacdf6cf8f446f2e672efc3761110cedf9d7",
+         x86_64_linux: "38e7f6d0f12f6c8b733ac8d2db392f670ebf75ee65618bf49abec49500d61940"
 
   url "https://github.com/docker-hardened-images/dhictl/releases/download/v#{version}/dhictl-#{os}-#{arch}"
   name "dhictl"
