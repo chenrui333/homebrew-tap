17da81ee973ebdcf1fbd3906bb50706ac27b36ba
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 14:27:06 +0000

    lanes 0.44.5

--- Casks/l/lanes.rb
+++ Casks/l/lanes.rb
@@ -1,6 +1,6 @@
 cask "lanes" do
-  version "0.44.4"
-  sha256 "bc50a9cb7f1e640f1f8bc7499127ee224ff45899ace9b3057874d57d67768ffc"
+  version "0.44.5"
+  sha256 "ebcc80f3d4e36c54915a6191ef181bafe704ec5cc3b92bcf19333b99a196c755"
 
   url "https://github.com/lanes-sh/app/releases/download/v#{version}/Lanes_#{version}_universal.dmg",
       verified: "github.com/lanes-sh/app/"
