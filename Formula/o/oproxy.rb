c5f989e25fb016db7cc8d43fde3fd7ccfa0ae8fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 06:57:20 +0000

    oproxy: update 0.1.10 bottle.

--- Formula/o/oproxy.rb
+++ Formula/o/oproxy.rb
@@ -8,11 +8,11 @@ class Oproxy < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "df4f0f38c7cfcd2000f4ead8e4c04d27cba571053629ef45627ea37c6f78ce46"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "00a3a67e654b4c806e8024e96f067cc600c5b79dcbd70f96de138c8905ed5a70"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "ba6d773c533257638acaf8ed9c4042ea0964c95e6762778f8de9713a167cc160"
-    sha256 cellar: :any,                 arm64_linux:   "e7a125f884e64b487b193b5fa0a7c15fc061e07e3327839cbf3cb9a0e2623767"
-    sha256 cellar: :any,                 x86_64_linux:  "5a6b409ccb6fe66210f0321e9dd643598f137979d682dc5d95c9c835e9e43414"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "468295a224d7fd5428411c5084ba08b997316499f3743a454a094eecf7ecf137"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "71e41a6b9ae8877743023f7c833de5cbae693921265fbd401401db4fd0cce704"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "563a2c289e28a370eb18e7cc4e8e85030eed659dd1fdf985dab0d22b069f7962"
+    sha256 cellar: :any,                 arm64_linux:   "dd3ac4f5b0fb0255584290ddd886c614ff13391d0cf92b51111ad1771221d945"
+    sha256 cellar: :any,                 x86_64_linux:  "1b1d3434ab7f3ec4efa1be03ceaf04d8065166b8e6bb61180bae3f23fb9d5ae8"
   end
 
   depends_on "node" => :build
