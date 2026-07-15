6adee9db1b3e1a7d264a612851412561232c490b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-15 21:36:45 +0000

    debugg-ai-mcp: update 3.7.1 bottle.

--- Formula/d/debugg-ai-mcp.rb
+++ Formula/d/debugg-ai-mcp.rb
@@ -8,7 +8,7 @@ class DebuggAiMcp < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "d5d5b6543e6df85640e9ff200777f4e11582cc3fa822f0a29a3023f69c554278"
+    sha256 cellar: :any_skip_relocation, all: "5b3f3511b1639e56024468b5ab3d7a1b074f27c1a9458d260382dbf33b2df277"
   end
 
   depends_on "node"
