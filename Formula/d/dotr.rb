3b97f2e69f6f6a157f5f20460833b69dd6de739b
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-16 14:50:40 +0000

    dotr: update 2.1.1 bottle.

--- Formula/d/dotr.rb
+++ Formula/d/dotr.rb
@@ -8,11 +8,11 @@ class Dotr < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4fa4abb8a9ed736abb3b2ffc6c2a080db800f03622f129578cf1c2fbb37c81ce"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c63d3e850d42a702e335d2793af8d4e103e5f761728916687b7cf9a30017b27"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5a69d5f2e9c5276811d0e33a08db9e2f34182c7a14300d5d8453ec3e165bcce4"
-    sha256 cellar: :any,                 arm64_linux:   "af010ffae2759dd51be643e7488850a58e9adb4ec0caeabde62f39cf5c5b4be5"
-    sha256 cellar: :any,                 x86_64_linux:  "7376e897db62818707e69fd81d1cdd3ee2030d3edb84ff40afa3957761b5f09b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9636671add37d49857eb3b81af66cc72cc3218235a0559f8977fd8d9d5028313"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1caaace9aed7964f574b3bc77f59376e40b227c03140fd7596073a835d1fd3c9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d338c939349c6f715b1faa8bd4ddc11ccc1691c11c70921c0a3c1ad7e1f5ceb0"
+    sha256 cellar: :any,                 arm64_linux:   "70b246e99b570b9f5e3e6643daf23127fff0b935e9e81d6ca4790dad60e0a45c"
+    sha256 cellar: :any,                 x86_64_linux:  "2f11b5f12c851872c47c7665edd616a72326bf077bc4f2f9c4f8942d6961972c"
   end
 
   depends_on "rust" => :build
