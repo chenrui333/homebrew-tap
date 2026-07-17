c271aa892fd1bf033f74f17a4bb5e51db910d034
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-17 19:44:36 +0000

    budgetclaw: update 1.2.0 bottle.

--- Formula/b/budgetclaw.rb
+++ Formula/b/budgetclaw.rb
@@ -7,11 +7,11 @@ class Budgetclaw < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3ebfee50c6997d9d8012271feb5b04c2b85ede256a5e225650c1a0090a13c581"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "86451b83db1b19719939d24dd978339ca194696923fb034739770c89eab11bd6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7660faeb9aadbb2a3d3ba00c7530048c4c9fb6e1b6f74eed4fee29942b5a5c21"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "2a34ab81677998968899dee1e731f0fff00d675daed8d2f43d67a331efe64b9f"
-    sha256 cellar: :any,                 x86_64_linux:  "7528d17b92c8ecae19c4301220a987384c9079ceaf67ed1df4d8ccf121a2a414"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "39b9c2a171668f0a5ab9d58c3ed3e6b084c82a33b815f6c7efbe81c92efc7018"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4606586531ed31fb0959b43a59f07a7131232b46a57058b1cc64081ccf21ac34"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d7ab6658c1874672a13edfa9bab172f5adb11bf4a078924225957c10928d525a"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5490c519ee7bfa689fc6ebd1e27e65b73888c961fe7a311dbfa35a6898aacdb3"
+    sha256 cellar: :any,                 x86_64_linux:  "f7ec66a7401829f88bb1cd4b5264a363808c73b5a9cae80c37f091c303bc70a1"
   end
 
   depends_on "go" => :build
