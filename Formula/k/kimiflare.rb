c6f82883e1a75e1fe3570b6c95707c2c5f43ec38
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-18 10:05:29 +0000

    kimiflare: update 0.97.0 bottle.

--- Formula/k/kimiflare.rb
+++ Formula/k/kimiflare.rb
@@ -7,11 +7,11 @@ class Kimiflare < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "c6eb5a3a2da8698d6ba7e7844d35f2b5dff6d098ca0f7f12ec5873a03bdf211e"
-    sha256 cellar: :any,                 arm64_sequoia: "93fa40cf1b9b97a73ed5001ecfd9e033a15d061d6f8f48f473b91e3791060d44"
-    sha256 cellar: :any,                 arm64_sonoma:  "93fa40cf1b9b97a73ed5001ecfd9e033a15d061d6f8f48f473b91e3791060d44"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "13b011d8df7f04701c6f533c4f1dfeacc68dc968a99d4a9b05693a1535ab1ca1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b18cbb6244f2df361e81790ae6c54630a3e0951cb1b154d021a97d0f78f505d9"
+    sha256 cellar: :any,                 arm64_tahoe:   "24acdac37ebbf2b5bba875c6f3085739c51914ea220d408c137fde0f77247623"
+    sha256 cellar: :any,                 arm64_sequoia: "24acdac37ebbf2b5bba875c6f3085739c51914ea220d408c137fde0f77247623"
+    sha256 cellar: :any,                 arm64_sonoma:  "24acdac37ebbf2b5bba875c6f3085739c51914ea220d408c137fde0f77247623"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "3352f2ac3a1a565253b9e2134437bfe3232b6b89fb45ffc6eb33372065320313"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a0c7dd1398941a7f1d967eefd2920cf7d031728f2dfe0c370e75529189c1a192"
   end
 
   depends_on "node"
