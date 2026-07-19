4d3edd69a733b721bf6cc0c9410a1d01e7f31462
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-19 21:08:28 +0000

    darya 0.1.6
    Closes #9374.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/d/darya.rb
+++ Formula/d/darya.rb
@@ -1,8 +1,8 @@
 class Darya < Formula
   desc "Disk usage explorer with a TUI and live treemap"
   homepage "https://github.com/mrkatebzadeh/darya"
-  url "https://github.com/mrkatebzadeh/darya/archive/refs/tags/v0.1.5.tar.gz"
-  sha256 "dea36d2b7cc41e7ae7b9c9bff19e34d3043f540832f8b2e61950cb4c6e17f9dc"
+  url "https://github.com/mrkatebzadeh/darya/archive/refs/tags/v0.1.6.tar.gz"
+  sha256 "c119350a4943ee1663a680046bba06fac9c0c6971fd61e93182ab3c366dca3ab"
   license "GPL-3.0-only"
   head "https://github.com/mrkatebzadeh/darya.git", branch: "main"
 
