805247b964bb048450abf29a3816d12456022409
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-20 21:56:47 +0000

    vaults3: update 4.4.33 bottle.

--- Formula/v/vaults3.rb
+++ Formula/v/vaults3.rb
@@ -8,11 +8,11 @@ class Vaults3 < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f707068140b7956a5c1ecc8164a8a8cf50c14a655aef9ab06cfecf1e5b5a8e37"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "f707068140b7956a5c1ecc8164a8a8cf50c14a655aef9ab06cfecf1e5b5a8e37"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "f707068140b7956a5c1ecc8164a8a8cf50c14a655aef9ab06cfecf1e5b5a8e37"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "a9fc5ed3d7f5c7206f661978b0261e156b12f877c5eae57c1195a2510e750857"
-    sha256 cellar: :any,                 x86_64_linux:  "607d23e6c0957594d98d7a333d078b9c1de4814870e3b56bfa4696bf12d6214d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c8d7d41e895207c4b56898000ec085803d8293b4d0265f7d12fbb4cd5cb211fb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8d7d41e895207c4b56898000ec085803d8293b4d0265f7d12fbb4cd5cb211fb"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8d7d41e895207c4b56898000ec085803d8293b4d0265f7d12fbb4cd5cb211fb"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "641db4823123be605c13d7200dfd1b9f74d515dcfa540d9c2478390ddd930ce5"
+    sha256 cellar: :any,                 x86_64_linux:  "994bc2348f3db24dee6c65cd05e17f3b211eb12ce68dcb1c21bd84468da5434e"
   end
 
   depends_on "go" => :build
