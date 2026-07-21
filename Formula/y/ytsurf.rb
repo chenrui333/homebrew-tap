6de12ae1c7b31368271efe6e968c4ab2a9c66e0e
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-21 14:32:12 +0000

    ytsurf 3.1.8
    Closes #9449.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/y/ytsurf.rb
+++ Formula/y/ytsurf.rb
@@ -1,8 +1,8 @@
 class Ytsurf < Formula
   desc "YouTube in your terminal without the usual browser clutter"
   homepage "https://github.com/Stan-breaks/ytsurf"
-  url "https://github.com/Stan-breaks/ytsurf/archive/refs/tags/v3.1.7.tar.gz"
-  sha256 "ba61e89fc6748c823030dec8406e4c2522aa2bb294db60a141e53b95e6bb0e22"
+  url "https://github.com/Stan-breaks/ytsurf/archive/refs/tags/v3.1.8.tar.gz"
+  sha256 "05de0fda9edb516db883152c385f2e80b7d8a43d95ae536c47fa85a402332aec"
   license "GPL-3.0-only"
   head "https://github.com/Stan-breaks/ytsurf.git", branch: "main"
 
@@ -21,20 +21,22 @@ class Ytsurf < Formula
   depends_on "fzf"
   depends_on "jq"
   depends_on "mpv"
+  depends_on "socat"
   depends_on "yt-dlp"
 
   def install
-    inreplace "ytsurf.sh", "#!/usr/bin/env bash", "#!#{Formula["bash"].opt_bin}/bash"
+    inreplace "ytsurf.sh", "#!/usr/bin/env bash", "#!#{formula_opt_bin("bash")}/bash"
 
     libexec.install "ytsurf.sh" => "ytsurf"
 
     path = [
-      Formula["chafa"].opt_bin,
-      Formula["ffmpeg"].opt_bin,
-      Formula["fzf"].opt_bin,
-      Formula["jq"].opt_bin,
-      Formula["mpv"].opt_bin,
-      Formula["yt-dlp"].opt_bin,
+      formula_opt_bin("chafa"),
+      formula_opt_bin("ffmpeg"),
+      formula_opt_bin("fzf"),
+      formula_opt_bin("jq"),
+      formula_opt_bin("mpv"),
+      formula_opt_bin("socat"),
+      formula_opt_bin("yt-dlp"),
       "${PATH}",
     ].join(":")
     (bin/"ytsurf").write_env_script(libexec/"ytsurf", PATH: path)
