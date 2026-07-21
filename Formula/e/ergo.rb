db3ab84565d4aeeb94202690c0b15e73de499bc8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-21 06:44:08 +0000

    ergo 2.19.0
    Closes #9434.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/e/ergo.rb
+++ Formula/e/ergo.rb
@@ -1,8 +1,8 @@
 class Ergo < Formula
   desc "Modern IRC server (daemon/ircd) written in Go"
   homepage "https://github.com/ergochat/ergo"
-  url "https://github.com/ergochat/ergo/archive/refs/tags/v2.18.0.tar.gz"
-  sha256 "5dafcdc9b1eaed0273d54dc274a050d983f79057fbc529af0c52704b1a540680"
+  url "https://github.com/ergochat/ergo/archive/refs/tags/v2.19.0.tar.gz"
+  sha256 "37abcccd951fb9b672efffaa90e021d5f7d8b6a300137e2d89d708986e041927"
   license "MIT"
   head "https://github.com/ergochat/ergo.git", branch: "master"
 
