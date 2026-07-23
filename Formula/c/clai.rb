478824039e236176ef58f685033ba490114aaaca
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-23 06:41:42 +0000

    clai 1.10.16
    Closes #9513.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/clai.rb
+++ Formula/c/clai.rb
@@ -1,8 +1,8 @@
 class Clai < Formula
   desc "Command-line artificial intelligence - Your local LLM context-feeder"
   homepage "https://github.com/baalimago/clai"
-  url "https://github.com/baalimago/clai/archive/refs/tags/v1.10.15.tar.gz"
-  sha256 "a69727c18d6e48d285527cab78ec809def6d4e51e92d9e60c55283a9f613ea4b"
+  url "https://github.com/baalimago/clai/archive/refs/tags/v1.10.16.tar.gz"
+  sha256 "21c1874ac9b0d1ad9e3020211e1845dbe2ea5494bd977c0034775dee3a33bed7"
   license "MIT"
   head "https://github.com/baalimago/clai.git", branch: "main"
 
