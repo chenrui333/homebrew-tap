80c1a9e7db3a72d0c02ca2df41404c496938cb61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-26 14:00:35 +0000

    iam-expand 0.11.77

--- Formula/i/iam-expand.rb
+++ Formula/i/iam-expand.rb
@@ -1,8 +1,8 @@
 class IamExpand < Formula
   desc "Expand IAM Actions with Wildcards"
   homepage "https://iam.cloudcopilot.io/tools/iam-expand"
-  url "https://registry.npmjs.org/@cloud-copilot/iam-expand/-/iam-expand-0.11.76.tgz"
-  sha256 "ca2bc042f12cb91bce86205f7c18d1c6f0215e4f098a81536f0aa54bd84278cd"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-expand/-/iam-expand-0.11.77.tgz"
+  sha256 "17185b08bc997066ccae761392d00f6c962abec4dfdf639bfd0e3ed3eba94513"
   license "AGPL-3.0-or-later"
 
   bottle do
