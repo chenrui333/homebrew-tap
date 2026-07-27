11604525ec3a6bac316ea1a9e6f01cb6a2735a08
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-27 20:09:58 +0000

    cargo-readme: update 3.4.0 bottle.

--- Formula/c/cargo-readme.rb
+++ Formula/c/cargo-readme.rb
@@ -7,11 +7,11 @@ class CargoReadme < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "87d40f1ad2aea10244d887c97c12f4e16befa1ff49ae0c6bcebf6f06de5dc3cd"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9a22ce61aa1c7035b6e010e4a938ea41cd424051e6f5d4b2769ccb048f6a2935"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b809625fb8242bf751cb250dfa153eaa1d6b390e6f3063d664ecb026de8a56d6"
-    sha256 cellar: :any,                 arm64_linux:   "b4ffbe545630e95f47387d7426535c2428004b4f6213c948484466dfcba1b8db"
-    sha256 cellar: :any,                 x86_64_linux:  "2e848fbdf55b613bbe23a435c477dfb873034d44f51bf42f821ce335b38966f7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "443d2a7ff42bccd4afb30f78cce7d8a1793d0d86c0efe811d59101b7da8fd904"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bc45cf809821fabd9cacdea0a4010914ffb0ddf2cd0af8b337e238d65d79bc67"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "80dce9b75d2d286862501f82658c0b040995b593317af4d4f30c4ef42e6bda45"
+    sha256 cellar: :any,                 arm64_linux:   "6525eb4ce2a0992cb0019995fb7d29f606c52888a8df00e049183a22b05b9fd1"
+    sha256 cellar: :any,                 x86_64_linux:  "0615a4dad9852fb18b39f17ebd4e9f6cab962526836d019b909f7274bd53e0fc"
   end
 
   depends_on "rust" => :build
