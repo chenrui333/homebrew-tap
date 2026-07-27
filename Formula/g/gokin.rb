f5040874c003f3c780c622adab3c4c6177728a2c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-27 11:40:35 +0000

    gokin 0.100.110

--- Formula/g/gokin.rb
+++ Formula/g/gokin.rb
@@ -1,8 +1,8 @@
 class Gokin < Formula
   desc "AI-powered CLI assistant for code"
   homepage "https://gokin.ginkida.dev"
-  url "https://github.com/ginkida/gokin/archive/refs/tags/v0.100.109.tar.gz"
-  sha256 "121c669306b8aee73903e875aefdb174885cad2d5e0c4048151bef4358ded0da"
+  url "https://github.com/ginkida/gokin/archive/refs/tags/v0.100.110.tar.gz"
+  sha256 "20d1eb3431620a0d0ffba59ee423fa4a2dfd2c5baf281929d80d2297a408226f"
   license "MIT"
   head "https://github.com/ginkida/gokin.git", branch: "main"
 
