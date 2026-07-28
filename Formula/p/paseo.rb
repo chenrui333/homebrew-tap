48833dc67be412ad92b2daf02d6524c775f436b1
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 18:14:24 +0000

    paseo: update 0.2.3 bottle.

--- Formula/p/paseo.rb
+++ Formula/p/paseo.rb
@@ -7,11 +7,11 @@ class Paseo < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256               arm64_tahoe:   "f180f8375b9ebf6841af458b26104a84234c17844bc0f1442b95260c3415de98"
-    sha256               arm64_sequoia: "f180f8375b9ebf6841af458b26104a84234c17844bc0f1442b95260c3415de98"
-    sha256               arm64_sonoma:  "f180f8375b9ebf6841af458b26104a84234c17844bc0f1442b95260c3415de98"
-    sha256 cellar: :any, arm64_linux:   "e1db980cf5da0c1a67dca7c60728fea5d207b31dfb7c7e3938268029ee2f6489"
-    sha256 cellar: :any, x86_64_linux:  "10b5e2705d3231c8ea7c611d222c5841af33549fd221e893ab9935a6ab7a55fa"
+    sha256               arm64_tahoe:   "7e6adc7cfd73320e57a7d4ac34015f7870ac711b96b2ed62acc88c5c572e7692"
+    sha256               arm64_sequoia: "7e6adc7cfd73320e57a7d4ac34015f7870ac711b96b2ed62acc88c5c572e7692"
+    sha256               arm64_sonoma:  "7e6adc7cfd73320e57a7d4ac34015f7870ac711b96b2ed62acc88c5c572e7692"
+    sha256 cellar: :any, arm64_linux:   "357176d6b46e7ff39663ab2c8535fc3396930ef3d46e8622201f2c5ee6f448a3"
+    sha256 cellar: :any, x86_64_linux:  "4f6e046c0b97a23eeff23eee3fd49b74177b5e2a6707c468f4f3faf604383777"
   end
 
   depends_on "node"
