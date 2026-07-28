5f74a2e5be0d9ecb2fc2ca4f7644b18672daebe4
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-28 18:06:39 +0000

    hulak: update 0.3.31 bottle.

--- Formula/h/hulak.rb
+++ Formula/h/hulak.rb
@@ -8,11 +8,11 @@ class Hulak < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8a414797b8559e8f89f4129b386d9d8c52247611db996733d9ed4a479b952305"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8a414797b8559e8f89f4129b386d9d8c52247611db996733d9ed4a479b952305"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "8a414797b8559e8f89f4129b386d9d8c52247611db996733d9ed4a479b952305"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7621361d44315526afe74318bacfabe7da0f9aaf2c1653292f55b94c24082fd8"
-    sha256 cellar: :any,                 x86_64_linux:  "cc4dbcc25a878262a800d214bdd6c22de1240c51ec66761aabe766b3f1ed2e43"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7159ba63f8c15830317d1f92e633d0096c29bcf61a7360cbcba6dd75401bd3c3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7159ba63f8c15830317d1f92e633d0096c29bcf61a7360cbcba6dd75401bd3c3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7159ba63f8c15830317d1f92e633d0096c29bcf61a7360cbcba6dd75401bd3c3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "47f13c3c454943da031ab107bfe69d8987b28417d73a7e6aa1b3360c6347a872"
+    sha256 cellar: :any,                 x86_64_linux:  "fe1f64d02f66000c7fdeb411128372660316375345d846492d54625b1739729e"
   end
 
   depends_on "go" => :build
