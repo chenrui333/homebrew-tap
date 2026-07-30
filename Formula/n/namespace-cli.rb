65448992bce4d4760cd77a443fe59d488c9551fe
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 14:39:23 +0000

    namespace-cli 0.0.551
    Closes #9729.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/n/namespace-cli.rb
+++ Formula/n/namespace-cli.rb
@@ -2,8 +2,8 @@ class NamespaceCli < Formula
   desc "Command-line interface for the Namespaces platform"
   homepage "https://github.com/namespacelabs/foundation"
   url "https://github.com/namespacelabs/foundation.git",
-      tag:      "v0.0.550",
-      revision: "ed9f86cb24938c06a29d5005e4eb7ff450978025"
+      tag:      "v0.0.551",
+      revision: "2f4c728fd5f29c6f8d16420e5c31c32b2d981e70"
   license "Apache-2.0"
   head "https://github.com/namespacelabs/foundation.git", branch: "main"
 
