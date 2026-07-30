c36b68342200faf3567e3e890619a34a4b231f61
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-07-30 14:30:18 +0000

    apw 1.1.1
    Closes #9725.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/a/apw.rb
+++ Formula/a/apw.rb
@@ -1,8 +1,8 @@
 class Apw < Formula
   desc "CLI for Apple Passwords (also known as iCloud Keychain)"
   homepage "https://github.com/bendews/apw"
-  url "https://github.com/bendews/apw/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "c0c3e00eb58ccd47fecf3f2fe0fd4bed9656887a3306fe9330234beb784868b9"
+  url "https://github.com/bendews/apw/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "f189a4e68a86f5ebbdfaf1b94533358f7452e5692057a348c27bf1decb7e3388"
   license "GPL-3.0-only"
   head "https://github.com/bendews/apw.git", branch: "main"
 
@@ -17,7 +17,6 @@ class Apw < Formula
   depends_on :macos
 
   def install
-    inreplace "src/const.ts", "1.0.1", version.to_s
     system "deno", "compile", "--allow-all", "--output", bin/"apw", "src/cli.ts"
   end
 
@@ -25,6 +24,6 @@ class Apw < Formula
     assert_match version.to_s, shell_output("#{bin}/apw --version")
 
     output = shell_output("#{bin}/apw pw list https://example.com 2>&1", 9)
-    assert_match "Missing encryption key", output
+    assert_match "APW is not running or not authenticated", output
   end
 end
