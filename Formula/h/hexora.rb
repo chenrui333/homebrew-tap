1826a50ef85ac7b7ad0c9ea49050c03d76a1b525
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 17:33:23 +0000

    hexora: update 0.3.1 bottle.

--- Formula/h/hexora.rb
+++ Formula/h/hexora.rb
@@ -8,11 +8,11 @@ class Hexora < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3dbc2a3c440bd948dcffcd562ac89d0f08c13bb322d4cb208ac88186f00b0516"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "25b1a17d5cb2cfb47b77464b3f6d6c6dc4e3daf8c04d7a3215de16f2c40aaf67"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e222c8a7790d7878a3d9b8594884e2dc3966e58327a0f575cc7a14b3ef765f25"
-    sha256 cellar: :any,                 arm64_linux:   "f92f151d24b5812242c09d53441302c92071a23930818d07b4b3f9d09da18193"
-    sha256 cellar: :any,                 x86_64_linux:  "7095c1536baa88b7c78527f97c75718ca22c3502c5b855dfd8cb41adcdfdb8b8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6f82a517308c910258bc171ea57afce78bd2ab870519692b922241f3e8682402"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa56e5d5d3280b9114c58577f9a445ed2d5a107b8d7ca65bdb1e8989efb6c9e6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9f590d75c5732d0be7b782afddfb97f1203ddcbba4fb314834a32a9f95711aaa"
+    sha256 cellar: :any,                 arm64_linux:   "04fbd7b42bd68f0a04d2dd4eff2184c1c2c2005dfa637873072a365b6c063191"
+    sha256 cellar: :any,                 x86_64_linux:  "27c150b4d51bc09b276ec8c1f847e087ef87e352d66866e166c8bda668ed7c75"
   end
 
   depends_on "rust" => :build
