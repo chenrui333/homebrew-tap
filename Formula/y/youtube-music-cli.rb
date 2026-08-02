63366bdf5289acf1266bc53bc2c54b60663d2806
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-02 10:53:55 +0000

    youtube-music-cli: update 0.1.2 bottle.

--- Formula/y/youtube-music-cli.rb
+++ Formula/y/youtube-music-cli.rb
@@ -8,11 +8,11 @@ class YoutubeMusicCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2be3ea98bd3b8b1471fa372374aa9ca3d1751fc85001bbbd70d5a82e996cfd0a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "8de0e5ff82be674106584f881c1a5b462ac16f57f8941a5473328b8c8360265e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "92a6e567090f5f0c0b07276107e564720700f35e4335c9fd0d36aed77f2b7ddd"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "d05bbca3a24dfdf051e4993368c5ff14a84dde2220958e8e119132aa271ca850"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "221cf2ac673e8fdba17bf8b814e6eeb3cf20670bf1e8b1eaeb3bb28fe32df5e8"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "12c3710e337f6213c9a62479c91cb993fd1ac7a7f2627f168b98b36df5ef376b"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "782430cee63ba11e7e960ccb9afb308b01ac54ea8695c04f378abbf60c0eb631"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fb7381ce2b7b2db3788a33c572547ed6d5b75e5342056d2a49a37f8f7c1fc7d7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "da1c51d469c2daa099b99031189506d712abe4480fa1320c1a5ace0ea471a78c"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f083562ea138229e787ce52e69bedc50fc98e25858e9765197dfa203d653fe1f"
   end
 
   depends_on "chenrui333/tap/bun"
