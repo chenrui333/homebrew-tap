31d47137e08b31d26e8296818532f2e91af6974c
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-04 19:58:13 +0000

    eas-cli 21.5.0
    Closes #9886.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/e/eas-cli.rb
+++ Formula/e/eas-cli.rb
@@ -1,8 +1,8 @@
 class EasCli < Formula
   desc "Fastest way to build, submit, and update iOS and Android apps"
   homepage "https://docs.expo.dev/eas/"
-  url "https://registry.npmjs.org/eas-cli/-/eas-cli-21.4.0.tgz"
-  sha256 "647702ea3d15d903c07cee45e391b5183b420cfcf5086d961c91995600990ea4"
+  url "https://registry.npmjs.org/eas-cli/-/eas-cli-21.5.0.tgz"
+  sha256 "7e94cd527ec80a4e8b561b44718881a79bcebfe7390c2d8e2484d29cfd0aa33d"
   license "MIT"
 
   bottle do
