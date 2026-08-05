b352d7721e3a8981e2cb9849143e2623230be027
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 20:40:01 +0000

    obelisk: update 0.41.0 bottle.

--- Formula/o/obelisk.rb
+++ Formula/o/obelisk.rb
@@ -8,11 +8,11 @@ class Obelisk < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e31e1838186740c8c6b084c50f44a846b4d38526fc85c44c25ca9ed532995f89"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7c0c05a7ad27b3904ac401c010cbbf9e183bf66f9a5dc909742937aec08824db"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1c98d7d2607f46170e7b267add5c8d0034482cc33082417c4c95783286ddff48"
-    sha256 cellar: :any,                 arm64_linux:   "20bed02a1f3afe7d3c25a5a3623f41e12c8bd32e4fc40ac4f098bb445af6c7c6"
-    sha256 cellar: :any,                 x86_64_linux:  "5630c1c2b5c6c197db5591e1dec27856d21a275bc7246ce32bde898050588fd0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e6751a8f3f2788eea4dbb2574bf7e6270cf6409a5658c588ca1d54892b1d1d38"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "faead6659ff8ed38659df5e4f25d77073b573ba17b7fef429abbf037127033ae"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "6a8f4ae5b31ffcb28c5fc1898b5e6bef5801fab6ab9fbc3548f4bed6d41eaefe"
+    sha256 cellar: :any,                 arm64_linux:   "9aebfb36d3c97d62aec94f50471d0528c38a3988bd151d85bc370b6dffd52749"
+    sha256 cellar: :any,                 x86_64_linux:  "c68122ea1d96e66168ebe6e7bd09cfaa3dbd1b76fede16d0f5d436eb8b2f8672"
   end
 
   depends_on "pkgconf" => :build
