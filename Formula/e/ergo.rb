1096619671b9166820ecef37ac9f24aa4f599bbc
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-05 03:55:14 +0000

    ergo: update 2.19.1 bottle.

--- Formula/e/ergo.rb
+++ Formula/e/ergo.rb
@@ -8,11 +8,11 @@ class Ergo < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "83170b6be54ba2284ef506398f2341c1b4c644b6505cb1da6916d38c203e8a17"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "83170b6be54ba2284ef506398f2341c1b4c644b6505cb1da6916d38c203e8a17"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "83170b6be54ba2284ef506398f2341c1b4c644b6505cb1da6916d38c203e8a17"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "48e7603cdd33704be86f5e2e1a59c66d5d410971671c6b78e673746b8fb53c3e"
-    sha256 cellar: :any,                 x86_64_linux:  "86a7f5286960bf69c050e876a3f8c75c3d5b9b63b79e3e9b2a9d78e22ee04b86"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "aa2d00b7bafdd35a08a03dc790c68c4642a8149c9580b95850f891d7c38d4366"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "aa2d00b7bafdd35a08a03dc790c68c4642a8149c9580b95850f891d7c38d4366"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "aa2d00b7bafdd35a08a03dc790c68c4642a8149c9580b95850f891d7c38d4366"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "f4fcca9b1de5a57286dc6edfbeeeccdf34e4b15a88587cad538b7291bd17a502"
+    sha256 cellar: :any,                 x86_64_linux:  "121ef001f8c326a02c2571a9e87c698617377036a9a43528581ec07c83b33f41"
   end
 
   depends_on "go" => :build
