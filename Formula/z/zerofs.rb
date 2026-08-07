52191fd15d1407b670c9d58b11fb5b136e958400
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 14:33:41 +0000

    zerofs: update 2.2.2 bottle.

--- Formula/z/zerofs.rb
+++ Formula/z/zerofs.rb
@@ -8,11 +8,11 @@ class Zerofs < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6dd9f189451ec67f98ec252f1663bd6c4db1b9f23af6e2dc6d357393c1ba42ff"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1cf7fa9ca243c87d529434787d751ed19d61040fafbcd049afc4bee84c1f7993"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "70dec05b0e20a9d0081f87a4601ee3ef24ff2dd35d4c4fd4ccbeacb1d78a55c2"
-    sha256 cellar: :any,                 arm64_linux:   "6c890704a2f2c4cef9af87e15c598c6c1f110e83ee8d23f9beeec92136b643f5"
-    sha256 cellar: :any,                 x86_64_linux:  "3d10c845493508041177b1c21c982ac2c3341174a3d5b1d5d53f0fdc733622bf"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "03a9ccf051d6579052a28603ef02b1492bc686bf5966f0a77b69cfdde224f318"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dee950a293ac63a665d1cbbfe44ee24f66f05bc6001314d37e9a447b0b157dec"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "91aef20bd10779f264a86efce750c109ce138946c66b98e16ab94ccef87e9348"
+    sha256 cellar: :any,                 arm64_linux:   "8b2c18c50592f26c151817a5bc739cda85caf2df234c1866ed7e03cf34e293c9"
+    sha256 cellar: :any,                 x86_64_linux:  "2feb39b79882be88e065d4989c9b4d208cb34f785c4dbb0054d3a161b3cedf6f"
   end
 
   depends_on "cmake" => :build
