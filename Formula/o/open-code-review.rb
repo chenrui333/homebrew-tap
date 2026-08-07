da4ab3a8400fb3dad6003de572c1616959da6f9d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-07 03:44:23 +0000

    open-code-review: update 1.8.10 bottle.

--- Formula/o/open-code-review.rb
+++ Formula/o/open-code-review.rb
@@ -8,11 +8,11 @@ class OpenCodeReview < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "90bad7201b1dc4f9be2a7c929667da7f2bbf7391d2327a9b8a75251a0e11ddb4"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "90bad7201b1dc4f9be2a7c929667da7f2bbf7391d2327a9b8a75251a0e11ddb4"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "90bad7201b1dc4f9be2a7c929667da7f2bbf7391d2327a9b8a75251a0e11ddb4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "0bd073153680cfaacda7ff77b82548e0049cdfd9fe3f13e9829e46db53f709f2"
-    sha256 cellar: :any,                 x86_64_linux:  "f7a1772483cd297a7f1283dc6b448b1f801314a0b2dbff55897d0df50d405934"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cbb446f39d5451227f79c48c4edc56ad7367a28fcf59c1ad7163528db7b66010"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cbb446f39d5451227f79c48c4edc56ad7367a28fcf59c1ad7163528db7b66010"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "cbb446f39d5451227f79c48c4edc56ad7367a28fcf59c1ad7163528db7b66010"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "eaf3cb53a9fe0e750ee91862a0bcff1782d255df5c90f97aafea249ac7aae136"
+    sha256 cellar: :any,                 x86_64_linux:  "9ddf4dd26250622e409a67c9dfcb1a59cecb48e63ce5791bdb3a98c57eabde78"
   end
 
   depends_on "go" => :build
