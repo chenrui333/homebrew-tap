a70326f771a21d8af31124a482651586a14591a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 06:36:53 +0000

    otel-tui: update 0.7.4 bottle.

--- Formula/o/otel-tui.rb
+++ Formula/o/otel-tui.rb
@@ -8,11 +8,11 @@ class OtelTui < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f267835a9717fd83de30d11525724c8c0cd76c5119cb828c12e1d8df492b46c5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2c27f3b7617da9f3384740ed0cd6dede046dde7a9c2e322843a3f70bf75c3696"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "54e7224e23f7962b3146303e36c8a866d4c14372dfb2ce79633dcd7f914b0149"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "7434740dcdc3be3b321a374d2ee5128e8da4b3a78d4d093cdf5b5a618297d3a7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "7eab4f1a27b0c33e2acd3bac3ac591a804ea16c9bc34f7634dc8d1b805b77bee"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f34406b77b2190e2c94f6ce7d4250daa4e8f138eb41e65845f3dbaf32edff466"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3ecc73c7240411169a00377ef530cf1c51ab8ed9d7a927ee144bb0cbe965e5e5"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "075ec4191b31440e8e11aabd9814ddb5e5d2ad076a11abba9d55a0759c80d871"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "6b2ea57c46c5e7a29ee5d8d188ff6a00af0f6a49dded2b63a15b31078762420a"
+    sha256 cellar: :any,                 x86_64_linux:  "f8ccbed2e08befccc7fe21188553d050fc14ac8de0a477565753d6419e638374"
   end
 
   depends_on "go" => :build
