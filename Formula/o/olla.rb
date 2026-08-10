29e8bb8dd038ddae3f8034e8934b1c828116c13f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 13:56:54 +0000

    olla 0.0.29

--- Formula/o/olla.rb
+++ Formula/o/olla.rb
@@ -1,8 +1,8 @@
 class Olla < Formula
   desc "Lightweight & fast AI inference proxy for self-hosted LLMs backends"
   homepage "https://thushan.github.io/olla/"
-  url "https://github.com/thushan/olla/archive/refs/tags/v0.0.28.tar.gz"
-  sha256 "e025ac4b55601e10d6b5f39c67f24f87d60534cc5f3a8cbf444f9be3baf069f1"
+  url "https://github.com/thushan/olla/archive/refs/tags/v0.0.29.tar.gz"
+  sha256 "9ae9d83bcb631f592fa987a14468b1a43cefb662f8259da750044d932a14a2d7"
   license "Apache-2.0"
   head "https://github.com/thushan/olla.git", branch: "master"
 
