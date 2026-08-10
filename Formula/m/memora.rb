5c057915af7d9936c893b69b32a85e692d62b774
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-10 08:47:15 +0000

    memora: update 0.3.0 bottle.

--- Formula/m/memora.rb
+++ Formula/m/memora.rb
@@ -10,11 +10,11 @@ class Memora < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any,                 arm64_tahoe:   "f3c6f4bce7258e4c5c7ed4f3d002a656e89f1dd0af6e9ecb8842cb40198f4a12"
-    sha256 cellar: :any,                 arm64_sequoia: "b561962d8f9569d49bd40b20162b2fc254c195564dfa73854c897d6fa9be20d2"
-    sha256 cellar: :any,                 arm64_sonoma:  "7b82cc12b6d94050e02b6239b6af626baff5077e0c00eaec1f8f273772723ab8"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4fd4516bf11597486c949fbd5dc388180a41f4bf37a47d3590b47cb572a27d66"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "88580230dfa1270d9a13ab34321e33955aa9d2452adce5ef7fe6af543d27473e"
+    sha256 cellar: :any, arm64_tahoe:   "61deedd562bc2991dc4209fde545fcaa34a890f1750b76e1e1a8b8729a21fa11"
+    sha256 cellar: :any, arm64_sequoia: "a6a06ca4aacee379ad56108645b527589f02751ecead2628955d33bfa75ff9ad"
+    sha256 cellar: :any, arm64_sonoma:  "382358bb17bfdd40435cae3f7a9de6e2092c09fe0d38c99da2614caf14c78919"
+    sha256 cellar: :any, arm64_linux:   "6709d23de959094f3a39db3d343c1508999a4ce2d2a114adf16ebf16bddc83ba"
+    sha256 cellar: :any, x86_64_linux:  "f4f0c86269153628fe7d5aba1785c66962a09a064563e8569948bd51cb477bad"
   end
 
   depends_on "pkgconf" => :build
