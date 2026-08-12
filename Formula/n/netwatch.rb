0721729da9b950922fb3d8cf626c8f4c182da464
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-12 08:02:20 +0000

    netwatch 0.29.0
    Closes #10124.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/n/netwatch.rb
+++ Formula/n/netwatch.rb
@@ -1,8 +1,8 @@
 class Netwatch < Formula
   desc "Real time network diagnostics in your terminal"
   homepage "https://github.com/matthart1983/netwatch"
-  url "https://github.com/matthart1983/netwatch/archive/refs/tags/v0.28.2.tar.gz"
-  sha256 "7c01bd6d6a9ba8d46a3e20921f53f5ee16166c4ed9b0b6243377d47b6974a39b"
+  url "https://github.com/matthart1983/netwatch/archive/refs/tags/v0.29.0.tar.gz"
+  sha256 "db9110535dfcde9ccb8736706465216932308499ab087f67e3f633decab32e04"
   license "MIT"
   head "https://github.com/matthart1983/netwatch.git", branch: "main"
 
