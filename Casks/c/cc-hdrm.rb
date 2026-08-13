00e5f2fe97996ac123bee977e11077832b35a624
Rui Chen <rui@chenrui.dev>
2026-08-13 07:46:22 -0400

    cc-hdrm 1.5.1 (#10170)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/c/cc-hdrm.rb
+++ Casks/c/cc-hdrm.rb
@@ -1,6 +1,6 @@
 cask "cc-hdrm" do
-  version "1.5.0"
-  sha256 "a09014a9f735f54f224cfd6100b89c19c47a971dd38ab8d32ecd84acc75319c5"
+  version "1.5.1"
+  sha256 "8c72cde7aaa2f744757f04ba9a9b593549965f68b5abe7c7cf7f0ba514543047"
 
   url "https://github.com/rajish/cc-hdrm/releases/download/v#{version}/cc-hdrm-#{version}.dmg"
   name "cc-hdrm"
