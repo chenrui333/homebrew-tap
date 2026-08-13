6fe3c333aae70f86314734d8d2bcf383a3fd9332
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-13 14:00:28 +0000

    taskdog 0.27.0
    Closes #10177.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/t/taskdog.rb
+++ Formula/t/taskdog.rb
@@ -3,8 +3,8 @@ class Taskdog < Formula
 
   desc "Task manager with CLI, TUI, and local REST API server"
   homepage "https://github.com/Kohei-Wada/taskdog"
-  url "https://github.com/Kohei-Wada/taskdog/archive/refs/tags/v0.26.0.tar.gz"
-  sha256 "b1d3b6b492350695d221d22a863d5f258ac997179162cf3697d68a97076c9668"
+  url "https://github.com/Kohei-Wada/taskdog/archive/refs/tags/v0.27.0.tar.gz"
+  sha256 "1fe27dff014c0a223a9d45e81f9a0d037f61039431d493d3066f1c6930c0a787"
   license "MIT"
   head "https://github.com/Kohei-Wada/taskdog.git", branch: "main"
 
