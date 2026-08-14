000015ed4110453b551b1668f9edf89d9a402301
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-14 03:09:16 +0000

    epiq: update 1.3.5 bottle.

--- Formula/e/epiq.rb
+++ Formula/e/epiq.rb
@@ -7,7 +7,7 @@ class Epiq < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "0bf75b14141234c738e111f16101bae27ad8d7543ddf79fecdfb1e1157033c86"
+    sha256 cellar: :any_skip_relocation, all: "22741b2528456da3e24dfd07975d034839b47a7a2a10a7e4aeff8d62c41face3"
   end
 
   depends_on "node"
