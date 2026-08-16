facd83cc4188ee4e393edf70a8ad29ef5ee97f8d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-16 16:58:19 +0000

    semaphore 2.19.8
    Closes #10268.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/s/semaphore.rb
+++ Formula/s/semaphore.rb
@@ -1,8 +1,8 @@
 class Semaphore < Formula
   desc "Modern UI and powerful API for Ansible, Terraform/OpenTofu/Terragrunt"
   homepage "https://filebrowser.org/"
-  url "https://github.com/semaphoreui/semaphore/archive/refs/tags/v2.19.7.tar.gz"
-  sha256 "5b41e36cb68be4872b18ae3082532cd668bbc7db245239352082130e66e4cfd4"
+  url "https://github.com/semaphoreui/semaphore/archive/refs/tags/v2.19.8.tar.gz"
+  sha256 "2a2b844d8f81364649dca7801e5a6de3b8bf9f1e5ce7b21d28e6ed1ce12edbeb"
   license "MIT"
   head "https://github.com/semaphoreui/semaphore.git", branch: "develop"
 
