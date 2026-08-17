dbd902c80ac5027608a17f0535f12a1e70616e79
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 17:01:28 +0000

    sidecar 1.0.1

--- Formula/s/sidecar.rb
+++ Formula/s/sidecar.rb
@@ -1,8 +1,8 @@
 class Sidecar < Formula
   desc "Terminal UI for diffs, file trees, conversation history, and tasks"
   homepage "https://github.com/marcus/sidecar"
-  url "https://github.com/marcus/sidecar/archive/refs/tags/v0.99.1.tar.gz"
-  sha256 "f947e5a652ad4b2bb42283f79d4f0e59afeed7e6eeac37d329dc6d221e869d94"
+  url "https://github.com/marcus/sidecar/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "d43e41afc06e85dce3424b76c7d40d845a8a8b21e9015d1a6cec028d0799aced"
   license "MIT"
   head "https://github.com/marcus/sidecar.git", branch: "main"
 
