8a84db3812bb9d5db1f443050b61388d781525a2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-17 19:58:26 +0000

    gitpane: update 0.12.0 bottle.

--- Formula/g/gitpane.rb
+++ Formula/g/gitpane.rb
@@ -8,11 +8,11 @@ class Gitpane < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "110a0cf6d524b89d76785580db9a1ddef49d146256a515ae929a35d9e3eabc11"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1b75b682e97e65934f7f742d93ca432dc09144dc0f78d368cb4619ef835406b7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4b8034f0ad67c2f908fceb91cb7cf7811dca7565a9e42fe8a0fca6c94c8eee07"
-    sha256 cellar: :any,                 arm64_linux:   "4a91d3508d16c5496d6df52c1c651321e4878f91cf53a5d49f619bddc3f2d564"
-    sha256 cellar: :any,                 x86_64_linux:  "2412f8134fa413a58d1d2d1269e67444d48d98a23f6cfc58a46d7e0e8544947e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f707c19a69ad9dfe57aae3681930f7a6062fe8b8d51e39eb40c3a38cf0392b9c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4f2803f0275fbe99dc6d48160b69327f96b1104632d3cd8480038801021e3cb3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "26ade7080253ebca5f0f2ea45862e76daffabdd444339c1c7153db6584294d8c"
+    sha256 cellar: :any,                 arm64_linux:   "c99b569cbd51f91c566ac6dd3a0b70ac2fb9bda2330b70f6cc927cb88957c403"
+    sha256 cellar: :any,                 x86_64_linux:  "9e495bffde4160f05f64c761c7d1eab435676c87cd70d034a06f06e6c1176134"
   end
 
   depends_on "rust" => :build
