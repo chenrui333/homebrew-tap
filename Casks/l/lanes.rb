172760d988a6e45a09fe7b95b9394a1e84fa1c9a
Rui Chen <rui@chenrui.dev>
2026-08-19 07:09:46 -0400

    lanes 0.46.2 (#10346)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/l/lanes.rb
+++ Casks/l/lanes.rb
@@ -1,6 +1,6 @@
 cask "lanes" do
-  version "0.46.1"
-  sha256 "b5326d1fb246a6ca2b26b81cd9bca9ddb592ac4fbf6f4cd8762850c48518d12c"
+  version "0.46.2"
+  sha256 "5c05669f3ee3e253733382a25b77abb224990ac229e0867545140315189167d2"
 
   url "https://github.com/lanes-sh/app/releases/download/v#{version}/Lanes_#{version}_universal.dmg",
       verified: "github.com/lanes-sh/app/"
