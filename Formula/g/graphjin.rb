3fa9be34176b9b765b7d8770a4a2d281f96dcac0
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-20 20:55:32 +0000

    graphjin 3.20.35
    Closes #10393.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/graphjin.rb
+++ Formula/g/graphjin.rb
@@ -1,8 +1,8 @@
 class Graphjin < Formula
   desc "Build NodeJS / GO APIs in 5 minutes not weeks"
   homepage "https://graphjin.com/"
-  url "https://github.com/dosco/graphjin/archive/refs/tags/v3.20.33.tar.gz"
-  sha256 "88769c66ca6cdded24ee1ac2737867172e64932f62b2b84b119e3b12d32b1e6a"
+  url "https://github.com/dosco/graphjin/archive/refs/tags/v3.20.35.tar.gz"
+  sha256 "a8dd1a78e6f449e8d3f56f079320eba5f77ca49b7afc0d3c3ce96b24611c2819"
   license "Apache-2.0"
   head "https://github.com/dosco/graphjin.git", branch: "master"
 
