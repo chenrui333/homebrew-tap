b5f0c9077d1265fdca96d50cd33c9fa383d8c47d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-21 23:18:37 +0000

    wolfpack: update 1.6.20 bottle.

--- Formula/w/wolfpack.rb
+++ Formula/w/wolfpack.rb
@@ -7,11 +7,11 @@ class Wolfpack < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "473b7223be65b0faeab3f472864a657792ecb341cd71a22116cb087a43613b08"
-    sha256                               arm64_sequoia: "473b7223be65b0faeab3f472864a657792ecb341cd71a22116cb087a43613b08"
-    sha256                               arm64_sonoma:  "473b7223be65b0faeab3f472864a657792ecb341cd71a22116cb087a43613b08"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "c4d5e82404b10558c008babbdede8856ffbcd916ba1498d95e4b13cc4eac609d"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "627676c7f2eb1d42b5b58bb859c27212aadee4bc9e6938837add35f00c6e354d"
+    sha256                               arm64_tahoe:   "37cb84e0bb3d34f7ff67262fbb74dc086b8956f6b71b172f95770c87ba895fe3"
+    sha256                               arm64_sequoia: "37cb84e0bb3d34f7ff67262fbb74dc086b8956f6b71b172f95770c87ba895fe3"
+    sha256                               arm64_sonoma:  "37cb84e0bb3d34f7ff67262fbb74dc086b8956f6b71b172f95770c87ba895fe3"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b5916d9c31af5c4e33a992f3b529549c6b970bfddea2e6e16b2fe4f8c83f5ce7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "d91527d83032f86eabfbd5f7cd73f66176e1528261416c836741a2c6b8c8ca56"
   end
 
   depends_on "node"
