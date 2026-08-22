60151b7bf167ad8e33d5d337240d19f68f8fc160
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 22:42:30 +0000

    crush 0.91.0
    Closes #10465.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/crush.rb
+++ Formula/c/crush.rb
@@ -1,8 +1,8 @@
 class Crush < Formula
   desc "Glamorous AI coding agent for your favorite terminal"
   homepage "https://github.com/charmbracelet/crush"
-  url "https://github.com/charmbracelet/crush/archive/refs/tags/v0.90.0.tar.gz"
-  sha256 "47a8b5ca440747b66f8aa8ea753ce1ef552e6917ad42237c4999bae928c0b17b"
+  url "https://github.com/charmbracelet/crush/archive/refs/tags/v0.91.0.tar.gz"
+  sha256 "3e558af212a740516187aa851a086b2b0b56ee4f2a726c342d9099a13bbd677e"
   # license "FSL-1.1-MIT"
   head "https://github.com/charmbracelet/crush.git", branch: "main"
 
