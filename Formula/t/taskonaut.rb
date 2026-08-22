05125f4aa36e6dad506927bfd31d64398df2e216
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 20:57:27 +0000

    taskonaut 1.10.11

--- Formula/t/taskonaut.rb
+++ Formula/t/taskonaut.rb
@@ -1,8 +1,8 @@
 class Taskonaut < Formula
   desc "Interactive CLI tool for exec into AWS ECS tasks"
   homepage "https://github.com/SchematicHQ/taskonaut"
-  url "https://registry.npmjs.org/@schematichq/taskonaut/-/taskonaut-1.10.10.tgz"
-  sha256 "26642cf2183ae9794788c506937593819c10cfa0732282e8e730b8a7ce55475a"
+  url "https://registry.npmjs.org/@schematichq/taskonaut/-/taskonaut-1.10.11.tgz"
+  sha256 "d7739dca5884d7b4c02e7c29b40c7a3a54c530098e13049439363a58b9217d9f"
   license "MIT"
 
   bottle do
