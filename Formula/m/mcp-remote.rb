bd5efa4256179d6e2fcb70dc883d0234c369e574
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-22 23:19:13 +0000

    mcp-remote: update 0.1.43 bottle.

--- Formula/m/mcp-remote.rb
+++ Formula/m/mcp-remote.rb
@@ -7,7 +7,7 @@ class McpRemote < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "56a5ad83177ab2bb712a0d3640c5591b17c4f890bb0902f3b0b1daa7cc81180d"
+    sha256 cellar: :any_skip_relocation, all: "5928c88061415b1802c4627ed08d2f2bacd5e39071614156a6afd80a596ffdc1"
   end
 
   depends_on "node"
