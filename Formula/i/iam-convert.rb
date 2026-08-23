92876772f386b77749bf1549d13736e12db05d03
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-23 13:09:09 +0000

    iam-convert 0.1.86
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/i/iam-convert.rb
+++ Formula/i/iam-convert.rb
@@ -1,8 +1,8 @@
 class IamConvert < Formula
   desc "Convert JSON IAM Policies to other formats"
   homepage "https://iam.cloudcopilot.io/tools/iam-convert"
-  url "https://registry.npmjs.org/@cloud-copilot/iam-convert/-/iam-convert-0.1.84.tgz"
-  sha256 "532bc55b8a629aa1028f9059d0ba7513575cfb6ef156d3c9bd1d6f6d61848ea8"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-convert/-/iam-convert-0.1.86.tgz"
+  sha256 "ce183622b49372025280e497821ec5bfa84e6b27cb6486a9408a27488829c281"
   license "AGPL-3.0-only"
 
   bottle do
@@ -18,14 +18,18 @@ class IamConvert < Formula
   end
 
   test do
-    resource "test_policy_json" do
-      url "https://government-secrets.s3.amazonaws.com/secret-policy.json"
-      sha256 "6348c4060ad668b8cc14aaad21fd426ca2bffb3002fc11abb89ab4279439b409"
-    end
+    (testpath/"policy.json").write <<~JSON
+      {
+        "Version": "2012-10-17",
+        "Statement": [{
+          "Effect": "Allow",
+          "Action": "s3:GetObject",
+          "Resource": "*"
+        }]
+      }
+    JSON
 
-    testpath.install resource("test_policy_json")
-
-    output = shell_output("#{bin}/iam-convert --file #{testpath}/secret-policy.json")
+    output = shell_output("#{bin}/iam-convert --file #{testpath}/policy.json")
     assert_match "data \"aws_iam_policy_document\" \"policy\"", output
 
     assert_match version.to_s, shell_output("#{bin}/iam-convert --version")
