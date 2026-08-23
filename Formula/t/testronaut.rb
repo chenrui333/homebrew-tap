0bf2da9898e7d3c2f85915fb2e247e21960f96d2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-23 07:36:41 +0000

    testronaut: update 1.4.1 bottle.

--- Formula/t/testronaut.rb
+++ Formula/t/testronaut.rb
@@ -7,11 +7,11 @@ class Testronaut < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "69004d698b7c64c3ab2d2bc8914b48073142ed774e42694f8d7612782c00929f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "69004d698b7c64c3ab2d2bc8914b48073142ed774e42694f8d7612782c00929f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "69004d698b7c64c3ab2d2bc8914b48073142ed774e42694f8d7612782c00929f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "997a5dd37ff7583502ca24e2e8b081e18db744b9e98859a17a0b8ba6207fb771"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "997a5dd37ff7583502ca24e2e8b081e18db744b9e98859a17a0b8ba6207fb771"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7cf296d58d6df1de2353e9e1bb2f3bac777eb7b51b468cf57b66aa6c1b8c7450"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7cf296d58d6df1de2353e9e1bb2f3bac777eb7b51b468cf57b66aa6c1b8c7450"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7cf296d58d6df1de2353e9e1bb2f3bac777eb7b51b468cf57b66aa6c1b8c7450"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "78d6879230ad71e58622f78ee2d5b3f8a2679199b0c1361a47cbb722e6e080bb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "78d6879230ad71e58622f78ee2d5b3f8a2679199b0c1361a47cbb722e6e080bb"
   end
 
   depends_on "node"
