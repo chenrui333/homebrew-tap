e42bfe13c99556fb79e729b7da58cf3556d53ab8
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-23 13:08:58 +0000

    iam-collect 0.1.211

--- Formula/i/iam-collect.rb
+++ Formula/i/iam-collect.rb
@@ -1,8 +1,8 @@
 class IamCollect < Formula
   desc "Collect IAM information from all your AWS organization, accounts, and resources"
   homepage "https://github.com/cloud-copilot/iam-collect"
-  url "https://registry.npmjs.org/@cloud-copilot/iam-collect/-/iam-collect-0.1.208.tgz"
-  sha256 "7e8a62c3d0b5c653fdeea2190ab16d1ef75dd77ed7e9b30cc81bf9cc9cde3b9a"
+  url "https://registry.npmjs.org/@cloud-copilot/iam-collect/-/iam-collect-0.1.211.tgz"
+  sha256 "5ec9d2edb7f03a113c0314abe7f1af5fcca294341392073a0f5b79c327fce886"
   license "AGPL-3.0-or-later"
 
   bottle do
