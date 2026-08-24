d293dd6744a39fcef289e4732e4ecc9a67523de5
Rui Chen <rui@chenrui.dev>
2026-08-24 15:34:40 -0400

    piebald 0.6.1 (#10538)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/p/piebald.rb
+++ Casks/p/piebald.rb
@@ -1,6 +1,6 @@
 cask "piebald" do
-  version "0.6.0"
-  sha256 "245054246ef14cda09b92ccd8722752581dbb8e4ee19baa6b58663879161ca52"
+  version "0.6.1"
+  sha256 "e179aa05ca79b80a8d1d9edb71989257113d2508b0e69206f9808f9d495f5e13"
 
   url "https://github.com/Piebald-AI/piebald-issues/releases/download/v#{version}/Piebald_#{version}_universal.dmg",
       verified: "github.com/Piebald-AI/piebald-issues/"
