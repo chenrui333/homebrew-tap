96f957c4c121a0d69cbc09b79182919807fb81fb
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 09:19:18 +0000

    namespace-cli 0.0.560
    Closes #10518.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/n/namespace-cli.rb
+++ Formula/n/namespace-cli.rb
@@ -2,8 +2,8 @@ class NamespaceCli < Formula
   desc "Command-line interface for the Namespaces platform"
   homepage "https://github.com/namespacelabs/foundation"
   url "https://github.com/namespacelabs/foundation.git",
-      tag:      "v0.0.559",
-      revision: "8f53ba50b40d09b7dbcc14bea6023f39391d1f91"
+      tag:      "v0.0.560",
+      revision: "025e0b6610fbe878fb2a2b67520d3504849e83f8"
   license "Apache-2.0"
   head "https://github.com/namespacelabs/foundation.git", branch: "main"
 
@@ -27,6 +27,6 @@ class NamespaceCli < Formula
     assert_match version.to_s, shell_output("#{bin}/nsc version")
 
     assert_match "not logged in", shell_output("#{bin}/nsc list 2>&1", 1)
-    assert_match "failed to get authentication token", shell_output("#{bin}/nsc registry list 2>&1", 1)
+    assert_match "not logged in", shell_output("#{bin}/nsc registry list 2>&1", 1)
   end
 end
