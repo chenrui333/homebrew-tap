ea35e1162125a512b8ca596753fcef48c4bfa684
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-24 15:23:32 +0000

    vaults3 4.4.64
    Closes #10528.
    
    Signed-off-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/v/vaults3.rb
+++ Formula/v/vaults3.rb
@@ -1,8 +1,8 @@
 class Vaults3 < Formula
   desc "Lightweight, S3-compatible object storage server with built-in web dashboard"
   homepage "https://github.com/Kodiqa-Solutions/VaultS3"
-  url "https://github.com/Kodiqa-Solutions/VaultS3/archive/refs/tags/v4.4.63.tar.gz"
-  sha256 "8c355f927b2c6aeb67040624fd916c234bfd7a299ad3637686ea1a4ee37f33cd"
+  url "https://github.com/Kodiqa-Solutions/VaultS3/archive/refs/tags/v4.4.64.tar.gz"
+  sha256 "d853d4ea014dba48cfa8c95a441787c4eab1c4e676279681fec9304698e0680f"
   license "AGPL-3.0-only"
   head "https://github.com/Kodiqa-Solutions/VaultS3.git", branch: "main"
 
