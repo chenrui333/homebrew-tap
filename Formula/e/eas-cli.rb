bf9585205de46dc8084a5d3b3d0643ef1e280d1a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-27 21:28:17 +0000

    eas-cli: update 22.5.0 bottle.

--- Formula/e/eas-cli.rb
+++ Formula/e/eas-cli.rb
@@ -7,7 +7,11 @@ class EasCli < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "57fc7aa0bead1e416dcd4bb7b71ad8dbfb49d01a0570203ddc365de701ff3412"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b3957a6db50ca3921102ec22f2040c43f7ef0ab544e7df0d102a2896d43da2f3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b3957a6db50ca3921102ec22f2040c43f7ef0ab544e7df0d102a2896d43da2f3"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5312fa6208250d075072413c385a01ab11c723dc3de5fc90307c7590635942ef"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5312fa6208250d075072413c385a01ab11c723dc3de5fc90307c7590635942ef"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "5312fa6208250d075072413c385a01ab11c723dc3de5fc90307c7590635942ef"
   end
 
   depends_on "node"
