efebdf79c2be60eebb1feaf9024f7794ee29dc54
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-29 02:59:17 +0000

    hulak 0.3.33

--- Formula/h/hulak.rb
+++ Formula/h/hulak.rb
@@ -1,8 +1,8 @@
 class Hulak < Formula
   desc "Lightweight file-based API client with encrypted secrets store"
   homepage "https://github.com/xaaha/hulak"
-  url "https://github.com/xaaha/hulak/archive/refs/tags/v0.3.32.tar.gz"
-  sha256 "3fa335bdcdd981e51c1f145bbf609faa2f1243f787fa9245f845200d9fc7f4c9"
+  url "https://github.com/xaaha/hulak/archive/refs/tags/v0.3.33.tar.gz"
+  sha256 "082d5ab2d036238fa2a008b502a4b01bd8b23935c40c50fdcc2ed16918a4f840"
   license "MIT"
   head "https://github.com/xaaha/hulak.git", branch: "main"
 
