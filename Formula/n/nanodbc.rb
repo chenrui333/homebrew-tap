99760d73be9fd095d9b138231b5ed4d682c05ab2
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-30 22:56:32 +0000

    nanodbc 3.0.1

--- Formula/n/nanodbc.rb
+++ Formula/n/nanodbc.rb
@@ -1,8 +1,8 @@
 class Nanodbc < Formula
   desc "Small C++ wrapper for the native C ODBC API"
   homepage "https://nanodbc.github.io/nanodbc/"
-  url "https://github.com/nanodbc/nanodbc/archive/refs/tags/v2.14.0.tar.gz"
-  sha256 "56228372042b689beccd96b0ac3476643ea85b3f57b3f23fb11ca4314e68b9a5"
+  url "https://github.com/nanodbc/nanodbc/archive/refs/tags/v3.0.1.tar.gz"
+  sha256 "3c1bfea0c4704acf6abb58c2290d45eb775c4c4c9d895010cfb4ac5f2e0d1e3e"
   license "MIT"
 
   bottle do
