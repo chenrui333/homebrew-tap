6efa75e1985993917ee2c6a837393fa2b9e94439
Rui Chen <rui@chenrui.dev>
2026-08-30 17:42:58 -0400

    gitcomet 0.2.2 (#10732)
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Casks/g/gitcomet.rb
+++ Casks/g/gitcomet.rb
@@ -1,9 +1,9 @@
 cask "gitcomet" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.2.1"
-  sha256 arm:   "554edc245f47717a1656d1b1c7e5352d6666fdcd3038b158997055469ed880ab",
-         intel: "f45a0eff9250e062bad8260ebb13286d329062e12d2999069e2068047d624197"
+  version "0.2.2"
+  sha256 arm:   "a9283796a0606e25b9f6407d9c9840ea1af4b4307433443b00d9dfc14c0fb46e",
+         intel: "d18bed3afcadcc49bd3708fd61d8f8041129417828d31177e89e5b72eaa52263"
 
   url "https://github.com/Auto-Explore/GitComet/releases/download/v#{version}/gitcomet-v#{version}-macos-#{arch}.dmg",
       verified: "github.com/Auto-Explore/GitComet/"
