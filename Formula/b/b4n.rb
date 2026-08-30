7b8971c2ab0cb0a6c70b95457cf1013d74c5e284
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-30 05:23:29 +0000

    b4n: update 0.5.7 bottle.

--- Formula/b/b4n.rb
+++ Formula/b/b4n.rb
@@ -8,11 +8,11 @@ class B4n < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9662d944e4be05eab4e70d02ca6757bfb23b2fa3b6c5018883fe334d4d868023"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b3a12edcfef5c45915765a5eb442bbb84165ba1b1176696784e59352f29a8b69"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e31db0e3bb6f1f8c0a9ddbba04d0d8eb32be4da3625ca125051cc67fe750cf41"
-    sha256 cellar: :any,                 arm64_linux:   "e85d6e558316c85e59f6f4fc1848b19569c027e75363ba7a1c6b58dfe3d51af3"
-    sha256 cellar: :any,                 x86_64_linux:  "ca40c529579595bc383c3dad5c4c44a9286fd19f07b831e3bd35c89e3b0e8cc9"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d6bed8efbe6838785de463dcf168b43b9882b9ce869c12e94f786e7853b5fbbb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e60c1aa1a657a3bd9d4724feda4c41a259d7a05192090f0edc6b1f30c5e7ef0"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "5df0518a9ab90228af0f7d7c76b08fb0dc2e9b38c05a539df63da6a12665d3d1"
+    sha256 cellar: :any,                 arm64_linux:   "76ed920e0e387a291e6ae798e7054d6c0c057620ba2dc3f986f89c517ce90212"
+    sha256 cellar: :any,                 x86_64_linux:  "58c2a4d0472052c63cbf1a0f3c1ea4d7b401db250c01c45e2ae7b4f1c45362c2"
   end
 
   depends_on "rust" => :build
