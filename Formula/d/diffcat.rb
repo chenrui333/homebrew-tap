7680a7a3907f88897025b5ba666b83ae20fd88a5
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-08-30 01:28:30 +0000

    diffcat: update 0.18.0 bottle.

--- Formula/d/diffcat.rb
+++ Formula/d/diffcat.rb
@@ -8,11 +8,11 @@ class Diffcat < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "e104f69f1fb4bc8fcd7ec2f5e435fd6215dac7274e5e8c8a989479f38feed927"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e104f69f1fb4bc8fcd7ec2f5e435fd6215dac7274e5e8c8a989479f38feed927"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e104f69f1fb4bc8fcd7ec2f5e435fd6215dac7274e5e8c8a989479f38feed927"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8ac77f27b562a2bb03b6ffbed7f4416e00c7da85da4a27d34b0606ee9efa1216"
-    sha256 cellar: :any,                 x86_64_linux:  "96d939359599486b5e450546de55bc7d1ed252887a47e66f777bc7bb4cad153e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d228841ccb9660707757e6f638c962cbe6af53db3bc025b90ff744afcc3e4a97"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d228841ccb9660707757e6f638c962cbe6af53db3bc025b90ff744afcc3e4a97"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d228841ccb9660707757e6f638c962cbe6af53db3bc025b90ff744afcc3e4a97"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "a2dad803658c356c01d82ac6640c4cce4430e127602da7369898fd56f9488fc4"
+    sha256 cellar: :any,                 x86_64_linux:  "c0523740b7aee4b3740b9e86337e60441f859571073f6c61355425474b403144"
   end
 
   depends_on "go" => :build
