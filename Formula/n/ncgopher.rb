f0b486742ea4af6dd1a2cfb6e09fd7b9481b1945
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 14:09:13 +0000

    ncgopher: add 0.8.0 bottle.

--- Formula/n/ncgopher.rb
+++ Formula/n/ncgopher.rb
@@ -6,6 +6,14 @@ class Ncgopher < Formula
   license "BSD-2-Clause"
   head "https://github.com/jansc/ncgopher.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6b9fa91122bc95796047d60ce5efbb05d8f77c97954b928d68f30f4e875a0253"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "96831c68da841819e221cf3d823d5389cdcd99755969e77ae90e4d37c96cb7bd"
+    sha256 cellar: :any,                 arm64_linux:   "6d4312f3b116949c6a0d530c947e0ab14d5bb7d1fb8b2abdf0a07833a670da4a"
+    sha256 cellar: :any,                 x86_64_linux:  "e0d122261548dbade234ed538282b6350b0fd5fe28672be54843e84c6e0a6141"
+  end
+
   depends_on "pkgconf" => :build
   depends_on "rust" => :build
   depends_on "ncurses"
