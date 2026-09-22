771ac33e72bccc2847a7843e36e36f8e0f9eefd2
Rui Chen <rui@chenrui.dev>
2026-09-21 22:41:31 -0400

    speaktype: remove deprecated verified parameter
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/s/speaktype.rb
+++ Casks/s/speaktype.rb
@@ -2,8 +2,7 @@ cask "speaktype" do
   version "1.3.0"
   sha256 "2aa7ec1a7e67631825e5bc045b875f49e28fb408de8b301cfbeadb3528891c2d"
 
-  url "https://github.com/karansinghgit/speaktype/releases/download/v#{version}/SpeakType-#{version}.dmg",
-      verified: "github.com/karansinghgit/speaktype/"
+  url "https://github.com/karansinghgit/speaktype/releases/download/v#{version}/SpeakType-#{version}.dmg"
   name "SpeakType"
   desc "Open-source offline voice dictation app"
   homepage "https://tryspeaktype.com/"
