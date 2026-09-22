cd39fb4fc40e442a6ddcae88bd9499af1cf2fb93
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 04:46:40 +0000

    excise: add 1.2.4 bottle.

--- Formula/e/excise.rb
+++ Formula/e/excise.rb
@@ -6,6 +6,14 @@ class Excise < Formula
   license "MIT"
   head "https://github.com/findyourexit/excise.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "685613a26dac195e4fc682531dab530904b44c059d7ec3a4cafaefd5bb552839"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2b56f4b94627e71aa894a624f15a031955eaf43b8c69d791c86fa3b3462d5c7c"
+    sha256 cellar: :any,                 arm64_linux:   "3f2ebdba34dc0ae3e3193ac298bd7bfbbb4d944f6e3fb3404dedfe4bb9b1439f"
+    sha256 cellar: :any,                 x86_64_linux:  "b8e2ca1fd539c9ef534f2b260a83967c6de68c6891969ab6fe04f4ffe7c31cf6"
+  end
+
   depends_on "rust" => :build
 
   deny_network_access!
