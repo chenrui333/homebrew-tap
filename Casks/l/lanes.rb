361e6ff852e2562e83561ae51dfaa00f55a126fd
Rui Chen <rui@chenrui.dev>
2026-09-21 23:32:54 -0400

    lanes: update to 0.49.4
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Casks/l/lanes.rb
+++ Casks/l/lanes.rb
@@ -1,6 +1,6 @@
 cask "lanes" do
-  version "0.47.3"
-  sha256 "feba1672d9889f3005ae930ad81f3c24d7156bf6b9df411cef5427edc3436971"
+  version "0.49.4"
+  sha256 "e7a8f2745826a452d0976b2dcf48494129abc4d4e48a193dec3a6824057fcbdc"
 
   url "https://github.com/lanes-sh/app/releases/download/v#{version}/Lanes_#{version}_universal.dmg"
   name "Lanes"
