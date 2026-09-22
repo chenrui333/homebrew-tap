df2d93785a1bc2babd4d0e17205b60ac91ee4244
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-22 10:00:13 +0000

    emeraldian: add 0.5.0 bottle.

--- Formula/e/emeraldian.rb
+++ Formula/e/emeraldian.rb
@@ -6,6 +6,14 @@ class Emeraldian < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/iamrohithrnair/emeraldian.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/chenrui333/tap"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9368cbd0b72910e1a191929be325eca346c0c01de03ff2c4983787ce869aafdf"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d5afbe1a8ec2e00e8cf4b01560943f690d883cd32efef8cecd921f94f1d85c88"
+    sha256 cellar: :any,                 arm64_linux:   "4bd1c3912d37e3258d07da378a88a2661e241917abd8ebea051be093c7816826"
+    sha256 cellar: :any,                 x86_64_linux:  "ea6061c261e4ba5166dde9ad8109d325cdf73d2a20f1f21990f6e1ea69f609e3"
+  end
+
   depends_on "rust" => :build
 
   deny_network_access!
