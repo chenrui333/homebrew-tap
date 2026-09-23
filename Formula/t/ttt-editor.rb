10db69ac85974151b031638de0b94cd7b4f431d6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 07:58:19 +0000

    ttt-editor 1.5.0

--- Formula/t/ttt-editor.rb
+++ Formula/t/ttt-editor.rb
@@ -1,8 +1,8 @@
 class TttEditor < Formula
   desc "Terminal editor with LSP and Git integration"
   homepage "https://github.com/eugenioenko/ttt"
-  url "https://github.com/eugenioenko/ttt/archive/refs/tags/v1.4.0.tar.gz"
-  sha256 "b01d8aa7dc64d17e261ce4d31d92aea1fefd69a0793c906d4ea1180ff78c28cb"
+  url "https://github.com/eugenioenko/ttt/archive/refs/tags/v1.5.0.tar.gz"
+  sha256 "b01897fa241276c5dde42742899ae3dc806172eece0a41dea21b58f05eb78cac"
   license "MIT"
   head "https://github.com/eugenioenko/ttt.git", branch: "main"
 
