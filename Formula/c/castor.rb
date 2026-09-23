13cc77ccc09b90fa793d20c0667ebbc9542d1404
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 07:20:59 +0000

    castor 1.8.1
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/c/castor.rb
+++ Formula/c/castor.rb
@@ -1,8 +1,8 @@
 class Castor < Formula
   desc "DX-oriented task runner and command launcher built in PHP"
   homepage "https://castor.jolicode.com/"
-  url "https://github.com/jolicode/castor/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "e1bf8848cb01ba96d5229fd48ec6621167edf79a851c8c9ecf9e3ce82e234d0b"
+  url "https://github.com/jolicode/castor/archive/refs/tags/v1.8.1.tar.gz"
+  sha256 "e5bd22385a989523ac11a6c33a1f01e1cb211a96e09e23a446e250114d547883"
   license "MIT"
 
   bottle do
