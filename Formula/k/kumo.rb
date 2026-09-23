8337e6721b1ef420d7f8146a1ceba6ad7ddd74ab
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 07:37:51 +0000

    kumo 0.30.0

--- Formula/k/kumo.rb
+++ Formula/k/kumo.rb
@@ -1,8 +1,8 @@
 class Kumo < Formula
   desc "Lightweight AWS service emulator written in Go"
   homepage "https://github.com/sivchari/kumo"
-  url "https://github.com/sivchari/kumo/archive/refs/tags/v0.29.0.tar.gz"
-  sha256 "53bdf4c857eb6e37330a652a6c3074ebcf739b1a1f6a7a15611867a45d812457"
+  url "https://github.com/sivchari/kumo/archive/refs/tags/v0.30.0.tar.gz"
+  sha256 "a9ccd8872e68032a7a6071fe7c3230d78159262af30a34f3206e924e59c685eb"
   license "MIT"
   head "https://github.com/sivchari/kumo.git", branch: "main"
 
