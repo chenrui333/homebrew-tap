d0c2b9c7c5da288cf0a4e26c2a3c86f2121ca22d
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 07:41:29 +0000

    ministack 1.5.15

--- Formula/m/ministack.rb
+++ Formula/m/ministack.rb
@@ -3,8 +3,8 @@ class Ministack < Formula
 
   desc "Local AWS service emulator and LocalStack replacement"
   homepage "https://github.com/ministackorg/ministack"
-  url "https://files.pythonhosted.org/packages/d4/f0/f2e58a3e1502a0949cd02e358d9de5b9fed4549f9aafe98cb819c6f7b341/ministack-1.5.8.tar.gz"
-  sha256 "615b29041e5dedd3b506a7614dbd428a1842badcfcfe62e3d8f9059682a5840d"
+  url "https://files.pythonhosted.org/packages/bf/a1/4eb68e76d9d813cd5a589154a7a178376309cbefddbe5c5344460c8a8b9f/ministack-1.5.15.tar.gz"
+  sha256 "947202b704082a8e7e955f28ff3bc34501c4754ba0213e52e375823227f469d4"
   license "MIT"
   head "https://github.com/ministackorg/ministack.git", branch: "main"
 
@@ -91,8 +91,8 @@ class Ministack < Formula
   end
 
   resource "urllib3" do
-    url "https://files.pythonhosted.org/packages/53/0c/06f8b233b8fd13b9e5ee11424ef85419ba0d8ba0b3138bf360be2ff56953/urllib3-2.7.0.tar.gz"
-    sha256 "231e0ec3b63ceb14667c67be60f2f2c40a518cb38b03af60abc813da26505f4c"
+    url "https://files.pythonhosted.org/packages/e3/05/b17359e1cefb4f909b5e40b1b90a496d987258916dbbf88e842c729f510e/urllib3-2.8.0.tar.gz"
+    sha256 "63bf2ead4c879426ebf22ef2a781eeb4aa3b4ae798a0435506f8687fd5bb9b63"
   end
 
   resource "wsproto" do
