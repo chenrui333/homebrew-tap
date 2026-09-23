b4743137d903e69a387f488c5ecbef6b253bd4fd
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-23 07:40:35 +0000

    mcp-remote 0.14.3
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/m/mcp-remote.rb
+++ Formula/m/mcp-remote.rb
@@ -1,8 +1,8 @@
 class McpRemote < Formula
   desc "Bridge stdio-only MCP clients to remote servers with auth"
   homepage "https://github.com/geelen/mcp-remote"
-  url "https://registry.npmjs.org/mcp-remote/-/mcp-remote-0.8.3.tgz"
-  sha256 "c9162084cf9d6e2c9e9c31da483d57dce90a45fa7dc95042a02718f7ad94917c"
+  url "https://registry.npmjs.org/mcp-remote/-/mcp-remote-0.14.3.tgz"
+  sha256 "f4ab0e33b38b24fff6a8b3234f9d683e47995ca44f3186481716d444836f7254"
   license "MIT"
 
   bottle do
@@ -18,7 +18,9 @@ class McpRemote < Formula
   end
 
   test do
-    output = shell_output("#{bin}/mcp-remote https://example.com/v1/sse/stream 2>&1", 1)
-    assert_match "Streamable HTTP error: Error POSTing to endpoint", output
+    assert_match version.to_s, shell_output("#{bin}/mcp-remote --version")
+
+    output = shell_output("#{bin}/mcp-remote 2>&1", 1)
+    assert_match "Usage: mcp-remote", output
   end
 end
