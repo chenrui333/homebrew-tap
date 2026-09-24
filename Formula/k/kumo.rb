e4f78b7a46d75115864988e29f60037e6927d9df
Rui Chen <rui@chenrui.dev>
2026-09-24 15:47:36 -0400

    kumo 0.30.0 (#11166)
    * kumo 0.30.0
    
    * kumo: update 0.30.0 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/k/kumo.rb
+++ Formula/k/kumo.rb
@@ -1,18 +1,17 @@
 class Kumo < Formula
   desc "Lightweight AWS service emulator written in Go"
   homepage "https://github.com/sivchari/kumo"
-  url "https://github.com/sivchari/kumo/archive/refs/tags/v0.29.0.tar.gz"
-  sha256 "53bdf4c857eb6e37330a652a6c3074ebcf739b1a1f6a7a15611867a45d812457"
+  url "https://github.com/sivchari/kumo/archive/refs/tags/v0.30.0.tar.gz"
+  sha256 "a9ccd8872e68032a7a6071fe7c3230d78159262af30a34f3206e924e59c685eb"
   license "MIT"
   head "https://github.com/sivchari/kumo.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7ba873052a8450f190f55c89607a69755e39602bfd7cbb9b536385fd82aecfde"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7ba873052a8450f190f55c89607a69755e39602bfd7cbb9b536385fd82aecfde"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7ba873052a8450f190f55c89607a69755e39602bfd7cbb9b536385fd82aecfde"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "1e9fbcb9da893c558cf29ff763b60f5dd6dac1f09d5ddb3f04adafa76ecf6525"
-    sha256 cellar: :any,                 x86_64_linux:  "6e60ed14b02519cf0131be5f73737449f6de35a9345e17dbbcaaa07440395717"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "28f90c4af8c5d580a266b4fa5407076744ac4bb7e55d7c21750a3ace5b6a7fff"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "28f90c4af8c5d580a266b4fa5407076744ac4bb7e55d7c21750a3ace5b6a7fff"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "13082d6337db450bdd8bffa81c4cb63906e27f891c53c9dddb25b44a71e65aba"
+    sha256 cellar: :any,                 x86_64_linux:  "85f9b50d6bdc7deeb77325389cd63a51a430d7e8b3ece5a808279b3bee4e8935"
   end
 
   depends_on "go" => :build
