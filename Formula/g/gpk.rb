97b05f46305e4f56dc861e7affac420fb9697044
Rui Chen <rui@chenrui.dev>
2026-09-24 05:04:00 -0400

    gpk 0.6.10 (#11148)
    * gpk 0.6.10
    
    * gpk: update 0.6.10 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/gpk.rb
+++ Formula/g/gpk.rb
@@ -1,18 +1,17 @@
 class Gpk < Formula
   desc "TUI dashboard that consolidates 36+ package managers into one interface"
   homepage "https://github.com/neur0map/glazepkg"
-  url "https://github.com/neur0map/glazepkg/archive/refs/tags/v0.6.8.tar.gz"
-  sha256 "42c6a369bd97a0e084ecb878dc52122f4610071c846cd3bd8209518f2c468a7e"
+  url "https://github.com/neur0map/glazepkg/archive/refs/tags/v0.6.10.tar.gz"
+  sha256 "0c7f708564e2e35613161ebba7ae9c980493cce667c0a5f8946ace75eb08c100"
   license "GPL-3.0-only"
   head "https://github.com/neur0map/glazepkg.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7fd2c6218bd76faff0d8a3f5574de9340d78ff64fff2314bae57939bd4569ca7"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7fd2c6218bd76faff0d8a3f5574de9340d78ff64fff2314bae57939bd4569ca7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7fd2c6218bd76faff0d8a3f5574de9340d78ff64fff2314bae57939bd4569ca7"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f6e557f14d7a6eb159a009517599cf9c129ca7ecf48f0cfeca7d1b0ecfc7ab8f"
-    sha256 cellar: :any,                 x86_64_linux:  "83c4ccbf134a8914d40730fe14c1ed787dc9080a17a2e4ab35af52ddf715828f"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "36024bc2367399e26b776e32489529505f69316557088daa84f42befba0e1d75"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "36024bc2367399e26b776e32489529505f69316557088daa84f42befba0e1d75"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "87ae2d50e84e2abc3a23cc3693bd364f81f635ce5e618b4db9a076f0cde036d7"
+    sha256 cellar: :any,                 x86_64_linux:  "a93fba1f9522d591ebf3dc37cee11a40c30d140eabd256f0ad2a280057014b07"
   end
 
   depends_on "go" => :build
