a03cdf7c91c017a65b24588197ad581c1cd5adb1
Rui Chen <rui@chenrui.dev>
2026-09-24 10:45:46 -0400

    codespelunker 3.2.0 (#11116)
    * codespelunker 3.2.0
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * codespelunker: update 3.2.0 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/codespelunker.rb
+++ Formula/c/codespelunker.rb
@@ -1,18 +1,17 @@
 class Codespelunker < Formula
   desc "Command-line codespelunker or code search"
   homepage "https://github.com/boyter/cs"
-  url "https://github.com/boyter/cs/archive/refs/tags/v3.1.0.tar.gz"
-  sha256 "1fb48991eea45067386309abc08dad91e4ff8077178130162902953f88100a34"
+  url "https://github.com/boyter/cs/archive/refs/tags/v3.2.0.tar.gz"
+  sha256 "faad9f1cdae00e4093d5750cfbe094b8b5c41650e8f439c5035c4b5f292eb45b"
   license any_of: ["MIT", "Unlicense"]
   head "https://github.com/boyter/cs.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d3e88dacd07849e7f0dd9e3fd86763c21e8fdf8e150d7989adf53c199e32a02c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d3e88dacd07849e7f0dd9e3fd86763c21e8fdf8e150d7989adf53c199e32a02c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d3e88dacd07849e7f0dd9e3fd86763c21e8fdf8e150d7989adf53c199e32a02c"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "b3ca2a4d1cd369f1c0731024a10624bd320403086f9d2a388bcbf191c86f08ec"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "117d68bd97296e1bdb849d43c9fd49eca73046c655f792e23177dc6d40e02598"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6cc4b6dbd1b7d464d6431326eb1742f79708e5e180831a632f622609761f281e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6cc4b6dbd1b7d464d6431326eb1742f79708e5e180831a632f622609761f281e"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "57088094fba032cb54ddd8b49ab9322d9100cf2262c21e30db605929ebe900e6"
+    sha256 cellar: :any,                 x86_64_linux:  "b547b85f71b171676d0e54c412270a263d2c7e491674c53837ef143db9fc040d"
   end
 
   depends_on "go" => :build
