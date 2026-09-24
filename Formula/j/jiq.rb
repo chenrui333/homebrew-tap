701f0597955147e35e5cd062259c8aa5a145c4de
Rui Chen <rui@chenrui.dev>
2026-09-24 08:30:13 -0400

    jiq 3.35.0 (#11161)
    * jiq 3.35.0
    
    * jiq: update 3.35.0 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/j/jiq.rb
+++ Formula/j/jiq.rb
@@ -1,17 +1,16 @@
 class Jiq < Formula
   desc "Interactive JSON query tool with real-time output and AI assistance"
   homepage "https://github.com/bellicose100xp/jiq"
-  url "https://github.com/bellicose100xp/jiq/archive/refs/tags/v3.34.0.tar.gz"
-  sha256 "0623a1240ae1b8de2ec96aee281ad147af476289b200d0761bdf0d6010f3f76b"
+  url "https://github.com/bellicose100xp/jiq/archive/refs/tags/v3.35.0.tar.gz"
+  sha256 "3ddb12971a6a15010c8f74f5b08a7e3587967f0f81eaee2a156df5fe910475b1"
   license "Apache-2.0"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7320a8f1d00f0ce8b25bc5ad945739a2810fd53d5b4310b3c0727203cb94aa2f"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3da2e1521f26716e025254664f57e6c3fdac1a78ca881035faf281b435aa358a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "14e55e1f5493ada0ce7722250f2c27b226b5023590d7c94f3ab06c34e4167645"
-    sha256 cellar: :any,                 arm64_linux:   "87fae827e9312fac1112b573ebe9972c23db7cb973ae64474c846823e6b7b08a"
-    sha256 cellar: :any,                 x86_64_linux:  "c7f656c70075123644c3d78669c4de1a215bcff825e4f4db6bf07a1807e67948"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "da9029426d951f99ace12b53a6d1182b769dd0dcf928efdd7902d4682bdac7ce"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bfc5fa664a9832a4f2afd8579b5f9f4620e5b76f2ca73230657a3c299d858fc9"
+    sha256 cellar: :any,                 arm64_linux:   "4e8230a7f985ff399a36dd3db04dfc1f8ef696be152a4bef2de416c80b7ffee7"
+    sha256 cellar: :any,                 x86_64_linux:  "a729a1bb740878aaea7efce4860d7def4eac2fab91078ddcd6b7c079dca1c624"
   end
 
   depends_on "rust" => :build
