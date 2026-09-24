54ad4278cd287e50d4d69dbc64fecd362873f998
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 08:23:34 +0000

    flow-network: update 0.3.0 bottle.

--- Formula/f/flow-network.rb
+++ Formula/f/flow-network.rb
@@ -8,11 +8,10 @@ class FlowNetwork < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ef6578a9c9b67c89d39fd25835060b7ea249c8706ab3c0b097830e50d5dd3565"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6cbeacc07b9d2ba51af223990d36e5717a99f0e9f654f4bbbbce39d1241bc106"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "97f327e4f2c0fbc33024d38cd0b0c34181e6a713c162a3a3e890a994e9edc43f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "29d05a1482151de68fde3468eca1d03e44cc8087408e48e3881ecca47820d822"
-    sha256 cellar: :any,                 x86_64_linux:  "a1990ff8d95244e078a1e6140be1f7cfd9362c3c713053959784be5454442ad2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c2bae4f883108b087111e062527a4a7ae1e8ee0ad259cb5f8a0cd50ce2a0f912"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7b49f48629ef7977c0b00b8862486e185a5608ceb4a2470ac7e157d9cf7a9e8c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "03fc4f53b8727a940389e6bddc4c045dc5c7e9b22b5a3b4cbb03000ecb73335d"
+    sha256 cellar: :any,                 x86_64_linux:  "f245d1f80118b75f3dbafe4c9db09fbf9004324bc200b01cb8b1765cd6fc5b22"
   end
 
   depends_on "go" => :build
