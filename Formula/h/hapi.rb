f8a96c9652afe101096184355d62ac14bf2396e4
Rui Chen <rui@chenrui.dev>
2026-09-24 05:38:52 -0400

    hapi 0.30.7 (#11153)
    * hapi 0.30.7
    
    * hapi: update 0.30.7 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/h/hapi.rb
+++ Formula/h/hapi.rb
@@ -1,17 +1,16 @@
 class Hapi < Formula
   desc "Agentic coding - access coding agent anywhere"
   homepage "https://github.com/tiann/hapi"
-  url "https://registry.npmjs.org/@twsxtd/hapi/-/hapi-0.29.0.tgz"
-  sha256 "d5e59110d6fa217f945148d6f773f30810974afc9da57092dc01757eda280e92"
+  url "https://registry.npmjs.org/@twsxtd/hapi/-/hapi-0.30.7.tgz"
+  sha256 "a00bcf85385765d09c953e8b121cac4dc79059e7505352bb36ad5d40492a9395"
   license "MIT"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "dae8f85d7c6afc56a584edea1399a72161d7ed27bc5ba9d1b27ac791fe69f123"
-    sha256                               arm64_sequoia: "dae8f85d7c6afc56a584edea1399a72161d7ed27bc5ba9d1b27ac791fe69f123"
-    sha256                               arm64_sonoma:  "dae8f85d7c6afc56a584edea1399a72161d7ed27bc5ba9d1b27ac791fe69f123"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "3e77d32405acfb624e11e84411aa13457bdfa6cec07aec44f33341261b9ab5c4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2c6edf45f758242a6d73dade0dd8636abe1f8a34b42c29af92832eb4c83a8bac"
+    sha256                               arm64_tahoe:   "4b1c562356ca28d8207cfd1520de7b27600b467ca392ba58106c8871e07c14d8"
+    sha256                               arm64_sequoia: "4b1c562356ca28d8207cfd1520de7b27600b467ca392ba58106c8871e07c14d8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "5cf1282b1dcba9ae5032b9c80dc04551548fe7509f35e4cfb10cc4661d9586ab"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "746e448a57a546399b7c0c281851184d3123a6f08795d751b4ec8dca24f1ef33"
   end
 
   depends_on "node"
