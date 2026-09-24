17e2ebc00aaae3fba2099ff0b36a7de7d33537f1
Rui Chen <rui@chenrui.dev>
2026-09-24 13:24:01 -0400

    flipt 2.13.0 (#11129)
    * flipt 2.13.0
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * flipt: update 2.13.0 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/f/flipt.rb
+++ Formula/f/flipt.rb
@@ -1,19 +1,18 @@
 class Flipt < Formula
   desc "Enterprise-ready, Git native feature management solution"
   homepage "https://flipt.io/"
-  url "https://github.com/flipt-io/flipt/archive/refs/tags/v2.11.0.tar.gz"
-  sha256 "772b6cbb2c61cdc312977476fa04b08f52d688650dc52dd726d90ffec6512ff5"
+  url "https://github.com/flipt-io/flipt/archive/refs/tags/v2.13.0.tar.gz"
+  sha256 "9896af34c35067ab66dfc7df0b39b6675c1704b1671379d968e7cfd21192b90a"
   # Fair Core License, Version 1.0, with a future MIT license.
   license :cannot_represent
   head "https://github.com/flipt-io/flipt.git", branch: "v2"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7a4adf5b3050d4996af068e42a902ada600a230abf664ada8469d7232d19834c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0750e474e2d66fbe5e78b1f54305035b24ee3cb184a762860cbaa798b1460a87"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "521effa8c28f63023b4c4b579fb64fa07eb885448f45b4d4e3c9e6f48330d3d1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "e2ca48af5785274ace7c30799a821eb55c6e6d110cca870db86cbf533391ed13"
-    sha256 cellar: :any,                 x86_64_linux:  "97a3ad460646ea7a833d0f67ce2a71e6359f83d650da99b4c1e900ebac96cf7e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "5e3bce9bfd3e709a907619454f0ec95d426425b1e3dc90a6265f0fda6a093f30"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "17561cdcbfbcd8e5e992b882a36da271f316ac8f6df9fd746d8830ea3573fdb5"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "7ec82f9be717c96e5c5b45c9cacc24b6593de66781f4cc0063939422d2660928"
+    sha256 cellar: :any,                 x86_64_linux:  "1c51ced713b777f72df9740080cd6aca7d06ed2c4d46d689dd2fa94d4e017b35"
   end
 
   depends_on "go" => :build
