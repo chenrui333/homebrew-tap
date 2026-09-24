3ac1bff4e071925d762c23727e38e67a4d61d3fb
Rui Chen <rui@chenrui.dev>
2026-09-24 06:05:26 -0400

    hauler 2.1.1 (#11156)
    * hauler 2.1.1
    
    * hauler: update 2.1.1 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/h/hauler.rb
+++ Formula/h/hauler.rb
@@ -1,18 +1,17 @@
 class Hauler < Formula
   desc "Airgap Swiss Army Knife"
   homepage "https://docs.hauler.dev/docs/intro"
-  url "https://github.com/hauler-dev/hauler/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "d9d3825979a496031ba36e49ba739e4cd32abf1e6683f2109e8eb2dbd8215acb"
+  url "https://github.com/hauler-dev/hauler/archive/refs/tags/v2.1.1.tar.gz"
+  sha256 "d947d36b79ab37ad505e17d7bb88a86751effcb3cb05b2d844ffefc092edeb78"
   license "Apache-2.0"
   head "https://github.com/hauler-dev/hauler.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "7e0c95e986e5de7b6ceedb672e65bb5d6ee183bd358c27682e28b67f4ff06102"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e85fc76583cb26df5cb2cca20dba7a776422d5729dc1c1933318a83d262fbdc6"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "41990bbc9147b0ffbb39175c95a879cc7b4135868b8f81ca9a43c6edf54a70d1"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "6de61221cb304b65b540b0d5d1c081320197b5dc02c50d8db79c215ff71b98dd"
-    sha256 cellar: :any,                 x86_64_linux:  "ea9e30a69ceeedd9ec5bdb1c9a6aa91ea4c28a092a0b3445b40960159abaa4cc"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "4356907544d5a2db432367c79d48f74579c8b69bd2ad1d5205e5ad792eff2d87"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "207d882f32c38599a7275a669fad9b4798278ad86f88d1c1a71c638666731b40"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "58a3a22a76295c9a47660cf8323eec4e1d40a40d9f8a84b66d1764bf54e49cb3"
+    sha256 cellar: :any,                 x86_64_linux:  "b4f8136ce59e50e8c4a9093d25e17c63b146d0359789d7183cc7c1b5701f00e7"
   end
 
   depends_on "go" => :build
