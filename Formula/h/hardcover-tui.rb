4db54b555f52954cfa4f1dd234335a9e35791885
Rui Chen <rui@chenrui.dev>
2026-09-24 05:54:59 -0400

    hardcover-tui 1.2.0 (#11155)
    * hardcover-tui 1.2.0
    
    * hardcover-tui: update 1.2.0 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/h/hardcover-tui.rb
+++ Formula/h/hardcover-tui.rb
@@ -1,18 +1,17 @@
 class HardcoverTui < Formula
   desc "Terminal UI client for Hardcover.app"
   homepage "https://github.com/NotMugil/hardcover-tui"
-  url "https://github.com/NotMugil/hardcover-tui/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "cfcccf491963e3dbc7edf84ce67a7bc90188ca7ef6387cbe725e12910becabfe"
+  url "https://github.com/NotMugil/hardcover-tui/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "3c42ef168b4cfe70f9c51726c2e2185156c9ee1e1b909dfcaee2dbcac218102d"
   license "AGPL-3.0-only"
   head "https://github.com/NotMugil/hardcover-tui.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c8e17f15b48c743f17d738f8fe07a08f20ecd5781523c21daececf831130660a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c8e17f15b48c743f17d738f8fe07a08f20ecd5781523c21daececf831130660a"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c8e17f15b48c743f17d738f8fe07a08f20ecd5781523c21daececf831130660a"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "ed292e51c883af22d15d2f79c198d548025bc558afdba902d0ae5cc44ce7bfde"
-    sha256 cellar: :any,                 x86_64_linux:  "147d59994788166a2ae761c199bd52d0e1d3707f1396837a979db894dd5d165b"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "37b12d5f1d576663ae49fa9899a589ca06bbb6bd80995b1a4d6f6645df1df8d7"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "37b12d5f1d576663ae49fa9899a589ca06bbb6bd80995b1a4d6f6645df1df8d7"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "d9ea7dbca77d2ff22ffdd183350d294ad371cbb2d134b9ce5f167aaf027ba6b6"
+    sha256 cellar: :any,                 x86_64_linux:  "c3820e0dc82067de4c4f604256a52c93a4e1011405c05ff83f08e5929e7d5922"
   end
 
   depends_on "go" => :build
