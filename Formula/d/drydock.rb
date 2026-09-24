759e6bef4d8c7d735d09cebb02fbe3cc085dfdf6
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 15:45:28 +0000

    drydock: update 1.1.4 bottle.

--- Formula/d/drydock.rb
+++ Formula/d/drydock.rb
@@ -8,11 +8,10 @@ class Drydock < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c9b8f40a87a456416453bc78eaf4dae87117b65e3e5be5b0a5ee9a31ab8c1bf6"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "499991368be498be31d2dde551ac605c1a6dc67fb724406ec4595e54e9ffc026"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d41a0678651d0b982beb7659b09f4d709766a3509a321c7da6213fa2caae9b3f"
-    sha256 cellar: :any,                 arm64_linux:   "c572f199086e26ce7439fe9271401a2af7e418cb262626e7c3c2650ccfdbc8f9"
-    sha256 cellar: :any,                 x86_64_linux:  "3e16d95b832333cf1fd016f7dde07c5cfb003874f8c1c9d767c24265023efebf"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0c0e53d0bbb75bb81fdb68504928b0f8b27144df9e7c58872e4a64704c31c8d6"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e3bd8954742575254de0eb9ff901e5831b30ffb2ad8450f8814e8eb8ea231d44"
+    sha256 cellar: :any,                 arm64_linux:   "2907c24780aa2917392ea39c183fd5ff95659d2195498f2b6214a8a58fb98af6"
+    sha256 cellar: :any,                 x86_64_linux:  "65e99c6b1577a7374149a176fad7e17d94c6103820430e3ce3ffdd0b47c0a31f"
   end
 
   depends_on "rust" => :build
