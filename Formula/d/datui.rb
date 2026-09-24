135c084289beb309be8baf7d4e305cf423189042
Rui Chen <rui@chenrui.dev>
2026-09-24 11:15:49 -0400

    datui 0.3.2 (#11119)
    * datui 0.3.2
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * datui: update 0.3.2 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/d/datui.rb
+++ Formula/d/datui.rb
@@ -1,15 +1,14 @@
 class Datui < Formula
   desc "Data exploration in the terminal"
   homepage "https://derekwisong.github.io/datui/"
-  url "https://github.com/derekwisong/datui/archive/refs/tags/v0.2.56.tar.gz"
-  sha256 "5e472338cb9cc29b6224007edd29c577bc1d422200d2970200b415df06c09a5e"
+  url "https://github.com/derekwisong/datui/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "82e709e755511b1c97330c77cbf6cb337914ce3c8c76a5a939cc0e2881082b79"
   license "MIT"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6bdda94957dbb8eb197f1df24c8d28f80c9522a6b0d5657d8ca4bd6040fb1b27"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "984a20c24578f20dff53367a1e3d5a59f6ffd1c9fbb4e258209f4b3cf5e0e5f7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "462d46dc1676d7bf9162d1918095f2384ba2a2a9b213e3f317c96403da600dd0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d5ad253254273cc518023173336b3fcc38ad0f6df3b459895d170ee2ea6d5350"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fea4f566fdea0cd3229a50418ce5ff407ee7edfe43887ecea8286b239d0294a8"
   end
 
   depends_on "pkgconf" => :build
