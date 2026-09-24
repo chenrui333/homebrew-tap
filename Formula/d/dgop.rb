fceea391c04af2287cf947f2888ad47400df958e
Rui Chen <rui@chenrui.dev>
2026-09-24 11:34:22 -0400

    dgop 1.6.2 (#11121)
    * dgop 1.6.2
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * dgop: update 1.6.2 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/d/dgop.rb
+++ Formula/d/dgop.rb
@@ -1,15 +1,15 @@
 class Dgop < Formula
   desc "API & CLI for System & Process Monitoring"
   homepage "https://danklinux.com/"
-  url "https://github.com/AvengeMedia/dgop/archive/refs/tags/v1.6.0.tar.gz"
-  sha256 "ab97d4bd9483de47051f984ecf3f877d289c316f8a35a9c3d9e5cfaa73b7f2b9"
+  url "https://github.com/AvengeMedia/dgop/archive/refs/tags/v1.6.2.tar.gz"
+  sha256 "1ed7295052f4c368f4e07472c4645e68a75b6d5f9b4cb5cb291a335a7f0d91e6"
   license "MIT"
   head "https://github.com/AvengeMedia/dgop.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "359532ca0d865bf2427a4164437fe579b10b665834896e7ff434aad055d052eb"
-    sha256 cellar: :any,                 x86_64_linux: "2d8cc358a102d2022345d898a54534e5c9c8376cb2c29e10bc44ecea9f5087a0"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "08029cc33b37529425d77bffea8bc3f7e1376aa664acc0037bc3695d0cc00b09"
+    sha256 cellar: :any,                 x86_64_linux: "22844d6ade04da5e5a3e1c6e40f040b93b8db5104b2800874a734759d21c3267"
   end
 
   depends_on "go" => :build
