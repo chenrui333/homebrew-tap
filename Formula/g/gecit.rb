957c628dde5522cfa862976e8000d0d081bd1d4c
Rui Chen <rui@chenrui.dev>
2026-09-24 13:54:32 -0400

    gecit 0.2.0 (#11133)
    * gecit 0.2.0
    
    Initialize generated BPF connection state before updating the map.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * gecit: update 0.2.0 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/gecit.rb
+++ Formula/g/gecit.rb
@@ -1,22 +1,30 @@
 class Gecit < Formula
   desc "DPI bypass tool using fake TLS ClientHello packets"
   homepage "https://github.com/boratanrikulu/gecit"
-  url "https://github.com/boratanrikulu/gecit/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "09f5dcdddcab4bab87c5e704f837060f41a8f99ba36959576e9ae16f530eae3b"
+  url "https://github.com/boratanrikulu/gecit/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "a31ebb62041a66eb01191585be3ef3e618803c3759b5ffd5767a285eab3ca020"
   license "GPL-3.0-only"
   head "https://github.com/boratanrikulu/gecit.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_linux:  "c87cbfa2add33c76d85a5f53ae539869b5665757c476eca41ef913064240caae"
-    sha256 cellar: :any,                 x86_64_linux: "2127c897c6c4ecb2e0c7a609ecfbc2c44b38eb52fd087a438ec8d5441ffa9035"
+    sha256 cellar: :any_skip_relocation, arm64_linux:  "9cfda35f778fc05e3f37ba4fa03612470d8fe35c743052bff4c780dd2b3eadf6"
+    sha256 cellar: :any,                 x86_64_linux: "b7bcf94cd870d78596efbecf36bfa18872e30fa9eb946caf05c2a6a753f6c77d"
   end
 
   depends_on "go" => :build
+  depends_on "libbpf" => :build
+  depends_on "llvm" => :build
   depends_on :linux
 
   def install
+    ENV.prepend_path "PATH", formula_opt_bin("llvm")
+    ENV.prepend_path "CPATH", formula_opt_include("libbpf")
+
+    system "go", "run", "github.com/boratanrikulu/gobee/cmd/gobee", "translate",
+           "--bindings-dir", "pkg/ebpf/bpf", "pkg/ebpf/bpf/src"
+    inreplace "pkg/ebpf/bpf/src/sockops.bpf.c", "struct ConnState state;", "struct ConnState state = {0};"
+    system "make", "-C", "pkg/ebpf/bpf/src", "bpf-all"
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/gecit"
   end
 
