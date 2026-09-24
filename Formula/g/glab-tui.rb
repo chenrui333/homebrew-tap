5686c1fee9a56c0f85bc4e636c1c071a6955ad2b
Rui Chen <rui@chenrui.dev>
2026-09-24 15:05:55 -0400

    glab-tui 0.9.1 (#11142)
    * glab-tui 0.9.1
    
    * glab-tui: update 0.9.1 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/glab-tui.rb
+++ Formula/g/glab-tui.rb
@@ -1,18 +1,17 @@
 class GlabTui < Formula
   desc "Terminal interface for GitLab and GitHub"
   homepage "https://github.com/rcieri/glab-tui"
-  url "https://github.com/rcieri/glab-tui/archive/refs/tags/v0.9.0.tar.gz"
-  sha256 "ec80641f6ed8429e079c727b861a72709a2bf542fc82946c3e59032799c8c3bb"
+  url "https://github.com/rcieri/glab-tui/archive/refs/tags/v0.9.1.tar.gz"
+  sha256 "d13a33eaa8729e127ba0eb83491a65cd1161cde89d37d9ae7273171f2d8ae7a1"
   license "MIT"
   head "https://github.com/rcieri/glab-tui.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "713f9e223bd0eef3f7d2ec06c94b0a1419a0f712d44f6ec9ccba33650f9a69f5"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "79e0b0706109334c93e9776f1ed286b065738f01a02335eda1ae087d6fe55f60"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d8fc71002b148ce34d1959468e3faaefa82c820eaa18a844432f3d685753cf26"
-    sha256 cellar: :any,                 arm64_linux:   "efd14e7aeb70eabe40e04ebc6ba828cf3833150b953d83ab39d65fa3f114b0a3"
-    sha256 cellar: :any,                 x86_64_linux:  "a1f7d49e77575c8d4629ea4ab6557f5cb8b82a732bbee3fa208fd4153c48857c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c4ca88348ba1df89f8d1609b6d20bf7aa8806d04f6e30a6dc41aa02070d5acd3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "0453032092ce8e3ec8b63b2b417613373bd1467f4b639f4939ea4cb31a4aeb98"
+    sha256 cellar: :any,                 arm64_linux:   "f277c1f8b262d985fa40f497f596c2fdd27bec4b049921de8d7b5cc9c5028bf8"
+    sha256 cellar: :any,                 x86_64_linux:  "acb835fe4b24c66b9d45ef0f4df12a5c198735526add36bb3af13cd5c000b180"
   end
 
   depends_on "rust" => :build
