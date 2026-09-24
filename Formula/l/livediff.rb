5afa1de22622ac6ee25c45c7c37ff1e0b1316a9d
Rui Chen <rui@chenrui.dev>
2026-09-24 16:28:52 -0400

    livediff 3.4.0 (#11170)
    * livediff 3.4.0
    
    * livediff: update 3.4.0 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/l/livediff.rb
+++ Formula/l/livediff.rb
@@ -1,18 +1,17 @@
 class Livediff < Formula
   desc "Watch file differences in the terminal"
   homepage "https://github.com/SoCkEt7/Livediff"
-  url "https://github.com/SoCkEt7/Livediff/archive/refs/tags/v3.2.0.tar.gz"
-  sha256 "a813b9df67351cb4e401cdd6d483b907ad590f345a94fdd876e1c46e998df16e"
+  url "https://github.com/SoCkEt7/Livediff/archive/refs/tags/v3.4.0.tar.gz"
+  sha256 "41b3d22f646cd16beadd4ff72868f76badf561f93c9996329c30ef6a40d576d7"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://github.com/SoCkEt7/Livediff.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b3c76b78a3e3e929a45147d206f4bc34e4c0da04057ab7cfdb825f71f6724e2c"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7077c3d0da783e8b83aaa16d2c10f3aecfa8dee97b3cf5931c5b01064320b8f3"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c11de5c25eb45ce8c77b8ac5015d587c13eb6435095ee6d81ca43b915b9b9e83"
-    sha256 cellar: :any,                 arm64_linux:   "2f3b612bfd1c27f3f9b0cab767dcdd6e5555266cfadb0dfc44c72bed24a7e95c"
-    sha256 cellar: :any,                 x86_64_linux:  "86be01a7e8afd53043bf32f278b5142dab5825e8ba008949650454bd3a1e4821"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1d784f65dc2edac108f4994b4b04285e8020c787deb5186703b6590ed03b383a"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d56ba68f87c7974743dab6532f9bc8eba3ae0522f4d532cb3f9905f841b578a7"
+    sha256 cellar: :any,                 arm64_linux:   "abce4209099f2d4cad6636bfb3f942525de580b75c59a0a2dba290c10d37fe15"
+    sha256 cellar: :any,                 x86_64_linux:  "50ec9aac193d1c46f24d180ab7d380bd86a7ec96ee248f25cdf56721f2a3990b"
   end
 
   depends_on "rust" => :build
