45e66233dc732083d3ec6b53c95d42afbe2b8a68
Rui Chen <rui@chenrui.dev>
2026-09-24 04:28:39 -0400

    flow-network 0.3.0 (#11131)
    * flow-network 0.3.0
    
    * flow-network: update 0.3.0 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/f/flow-network.rb
+++ Formula/f/flow-network.rb
@@ -1,18 +1,17 @@
 class FlowNetwork < Formula
   desc "Real-time network throughput dashboard"
   homepage "https://github.com/programmersd21/flow"
-  url "https://github.com/programmersd21/flow/archive/refs/tags/v0.2.5.tar.gz"
-  sha256 "be7b6dbfdcdaa393f0e86cddefb209a58228a172f6a2ff7cf63f67edaedb3514"
+  url "https://github.com/programmersd21/flow/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "b6bcd7afdaa290253e33d9196fdb2349dc044c6bfe3b6e6500d0a3df7c0b13e4"
   license "MIT"
   head "https://github.com/programmersd21/flow.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ef6578a9c9b67c89d39fd25835060b7ea249c8706ab3c0b097830e50d5dd3565"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "6cbeacc07b9d2ba51af223990d36e5717a99f0e9f654f4bbbbce39d1241bc106"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "97f327e4f2c0fbc33024d38cd0b0c34181e6a713c162a3a3e890a994e9edc43f"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "29d05a1482151de68fde3468eca1d03e44cc8087408e48e3881ecca47820d822"
-    sha256 cellar: :any,                 x86_64_linux:  "a1990ff8d95244e078a1e6140be1f7cfd9362c3c713053959784be5454442ad2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c2bae4f883108b087111e062527a4a7ae1e8ee0ad259cb5f8a0cd50ce2a0f912"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "7b49f48629ef7977c0b00b8862486e185a5608ceb4a2470ac7e157d9cf7a9e8c"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "03fc4f53b8727a940389e6bddc4c045dc5c7e9b22b5a3b4cbb03000ecb73335d"
+    sha256 cellar: :any,                 x86_64_linux:  "f245d1f80118b75f3dbafe4c9db09fbf9004324bc200b01cb8b1765cd6fc5b22"
   end
 
   depends_on "go" => :build
