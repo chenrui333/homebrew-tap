20cc7959c572f9046934611571ada6c2a82eabcf
Rui Chen <rui@chenrui.dev>
2026-09-24 09:22:56 -0400

    gecit 0.2.0
    Initialize generated BPF connection state before updating the map.
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- Formula/g/gecit.rb
+++ Formula/g/gecit.rb
@@ -1,8 +1,8 @@
 class Gecit < Formula
   desc "DPI bypass tool using fake TLS ClientHello packets"
   homepage "https://github.com/boratanrikulu/gecit"
-  url "https://github.com/boratanrikulu/gecit/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "09f5dcdddcab4bab87c5e704f837060f41a8f99ba36959576e9ae16f530eae3b"
+  url "https://github.com/boratanrikulu/gecit/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "a31ebb62041a66eb01191585be3ef3e618803c3759b5ffd5767a285eab3ca020"
   license "GPL-3.0-only"
   head "https://github.com/boratanrikulu/gecit.git", branch: "main"
 
@@ -14,9 +14,18 @@ class Gecit < Formula
   end
 
   depends_on "go" => :build
+  depends_on "libbpf" => :build
+  depends_on "llvm" => :build
   depends_on :linux
 
   def install
+    ENV.prepend_path "PATH", formula_opt_bin("llvm")
+    ENV.prepend_path "CPATH", formula_opt_include("libbpf")
+
+    system "go", "run", "github.com/boratanrikulu/gobee/cmd/gobee", "translate",
+           "--bindings-dir", "pkg/ebpf/bpf", "pkg/ebpf/bpf/src"
+    inreplace "pkg/ebpf/bpf/src/sockops.bpf.c", "struct ConnState state;", "struct ConnState state = {0};"
+    system "make", "-C", "pkg/ebpf/bpf/src", "bpf-all"
     system "go", "build", *std_go_args(ldflags: "-s -w"), "./cmd/gecit"
   end
 
