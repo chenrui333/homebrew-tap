d50f65c3f00922424280384a99c90f747c6422a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-24 15:09:19 +0000

    datui: update 0.3.2 bottle.

--- Formula/d/datui.rb
+++ Formula/d/datui.rb
@@ -7,9 +7,8 @@ class Datui < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "6bdda94957dbb8eb197f1df24c8d28f80c9522a6b0d5657d8ca4bd6040fb1b27"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "984a20c24578f20dff53367a1e3d5a59f6ffd1c9fbb4e258209f4b3cf5e0e5f7"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "462d46dc1676d7bf9162d1918095f2384ba2a2a9b213e3f317c96403da600dd0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d5ad253254273cc518023173336b3fcc38ad0f6df3b459895d170ee2ea6d5350"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fea4f566fdea0cd3229a50418ce5ff407ee7edfe43887ecea8286b239d0294a8"
   end
 
   depends_on "pkgconf" => :build
