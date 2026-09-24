7369567fb2dee3d01820d9635067765f8dc82519
Rui Chen <rui@chenrui.dev>
2026-09-24 10:23:43 -0400

    codebuff 1.0.688 (#11114)
    * codebuff 1.0.688
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * codebuff: update 1.0.688 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/c/codebuff.rb
+++ Formula/c/codebuff.rb
@@ -1,13 +1,13 @@
 class Codebuff < Formula
   desc "Generate code from the terminal"
   homepage "https://www.codebuff.com/"
-  url "https://registry.npmjs.org/codebuff/-/codebuff-1.0.686.tgz"
-  sha256 "4c62f5d552b99d1b0011ee0e37c6359560ce69f73b948006ac38de8a0e2c4775"
+  url "https://registry.npmjs.org/codebuff/-/codebuff-1.0.688.tgz"
+  sha256 "39b430fdb9c21de80d4c08d152a935abf5f83de14fb039880b9c2b669b228483"
   license "MIT"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, all: "9207ba6761cfda65d5417aa1a43627323be742c01cfe501ea03c5611a241a32b"
+    sha256 cellar: :any_skip_relocation, all: "769f102511e5405af434b4a08c6cdf0f570cf38de3891a09a032aad58a97d0fb"
   end
 
   depends_on "node"
