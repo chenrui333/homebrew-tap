281f4147a351db97a5b04fc5037dbaf7e0772faf
Rui Chen <rui@chenrui.dev>
2026-09-24 05:32:31 -0400

    gsty 0.1.19 (#11152)
    * gsty 0.1.19
    
    * gsty: update 0.1.19 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/g/gsty.rb
+++ Formula/g/gsty.rb
@@ -1,18 +1,17 @@
 class Gsty < Formula
   desc "Browse and apply Ghostty themes"
   homepage "https://github.com/tappunk/gsty"
-  url "https://github.com/tappunk/gsty/archive/refs/tags/v0.1.15.tar.gz"
-  sha256 "21ddee5de338678e46a37b15a38b01b4cc44655d19a30c10b07abb1d0cc3a920"
+  url "https://github.com/tappunk/gsty/archive/refs/tags/v0.1.19.tar.gz"
+  sha256 "baa2f0a7b4e6c12c26f39c6418561039b96719a9d3f542befdc3c08f5095c11c"
   license "MIT"
   head "https://github.com/tappunk/gsty.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "85409b7617c41f72b46f938d869e1d7c50b864097af800c0a801ff1b8f172022"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "de7c78fee6b58b9f3de659541bbc4f6fa10f6e2b548eb76340ac56036de03953"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "e59683ec92feb4b813cb3007dcba2f8af422e101e03bf6eda22db658a01eb559"
-    sha256 cellar: :any,                 arm64_linux:   "a060f5652329130e0fa54205a56278adf4c435fa2b1c3e82a4ae17f095644cf7"
-    sha256 cellar: :any,                 x86_64_linux:  "a926849a1787310774c1d7ed0d56c79f5255d7a50a3d711f66a7fade5cc618d3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "73d79d8900044ecb77f6f67accbc5bb5f718170d94f139cccfe72f72f9745b60"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "230f44e8e9bd9fccaab154e790802a9571198b1183520e63969b1c662a446570"
+    sha256 cellar: :any,                 arm64_linux:   "8c6f53ea5e59d02916bae1cb0baad486a352af9119badc318f1f926cefcc53b2"
+    sha256 cellar: :any,                 x86_64_linux:  "e81fc9a43d2f4d5fccc9dbf33c35f041b4f8cd73dc83c17a1ae4c9ad46948e37"
   end
 
   depends_on "rust" => :build
