f24e0cc1813303977f816024fd2e9364d61ddbc3
Rui Chen <rui@chenrui.dev>
2026-09-24 08:40:16 -0400

    justray 1.6.3 (#11162)
    * justray 1.6.3
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * justray: update 1.6.3 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/j/justray.rb
+++ Formula/j/justray.rb
@@ -1,22 +1,21 @@
 class Justray < Formula
   desc "Terminal VPN client"
   homepage "https://github.com/luynrs/justray"
-  url "https://github.com/luynrs/justray/archive/refs/tags/v1.4.5.tar.gz"
-  sha256 "0913901f21cf1a68fea7554fdb15f3bcec80cbf43f1583e293c9d564fc880dbb"
+  url "https://github.com/luynrs/justray/archive/refs/tags/v1.6.3.tar.gz"
+  sha256 "f85a7515f8986032402074e22d705ec2d4f800aea3a56736c47f6c6c0f85ab3c"
   license "GPL-3.0-only"
   head "https://github.com/luynrs/justray.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "559079e73fda99f270e5a51f492626b070c2cafe6e82b80a92b665509ac6795a"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4d10f9723906a02bc6f8746b61e69dbc10aa10051555aaa6899a59ddf3fe5e74"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3ed67bab9290ef51394a2fab0231bc1dcc054a3b117a4ea09b9e62c775909c97"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "8a44aad88f87c9d961a12b9b9c9a57d69057df1cf38328b7e6d2e9ed32ee7325"
-    sha256 cellar: :any,                 x86_64_linux:  "00befb2cd3b99aa59e76c7c6180559388b0e2299f3003270df4acbe46399804c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dc802536e4169c3dcaad8b13003b221407e48436e345512518cbedf5529d8285"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "111210c43a83e6fffc3ccedd7a863294d5526e088448e6d10c35b4ac11cb2f4d"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "321d42e8f8e4537e57cbcdf86c11379ff987c16201843f708db6b2ebf0c48054"
+    sha256 cellar: :any,                 x86_64_linux:  "9c80afb224be3a0225a6e0d0a1c60148bdc69f6745a8cd8195bdf346115a7045"
   end
 
-  # Match upstream release CI while sing-box relies on private HTTP/2 symbols.
-  depends_on "go@1.26" => :build
+  # Match upstream release CI; sing-box relies on private HTTP/2 symbols.
+  depends_on "go" => :build
 
   def install
     ldflags = "-s -w -X github.com/luynrs/justray/internal/version.Version=#{version}"
