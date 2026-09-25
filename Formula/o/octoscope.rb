e36e05a17a8042aa4b3bf35f1ceb6f12351143d0
Rui Chen <rui@chenrui.dev>
2026-09-25 07:41:39 -0400

    octoscope 0.35.0 (#11198)
    * octoscope 0.35.0
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * octoscope: update 0.35.0 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/o/octoscope.rb
+++ Formula/o/octoscope.rb
@@ -1,18 +1,17 @@
 class Octoscope < Formula
   desc "Terminal dashboard for your GitHub account"
   homepage "https://github.com/gfazioli/octoscope"
-  url "https://github.com/gfazioli/octoscope/archive/refs/tags/v0.31.0.tar.gz"
-  sha256 "1f2f50fc953ba88eceb5e93fd512e82cb459d835b4d4e92ed4bb094ce383d52d"
+  url "https://github.com/gfazioli/octoscope/archive/refs/tags/v0.35.0.tar.gz"
+  sha256 "7303a738f6f5ab9efbec3f4dc03d3d434513d1c45b50a997553ac2dd569fa4d4"
   license "MIT"
   head "https://github.com/gfazioli/octoscope.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a1013b6834ed19053f9194e865e56ab9cb33b4ff05faf0696d46ddb8fac5f265"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a1013b6834ed19053f9194e865e56ab9cb33b4ff05faf0696d46ddb8fac5f265"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "a1013b6834ed19053f9194e865e56ab9cb33b4ff05faf0696d46ddb8fac5f265"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "fc3ecce8c6805c0d0f4e86f61ed603dda1c5af44c6dffc2807be61265654972f"
-    sha256 cellar: :any,                 x86_64_linux:  "6aa07b53d9481d7084f0908a19e9be5ac1b5a26fc034b0b419f24ac83b8fa61e"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "a41287c12b15187c0b99d34fdfd1b9dc55dcb4588f6ca11e8742a029c4d02711"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a41287c12b15187c0b99d34fdfd1b9dc55dcb4588f6ca11e8742a029c4d02711"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "ae6ec77d3fa4733c378ffe19188a6b1b71a0c4c97331df998cd8e4e111b3541d"
+    sha256 cellar: :any,                 x86_64_linux:  "782038e3f3af5afff3e237dbf29bc91bf835b80b58e66f5b3a84c59fd2eca312"
   end
 
   depends_on "go" => :build
