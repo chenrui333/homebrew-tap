05e4b141a698d58cfdfadc02d84c07eabf632335
Rui Chen <rui@chenrui.dev>
2026-09-25 03:04:19 -0400

    netwatch 0.32.3 (#11192)
    * netwatch 0.32.3
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * netwatch: update 0.32.3 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/n/netwatch.rb
+++ Formula/n/netwatch.rb
@@ -1,18 +1,17 @@
 class Netwatch < Formula
   desc "Real time network diagnostics in your terminal"
   homepage "https://github.com/matthart1983/netwatch"
-  url "https://github.com/matthart1983/netwatch/archive/refs/tags/v0.30.0.tar.gz"
-  sha256 "95dfbbf1208806d383bf8309ff40ec29fc55e1081ecd7c41ee8f110c8aa32227"
+  url "https://github.com/matthart1983/netwatch/archive/refs/tags/v0.32.3.tar.gz"
+  sha256 "f91dfa39c0cf0dd721d5f8ec82bac46666c3ec2be1eb234b00b7524bc58122ba"
   license "MIT"
   head "https://github.com/matthart1983/netwatch.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "1e97172f180652bdad094b1656f9cbde5d724c6fd8dbf95f95abd0387a570ea8"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "20073256e0573f2cffd1da51b459566fd6ad4bc03ea745fb82932212821c550f"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "7052c45233e8c86a2187002c685e135c19cb33db8b89fda0696d9963305a2d6c"
-    sha256 cellar: :any,                 arm64_linux:   "5fd37acd45eb5d300f4ae92668ed453264c8e19a7fab319db5523524d569429e"
-    sha256 cellar: :any,                 x86_64_linux:  "497b3caac59da25b018295cba72535c0153703cedbc62c82faf731d7232fb4bd"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "898481fd20222c8c660f09d9c038af1164d4c561ba2677ab3310edbb907fcba8"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ef642fa32edf3cd9655c10d63a6da5be6913c55d7d578eb4c67cf14276cf6aa9"
+    sha256 cellar: :any,                 arm64_linux:   "248d44d65df0bec391dd3dd161fe102838f7890c393bbca72033568d2d6a100e"
+    sha256 cellar: :any,                 x86_64_linux:  "563451f49a5d3baa53c4ffac9a8e4fc44f911ee31d05d38a4c56c955b7fb4d01"
   end
 
   depends_on "rust" => :build
