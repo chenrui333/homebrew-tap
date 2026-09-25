65aab1b7a4dc588ef2c9d66fb3d1b001a22acdda
Rui Chen <rui@chenrui.dev>
2026-09-24 23:13:42 -0400

    mult 0.3.1 (#11186)
    * mult 0.3.1
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    
    * mult: update 0.3.1 bottle.
    
    ---------
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/m/mult.rb
+++ Formula/m/mult.rb
@@ -1,19 +1,17 @@
 class Mult < Formula
   desc "Run a command multiple times and glance at the outputs"
   homepage "https://github.com/dhth/mult"
-  url "https://github.com/dhth/mult/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "b8115600e51155a8b2f639c90a4f50c1a019b3551e80b9c9843a729fd711b453"
+  url "https://github.com/dhth/mult/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "73ecbe739b5f8bef3508f22641771818eed4e250564a9247e84c28190f293d43"
   license "MIT"
   head "https://github.com/dhth/mult.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "9b3b650a0e11d5713ca59fa6fd77ae7f182801f1807df7799cdb032cb244ce81"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "9b3b650a0e11d5713ca59fa6fd77ae7f182801f1807df7799cdb032cb244ce81"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "9b3b650a0e11d5713ca59fa6fd77ae7f182801f1807df7799cdb032cb244ce81"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "4ec0d6194a3529b102d5e7a5f0f49b58d052073febaf8cc0fcc278895c600574"
-    sha256 cellar: :any,                 x86_64_linux:  "c652832ed24cce40d9db29b584b93fef5b58c2395c2d213d8fdb1afdc960c42c"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "ab6c845794d8ec029fc62d8ea1233097e778ee66127fded81043d28a24630364"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "ab6c845794d8ec029fc62d8ea1233097e778ee66127fded81043d28a24630364"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "967bd2bf3839420614bdcc6ada9d299d8b5e2fc0ad5abfc6b86d19d546efab60"
+    sha256 cellar: :any,                 x86_64_linux:  "c1f175d1b8d04afcc74d7d5e27f2b9df926b34746903c68a8556aa3e73458c0b"
   end
 
   depends_on "go" => :build
@@ -25,44 +23,7 @@ class Mult < Formula
   test do
     # FIXME: Upstream does not expose a version command; replace this with a version assertion when available.
 
-    output = +""
-    require "pty"
-
-    PTY.spawn(bin/"mult", "-s", "-n", "2", "--", "sh", "-c", "printf 'hi\\n'") do |r, w, pid|
-      deadline = Time.now + 5
-      while Time.now < deadline
-        next unless r.wait_readable(0.2)
-
-        begin
-          output << r.read_nonblock(4096)
-        rescue IO::WaitReadable
-          next
-        rescue EOFError, Errno::EIO
-          break
-        end
-
-        break if output.include?("average time:")
-      end
-
-      w.write "q"
-      w.close
-
-      Timeout.timeout(5) do
-        loop do
-          output << r.read_nonblock(4096)
-        rescue IO::WaitReadable
-          r.wait_readable(0.2)
-          retry
-        rescue EOFError, Errno::EIO
-          break
-        end
-      ensure
-        Process.wait(pid)
-      end
-    end
-
-    assert_match "2 runs", output
-    assert_match "finished after", output
-    assert_match "average time:", output
+    output = shell_output("#{bin}/mult -n 1 -- true 2>&1", 1)
+    assert_match "invalid number of runs requested", output
   end
 end
