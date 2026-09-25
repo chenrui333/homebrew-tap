46e1c1afacb2500463d2a3c70862259121a5fb7a
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-25 07:40:59 +0000

    newsjack: update 0.1.19 bottle.

--- Formula/n/newsjack.rb
+++ Formula/n/newsjack.rb
@@ -7,11 +7,10 @@ class Newsjack < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "dcda400e7467f0167fb093df8dc2b514fef084ac3304397ec3f1063f913a9b9b"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "dcda400e7467f0167fb093df8dc2b514fef084ac3304397ec3f1063f913a9b9b"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dcda400e7467f0167fb093df8dc2b514fef084ac3304397ec3f1063f913a9b9b"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "84fc5b5d42bf5dc00a0c560dfe684eb3d16b1abea3a8f26d019cbda7473d2b53"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "34bd2070250c9a571fa22c475e292d2035e1440fb2e17b3e06b28a8dd345ae21"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2f1bff4714afbc81233fde4140356b805213679c51e2762510c431888d3eb117"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2f1bff4714afbc81233fde4140356b805213679c51e2762510c431888d3eb117"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "c538d5c379cf2ac224fb30203e7da1375ddf95e3aae91d6af8bcbfbb039c6e33"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "8c2561ba6cbf93204d62fbf08d2d80537bb21ebec2d6a2470c64e2ed2c158b1c"
   end
 
   depends_on "node"
