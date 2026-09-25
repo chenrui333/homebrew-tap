a29f8cc6d5508d56cbf203efb91791c382acf5cd
Rui Chen <rui@chenrui.dev>
2026-09-24 22:46:39 -0400

    mongodb-mcp-server 3.0.4 (#11184)
    * mongodb-mcp-server 3.0.4
    
    * mongodb-mcp-server: update 3.0.4 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/m/mongodb-mcp-server.rb
+++ Formula/m/mongodb-mcp-server.rb
@@ -1,17 +1,16 @@
 class MongodbMcpServer < Formula
   desc "MCP Server to connect to MongoDB databases and MongoDB Atlas Clusters"
   homepage "https://github.com/mongodb-js/mongodb-mcp-server"
-  url "https://registry.npmjs.org/mongodb-mcp-server/-/mongodb-mcp-server-2.1.1.tgz"
-  sha256 "cac3f1ff1492df0188e4e4d79655b438f7a5a8a2a1f4ebaff3e7261e116c5ee6"
+  url "https://registry.npmjs.org/mongodb-mcp-server/-/mongodb-mcp-server-3.0.4.tgz"
+  sha256 "5b528ead24bf5c999220c930d73f42ae8ef15e648fc4e864579b682847b401d9"
   license "Apache-2.0"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256                               arm64_tahoe:   "da8c82515e02d9f514fb5f0d66aeb47b896a7cec7ccf071e1e46ec4ef1fa68e4"
-    sha256                               arm64_sequoia: "da8c82515e02d9f514fb5f0d66aeb47b896a7cec7ccf071e1e46ec4ef1fa68e4"
-    sha256                               arm64_sonoma:  "da8c82515e02d9f514fb5f0d66aeb47b896a7cec7ccf071e1e46ec4ef1fa68e4"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "f6284c4ab5038c0d9185c7fc994cb59452f416d970fc2682055ca8e234cdea45"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "29aed6abe4ce81ee3d14c771d1cda27c372ad2bd18e3c913f039141ada8e0672"
+    sha256                               arm64_tahoe:   "66dd37fad68886e7b5650fd966765a13c23ed2bbf32273c92ab19314341f0679"
+    sha256                               arm64_sequoia: "0be7ea7c7218a6458211ab58dca1e7ec47123276c69efa0b57cf37ded97ed2c8"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "11a0d4d74beb80b69fd82c0313a491b03368526569b0fe618fd6ebc5ce471471"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "55ba87221971deeaf52828a28526958d6568d411bc44becf313ae9ecc16bb2a7"
   end
 
   depends_on "node"
