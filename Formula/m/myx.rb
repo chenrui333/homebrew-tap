0548ff117639272eacf60665df82320966f9da9a
Rui Chen <rui@chenrui.dev>
2026-09-25 01:16:18 -0400

    myx 0.5.0 (#11189)
    * myx 0.5.0
    
    * myx: update 0.5.0 bottle.
    
    ---------
    
    Co-authored-by: BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>

--- Formula/m/myx.rb
+++ Formula/m/myx.rb
@@ -1,18 +1,17 @@
 class Myx < Formula
   desc "Terminal Spotify player"
   homepage "https://github.com/HaseebKhalid1507/Myx"
-  url "https://github.com/HaseebKhalid1507/Myx/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "50a689fa07d9a4bea909e265351a5acfe7cbd7183d055543d4c1ee7c4cb220f4"
+  url "https://github.com/HaseebKhalid1507/Myx/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "42c48a29910a44bca6eef52692a7f3774c80ceffed2c11c7a6b541b8cca645e3"
   license "MIT"
   head "https://github.com/HaseebKhalid1507/Myx.git", branch: "main"
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8c0ec957e14e0ab4d9164a975d5c6eff58ad7534e81f2705c1bf88e447da77bb"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "582b9f08f0a97021131ab417054f09eba3a71cb2cea4da5426a1f82847111f0e"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fa11231120a51d4fb189d291cd17f0c1e2fe389e717848322a6fc91eaac4c0e0"
-    sha256 cellar: :any,                 arm64_linux:   "a609faa082543a24c1086beb54fc7a5639dfd199a16a9dc3c17a69a787796001"
-    sha256 cellar: :any,                 x86_64_linux:  "8271ea5c2bf7138abd45b0c7fcdd0e765031a77c06047e1ffe8a987bf0c27e44"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "b4b022236bb2d85e98183e605b9e7d219958cec404bd962bf98b5b4948236111"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "905d2898d25d9fcb05653e8af725011d1cdd6c8a9c0aefbdb36660eb13fe50fa"
+    sha256 cellar: :any,                 arm64_linux:   "ad67d7cd035215b654e79cba6db8faa86cb1633b7b15afeac9347cbcf055dde7"
+    sha256 cellar: :any,                 x86_64_linux:  "f1216ecfb05df07e96d9ae34dcca07669a1eeda2fc8348e98c45d5d61de2beba"
   end
 
   depends_on "pkgconf" => :build
