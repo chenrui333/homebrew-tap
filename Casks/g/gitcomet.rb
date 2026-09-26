e2a0d625a9f2c586611cdf4294d776ef37d24344
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 11:48:24 +0000

    gitcomet 0.2.6

--- Casks/g/gitcomet.rb
+++ Casks/g/gitcomet.rb
@@ -1,9 +1,9 @@
 cask "gitcomet" do
   arch arm: "arm64", intel: "x86_64"
 
-  version "0.2.5"
-  sha256 arm:   "8e6b46b641c5206c233534c723ddc6e127673b64a86654ac0f635d072e8a3255",
-         intel: "7205148bf1a8ca3f93a4ee4c348714b727522a73ccf747fce4847642c310e98b"
+  version "0.2.6"
+  sha256 arm:   "6321da7ea5f04d9f366afc6be7937608b79a54f6467e5a1e85abd8458759e4c9",
+         intel: "b7d87c334c4aa149c93e2189c76fe577b42c5ae40597c0767fc8494b92620875"
 
   url "https://github.com/Auto-Explore/GitComet/releases/download/v#{version}/gitcomet-v#{version}-macos-#{arch}.dmg"
   name "GitComet"
