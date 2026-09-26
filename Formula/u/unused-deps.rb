ce798011f0f2a80d5ad931c0d6a1a87dbc21d492
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-26 18:16:37 +0000

    unused-deps: update 10.1.0 bottle.

--- Formula/u/unused-deps.rb
+++ Formula/u/unused-deps.rb
@@ -8,11 +8,10 @@ class UnusedDeps < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/chenrui333/tap"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "d207ab9aec542d9600932a2fa849b07d5a36b8fea6fe2f8b35a7d022dffa5348"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d207ab9aec542d9600932a2fa849b07d5a36b8fea6fe2f8b35a7d022dffa5348"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d207ab9aec542d9600932a2fa849b07d5a36b8fea6fe2f8b35a7d022dffa5348"
-    sha256 cellar: :any_skip_relocation, arm64_linux:   "963a8f1462bce3a73bd946ea7bdeb2c5566dc1188f4fd9d0eb8786419c2f1154"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fccbedd8456a72f1e42ba3a3b79393c358e2ef6cd53499a3c22a86e385dd6d41"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "cc0cfff4fdf03628eafdfaa36a5ec17ea2a99bff7a6bd18d5fad7948927afe83"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "cc0cfff4fdf03628eafdfaa36a5ec17ea2a99bff7a6bd18d5fad7948927afe83"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "b3a0c00ac3d2bde8e7ba072926497e5fe7bf7a6921e91ea926f60faf9355da13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2ca7e9cf4d67fc7c9f395ca559ed6a07441be3269edd6241ab1abf749abc2baa"
   end
 
   depends_on "go" => :build
